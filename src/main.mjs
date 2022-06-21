f108894678da068afd31c83c8ccf8bdeafad8e66
minsoo.park <topangel777@gmail.com>
2022-06-21 11:11:55 +0900

    1642. Furthest Building You Can Reach - Elxir cant....

--- src/main.mjs
+++ src/main.mjs
@@ -1,27 +1,54 @@
-function minimumLengthEncoding (words) {
-  const filter = new Set(words)
-  for (const word of words) {
-    for (let i = 1; i < word.length; i += 1) {
-      filter.delete(word.substring(i))
-    }
-  }
-
+function findMaxDiffIndex (arr) {
+  let max = arr[0]
   let result = 0
-  for (const word of filter) {
-    result += word.length + 1
+  for (let i = 1; i < arr.length; i += 1) {
+    const val = arr[i]
+    if (max < val) {
+      max = val
+      result = i
+    }
   }
 
   return result
 }
 
+function furthestBuilding (heights, bricks, ladders) {
+  const maxHeap = []
+
+  for (let i = 1; i < heights.length; i += 1) {
+    const diff = heights[i] - heights[i - 1]
+    if (diff <= 0) {
+      continue
+    }
+
+    maxHeap.push(diff)
+    bricks -= diff
+    if (bricks < 0) {
+      const maxIndex = findMaxDiffIndex(maxHeap)
+      const max = maxHeap[maxIndex]
+      maxHeap.splice(maxIndex, 1)
+      bricks += max
+      ladders -= 1
+    }
+
+    if (ladders < 0) {
+      return i - 1
+    }
+  }
+
+  return heights.length - 1
+}
+
 async function main () {
   const inputs = [
-    ['time', 'me', 'bell'],
-    ['t']
+    { heights: [4, 2, 7, 6, 9, 14, 12], bricks: 5, leadders: 1 },
+    { heights: [4, 12, 2, 7, 3, 18, 20, 3, 19], bricks: 10, leadders: 2 },
+    { heights: [14, 3, 19, 3], bricks: 17, leadders: 0 },
+    { heights: [1, 5, 1, 2, 3, 4, 10000], bricks: 4, leadders: 1 }
   ]
 
   for (const input of inputs) {
-    const result = minimumLengthEncoding(input)
+    const result = furthestBuilding(input.heights, input.bricks, input.leadders)
     console.log(result)
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,67 @@
-use std::collections::HashSet;
+use std::collections::BinaryHeap;
 
 struct Solution {}
 impl Solution {
-    pub fn minimum_length_encoding(words: Vec<String>) -> i32 {
-        let mut filter = HashSet::<String>::new();
+    pub fn furthest_building(heights: Vec<i32>, bricks: i32, ladders: i32) -> i32 {
+        let mut heap = BinaryHeap::<i32>::new();
+        let mut bricks = bricks;
+        let mut ladders = ladders;
 
-        for word in &words {
-            filter.insert(word.clone());
-        }
+        for i in 1..heights.len() {
+            let diff = heights[i] - heights[i - 1];
+            if diff <= 0 {
+                continue;
+            }
 
-        for word in &words {
-            for i in 1..word.len() {
-                let sub = &word[i..];
-                filter.remove(sub);
+            heap.push(diff);
+            bricks -= diff;
+            if bricks < 0 {
+                let max = heap.pop().unwrap();
+                bricks += max;
+                ladders -= 1;
+            }
+
+            if ladders < 0 {
+                return (i as i32) - 1;
             }
         }
 
-        let mut result = 0usize;
-        for word in filter {
-            result += word.len() + 1;
-        }
-
-        result as i32
+        (heights.len() as i32) - 1
     }
 }
 
+struct Input {
+    heights: Vec<i32>,
+    bricks: i32,
+    leadders: i32,
+}
+
 fn main() {
-    let inputs = [vec!["time", "me", "bell"], vec!["t"]];
+    let inputs = [
+        Input {
+            heights: vec![4, 2, 7, 6, 9, 14, 12],
+            bricks: 5,
+            leadders: 1,
+        },
+        Input {
+            heights: vec![4, 12, 2, 7, 3, 18, 20, 3, 19],
+            bricks: 10,
+            leadders: 2,
+        },
+        Input {
+            heights: vec![14, 3, 19, 3],
+            bricks: 17,
+            leadders: 0,
+        },
+        Input {
+            heights: vec![1, 5, 1, 2, 3, 4, 10000],
+            bricks: 4,
+            leadders: 1,
+        },
+    ];
 
     for input in inputs {
-        let words = input.iter().map(|s| String::from(*s)).collect();
-        let result = Solution::minimum_length_encoding(words);
+        let result = Solution::furthest_building(input.heights, input.bricks, input.leadders);
         println!("{result:?}");
     }
 }
