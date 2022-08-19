a75401be8178ce271516cae42d1ce7965b069b7f
minsoo.park <topangel777@gmail.com>
2022-08-19 15:29:17 +0900

    659. Split Array into Consecutive Subsequences

--- lib/main.ex
+++ lib/main.ex
@@ -1,9 +1,51 @@
 defmodule Solution do
-  use Bitwise
+  @type mymap :: %{optional(integer) => integer}
+  @spec is_possible(nums :: [integer]) :: boolean
+  def is_possible(nums) do
+    lefts = Enum.frequencies(nums)
+    rights = %{}
+    loop(nums, lefts, rights)
+  end
 
-  @spec is_power_of_two(n :: integer) :: boolean
-  def is_power_of_two(n) when n > 0, do: (n &&& n - 1) == 0
-  def is_power_of_two(_), do: false
+  @spec loop(nums :: [integer], lefts :: mymap, rights :: mymap) :: boolean
+  defp loop([cur | nums], lefts, rights) do
+    if get_value(lefts, cur) == 0 do
+      loop(nums, lefts, rights)
+    else
+      new_lefts = inc_values(lefts, [{cur, -1}])
+      before1 = cur - 1
+
+      if get_value(rights, before1) > 0 do
+        loop(nums, new_lefts, inc_values(rights, [{before1, -1}, {cur, 1}]))
+      else
+        after1 = cur + 1
+        after2 = cur + 2
+
+        if get_value(new_lefts, after1) > 0 and get_value(new_lefts, after2) > 0 do
+          loop(
+            nums,
+            inc_values(new_lefts, [{after1, -1}, {after2, -1}]),
+            inc_values(rights, [{after2, 1}])
+          )
+        else
+          false
+        end
+      end
+    end
+  end
+
+  defp loop([], _, _), do: true
+
+  @spec get_value(m :: mymap, k :: integer) :: integer
+  defp get_value(m, k), do: Map.get(m, k, 0)
+
+  @spec inc_values(m :: mymap, keys :: [{integer, integer}]) :: mymap
+  defp inc_values(m, [{k, v} | keys]) do
+    value = get_value(m, k)
+    inc_values(Map.put(m, k, value + v), keys)
+  end
+
+  defp inc_values(m, []), do: m
 end
 
 defmodule Main do
@@ -11,21 +53,21 @@ defmodule Main do
   def main() do
     main([
       %{
-        n: 1
+        nums: [1, 2, 3, 3, 4, 5]
       },
       %{
-        n: 16
+        nums: [1, 2, 3, 3, 4, 4, 5, 5]
       },
       %{
-        n: 3
+        nums: [1, 2, 3, 4, 4, 5]
       }
     ])
   end
 
   @spec main(list[any]) :: nil
   def main([input | remains]) do
-    n = input.n
-    result = Solution.is_power_of_two(n)
+    nums = input.nums
+    result = Solution.is_possible(nums)
     IO.puts(result)
     main(remains)
   end
