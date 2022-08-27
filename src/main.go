67383d8e71fcba37bef5250748b65a0a21b7e0b2
minsoo.park <topangel777@gmail.com>
2022-08-28 00:09:33 +0900

    363. Max Sum of Rectangle No Larger Than K

--- src/main.go
+++ src/main.go
@@ -1,31 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func maxSumSubmatrix(matrix [][]int, k int) int {
+	rowCount := len(matrix)
+	colCount := len(matrix[0])
+	maxSum := math.MinInt
 
+	for i0 := 0; i0 < colCount; i0 += 1 {
+		sums := make([]int, rowCount)
+		for i1 := i0; i1 < colCount; i1 += 1 {
+			for i2 := 0; i2 < rowCount; i2 += 1 {
+				sums[i2] += matrix[i2][i1]
+			}
+
+			for i2 := 0; i2 < rowCount; i2 += 1 {
+				sum := 0
+				for i3 := i2; i3 < rowCount; i3 += 1 {
+					sum += sums[i3]
+					if (sum > maxSum) && (sum <= k) {
+						maxSum = sum
+					}
+				}
+			}
+		}
+	}
+
+	return maxSum
 }
 
 type input struct {
-	n int
+	matrix [][]int
+	k      int
 }
 
 func main() {
 	inputs := []input{
 		{
-			n: 1,
+			matrix: [][]int{{1, 0, 1}, {0, -2, 3}},
+			k:      2,
 		},
 		{
-			n: 10,
-		},
-		{
-			n: 46,
+			matrix: [][]int{{2, 2, -1}},
+			k:      3,
 		},
 	}
 
 	for _, input := range inputs {
-		n := input.n
-		result := reorderedPowerOf2(n)
+		matrix := input.matrix
+		k := input.k
+		result := maxSumSubmatrix(matrix, k)
 		fmt.Println(result)
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,54 @@
 struct Solution {}
 impl Solution {
-    pub fn reordered_power_of2(n: i32) -> bool {
-        let counts = Self::get_counts(n);
+    pub fn max_sum_submatrix(matrix: Vec<Vec<i32>>, k: i32) -> i32 {
+        let row_count = matrix.len();
+        let col_count = matrix[0].len();
+        let mut max_sum = i32::min_value();
 
-        for i in 0..32 {
-            if counts == Self::get_counts(1 << i) {
-                return true;
+        for i0 in 0..col_count {
+            let mut sums = vec![0; row_count];
+            for i1 in i0..col_count {
+                for i2 in 0..row_count {
+                    sums[i2] += matrix[i2][i1];
+                }
+
+                for i2 in 0..row_count {
+                    let mut sum = 0;
+                    for i3 in i2..row_count {
+                        sum += sums[i3];
+                        if (sum > max_sum) && (sum <= k) {
+                            max_sum = sum;
+                        }
+                    }
+                }
             }
         }
 
-        false
-    }
-
-    fn get_counts(n: i32) -> Vec<i32> {
-        let mut n = n;
-        let mut result = vec![0; 10];
-        while n > 0 {
-            let idx = (n % 10) as usize;
-            result[idx] += 1;
-            n /= 10;
-        }
-
-        return result;
+        max_sum
     }
 }
 
 struct Input {
-    n: i32,
+    matrix: Vec<Vec<i32>>,
+    k: i32,
 }
 
 fn main() {
-    let inputs: Vec<Input> = vec![Input { n: 1 }, Input { n: 10 }, Input { n: 46 }];
+    let inputs: Vec<Input> = vec![
+        Input {
+            matrix: vec![vec![1, 0, 1], vec![0, -2, 3]],
+            k: 2,
+        },
+        Input {
+            matrix: vec![vec![2, 2, -1]],
+            k: 3,
+        },
+    ];
 
     for input in inputs.iter() {
-        let n = input.n;
-        let result = Solution::reordered_power_of2(n);
+        let matrix = input.matrix.clone();
+        let k = input.k;
+        let result = Solution::max_sum_submatrix(matrix, k);
         println!("{result}");
     }
 }
