08e6bb0504d1e2733b7316d2e15c18f5ee7590b9
minsoo.park <topangel777@gmail.com>
2022-10-30 23:40:37 +0900

    1293. Shortest Path in a Grid with Obstacles Elimination

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,71 @@
-function earliestFullBloom (plantTime: number[], growTime: number[]): number {
-  const N = growTime.length
-  const indices = new Array<number>(N)
-  for (let i = 0; i < N; i += 1) {
-    indices[i] = i
+function shortestPath (grid: number[][], k: number): number {
+  const M = grid.length
+  const N = grid[0].length
+  const visits = new Array<number[]>(M)
+  for (let i = 0; i < M; i += 1) {
+    visits[i] = new Array<number>(N).fill(-1)
   }
 
-  indices.sort((a, b) => growTime[b] - growTime[a])
-  let result = 0
-  let curPlantTime = 0
-  for (let i = 0; i < N; i += 1) {
-    const idx = indices[i]
-    const ptime = plantTime[idx]
-    const time = curPlantTime + ptime + growTime[idx]
-    result = Math.max(result, time)
-    curPlantTime += ptime
+  const LM = M - 1
+  const LN = N - 1
+  const queue: number[][] = [[0, 0, 0, k]]
+  for (let head = queue.shift(); head != null; head = queue.shift()) {
+    const [x, y, step] = head
+
+    if ((x < 0) || (y < 0) || (x >= M) || (y >= N)) {
+      continue
+    }
+
+    if ((x === LM) && (y === LN)) {
+      return step
+    }
+
+    let chance = head[3]
+    if (grid[x][y] === 1) {
+      if (chance > 0) {
+        chance -= 1
+      } else {
+        continue
+      }
+    }
+
+    if ((visits[x][y] !== -1) && (visits[x][y] >= chance)) {
+      continue
+    }
+
+    visits[x][y] = chance
+
+    const next = step + 1
+    queue.push(
+      [x + 1, y, next, chance],
+      [x, y + 1, next, chance],
+      [x - 1, y, next, chance],
+      [x, y - 1, next, chance]
+    )
   }
 
-  return result
+  return -1
 }
 
 interface Input {
-  readonly plantTime: number[]
-  readonly growTime: number[]
+  readonly grid: number[][]
+  readonly k: number
 }
 
 async function main (): Promise<void> {
   const inputs: Input[] = [
     {
-      plantTime: [1, 4, 3],
-      growTime: [2, 3, 1]
+      grid: [[0, 0, 0], [1, 1, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]],
+      k: 1
     },
     {
-      plantTime: [1, 2, 3, 2],
-      growTime: [2, 1, 2, 1]
-    },
-    {
-      plantTime: [1],
-      growTime: [1]
+      grid: [[0, 1, 1], [1, 1, 1], [1, 0, 0]],
+      k: 1
     }
   ]
 
-  for (const { plantTime, growTime } of inputs) {
-    const result = earliestFullBloom(plantTime, growTime)
+  for (const { grid, k } of inputs) {
+    const result = shortestPath(grid, k)
     console.log(result)
   }
 }
