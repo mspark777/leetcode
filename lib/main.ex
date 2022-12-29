6769510badd7ca7d17ea3b337b513894452dd1f1
minsoo.park <topangel777@gmail.com>
2022-12-29 10:30:03 +0900

    292. Nim Game

--- lib/main.ex
+++ lib/main.ex
@@ -1,18 +1,30 @@
 defmodule Solution do
-  @spec maximum_bags(capacity :: [integer], rocks :: [integer], additional_rocks :: integer) ::
-          integer
-  def maximum_bags(capacity, rocks, additional_rocks) do
-    Enum.zip(capacity, rocks)
-    |> Enum.map(fn {c, r} -> c - r end)
-    |> Enum.sort()
-    |> loop(additional_rocks, 0)
-  end
-
-  defp loop([remain | remains], additional_rocks, result)
-       when additional_rocks >= remain do
-    loop(remains, additional_rocks - remain, result + 1)
-  end
-
-  defp loop([], _, result), do: result
-  defp loop(_, _, result), do: result
+  @spec can_win_nim(n :: integer) :: boolean
+  def can_win_nim(n), do: rem(n, 4) != 0
 end
+
+defmodule Main do
+  @moduledoc """
+  Documentation for `Leetcode`.
+  """
+
+  def main([n | inputs]) do
+    result = Solution.can_win_nim(n)
+
+    IO.puts(result)
+    main(inputs)
+  end
+
+  def main([]) do
+  end
+
+  def main do
+    main([
+      4,
+      1,
+      2
+    ])
+  end
+end
+
+Main.main()
--- src/main.rb
+++ src/main.rb
@@ -1,40 +1,18 @@
-# @param capacity [Array<Integer>]
-# @param rocks [Array<Integer>]
-# @param additional_rocks [Integer]
-# @return [Integer]
-def maximum_bags(capacity, rocks, additional_rocks)
-  # @type [Array<Integer>]
-  remains = capacity.map.with_index { |c, i| c - rocks[i] }
-  remains.sort!
-
-  result = 0
-
-  for remain in remains do
-    break unless additional_rocks >= remain
-
-    additional_rocks -= remain
-    result += 1
-  end
-
-  result
+# @param n [Integer]
+# @return [Boolean]
+def can_win_nim(n)
+  (n % 4) != 0
 end
 
 def main
   inputs = [
-    {
-      capacity: [2, 3, 4, 5],
-      rocks: [1, 2, 4, 4],
-      additional_rocks: 2
-    },
-    {
-      capacity: [10, 2, 2],
-      rocks: [2, 2, 0],
-      additional_rocks: 100
-    }
+    4,
+    1,
+    2
   ]
 
   inputs.each do |input|
-    result = maximum_bags input[:capacity], input[:rocks], input[:additional_rocks]
+    result = can_win_nim input
     puts result
   end
 end
