25a4b426cd04101741a868e911908dd9a266bf8b
minsoo.park <topangel777@gmail.com>
2023-04-24 09:52:16 +0900

    1046. Last Stone Weight

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,34 @@
 mod utils;
 
-use utils::{Solution, TreeNode};
+use std::collections::BinaryHeap;
 
-use std::cell::RefCell;
-use std::rc::Rc;
+use utils::Solution;
 
 impl Solution {
-    pub fn width_of_binary_tree(some_root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if some_root.is_none() {
-            return 0;
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut queue = BinaryHeap::<i32>::with_capacity(stones.len());
+
+        for &s in stones.iter() {
+            queue.push(s);
         }
 
-        let root = some_root.as_ref().unwrap();
-        let mut result = 1;
-        let mut queue = vec![(Rc::clone(root), 0)];
-
-        while !queue.is_empty() {
-            let start = queue.first().unwrap().1;
-            let end = queue.last().unwrap().1;
-            result = result.max(end - start + 1);
-
-            let mut children = Vec::<(Rc<RefCell<TreeNode>>, i32)>::new();
-            for qnode in queue.iter() {
-                let node = qnode.0.borrow();
-                let idx = qnode.1 - start;
-                let some_left = &node.left;
-                let some_right = &node.right;
-
-                if let Some(left) = some_left {
-                    children.push((Rc::clone(left), 2 * idx + 1));
-                }
-
-                if let Some(right) = some_right {
-                    children.push((Rc::clone(right), 2 * (idx + 1)));
-                }
+        while let Some(y) = queue.pop() {
+            if let Some(x) = queue.pop() {
+                queue.push(y - x);
+            } else {
+                return y;
             }
-
-            queue = children;
         }
 
-        return result;
+        return 0;
     }
 }
 
 fn main() {
-    let inputs = [
-        TreeNode::new_node(
-            1,
-            TreeNode::new_node(3, TreeNode::new_val(5), TreeNode::new_val(3)),
-            TreeNode::new_right(2, TreeNode::new_val(9)),
-        ),
-        TreeNode::new_node(
-            1,
-            TreeNode::new_left(3, TreeNode::new_left(5, TreeNode::new_val(6))),
-            TreeNode::new_right(2, TreeNode::new_left(9, TreeNode::new_val(7))),
-        ),
-        TreeNode::new_node(
-            1,
-            TreeNode::new_left(3, TreeNode::new_val(5)),
-            TreeNode::new_val(2),
-        ),
-    ];
+    let inputs = [vec![2, 7, 4, 1, 8, 1], vec![1]];
 
-    for root in inputs {
-        let result = Solution::width_of_binary_tree(root);
+    for stones in inputs {
+        let result = Solution::last_stone_weight(stones);
         println!("{result}");
     }
 }
--- src/py/main.py
+++ src/py/main.py
@@ -1,55 +1,32 @@
-from typing import Optional
-
-from utils import TreeNode, new_tree_right, new_tree_node, new_tree_val, new_tree_left
+from typing import List
+from queue import PriorityQueue
 
 
 class Solution:
-    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        queue = PriorityQueue[int]()
 
-        result = 1
-        queue: list[tuple[TreeNode, int]] = [(root, 0)]
-        while queue:
-            count = len(queue)
-            start = queue[0][1]
-            end = queue[-1][1]
-            result = max(result, end - start + 1)
+        for s in stones:
+            queue.put(-s)
 
-            for i in range(count):
-                node, node_idx = queue[i]
-                idx = node_idx - start
-                left = node.left
-                right = node.right
+        while not queue.empty():
+            y = queue.get()
+            if queue.empty():
+                return -y
 
-                if left is not None:
-                    queue.append((left, 2 * idx + 1))
+            x = queue.get()
+            if y != x:
+                queue.put(y - x)
 
-                if right is not None:
-                    queue.append((right, 2 * (idx + 1)))
-            queue = queue[count:]
-
-        return result
+        return 0
 
 
 def main():
-    inputs: list[Optional[TreeNode]] = [
-        new_tree_node(
-            1,
-            new_tree_node(3, new_tree_val(5), new_tree_val(3)),
-            new_tree_right(2, new_tree_val(9)),
-        ),
-        new_tree_node(
-            1,
-            new_tree_left(3, new_tree_left(5, new_tree_val(6))),
-            new_tree_right(2, new_tree_left(9, new_tree_val(7))),
-        ),
-        new_tree_node(1, new_tree_left(3, new_tree_val(5)), new_tree_val(2)),
-    ]
+    inputs: list[list[int]] = [[2, 7, 4, 1, 8, 1], [1]]
 
-    for root in inputs:
+    for stones in inputs:
         solution = Solution()
-        result = solution.widthOfBinaryTree(root)
+        result = solution.lastStoneWeight(stones)
         print(result)
 
 
