6eee2f66771fd1f3f13524383e464de5a80fa980
mspark777 <topangel777@gmail.com>
2023-05-24 09:44:14 +0900

    2542. Maximum Subsequence Score

--- src/main.py
+++ src/main.py
@@ -2,35 +2,49 @@ from typing import List
 from queue import PriorityQueue
 
 
-class KthLargest:
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        N = len(nums1)
+        pairs = [(nums1[i], nums2[i]) for i in range(N)]
+        pairs.sort(key=lambda a: -a[1])
+        queue = PriorityQueue()
+        topk_sum = 0
+        for i in range(k):
+            topk_sum += pairs[i][0]
+            queue.put(pairs[i][0])
+
+        result = topk_sum * pairs[k - 1][1]
+
+        for i in range(k, N):
+            topk_sum += pairs[i][0] - queue.get()
+            queue.put(pairs[i][0])
+
+            result = max(result, topk_sum * pairs[i][1])
+
+        return result
+
+
+class Input:
+    nums1: list[int]
+    nums2: list[int]
     k: int
-    queue: PriorityQueue
 
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, nums1: list[int], nums2: list[int], k: int):
+        self.nums1 = nums1
+        self.nums2 = nums2
         self.k = k
-        self.queue = PriorityQueue()
-
-        for num in nums:
-            self.add(num)
-
-    def add(self, val: int) -> int:
-        self.queue.put(val)
-
-        while self.queue.qsize() > self.k:
-            self.queue.get()
-
-        v = self.queue.get()
-        self.queue.put(v)
-        return v
 
 
 def main():
-    kth_largest = KthLargest(3, [4, 5, 8, 2])
-    print(kth_largest.add(3))
-    print(kth_largest.add(5))
-    print(kth_largest.add(10))
-    print(kth_largest.add(9))
-    print(kth_largest.add(4))
+    inputs = [
+        Input(nums1=[1, 3, 3, 2], nums2=[2, 1, 3, 4], k=3),
+        Input(nums1=[4, 2, 3, 1, 1], nums2=[7, 5, 10, 9, 6], k=1),
+    ]
+
+    for input in inputs:
+        solution = Solution()
+        result = solution.maxScore(input.nums1, input.nums2, input.k)
+        print(result)
 
 
 if __name__ == "__main__":
