6f3e48bfa4bd5d8c0f6a3c897cc5ad4b069d1928
minsoo.park <topangel777@gmail.com>
2023-06-23 13:22:01 +0900

    1027. Longest Arithmetic Subsequence

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,33 @@
 import '@total-typescript/ts-reset'
 
-function getCost (nums: number[], cost: number[], base: number): number {
+function longestArithSeqLength (nums: number[]): number {
   let result = 0
-  for (const [i, num] of nums.entries()) {
-    result += Math.abs(num - base) * cost[i]
-  }
+  const dp = new Array<Map<number, number>>(nums.length)
+  for (let right = 0; right < nums.length; right += 1) {
+    dp[right] = new Map()
+    for (let left = 0; left < right; left += 1) {
+      const diff = nums[left] - nums[right]
+      const rmap = dp[right]
+      const lmap = dp[left]
 
-  return result
-}
-
-function minCost (nums: number[], cost: number[]): number {
-  let left = 1000001
-  let right = 0
-  for (const num of nums) {
-    left = Math.min(left, num)
-    right = Math.max(right, num)
-  }
-
-  let result = getCost(nums, cost, nums[0])
-  while (left < right) {
-    const mid = Math.trunc((left + right) / 2)
-    const cost1 = getCost(nums, cost, mid)
-    const cost2 = getCost(nums, cost, mid + 1)
-    result = Math.min(cost1, cost2)
-
-    if (cost1 > cost2) {
-      left = mid + 1
-    } else {
-      right = mid
+      const curLen = (lmap.get(diff) ?? 1) + 1
+      rmap.set(diff, curLen)
+      result = Math.max(result, curLen)
     }
   }
 
   return result
 }
 
-interface Input {
-  readonly nums: number[]
-  readonly cost: number[]
-}
-
 function main (): void {
-  const inputs: Input[] = [
-    { nums: [1, 3, 5, 2], cost: [2, 3, 1, 14] },
-    { nums: [2, 2, 2, 2, 2], cost: [4, 2, 8, 1, 3] }
+  const inputs: number[][] = [
+    [3, 6, 9, 12],
+    [9, 4, 7, 2, 10],
+    [20, 1, 15, 3, 10, 5, 8]
   ]
 
-  for (const { nums, cost } of inputs) {
-    const result = minCost(nums, cost)
+  for (const nums of inputs) {
+    const result = longestArithSeqLength(nums)
     console.log(result)
   }
 }
