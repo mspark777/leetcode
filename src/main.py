35fabbdbeaf9f6f58e0d97be10ca78b359dafa1c
minsoo.park <topangel777@gmail.com>
2023-06-23 14:21:45 +0900

    1027. Longest Arithmetic Subsequence

--- src/main.py
+++ src/main.py
@@ -2,41 +2,32 @@ from typing import List
 
 
 class Solution:
-    def minCost(self, nums: List[int], cost: List[int]) -> int:
-        left = 1000001
-        right = 0
-        for num in nums:
-            left = min(left, num)
-            right = max(right, num)
-
-        result = self.get_cost(nums, cost, nums[0])
-        while left < right:
-            mid = (left + right) // 2
-            cost1 = self.get_cost(nums, cost, mid)
-            cost2 = self.get_cost(nums, cost, mid + 1)
-            result = min(cost1, cost2)
-
-            if cost1 > cost2:
-                left = mid + 1
-            else:
-                right = mid
-
-        return result
-
-    def get_cost(self, nums: list[int], cost: list[int], base: int) -> int:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
         result = 0
-        for i, num in enumerate(nums):
-            result += abs(num - base) * cost[i]
+        dp: list[dict[int, int]] = []
+        for right in range(len(nums)):
+            dp.append(dict())
+            for left in range(right):
+                diff = nums[left] - nums[right]
+                lmap = dp[left]
+                rmap = dp[right]
+
+                cur_len = lmap.get(diff)
+                if cur_len is None:
+                    cur_len = 1
+                cur_len += 1
+                rmap[diff] = cur_len
+                result = max(result, cur_len)
 
         return result
 
 
 def main():
-    inputs = [([1, 3, 5, 2], [2, 3, 1, 14]), ([2, 2, 2, 2, 2], [4, 2, 8, 1, 3])]
+    inputs = [[3, 6, 9, 12], [9, 4, 7, 2, 10], [20, 1, 15, 3, 10, 5, 8]]
 
-    for nums, cost in inputs:
+    for nums in inputs:
         solution = Solution()
-        result = solution.minCost(nums, cost)
+        result = solution.longestArithSeqLength(nums)
         print(result)
 
 
