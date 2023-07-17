fdc2597c951fe1d33d72fa866b66e8cb558085ba
minsoo.park <topangel777@gmail.com>
2023-07-17 10:30:16 +0900

    445. Add Two Numbers II

--- src/main.mjs
+++ src/main.mjs
@@ -1,34 +1,98 @@
+class ListNode {
+  /** @type {number} */
+  val
+  /** @type {ListNode|null} */
+  next
+
+  /**
+    * @param {number} [val]
+    * @param {ListNode|null} [next]
+    */
+  constructor (val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
 /**
-  * @param {number[]} arr
-  * @param {number} difference
-  * @returns {number}
+  * @param {ListNode|null} node
+  * @returns {number[]}
   */
-function longestSubsequence (arr, difference) {
-  /** @type {Map<number, number>} */
-  const dp = new Map()
-  let result = 1
-
-  for (const num of arr) {
-    const before = dp.get(num - difference) ?? 0
-    const now = before + 1
-    dp.set(num, now)
-
-    result = Math.max(result, now)
+function ltoa (node) {
+  const arr = []
+  while (node != null) {
+    arr.push(node.val)
+    node = node.next
   }
 
-  return result
+  return arr
+}
+
+/**
+  * @param {number[]} arr
+  * @returns {boolean}
+  */
+function isNotEmpty (arr) {
+  return arr.length > 0
+}
+
+/**
+  * @param {ListNode|null} l1
+  * @param {ListNode|null} l2
+  * @returns {ListNode|null}
+  */
+function addTwoNumbers (l1, l2) {
+  const stack1 = ltoa(l1)
+  const stack2 = ltoa(l2)
+
+  let total = 0n
+  let carry = 0n
+  let result = new ListNode()
+  while (isNotEmpty(stack1) || isNotEmpty(stack2)) {
+    if (isNotEmpty(stack1)) {
+      total += BigInt(stack1.pop())
+    }
+
+    if (isNotEmpty(stack2)) {
+      total += BigInt(stack2.pop())
+    }
+
+    result.val = Number(total % 10n)
+    carry = total / 10n
+    const head = new ListNode(Number(carry))
+    head.next = result
+    result = head
+    total = carry
+  }
+
+  return carry === 0n ? result.next : result
+}
+
+/**
+  * @param {number[]} arr
+  * @returns {ListNode|null}
+  */
+function atol (arr) {
+  const head = new ListNode()
+  let tail = head
+  for (const num of arr) {
+    tail.next = new ListNode(num)
+    tail = tail.next
+  }
+
+  return head.next
 }
 
 function main () {
   const inputs = [
-    { arr: [1, 2, 3, 4], difference: 1 },
-    { arr: [1, 3, 5, 7], difference: 1 },
-    { arr: [1, 5, 7, 8, 5, 3, 4, 2, 1], difference: -2 }
+    [atol([7, 2, 4, 3]), atol([5, 6, 4])],
+    [atol([2, 4, 3]), atol([5, 6, 4])],
+    [atol([0]), atol([0])]
   ]
 
-  for (const { arr, difference } of inputs) {
-    const result = longestSubsequence(arr, difference)
-    console.log(result)
+  for (const [l1, l2] of inputs) {
+    const result = addTwoNumbers(l1, l2)
+    console.log(ltoa(result))
   }
 }
 main()
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,76 @@
 import '@total-typescript/ts-reset'
 
-function longestSubsequence (arr: number[], difference: number): number {
-  const dp = new Map<number, number>()
-  let result = 1
-
-  for (const num of arr) {
-    const before = dp.get(num - difference) ?? 0
-    const now = before + 1
-    dp.set(num, now)
-
-    result = Math.max(result, now)
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor (val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
   }
-
-  return result
 }
 
-interface Input {
-  readonly arr: number[]
-  readonly difference: number
+function ltoa (node: ListNode | null): number[] {
+  const arr: number[] = []
+  while (node != null) {
+    arr.push(node.val)
+    node = node.next
+  }
+
+  return arr
+}
+
+function isNotEmpty (arr: number[]): boolean {
+  return arr.length > 0
+}
+
+function addTwoNumbers (l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const stack1 = ltoa(l1)
+  const stack2 = ltoa(l2)
+
+  let total = 0n
+  let carry = 0n
+  let result = new ListNode()
+  while (isNotEmpty(stack1) || isNotEmpty(stack2)) {
+    if (isNotEmpty(stack1)) {
+      total += BigInt(stack1.pop() as number)
+    }
+
+    if (isNotEmpty(stack2)) {
+      total += BigInt(stack2.pop() as number)
+    }
+
+    result.val = Number(total % 10n)
+    carry = total / 10n
+    const head = new ListNode(Number(carry))
+    head.next = result
+    result = head
+    total = carry
+  }
+
+  return carry === 0n ? result.next : result
+}
+
+function atol (arr: number[]): ListNode | null {
+  const head = new ListNode()
+  let tail = head
+  for (const num of arr) {
+    tail.next = new ListNode(num)
+    tail = tail.next
+  }
+
+  return head.next
 }
 
 function main (): void {
-  const inputs: Input[] = [
-    { arr: [1, 2, 3, 4], difference: 1 },
-    { arr: [1, 3, 5, 7], difference: 1 },
-    { arr: [1, 5, 7, 8, 5, 3, 4, 2, 1], difference: -2 }
+  const inputs = [
+    [atol([7, 2, 4, 3]), atol([5, 6, 4])],
+    [atol([2, 4, 3]), atol([5, 6, 4])],
+    [atol([0]), atol([0])]
   ]
 
-  for (const { arr, difference } of inputs) {
-    const result = longestSubsequence(arr, difference)
-    console.log(result)
+  for (const [l1, l2] of inputs) {
+    const result = addTwoNumbers(l1 as any, l2 as any)
+    console.log(ltoa(result))
   }
 }
 main()
