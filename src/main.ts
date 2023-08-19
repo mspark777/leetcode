68e2a1eafaea1a251b81d88b805c2a1ba7cf16c7
mspark777 <topangel777@gmail.com>
2023-08-19 12:00:18 +0900

    1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,127 @@
 import '@total-typescript/ts-reset'
 
-function maximalNetworkRank (n: number, roads: number[][]): number {
-  const adjacents = new Map<number, Set<number>>()
-  for (const road of roads) {
-    const [a, b] = road as [number, number]
-    const aSet = adjacents.get(a) ?? new Set()
-    const bSet = adjacents.get(b) ?? new Set()
+class UnionFind {
+  public readonly parents: number[]
+  public readonly sizes: number[]
+  public maxSize: number
 
-    aSet.add(b)
-    bSet.add(a)
-    adjacents.set(a, aSet)
-    adjacents.set(b, bSet)
+  public constructor (n: number) {
+    this.parents = new Array(n)
+    this.sizes = new Array(n).fill(1)
+    this.maxSize = 1
+
+    for (let i = 0; i < n; i += 1) {
+      this.parents[i] = i
+    }
   }
 
-  let result = 0
-  for (let node0 = 0; node0 < n; node0 += 1) {
-    const set0 = adjacents.get(node0) ?? new Set()
-    const rank0 = set0.size ?? 0
-    for (let node1 = node0 + 1; node1 < n; node1 += 1) {
-      const rank1 = adjacents.get(node1)?.size ?? 0
-      let rank = rank0 + rank1
-      if (set0.has(node1)) {
-        rank -= 1
+  public find (x: number): number {
+    if (x !== this.getParent(x)) {
+      this.parents[x] = this.find(this.getParent(x))
+    }
+
+    return this.getParent(x)
+  }
+
+  public union (x: number, y: number): boolean {
+    let rootX = this.find(x)
+    let rootY = this.find(y)
+    if (rootX !== rootY) {
+      if (this.getSize(rootX) < this.getSize(rootY)) {
+        const temp = rootX
+        rootX = rootY
+        rootY = temp
       }
 
-      result = Math.max(result, rank)
+      this.parents[rootY] = rootX
+      this.sizes[rootX] += this.getSize(rootY)
+      this.maxSize = Math.max(this.maxSize, this.getSize(rootX))
+      return true
+    }
+
+    return false
+  }
+
+  private getParent (x: number): number {
+    return this.parents[x] as number
+  }
+
+  private getSize (x: number): number {
+    return this.sizes[x] as number
+  }
+}
+
+function get2Dimension<T> (nums: T[][], r: number, c: number): T {
+  return nums.at(r)?.at(c) as T
+}
+
+function get1Dimenstion<T> (nums: T[], i: number): T {
+  return nums[i] as T
+}
+
+function findCriticalAndPseudoCriticalEdges (n: number, edges: number[][]): number[][] {
+  const edgeCount = edges.length
+  const newEdges = new Array<number[]>(edgeCount)
+  for (let i = 0; i < edgeCount; i += 1) {
+    const edge = get1Dimenstion(edges, i)
+    const newEdge = new Array<number>(4)
+    for (let j = 0; j < 3; j += 1) {
+      newEdge[j] = get1Dimenstion(edge, j)
+    }
+    newEdge[3] = i
+    newEdges[i] = newEdge
+  }
+
+  newEdges.sort((as, bs) => {
+    const a = as[2] as number
+    const b = bs[2] as number
+    return a - b
+  })
+
+  const stdUF = new UnionFind(n)
+  let stdWeight = 0
+  for (const edge of newEdges) {
+    const [a, b, w] = edge as [number, number, number]
+    if (stdUF.union(a, b)) {
+      stdWeight += w
+    }
+  }
+
+  const result: number[][] = [[], []]
+  for (let i = 0; i < edgeCount; i += 1) {
+    const ignoreUF = new UnionFind(n)
+    let ignoreWeight = 0
+    for (let j = 0; j < edgeCount; j += 1) {
+      if (i === j) {
+        continue
+      }
+
+      if (ignoreUF.union(get2Dimension(newEdges, j, 0), get2Dimension(newEdges, j, 1))) {
+        ignoreWeight += get2Dimension(newEdges, j, 2)
+      }
+    }
+
+    if (ignoreUF.maxSize < n) {
+      result.at(0)?.push(get2Dimension(newEdges, i, 3))
+    } else if (ignoreWeight > stdWeight) {
+      result.at(0)?.push(get2Dimension(newEdges, i, 3))
+    } else {
+      const forceUF = new UnionFind(n)
+      forceUF.union(get2Dimension(newEdges, i, 0), get2Dimension(newEdges, i, 1))
+      let forceWeight = get2Dimension(newEdges, i, 2)
+      for (let j = 0; j < edgeCount; j += 1) {
+        if (i === j) {
+          continue
+        }
+
+        if (forceUF.union(get2Dimension(newEdges, j, 0), get2Dimension(newEdges, j, 1))) {
+          forceWeight += get2Dimension(newEdges, j, 2)
+        }
+      }
+
+      if (forceWeight === stdWeight) {
+        result.at(1)?.push(get2Dimension(newEdges, i, 3))
+      }
     }
   }
 
@@ -33,18 +130,17 @@ function maximalNetworkRank (n: number, roads: number[][]): number {
 
 interface Input {
   readonly n: number
-  readonly roads: number[][]
+  readonly edges: number[][]
 }
 
 function main (): void {
   const inputs: Input[] = [
-    { n: 4, roads: [[0, 1], [0, 3], [1, 2], [1, 3]] },
-    { n: 5, roads: [[0, 1], [0, 3], [1, 2], [1, 3], [2, 3], [2, 4]] },
-    { n: 8, roads: [[0, 1], [1, 2], [2, 3], [2, 4], [5, 6], [5, 7]] }
+    { n: 5, edges: [[0, 1, 1], [1, 2, 1], [2, 3, 2], [0, 3, 2], [0, 4, 3], [3, 4, 3], [1, 4, 6]] },
+    { n: 4, edges: [[0, 1, 1], [1, 2, 1], [2, 3, 1], [0, 3, 1]] }
   ]
 
-  for (const { n, roads } of inputs) {
-    const result = maximalNetworkRank(n, roads)
+  for (const { n, edges } of inputs) {
+    const result = findCriticalAndPseudoCriticalEdges(n, edges)
     console.log(result)
   }
 }
