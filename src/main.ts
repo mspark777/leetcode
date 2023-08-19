3f2c3d84f4ce7d189d6a139e3b67d681772ca2f5
mspark777 <topangel777@gmail.com>
2023-08-20 00:06:17 +0900

    72. Edit Distance

--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,62 @@
 import '@total-typescript/ts-reset'
 
-function div (a: number, b: number): number {
-  return Number(BigInt(a) / BigInt(b))
+function get (dp: number[][], r: number, c: number): number {
+  const row = dp[r] as number[]
+  return row[c] as number
 }
 
-function mod (a: number, b: number): number {
-  return Number(BigInt(a) % BigInt(b))
+function set (dp: number[][], r: number, c: number, v: number): void {
+  const row = dp[r] as number[]
+  row[c] = v
 }
 
-function getWords (i: number, words: string[], maxWidth: number): string[] {
-  const line: string[] = []
-  let lineLength = 0
-  while (i < words.length) {
-    const word = words.at(i) as string
-    const len = lineLength + word.length
-    if (len > maxWidth) {
-      break
+function minDistance (word1: string, word2: string): number {
+  const len1 = word1.length
+  const len2 = word2.length
+  const dp = new Array<number[]>(len1 + 1)
+  for (let i = 0; i <= len1; i += 1) {
+    dp[i] = new Array(len2 + 1).fill(0)
+  }
+
+  for (let i = 0; i <= len1; i += 1) {
+    const row = dp[i] as number[]
+    row[0] = i
+  }
+
+  for (let i = 0; i <= len2; i += 1) {
+    const row = dp[0] as number[]
+    row[i] = i
+  }
+
+  for (let i = 1; i <= len1; i += 1) {
+    const ch1 = word1.at(i - 1) as string
+    for (let j = 1; j <= len2; j += 1) {
+      if (ch1 === word2.at(j - 1)) {
+        set(dp, i, j, get(dp, i - 1, j - 1))
+      } else {
+        const memo = Math.min(
+          get(dp, i - 1, j - 1),
+          Math.min(
+            get(dp, i - 1, j),
+            get(dp, i, j - 1)
+          )
+        )
+        set(dp, i, j, memo + 1)
+      }
     }
-
-    line.push(word)
-    lineLength += word.length + 1
-    i += 1
   }
 
-  return line
-}
-
-function createLine (line: string[], i: number, words: string[], maxWidth: number): string {
-  let baseLength = -1
-  for (const word of line) {
-    baseLength += word.length + 1
-  }
-
-  const extraSpaces = maxWidth - baseLength
-  if ((line.length === 1) || (words.length === i)) {
-    return line.join(' ') + ' '.repeat(extraSpaces)
-  }
-
-  const wordCount = line.length - 1
-  const spacesPerWord = div(extraSpaces, wordCount)
-  const needsExtraSpace = mod(extraSpaces, wordCount)
-  for (let j = 0; j < needsExtraSpace; j += 1) {
-    const w = line[j] as string
-    line[j] = w + ' '
-  }
-
-  for (let j = 0; j < wordCount; j += 1) {
-    const w = line[j] as string
-    line[j] = w + ' '.repeat(spacesPerWord)
-  }
-
-  return line.join(' ')
-}
-
-function fullJustify (words: string[], maxWidth: number): string[] {
-  const result: string[] = []
-  let i = 0
-  while (i < words.length) {
-    const line = getWords(i, words, maxWidth)
-    i += line.length
-    result.push(createLine(line, i, words, maxWidth))
-  }
-
-  return result
+  return get(dp, len1, len2)
 }
 
 function main (): void {
-  const inputs: Array<[string[], number]> = [
-    [['This', 'is', 'an', 'example', 'of', 'text', 'justification.'], 16],
-    [['What', 'must', 'be', 'acknowledgment', 'shall', 'be'], 16],
-    [['Science', 'is', 'what', 'we', 'understand', 'well', 'enough', 'to', 'explain', 'to', 'a', 'computer.', 'Art', 'is', 'everything', 'else', 'we', 'do'], 20]
+  const inputs: Array<[string, string]> = [
+    ['horse', 'ros'],
+    ['intention', 'execution']
   ]
 
-  for (const [words, maxWidth] of inputs) {
-    const result = fullJustify(words, maxWidth)
+  for (const [word1, word2] of inputs) {
+    const result = minDistance(word1, word2)
     console.log(result)
   }
 }
