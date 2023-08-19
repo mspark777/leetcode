ed867c563ac65c39a9f9e1643cfc1a6f49a97ce1
mspark777 <topangel777@gmail.com>
2023-08-19 15:33:30 +0900

    61. Rotate List

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,52 @@
 import '@total-typescript/ts-reset'
 
-function div (left: number, right: number): number {
-  return Number(BigInt(left) / BigInt(right))
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor (val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
 }
 
 function mod (left: number, right: number): number {
   return Number(BigInt(left) % BigInt(right))
 }
 
-function getPermutation (n: number, k: number): string {
-  if ([0, 1].includes(n)) {
-    return n.toString()
+function rotateRight (head: ListNode | null, k: number): ListNode | null {
+  if (head?.next == null) {
+    return head
+  } else if (k < 1) {
+    return head
   }
 
-  const nums = new Array<number>(n)
-  let fact = 1
-  for (let i = 1; i < n; i += 1) {
-    nums[i - 1] = i
-    fact *= i
-  }
-  nums[n - 1] = n
-
-  k -= 1
-  const result: number[] = []
-  while (nums.length > 0) {
-    const pos = div(k, fact)
-    result.push(nums[pos] as number)
-    nums.splice(pos, 1)
-    if (nums.length < 1) {
-      break
-    }
-
-    k = mod(k, fact)
-    fact = div(fact, nums.length)
+  let cur = head
+  let len = 1
+  while (cur.next != null) {
+    len += 1
+    cur = cur.next
   }
 
-  return result.join('')
+  cur.next = head
+  k = mod(k, len)
+  for (let i = len - k; i > 0; i -= 1) {
+    cur = cur?.next as ListNode
+  }
+
+  head = cur.next
+  cur.next = null
+
+  return head
 }
 
 function main (): void {
-  const inputs: Array<[number, number]> = [
-    [3, 3],
-    [4, 9],
-    [3, 1]
+  const inputs: Array<[number[], number]> = [
+    [[1, 2, 3, 4, 5], 2],
+    [[0, 1, 2], 4]
   ]
 
-  for (const [n, k] of inputs) {
-    const result = getPermutation(n, k)
+  for (const [head, k] of inputs) {
+    const result = rotateRight(null, 0)
     console.log(result)
   }
 }
