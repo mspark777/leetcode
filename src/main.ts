b25ca1f96e241e273f8d45fcdfdca5368aa397b9
mspark777 <topangel777@gmail.com>
2023-08-20 21:46:42 +0900

    73. Set Matrix Zeroes

--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,65 @@
 import '@total-typescript/ts-reset'
 
-function topologicalSort (graph: Map<number, number[]>, indegree: number[]): number[] {
-  const visited: number[] = []
-  const stack: number[] = []
-  for (const key of graph.keys()) {
-    if (indegree.at(key) === 0) {
-      stack.push(key)
-    }
-  }
-
-  for (let key = stack.pop(); key != null; key = stack.pop()) {
-    visited.push(key)
-
-    for (const prev of graph.get(key) ?? []) {
-      const degree = indegree.at(prev) as number
-      indegree[prev] = degree - 1
-      if (degree === 1) {
-        stack.push(prev)
-      }
-    }
-  }
-
-  return visited.length === graph.size ? visited : []
+function get (m: number[][], r: number, c: number): number {
+  return m.at(r)?.at(c) as number
 }
 
-function sortItems (n: number, m: number, group: number[], beforeItems: number[][]): number[] {
-  let groupID = m
-  const itemGraph = new Map<number, number[]>()
-  for (let i = 0; i < n; i += 1) {
-    itemGraph.set(i, [])
-    if (group[i] === -1) {
-      group[i] = groupID
-      groupID += 1
-    }
-  }
+function set (m: number[][], r: number, c: number, v: number): void {
+  const row = m.at(r) as number[]
+  row[c] = v
+}
 
-  const itemIndegree = new Array<number>(n).fill(0)
-  const groupGraph = new Map<number, number[]>()
-  const groupIndegree = new Array<number>(groupID).fill(0)
-  for (let i = 0; i < groupID; i += 1) {
-    groupGraph.set(i, [])
-  }
-
-  for (let curr = 0; curr < n; curr += 1) {
-    for (const prev of beforeItems.at(curr) ?? []) {
-      itemGraph.get(prev)?.push(curr)
-      itemIndegree[curr] += 1
-      if (group.at(curr) !== group.at(prev)) {
-        groupGraph.get(group[prev] as number)?.push(group.at(curr) as number)
-        groupIndegree[group.at(curr) as number] += 1
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes (matrix: number[][]): void {
+  const rowCount = matrix.length
+  const colCount = matrix.at(0)?.length as number
+  let fr = false
+  let fc = false
+  for (let r = 0; r < rowCount; r += 1) {
+    for (let c = 0; c < colCount; c += 1) {
+      if (get(matrix, r, c) === 0) {
+        fr ||= r === 0
+        fc ||= c === 0
+        set(matrix, 0, c, 0)
+        set(matrix, r, 0, 0)
       }
     }
   }
 
-  const itemOrder = topologicalSort(itemGraph, itemIndegree)
-  const groupOrder = topologicalSort(groupGraph, groupIndegree)
-
-  if (itemOrder.length < 1) {
-    return []
-  } else if (groupOrder.length < 1) {
-    return []
+  for (let r = 1; r < rowCount; r += 1) {
+    for (let c = 1; c < colCount; c += 1) {
+      if (get(matrix, r, 0) === 0) {
+        set(matrix, r, c, 0)
+      } else if (get(matrix, 0, c) === 0) {
+        set(matrix, r, c, 0)
+      }
+    }
   }
 
-  const orderedGroups = new Map<number, number[]>()
-  for (const item of itemOrder) {
-    const key = group[item] as number
-    const ordered = orderedGroups.get(key) ?? []
-    ordered.push(item)
-    orderedGroups.set(key, ordered)
+  if (fr) {
+    for (let c = 0; c < colCount; c += 1) {
+      set(matrix, 0, c, 0)
+    }
   }
 
-  const result: number[] = []
-  for (const groupIndex of groupOrder) {
-    const ordered = orderedGroups.get(groupIndex) ?? []
-    result.push(...ordered)
+  if (fc) {
+    for (let r = 0; r < rowCount; r += 1) {
+      set(matrix, r, 0, 0)
+    }
   }
-
-  return result
 }
 
 function main (): void {
-  const inputs: Array<[number, number, number[], number[][]]> = [
-    [8, 2, [-1, -1, 1, 0, 0, 1, 0, -1], [[], [6], [5], [6], [3, 6], [], [], []]],
-    [8, 2, [-1, -1, 1, 0, 0, 1, 0, -1], [[], [6], [5], [6], [3], [], [4], []]]
+  const inputs: number[][][] = [
+    [[1, 1, 1], [1, 0, 1], [1, 1, 1]],
+    [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
   ]
 
-  for (const [n, m, group, beforeItems] of inputs) {
-    const result = sortItems(n, m, group, beforeItems)
-    console.log(result)
+  for (const matrix of inputs) {
+    setZeroes(matrix)
+    console.log(matrix)
   }
 }
 main()
