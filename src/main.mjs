4e9c8ab7ddd6bea84be52c294de059a3ee19239a
mspark777 <topangel777@gmail.com>
2023-08-21 21:44:42 +0900

    459. Repeated Substring Pattern

--- src/main.mjs
+++ src/main.mjs
@@ -1,81 +1,23 @@
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-const QUEEN = 'Q'
-const EMPTY = '.'
-
 /**
-  * @param {number} row
-  * @param {number} col
-  * @param {string[][]} board
+  * @param {string} s
   * @returns {boolean}
   */
-function isSafe (row, col, board) {
-  for (let r = row, c = col; (r >= 0) && (c >= 0); r -= 1, c -= 1) {
-    if (board.at(r)?.at(c) === QUEEN) {
-      return false
-    }
-  }
-
-  for (let c = col; c >= 0; c -= 1) {
-    if (board.at(row)?.at(c) === QUEEN) {
-      return false
-    }
-  }
-
-  for (let r = row, c = col; (r < board.length) && (c >= 0); r += 1, c -= 1) {
-    if (board.at(r)?.at(c) === QUEEN) {
-      return false
-    }
-  }
-
-  return true
-}
-
-/**
-  * @param {number} col
-  * @param {string[][]} board
-  * @returns {number}
-  */
-function solve (col, board) {
-  if (col >= board.length) {
-    return 1
-  }
-
-  let count = 0
-  for (let r = 0; r < board.length; r += 1) {
-    if (isSafe(r, col, board)) {
-      const row = board[r]
-      row[col] = QUEEN
-      count += solve(col + 1, board)
-      row[col] = EMPTY
-    }
-  }
-
-  return count
-}
-
-/**
-  * @param {number} n
-  * @returns {number}
-  */
-function totalNQueens (n) {
-  /** @type {string[][]} */
-  const board = new Array(n)
-  for (let i = 0; i < n; i += 1) {
-    board[i] = new Array(n).fill(EMPTY)
-  }
-
-  return solve(0, board)
+function repeatedSubstringPattern (s) {
+  return s.repeat(2).slice(1, -1).includes(s)
 }
 
 function main () {
   const inputs = [
-    4, 1
+    'abab',
+    'aba',
+    'abcabcabcabc'
   ]
 
-  for (const n of inputs) {
-    const result = totalNQueens(n)
+  for (const s of inputs) {
+    const result = repeatedSubstringPattern(s)
     console.log(result)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,14 @@ function mod (a: number, b: number): number {
   return Number(BigInt(a) % BigInt(b))
 }
 
-function searchMatrix (matrix: number[][], target: number): boolean {
-  const rowCount = matrix.length
-  const colCount = matrix.at(0)?.length as number
-  let left = 0
-  let right = (rowCount * colCount) - 1
+function repeatedSubstringPattern (s: string): boolean {
+  for (let i = 1; i <= div(s.length, 2); i += 1) {
+    if (mod(s.length, i) !== 0) {
+      continue
+    }
 
-  while (left <= right) {
-    const mid = div(left + right, 2)
-    const row = div(mid, colCount)
-    const col = mod(mid, colCount)
-    const guess = matrix.at(row)?.at(col) as number
-    if (guess < target) {
-      left = mid + 1
-    } else if (guess > target) {
-      right = mid - 1
-    } else {
+    const pattern = s.substring(0, i).repeat(div(s.length, i))
+    if (pattern === s) {
       return true
     }
   }
@@ -32,13 +24,14 @@ function searchMatrix (matrix: number[][], target: number): boolean {
 }
 
 function main (): void {
-  const inputs: Array<[number[][], number]> = [
-    [[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3],
-    [[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13]
+  const inputs: string[] = [
+    'abab',
+    'aba',
+    'abcabcabcabc'
   ]
 
-  for (const [matrix, target] of inputs) {
-    const result = searchMatrix(matrix, target)
+  for (const s of inputs) {
+    const result = repeatedSubstringPattern(s)
     console.log(result)
   }
 }
