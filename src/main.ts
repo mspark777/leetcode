fbb245d9a81affd1b31a893019beddc2529598ef
mspark777 <topangel777@gmail.com>
2023-08-25 09:26:19 +0900

    97. Interleaving String

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,51 @@
 import '@total-typescript/ts-reset'
 
-function ncr (n: bigint, r: bigint): bigint {
-  let result = 1n
-  for (let i = 0n; i < r; i += 1n) {
-    result *= n - i
-    result /= i + 1n
-  }
-
-  return result
+function bat (arr: boolean[], i: number): boolean {
+  return arr.at(i) as boolean
 }
 
-function numTrees (n: number): number {
-  const num = BigInt(n)
-  return Number(ncr(2n * num, num) / (num + 1n))
+function sat (s: string, i: number): string {
+  return s.at(i) as string
+}
+
+function isInterleave (s1: string, s2: string, s3: string): boolean {
+  const s1len = s1.length
+  const s2len = s2.length
+  const s3len = s3.length
+  if ((s1len + s2len) !== s3len) {
+    return false
+  }
+
+  if (s1len < s2len) {
+    return isInterleave(s2, s1, s3)
+  }
+
+  const dp = new Array<boolean>(s2len + 1).fill(false)
+  dp[0] = true
+  for (let i = 1; i <= s2len; i += 1) {
+    dp[i] = (sat(s2, i - 1) === sat(s3, i - 1)) && bat(dp, i - 1)
+  }
+
+  for (let i = 1; i <= s1len; i += 1) {
+    dp[0] = (sat(s3, i - 1) === sat(s1, i - 1)) && bat(dp, 0)
+    for (let j = 1; j <= s2len; j += 1) {
+      dp[j] = (sat(s3, i + j - 1) === sat(s1, i - 1)) && bat(dp, j)
+      dp[j] = bat(dp, j) || ((sat(s3, i + j - 1) === sat(s2, j - 1)) && bat(dp, j - 1))
+    }
+  }
+
+  return bat(dp, -1)
 }
 
 function main (): void {
-  const inputs: number[] = [
-    3, 1, 4
+  const inputs: Array<[string, string, string]> = [
+    ['aabcc', 'dbbca', 'aadbbcbcac'],
+    ['aabcc', 'dbbca', 'aadbbbaccc'],
+    ['', '', '']
   ]
 
-  for (const n of inputs) {
-    const result = numTrees(n)
+  for (const [s1, s2, s3] of inputs) {
+    const result = isInterleave(s1, s2, s3)
     console.log(result)
   }
 }
