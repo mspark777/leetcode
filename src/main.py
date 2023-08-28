dfd9aebe2fd8771d7ba2686ceccae78a673e2ba1
mspark777 <topangel777@gmail.com>
2023-08-28 09:47:27 +0900

    225. Implement Stack using Queues

--- src/main.py
+++ src/main.py
@@ -1,28 +1,45 @@
 from __future__ import annotations
-from typing import List
 
 
-class Solution:
-    def findLongestChain(self, pairs: List[List[int]]) -> int:
-        pairs.sort(key=lambda x: x[1])
+class MyStack:
+    q0: list[int]
+    q1: list[int]
+    t: int
 
-        result = 0
-        pright = -1001
-        for [left, right] in pairs:
-            if pright < left:
-                result += 1
-                pright = right
+    def __init__(self):
+        self.q0 = []
+        self.q1 = []
+        self.t = 0
 
-        return result
+    def push(self, x: int) -> None:
+        self.q0.append(x)
+        self.t = x
+
+    def pop(self) -> int:
+        while len(self.q0) > 1:
+            self.t = self.q0.pop(0)
+            self.q1.append(self.t)
+
+        t = self.q0.pop(0)
+        temp = self.q0
+        self.q0 = self.q1
+        self.q1 = temp
+        return t
+
+    def top(self) -> int:
+        return self.t
+
+    def empty(self) -> bool:
+        return (len(self.q0) + len(self.q1)) < 1
 
 
 def main():
-    inputs = [[[1, 2], [2, 3], [3, 4]], [[1, 2], [7, 8], [4, 5]]]
-
-    for pairs in inputs:
-        solution = Solution()
-        result = solution.findLongestChain(pairs)
-        print(result)
+    stack = MyStack()
+    stack.push(1)
+    stack.push(2)
+    print(stack.top())
+    stack.push(3)
+    print(stack.top())
 
 
 if __name__ == "__main__":
