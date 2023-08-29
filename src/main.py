1c7d83a13e021ea1851dd2c455c42c7d23f9a2b8
mspark777 <topangel777@gmail.com>
2023-08-29 09:19:26 +0900

    2483. Minimum Penalty for a Shop

--- src/main.py
+++ src/main.py
@@ -1,45 +1,33 @@
 from __future__ import annotations
 
 
-class MyStack:
-    q0: list[int]
-    q1: list[int]
-    t: int
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        INCOME = "Y"
+        cur_penalty = customers.count(INCOME)
+        min_penalty = cur_penalty
+        result = 0
 
-    def __init__(self):
-        self.q0 = []
-        self.q1 = []
-        self.t = 0
+        for i, ch in enumerate(customers):
+            if ch == INCOME:
+                cur_penalty -= 1
+            else:
+                cur_penalty += 1
 
-    def push(self, x: int) -> None:
-        self.q0.append(x)
-        self.t = x
+            if cur_penalty < min_penalty:
+                min_penalty = cur_penalty
+                result = i + 1
 
-    def pop(self) -> int:
-        while len(self.q0) > 1:
-            self.t = self.q0.pop(0)
-            self.q1.append(self.t)
-
-        t = self.q0.pop(0)
-        temp = self.q0
-        self.q0 = self.q1
-        self.q1 = temp
-        return t
-
-    def top(self) -> int:
-        return self.t
-
-    def empty(self) -> bool:
-        return (len(self.q0) + len(self.q1)) < 1
+        return result
 
 
 def main():
-    stack = MyStack()
-    stack.push(1)
-    stack.push(2)
-    print(stack.top())
-    stack.push(3)
-    print(stack.top())
+    inputs = ["YYNY", "NNNNN", "YYYY"]
+
+    for customers in inputs:
+        solution = Solution()
+        result = solution.bestClosingTime(customers)
+        print(result)
 
 
 if __name__ == "__main__":
