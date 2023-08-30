1d50e33df942eb671d9cac2f425677d386e370d5
mspark777 <topangel777@gmail.com>
2023-08-30 15:00:42 +0900

    2366. Minimum Replacements to Sort the Array

--- src/main.py
+++ src/main.py
@@ -1,32 +1,30 @@
 from __future__ import annotations
+from typing import List
 
 
 class Solution:
-    def bestClosingTime(self, customers: str) -> int:
-        INCOME = "Y"
-        cur_penalty = customers.count(INCOME)
-        min_penalty = cur_penalty
+    def minimumReplacement(self, nums: List[int]) -> int:
         result = 0
 
-        for i, ch in enumerate(customers):
-            if ch == INCOME:
-                cur_penalty -= 1
-            else:
-                cur_penalty += 1
+        for i in range(len(nums) - 2, -1, -1):
+            n0 = nums[i]
+            n1 = nums[i + 1]
+            if n0 <= n1:
+                continue
 
-            if cur_penalty < min_penalty:
-                min_penalty = cur_penalty
-                result = i + 1
+            num_elements = (n0 + n1 - 1) // n1
+            result += num_elements - 1
+            nums[i] = n0 // num_elements
 
         return result
 
 
 def main():
-    inputs = ["YYNY", "NNNNN", "YYYY"]
+    inputs = [[3, 9, 3], [1, 2, 3, 4, 5]]
 
-    for customers in inputs:
+    for nums in inputs:
         solution = Solution()
-        result = solution.bestClosingTime(customers)
+        result = solution.minimumReplacement(nums)
         print(result)
 
 
