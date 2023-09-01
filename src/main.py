1b48e74a5f41adcc297fd6591a06a11cd57ffeb3
mspark777 <topangel777@gmail.com>
2023-09-01 09:46:02 +0900

    338. Counting Bits

--- src/main.py
+++ src/main.py
@@ -3,35 +3,20 @@ from typing import List
 
 
 class Solution:
-    def minTaps(self, n: int, ranges: List[int]) -> int:
-        max_reach = [0 for _ in range(n + 1)]
-        for i, r in enumerate(ranges):
-            start = max(0, i - r)
-            end = min(n, i + r)
-            max_reach[start] = max(max_reach[start], end)
+    def countBits(self, n: int) -> List[int]:
+        result = [0 for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            result[i] = result[i >> 1] + (i & 1)
 
-        taps = 0
-        curr_end = 0
-        next_end = 0
-        for i in range(n + 1):
-            if i > next_end:
-                return -1
-
-            if i > curr_end:
-                taps += 1
-                curr_end = next_end
-
-            next_end = max(next_end, max_reach[i])
-
-        return taps
+        return result
 
 
 def main():
-    inputs = [(5, [3, 4, 1, 1, 0, 0]), (3, [0, 0, 0, 0])]
+    inputs = [2, 5]
 
-    for n, ranges in inputs:
+    for n in inputs:
         solution = Solution()
-        result = solution.minTaps(n, ranges)
+        result = solution.countBits(n)
         print(result)
 
 
