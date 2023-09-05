b6742a8dc551057645af1fa560082e9c6827b667
mspark777 <topangel777@gmail.com>
2023-09-06 08:58:05 +0900

    82. Remove Duplicates from Sorted List II

--- src/main.py
+++ src/main.py
@@ -1,25 +1,64 @@
 from __future__ import annotations
-from typing import List
+from typing import Optional
+
+
+class ListNode:
+    val: int
+    next: Optional[ListNode]
+
+    def __init__(self, val: int = 0, next: Optional[ListNode] = None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        j = 1
-        for i in range(2, len(nums)):
-            if nums[i] != nums[j - 1]:
-                j += 1
-                nums[j] = nums[i]
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        node = dummy
 
-        return j + 1
+        while (node.next is not None) and (node.next.next is not None):
+            if node.next.val == node.next.next.val:
+                while (
+                    (node.next is not None)
+                    and (node.next.next is not None)
+                    and (node.next.val == node.next.next.val)
+                ):
+                    node.next = node.next.next
+                node.next = node.next.next
+            else:
+                node = node.next
+
+        return dummy.next
+
+
+def arrtolist(nums: list[int]) -> Optional[ListNode]:
+    dummy = ListNode()
+    tail = dummy
+    for num in nums:
+        next = ListNode(num)
+        tail.next = next
+        tail = next
+
+    return dummy.next
+
+
+def listtoarr(node: Optional[ListNode]) -> list[int]:
+    nums: list[int] = []
+
+    while node is not None:
+        nums.append(node.val)
+        node = node.next
+
+    return nums
 
 
 def main():
-    inputs = [[1, 1, 1, 2, 2, 3], [0, 0, 1, 1, 1, 1, 2, 3, 3]]
+    inputs = [[1, 2, 3, 3, 4, 4, 5], [1, 1, 1, 2, 3]]
 
     for nums in inputs:
         solution = Solution()
-        result = solution.removeDuplicates(nums)
-        print(result)
+        result = solution.deleteDuplicates(arrtolist(nums))
+        print(listtoarr(result))
 
 
 if __name__ == "__main__":
