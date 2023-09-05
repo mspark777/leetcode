1538a3ce1ab414212ef24b3b77b84a2c440d0677
mspark777 <topangel777@gmail.com>
2023-09-05 13:05:21 +0900

    138. Copy List with Random Pointer

--- src/main.py
+++ src/main.py
@@ -1,30 +1,63 @@
 from __future__ import annotations
-from typing import List
+from typing import Optional
+from copy import deepcopy
+
+
+class Node:
+    val: int
+    next: Optional[Node]
+    random: Optional[Node]
+
+    def __init__(
+        self, x: int, next: Optional[Node] = None, random: Optional[Node] = None
+    ):
+        self.val = int(x)
+        self.next = next
+        self.random = random
 
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        result: list[list[int]] = []
-        self.solve(0, nums, [], result)
+    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        return deepcopy(head)
 
-        return result
 
-    def solve(self, i: int, nums: list[int], temp: list[int], result: list[list[int]]):
-        result.append(temp.copy())
+def arrtolist(pairs: list[tuple[int, Optional[int]]]) -> Optional[Node]:
+    nodes: dict[int, Node] = {}
 
-        for j in range(i, len(nums)):
-            temp.append(nums[j])
-            self.solve(j + 1, nums, temp, result)
-            temp.pop()
+    dummy = Node(0)
+    tail = dummy
+    for i, (val, _) in enumerate(pairs):
+        next = Node(val)
+        tail.next = next
+        tail = next
+        nodes[i] = next
+
+    for i, (_, r) in enumerate(pairs):
+        if r is None:
+            continue
+        node = nodes[i]
+        node.random = nodes[r]
+
+    return dummy.next
+
+
+def listtoarr(node: Optional[Node]) -> list[int]:
+    pairs: list[int] = []
+    while node is not None:
+        pairs.append(node.val)
+        node = node.next
+
+    return pairs
 
 
 def main():
-    inputs = [[1, 2, 3], [0]]
+    inputs = [[(7, None), (13, 0), (11, 4), (10, 2), (1, 0)], [(1, 1), (2, 1)]]
 
-    for nums in inputs:
+    for pairs in inputs:
+        head = arrtolist(pairs)
         solution = Solution()
-        result = solution.subsets(nums)
-        print(result)
+        result = solution.copyRandomList(head)
+        print(listtoarr(result))
 
 
 if __name__ == "__main__":
