dada808f2d4af6d8ad60386a62ae2015f6b54afb
mspark777 <topangel777@gmail.com>
2023-09-07 12:16:58 +0900

    92. Reverse Linked List II

--- src/main.py
+++ src/main.py
@@ -1,29 +1,78 @@
 from __future__ import annotations
-from typing import List
+from typing import Optional
+
+
+class ListNode:
+    val: int
+    next: Optional[ListNode]
+
+    def __init__(self, val: int = 0, next: Optional[ListNode] = None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        stack: list[int] = []
-        result = 0
+    def reverseBetween(
+        self, head: Optional[ListNode], left: int, right: int
+    ) -> Optional[ListNode]:
+        if head is None:
+            return None
 
-        for i, h in enumerate(heights + [0]):
-            while stack and (heights[stack[-1]] >= h):
-                idx = stack.pop()
-                w = i if not stack else i - stack[-1] - 1
-                result = max(result, heights[idx] * w)
-            stack.append(i)
+        cur = head
+        prev: Optional[ListNode] = None
+        while (left > 1) and (cur is not None):
+            prev = cur
+            cur = cur.next
+            left -= 1
+            right -= 1
 
-        return result
+        tail = cur
+        con = prev
+        while (right > 0) and (cur is not None):
+            third = cur.next
+            cur.next = prev
+            prev = cur
+            cur = third
+            right -= 1
+
+        if con is not None:
+            con.next = prev
+        else:
+            head = prev
+
+        if tail is not None:
+            tail.next = cur
+
+        return head
+
+
+def arrtolist(nums: list[int]) -> Optional[ListNode]:
+    dummy = ListNode()
+    tail = dummy
+    for num in nums:
+        next = ListNode(num)
+        tail.next = next
+        tail = next
+
+    return dummy.next
+
+
+def listtoarr(node: Optional[ListNode]) -> list[int]:
+    nums: list[int] = []
+    while node is not None:
+        nums.append(node.val)
+        node = node.next
+
+    return nums
 
 
 def main():
-    inputs = [[2, 1, 5, 6, 2, 3], [2, 4]]
+    inputs = [([1, 2, 3, 4, 5], 2, 4), ([5], 1, 1)]
 
-    for heights in inputs:
+    for nums, left, right in inputs:
         solution = Solution()
-        result = solution.largestRectangleArea(heights)
-        print(result)
+        result = solution.reverseBetween(arrtolist(nums), left, right)
+        print(listtoarr(result))
 
 
 if __name__ == "__main__":
