1ea4293a20f72934eba3cbdce2e1c693fdcf4fad
mspark777 <topangel777@gmail.com>
2023-09-11 12:28:55 +0900

    89. Gray Code

--- src/main.py
+++ src/main.py
@@ -3,30 +3,21 @@ from typing import List
 
 
 class Solution:
-    def groupThePeople(self, group_sizes: List[int]) -> List[List[int]]:
-        result: list[list[int]] = []
-
-        groups: dict[int, list[int]] = {}
-        for i, size in enumerate(group_sizes):
-            if size not in groups:
-                groups[size] = []
-
-            group = groups[size]
-            group.append(i)
-
-            if len(group) == size:
-                result.append(group)
-                groups.pop(size)
+    def grayCode(self, n: int) -> List[int]:
+        result: list[int] = []
+        p = 1 << n
+        for i in range(p):
+            result.append(i ^ (i >> 1))
 
         return result
 
 
 def main():
-    inputs = [[3, 3, 3, 3, 3, 1, 3], [2, 1, 3, 3, 3, 2]]
+    inputs = [2, 1]
 
-    for group_sizes in inputs:
+    for n in inputs:
         solution = Solution()
-        result = solution.groupThePeople(group_sizes)
+        result = solution.grayCode(n)
         print(result)
 
 
