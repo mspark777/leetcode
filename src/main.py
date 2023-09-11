a0d70c063f8e8b16fdcaa408d91ffbf38f0c8424
mspark777 <topangel777@gmail.com>
2023-09-11 10:06:25 +0900

    1282. Group the People Given the Group Size They Belong To

--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
 from __future__ import annotations
+from typing import List
 
 
 class Solution:
-    def countOrders(self, n: int) -> int:
-        result = 1
-        MOD = (10**9) + 7
-        for i in range(2, n + 1):
-            result = (result * (2 * i - 1) * i) % MOD
+    def groupThePeople(self, group_sizes: List[int]) -> List[List[int]]:
+        result: list[list[int]] = []
+
+        groups: dict[int, list[int]] = {}
+        for i, size in enumerate(group_sizes):
+            if size not in groups:
+                groups[size] = []
+
+            group = groups[size]
+            group.append(i)
+
+            if len(group) == size:
+                result.append(group)
+                groups.pop(size)
 
         return result
 
 
 def main():
-    inputs = [1, 2, 3]
+    inputs = [[3, 3, 3, 3, 3, 1, 3], [2, 1, 3, 3, 3, 2]]
 
-    for n in inputs:
+    for group_sizes in inputs:
         solution = Solution()
-        result = solution.countOrders(n)
+        result = solution.groupThePeople(group_sizes)
         print(result)
 
 
