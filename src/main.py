47f651ec5c594c780d9156c98274c2211c18520a
mspark777 <topangel777@gmail.com>
2023-09-17 15:02:20 +0900

    847. Shortest Path Visiting All Nodes

--- src/main.py
+++ src/main.py
@@ -1,68 +1,52 @@
 from __future__ import annotations
-from queue import PriorityQueue
+from collections import deque
+
+
+class Node:
+    node: int
+    mask: int
+    cost: int
+
+    def __init__(self, node: int, mask: int, cost: int):
+        self.node = node
+        self.mask = mask
+        self.cost = cost
 
 
 class Solution:
-    def minimumEffortPath(self, heights: list[list[int]]) -> int:
-        efforts = [[2**31 for _ in row] for row in heights]
-        efforts[0][0] = 0
+    def shortestPathLength(self, graph: list[list[int]]) -> int:
+        node_count = len(graph)
+        visited = set[tuple[int, int]]()
+        all = (1 << node_count) - 1
+        queue = deque[Node]()
 
-        return self.dijkstra(heights, efforts)
+        for i in range(node_count):
+            mask = 1 << i
+            queue.append(Node(i, mask, 1))
+            visited.add((i, mask))
 
-    def dijkstra(self, heights: list[list[int]], efforts: list[list[int]]) -> int:
-        row_count = len(heights)
-        col_count = len(heights[0])
+        while queue:
+            curr = queue.popleft()
+            if curr.mask == all:
+                return curr.cost - 1
 
-        queue = PriorityQueue[tuple[int, tuple[int, int]]]()
-        queue.put((0, (0, 0)))
+            for adj in graph[curr.node]:
+                visited_mask = curr.mask | (1 << adj)
+                this_node = Node(adj, visited_mask, curr.cost + 1)
 
-        dirs: list[tuple[int, int]] = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+                if (adj, visited_mask) not in visited:
+                    visited.add((adj, visited_mask))
+                    queue.append(this_node)
 
-        while not queue.empty():
-            (cost, (r, c)) = queue.get()
-
-            if cost > efforts[r][c]:
-                continue
-
-            if ((r + 1) == row_count) and ((c + 1) == col_count):
-                return cost
-
-            for dr, dc in dirs:
-                new_r = r + dr
-                new_c = c + dc
-
-                if (new_r < 0) or (new_c < 0):
-                    continue
-                elif (new_r >= row_count) or (new_c >= col_count):
-                    continue
-
-                new_effort = max(
-                    efforts[r][c], abs(heights[r][c] - heights[new_r][new_c])
-                )
-
-                if new_effort < efforts[new_r][new_c]:
-                    efforts[new_r][new_c] = new_effort
-                    queue.put((new_effort, (new_r, new_c)))
-
-        return efforts[-1][-1]
+        return -1
 
 
 def main():
-    inputs = [
-        [[1, 2, 2], [3, 8, 2], [5, 3, 5]],
-        [[1, 2, 3], [3, 8, 4], [5, 3, 5]],
-        [
-            [1, 2, 1, 1, 1],
-            [1, 2, 1, 2, 1],
-            [1, 2, 1, 2, 1],
-            [1, 2, 1, 2, 1],
-            [1, 1, 1, 2, 1],
-        ],
-    ]
+    inputs = [[[1, 2, 3], [0], [0], [0]], [[1], [0, 2, 4], [1, 3, 4], [2], [1, 2]]]
 
-    for heights in inputs:
+    for graph in inputs:
         solution = Solution()
-        result = solution.minimumEffortPath(heights)
+        result = solution.shortestPathLength(graph)
         print(result)
 
 
