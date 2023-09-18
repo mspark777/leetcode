ba9e4145f1417b62bd52b0319c3ba10f307e673f
mspark777 <topangel777@gmail.com>
2023-09-18 22:01:51 +0900

    1337. The K Weakest Rows in a Matrix

--- src/main.py
+++ src/main.py
@@ -1,69 +1,31 @@
 from __future__ import annotations
-from typing import Optional
-from collections import deque
-
-
-class TreeNode:
-    val: int
-    left: Optional[TreeNode]
-    right: Optional[TreeNode]
-
-    def __init__(
-        self,
-        val: int = 0,
-        left: Optional[TreeNode] = None,
-        right: Optional[TreeNode] = None,
-    ):
-        self.val = val
-        self.left = left
-        self.right = right
 
 
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
-        if root is None:
-            return []
-
-        result: list[list[int]] = []
-        node_queue = deque[tuple[TreeNode, int]]([(root, 0)])
-        prev_level: Optional[int] = None
-        level_nodes = deque[int]()
-
-        while node_queue:
-            node, level = node_queue.popleft()
-            if prev_level is not None:
-                if prev_level != level:
-                    result.append(list(level_nodes))
-                    level_nodes = deque()
-
-            if (level & 1) == 0:
-                level_nodes.append(node.val)
-            else:
-                level_nodes.appendleft(node.val)
-            prev_level = level
-
-            if node.left is not None:
-                node_queue.append((node.left, level + 1))
-
-            if node.right is not None:
-                node_queue.append((node.right, level + 1))
-
-        if level_nodes:
-            result.append(list(level_nodes))
-
-        return result
+    def kWeakestRows(self, mat: list[list[int]], k: int) -> list[int]:
+        rows = [(sum(row), i) for i, row in enumerate(mat)]
+        rows.sort(key=lambda x: (x[0], x[1]))
+        return [x[1] for x in rows[:k]]
 
 
 def main():
     inputs = [
-        TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))),
-        TreeNode(1),
-        None,
+        (
+            [
+                [1, 1, 0, 0, 0],
+                [1, 1, 1, 1, 0],
+                [1, 0, 0, 0, 0],
+                [1, 1, 0, 0, 0],
+                [1, 1, 1, 1, 1],
+            ],
+            3,
+        ),
+        ([[1, 0, 0, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 0]], 2),
     ]
 
-    for root in inputs:
+    for mat, k in inputs:
         solution = Solution()
-        result = solution.zigzagLevelOrder(root)
+        result = solution.kWeakestRows(mat, k)
         print(result)
 
 
