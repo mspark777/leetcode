196a6ae7772c9886ecb2a3defdd7fd48e774e55b
mspark777 <topangel777@gmail.com>
2023-09-22 12:59:04 +0900

    392. Is Subsequence

--- src/main.py
+++ src/main.py
@@ -3,28 +3,24 @@ from typing import Optional, List
 
 
 class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        slen = len(s)
-        tlen = len(t)
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s == "":
+            return True
+        elif t == "":
+            return False
 
-        dp = [[0 if i != 0 else 1 for i in range(tlen + 1)] for _ in range(slen + 1)]
+        if s[0] == t[0]:
+            return self.isSubsequence(s[1:], t[1:])
 
-        for i in range(1, slen + 1):
-            for j in range(1, tlen + 1):
-                if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j]
-
-        return dp[slen][tlen]
+        return self.isSubsequence(s, t[1:])
 
 
 def main():
-    inputs = [("rabbbit", "rabbit"), ("babgbag", "bag")]
+    inputs = [("abc", "ahbgdc"), ("axc", "ahbgdc")]
 
     for s, t in inputs:
         solution = Solution()
-        result = solution.numDistinct(s, t)
+        result = solution.isSubsequence(s, t)
         print(result)
 
 
