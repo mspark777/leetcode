9e2ba00d8852c237514d3d380a326e8230cfab07
mspark777 <topangel777@gmail.com>
2023-09-27 08:39:47 +0900

    123. Best Time to Buy and Sell Stock III

--- src/main.py
+++ src/main.py
@@ -4,30 +4,36 @@ from typing import Optional, List
 
 
 class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        counts = Counter(s)
-        result = list[str]()
-        visited = set[str]()
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        cur = [[0] * 3 for _ in range(2)]
+        next = [[0] * 3 for _ in range(2)]
 
-        for ch in s:
-            counts[ch] -= 1
-            if ch in visited:
-                continue
-            while result and (ch < result[-1]) and (counts[result[-1]] > 0):
-                visited.remove(result.pop())
+        for idx in range(n - 1, -1, -1):
+            for buy in range(2):
+                for limits in range(1, 3):
+                    profit = 0
+                    if buy == 0:
+                        buy_profit = prices[idx] + next[1][limits - 1]
+                        skip_profit = next[0][limits]
+                        profit = max(buy_profit, skip_profit)
+                    else:
+                        sell_profit = next[0][limits] - prices[idx]
+                        skip_profit = next[1][limits]
+                        profit = max(sell_profit, skip_profit)
 
-            visited.add(ch)
-            result.append(ch)
+                    cur[buy][limits] = profit
+            next = cur.copy()
 
-        return "".join(result)
+        return next[1][2]
 
 
 def main():
-    inputs = ["bcabc", "cbacdcbc"]
+    inputs = [[3, 3, 5, 0, 0, 3, 1, 4], [1, 2, 3, 4, 5], [7, 6, 4, 3, 1]]
 
-    for s in inputs:
+    for prices in inputs:
         solution = Solution()
-        result = solution.removeDuplicateLetters(s)
+        result = solution.maxProfit(prices)
         print(result)
 
 
