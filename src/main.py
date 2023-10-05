06cacc4b4d9b2b66a17b25dc70461850b06c5562
mspark777 <topangel777@gmail.com>
2023-10-06 08:42:20 +0900

    127. Word Ladder

--- src/main.py
+++ src/main.py
@@ -3,37 +3,64 @@ from typing import Optional, List
 
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return []
+    def ladderLength(self, begin_word: str, end_word: str, words: List[str]) -> int:
+        word_set = set(words)
+        if end_word not in word_set:
+            return 0
 
-        candidate0 = 0
-        candidate1 = 1
-        count0 = 0
-        count1 = 0
+        visited = set([begin_word])
+        queue = [begin_word]
 
-        for num in nums:
-            if num == candidate0:
-                count0 += 1
-            elif num == candidate1:
-                count1 += 1
-            elif count0 == 0:
-                candidate0 = num
-                count0 = 1
-            elif count1  == 0:
-                candidate1 = num
-                count1 = 1
-            else:
-                count0 -= 1
-                count1 -= 1
+        result = 1
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                word = queue.pop(0)
+                if word == end_word:
+                    return result
 
-        return [num for num in (candidate0, candidate1) if nums.count(num) > len(nums) // 3]
+                for j in range(len(word)):
+                    for k in range(ord("a"), ord("z") + 1):
+                        chs = list(word)
+                        chs[j] = chr(k)
+                        w = "".join(chs)
+                        if w not in word_set:
+                            continue
+                        elif w in visited:
+                            continue
+
+                        queue.append(w)
+                        visited.add(w)
+            result += 1
+
+        return 0
+
 
 def main():
-    inputs = ([3, 2, 3], [1], [1, 2])
+    inputs = (
+        ("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]),
+        ("hit", "cog", ["hot", "dot", "dog", "lot", "log"]),
+        (
+            "ymain",
+            "oecij",
+            [
+                "ymann",
+                "yycrj",
+                "oecij",
+                "ymcnj",
+                "yzcrj",
+                "yycij",
+                "xecij",
+                "yecij",
+                "ymanj",
+                "yzcnj",
+                "ymain",
+            ],
+        ),
+    )
 
-    for nums in inputs:
-        result = Solution().majorityElement(nums)
+    for begin, end, words in inputs:
+        result = Solution().ladderLength(begin, end, words)
         print(result)
 
 
