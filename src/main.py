95647684bcf52ae9f5937e0c78bf8098a83830ef
mspark777 <topangel777@gmail.com>
2023-10-05 09:00:06 +0900

    124. Binary Tree Maximum Path Sum

--- src/main.py
+++ src/main.py
@@ -2,32 +2,58 @@ from __future__ import annotations
 from typing import Optional, List
 
 
-class MyHashMap:
-    nums: list[int]
+class TreeNode:
+    val: int
+    left: Optional[TreeNode]
+    right: Optional[TreeNode]
+
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional[TreeNode] = None,
+        right: Optional[TreeNode] = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Dfs:
+    result: int
 
     def __init__(self):
-        self.nums = [-1] * 1000001
+        self.result = -2147483647
 
-    def put(self, key: int, value: int) -> None:
-        self.nums[key] = value
+    def dfs(self, node: Optional[TreeNode]) -> int:
+        if node is None:
+            return 0
 
-    def get(self, key: int) -> int:
-        return self.nums[key]
+        left = max(0, self.dfs(node.left))
+        right = max(0, self.dfs(node.right))
 
-    def remove(self, key: int) -> None:
-        self.nums[key] = -1
+        path0 = node.val + left + right
+        path1 = node.val + max(left, right)
+        self.result = max(self.result, path0, path1)
+        return path1
+
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        dfs = Dfs()
+        dfs.dfs(root)
+        return dfs.result
 
 
 def main():
-    obj = MyHashMap()
-    obj.put(1, 1)
-    obj.put(2, 2)
-    print(obj.get(1))
-    print(obj.get(3))
-    obj.put(2, 1)
-    print(obj.get(2))
-    obj.remove(2)
-    print(obj.get(2))
+    inputs = (
+        TreeNode(1, TreeNode(2), TreeNode(3)),
+        TreeNode(-10, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))),
+        TreeNode(2, TreeNode(-1), TreeNode(-2)),
+    )
+
+    for root in inputs:
+        result = Solution().maxPathSum(root)
+        print(result)
 
 
 if __name__ == "__main__":
