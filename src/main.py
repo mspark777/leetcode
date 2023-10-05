893720ab0b0c94a4669e690d05bb624c8beeb9fd
mspark777 <topangel777@gmail.com>
2023-10-05 22:21:35 +0900

    229. Majority Element II

--- src/main.py
+++ src/main.py
@@ -2,57 +2,38 @@ from __future__ import annotations
 from typing import Optional, List
 
 
-class TreeNode:
-    val: int
-    left: Optional[TreeNode]
-    right: Optional[TreeNode]
-
-    def __init__(
-        self,
-        val: int = 0,
-        left: Optional[TreeNode] = None,
-        right: Optional[TreeNode] = None,
-    ):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Dfs:
-    result: int
-
-    def __init__(self):
-        self.result = -2147483647
-
-    def dfs(self, node: Optional[TreeNode]) -> int:
-        if node is None:
-            return 0
-
-        left = max(0, self.dfs(node.left))
-        right = max(0, self.dfs(node.right))
-
-        path0 = node.val + left + right
-        path1 = node.val + max(left, right)
-        self.result = max(self.result, path0, path1)
-        return path1
-
-
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        dfs = Dfs()
-        dfs.dfs(root)
-        return dfs.result
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
 
+        candidate0 = 0
+        candidate1 = 1
+        count0 = 0
+        count1 = 0
+
+        for num in nums:
+            if num == candidate0:
+                count0 += 1
+            elif num == candidate1:
+                count1 += 1
+            elif count0 == 0:
+                candidate0 = num
+                count0 = 1
+            elif count1  == 0:
+                candidate1 = num
+                count1 = 1
+            else:
+                count0 -= 1
+                count1 -= 1
+
+        return [num for num in (candidate0, candidate1) if nums.count(num) > len(nums) // 3]
 
 def main():
-    inputs = (
-        TreeNode(1, TreeNode(2), TreeNode(3)),
-        TreeNode(-10, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))),
-        TreeNode(2, TreeNode(-1), TreeNode(-2)),
-    )
+    inputs = ([3, 2, 3], [1], [1, 2])
 
-    for root in inputs:
-        result = Solution().maxPathSum(root)
+    for nums in inputs:
+        result = Solution().majorityElement(nums)
         print(result)
 
 
