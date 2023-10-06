79cb33ffb2a9e1572734a7bbb81486ed21d9a620
mspark777 <topangel777@gmail.com>
2023-10-06 13:07:14 +0900

    343. Integer Break

--- src/main.py
+++ src/main.py
@@ -3,64 +3,24 @@ from typing import Optional, List
 
 
 class Solution:
-    def ladderLength(self, begin_word: str, end_word: str, words: List[str]) -> int:
-        word_set = set(words)
-        if end_word not in word_set:
-            return 0
+    def integerBreak(self, n: int) -> int:
+        if n <= 3:
+            return n - 1
 
-        visited = set([begin_word])
-        queue = [begin_word]
+        if (n % 3) == 0:
+            return 3 ** (n // 3)
 
-        result = 1
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                word = queue.pop(0)
-                if word == end_word:
-                    return result
+        if (n % 3) == 1:
+            return 3 ** ((n // 3) - 1) * 4
 
-                for j in range(len(word)):
-                    for k in range(ord("a"), ord("z") + 1):
-                        chs = list(word)
-                        chs[j] = chr(k)
-                        w = "".join(chs)
-                        if w not in word_set:
-                            continue
-                        elif w in visited:
-                            continue
-
-                        queue.append(w)
-                        visited.add(w)
-            result += 1
-
-        return 0
+        return 3 ** (n // 3) * 2
 
 
 def main():
-    inputs = (
-        ("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]),
-        ("hit", "cog", ["hot", "dot", "dog", "lot", "log"]),
-        (
-            "ymain",
-            "oecij",
-            [
-                "ymann",
-                "yycrj",
-                "oecij",
-                "ymcnj",
-                "yzcrj",
-                "yycij",
-                "xecij",
-                "yecij",
-                "ymanj",
-                "yzcnj",
-                "ymain",
-            ],
-        ),
-    )
+    inputs = (2, 10)
 
-    for begin, end, words in inputs:
-        result = Solution().ladderLength(begin, end, words)
+    for n in inputs:
+        result = Solution().integerBreak(n)
         print(result)
 
 
