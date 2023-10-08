617a68bf80534668de5f8f040787630e72af53ed
mspark777 <topangel777@gmail.com>
2023-10-08 14:35:54 +0900

    1458. Max Dot Product of Two Subsequences

--- src/main.py
+++ src/main.py
@@ -3,24 +3,49 @@ from typing import Optional, List
 
 
 class Solution:
-    def integerBreak(self, n: int) -> int:
-        if n <= 3:
-            return n - 1
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        max0 = -2147483648
+        max1 = -2147483648
+        min0 = 2147483647
+        min1 = 2147483647
 
-        if (n % 3) == 0:
-            return 3 ** (n // 3)
+        for num in nums1:
+            max0 = max(max0, num)
+            min0 = min(min0, num)
 
-        if (n % 3) == 1:
-            return 3 ** ((n // 3) - 1) * 4
+        for num in nums2:
+            max1 = max(max1, num)
+            min1 = min(min1, num)
 
-        return 3 ** (n // 3) * 2
+        if (max0 < 0) and (min1 > 0):
+            return max0 * min1
+
+        if (min0 > 0) and (max1 < 0):
+            return min0 * max1
+
+        m = len(nums2) + 1
+        prev_dp = [0] * m
+        dp = prev_dp
+        for i in range(len(nums1) - 1, -1, -1):
+            dp = [0] * m
+            for j in range(m - 2, -1, -1):
+                use = nums1[i] * nums2[j] + prev_dp[j + 1]
+                dp[j] = max(use, max(prev_dp[j], dp[j + 1]))
+
+            prev_dp = dp
+
+        return dp[0]
 
 
 def main():
-    inputs = (2, 10)
+    inputs = (
+        ([2, 1, -2, 5], [3, 0, -6]),
+        ([3, -2], [2, -6, 7]),
+        ([-1, -1], [1, 1]),
+    )
 
-    for n in inputs:
-        result = Solution().integerBreak(n)
+    for nums1, nums2 in inputs:
+        result = Solution().maxDotProduct(nums1, nums2)
         print(result)
 
 
