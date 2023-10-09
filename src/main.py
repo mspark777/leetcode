7c0775c16ad9bb12841f932dcf90815c17a37e35
mspark777 <topangel777@gmail.com>
2023-10-09 15:40:44 +0900

    34. Find First and Last Position of Element in Sorted Array

--- src/main.py
+++ src/main.py
@@ -3,49 +3,40 @@ from typing import Optional, List
 
 
 class Solution:
-    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        max0 = -2147483648
-        max1 = -2147483648
-        min0 = 2147483647
-        min1 = 2147483647
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first = self.search(nums, -1, 0, len(nums) - 1, target, True)
+        second = self.search(nums, -1, 0, len(nums) - 1, target, False)
+        return [first, second]
 
-        for num in nums1:
-            max0 = max(max0, num)
-            min0 = min(min0, num)
+    def search(
+        self,
+        nums: list[int],
+        result: int,
+        left: int,
+        right: int,
+        target: int,
+        first: bool,
+    ) -> int:
+        if left > right:
+            return result
 
-        for num in nums2:
-            max1 = max(max1, num)
-            min1 = min(min1, num)
+        mid = (left + right) // 2
+        num = nums[mid]
+        if num > target:
+            return self.search(nums, result, left, mid - 1, target, first)
+        elif num < target:
+            return self.search(nums, result, mid + 1, right, target, first)
+        elif first:
+            return self.search(nums, mid, left, mid - 1, target, first)
 
-        if (max0 < 0) and (min1 > 0):
-            return max0 * min1
-
-        if (min0 > 0) and (max1 < 0):
-            return min0 * max1
-
-        m = len(nums2) + 1
-        prev_dp = [0] * m
-        dp = prev_dp
-        for i in range(len(nums1) - 1, -1, -1):
-            dp = [0] * m
-            for j in range(m - 2, -1, -1):
-                use = nums1[i] * nums2[j] + prev_dp[j + 1]
-                dp[j] = max(use, max(prev_dp[j], dp[j + 1]))
-
-            prev_dp = dp
-
-        return dp[0]
+        return self.search(nums, mid, mid + 1, right, target, first)
 
 
 def main():
-    inputs = (
-        ([2, 1, -2, 5], [3, 0, -6]),
-        ([3, -2], [2, -6, 7]),
-        ([-1, -1], [1, 1]),
-    )
+    inputs = (([5, 7, 7, 8, 8, 10], 8), ([5, 7, 7, 8, 8, 10], 6), ([], 0))
 
-    for nums1, nums2 in inputs:
-        result = Solution().maxDotProduct(nums1, nums2)
+    for nums, target in inputs:
+        result = Solution().searchRange(nums, target)
         print(result)
 
 
