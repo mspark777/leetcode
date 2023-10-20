f4f0962ceaf412fecdb58baddfebcb2b508d11d7
mspark777 <topangel777@gmail.com>
2023-10-20 12:29:33 +0900

    341. Flatten Nested List Iterator

--- src/main.py
+++ src/main.py
@@ -2,29 +2,55 @@ from __future__ import annotations
 from typing import Optional, List
 
 
-class Solution:
-    def wordBreak(self, s: str, words: List[str]) -> List[str]:
-        word_set = set(words)
-        n = len(s)
-        dp: list[list[str]] = [[] for _ in range(n + 1)]
-        dp[0].append("")
+class NestedInteger:
+    def isInteger(self) -> bool:
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        """
+        return False
 
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                temp = s[i:j]
-                if temp in word_set:
-                    for t in dp[i]:
-                        dp[j].append(t + (" " if t != "" else t) + temp)
+    def getInteger(self) -> int:
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        """
+        return 0
 
-        return dp[n]
+    def getList(self) -> list[NestedInteger]:
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+        """
+        return []
+
+
+class NestedIterator:
+    stack: list[NestedInteger]
+
+    def __init__(self, nestedList: list[NestedInteger]):
+        self.stack = nestedList[::-1]
+
+    def next(self) -> int:
+        return self.stack.pop().getInteger()
+
+    def hasNext(self) -> bool:
+        while self.stack:
+            top = self.stack[-1]
+            if top.isInteger():
+                return True
+
+            self.stack = self.stack[:-1] + top.getList()[::-1]
+
+        return False
+
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
 
 
 def main():
-    inputs = (
-        ("catsanddog", ["cat", "cats", "and", "sand", "dog"]),
-        ("pineapplepenapple", ["apple", "pen", "applepen", "pine", "pineapple"]),
-        ("catsandog", ["cats", "dog", "sand", "and", "cat"]),
-    )
+    inputs = ([[1, 1], 2, [1, 1]], [1, [4, [6]]])
 
     for s, words in inputs:
         result = Solution().wordBreak(s, words)
