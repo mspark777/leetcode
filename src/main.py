54f3acb8a8d8ef06b8415c7f7e0c4a755c2c2715
mspark777 <topangel777@gmail.com>
2023-10-24 09:40:14 +0900

    515. Find Largest Value in Each Tree Row

--- src/main.py
+++ src/main.py
@@ -2,23 +2,55 @@ from __future__ import annotations
 from typing import Optional, List
 
 
-class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if n == 1:
-            return True
-        elif n == 0:
-            return False
-        elif n % 4 != 0:
-            return False
+class TreeNode:
+    val: int
+    left: Optional[TreeNode]
+    right: Optional[TreeNode]
 
-        return self.isPowerOfFour(n // 4)
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional[TreeNode] = None,
+        right: Optional[TreeNode] = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        result: list[int] = []
+        stack: list[tuple[TreeNode, int]] = [(root, 0)]
+
+        while stack:
+            node, depth = stack.pop()
+            if depth == len(result):
+                result.append(node.val)
+            else:
+                result[depth] = max(result[depth], node.val)
+
+            if node.left is not None:
+                stack.append((node.left, depth + 1))
+            if node.right is not None:
+                stack.append((node.right, depth + 1))
+
+        return result
 
 
 def main():
-    inputs = (16, 5, 1)
+    inputs = (
+        TreeNode(
+            1, TreeNode(3, TreeNode(5), TreeNode(3)), TreeNode(2, right=TreeNode(9))
+        ),
+        TreeNode(1, TreeNode(2), TreeNode(3)),
+    )
 
-    for n in inputs:
-        result = Solution().isPowerOfFour(n)
+    for root in inputs:
+        result = Solution().largestValues(root)
         print(result)
 
 
