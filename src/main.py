298d1ed99ddfdf1b575b28f1fa0bf44c72293d39
mspark777 <topangel777@gmail.com>
2023-10-30 22:46:10 +0900

    155. Min Stack

--- src/main.py
+++ src/main.py
@@ -2,21 +2,49 @@ from __future__ import annotations
 from typing import Optional, List
 
 
-class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
-        arr.sort(key=lambda n: (n.bit_count(), n))
-        return arr
+class Node:
+    val: int
+    minval: int
+    next: Optional[Node]
+
+    def __init__(self, val: int, minval: int, next: Optional[Node]):
+        self.val = val
+        self.minval = minval
+        self.next = next
+
+
+class MinStack:
+    head: Optional[Node]
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, val: int) -> None:
+        if self.head is None:
+            self.head = Node(val, val, None)
+        else:
+            self.head = Node(val, min(val, self.head.minval), self.head)
+
+    def pop(self) -> None:
+        if self.head is not None:
+            self.head = self.head.next
+
+    def top(self) -> int:
+        return self.head.val if self.head is not None else 0
+
+    def getMin(self) -> int:
+        return self.head.minval if self.head is not None else 0
 
 
 def main():
-    inputs = (
-        [0, 1, 2, 3, 4, 5, 6, 7, 8],
-        [1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1],
-    )
-
-    for arr in inputs:
-        result = Solution().sortByBits(arr)
-        print(result)
+    stack = MinStack()
+    stack.push(-2)
+    stack.push(0)
+    stack.push(-3)
+    print(stack.getMin())
+    stack.pop()
+    print(stack.top())
+    print(stack.getMin())
 
 
 if __name__ == "__main__":
