5ee7533a1c1d410f08888747a9de1b54b588edba
mspark777 <topangel777@gmail.com>
2023-11-30 09:24:51 +0900

    1611. Minimum One Bit Operations to Make Integers Zero

--- src/py/main.py
+++ src/py/main.py
@@ -2,20 +2,20 @@ from __future__ import annotations
 
 
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        result = 0
-        while n != 0:
-            n &= n - 1
-            result += 1
-
-        return result
+    def minimumOneBitOperations(self, n: int) -> int:
+        n ^= n >> 16
+        n ^= n >> 8
+        n ^= n >> 4
+        n ^= n >> 2
+        n ^= n >> 1
+        return n
 
 
 def main():
-    inputs = (0b1011, 0b10000000, 0b11111111111111111111111111111101)
+    inputs = (3, 6)
 
     for n in inputs:
-        result = Solution().hammingWeight(n)
+        result = Solution().minimumOneBitOperations(n)
         print(result)
 
 
