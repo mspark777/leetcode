4abea2a437f55691c68dd76f340ad869af4040bf
mspark777 <topangel777@gmail.com>
2023-12-02 23:29:38 +0900

    1160. Find Words That Can Be Formed by Characters

--- src/py/main.py
+++ src/py/main.py
@@ -1,21 +1,42 @@
 from __future__ import annotations
+from typing import List
+from collections import defaultdict
 
 
 class Solution:
-    def minimumOneBitOperations(self, n: int) -> int:
-        n ^= n >> 16
-        n ^= n >> 8
-        n ^= n >> 4
-        n ^= n >> 2
-        n ^= n >> 1
-        return n
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        counts = defaultdict[str, int](int)
+
+        for ch in chars:
+            counts[ch] += 1
+
+        result = 0
+
+        for word in words:
+            wcounts = defaultdict[str, int](int)
+            for w in word:
+                wcounts[w] += 1
+
+            good = True
+            for c, freq in wcounts.items():
+                if counts[c] < freq:
+                    good = False
+                    break
+
+            if good:
+                result += len(word)
+
+        return result
 
 
 def main():
-    inputs = (3, 6)
+    inputs = (
+        (["cat", "bt", "hat", "tree"], "atach"),
+        (["hello", "world", "leetcode"], "welldonehoneyr"),
+    )
 
-    for n in inputs:
-        result = Solution().minimumOneBitOperations(n)
+    for words, chars in inputs:
+        result = Solution().countCharacters(words, chars)
         print(result)
 
 
