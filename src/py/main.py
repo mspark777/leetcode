aec067e805a5a2606f142ee618753c96c672dfa2
mspark777 <topangel777@gmail.com>
2023-12-05 09:29:56 +0900

    1688. Count of Matches in Tournament

--- src/py/main.py
+++ src/py/main.py
@@ -2,22 +2,15 @@ from __future__ import annotations
 
 
 class Solution:
-    def largestGoodInteger(self, num: str) -> str:
-        result = -1
-
-        for i in range(len(num) - 2):
-            if num[i] == num[i + 1] == num[i + 2]:
-                n = int(num[i])
-                result = max(n, result)
-
-        return str(result) * 3 if result >= 0 else ""
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
 
 
 def main():
-    inputs = ("6777133339", "2300019", "42352338")
+    inputs = (7, 14)
 
-    for num in inputs:
-        result = Solution().largestGoodInteger(num)
+    for n in inputs:
+        result = Solution().numberOfMatches(n)
         print(result)
 
 
