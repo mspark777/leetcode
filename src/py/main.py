1f0f72ec80720e0d50078bf4a9d74d5b012d593e
mspark777 <topangel777@gmail.com>
2023-12-05 17:17:52 +0900

    150. Evaluate Reverse Polish Notation

--- src/py/main.py
+++ src/py/main.py
@@ -1,16 +1,44 @@
 from __future__ import annotations
+from typing import List
 
 
 class Solution:
-    def numberOfMatches(self, n: int) -> int:
-        return n - 1
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack: list[int] = []
+
+        for token in tokens:
+            if token == "+":
+                left, right = self.pop(stack)
+                stack.append(left + right)
+            elif token == "-":
+                left, right = self.pop(stack)
+                stack.append(left - right)
+            elif token == "*":
+                left, right = self.pop(stack)
+                stack.append(left * right)
+            elif token == "/":
+                left, right = self.pop(stack)
+                stack.append(int(left / right))
+            else:
+                stack.append(int(token))
+
+        return stack[0]
+
+    def pop(self, stack: list[int]) -> tuple[int, int]:
+        right = stack.pop()
+        left = stack.pop()
+        return (left, right)
 
 
 def main():
-    inputs = (7, 14)
+    inputs = (
+        ["2", "1", "+", "3", "*"],
+        ["4", "13", "5", "/", "+"],
+        ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"],
+    )
 
-    for n in inputs:
-        result = Solution().numberOfMatches(n)
+    for tokens in inputs:
+        result = Solution().evalRPN(tokens)
         print(result)
 
 
