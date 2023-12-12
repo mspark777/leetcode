a44a0182ae655a4468f7277329b6af245a22a955
mspark777 <topangel777@gmail.com>
2023-12-12 23:32:01 +0900

    164. Maximum Gap

--- src/py/main.py
+++ src/py/main.py
@@ -1,47 +1,100 @@
 from __future__ import annotations
+from typing import List
+from math import ceil
 
 
 class Solution:
-    def toHex(self, num: int) -> str:
-        if num == 0:
-            return "0"
+    def maximumGap(self, nums: List[int]) -> int:
+        min_num = min(nums)
+        max_num = max(nums)
+        if min_num == max_num:
+            return 0
 
-        if num < 0:
-            num += 2**32
+        n = len(nums)
+        bucket_len = ceil((max_num - min_num) / (n - 1))
+        MAX = 10000000000
+        min_bucket = [MAX] * n
+        max_bucket = [-1] * n
+        for num in nums:
+            idx = (num - min_num) // bucket_len
+            min_bucket[idx] = min(min_bucket[idx], num)
+            max_bucket[idx] = max(max_bucket[idx], num)
 
-        hexes = (
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-        )
+        max_gap = bucket_len
+        prev = max_bucket[0]
+        for i in range(1, n):
+            if min_bucket[i] == MAX:
+                continue
 
-        result: list[str] = []
-        while num != 0:
-            result.append(hexes[num % 16])
-            num //= 16
-        result.reverse()
+            max_gap = max(max_gap, min_bucket[i] - prev)
+            prev = max_bucket[i]
 
-        return "".join(result)
+        return max_gap
 
 
 def main():
-    inputs = (26, -1)
+    inputs = (
+        [3, 6, 9, 1],
+        [10],
+        [
+            494767408,
+            862126209,
+            213511142,
+            768240025,
+            631263193,
+            839199222,
+            990848796,
+            214568815,
+            540853864,
+            760724418,
+            980162605,
+            976743981,
+            168965760,
+            680875496,
+            256709197,
+            970953816,
+            948680062,
+            347254784,
+            732201399,
+            786249847,
+            782805044,
+            40906641,
+            674241381,
+            784330934,
+            175502610,
+            731105392,
+            424650824,
+            549764101,
+            986090032,
+            710542549,
+            249208107,
+            448419816,
+            527708664,
+            158499246,
+            223421723,
+            114552457,
+            466978424,
+            821491411,
+            19614107,
+            115389497,
+            902211438,
+            2644108,
+            744489871,
+            897895073,
+            372311214,
+            551142753,
+            933294894,
+            426217662,
+            217504874,
+            983488406,
+            516890023,
+            426853110,
+            971124103,
+        ],
+    )
 
-    for num in inputs:
-        result = Solution().toHex(num)
+    for nums in inputs:
+        result = Solution().maximumGap(nums)
         print(result)
 
 
