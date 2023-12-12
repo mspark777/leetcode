4dce15f8b9181f6d553fb50499c4a9377b86905f
mspark777 <topangel777@gmail.com>
2023-12-12 13:19:42 +0900

    154. Find Minimum in Rotated Sorted Array II

--- src/py/main.py
+++ src/py/main.py
@@ -15,17 +15,19 @@ class Solution:
             l = nums[left]
             r = nums[right]
 
-            result = min(result, m)
-            if l < r and l < m:
-                right = mid - 1
+            result = min(result, l)
+            if m > r:
+                left = mid + 1
+            elif m < r:
+                right = mid
             else:
-                left = left + 1
+                right -= 1
 
         return result
 
 
 def main():
-    inputs = ([3, 4, 5, 1, 2], [4, 5, 6, 7, 0, 1, 2], [11, 13, 15, 17])
+    inputs = ([1, 3, 5], [2, 2, 2, 0, 1], [1])
 
     for nums in inputs:
         result = Solution().findMin(nums)
