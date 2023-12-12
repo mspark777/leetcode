248d84b04ca833d277c4799db10489c60879adc7
mspark777 <topangel777@gmail.com>
2023-12-12 16:58:22 +0900

    162. Find Peak Element

--- src/py/main.py
+++ src/py/main.py
@@ -3,34 +3,25 @@ from typing import List
 
 
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        n = len(nums)
+    def findPeakElement(self, nums: List[int]) -> int:
         left = 0
-        right = n - 1
-        result = 10000
+        right = len(nums) - 1
 
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-            m = nums[mid]
-            l = nums[left]
-            r = nums[right]
-
-            result = min(result, l)
-            if m > r:
-                left = mid + 1
-            elif m < r:
+            if nums[mid] > nums[mid + 1]:
                 right = mid
             else:
-                right -= 1
+                left = mid + 1
 
-        return result
+        return left
 
 
 def main():
-    inputs = ([1, 3, 5], [2, 2, 2, 0, 1], [1])
+    inputs = ([1, 2, 3, 1], [1, 2, 1, 3, 5, 6, 4])
 
     for nums in inputs:
-        result = Solution().findMin(nums)
+        result = Solution().findPeakElement(nums)
         print(result)
 
 
