e0b7bd724ec39c77dc2bea55b1c4140599729186
mspark777 <topangel777@gmail.com>
2023-12-12 10:57:09 +0900

    153. Find Minimum in Rotated Sorted Array

--- src/py/main.py
+++ src/py/main.py
@@ -3,29 +3,32 @@ from typing import List
 
 
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        i_num = 0
-        j_num = 0
-        for num in nums:
-            if num > i_num:
-                j_num = i_num
-                i_num = num
-            else:
-                j_num = max(j_num, num)
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        result = 10000
 
-        return (i_num - 1) * (j_num - 1)
+        while left <= right:
+            mid = (left + right) // 2
+            m = nums[mid]
+            l = nums[left]
+            r = nums[right]
+
+            result = min(result, m)
+            if l < r and l < m:
+                right = mid - 1
+            else:
+                left = left + 1
+
+        return result
 
 
 def main():
-    inputs = (
-        [3, 4, 5, 2],
-        [1, 5, 4, 5],
-        [3, 7],
-        [2, 2, 1, 8, 1, 5, 4, 5, 2, 10, 3, 6, 5, 2, 3],
-    )
+    inputs = ([3, 4, 5, 1, 2], [4, 5, 6, 7, 0, 1, 2], [11, 13, 15, 17])
 
     for nums in inputs:
-        result = Solution().maxProduct(nums)
+        result = Solution().findMin(nums)
         print(result)
 
 
