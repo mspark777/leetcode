ee915c801f9990de9dbcf8b377370a510a054e4c
mspark777 <topangel777@gmail.com>
2023-12-13 09:19:48 +0900

    1582. Special Positions in a Binary Matrix

--- src/py/main.py
+++ src/py/main.py
@@ -1,100 +1,35 @@
 from __future__ import annotations
 from typing import List
-from math import ceil
 
 
 class Solution:
-    def maximumGap(self, nums: List[int]) -> int:
-        min_num = min(nums)
-        max_num = max(nums)
-        if min_num == max_num:
-            return 0
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        m = len(mat)
+        n = len(mat[0])
+        rows = [0] * m
+        cols = [0] * n
+        for r in range(m):
+            for c in range(n):
+                rows[r] += mat[r][c]
 
-        n = len(nums)
-        bucket_len = ceil((max_num - min_num) / (n - 1))
-        MAX = 10000000000
-        min_bucket = [MAX] * n
-        max_bucket = [-1] * n
-        for num in nums:
-            idx = (num - min_num) // bucket_len
-            min_bucket[idx] = min(min_bucket[idx], num)
-            max_bucket[idx] = max(max_bucket[idx], num)
+        for c in range(n):
+            for r in range(m):
+                cols[c] += mat[r][c]
 
-        max_gap = bucket_len
-        prev = max_bucket[0]
-        for i in range(1, n):
-            if min_bucket[i] == MAX:
-                continue
+        result = 0
+        for r, row in enumerate(mat):
+            for c, col in enumerate(row):
+                if col == 1 and rows[r] == 1 and cols[c] == 1:
+                    result += 1
 
-            max_gap = max(max_gap, min_bucket[i] - prev)
-            prev = max_bucket[i]
-
-        return max_gap
+        return result
 
 
 def main():
-    inputs = (
-        [3, 6, 9, 1],
-        [10],
-        [
-            494767408,
-            862126209,
-            213511142,
-            768240025,
-            631263193,
-            839199222,
-            990848796,
-            214568815,
-            540853864,
-            760724418,
-            980162605,
-            976743981,
-            168965760,
-            680875496,
-            256709197,
-            970953816,
-            948680062,
-            347254784,
-            732201399,
-            786249847,
-            782805044,
-            40906641,
-            674241381,
-            784330934,
-            175502610,
-            731105392,
-            424650824,
-            549764101,
-            986090032,
-            710542549,
-            249208107,
-            448419816,
-            527708664,
-            158499246,
-            223421723,
-            114552457,
-            466978424,
-            821491411,
-            19614107,
-            115389497,
-            902211438,
-            2644108,
-            744489871,
-            897895073,
-            372311214,
-            551142753,
-            933294894,
-            426217662,
-            217504874,
-            983488406,
-            516890023,
-            426853110,
-            971124103,
-        ],
-    )
+    inputs = ([[1, 0, 0], [0, 0, 1], [1, 0, 0]], [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
     for nums in inputs:
-        result = Solution().maximumGap(nums)
+        result = Solution().numSpecial(nums)
         print(result)
 
 
