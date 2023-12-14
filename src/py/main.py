4f8725eda005d8042392a8bb317fcc027147abbc
mspark777 <topangel777@gmail.com>
2023-12-14 09:54:06 +0900

    2482. Difference Between Ones and Zeros in Row and Column

--- src/py/main.py
+++ src/py/main.py
@@ -3,49 +3,31 @@ from typing import List
 
 
 class Solution:
-    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        row_count = len(mat)
-        col_count = len(mat[0])
-        result: list[int] = []
-        is_up = True
-        row = 0
-        col = 0
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        row_count = len(grid)
+        col_count = len(grid[0])
+        rows = [0 for _ in range(row_count)]
+        cols = [0 for _ in range(col_count)]
 
-        while row < row_count and col < col_count:
-            result.append(mat[row][col])
-            next_row = row + (-1 if is_up else 1)
-            next_col = col + (1 if is_up else -1)
+        for r in range(row_count):
+            for c in range(col_count):
+                is_one = grid[r][c] == 1
+                rows[r] += 1 if is_one else -1
+                cols[c] += 1 if is_one else -1
 
-            if (
-                next_row < 0
-                or next_row >= row_count
-                or next_col < 0
-                or next_col >= col_count
-            ):
-                if is_up:
-                    if next_col >= col_count:
-                        row += 1
-                    else:
-                        col += 1
-                else:
-                    if next_row >= row_count:
-                        col += 1
-                    else:
-                        row += 1
-
-                is_up = not is_up
-            else:
-                row = next_row
-                col = next_col
+        result = [[0 for _ in range(col_count)] for _ in range(row_count)]
+        for r in range(row_count):
+            for c in range(col_count):
+                result[r][c] = rows[r] + cols[c]
 
         return result
 
 
 def main():
-    inputs = ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 2], [3, 4]])
+    inputs = ([[0, 1, 1], [1, 0, 1], [0, 0, 1]], [[1, 1, 1], [1, 1, 1]])
 
     for mat in inputs:
-        result = Solution().findDiagonalOrder(mat)
+        result = Solution().onesMinusZeros(mat)
         print(result)
 
 
