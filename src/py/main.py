055f4e8574d7aa6679ef06d9b04b59d5ba9d4535
mspark777 <topangel777@gmail.com>
2023-12-15 16:45:50 +0900

    189. Rotate Array

--- src/py/main.py
+++ src/py/main.py
@@ -3,25 +3,32 @@ from typing import List
 
 
 class Solution:
-    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        row_count = len(dungeon)
-        col_count = len(dungeon[0])
-        dp = [2**32 for _ in range(row_count + 1)]
-        dp[row_count - 1] = 1
-        for col in range(col_count - 1, -1, -1):
-            for row in range(row_count - 1, -1, -1):
-                health = min(dp[row], dp[row + 1]) - dungeon[row][col]
-                dp[row] = max(health, 1)
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = len(nums)
+        k %= l
+        pivot = l - k
+        left = nums[pivot:]
+        right = nums[:pivot]
 
-        return dp[0]
+        i = 0
+        for n in left:
+            nums[i] = n
+            i += 1
+
+        for n in right:
+            nums[i] = n
+            i += 1
 
 
 def main():
-    inputs = ([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]], [[0]])
+    inputs = (([1, 2, 3, 4, 5, 6, 7], 3), ([-1, -100, 3, 99], 2))
 
-    for dungeon in inputs:
-        result = Solution().calculateMinimumHP(dungeon)
-        print(result)
+    for nums, k in inputs:
+        Solution().rotate(nums, k)
+        print(nums)
 
 
 if __name__ == "__main__":
