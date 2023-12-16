9c6462bde89a220b97d580590edf8927dd7a894e
mspark777 <topangel777@gmail.com>
2023-12-16 14:57:37 +0900

    242. Valid Anagram

--- src/c/main.c
+++ src/c/main.c
@@ -2,54 +2,36 @@
 
 #define bool int
 
-bool arrayStringsAreEqual(char** word1, int word1_size, char** word2, int word2_size) {
-  int w_idx1 = 0;
-  int w_idx2 = 0;
-  int ch_idx1 = 0;
-  int ch_idx2 = 0;
+bool isAnagram(char *s, char *t) {
+  int counts[26] = {
+      0,
+  };
 
-  while (w_idx1 < word1_size && w_idx2 < word2_size) {
-    char *w1 = word1[w_idx1];
-    char *w2 = word2[w_idx2];
-    char ch1 = w1[ch_idx1];
-    char ch2 = w2[ch_idx2];
-
-    if (ch1 == 0) {
-      ch_idx1 = 0;
-      w_idx1 += 1;
-    } 
-
-    if (ch2 == 0) {
-      ch_idx2 = 0;
-      w_idx2 += 1;
-    }
-
-    if (ch1 == 0 || ch2 == 0) {
-      continue;
-    }
-
-    if (ch1 != ch2) {
-      return 0;
-    }
-
-    ch_idx1 += 1;
-    ch_idx2 += 1;
+  int A = 'a';
+  for (char *iter = s; *iter != 0; ++iter) {
+    counts[*iter - A] += 1;
   }
 
-  return w_idx1 == word1_size && w_idx2 == word2_size;
+  for (char *iter = t; *iter != 0; ++iter) {
+    counts[*iter - A] -= 1;
+  }
+
+  for (int i = 0; i < 26; i += 1) {
+    if (counts[i] != 0) {
+      return 0;
+    }
+  }
+
+  return 1;
 }
 
-
 int main() {
-  char* word0_0 = "ab";
-  char* word0_1 = "c";
-  char *word0[] = {word0_0, word0_1};
+  char *input[] = {"anagram", "nagaram", "rat", "car"};
 
-  char* word1_0 = "a";
-  char* word1_1 = "bc";
-  char *word1[] = {word1_0, word1_1};
-  printf("%d\n", arrayStringsAreEqual(word0, 2, word1, 2));
+  for (unsigned long i = 0; i < sizeof(input) / sizeof(input[0]); i += 2) {
+    const bool result = isAnagram(input[i], input[i + 1]);
+    printf("%d\n", result);
+  }
 
   return 0;
-
 }
