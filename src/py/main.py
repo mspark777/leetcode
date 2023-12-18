a074877cf6d5eed25fa869dc635ca61da9301ec9
mspark777 <topangel777@gmail.com>
2023-12-18 09:37:53 +0900

    1913. Maximum Product Difference Between Two Pairs

--- src/py/main.py
+++ src/py/main.py
@@ -1,70 +1,36 @@
 from __future__ import annotations
 from typing import List
-from collections import defaultdict
-from heapq import heappush, heappop
 
 
-class Food:
-    rating: int
-    food: str
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        w = 0
+        x = 0
+        y = 10**5
+        z = 10**5
 
-    def __init__(self, rating: int, food: str):
-        self.rating = rating
-        self.food = food
+        for num in nums:
+            if num > w:
+                x = w
+                w = num
+            elif num > x:
+                x = num
 
-    def __lt__(self, other: Food):
-        if self.rating == other.rating:
-            return self.food < other.food
-        return self.rating > other.rating
+            if num < y:
+                z = y
+                y = num
+            elif num < z:
+                z = num
 
-
-class FoodRatings:
-    food_rating_map: dict[str, int]
-    food_cuisine_map: dict[str, str]
-    cuisine_food_map: dict[str, list[Food]]
-
-    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        self.food_rating_map = {}
-        self.food_cuisine_map = {}
-        self.cuisine_food_map = defaultdict(list)
-
-        for food, cuisine, rating in zip(foods, cuisines, ratings):
-            self.food_rating_map[food] = rating
-            self.food_cuisine_map[food] = cuisine
-            heappush(self.cuisine_food_map[cuisine], Food(rating, food))
-
-    def changeRating(self, food: str, newRating: int) -> None:
-        self.food_rating_map[food] = newRating
-        cuisineName = self.food_cuisine_map[food]
-        heappush(self.cuisine_food_map[cuisineName], Food(newRating, food))
-
-    def highestRated(self, cuisine: str) -> str:
-        highest_rated = self.cuisine_food_map[cuisine][0]
-
-        while self.food_rating_map[highest_rated.food] != highest_rated.rating:
-            heappop(self.cuisine_food_map[cuisine])
-            highest_rated = self.cuisine_food_map[cuisine][0]
-
-        return highest_rated.food
+        return (w * x) - (y * z)
 
 
 def main():
-    inputs = (
-        (
-            ["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"],
-            ["korean", "japanese", "japanese", "greek", "japanese", "korean"],
-            [9, 12, 8, 15, 14, 7],
-        ),
-    )
+    inputs = ([5, 6, 2, 7, 4], [4, 2, 5, 9, 7, 4, 8])
 
-    for foods, cuisines, ratings in inputs:
-        ratings = FoodRatings(foods, cuisines, ratings)
-        print(ratings.highestRated("korean"))
-        print(ratings.highestRated("japanese"))
-        ratings.changeRating("sushi", 16)
-        print(ratings.highestRated("japanese"))
-        ratings.changeRating("ramen", 16)
-        print(ratings.highestRated("japanese"))
+    for nums in inputs:
+        result = Solution().maxProductDifference(nums)
+        print(result)
 
 
 if __name__ == "__main__":
