eb977f0fe359c8492a682b2e825fc8b6103e49c0
mspark777 <topangel777@gmail.com>
2023-12-18 16:16:58 +0900

    178. Rank Scores

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,11 +1,2 @@
-CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
-BEGIN
-  RETURN QUERY (
-    -- Write your PostgreSQL query statement below.
-    SELECT t1.salary from
-    (SELECT e.salary, dense_rank() over(order by e.salary desc) as rank from Employee e) t1
-    WHERE rank = N
-    LIMIT 1
-  );
-END;
-$$ LANGUAGE plpgsql;
+
+SELECT score, dense_rank() over(ORDER BY Scores.score DESC) AS rank FROM Scores
