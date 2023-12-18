1b70ba5c683d12c4a85a3fb24097d1e8817ac0a9
mspark777 <topangel777@gmail.com>
2023-12-18 16:30:23 +0900

    179. Largest Number

--- src/py/main.py
+++ src/py/main.py
@@ -1,44 +1,23 @@
 from __future__ import annotations
+from typing import List
+
+
+class Compare(str):
+    def __lt__(self, right: str) -> bool:
+        return self + right > right + self
 
 
 class Solution:
-    def countPrimes(self, n: int) -> int:
-        if n <= 2:
-            return 0
-
-        dp = [i & 1 == 1 for i in range(n)]
-        dp[1] = False
-        dp[2] = True
-
-        i = 3
-        while True:
-            j = i * i
-            if j >= n:
-                break
-
-            if not dp[i]:
-                i += 2
-                continue
-
-            k = j
-            for k in range(j, n, i * 2):
-                dp[k] = False
-
-            i += 2
-
-        result = 0
-        for is_prime in dp[2:]:
-            if is_prime:
-                result += 1
-
-        return result
+    def largestNumber(self, nums: List[int]) -> str:
+        strs = sorted(map(str, nums), key=Compare)
+        return "".join(strs) if strs[0] != "0" else "0"
 
 
 def main():
-    inputs = (10, 0, 1, 10000)
+    inputs = ([10, 2], [3, 30, 34, 5, 9])
 
-    for n in inputs:
-        result = Solution().countPrimes(n)
+    for nums in inputs:
+        result = Solution().largestNumber(nums)
         print(result)
 
 
--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,2 +1,1 @@
-
 SELECT score, dense_rank() over(ORDER BY Scores.score DESC) AS rank FROM Scores
