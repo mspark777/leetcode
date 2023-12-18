f39b227e7351670efe44cdf1e0edd37b2156c604
mspark777 <topangel777@gmail.com>
2023-12-18 23:41:12 +0900

    2586. Count the Number of Vowel Strings in Range

--- src/py/main.py
+++ src/py/main.py
@@ -3,36 +3,23 @@ from typing import List
 
 
 class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        n = len(nums)
-        prev: list[list[int]] = [[] for _ in range(n + 1)]
-        for i in range(1, n + 1):
-            curr: list[list[int]] = [[] for _ in range(n + 1)]
-            for j in range(1, n + 1):
-                x: list[int] = []
-                if j == 1:
-                    x = prev[i] + [nums[i - 1]]
-                elif nums[j - 1] % nums[i - 1] == 0:
-                    x = prev[i] + [nums[i - 1]]
-                elif nums[i - 1] % nums[j - 1] == 0:
-                    x = prev[i] + [nums[i - 1]]
+    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+        vowels = set(["a", "e", "i", "o", "u"])
 
-                y = prev[j]
-                if len(y) > len(x):
-                    curr[j] = y[:]
-                else:
-                    curr[j] = x[:]
-            prev = curr[:]
+        result = 0
+        for i in range(left, right + 1):
+            word = words[i]
+            if word[0] in vowels and word[-1] in vowels:
+                result += 1
 
-        return prev[1]
+        return result
 
 
 def main():
-    inputs = ([1, 2, 3], [1, 2, 4, 8])
+    inputs = ((["are", "amy", "u"], 0, 2), (["hey", "aeo", "mu", "ooo", "artro"], 1, 4))
 
-    for nums in inputs:
-        result = Solution().largestDivisibleSubset(nums)
+    for words, left, right in inputs:
+        result = Solution().vowelStrings(words, left, right)
         print(result)
 
 
