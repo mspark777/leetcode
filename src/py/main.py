17248fb1beb44b5be87dbb26b91429c06c4f49c4
mspark777 <topangel777@gmail.com>
2023-12-18 16:52:32 +0900

    238. Product of Array Except Self

--- src/py/main.py
+++ src/py/main.py
@@ -2,22 +2,26 @@ from __future__ import annotations
 from typing import List
 
 
-class Compare(str):
-    def __lt__(self, right: str) -> bool:
-        return self + right > right + self
-
-
 class Solution:
-    def largestNumber(self, nums: List[int]) -> str:
-        strs = sorted(map(str, nums), key=Compare)
-        return "".join(strs) if strs[0] != "0" else "0"
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        num_count = len(nums)
+        result = [1] * num_count
+        for i in range(0, num_count - 1):
+            result[i + 1] = result[i] * nums[i]
+
+        right = nums[-1]
+        for i in range(num_count - 2, -1, -1):
+            result[i] *= right
+            right *= nums[i]
+
+        return result
 
 
 def main():
-    inputs = ([10, 2], [3, 30, 34, 5, 9])
+    inputs = ([1, 2, 3, 4], [-1, 1, 0, -3, 3])
 
     for nums in inputs:
-        result = Solution().largestNumber(nums)
+        result = Solution().productExceptSelf(nums)
         print(result)
 
 
