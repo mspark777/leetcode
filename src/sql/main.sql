0fe1661c0ff36504c175fd11b921ef62a3c8683b
mspark777 <topangel777@gmail.com>
2023-12-18 15:10:22 +0900

    177. Nth Highest Salary

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,3 +1,11 @@
-SELECT MAX(salary) as SecondHighestSalary FROM Employee WHERE salary NOT IN (
-  SELECT MAX(salary) FROM Employee
-)
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+    SELECT t1.salary from
+    (SELECT e.salary, dense_rank() over(order by e.salary desc) as rank from Employee e) t1
+    WHERE rank = N
+    LIMIT 1
+  );
+END;
+$$ LANGUAGE plpgsql;
