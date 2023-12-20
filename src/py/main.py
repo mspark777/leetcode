8e84038d01ea0a6328c2156245ecc06a07a01499
mspark777 <topangel777@gmail.com>
2023-12-20 16:08:56 +0900

    962. Maximum Width Ramp

--- src/py/main.py
+++ src/py/main.py
@@ -1,49 +1,36 @@
 from __future__ import annotations
 from typing import List
 
-MAX: int = int(1e9)
 
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        stack: list[int] = [0]
+        n = len(nums)
 
-class Graph:
-    n: int
-    matrix: list[list[int]]
+        for i in range(1, n):
+            top = stack[-1]
+            if nums[top] > nums[i]:
+                stack.append(i)
 
-    def __init__(self, n: int, edges: List[List[int]]):
-        self.n = n
-        self.matrix = [[MAX for _ in range(n)] for _ in range(n)]
+        result = 0
+        for i in range(n - 1, -1, -1):
+            if not stack:
+                break
+            elif i < result:
+                break
 
-        for f, to, cost in edges:
-            self.matrix[f][to] = cost
+            while stack and nums[stack[-1]] <= nums[i]:
+                result = max(result, i - stack.pop())
 
-        for i in range(n):
-            self.matrix[i][i] = 0
-
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    self.matrix[j][k] = min(
-                        self.matrix[j][k], self.matrix[j][i] + self.matrix[i][k]
-                    )
-
-    def addEdge(self, edge: List[int]) -> None:
-        f, to, cost = edge
-        for i in range(self.n):
-            for j in range(self.n):
-                self.matrix[i][j] = min(
-                    self.matrix[i][j], self.matrix[i][f] + self.matrix[to][j] + cost
-                )
-
-    def shortestPath(self, node1: int, node2: int) -> int:
-        cost = self.matrix[node1][node2]
-        return cost if cost != MAX else -1
+        return result
 
 
 def main():
-    g = Graph(4, [[0, 2, 5], [0, 1, 2], [1, 2, 1], [3, 0, 3]])
-    print(g.shortestPath(3, 2))
-    print(g.shortestPath(0, 3))
-    g.addEdge([1, 3, 4])
-    print(g.shortestPath(0, 3))
+    input = ([6, 0, 8, 2, 1, 5], [9, 8, 1, 0, 1, 9, 4, 0, 4, 1], [2, 2, 1])
+
+    for nums in input:
+        result = Solution().maxWidthRamp(nums)
+        print(result)
 
 
 if __name__ == "__main__":
