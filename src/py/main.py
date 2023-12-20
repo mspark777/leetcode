9f4500c6e6132e9a623a4e9287ac4320ce4d4301
mspark777 <topangel777@gmail.com>
2023-12-20 10:10:27 +0900

    2642. Design Graph With Shortest Path Calculator

--- src/py/main.py
+++ src/py/main.py
@@ -1,32 +1,49 @@
 from __future__ import annotations
 from typing import List
 
+MAX: int = int(1e9)
 
-class Solution:
-    def buyChoco(self, prices: List[int], money: int) -> int:
-        first = min(prices[0], prices[1])
-        second = max(prices[0], prices[1])
 
-        for price in prices[2:]:
-            if first > price:
-                second = first
-                first = price
-            elif second > price:
-                second = price
+class Graph:
+    n: int
+    matrix: list[list[int]]
 
-        total = first + second
-        if total > money:
-            return money
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.n = n
+        self.matrix = [[MAX for _ in range(n)] for _ in range(n)]
 
-        return money - total
+        for f, to, cost in edges:
+            self.matrix[f][to] = cost
+
+        for i in range(n):
+            self.matrix[i][i] = 0
+
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    self.matrix[j][k] = min(
+                        self.matrix[j][k], self.matrix[j][i] + self.matrix[i][k]
+                    )
+
+    def addEdge(self, edge: List[int]) -> None:
+        f, to, cost = edge
+        for i in range(self.n):
+            for j in range(self.n):
+                self.matrix[i][j] = min(
+                    self.matrix[i][j], self.matrix[i][f] + self.matrix[to][j] + cost
+                )
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        cost = self.matrix[node1][node2]
+        return cost if cost != MAX else -1
 
 
 def main():
-    inputs = (([1, 2, 2], 3), ([3, 2, 3], 3))
-
-    for prices, money in inputs:
-        result = Solution().buyChoco(prices, money)
-        print(result)
+    g = Graph(4, [[0, 2, 5], [0, 1, 2], [1, 2, 1], [3, 0, 3]])
+    print(g.shortestPath(3, 2))
+    print(g.shortestPath(0, 3))
+    g.addEdge([1, 3, 4])
+    print(g.shortestPath(0, 3))
 
 
 if __name__ == "__main__":
