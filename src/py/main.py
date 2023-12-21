22aa4e7fb8737317d83fa8ec4457da6ab767f09b
mspark777 <topangel777@gmail.com>
2023-12-21 09:56:38 +0900

    896. Monotonic Array

--- src/py/main.py
+++ src/py/main.py
@@ -3,24 +3,29 @@ from typing import List
 
 
 class Solution:
-    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        points.sort(key=lambda a: a[0])
+    def isMonotonic(self, nums: List[int]) -> bool:
+        increasing = False
+        decreasing = False
 
-        result = 0
-        for i in range(1, len(points)):
-            result = max(result, points[i][0] - points[i - 1][0])
+        for i in range(1, len(nums)):
+            left = nums[i - 1]
+            right = nums[i]
+            if left < right:
+                increasing = True
+            elif left > right:
+                decreasing = True
 
-        return result
+            if increasing and decreasing:
+                return False
+
+        return True
 
 
 def main():
-    input = (
-        [[8, 7], [9, 9], [7, 4], [9, 7]],
-        [[3, 1], [9, 0], [1, 0], [1, 4], [5, 3], [8, 8]],
-    )
+    input = ([1, 2, 2, 3], [6, 5, 4, 4], [1, 3, 2])
 
-    for points in input:
-        result = Solution().maxWidthOfVerticalArea(points)
+    for nums in input:
+        result = Solution().isMonotonic(nums)
         print(result)
 
 
