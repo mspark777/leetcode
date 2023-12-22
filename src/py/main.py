82e6c0be756c81c2f59c17e21d9a5dda0b79fee4
mspark777 <topangel777@gmail.com>
2023-12-22 10:53:47 +0900

    1422. Maximum Score After Splitting a String

--- src/py/main.py
+++ src/py/main.py
@@ -1,44 +1,32 @@
 from __future__ import annotations
-from typing import List
 
 
 class Solution:
-    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
-        row_count = len(matrix)
-        col_count = len(matrix[0])
-        prev_heights: list[tuple[int, int]] = []
-        result = 0
+    def maxScore(self, s: str) -> int:
+        ones = 0
+        zeros = 0
+        score = -1000
+        s_len = len(s)
 
-        for row in range(row_count):
-            heights: list[tuple[int, int]] = []
-            seen: list[bool] = [False] * col_count
+        for ch in s[: s_len - 1]:
+            if ch == "1":
+                ones += 1
+            else:
+                zeros += 1
 
-            for height, col in prev_heights:
-                if matrix[row][col] == 1:
-                    heights.append((height + 1, col))
-                    seen[col] = True
+            score = max(score, zeros - ones)
 
-            for col in range(col_count):
-                if not seen[col] and matrix[row][col] == 1:
-                    heights.append((1, col))
+        if s[-1] == "1":
+            ones += 1
 
-            for i, (height, _) in enumerate(heights):
-                result = max(result, height * (i + 1))
-
-            prev_heights = heights
-
-        return result
+        return score + ones
 
 
 def main():
-    input = (
-        [[0, 0, 1], [1, 1, 1], [1, 0, 1]],
-        [[1, 0, 1, 0, 1]],
-        [[1, 1, 0], [1, 0, 1]],
-    )
+    input = ("011101", "00111", "1111")
 
-    for matrix in input:
-        result = Solution().largestSubmatrix(matrix)
+    for s in input:
+        result = Solution().maxScore(s)
         print(result)
 
 
