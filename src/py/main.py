5ee2da92eb24bebb2d5bf239e11e25f3e215cf78
mspark777 <topangel777@gmail.com>
2023-12-25 23:19:30 +0900

    404. Sum of Left Leaves

--- src/py/main.py
+++ src/py/main.py
@@ -1,69 +1,33 @@
 from __future__ import annotations
-from typing import List
+from typing import Optional
+from tree_node import TreeNode
 
 
 class Solution:
-    def maximumScoreAfterOperations(
-        self, edges: List[List[int]], values: List[int]
-    ) -> int:
-        node_count = len(values)
-        adj = [[] for _ in range(node_count)]
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
 
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
+        s = 0
+        if root.left is not None:
+            left = root.left
+            if left.left is None and left.right is None:
+                s += left.val
 
-        subtree = [n for n in values]
-        self.pre(adj, 0, -1, subtree, values)
+        s += self.sumOfLeftLeaves(root.left)
+        s += self.sumOfLeftLeaves(root.right)
 
-        dp = [-1 for _ in range(node_count)]
-        self.dfs(adj, 0, -1, dp, subtree, values)
-        return dp[0]
-
-    def pre(
-        self,
-        adj: list[list[int]],
-        node: int,
-        parent: int,
-        subtree: list[int],
-        values: list[int],
-    ):
-        for child in adj[node]:
-            if child != parent:
-                self.pre(adj, child, node, subtree, values)
-                subtree[node] += subtree[child]
-
-    def dfs(
-        self,
-        adj: list[list[int]],
-        node: int,
-        parent: int,
-        dp: list[int],
-        subtree: list[int],
-        values: list[int],
-    ):
-        dp[node] = subtree[node] - values[node]
-        sum = 0
-        cnt = 0
-
-        for child in adj[node]:
-            if child != parent:
-                self.dfs(adj, child, node, dp, subtree, values)
-                cnt += 1
-                sum += dp[child]
-
-        if cnt > 0:
-            dp[node] = max(dp[node], values[node] + sum)
+        return s
 
 
 def main():
     input = (
-        ([[0, 1], [0, 2], [0, 3], [2, 4], [4, 5]], [5, 2, 5, 2, 1, 1]),
-        ([[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6]], [20, 10, 9, 7, 4, 3, 5]),
+        TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))),
+        TreeNode(1),
     )
 
-    for edges, values in input:
-        result = Solution().maximumScoreAfterOperations(edges, values)
+    for root in input:
+        result = Solution().sumOfLeftLeaves(root)
         print(result)
 
 
