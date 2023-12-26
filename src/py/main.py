e3b9df8c60963cf67950028c22a34fd281a32bf6
mspark777 <topangel777@gmail.com>
2023-12-27 08:55:58 +0900

    434. Number of Segments in a String

--- src/py/main.py
+++ src/py/main.py
@@ -2,33 +2,21 @@ from __future__ import annotations
 
 
 class Solution:
-    def addStrings(self, num1: str, num2: str) -> str:
-        ZERO = ord("0")
-        carry = 0
-        pos1 = len(num1) - 1
-        pos2 = len(num2) - 1
-        result: list[str] = []
-        while pos1 >= 0 or pos2 >= 0 or carry > 0:
-            s1 = num1[pos1] if pos1 >= 0 else "0"
-            s2 = num2[pos2] if pos2 >= 0 else "0"
-            n1 = ord(s1) - ZERO
-            n2 = ord(s2) - ZERO
-            s = n1 + n2 + carry
-            carry = s // 10
-            result.append(chr(s % 10 + ZERO))
+    def countSegments(self, s: str) -> int:
+        n = len(s)
+        result = 0
+        for i in range(n):
+            if (i == 0 or s[i - 1] == " ") and s[i] != " ":
+                result += 1
 
-            pos1 -= 1
-            pos2 -= 1
-
-        result.reverse()
-        return "".join(result)
+        return result
 
 
 def main():
-    input = (("11", "123"), ("456", "77"), ("0", "0"))
+    input = ("Hello, my name is John", "Hello", "", "                ")
 
-    for num1, num2 in input:
-        result = Solution().addStrings(num1, num2)
+    for s in input:
+        result = Solution().countSegments(s)
         print(result)
 
 
