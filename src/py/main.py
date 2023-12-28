4c024d80057e28b72ea940d08a2ccb56d4ec3ca9
mspark777 <topangel777@gmail.com>
2023-12-28 22:57:53 +0900

    463. Island Perimeter

--- src/py/main.py
+++ src/py/main.py
@@ -1,24 +1,39 @@
 from __future__ import annotations
+from typing import List
 
 
 class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        xor = x ^ y
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
         result = 0
+        row_count = len(grid)
+        col_count = len(grid[0])
 
-        while xor != 0:
-            if xor & 1 == 1:
-                result += 1
-            xor >>= 1
+        for row in range(row_count):
+            for col in range(col_count):
+                if grid[row][col] != 1:
+                    continue
+
+                result += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    result -= 1
+
+                if col > 0 and grid[row][col - 1] == 1:
+                    result -= 1
+
+                if (row + 1) < row_count and grid[row + 1][col] == 1:
+                    result -= 1
+
+                if (col + 1) < col_count and grid[row][col + 1] == 1:
+                    result -= 1
 
         return result
 
 
 def main():
-    input = ((1, 4), (3, 1))
+    input = ([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]], [[1]], [[1, 0]])
 
-    for x, y in input:
-        result = Solution().hammingDistance(x, y)
+    for grid in input:
+        result = Solution().islandPerimeter(grid)
         print(result)
 
 
