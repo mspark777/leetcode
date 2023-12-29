e781733b04e2ce1f7caf89820d88ffb1d792ec6f
mspark777 <topangel777@gmail.com>
2023-12-29 09:54:54 +0900

    1335. Minimum Difficulty of a Job Schedule

--- src/py/main.py
+++ src/py/main.py
@@ -1,24 +1,53 @@
 from __future__ import annotations
+from typing import List
 
 
 class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        xor = x ^ y
-        result = 0
+    def minDifficulty(self, job_difficulty: List[int], d: int) -> int:
+        job_count = len(job_difficulty)
+        if job_count < d:
+            return -1
+        elif job_count == d:
+            return sum(job_difficulty)
 
-        while xor != 0:
-            if xor & 1 == 1:
-                result += 1
-            xor >>= 1
+        dp = [0] * job_count
+        dp[0] = job_difficulty[0]
 
-        return result
+        for i in range(job_count):
+            dp[i] = max(job_difficulty[i], dp[i - 1])
+
+        dp_prev = [0] * job_count
+        stack: list[int] = []
+
+        for i in range(1, d):
+            temp = dp
+            dp = dp_prev
+            dp_prev = temp
+            stack.clear()
+
+            for j in range(i, job_count):
+                dp[j] = job_difficulty[j] + dp_prev[j - 1]
+
+                while stack and job_difficulty[stack[-1]] <= job_difficulty[j]:
+                    last_idx = stack.pop()
+                    dp[j] = min(
+                        dp[j],
+                        dp[last_idx] + job_difficulty[j] - job_difficulty[last_idx],
+                    )
+
+                if stack:
+                    dp[j] = min(dp[j], dp[stack[-1]])
+
+                stack.append(j)
+
+        return dp[-1]
 
 
 def main():
-    input = ((1, 4), (3, 1))
+    input = (([6, 5, 4, 3, 2, 1], 2), ([9, 9, 9], 4), ([1, 1, 1], 3))
 
-    for x, y in input:
-        result = Solution().hammingDistance(x, y)
+    for jobDifficulty, d in input:
+        result = Solution().minDifficulty(jobDifficulty, d)
         print(result)
 
 
