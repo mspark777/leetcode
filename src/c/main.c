24a92bab28a2f1bb0a6acab714f9fbba7a9e9a26
mspark777 <topangel777@gmail.com>
2023-12-29 11:02:30 +0900

    476. Number Complement

--- src/c/main.c
+++ src/c/main.c
@@ -2,39 +2,23 @@
 
 #define bool int
 
-int minCost(char *colors, int *needed_time, int needed_time_size) {
-  int total = 0;
-  int cur = needed_time[0];
+int findComplement(int num) {
+  unsigned int mask = ~0;
 
-  for (int i = 1; i < needed_time_size; i += 1) {
-    if (colors[i] != colors[i - 1]) {
-      cur = 0;
-    }
-
-    int needed = needed_time[i];
-    if (cur < needed) {
-      total += cur;
-      cur = needed;
-    } else {
-      total += needed;
-    }
+  while (num & mask) {
+    mask <<= 1;
   }
 
-  return total;
+  return ~mask ^ num;
 }
 
 int main() {
-  int needed_time0[] = {1, 2, 3, 4, 5};
-  printf("%d\n",
-         minCost("abaac", needed_time0, sizeof(needed_time0) / sizeof(int)));
+  int inputs[] = {5, 1};
+  int input_count = sizeof(inputs) / sizeof(inputs[0]);
 
-  int needed_time1[] = {1, 2, 3};
-  printf("%d\n",
-         minCost("abc", needed_time1, sizeof(needed_time1) / sizeof(int)));
-
-  int needed_time2[] = {1, 2, 3, 4, 1};
-  printf("%d\n",
-         minCost("aabaa", needed_time2, sizeof(needed_time2) / sizeof(int)));
-
+  for (int i = 0; i < input_count; i += 1) {
+    int result = findComplement(inputs[i]);
+    printf("%d\n", result);
+  }
   return 0;
 }
--- src/py/main.py
+++ src/py/main.py
@@ -1,52 +1,21 @@
 from __future__ import annotations
-from typing import List
 
 
 class Solution:
-    def minDifficulty(self, job_difficulty: List[int], d: int) -> int:
-        job_count = len(job_difficulty)
-        if job_count < d:
-            return -1
-        elif job_count == d:
-            return sum(job_difficulty)
+    def findComplement(self, num: int) -> int:
+        mask = ~0
 
-        dp = [0] * job_count
-        dp[0] = job_difficulty[0]
-        for i in range(job_count):
-            dp[i] = max(job_difficulty[i], dp[i - 1])
+        while (num & mask) != 0:
+            mask <<= 1
 
-        dp_prev = [0] * job_count
-        stack: list[int] = []
-
-        for i in range(1, d):
-            temp = dp
-            dp = dp_prev
-            dp_prev = temp
-            stack.clear()
-
-            for j in range(i, job_count):
-                dp[j] = job_difficulty[j] + dp_prev[j - 1]
-
-                while stack and job_difficulty[stack[-1]] <= job_difficulty[j]:
-                    last_idx = stack.pop()
-                    dp[j] = min(
-                        dp[j],
-                        dp[last_idx] + job_difficulty[j] - job_difficulty[last_idx],
-                    )
-
-                if stack:
-                    dp[j] = min(dp[j], dp[stack[-1]])
-
-                stack.append(j)
-
-        return dp[-1]
+        return ~mask ^ num
 
 
 def main():
-    input = (([6, 5, 4, 3, 2, 1], 2), ([9, 9, 9], 4), ([1, 1, 1], 3))
+    input = (5, 1)
 
-    for jobDifficulty, d in input:
-        result = Solution().minDifficulty(jobDifficulty, d)
+    for num in input:
+        result = Solution().findComplement(num)
         print(result)
 
 
