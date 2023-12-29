73aeac0800b9377448959fda8d44178f9b60d6ae
mspark777 <topangel777@gmail.com>
2023-12-30 00:10:06 +0900

    482. License Key Formatting

--- src/py/main.py
+++ src/py/main.py
@@ -2,20 +2,30 @@ from __future__ import annotations
 
 
 class Solution:
-    def findComplement(self, num: int) -> int:
-        mask = ~0
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        chars = "".join(s.split("-"))
+        char_count = len(chars)
+        first_len = char_count % k
+        if first_len == 0:
+            first_len = k
 
-        while (num & mask) != 0:
-            mask <<= 1
+        result = [chars[0:first_len].upper()]
 
-        return ~mask ^ num
+        begin = first_len
+        end = begin + k
+        while end <= char_count:
+            result.append(chars[begin:end].upper())
+            begin = end
+            end += k
+
+        return "-".join(result)
 
 
 def main():
-    input = (5, 1)
+    input = (("5F3Z-2e-9-w", 4), ("2-5g-3-J", 2), ("r", 1))
 
-    for num in input:
-        result = Solution().findComplement(num)
+    for s, k in input:
+        result = Solution().licenseKeyFormatting(s, k)
         print(result)
 
 
