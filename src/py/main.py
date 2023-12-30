e9245fb7b5d5297bbc98d60b61141b328cd938e6
mspark777 <topangel777@gmail.com>
2023-12-30 23:29:24 +0900

    1897. Redistribute Characters to Make All Strings Equal

--- src/py/main.py
+++ src/py/main.py
@@ -1,31 +1,29 @@
 from __future__ import annotations
+from typing import List
+from collections import defaultdict
 
 
 class Solution:
-    def licenseKeyFormatting(self, s: str, k: int) -> str:
-        chars = "".join(s.split("-"))
-        char_count = len(chars)
-        first_len = char_count % k
-        if first_len == 0:
-            first_len = k
+    def makeEqual(self, words: List[str]) -> bool:
+        counts = defaultdict[str, int](int)
 
-        result = [chars[0:first_len].upper()]
+        for word in words:
+            for ch in word:
+                counts[ch] += 1
 
-        begin = first_len
-        end = begin + k
-        while end <= char_count:
-            result.append(chars[begin:end].upper())
-            begin = end
-            end += k
+        word_count = len(words)
+        for count in counts.values():
+            if count % word_count != 0:
+                return False
 
-        return "-".join(result)
+        return True
 
 
 def main():
-    input = (("5F3Z-2e-9-w", 4), ("2-5g-3-J", 2), ("r", 1))
+    input = (["abc", "aabc", "bc"], ["ab", "a"])
 
-    for s, k in input:
-        result = Solution().licenseKeyFormatting(s, k)
+    for words in input:
+        result = Solution().makeEqual(words)
         print(result)
 
 
