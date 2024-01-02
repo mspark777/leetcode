4dacd50e569f21f726b032f63cf42d506da6971c
mspark777 <topangel777@gmail.com>
2024-01-02 14:45:42 +0900

    492. Construct the Rectangle

--- src/py/main.py
+++ src/py/main.py
@@ -1,39 +1,22 @@
 from __future__ import annotations
 from typing import List
+from math import sqrt
 
 
 class Solution:
-    def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        num_map = dict[int, list[int]]()
-        for num in nums:
-            if num in num_map:
-                num_map[num].append(num)
-            else:
-                num_map[num] = [num]
+    def constructRectangle(self, area: int) -> List[int]:
+        w = int(sqrt(area))
+        while area % w != 0:
+            w -= 1
 
-        result: list[list[int]] = []
-        while num_map:
-            row: list[int] = []
-            empty_nums: list[int] = []
-            for nums in num_map.values():
-                num = nums.pop()
-                row.append(num)
-                if len(nums) < 1:
-                    empty_nums.append(num)
-
-            for num in empty_nums:
-                num_map.pop(num)
-
-            result.append(row)
-
-        return result
+        return [area // w, w]
 
 
 def main():
-    input = ([1, 3, 4, 1, 2, 3, 1], [1, 2, 3, 4])
+    input = (4, 37, 122122)
 
-    for nums in input:
-        result = Solution().findMatrix(nums)
+    for area in input:
+        result = Solution().constructRectangle(area)
         print(result)
 
 
