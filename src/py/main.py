2a0f4669bc0aca2a65acc8916b4000d3c49d4fb0
mspark777 <topangel777@gmail.com>
2024-01-02 09:57:43 +0900

    2610. Convert an Array Into a 2D Array With Conditions

--- src/py/main.py
+++ src/py/main.py
@@ -3,26 +3,37 @@ from typing import List
 
 
 class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        result = 0
-        count = 0
-
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        num_map = dict[int, list[int]]()
         for num in nums:
-            if num == 0:
-                result = max(result, count)
-                count = 0
+            if num in num_map:
+                num_map[num].append(num)
             else:
-                count += 1
+                num_map[num] = [num]
 
-        result = max(result, count)
+        result: list[list[int]] = []
+        while num_map:
+            row: list[int] = []
+            empty_nums: list[int] = []
+            for nums in num_map.values():
+                num = nums.pop()
+                row.append(num)
+                if len(nums) < 1:
+                    empty_nums.append(num)
+
+            for num in empty_nums:
+                num_map.pop(num)
+
+            result.append(row)
+
         return result
 
 
 def main():
-    input = ([1, 1, 0, 1, 1, 1], [1, 0, 1, 1, 0, 1])
+    input = ([1, 3, 4, 1, 2, 3, 1], [1, 2, 3, 4])
 
     for nums in input:
-        result = Solution().findMaxConsecutiveOnes(nums)
+        result = Solution().findMatrix(nums)
         print(result)
 
 
