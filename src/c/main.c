2fb1de6d6aa014863708b7add56c825cf278af1a
mspark777 <topangel777@gmail.com>
2024-01-05 11:12:14 +0900

    300. Longest Increasing Subsequence

--- src/c/main.c
+++ src/c/main.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define bool int
 
-int findComplement(int num) {
-  unsigned int mask = ~0;
+int search(const int *nums, const int size, const int num) {
+  int left = 0;
+  int right = size - 1;
 
-  while (num & mask) {
-    mask <<= 1;
+  while (left <= right) {
+    const int mid = (right + left) / 2;
+    if (nums[mid] < num) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
 
-  return ~mask ^ num;
+  return left;
+}
+
+int lengthOfLIS(int *nums, int nums_size) {
+  int *temp = calloc(nums_size, sizeof(int));
+  int result = 0;
+
+  temp[0] = nums[0];
+
+  for (int i = 0; i < nums_size; i += 1) {
+    const int num = nums[i];
+    if (num > temp[result]) {
+      result += 1;
+      temp[result] = num;
+    } else {
+      const int index = search(temp, result + 1, num);
+      temp[index] = num;
+    }
+  }
+
+  free(temp);
+  temp = NULL;
+
+  return result + 1;
 }
 
 int main() {
-  int inputs[] = {5, 1};
-  int input_count = sizeof(inputs) / sizeof(inputs[0]);
+  int nums0[] = {10, 9, 2, 5, 3, 7, 101, 18};
+  printf("%d\n", lengthOfLIS(nums0, sizeof(nums0) / sizeof(nums0[0])));
 
-  for (int i = 0; i < input_count; i += 1) {
-    int result = findComplement(inputs[i]);
-    printf("%d\n", result);
-  }
+  int nums1[] = {0, 1, 0, 3, 2, 3};
+  printf("%d\n", lengthOfLIS(nums1, sizeof(nums1) / sizeof(nums1[0])));
+
+  int nums2[] = {7, 7, 7, 7, 7, 7, 7};
+  printf("%d\n", lengthOfLIS(nums2, sizeof(nums2) / sizeof(nums2[0])));
   return 0;
 }
