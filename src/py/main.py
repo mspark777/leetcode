2d94075cd50954c6d4269ff3878ddc4fe9f7c032
mspark777 <topangel777@gmail.com>
2024-01-10 23:25:51 +0900

    2385. Amount of Time for Binary Tree to Be Infected

--- src/py/main.py
+++ src/py/main.py
@@ -1,32 +1,58 @@
 from __future__ import annotations
 from typing import Optional
-from tree_node import TreeNode
+from tree_node import (
+    TreeNode,
+    new_tree_val,
+    new_tree_node,
+    new_tree_right,
+)
 
 
 class Solution:
-    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        list1 = self.to_list(root1, [])
-        list2 = self.to_list(root2, [])
+    max_distance: int
 
-        return list1 == list2
+    def __init__(self):
+        self.max_distance = 0
 
-    def to_list(self, node: Optional[TreeNode], result: list[int]) -> list[int]:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        self.travel(root, start)
+        return self.max_distance
+
+    def travel(self, node: Optional[TreeNode], start: int) -> int:
         if node is None:
-            return result
+            return 0
 
-        self.to_list(node.left, result)
-        self.to_list(node.right, result)
-        if node.left is None and node.right is None:
-            result.append(node.val)
+        depth = 0
+        left_depth = self.travel(node.left, start)
+        right_depth = self.travel(node.right, start)
+        if node.val == start:
+            self.max_distance = max(left_depth, right_depth)
+            depth = -1
+        elif left_depth >= 0 and right_depth >= 0:
+            depth = max(left_depth, right_depth) + 1
+        else:
+            distance = abs(left_depth) + abs(right_depth)
+            self.max_distance = max(self.max_distance, distance)
+            depth = min(left_depth, right_depth) - 1
 
-        return result
+        return depth
 
 
 def main():
-    input = ()
+    input = (
+        (
+            new_tree_node(
+                1,
+                new_tree_right(5, new_tree_node(4, new_tree_val(9), new_tree_val(2))),
+                new_tree_node(3, new_tree_val(10), new_tree_val(6)),
+            ),
+            3,
+        ),
+        (new_tree_val(1), 1),
+    )
 
-    for root1, root2 in input:
-        result = Solution().leafSimilar(root1, root2)
+    for root, start in input:
+        result = Solution().amountOfTime(root, start)
         print(result)
 
 
