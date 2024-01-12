da7fcd270857c37610decfefe4e32bcdaaad4ee6
mspark777 <topangel777@gmail.com>
2024-01-12 14:35:21 +0900

    1704. Determine if String Halves Are Alike.

--- src/py/main.py
+++ src/py/main.py
@@ -1,40 +1,31 @@
 from __future__ import annotations
-from typing import Optional
-from tree_node import TreeNode
 
 
 class Solution:
-    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        return self.travel(root, root.val, root.val) if root is not None else 0
+    def halvesAreAlike(self, s: str) -> bool:
+        n = len(s)
+        halves = n // 2
+        a = s[0:halves]
+        b = s[halves:n]
 
-    def travel(self, node: Optional[TreeNode], curmax: int, curmin: int) -> int:
-        if node is None:
-            return curmax - curmin
+        vowels = set[str](["a", "e", "i", "o", "u"])
+        acount = 0
+        bcount = 0
+        for i in range(halves):
+            if a[i].lower() in vowels:
+                acount += 1
 
-        val = node.val
-        left = node.left
-        right = node.right
-        curmax = max(curmax, val)
-        curmin = min(curmin, val)
+            if b[i].lower() in vowels:
+                bcount += 1
 
-        l = self.travel(left, curmax, curmin)
-        r = self.travel(right, curmax, curmin)
-
-        return max(l, r)
+        return acount == bcount
 
 
 def main():
-    input = (
-        TreeNode(
-            8,
-            TreeNode(3, TreeNode(1), TreeNode(6, TreeNode(4), TreeNode(7))),
-            TreeNode(10, None, TreeNode(14, TreeNode(13))),
-        ),
-        TreeNode(1, None, TreeNode(2, None, TreeNode(0, TreeNode(3)))),
-    )
+    input = ("book", "textbook")
 
-    for root in input:
-        result = Solution().maxAncestorDiff(root)
+    for s in input:
+        result = Solution().halvesAreAlike(s)
         print(result)
 
 
