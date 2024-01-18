f2eb2bda6a502bc031e70f23aaec077932a991ed
mspark777 <topangel777@gmail.com>
2024-01-18 09:07:30 +0900

    530. Minimum Absolute Difference in BST

--- src/py/main.py
+++ src/py/main.py
@@ -1,19 +1,48 @@
 from __future__ import annotations
-from typing import List
-from collections import Counter
+from tree_node import TreeNode
+from typing import Optional
 
 
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        counts = Counter(arr)
-        return len(set(counts.values())) == len(counts)
+    result: int
+    last_val: Optional[int]
+
+    def __init__(self):
+        self.result = 10**5 + 1
+        self.last_val = None
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.travel(root)
+        return self.result
+
+    def travel(self, node: Optional[TreeNode]):
+        if node is None:
+            return
+
+        if node.left is not None:
+            self.travel(node.left)
+
+        if self.last_val is not None:
+            self.result = min(self.result, abs(node.val - self.last_val))
+            self.last_val = node.val
+        else:
+            self.last_val = node.val
+
+        if node.right is not None:
+            self.travel(node.right)
 
 
 def main():
-    input = ([1, 2, 2, 1, 1, 3], [1, 2], [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0])
+    input = (
+        TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(6)),
+        TreeNode(1, TreeNode(0), TreeNode(48, TreeNode(12), TreeNode(49))),
+        TreeNode(
+            236, TreeNode(104, None, TreeNode(227)), TreeNode(701, None, TreeNode(911))
+        ),
+    )
 
-    for arr in input:
-        result = Solution().uniqueOccurrences(arr)
+    for root in input:
+        result = Solution().getMinimumDifference(root)
         print(result)
 
 
