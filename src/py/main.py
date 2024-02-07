bb0e4d119b7c65bd1bdd950359b0687b60da3631
mspark777 <topangel777@gmail.com>
2024-02-07 11:42:28 +0900

    451. Sort Characters By Frequency

--- src/py/main.py
+++ src/py/main.py
@@ -1,33 +1,25 @@
 from __future__ import annotations
-from typing import List
+from collections import Counter
 
 
 class Solution:
-    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        n = len(arr)
+    def frequencySort(self, s: str) -> str:
+        counts = Counter(s)
+        keys = list(counts.keys())
+        keys.sort(key=lambda k: -counts[k])
+        result: list[str] = []
+        for key in keys:
+            for _ in range(counts[key]):
+                result.append(key)
 
-        dp = [0 for _ in range(n + 1)]
-
-        for start in range(n - 1, -1, -1):
-            curr_max = 0
-            end = min(n, start + k)
-
-            for i in range(start, end):
-                curr_max = max(curr_max, arr[i])
-                dp[start] = max(dp[start], dp[i + 1] + curr_max * (i - start + 1))
-
-        return dp[0]
+        return "".join(result)
 
 
 def main():
-    input = (
-        ([1, 15, 7, 9, 2, 5, 10], 3),
-        ([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4),
-        ([1], 1),
-    )
+    input = ("tree", "cccaaa", "Aabb", "loveleetcode")
 
-    for arr, k in input:
-        result = Solution().maxSumAfterPartitioning(arr, k)
+    for s in input:
+        result = Solution().frequencySort(s)
         print(result)
 
 
