c25b8803e34031e4ce782804b98f33cdc7612130
mspark777 <topangel777@gmail.com>
2024-02-29 21:47:03 +0900

    572. Subtree of Another Tree

--- src/py/main.py
+++ src/py/main.py
@@ -4,50 +4,39 @@ from tree_node import TreeNode, to_tree
 
 
 class Solution:
-    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+    def isSubtree(self, root: Optional[TreeNode], sub_root: Optional[TreeNode]) -> bool:
         if root is None:
             return False
+        if self.dfs(root, sub_root):
+            return True
 
-        nodes = [root]
-        level = 0
-        while nodes:
-            node_count = len(nodes)
-            is_even = level % 2 == 0
-            prev = nodes[0].val + (-1 if is_even else 1)
-            for i in range(node_count):
-                node = nodes[i]
-                val = node.val
-                if not self.check_condition(is_even, val, prev):
-                    return False
-                prev = val
+        return self.isSubtree(root.left, sub_root) or self.isSubtree(
+            root.right, sub_root
+        )
 
-                if node.left is not None:
-                    nodes.append(node.left)
+    def dfs(
+        self,
+        node: Optional[TreeNode],
+        sub: Optional[TreeNode],
+    ) -> bool:
+        if node is None and sub is None:
+            return True
+        elif node is None or sub is None:
+            return False
+        elif node.val != sub.val:
+            return False
 
-                if node.right is not None:
-                    nodes.append(node.right)
-
-            level += 1
-            nodes = nodes[node_count:]
-
-        return True
-
-    def check_condition(self, is_even: bool, val: int, prev: int) -> bool:
-        if is_even:
-            return val % 2 != 0 and val > prev
-
-        return val % 2 == 0 and val < prev
+        return self.dfs(node.left, sub.left) and self.dfs(node.right, sub.right)
 
 
 def main():
-    input: list[list[Optional[int]]] = [
-        [1, 10, 4, 3, None, 7, 9, 12, 8, 6, None, None, 2],
-        [5, 4, 2, 3, 3, 7],
-        [5, 9, 1, 3, 5, 7],
+    input = [
+        ([[3, 4, 5, 1, 2], [4, 1, 2]]),
+        ([3, 4, 5, 1, 2, None, None, None, None, 0], [4, 1, 2]),
+        ([1, 1], [1]),
     ]
-
-    for vals in input:
-        result = Solution().isEvenOddTree(to_tree(vals))
+    for root, sub_root in input:
+        result = Solution().isSubtree(to_tree(root), to_tree(sub_root))
         print(result)
 
 
