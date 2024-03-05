153dbd0478e55841fed38c5aa44b127ce9087bf1
mspark777 <topangel777@gmail.com>
2024-03-05 22:31:30 +0900

    1750. Minimum Length of String After Deleting Similar Ends

--- src/py/main.py
+++ src/py/main.py
@@ -1,32 +1,31 @@
 from __future__ import annotations
-from typing import List
 
 
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        l = 0
-        r = n - 1
-        i = n - 1
-        result = [0] * n
-        while i >= 0:
-            left = nums[l] * nums[l]
-            right = nums[r] * nums[r]
-            if left > right:
-                result[i] = left
-                l += 1
-            else:
-                result[i] = right
-                r -= 1
-            i -= 1
+    def minimumLength(self, s: str) -> int:
+        left = 0
+        right = len(s) - 1
+        while left < right and s[left] == s[right]:
+            ch = s[left]
 
-        return result
+            while left <= right and s[left] == ch:
+                left += 1
+
+            while right > left and s[right] == ch:
+                right -= 1
+
+        return right - left + 1
 
 
 def main():
-    input = [[-4, -1, 0, 3, 10], [-7, -3, 2, 3, 11]]
-    for nums in input:
-        result = Solution().sortedSquares(nums)
+    input = [
+        "ca",
+        "cabaabac",
+        "aabccabba",
+        "bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb",
+    ]
+    for s in input:
+        result = Solution().minimumLength(s)
         print(result)
 
 
