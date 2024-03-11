e8dfb858ea0228e620befd5dfeb0ed803631eeef
mspark777 <topangel777@gmail.com>
2024-03-11 15:41:11 +0900

    791. Custom Sort String

--- src/py/main.py
+++ src/py/main.py
@@ -1,30 +1,20 @@
 from __future__ import annotations
-from typing import List
 
 
 class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        i1 = 0
-        i2 = 0
+    def customSortString(self, order: str, s: str) -> str:
+        orders = list(order)
+        chars = list(s)
+        chars.sort(key=lambda x: orders.index(x) if x in orders else len(orders))
 
-        while i1 < len(nums1) and i2 < len(nums2):
-            n1 = nums1[i1]
-            n2 = nums2[i2]
-
-            if n1 < n2:
-                i1 += 1
-            elif n1 > n2:
-                i2 += 1
-            else:
-                return n1
-
-        return -1
+        return "".join(chars)
 
 
 def main():
-    input = [([1, 2, 3], [2, 4]), ([1, 2, 3, 6], [2, 3, 4, 5])]
-    for nums1, nums2 in input:
-        result = Solution().getCommon(nums1, nums2)
+    input = [("cba", "abcd"), ("bcafg", "abcd")]
+
+    for order, s in input:
+        result = Solution().customSortString(order, s)
         print(result)
 
 
