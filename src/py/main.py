21e465c733e624a77c87ac35b35b3e4eb2f59407
mspark777 <topangel777@gmail.com>
2024-03-25 22:48:34 +0900

    442. Find All Duplicates in an Array

--- src/py/main.py
+++ src/py/main.py
@@ -3,33 +3,23 @@ from typing import List
 
 
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        frequencies = [0] * 26
-        max_count = 0
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        result: List[int] = []
+        for num in nums:
+            i = abs(num) - 1
+            if nums[i] < 0:
+                result.append(i + 1)
+            else:
+                nums[i] = -nums[i]
 
-        for task in tasks:
-            i = ord(task) - ord("A")
-            frequencies[i] += 1
-            max_count = max(max_count, frequencies[i])
-
-        time = (max_count - 1) * (n + 1)
-
-        for f in frequencies:
-            if f == max_count:
-                time += 1
-
-        return max(len(tasks), time)
+        return result
 
 
 def main():
-    input = [
-        (["A", "A", "A", "B", "B", "B"], 2),
-        (["A", "C", "A", "B", "D", "B"], 1),
-        (["A", "A", "A", "B", "B", "B"], 3),
-    ]
+    input = [[4, 3, 2, 7, 8, 2, 3, 1], [1, 1, 2], [1]]
 
-    for tasks, n in input:
-        result = Solution().leastInterval(tasks, n)
+    for nums in input:
+        result = Solution().findDuplicates(nums)
         print(result)
 
 
