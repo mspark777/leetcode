d1ffb27c007c04a4075c8b5814710d790536b833
mspark777 <topangel777@gmail.com>
2024-03-26 22:28:54 +0900

    41. First Missing Positive

--- src/c/main.c
+++ src/c/main.c
@@ -1,57 +1,34 @@
 #include <stdio.h>
 
-void swap(int* a, int* b) {
-  int l = a[0];
-  int r = a[1];
-  a[0] = b[0];
-  a[1] = b[1];
-  b[0] = l;
-  b[1] = r;
-}
-
-int partition(int** points, int low, int high) {
-  int pivot = points[high][1];
-  int i = low - 1;
-  for (int j = low; j < high; j++) {
-    if (points[j][1] <= pivot) {
-      i++;
-      swap(points[i], points[j]);
+int firstMissingPositive(int* nums, int numsSize) {
+  for (int i = 0; i < numsSize; i += 1) {
+    while ((nums[i] > 0) && (nums[i] <= numsSize) &&
+           (nums[nums[i] - 1] != nums[i])) {
+      int temp = nums[i];
+      nums[i] = nums[temp - 1];
+      nums[temp - 1] = temp;
     }
   }
 
-  swap(points[i + 1], points[high]);
-
-  return i + 1;
-}
-
-void quickSort(int** points, int low, int high) {
-  if (low < high) {
-    int pi = partition(points, low, high);
-
-    quickSort(points, low, pi - 1);
-    quickSort(points, pi + 1, high);
-  }
-}
-
-int findMinArrowShots(int** points, int pointsSize, int* pointsColSize) {
-  *pointsColSize = 2;
-
-  quickSort(points, 0, pointsSize - 1);
-  int result = 1;
-  int i = 0;
-  int j = 1;
-  while (j < pointsSize) {
-    if (points[i][1] < points[j][0]) {
-      result++;
-      i = j;
+  for (int i = 0; i < numsSize; i += 1) {
+    if (nums[i] != i + 1) {
+      return i + 1;
     }
-    j++;
   }
 
-  return result;
+  return numsSize + 1;
 }
 
 int main() {
-  printf("Hello, World!\n");
+  int input0[] = {1, 2, 0};
+  printf("%d\n", firstMissingPositive(input0, 3));
+
+  int input1[] = {3, 4, -1, 1};
+  printf("%d\n", firstMissingPositive(input1, 4));
+
+  int input2[] = {7, 8, 9, 11, 12};
+  printf("%d\n",
+         firstMissingPositive(input2, sizeof(input2) / sizeof(input2[0])));
+
   return 0;
 }
