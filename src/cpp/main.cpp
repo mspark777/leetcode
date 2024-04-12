87af680107b2cfa6be3e426c6a5283e346f172a9
mspark777 <topangel777@gmail.com>
2024-04-12 09:52:48 +0900

    42. Trapping Rain Water

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,50 +3,48 @@
 
 class Solution {
  public:
-  std::string removeKdigits(std::string num, int k) {
-    std::vector<char> stack;
-    stack.reserve(num.length());
+  int trap(std::vector<int>& height) {
+    int left = 0;
+    int right = static_cast<int>(height.size()) - 1;
+    int leftMax = 0;
+    int rightMax = 0;
+    int result = 0;
 
-    for (const char ch : num) {
-      while (k > 0 && !stack.empty() && stack.back() > ch) {
-        stack.pop_back();
-        k -= 1;
+    while (left < right) {
+      const int lheight = height[left];
+      const int rheight = height[right];
+      if (lheight < rheight) {
+        left += 1;
+        if (lheight >= leftMax) {
+          leftMax = lheight;
+        } else {
+          result += leftMax - lheight;
+        }
+      } else {
+        right -= 1;
+        if (rheight >= rightMax) {
+          rightMax = rheight;
+        } else {
+          result += rightMax - rheight;
+        }
       }
-
-      stack.push_back(ch);
     }
 
-    while (k > 0) {
-      stack.pop_back();
-      k -= 1;
-    }
-
-    std::string result;
-    result.reserve(stack.size());
-
-    for (const char ch : stack) {
-      if (result.empty() && ch == '0') {
-        continue;
-      }
-
-      result.push_back(ch);
-    }
-
-    return result.empty() ? "0" : result;
+    return result;
   }
 };
 
 struct Input {
-  std::string num;
-  int k;
+  std::vector<int> height;
 };
 
 int main() {
-  const Input inputs[] = {{"1432219", 3}, {"10200", 1}, {"10", 2}};
+  const Input inputs[] = {{{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}},
+                          {{4, 2, 0, 3, 2, 5}}};
 
   for (auto input : inputs) {
     Solution s;
-    const auto result = s.removeKdigits(input.num, input.k);
+    const auto result = s.trap(input.height);
     std::cout << result << std::endl;
   }
 
