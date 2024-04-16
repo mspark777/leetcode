5f41fcea555a1d9257a5845e79d86347c4183f08
mspark777 <topangel777@gmail.com>
2024-04-16 11:38:19 +0900

    623. Add One Row to Tree

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,20 +5,39 @@
 
 class Solution {
  public:
-  int sumNumbers(TreeNode* root) { return this->sum(root, 0); }
+  TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+    if (depth < 2) {
+      return new TreeNode(val, root, nullptr);
+    }
+
+    this->travel(root, nullptr, val, depth, true);
+    return root;
+  }
 
  private:
-  int sum(TreeNode* node, int s) {
-    if (node == nullptr) {
-      return 0;
+  void travel(TreeNode* node, TreeNode* parent, int val, int depth,
+              bool leftSide) {
+    if (node == nullptr && depth > 1 && parent == nullptr) {
+      return;
     }
 
-    const int news = (s * 10) + node->val;
-    if (node->left == nullptr && node->right == nullptr) {
-      return news;
+    if (depth == 1) {
+      TreeNode* root = new TreeNode(val);
+      if (leftSide) {
+        root->left = node;
+        if (parent != nullptr) {
+          parent->left = root;
+        }
+      } else {
+        root->right = node;
+        if (parent != nullptr) {
+          parent->right = root;
+        }
+      }
+    } else if (node != nullptr) {
+      this->travel(node->left, node, val, depth - 1, true);
+      this->travel(node->right, node, val, depth - 1, false);
     }
-
-    return this->sum(node->left, news) + this->sum(node->right, news);
   }
 };
 
