5c4cd3006c9061aaed343d712f1c52a4d244db2c
mspark777 <topangel777@gmail.com>
2024-04-22 22:47:52 +0900

    752. Open the Lock

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,56 +1,80 @@
 #include <iostream>
+#include <map>
+#include <queue>
+#include <set>
 #include <vector>
 
 class Solution {
  public:
-  bool validPath(int n, std::vector<std::vector<int>>& edges, int source,
-                 int destination) {
-    if (n == 1) {
-      return true;
+  int openLock(std::vector<std::string>& deadends, std::string target) {
+    const std::map<char, char> nextSlot = {
+        {'0', '1'}, {'1', '2'}, {'2', '3'}, {'3', '4'}, {'4', '5'},
+        {'5', '6'}, {'6', '7'}, {'7', '8'}, {'8', '9'}, {'9', '0'}};
+
+    const std::map<char, char> prevSlot = {
+        {'0', '9'}, {'1', '0'}, {'2', '1'}, {'3', '2'}, {'4', '3'},
+        {'5', '4'}, {'6', '5'}, {'7', '6'}, {'8', '7'}, {'9', '8'}};
+
+    std::set<std::string> visited(deadends.begin(), deadends.end());
+    std::queue<std::string> queue;
+
+    if (visited.find("0000") != visited.end()) {
+      return -1;
     }
 
-    std::vector<bool> visited(n, false);
-    visited[source] = true;
+    int result = 0;
+    queue.push("0000");
+    visited.insert("0000");
 
-    bool loop = true;
-    while (loop) {
-      loop = false;
-      for (const std::vector<int>& edge : edges) {
-        const int u = edge[0];
-        const int v = edge[1];
-        if (visited[u] != visited[v]) {
-          visited[u] = true;
-          visited[v] = true;
-          loop = true;
+    while (!queue.empty()) {
+      const int nodeCount = static_cast<int>(queue.size());
+      for (int i = 0; i < nodeCount; i += 1) {
+        const std::string curr = queue.front();
+        queue.pop();
+
+        if (curr == target) {
+          return result;
         }
 
-        if (visited[destination]) {
-          return true;
+        for (int wheel = 0; wheel < 4; wheel += 1) {
+          std::string prev = curr;
+          prev[wheel] = nextSlot.at(curr.at(wheel));
+          if (visited.find(prev) == visited.end()) {
+            queue.push(prev);
+            visited.insert(prev);
+          }
+
+          std::string next = curr;
+          next[wheel] = prevSlot.at(curr.at(wheel));
+          if (visited.find(next) == visited.end()) {
+            queue.push(next);
+            visited.insert(next);
+          }
         }
       }
+
+      result += 1;
     }
 
-    return false;
-  }
+    return -1;
+  };
 };
 
 struct Input {
-  int n;
-  std::vector<std::vector<int>> edges;
-  int source;
-  int destination;
+  std::vector<std::string> deadends;
+  std::string target;
 };
 
 int main() {
   const Input inputs[] = {
-      {3, {{0, 1}, {1, 2}, {2, 0}}, 0, 2},
-      {6, {{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5},
-  };
+      {{"0201", "0101", "0102", "1212", "2002"}, "0202"},
+      {{"8888"}, "0009"},
+      {{"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"},
+       "8888"}};
 
   for (auto input : inputs) {
     Solution s;
-    const auto result =
-        s.validPath(input.n, input.edges, input.source, input.destination);
+    const auto result = s.openLock(input.deadends, input.target);
 
     std::cout << result << std::endl;
   }
