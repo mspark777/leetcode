2586b087471c7b659eec4563e159a952646ade96
mspark777 <topangel777@gmail.com>
2024-04-25 22:38:40 +0900

    2370. Longest Ideal Subsequence

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,36 +1,44 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 class Solution {
  public:
-  int tribonacci(int n) {
-    if (n < 1) {
-      return 0;
+  int longestIdealString(std::string s, int k) {
+    const int N = std::size(s);
+    const int A_CODE = static_cast<int>('a');
+    const int RANGE = static_cast<int>('z') - A_CODE + 1;
+    std::vector<int> dp(RANGE, 0);
+
+    int result = 0;
+    for (int i = 0; i < N; i += 1) {
+      const int curr = static_cast<int>(s.at(i)) - A_CODE;
+      int best = 0;
+      for (int prev = 0; prev < RANGE; prev += 1) {
+        if (std::abs(prev - curr) <= k) {
+          best = std::max(best, dp[prev]);
+        }
+      }
+
+      dp[curr] = std::max(dp[curr], best + 1);
+      result = std::max(result, dp[curr]);
     }
 
-    int t0 = 0;
-    int t1 = 1;
-    int t2 = 1;
-    for (int i = 2; i < n; i += 1) {
-      const int temp = t2;
-      t2 += t1 + t0;
-      t0 = t1;
-      t1 = temp;
-    }
-
-    return t2;
+    return result;
   }
 };
 
 struct Input {
-  int n;
+  std::string s;
+  int k;
 };
 
 int main() {
-  const Input inputs[] = {{4}, {25}, {0}};
+  const Input inputs[] = {{"acfgbd", 2}, {"abcd", 3}};
 
   for (auto input : inputs) {
     Solution s;
-    const auto result = s.tribonacci(input.n);
+    const auto result = s.longestIdealString(input.s, input.k);
 
     std::cout << result << std::endl;
   }
