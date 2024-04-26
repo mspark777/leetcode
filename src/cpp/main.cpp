84c001cd560b83784b1cce5eb25cc8b1cbaccc22
mspark777 <topangel777@gmail.com>
2024-04-26 22:41:26 +0900

    1289. Minimum Falling Path Sum II

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,44 +1,61 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 
 class Solution {
  public:
-  int longestIdealString(std::string s, int k) {
-    const int N = std::size(s);
-    const int A_CODE = static_cast<int>('a');
-    const int RANGE = static_cast<int>('z') - A_CODE + 1;
-    std::vector<int> dp(RANGE, 0);
+  int minFallingPathSum(std::vector<std::vector<int>>& grid) {
+    const int N = grid.size();
+    int nextMin1C = -1;
+    int nextMin1 = -1;
+    int nextMin2 = -1;
 
-    int result = 0;
-    for (int i = 0; i < N; i += 1) {
-      const int curr = static_cast<int>(s.at(i)) - A_CODE;
-      int best = 0;
-      for (int prev = 0; prev < RANGE; prev += 1) {
-        if (std::abs(prev - curr) <= k) {
-          best = std::max(best, dp[prev]);
+    for (int col = 0; col < N; col += 1) {
+      const int c = grid[N - 1][col];
+      if (nextMin1 == -1 || c <= nextMin1) {
+        nextMin2 = nextMin1;
+        nextMin1 = c;
+        nextMin1C = col;
+      } else if (nextMin2 == -1 || c <= nextMin2) {
+        nextMin2 = c;
+      }
+    }
+
+    for (int row = N - 2; row >= 0; row -= 1) {
+      int min1C = -1;
+      int min1 = -1;
+      int min2 = -1;
+      for (int col = 0; col < N; col += 1) {
+        int value = grid[row][col] + (col != nextMin1C ? nextMin1 : nextMin2);
+        if (min1 == -1 || value < min1) {
+          min2 = min1;
+          min1 = value;
+          min1C = col;
+        } else if (min2 == -1 || value < min2) {
+          min2 = value;
         }
       }
 
-      dp[curr] = std::max(dp[curr], best + 1);
-      result = std::max(result, dp[curr]);
+      nextMin1C = min1C;
+      nextMin1 = min1;
+      nextMin2 = min2;
     }
 
-    return result;
+    return nextMin1;
   }
 };
 
 struct Input {
-  std::string s;
-  int k;
+  std::vector<std::vector<int>> grid;
 };
 
 int main() {
-  const Input inputs[] = {{"acfgbd", 2}, {"abcd", 3}};
+  const Input inputs[] = {{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}, {{{7}}}
+
+  };
 
   for (auto input : inputs) {
     Solution s;
-    const auto result = s.longestIdealString(input.s, input.k);
+    const auto result = s.minFallingPathSum(input.grid);
 
     std::cout << result << std::endl;
   }
