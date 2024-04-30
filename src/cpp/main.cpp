3c35269c9dceb9c1dc55d1ccc067b777e593ee05
mspark777 <topangel777@gmail.com>
2024-04-30 22:32:38 +0900

    1915. Number of Wonderful Substrings

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
-#include <vector>
+#include <map>
+#include <string>
 
 class Solution {
  public:
-  int minOperations(std::vector<int>& nums, int k) {
-    int result = 0;
-    for (int n : nums) {
-      result ^= n;
+  long long wonderfulSubstrings(std::string word) {
+    const int N = word.length();
+    std::map<int, int> frequencies;
+    frequencies[0] = 1;
+
+    int mask = 0;
+    long long result = 0;
+    for (int i = 0; i < N; i += 1) {
+      const char ch = word.at(i);
+      const int bit = ch - 'a';
+      mask ^= 1 << bit;
+      const int frequency = frequencies[mask];
+      result += frequency;
+      frequencies[mask] = frequency + 1;
+      for (int odd = 0; odd < 10; odd += 1) {
+        result += frequencies[mask ^ (1 << odd)];
+      }
     }
 
-    return __builtin_popcount(result ^ k);
+    return result;
   }
 };
 
 struct Input {
-  std::vector<int> nums;
-  int k;
+  std::string word;
 };
 
 int main() {
-  const Input inputs[] = {
-      {{2, 1, 3, 4}, 1},
-      {{2, 0, 2, 0}, 0},
-  };
+  const Input inputs[] = {{"aba"}, {"aabb"}, {"he"}};
 
   for (auto input : inputs) {
     Solution s;
-    const auto result = s.minOperations(input.nums, input.k);
+    const auto result = s.wonderfulSubstrings(input.word);
     std::cout << result << std::endl;
   }
 
