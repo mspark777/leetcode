79cfc72f3d395bfa39f7ddff1833d93fd38a3d98
mspark777 <topangel777@gmail.com>
2024-05-02 23:24:41 +0900

    2441. Largest Positive Integer That Exists With Its Negative

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,43 +1,38 @@
+#include <bitset>
+#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <vector>
 
 class Solution {
  public:
-  std::string reversePrefix(std::string word, char ch) {
-    int right = word.find(ch);
-    if (right == static_cast<int>(std::string::npos)) {
-      return word;
+  int findMaxK(std::vector<int>& nums) {
+    int result = -1;
+    std::bitset<2048> seen;
+
+    for (const int num : nums) {
+      const int anum = std::abs(num);
+      if (anum > result && seen[1024 - num]) {
+        result = anum;
+      }
+
+      seen[num + 1024] = true;
     }
 
-    int left = 0;
-    while (left < right) {
-      const char temp = word[left];
-      word[left] = word[right];
-      word[right] = temp;
-      left += 1;
-      right -= 1;
-    }
-
-    return word;
+    return result;
   }
 };
 
 struct Input {
-  std::string word;
-  char ch;
+  std::vector<int> nums;
 };
 
 int main() {
   const Input inputs[] = {
-      {"abcdefd", 'd'},
-      {"xyxzxe", 'z'},
-      {"abcd", 'z'},
-
-  };
+      {{-1, 2, -3, 3}}, {{-1, 10, 6, 7, -7, 1}}, {{-10, 8, 6, 7, -2, -3}}};
 
   for (auto input : inputs) {
     Solution s;
-    const auto result = s.reversePrefix(input.word, input.ch);
+    const auto result = s.findMaxK(input.nums);
     std::cout << result << std::endl;
   }
 
