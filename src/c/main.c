57b06b9ae36a546929eecb4a08a027443f4cbe98
mspark777 <topangel777@gmail.com>
2024-05-07 23:02:38 +0900

    2816. Double a Number Represented as a Linked List

--- src/c/main.c
+++ src/c/main.c
@@ -1,53 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "./main.h"
 
-struct ListNode *reverse(struct ListNode *head)
+struct ListNode *doubleIt(struct ListNode *head)
 {
-	struct ListNode *prev = NULL;
-	struct ListNode *current = head;
-
-	while (current != NULL) {
-		struct ListNode *next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+	if (head->val > 4) {
+		struct ListNode *temp = head;
+		head = calloc(1, sizeof(*head));
+		head->next = temp;
 	}
 
-	return prev;
-}
-
-struct ListNode *removeNodes(struct ListNode *head)
-{
-	head = reverse(head);
-
-	int max = 0;
-	struct ListNode *prev = NULL;
-	struct ListNode *current = head;
-	while (current != NULL) {
-		if (current->val < max) {
-			prev->next = current->next;
-			current = current->next;
-		} else {
-			max = current->val;
-			prev = current;
-			current = current->next;
+	for (struct ListNode *node = head; node != NULL; node = node->next) {
+		node->val = (node->val * 2) % 10;
+		const struct ListNode *next = node->next;
+		if ((next != NULL) && (next->val > 4)) {
+			node->val += 1;
 		}
 	}
 
-	return reverse(head);
+	return head;
 }
 
 int main()
 {
-	const int values0[] = { 5, 2, 13, 3, 8 };
+	const int values0[] = { 1, 8, 9 };
 	struct ListNode *head0 =
 		list_node_create(values0, sizeof(values0) / sizeof(values0[0]));
-	list_node_print(removeNodes(head0));
-	head0 = list_node_delete(head0);
+	list_node_print(doubleIt(head0));
 
-	const int values1[] = { 1, 1, 1, 1 };
+	const int values1[] = { 9, 9, 9 };
 	struct ListNode *head1 =
 		list_node_create(values1, sizeof(values1) / sizeof(values1[0]));
-	list_node_print(removeNodes(head1));
-	head1 = list_node_delete(head1);
+	list_node_print(doubleIt(head1));
+
+	const int values2[] = { 0 };
+	struct ListNode *head2 =
+		list_node_create(values2, sizeof(values2) / sizeof(values2[0]));
+	list_node_print(doubleIt(head2));
 }
