334756c63fdca239e10147ad83445e53eb2589f3
mspark777 <topangel777@gmail.com>
2024-05-09 23:13:22 +0900

    3075. Maximize Happiness of Selected Children

--- src/c/main.c
+++ src/c/main.c
@@ -1,40 +1,78 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "./main.h"
 
-struct ListNode *doubleIt(struct ListNode *head)
+int partition(int *arr, int left, int right)
 {
-	if (head->val > 4) {
-		struct ListNode *temp = head;
-		head = calloc(1, sizeof(*head));
-		head->next = temp;
-	}
-
-	for (struct ListNode *node = head; node != NULL; node = node->next) {
-		node->val = (node->val * 2) % 10;
-		const struct ListNode *next = node->next;
-		if ((next != NULL) && (next->val > 4)) {
-			node->val += 1;
+	int pivot_idx = -1;
+	for (int i = left; i < right; i += 1) {
+		if (arr[i] < arr[i + 1]) {
+			pivot_idx = i;
+			break;
 		}
 	}
 
-	return head;
+	if (pivot_idx < 0) {
+		return pivot_idx;
+	}
+
+	const int pivot = arr[pivot_idx];
+	arr[pivot_idx] = arr[right];
+	arr[right] = pivot;
+
+	int l = left - 1;
+	for (int r = left; r < right; r += 1) {
+		const int n = arr[r];
+		if (n > pivot) {
+			l += 1;
+			arr[r] = arr[l];
+			arr[l] = n;
+		}
+	}
+
+	const int next_pivot_idx = l + 1;
+	arr[right] = arr[next_pivot_idx];
+	arr[next_pivot_idx] = pivot;
+	return next_pivot_idx;
+}
+
+void sort(int *arr, int left, int right)
+{
+	if (left < right) {
+		const int pivot = partition(arr, left, right);
+		if (pivot >= 0) {
+			sort(arr, left, pivot - 1);
+			sort(arr, pivot + 1, right);
+		}
+	}
+}
+
+long long maximumHappinessSum(int *happiness, int happinessSize, int k)
+{
+	sort(happiness, 0, happinessSize - 1);
+
+	long long result = 0;
+	for (int turn = 0; turn < k; turn += 1) {
+		const int value = happiness[turn] - turn;
+		if (value > 0) {
+			result += value;
+		} else {
+			break;
+		}
+	}
+
+	return result;
 }
 
 int main()
 {
-	const int values0[] = { 1, 8, 9 };
-	struct ListNode *head0 =
-		list_node_create(values0, sizeof(values0) / sizeof(values0[0]));
-	list_node_print(doubleIt(head0));
+	int happiness0[] = { 1, 2, 3 };
+	printf("%lld\n", maximumHappinessSum(happiness0, 3, 2));
 
-	const int values1[] = { 9, 9, 9 };
-	struct ListNode *head1 =
-		list_node_create(values1, sizeof(values1) / sizeof(values1[0]));
-	list_node_print(doubleIt(head1));
+	int happiness1[] = { 1, 1, 1, 1 };
+	printf("%lld\n", maximumHappinessSum(happiness1, 4, 2));
 
-	const int values2[] = { 0 };
-	struct ListNode *head2 =
-		list_node_create(values2, sizeof(values2) / sizeof(values2[0]));
-	list_node_print(doubleIt(head2));
+	int happiness2[] = { 2, 3, 4, 5 };
+	printf("%lld\n", maximumHappinessSum(happiness2, 4, 1));
+
+	int happiness3[] = { 7, 2, 28 };
+	printf("%lld\n", maximumHappinessSum(happiness3, 3, 2));
 }
