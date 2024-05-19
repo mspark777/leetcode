d0ef39e06cf39425c65bcf41389d0ec87c4e7ebd
mspark777 <topangel777@gmail.com>
2024-05-19 16:50:07 +0900

    3068. Find the Maximum Sum of Node Values

--- src/c/main.c
+++ src/c/main.c
@@ -1,28 +1,41 @@
-#include <stdio.h>
-#include "./main.h"
-
-int abs(int i)
+int min(int left, int right)
 {
-	return i >= 0 ? i : -1;
+	return left < right ? left : right;
 }
 
-int dfs(struct TreeNode *node, int *move)
+int max(int left, int right)
 {
-	if (node == NULL) {
-		return 0;
+	return left < right ? right : left;
+}
+
+long long maximumValueSum(int *nums, int nums_size, int k, int **edges,
+			  int edgesSize, int *edgesColSize)
+{
+	long long sum = 0;
+	int count = 0;
+	int positive_minimum = 1 << 30;
+	int negative_maximum = -positive_minimum;
+
+	for (int i = 0; i < nums_size; i += 1) {
+		const int num = nums[i];
+		const int operated_node_value = num ^ k;
+
+		sum += num;
+		const int net_change = operated_node_value - num;
+		if (net_change > 0) {
+			positive_minimum = min(positive_minimum, net_change);
+			sum += net_change;
+			count += 1;
+		} else {
+			negative_maximum = max(negative_maximum, net_change);
+		}
 	}
 
-	const int left = dfs(node->left, move);
-	const int right = dfs(node->right, move);
+	if ((count & 1) == 0) {
+		return sum;
+	}
 
-	*move += abs(left) + abs(right);
-
-	return (node->val - 1) + left + right;
-}
-
-int distributeCoins(struct TreeNode *root)
-{
-	int result = 0;
-	dfs(root, &result);
-	return result;
+	const long long with_positive = sum - positive_minimum;
+	const long long with_negative = sum + negative_maximum;
+	return with_positive > with_negative ? with_positive : with_negative;
 }
