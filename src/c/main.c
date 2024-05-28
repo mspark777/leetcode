75eed15223994158639c0f21527706e98a4d59cd
mspark777 <topangel777@gmail.com>
2024-05-28 21:49:49 +0900

    1208. Get Equal Substrings Within Budget

--- src/c/main.c
+++ src/c/main.c
@@ -1,10 +1,31 @@
-int subsetXORSum(int *nums, int nums_size)
-{
-	int result = 0;
+#include <string.h>
 
-	for (int i = 0; i < nums_size; i += 1) {
-		result |= nums[i];
+int abs(int i)
+{
+	return i >= 0 ? i : -i;
+}
+
+int max(int left, int right)
+{
+	return left < right ? right : left;
+}
+
+int equalSubstring(char *s, char *t, int max_cost)
+{
+	const int n = strlen(s);
+	int max_len = 0;
+	int start = 0;
+	int curr_cost = 0;
+
+	for (int i = 0; i < n; i += 1) {
+		curr_cost += abs(s[i] - t[i]);
+		while (curr_cost > max_cost) {
+			curr_cost -= abs(s[start] - t[start]);
+			start += 1;
+		}
+
+		max_len = max(max_len, i - start + 1);
 	}
 
-	return result << (nums_size - 1);
+	return max_len;
 }
