28a974fb32831c8a4b86e51e484cc6ce182f90cc
mspark777 <topangel777@gmail.com>
2024-06-07 22:51:51 +0900

    648. Replace Words

--- src/py/main.py
+++ src/py/main.py
@@ -1,40 +1,67 @@
 from __future__ import annotations
-from typing import List
-from collections import Counter
+from typing import List, Optional
+
+
+class TrieNode:
+    is_end: bool
+    children: list[Optional[TrieNode]]
+
+    def __init__(self):
+        self.is_end = False
+        self.children = [None] * 26
+
+
+class Trie:
+    root: TrieNode
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str):
+        node = self.root
+        for c in word:
+            code = ord(c) - ord("a")
+            child = node.children[code]
+            if child is None:
+                child = TrieNode()
+                node.children[code] = child
+                node = child
+            else:
+                node = child
+        node.is_end = True
+
+    def shortest_root(self, word: str) -> str:
+        node = self.root
+        for i, ch in enumerate(word):
+            code = ord(ch) - ord("a")
+            child = node.children[code]
+            if child is None:
+                return word
+
+            node = child
+            if node.is_end:
+                return word[: i + 1]
+
+        return word
 
 
 class Solution:
-    def isNStraightHand(self, hand: List[int], group_size: int) -> bool:
-        if len(hand) % group_size != 0:
-            return False
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
 
-        card_counts = Counter(hand)
-
-        for card in hand:
-            start_card = card
-            while card_counts[start_card - 1]:
-                start_card -= 1
-
-            while start_card <= card:
-                while card_counts[start_card]:
-                    for next_card in range(start_card, start_card + group_size):
-                        if not card_counts[next_card]:
-                            return False
-                        card_counts[next_card] -= 1
-
-                start_card += 1
-
-        return True
+        return " ".join([trie.shortest_root(word) for word in sentence.split()])
 
 
 def main():
-    input: list[tuple[list[int], int]] = [
-        ([1, 2, 3, 6, 2, 3, 4, 7, 8], 3),
-        ([1, 2, 3, 4, 5], 4),
+    input: list[tuple[list[str], str]] = [
+        (["cat", "bat", "rat"], "the cattle was rattled by the battery"),
+        (["a", "b", "c"], "aadsfasf absbs bbab cadsfafs"),
     ]
 
-    for hand, group_size in input:
-        result = Solution().isNStraightHand(hand, group_size)
+    for dictionary, sentence in input:
+        result = Solution().replaceWords(dictionary, sentence)
         print(result)
 
 
