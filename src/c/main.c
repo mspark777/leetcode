bfab8a7a8b090636581cfa263ca12a8802558442
mspark777 <topangel777@gmail.com>
2024-06-09 22:29:41 +0900

    974. Subarray Sums Divisible by K

--- src/c/main.c
+++ src/c/main.c
@@ -1,53 +1,21 @@
 #include <stdlib.h>
 
-int min(int left, int right)
+int subarraysDivByK(int *nums, int nums_size, int k)
 {
-	return left < right ? left : right;
-}
+	int prefix = 0;
+	int result = 0;
 
-char **commonChars(char **words, int words_size, int *return_size)
-{
-	int counts[26] = {
-		0,
-	};
+	int *groups = calloc(sizeof(int), k);
+	groups[0] = 1;
 
-	for (int i = 0; words[0][i] != 0; i += 1) {
-		const char ch = words[0][i];
-		counts[ch - 'a'] += 1;
+	for (int i = 0; i < nums_size; i += 1) {
+		prefix = (prefix + k + (nums[i] % k)) % k;
+		result += groups[prefix];
+		groups[prefix] += 1;
 	}
 
-	for (int i = 1; i < words_size; i += 1) {
-		int temp_counts[26] = {
-			0,
-		};
-
-		for (int j = 0; words[i][j] != 0; j += 1) {
-			const char ch = words[i][j];
-			temp_counts[ch - 'a'] += 1;
-		}
-
-		for (int j = 0; j < 26; j += 1) {
-			counts[j] = min(counts[j], temp_counts[j]);
-		}
-	}
-
-	int result_size = 0;
-	for (int i = 0; i < 26; i += 1) {
-		result_size += counts[i];
-	}
-	*return_size = result_size;
-
-	char **result = malloc(sizeof(char *) * result_size);
-	char **pos = result;
-	for (int i = 0; i < 26; i += 1) {
-		const int count = counts[i];
-		for (int j = 0; j < count; j += 1) {
-			char *str = calloc(2, sizeof(char));
-			str[0] = (char)(i + 'a');
-			*pos = str;
-			pos += 1;
-		}
-	}
+	free(groups);
+	groups = NULL;
 
 	return result;
 }
