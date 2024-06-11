2fd1446eacfb6d611887fa62a5049d4a32d9d574
mspark777 <topangel777@gmail.com>
2024-06-11 23:22:33 +0900

    1122. Relative Sort Array

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,40 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {number} i
- * @param {number[]} nums
- * @param {number[]} temp
- * @param {number[][]} result
- * @returns {void}
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
  */
-function solve(i, nums, temp, result) {
-  result.push([...temp]);
-
-  for (let j = i; j < nums.length; j += 1) {
-    temp.push(nums[j]);
-    solve(j + 1, nums, temp, result);
-    temp.pop();
+function relativeSortArray(arr1, arr2) {
+  const indexMap = new Map();
+  for (const [i, n] of arr2.entries()) {
+    indexMap.set(n, i);
   }
-}
 
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-function subsets(nums) {
-  const result = [];
-  solve(0, nums, [], result);
-
-  return result;
+  return arr1.sort((left, right) => {
+    const left2 = indexMap.get(left);
+    const right2 = indexMap.get(right);
+    if (left2 != null && right2 != null) {
+      return left2 - right2;
+    } else if (left2 == null && right2 == null) {
+      return left - right;
+    } else if (left2 != null) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
 }
 
 function main() {
-  const inputs = [[1, 2, 3], [0]];
+  const inputs = [
+    [
+      [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19],
+      [2, 1, 4, 3, 9, 6],
+    ],
+    [
+      [28, 6, 22, 8, 44, 17],
+      [22, 28, 8, 6],
+    ],
+  ];
 
-  for (const nums of inputs) {
-    const result = subsets(nums);
+  for (const [arr1, arr2] of inputs) {
+    const result = relativeSortArray(arr1, arr2);
     console.log(result);
   }
 }
