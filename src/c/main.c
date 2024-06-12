f2d52841885bbaf6a5426a888b7a1b564fdff08f
mspark777 <topangel777@gmail.com>
2024-06-12 22:35:32 +0900

    75. Sort Colors

--- src/c/main.c
+++ src/c/main.c
@@ -1,21 +1,32 @@
-#include <stdlib.h>
-
-int subarraysDivByK(int *nums, int nums_size, int k)
+void swap(int *left, int *right)
 {
-	int prefix = 0;
-	int result = 0;
+	const int l = *left;
+	const int r = *right;
+	*left = r;
+	*right = l;
+}
 
-	int *groups = calloc(sizeof(int), k);
-	groups[0] = 1;
-
-	for (int i = 0; i < nums_size; i += 1) {
-		prefix = (prefix + k + (nums[i] % k)) % k;
-		result += groups[prefix];
-		groups[prefix] += 1;
+void sortColors(int *nums, int nums_size)
+{
+	const int RED = 0;
+	const int WHITE = 1;
+	int left = 0;
+	int mid = 0;
+	int right = nums_size - 1;
+	while (mid <= right) {
+		const int num = nums[mid];
+		switch (num) {
+		case RED:
+			swap(nums + left, nums + mid);
+			left += 1;
+			mid += 1;
+			break;
+		case WHITE:
+			mid += 1;
+			break;
+		default:
+			swap(nums + mid, nums + right);
+			right -= 1;
+		}
 	}
-
-	free(groups);
-	groups = NULL;
-
-	return result;
 }
