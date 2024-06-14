680cc8d32512dd757e238cdc0d545f487449490e
mspark777 <topangel777@gmail.com>
2024-06-14 23:35:33 +0900

    945. Minimum Increment to Make Array Unique

--- src/py/main.py
+++ src/py/main.py
@@ -1,67 +1,27 @@
 from __future__ import annotations
-from typing import List, Optional
-
-
-class TrieNode:
-    is_end: bool
-    children: list[Optional[TrieNode]]
-
-    def __init__(self):
-        self.is_end = False
-        self.children = [None] * 26
-
-
-class Trie:
-    root: TrieNode
-
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str):
-        node = self.root
-        for c in word:
-            code = ord(c) - ord("a")
-            child = node.children[code]
-            if child is None:
-                child = TrieNode()
-                node.children[code] = child
-                node = child
-            else:
-                node = child
-        node.is_end = True
-
-    def shortest_root(self, word: str) -> str:
-        node = self.root
-        for i, ch in enumerate(word):
-            code = ord(ch) - ord("a")
-            child = node.children[code]
-            if child is None:
-                return word
-
-            node = child
-            if node.is_end:
-                return word[: i + 1]
-
-        return word
+from typing import List
 
 
 class Solution:
-    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        trie = Trie()
-        for word in dictionary:
-            trie.insert(word)
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        result = 0
+        nums.sort()
 
-        return " ".join([trie.shortest_root(word) for word in sentence.split()])
+        for i in range(1, len(nums)):
+            prev = nums[i - 1]
+            curr = nums[i]
+            if prev >= curr:
+                result += prev + 1 - curr
+                nums[i] = prev + 1
+
+        return result
 
 
 def main():
-    input: list[tuple[list[str], str]] = [
-        (["cat", "bat", "rat"], "the cattle was rattled by the battery"),
-        (["a", "b", "c"], "aadsfasf absbs bbab cadsfafs"),
-    ]
+    input: list[list[int]] = [[1, 2, 2], [3, 2, 1, 2, 1, 7]]
 
-    for dictionary, sentence in input:
-        result = Solution().replaceWords(dictionary, sentence)
+    for nums in input:
+        result = Solution().minIncrementForUnique(nums)
         print(result)
 
 
