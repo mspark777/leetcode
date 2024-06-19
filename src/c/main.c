71c4d237c61c0d8c57d7d4a394ef1b8c5b17c033
mspark777 <topangel777@gmail.com>
2024-06-19 10:19:41 +0900

    1482. Minimum Number of Days to Make m Bouquets

--- src/c/main.c
+++ src/c/main.c
@@ -1,20 +1,64 @@
 #define static_cast(t, v) ((t)v)
 
-int minPatches(int *nums, int nums_size, int n)
+int get_num_of_bouguests(int *days, int size, int mid, int k)
 {
-	const long long N = n;
-	long long missing = 1;
-	int result = 0;
-	int i = 0;
+	int num_of_bouquests = 0;
+	int count = 0;
 
-	while (missing <= N) {
-		if (i < nums_size &&
-		    static_cast(long long, nums[i]) <= missing) {
-			missing += nums[i];
-			i += 1;
+	for (int i = 0; i < size; i += 1) {
+		int day = days[i];
+		if (day <= mid) {
+			count += 1;
 		} else {
-			missing += missing;
-			result += 1;
+			count = 0;
+		}
+
+		if (count == k) {
+			num_of_bouquests += 1;
+			count = 0;
+		}
+	}
+
+	return num_of_bouquests;
+}
+
+int max(int *days, int size)
+{
+	int m = 0;
+	for (int i = 0; i < size; i += 1) {
+		if (days[i] > m) {
+			m = days[i];
+		}
+	}
+
+	return m;
+}
+
+int check(int size, int m, int k)
+{
+	long long lsize = size;
+	long long lm = m;
+	long long lk = k;
+
+	return (lm * lk) > lsize;
+}
+
+int minDays(int *days, int size, int m, int k)
+{
+	if (check(size, m, k)) {
+		return -1;
+	}
+
+	int start = 0;
+	int end = max(days, size);
+	int result = -1;
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		if (get_num_of_bouguests(days, size, mid, k) >= m) {
+			result = mid;
+			end = mid - 1;
+		} else {
+			start = mid + 1;
 		}
 	}
 
