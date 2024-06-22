7db66bc6fa84e7a5643bc42d864f067b7c21a00f
mspark777 <topangel777@gmail.com>
2024-06-22 23:07:04 +0900

    1248. Count Number of Nice Subarrays

--- src/c/main.c
+++ src/c/main.c
@@ -1,35 +1,25 @@
 #define static_cast(t, v) ((t)v)
 
-int max(int left, int right)
+int at_most(int *nums, int nums_size, int k)
 {
-	return left > right ? left : right;
+	int window_size = 0;
+	int subarrays = 0;
+	int start = 0;
+
+	for (int end = 0; end < nums_size; end += 1) {
+		window_size += nums[end] & 1;
+		while (window_size > k) {
+			window_size -= nums[start] & 1;
+			start += 1;
+		}
+
+		subarrays += end - start + 1;
+	}
+
+	return subarrays;
 }
 
-int maxSatisfied(int *customers, int customers_size, int *grumpy,
-		 int grumpy_size, int minutes)
+int numberOfSubarrays(int *nums, int nums_size, int k)
 {
-	customers_size = grumpy_size;
-
-	int unrealized_customers = 0;
-
-	for (int i = 0; i < minutes; i += 1) {
-		unrealized_customers += customers[i] * grumpy[i];
-	}
-
-	int max_unrealized_customers = unrealized_customers;
-	for (int i = minutes; i < customers_size; i += 1) {
-		unrealized_customers += customers[i] * grumpy[i];
-		unrealized_customers -=
-			customers[i - minutes] * grumpy[i - minutes];
-
-		max_unrealized_customers =
-			max(max_unrealized_customers, unrealized_customers);
-	}
-
-	int result = max_unrealized_customers;
-	for (int i = 0; i < customers_size; i += 1) {
-		result += customers[i] * (1 - grumpy[i]);
-	}
-
-	return result;
+	return at_most(nums, nums_size, k) - at_most(nums, nums_size, k - 1);
 }
