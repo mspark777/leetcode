45cfa1bb61933c16c0c817bcbc7a00cc469745f7
mspark777 <topangel777@gmail.com>
2024-06-24 21:51:42 +0900

    995. Minimum Number of K Consecutive Bit Flips

--- src/c/main.c
+++ src/c/main.c
@@ -1,25 +1,25 @@
 #define static_cast(t, v) ((t)v)
 
-int at_most(int *nums, int nums_size, int k)
+int minKBitFlips(int *nums, int nums_size, int k)
 {
-	int window_size = 0;
-	int subarrays = 0;
-	int start = 0;
+	int current_flips = 0;
+	int result = 0;
 
-	for (int end = 0; end < nums_size; end += 1) {
-		window_size += nums[end] & 1;
-		while (window_size > k) {
-			window_size -= nums[start] & 1;
-			start += 1;
+	for (int i = 0; i < nums_size; i += 1) {
+		if ((i >= k) && (nums[i - k] == 2)) {
+			current_flips -= 1;
 		}
 
-		subarrays += end - start + 1;
+		if ((current_flips & 1) == nums[i]) {
+			if ((i + k) > nums_size) {
+				return -1;
+			}
+
+			nums[i] = 2;
+			current_flips += 1;
+			result += 1;
+		}
 	}
 
-	return subarrays;
-}
-
-int numberOfSubarrays(int *nums, int nums_size, int k)
-{
-	return at_most(nums, nums_size, k) - at_most(nums, nums_size, k - 1);
+	return result;
 }
