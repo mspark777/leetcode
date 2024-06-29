a3df511c3e0696826484016368e2fda2f6cd523a
mspark777 <topangel777@gmail.com>
2024-06-30 00:23:01 +0900

    2192. All Ancestors of a Node in a Directed Acyclic Graph

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
@@ -5,40 +7,58 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {number[]} nums
- * @param {number} limit
- * @return {number}
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[][]}
  */
-function longestSubarray(nums, limit) {
-  const maxDeque = [];
-  const minDeque = [];
-  let left = 0;
-  let result = 0;
+function getAncestors(n, edges) {
+  const adjacencyList = [];
+  const indegrees = [];
+  const ancestorsSetList = [];
+  for (let i = 0; i < n; i += 1) {
+    adjacencyList.push([]);
+    indegrees.push(0);
+    ancestorsSetList.push(new Set());
+  }
 
-  for (const [right, rnum] of nums.entries()) {
-    while (maxDeque.length > 0 && maxDeque.at(-1) < rnum) {
-      maxDeque.pop();
+  for (const [from, to] of edges) {
+    adjacencyList[from].push(to);
+    indegrees[to] += 1;
+  }
+
+  const nodesWithZeroIndegree = [];
+  for (const [i, indegree] of indegrees.entries()) {
+    if (indegree === 0) {
+      nodesWithZeroIndegree.push(i);
     }
-    maxDeque.push(rnum);
+  }
 
-    while (minDeque.length > 0 && minDeque.at(-1) > rnum) {
-      minDeque.pop();
-    }
-    minDeque.push(rnum);
+  const topologicalOrder = [];
+  while (nodesWithZeroIndegree.length > 0) {
+    const currentNode = nodesWithZeroIndegree.shift();
+    topologicalOrder.push(currentNode);
 
-    while (maxDeque[0] - minDeque[0] > limit) {
-      const lnum = nums[left];
-      if (maxDeque[0] === lnum) {
-        maxDeque.shift();
+    for (const neighbor of adjacencyList[currentNode]) {
+      indegrees[neighbor] -= 1;
+      if (indegrees[neighbor] === 0) {
+        nodesWithZeroIndegree.push(neighbor);
       }
-
-      if (minDeque[0] === lnum) {
-        minDeque.shift();
-      }
-      left += 1;
     }
+  }
 
-    result = Math.max(result, right - left + 1);
+  const result = [];
+  for (const node of topologicalOrder) {
+    for (const neighbor of adjacencyList[node]) {
+      ancestorsSetList[neighbor].add(node);
+      for (const n of ancestorsSetList[node]) {
+        ancestorsSetList[neighbor].add(n);
+      }
+    }
+  }
+  for (let i = 0; i < n; i += 1) {
+    const list = [...ancestorsSetList[i]];
+    list.sort((l, r) => l - r);
+    result.push(list);
   }
 
   return result;
@@ -46,13 +66,39 @@ function longestSubarray(nums, limit) {
 
 function main() {
   const inputs = [
-    [[8, 2, 4, 7], 4],
-    [[10, 1, 2, 4, 7, 2], 5],
-    [[4, 2, 2, 2, 4, 4, 2, 2], 0],
+    [
+      [
+        [0, 3],
+        [0, 4],
+        [1, 3],
+        [2, 4],
+        [2, 7],
+        [3, 5],
+        [3, 6],
+        [3, 7],
+        [4, 6],
+      ],
+      8,
+    ],
+    [
+      [
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+        [1, 2],
+        [1, 3],
+        [1, 4],
+        [2, 3],
+        [2, 4],
+        [3, 4],
+      ],
+      5,
+    ],
   ];
 
-  for (const [nums, limit] of inputs) {
-    const result = longestSubarray(nums, limit);
+  for (const [edges, n] of inputs) {
+    const result = getAncestors(n, edges);
     console.log(result);
   }
 }
