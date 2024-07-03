1a741282c4fc218053784a57a8c4200dc778d3a1
mspark777 <topangel777@gmail.com>
2024-07-03 22:13:46 +0900

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves

--- src/go/main.go
+++ src/go/main.go
@@ -5,46 +5,50 @@ import (
 	"sort"
 )
 
-func maximumImportance(n int, roads [][]int) int64 {
-	degree := make([]int64, n)
-
-	for _, edge := range roads {
-		degree[edge[0]] += 1
-		degree[edge[1]] += 1
+func minDifference(nums []int) int {
+	n := len(nums)
+	if n <= 4 {
+		return 0
 	}
 
-	sort.Slice(degree, func(i, j int) bool {
-		return degree[i] < degree[j]
-	})
+	sort.Ints(nums)
 
-	value := int64(1)
-	result := int64(0)
+	result := 0x7FFFFFFFFFFFFFFF
 
-	for _, d := range degree {
-		result += value * d
-		value += 1
+	left := 0
+	right := n - 4
+	for left < 4 {
+		diff := nums[right] - nums[left]
+		if diff < result {
+			result = diff
+		}
+
+		left += 1
+		right += 1
 	}
 
 	return result
 }
 
 type input struct {
-	roads [][]int
-	n     int
+	nums []int
 }
 
 func main() {
 	inputs := []input{
 		{
-			[][]int{{0, 1}, {1, 2}, {2, 3}, {0, 2}, {1, 3}, {2, 4}}, 5,
+			[]int{5, 3, 2, 4},
 		},
 		{
-			[][]int{{0, 3}, {2, 4}, {1, 3}}, 5,
+			[]int{1, 5, 0, 10, 14},
+		},
+		{
+			[]int{3, 100, 20},
 		},
 	}
 
 	for _, input := range inputs {
-		result := maximumImportance(input.n, input.roads)
+		result := minDifference(input.nums)
 		fmt.Println(result)
 	}
 }
