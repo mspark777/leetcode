31ced23fa3acd15c0c6147d8d407b743644fa617
mspark777 <topangel777@gmail.com>
2024-07-13 16:57:29 +0900

    2751. Robot Collisions

--- src/py/main.py
+++ src/py/main.py
@@ -3,25 +3,63 @@ from typing import List
 
 
 class Solution:
-    def minIncrementForUnique(self, nums: List[int]) -> int:
-        result = 0
-        nums.sort()
+    def survivedRobotsHealths(
+        self, positions: List[int], healths: List[int], directions: str
+    ) -> List[int]:
+        n = len(positions)
+        indices = list(range(n))
+        stack: list[int] = []
 
-        for i in range(1, len(nums)):
-            prev = nums[i - 1]
-            curr = nums[i]
-            if prev >= curr:
-                result += prev + 1 - curr
-                nums[i] = prev + 1
+        indices.sort(key=lambda x: positions[x])
+
+        for curr in indices:
+            if directions[curr] == "R":
+                stack.append(curr)
+            else:
+                while stack and healths[curr] > 0:
+                    top = stack.pop()
+
+                    if healths[top] > healths[curr]:
+                        healths[top] -= 1
+                        healths[curr] = 0
+                        stack.append(top)
+                    elif healths[top] < healths[curr]:
+                        healths[curr] -= 1
+                        healths[top] = 0
+                    else:
+                        healths[curr] = 0
+                        healths[top] = 0
+
+        result: list[int] = []
+        for health in healths:
+            if health > 0:
+                result.append(health)
 
         return result
 
 
-def main():
-    input: list[list[int]] = [[1, 2, 2], [3, 2, 1, 2, 1, 7]]
+class Input:
+    positions: list[int]
+    healths: list[int]
+    directions: str
 
-    for nums in input:
-        result = Solution().minIncrementForUnique(nums)
+    def __init__(self, positions: list[int], healths: list[int], directions: str):
+        self.positions = positions
+        self.healths = healths
+        self.directions = directions
+
+
+def main():
+    inputs: list[Input] = [
+        Input([5, 4, 3, 2, 1], [2, 17, 9, 15, 10], "RRRRR"),
+        Input([3, 5, 2, 6], [10, 10, 15, 12], "RLRL"),
+        Input([1, 2, 5, 6], [10, 10, 11, 11], "RLRL"),
+    ]
+
+    for input in inputs:
+        result = Solution().survivedRobotsHealths(
+            input.positions, input.healths, input.directions
+        )
         print(result)
 
 
