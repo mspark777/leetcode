24cda950b1bb983cf713bc7fd16056275469c883
mspark777 <topangel777@gmail.com>
2024-07-17 23:51:37 +0900

    1110. Delete Nodes And Return Forest

--- src/py/main.py
+++ src/py/main.py
@@ -1,64 +1,53 @@
 from __future__ import annotations
-from typing import Optional
-from tree_node import TreeNode
+from typing import Optional, List
+from tree_node import TreeNode, to_tree
+from collections import deque
 
 
 class Solution:
-    def getDirections(
-        self, root: Optional[TreeNode], start_value: int, dest_value: int
-    ) -> str:
-        start_path: list[str] = []
-        dest_path: list[str] = []
+    def delNodes(
+        self, root: Optional[TreeNode], to_delete: List[int]
+    ) -> List[TreeNode]:
+        if root is None:
+            return []
 
-        self.find_path(root, start_value, start_path)
-        self.find_path(root, dest_value, dest_path)
+        to_delete_set = set(to_delete)
+        result: List[TreeNode] = []
+        queue = deque([root])
 
-        common_path_length = 0
+        while queue:
+            current = queue.popleft()
+            if current.left is not None:
+                queue.append(current.left)
+                if current.left.val in to_delete_set:
+                    current.left = None
 
-        while True:
-            if common_path_length >= len(start_path):
-                break
-            elif common_path_length >= len(dest_path):
-                break
-            elif start_path[common_path_length] != dest_path[common_path_length]:
-                break
-            else:
-                common_path_length += 1
+            if current.right is not None:
+                queue.append(current.right)
+                if current.right.val in to_delete_set:
+                    current.right = None
 
-        directions: list[str] = []
-        directions.extend("U" * (len(start_path) - common_path_length))
-        directions.extend(dest_path[common_path_length:])
+            if current.val in to_delete_set:
+                if current.left is not None:
+                    result.append(current.left)
 
-        return "".join(directions)
+                if current.right is not None:
+                    result.append(current.right)
 
-    def find_path(self, node: Optional[TreeNode], target: int, path: list[str]) -> bool:
-        if node is None:
-            return False
+        if root.val not in to_delete_set:
+            result.append(root)
 
-        if node.val == target:
-            return True
-
-        path.append("L")
-        if self.find_path(node.left, target, path):
-            return True
-        path.pop()
-
-        path.append("R")
-        if self.find_path(node.right, target, path):
-            return True
-        path.pop()
-
-        return False
+        return result
 
 
 def main():
-    inputs: list[list[list[int]]] = [
-        [[20, 15, 1], [20, 17, 0], [50, 20, 1], [50, 80, 0], [80, 19, 1]],
-        [[1, 2, 1], [2, 3, 0], [3, 4, 1]],
+    inputs: list[tuple[Optional[TreeNode], list[int]]] = [
+        (to_tree([1, 2, 3, 4, 5, 6, 7]), [3, 5]),
+        (to_tree([1, 2, 4, None, 3]), [3]),
     ]
 
-    for input in inputs:
-        result = Solution().createBinaryTree(input)
+    for root, to_delete in inputs:
+        result = Solution().delNodes(root, to_delete)
         print(result)
 
 
