d1f927168effef596dae0003c6be4d10ceb7137f
mspark777 <topangel777@gmail.com>
2024-07-25 00:30:10 +0900

    2191. Sort the Jumbled Numbers

--- src/py/main.py
+++ src/py/main.py
@@ -1,23 +1,39 @@
 from __future__ import annotations
-from collections import Counter
 from typing import List
 
 
 class Solution:
-    def frequencySort(self, nums: List[int]) -> List[int]:
-        counts = Counter(nums)
-        return sorted(nums, key=lambda x: (counts[x], -x))
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        store_pairs: list[tuple[int, int]] = []
+
+        for i, num in enumerate(nums):
+            if num == 0:
+                store_pairs.append((mapping[0], i))
+                continue
+
+            mapped_value = 0
+            place = 1
+
+            while num != 0:
+                mapped_value = place * mapping[num % 10] + mapped_value
+                place *= 10
+                num //= 10
+
+            store_pairs.append((mapped_value, i))
+
+        store_pairs.sort()
+
+        return [nums[pair[1]] for pair in store_pairs]
 
 
 def main():
-    inputs: list[list[int]] = [
-        [1, 1, 2, 2, 2, 3],
-        [2, 3, 1, 3, 2],
-        [-1, 1, -6, 4, 5, -6, 1, 4, 1],
+    inputs: list[tuple[list[int], list[int]]] = [
+        ([8, 9, 4, 0, 2, 1, 3, 5, 7, 6], [991, 338, 38]),
+        ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [789, 456, 123]),
     ]
 
-    for nums in inputs:
-        result = Solution().frequencySort(nums)
+    for mapping, nums in inputs:
+        result = Solution().sortJumbled(mapping, nums)
         print(result)
 
 
