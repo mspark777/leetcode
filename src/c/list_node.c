cf52cbb6aba643468770a93c30c4d7dbe43b9cf8
mspark777 <topangel777@gmail.com>
2024-07-26 00:39:39 +0900

    912. Sort an Array

--- src/c/list_node.c
+++ src/c/list_node.c
@@ -90,3 +90,57 @@ void sort(int *arr, int left, int right)
 		}
 	}
 }
+
+void merge(int *nums, int *buf, int left, int mid, int right)
+{
+	int l = left;
+	int r = mid + 1;
+	int i = 0;
+
+	while (l <= mid && r <= right) {
+		if (nums[l] <= nums[r]) {
+			buf[i] = nums[l];
+			l += 1;
+			i += 1;
+		} else {
+			buf[i] = nums[r];
+			i += 1;
+			r += 1;
+		}
+	}
+
+	while (l <= mid) {
+		buf[i] = nums[l];
+		i += 1;
+		l += 1;
+	}
+
+	while (r <= right) {
+		buf[i] = nums[r];
+		i += 1;
+		r += 1;
+	}
+
+	for (int j = 0; j < i; j += 1) {
+		nums[left + j] = buf[j];
+	}
+}
+
+void merge_sort_impl(int *nums, int *buf, int left, int right)
+{
+	if (left >= right) {
+		return;
+	}
+
+	const int mid = (left + right) / 2;
+	merge_sort_impl(nums, buf, left, mid);
+	merge_sort_impl(nums, buf, mid + 1, right);
+	merge(nums, buf, left, mid, right);
+}
+
+void merge_sort(int *nums, int left, int right)
+{
+	const int size = right - left + 1;
+	int *buf = malloc(sizeof(int) * size);
+	merge_sort_impl(nums, buf, left, right);
+}
--- src/c/main.c
+++ src/c/main.c
@@ -1,47 +1,66 @@
 #include "./main.h"
 
-int *luckyNumbers(int **matrix, int row_count, int *col_count, int *return_size)
+void merge(int *nums, int *buf, int left, int mid, int right)
 {
-	const int N = row_count;
-	const int M = *col_count;
+	int l = left;
+	int r = mid + 1;
+	int i = 0;
 
-	int row_min_max = -1;
-	for (int i = 0; i < N; i += 1) {
-		int row_min = 1000000;
-		for (int j = 0; j < M; j += 1) {
-			const int cell = matrix[i][j];
-			if (cell < row_min) {
-				row_min = cell;
-			}
-		}
-
-		if (row_min > row_min_max) {
-			row_min_max = row_min;
+	while (l <= mid && r <= right) {
+		if (nums[l] <= nums[r]) {
+			buf[i] = nums[l];
+			l += 1;
+			i += 1;
+		} else {
+			buf[i] = nums[r];
+			i += 1;
+			r += 1;
 		}
 	}
 
-	int col_max_min = 1000000;
-	for (int i = 0; i < M; i += 1) {
-		int col_max = -1;
-		for (int j = 0; j < N; j += 1) {
-			const int cell = matrix[j][i];
-			if (cell > col_max) {
-				col_max = cell;
-			}
-		}
-
-		if (col_max < col_max_min) {
-			col_max_min = col_max;
-		}
+	while (l <= mid) {
+		buf[i] = nums[l];
+		i += 1;
+		l += 1;
 	}
 
-	int *result = malloc(sizeof(int));
-	if (row_min_max == col_max_min) {
-		*result = row_min_max;
-		*return_size = 1;
-	} else {
-		*return_size = 0;
+	while (r <= right) {
+		buf[i] = nums[r];
+		i += 1;
+		r += 1;
 	}
+
+	for (int j = 0; j < i; j += 1) {
+		nums[left + j] = buf[j];
+	}
+}
+
+void merge_sort_impl(int *nums, int *buf, int left, int right)
+{
+	if (left >= right) {
+		return;
+	}
+
+	const int mid = (left + right) / 2;
+	merge_sort_impl(nums, buf, left, mid);
+	merge_sort_impl(nums, buf, mid + 1, right);
+	merge(nums, buf, left, mid, right);
+}
+
+void merge_sort(int *nums, int left, int right)
+{
+	const int size = right - left + 1;
+	int *buf = malloc(sizeof(int) * size);
+	merge_sort_impl(nums, buf, left, right);
+}
+
+int *sortArray(int *nums, int nums_size, int *return_size)
+{
+	*return_size = nums_size;
+	const int size = sizeof(int) * nums_size;
+	int *result = malloc(size);
+	memcpy(result, nums, size);
+	merge_sort(result, 0, nums_size - 1);
 
 	return result;
 }
