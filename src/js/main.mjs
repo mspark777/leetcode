ed04ff5e95646d5ac2e0e89dd71be1148946c876
mspark777 <topangel777@gmail.com>
2024-07-29 22:36:44 +0900

    1395. Count Number of Teams

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -7,77 +7,77 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {number} n
- * @param {number[][]} edges
- * @param {number} time
- * @param {number} change
+ * @param {number[]} bits
+ * @param {number} i
+ * @param {number} value
+ * @returns {undefined}
+ */
+function updateBits(bits, i, value) {
+  while (i < bits.length) {
+    bits[i] += value;
+    i += i & -i;
+  }
+}
+
+/**
+ * @param {number[]} bits
+ * @param {number} i
+ * @returns {number}
+ */
+function getPrefixSum(bits, i) {
+  let sum = 0;
+  while (i > 0) {
+    sum += bits[i];
+    i -= i & -i;
+  }
+  return sum;
+}
+
+/**
+ * @param {number[]} rating
  * @return {number}
  */
-function secondMinimum(n, edges, time, change) {
-  const adjacencyMap = new Map();
-  for (const [a, b] of edges) {
-    const aList = adjacencyMap.get(a) ?? [];
-    const bList = adjacencyMap.get(b) ?? [];
-
-    aList.push(b);
-    bList.push(a);
-
-    adjacencyMap.set(a, aList);
-    adjacencyMap.set(b, bList);
+function numTeams(rating) {
+  const maxRating = Math.max(...rating);
+  const leftBits = [];
+  const rightBits = [];
+  for (let i = 0; i <= maxRating; i += 1) {
+    leftBits.push(0);
+    rightBits.push(0);
   }
 
-  const dist1 = [-1];
-  const dist2 = [-1];
-  for (let i = 0; i < n; i += 1) {
-    dist1.push(-1);
-    dist2.push(-1);
+  for (const r of rating) {
+    updateBits(rightBits, r, 1);
   }
 
-  const queue = [[1, 1]];
-  dist1[1] = 0;
+  let result = 0;
+  for (const r of rating) {
+    updateBits(rightBits, r, -1);
 
-  while (queue.length > 0) {
-    const [node, freq] = queue.shift();
-    let timeTaken = freq === 1 ? dist1[node] : dist2[node];
-    if ((BigInt(timeTaken) / BigInt(change)) % 2n === 1n) {
-      timeTaken =
-        change * Number(BigInt(timeTaken) / BigInt(change) + 1n) + time;
-    } else {
-      timeTaken = timeTaken + time;
-    }
+    const smallerRatingLeft = getPrefixSum(leftBits, r - 1);
+    const smallerRatingRight = getPrefixSum(rightBits, r - 1);
+    const largerRatingLeft =
+      getPrefixSum(leftBits, maxRating) - getPrefixSum(leftBits, r);
+    const largerRatingRight =
+      getPrefixSum(rightBits, maxRating) - getPrefixSum(rightBits, r);
 
-    for (const neighbor of adjacencyMap.get(node) ?? []) {
-      if (dist1[neighbor] === -1) {
-        dist1[neighbor] = timeTaken;
-        queue.push([neighbor, 1]);
-      } else if (dist2[neighbor] === -1 && dist1[neighbor] !== timeTaken) {
-        if (neighbor === n) {
-          return timeTaken;
-        }
-
-        dist2[neighbor] = timeTaken;
-        queue.push([neighbor, 2]);
-      }
-    }
+    result += smallerRatingLeft * largerRatingRight;
+    result += largerRatingLeft * smallerRatingRight;
+    updateBits(leftBits, r, 1);
   }
 
-  return 0;
+  return result;
 }
 
 function main() {
   const inputs = [
-    [
-      [3, 8],
-      [4, 7],
-    ],
-    [
-      [5, 7, 10],
-      [8, 6, 8],
-    ],
+    [2, 5, 3, 4, 1],
+    [2, 1, 3],
+    [1, 2, 3, 4],
   ];
 
-  for (const [rowSum, colSum] of inputs) {
-    const result = restoreMatrix(rowSum, colSum);
+  for (const rating of inputs) {
+    const result = numTeams(rating);
     console.log(result);
   }
 }
