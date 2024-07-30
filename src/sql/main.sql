cc98da1a50a4874418b8b5abc9872593c32d2e53
mspark777 <topangel777@gmail.com>
2024-07-30 14:59:31 +0900

    607. Sales Person

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,1 +1,13 @@
- SELECT "class" FROM Courses GROUP BY "class" HAVING COUNT("class") >= 5;
+SELECT
+  name
+FROM
+  SalesPerson
+WHERE
+  sales_id NOT IN (
+    SELECT
+    sales_id
+    FROM
+    Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+  );
