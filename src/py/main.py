f88c6c94860839f0b0ded9a9aa3e704833bea4fd
mspark777 <topangel777@gmail.com>
2024-07-31 14:48:07 +0900

    1105. Filling Bookcase Shelves

--- src/py/main.py
+++ src/py/main.py
@@ -3,50 +3,46 @@ from typing import List
 
 
 class Solution:
-    def minimumCost(
-        self,
-        source: str,
-        target: str,
-        original: List[str],
-        changed: List[str],
-        cost: List[int],
-    ) -> int:
-        result = 0
-        min_cost = [[float("inf")] * 26 for _ in range(26)]
+    def minHeightShelves(self, books: List[List[int]], shelf_width: int) -> int:
+        n = len(books)
+        dp = [0] * (n + 1)
 
-        for orig, chg, cst in zip(original, changed, cost):
-            start_char = ord(orig) - ord("a")
-            end_char = ord(chg) - ord("a")
-            min_cost[start_char][end_char] = min(min_cost[start_char][end_char], cst)
+        dp[0] = 0
+        dp[1] = books[0][1]
 
-        for k in range(26):
-            for i in range(26):
-                for j in range(26):
-                    min_cost[i][j] = min(
-                        min_cost[i][j], min_cost[i][k] + min_cost[k][j]
-                    )
+        for i in range(2, n + 1):
+            remaning_shelf_width = shelf_width - books[i - 1][0]
+            max_height = books[i - 1][1]
+            dp[i] = books[i - 1][1] + dp[i - 1]
 
-        for src, tgt in zip(source, target):
-            if src == tgt:
-                continue
-            source_char = ord(src) - ord("a")
-            target_char = ord(tgt) - ord("a")
+            j = i - 1
 
-            if min_cost[source_char][target_char] == float("inf"):
-                return -1
-            result += int(min_cost[source_char][target_char])
+            while j > 0 and remaning_shelf_width - books[j - 1][0] >= 0:
+                max_height = max(max_height, books[j - 1][1])
+                remaning_shelf_width -= books[j - 1][0]
+                dp[i] = min(dp[i], max_height + dp[j - 1])
+                j -= 1
 
-        return result
+        return dp[n]
+
+
+class Input:
+    books: list[list[int]]
+    shelf_width: int
+
+    def __init__(self, books: list[list[int]], shelf_width: int):
+        self.books = books
+        self.shelf_width = shelf_width
 
 
 def main():
-    inputs: list[tuple[list[int], list[int]]] = [
-        ([8, 9, 4, 0, 2, 1, 3, 5, 7, 6], [991, 338, 38]),
-        ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [789, 456, 123]),
+    inputs: list[Input] = [
+        Input([[1, 1], [2, 3], [2, 3], [1, 1], [1, 1], [1, 1], [1, 2]], 4),
+        Input([[1, 3], [2, 4], [3, 2]], 6),
     ]
 
-    for mapping, nums in inputs:
-        result = Solution().sortJumbled(mapping, nums)
+    for input in inputs:
+        result = Solution().minHeightShelves(input.books, input.shelf_width)
         print(result)
 
 
