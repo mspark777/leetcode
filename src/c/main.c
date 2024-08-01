afed8f906f4d98b74827eb48d4d327ad72015618
mspark777 <topangel777@gmail.com>
2024-08-01 21:40:03 +0900

    2678. Number of Senior Citizens

--- src/c/main.c
+++ src/c/main.c
@@ -1,22 +1,14 @@
 #include "./main.h"
 
-int minimumDeletions(char *s)
+int countSeniors(char **details, int details_size)
 {
 	int result = 0;
-	int b_count = 0;
-
-	for (int i = 0; s[i] != '\0'; i += 1) {
-		const char ch = s[i];
-		if (ch == 'b') {
-			b_count += 1;
-		} else {
-			const int min_deletions = result + 1;
-			if (min_deletions < b_count) {
-				result = min_deletions;
-			} else {
-				result = b_count;
-			}
-		}
+	for (int i = 0; i < details_size; i += 1) {
+		const char *d = details[i];
+		const int left = (int)(d[11] - '0');
+		const int right = (int)(d[12] - '0');
+		const int age = left * 10 + right;
+		result += age > 60;
 	}
 
 	return result;
