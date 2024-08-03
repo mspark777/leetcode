344308f32d652f6f20882da052c9839933c7cee1
mspark777 <topangel777@gmail.com>
2024-08-03 23:02:30 +0900

    1460. Make Two Arrays Equal by Reversing Subarrays

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -7,63 +7,40 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {string[]} list1
- * @param {string[]} list2
- * @return {string[]}
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
  */
-function findRestaurant(list1, list2) {
-  const map1 = new Map();
-  for (const [i, name] of list1.entries()) {
-    map1.set(name, i);
+function canBeEqual(target, arr) {
+  const frequencies = new Map();
+  for (const n of target) {
+    const frequency = frequencies.get(n) ?? 0;
+    frequencies.set(n, frequency + 1);
   }
 
-  let result = [];
-  let min_sum = Number.MAX_SAFE_INTEGER;
-  for (const [i, name2] of list2.entries()) {
-    if (i > min_sum) {
-      break;
-    }
-
-    const at1 = map1.get(name2);
-    if (at1 == null) {
-      continue;
-    }
-
-    const sum = i + at1;
-    if (sum < min_sum) {
-      result = [name2];
-      min_sum = sum;
-    } else if (sum == min_sum) {
-      result.push(name2);
-    }
+  for (const n of arr) {
+    const frequency = frequencies.get(n) ?? 0;
+    frequencies.set(n, frequency - 1);
   }
 
-  return result;
+  return [...frequencies.values()].every((f) => f === 0);
 }
 
 function main() {
   const inputs = [
     [
-      ["Shogun", "Tapioca Express", "Burger King", "KFC"],
-      [
-        "Piatti",
-        "The Grill at Torrey Pines",
-        "Hungry Hunter Steakhouse",
-        "Shogun",
-      ],
+      [1, 2, 3, 4],
+      [2, 4, 1, 3],
     ],
+    [[7], [7]],
     [
-      ["Shogun", "Tapioca Express", "Burger King", "KFC"],
-      ["KFC", "Shogun", "Burger King"],
-    ],
-    [
-      ["happy", "sad", "good"],
-      ["sad", "happy", "good"],
+      [3, 7, 9],
+      [3, 7, 11],
     ],
   ];
 
-  for (const [list1, list2] of inputs) {
-    const result = findRestaurant(list1, list2);
+  for (const [target, arr] of inputs) {
+    const result = canBeEqual(target, arr);
     console.log(result);
   }
 }
