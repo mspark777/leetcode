10bbb57e1265c5626f16dc0552ac84aa2983d677
mspark777 <topangel777@gmail.com>
2024-08-18 21:44:22 +0900

    264. Ugly Number II

--- src/py/main.py
+++ src/py/main.py
@@ -1,45 +1,34 @@
 from __future__ import annotations
-from typing import List
+from queue import PriorityQueue
 
 
 class Solution:
-    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        result = 0
-        m = len(grid)
-        n = len(grid[0])
-        for row in range(m - 2):
-            for col in range(n - 2):
-                if self.is_magic_square(grid, row, col):
-                    result += 1
+    def nthUglyNumber(self, n: int) -> int:
+        queue = PriorityQueue[int]()
+        seen = set[int]()
+        factors = [2, 3, 5]
+
+        queue.put(1)
+        seen.add(1)
+
+        result = 1
+
+        for _ in range(n):
+            result = queue.get()
+            for prime in factors:
+                ugly = prime * result
+                if ugly not in seen:
+                    queue.put(ugly)
+                    seen.add(ugly)
 
         return result
 
-    def is_magic_square(self, grid: list[list[int]], row: int, col: int) -> bool:
-        sequence = "2943816729438167"
-        sequence_reversed = "7618349276183492"
-
-        border: list[str] = []
-        border_indices = [0, 1, 2, 5, 8, 7, 6, 3]
-        for i in border_indices:
-            num = grid[row + i // 3][col + (i % 3)]
-            border.append(str(num))
-
-        border_converted = "".join(border)
-        return (
-            grid[row][col] % 2 == 0
-            and (
-                sequence.find(border_converted) != -1
-                or sequence_reversed.find(border_converted) != -1
-            )
-            and grid[row + 1][col + 1] == 5
-        )
-
 
 def main():
-    inputs: list[list[list[int]]] = [[[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]], [[8]]]
+    inputs: list[int] = [10, 1]
 
     for input in inputs:
-        result = Solution().numMagicSquaresInside(input)
+        result = Solution().nthUglyNumber(input)
         print(result)
 
 
