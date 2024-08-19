4d7559ce0b6b25a1bfcac00028c3ac8f18becacd
mspark777 <topangel777@gmail.com>
2024-08-19 22:06:38 +0900

    650. 2 Keys Keyboard

--- src/c/main.c
+++ src/c/main.c
@@ -1,33 +1,15 @@
 #include "./main.h"
 
-int maxDistance(int **arrays, int arrays_size, int *arrays_col_size)
+int minSteps(int n)
 {
-	int min = 1e5;
-	int max = -1e5;
 	int result = 0;
-
-	for (int i = 0; i < arrays_size; i += 1) {
-		int *arr = arrays[i];
-		int left = arr[0];
-		int right = arr[arrays_col_size[i] - 1];
-		int min_diff = right - min;
-		int max_diff = max - left;
-
-		if (min_diff > result) {
-			result = min_diff;
+	int d = 2;
+	while (n > 1) {
+		while (n % d == 0) {
+			result += d;
+			n /= d;
 		}
-
-		if (max_diff > result) {
-			result = max_diff;
-		}
-
-		if (left < min) {
-			min = left;
-		}
-
-		if (right > max) {
-			max = right;
-		}
+		d += 1;
 	}
 
 	return result;
