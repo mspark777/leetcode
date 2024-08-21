d16d475999623d9d022f871d54fac302ea276f07
mspark777 <topangel777@gmail.com>
2024-08-21 22:28:08 +0900

    664. Strange Printer

--- src/c/main.c
+++ src/c/main.c
@@ -1,16 +1,38 @@
 #include "./main.h"
 
-int minSteps(int n)
+int strangePrinter(char *s)
 {
-	int result = 0;
-	int d = 2;
-	while (n > 1) {
-		while (n % d == 0) {
-			result += d;
-			n /= d;
+	const int n = strlen(s);
+	int *dp = calloc(n * n, sizeof(int));
+	for (int l = 1; l <= n; l += 1) {
+		for (int left = 0; left <= n - l; left += 1) {
+			const int right = left + l - 1;
+			int j = -1;
+			dp[left * n + right] = n;
+			for (int i = left; i < right; i += 1) {
+				if (s[i] != s[right] && j == -1) {
+					j = i;
+				}
+
+				if (j != -1) {
+					const int lmin = dp[left * n + right];
+					const int rmin =
+						1 + dp[j * n + i] +
+						dp[(i + 1) * n + right];
+					dp[left * n + right] =
+						lmin < rmin ? lmin : rmin;
+				}
+			}
+
+			if (j == -1) {
+				dp[left * n + right] = 0;
+			}
 		}
-		d += 1;
 	}
+
+	const int result = dp[n - 1] + 1;
+	free(dp);
+	dp = NULL;
 
 	return result;
 }
