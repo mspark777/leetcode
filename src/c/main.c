54ea416cb6781bd526737591c44abfb67ae8af0c
mspark777 <topangel777@gmail.com>
2024-08-25 21:59:31 +0900

    145. Binary Tree Postorder Traversal

--- src/c/main.c
+++ src/c/main.c
@@ -1,38 +1,33 @@
 #include "./main.h"
 
-int strangePrinter(char *s)
+void get_tree_node_count(struct TreeNode *node, int *count)
 {
-	const int n = strlen(s);
-	int *dp = calloc(n * n, sizeof(int));
-	for (int l = 1; l <= n; l += 1) {
-		for (int left = 0; left <= n - l; left += 1) {
-			const int right = left + l - 1;
-			int j = -1;
-			dp[left * n + right] = n;
-			for (int i = left; i < right; i += 1) {
-				if (s[i] != s[right] && j == -1) {
-					j = i;
-				}
-
-				if (j != -1) {
-					const int lmin = dp[left * n + right];
-					const int rmin =
-						1 + dp[j * n + i] +
-						dp[(i + 1) * n + right];
-					dp[left * n + right] =
-						lmin < rmin ? lmin : rmin;
-				}
-			}
-
-			if (j == -1) {
-				dp[left * n + right] = 0;
-			}
-		}
+	if (node != NULL) {
+		*count += 1;
+		get_tree_node_count(node->left, count);
+		get_tree_node_count(node->right, count);
 	}
+}
 
-	const int result = dp[n - 1] + 1;
-	free(dp);
-	dp = NULL;
+void post(struct TreeNode *node, int *nums, int *pos)
+{
+	if (node != NULL) {
+		post(node->left, nums, pos);
+		post(node->right, nums, pos);
+		const int p = *pos;
+		nums[p] = node->val;
+		*pos += 1;
+	}
+}
 
-	return result;
+int *postorderTraversal(struct TreeNode *root, int *return_size)
+{
+	*return_size = 0;
+	get_tree_node_count(root, return_size);
+	int *nums = malloc(sizeof(int) * (*return_size));
+
+	int pos = 0;
+	post(root, nums, &pos);
+
+	return nums;
 }
