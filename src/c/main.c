4f340e05226e04c96a80e403379cfaf3e7d4f852
mspark777 <topangel777@gmail.com>
2024-09-11 22:53:25 +0900

    2220. Minimum Bit Flips to Convert Number

--- src/c/main.c
+++ src/c/main.c
@@ -1,42 +1,14 @@
 #include "./main.h"
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
-int calculate_gcd(int a, int b)
+int minBitFlips(int start, int goal)
 {
-	while (b != 0) {
-		int temp = b;
-		b = a % b;
-		a = temp;
+	int xor_result = start ^ goal;
+	int result = 0;
+
+	while (xor_result != 0) {
+		xor_result &= xor_result - 1;
+		result += 1;
 	}
 
-	return a;
-}
-struct ListNode *insertGreatestCommonDivisors(struct ListNode *head)
-{
-	if (head == NULL) {
-		return head;
-	}
-
-	struct ListNode *node1 = head;
-	struct ListNode *node2 = head->next;
-
-	while (node2 != NULL) {
-		struct ListNode *node = malloc(sizeof(struct ListNode));
-		node->val = calculate_gcd(node1->val, node2->val);
-
-		node1->next = node;
-		node->next = node2;
-
-		node1 = node2;
-		node2 = node2->next;
-	}
-
-	return head;
+	return result;
 }
