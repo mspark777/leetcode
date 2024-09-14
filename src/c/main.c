7edb965d442cfa02c14c2d208f10b3b27e920262
mspark777 <topangel777@gmail.com>
2024-09-14 16:38:10 +0900

    2419. Longest Subarray With Maximum Bitwise AND

--- src/c/main.c
+++ src/c/main.c
@@ -1,25 +1,27 @@
 #include "./main.h"
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int *xorQueries(int *arr, int arr_size, int **queries, int queries_size,
-		int *queries_col_size, int *return_size)
+int longestSubarray(int *nums, int nums_size)
 {
-	for (int i = 1; i < arr_size; i += 1) {
-		arr[i] ^= arr[i - 1];
-	}
+	int max_num = 0;
+	int result = 0;
+	int current = 0;
 
-	*return_size = queries_size;
-	int *result = malloc(sizeof(int) * queries_size);
-	for (int i = 0; i < queries_size; i += 1) {
-		const int l = queries[i][0];
-		const int r = queries[i][1];
+	for (int i = 0; i < nums_size; i += 1) {
+		int num = nums[i];
+		if (max_num < num) {
+			max_num = num;
+			result = 0;
+			current = 0;
+		}
 
-		if (l > 0) {
-			result[i] = arr[l - 1] ^ arr[r];
+		if (max_num == num) {
+			current += 1;
 		} else {
-			result[i] = arr[r];
+			current = 0;
+		}
+
+		if (result < current) {
+			result = current;
 		}
 	}
 
