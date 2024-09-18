2c6b7a6569f9dfbc96d85dd2b87eeb46007253ff
mspark777 <topangel777@gmail.com>
2024-09-18 15:54:22 +0900

    179. Largest Number

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,17 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.cjs"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.mjs"], languageOptions: { sourceType: "module" } },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  eslintPluginPrettierRecommended,
   {
+    files: ["src/**/*.js", "src/**/*.ts"],
     languageOptions: {
+      sourceType: "commonjs",
       globals: {
-        ...globals.es2021,
+        ...globals.jest,
+        ...globals.es2022,
         ...globals.node,
       },
       parserOptions: {
@@ -19,14 +23,50 @@ export default [
         project: "./tsconfig.json",
       },
     },
-    ignores: ["dist/*", "node_modules/*", "temp/*"],
-  },
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  pluginJs.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
     rules: {
+      "@typescript-eslint/no-extraneous-class": "off",
+      "@typescript-eslint/explicit-function-return-type": "error",
+    },
+  },
+  {
+    files: ["**/*.cjs"],
+    ignores: ["dist/*", "node_modules/*"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.jest,
+        ...globals.es2022,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-extraneous-class": "off",
+      "@typescript-eslint/explicit-function-return-type": "error",
+    },
+  },
+  {
+    files: ["**/*.mjs"],
+    ignores: ["dist/*", "node_modules/*"],
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.jest,
+        ...globals.es2022,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-extraneous-class": "off",
       "@typescript-eslint/explicit-function-return-type": "error",
     },
   },
--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,63 +1,33 @@
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-extraneous-class */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {string} s1
- * @param {string} s2
- * @return {string[]}
+ * @param {number[]} nums
+ * @return {string}
  */
-function uncommonFromSentences(s1, s2) {
-  const counts = new Map();
-  for (const word of s1.split(" ")) {
-    const count = counts.get(word) ?? 0;
-    counts.set(word, count + 1);
-  }
-  for (const word of s2.split(" ")) {
-    const count = counts.get(word) ?? 0;
-    counts.set(word, count + 1);
+function largestNumber(nums) {
+  const strs = nums
+    .map((n) => n.toString())
+    .sort((left, right) => {
+      const l = left + right;
+      const r = right + left;
+      return r.localeCompare(l);
+    });
+
+  if (strs.at(0) === "0") {
+    return "0";
   }
 
-  const result = [];
-  for (const [word, count] of counts) {
-    if (count === 1) {
-      result.push(word);
-    }
-  }
-
-  return result;
+  return strs.join("");
 }
-
-/*
-class Solution(object):
-    def uncommonFromSentences(self, A, B):
-        count = {}
-        for word in A.split():
-            count[word] = count.get(word, 0) + 1
-        for word in B.split():
-            count[word] = count.get(word, 0) + 1
-
-        #Alternatively:
-        #count = collections.Counter(A.split())
-        #count += collections.Counter(B.split())
-
-        return [word for word in count if count[word] == 1]
- */
 
 function main() {
   const inputs = [
-    ["this apple is sweet", "this apple is sour"],
-    ["apple apple", "banana"],
+    [10, 2],
+    [3, 30, 34, 5, 9],
   ];
 
-  for (const [s1, s2] of inputs) {
-    const result = uncommonFromSentences(s1, s2);
+  for (const nums of inputs) {
+    const result = largestNumber(nums);
     console.log(result);
   }
 }
