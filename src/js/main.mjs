2d44ea2ccdf1c43d3fdc3d1db7afacd443bf96f5
mspark777 <topangel777@gmail.com>
2024-09-19 22:55:27 +0900

    241. Different Ways to Add Parentheses

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,33 +1,124 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {number[]} nums
- * @return {string}
+ * @param {string} ch
+ * @return {boolean}
  */
-function largestNumber(nums) {
-  const strs = nums
-    .map((n) => n.toString())
-    .sort((left, right) => {
-      const l = left + right;
-      const r = right + left;
-      return r.localeCompare(l);
-    });
+function isDigit(ch) {
+  const ZERO = "0".charCodeAt(0);
+  const NINE = "9".charCodeAt(0);
+  const code = ch.charCodeAt(0);
+  return ZERO <= code && code <= NINE;
+}
 
-  if (strs.at(0) === "0") {
-    return "0";
+/**
+ * @param {string} expression
+ * @return {number[][][]}
+ */
+function initializeBaseCases(expression) {
+  const dp = [];
+
+  for (let i = 0; i < expression.length; i += 1) {
+    const row = [];
+    for (let j = 0; j < expression.length; j += 1) {
+      row.push([]);
+    }
+
+    dp.push(row);
   }
 
-  return strs.join("");
+  for (let i = 0; i < expression.length; i += 1) {
+    for (let j = 0; j < expression.length; j += 1) {
+      dp[i][j] = [];
+    }
+  }
+
+  const ZERO = "0".charCodeAt(0);
+  for (let i = 0; i < expression.length; i += 1) {
+    if (!isDigit(expression.at(i))) {
+      continue;
+    }
+
+    const dig1 = expression.charCodeAt(i) - ZERO;
+    if (i + 1 < expression.length && isDigit(expression.at(i + 1))) {
+      const dig2 = expression.charCodeAt(i + 1) - ZERO;
+      const n = dig1 * 10 + dig2;
+      dp[i][i + 1].push(n);
+    }
+    dp[i][i].push(dig1);
+  }
+
+  return dp;
+}
+
+/**
+ * @param {string} op
+ * @param {number[]} leftResults
+ * @param {number[]} rightResults
+ * @param {number[]} results
+ * @returns {undefined}
+ */
+function computeResults(op, leftResults, rightResults, results) {
+  for (const leftValue of leftResults) {
+    for (const rightValue of rightResults) {
+      if (op === "+") {
+        results.push(leftValue + rightValue);
+      } else if (op === "-") {
+        results.push(leftValue - rightValue);
+      } else if (op === "*") {
+        results.push(leftValue * rightValue);
+      }
+    }
+  }
+}
+
+/**
+ * @param {string} expression
+ * @param {number[][][]} dp
+ * @param {number} start
+ * @param {number} end
+ * @returns {undefined}
+ */
+function processSubexpression(expression, dp, start, end) {
+  for (let split = start; split <= end; split += 1) {
+    if (isDigit(expression.at(split))) {
+      continue;
+    }
+
+    const leftResults = dp[start][split - 1];
+    const rightResults = dp[split + 1][end];
+
+    computeResults(
+      expression.at(split),
+      leftResults,
+      rightResults,
+      dp[start][end],
+    );
+  }
+}
+
+/**
+ * @param {string} expression
+ * @return {number[]}
+ */
+function diffWaysToCompute(expression) {
+  const dp = initializeBaseCases(expression);
+
+  for (let i = 3; i <= expression.length; i += 1) {
+    for (let j = 0; j + i - 1 < expression.length; j += 1) {
+      const k = i + j - 1;
+      processSubexpression(expression, dp, j, k);
+    }
+  }
+
+  return dp[0][expression.length - 1];
 }
 
 function main() {
-  const inputs = [
-    [10, 2],
-    [3, 30, 34, 5, 9],
-  ];
+  const inputs = ["2-1-1", "2*3-4*5"];
 
-  for (const nums of inputs) {
-    const result = largestNumber(nums);
+  for (const expression of inputs) {
+    const result = diffWaysToCompute(expression);
     console.log(result);
   }
 }
