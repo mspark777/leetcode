e7258a3b97255f47d868032e9c61f7cf24d6f15a
mspark777 <topangel777@gmail.com>
2024-09-20 22:56:12 +0900

    214. Shortest Palindrome

--- src/c/main.c
+++ src/c/main.c
@@ -1,39 +1,40 @@
 #include "./main.h"
 
-int findTheLongestSubstring(char *s)
+char *shortestPalindrome(char *s)
 {
-	const char A = 'a';
-	const char E = 'e';
-	const char I = 'i';
-	const char O = 'o';
-	const char U = 'u';
+	long long hash_base = 29;
+	long long mod_value = 1e9 + 7;
+	long long forward_hash = 0;
+	long long reverse_hash = 0;
+	long long power_value = 1;
+	long long palindrome_end_index = -1;
 
-	int prefix_xor = 0;
-	int char_map[26] = {
-		0,
-	};
-	char_map[0] = 1;
-	char_map[E - A] = 2;
-	char_map[I - A] = 4;
-	;
-	char_map[O - A] = 8;
-	char_map[U - A] = 16;
-
-	int mp[32] = {};
-	memset(mp, -1, sizeof(mp));
-	int result = 0;
-
+	int slen = 0;
 	for (int i = 0; s[i] != 0; i += 1) {
-		prefix_xor ^= char_map[s[i] - A];
-		if (mp[prefix_xor] == -1 && prefix_xor != 0) {
-			mp[prefix_xor] = i;
-		}
+		slen += 1;
 
-		int len = i - mp[prefix_xor];
-		if (result < len) {
-			result = len;
+		char ch = s[i];
+		int j = ch - 'a';
+		forward_hash = (forward_hash * hash_base + (j + 1)) % mod_value;
+
+		reverse_hash =
+			(reverse_hash + (j + 1) * power_value) % mod_value;
+		power_value = (power_value * hash_base) % mod_value;
+
+		if (forward_hash == reverse_hash) {
+			palindrome_end_index = i;
 		}
 	}
+
+	int suffix_len = slen - (palindrome_end_index + 1);
+	char *suffix = s + palindrome_end_index + 1;
+	char *result = calloc(slen + suffix_len + 1, sizeof(char));
+
+	for (int i = 0; i < suffix_len; i += 1) {
+		result[i] = suffix[suffix_len - (i + 1)];
+	}
+
+	strcpy(result + suffix_len, s);
 
 	return result;
 }
