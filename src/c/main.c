4194efaf6a46083593aeb902a602bfdbb6f9132c
mspark777 <topangel777@gmail.com>
2024-09-28 23:55:57 +0900

    641. Design Circular Deque

--- src/c/main.c
+++ src/c/main.c
@@ -1,33 +1,102 @@
 #include "./main.h"
 
-int count_steps(long long n, long long prefix1, long long prefix2)
-{
-	long long n1 = n + 1;
-	long long steps = 0;
-	while (prefix1 <= n) {
-		steps += (n1 < prefix2 ? n1 : prefix2) - prefix1;
-		prefix1 *= 10;
-		prefix2 *= 10;
-	}
+typedef struct {
+	int *queue;
+	int front;
+	int rear;
+	int size;
+	int capacity;
+} MyCircularDeque;
 
-	return steps;
+MyCircularDeque *myCircularDequeCreate(int k)
+{
+	MyCircularDeque *deque = malloc(sizeof(MyCircularDeque));
+	deque->queue = malloc(sizeof(int) * k);
+	deque->front = 0;
+	deque->rear = k - 1;
+	deque->size = 0;
+	deque->capacity = k;
+
+	return deque;
 }
 
-int findKthNumber(int n, int k)
+void myCircularDequeFree(MyCircularDeque *obj)
 {
-	long long result = 1;
-	k -= 1;
+	free(obj->queue);
+	free(obj);
+}
 
-	while (k > 0) {
-		int step = count_steps(n, result, result + 1);
-		if (step <= k) {
-			result += 1;
-			k -= step;
-		} else {
-			result *= 10;
-			k -= 1;
-		}
+bool myCircularDequeInsertFront(MyCircularDeque *obj, int value);
+bool myCircularDequeInsertLast(MyCircularDeque *obj, int value);
+bool myCircularDequeDeleteFront(MyCircularDeque *obj);
+bool myCircularDequeDeleteLast(MyCircularDeque *obj);
+int myCircularDequeGetFront(MyCircularDeque *obj);
+int myCircularDequeGetRear(MyCircularDeque *obj);
+bool myCircularDequeIsEmpty(MyCircularDeque *obj);
+bool myCircularDequeIsFull(MyCircularDeque *obj);
+
+bool myCircularDequeInsertFront(MyCircularDeque *obj, int value)
+{
+	if (myCircularDequeIsFull(obj)) {
+		return 0;
 	}
 
-	return result;
+	obj->front = (obj->front - 1 + obj->capacity) % obj->capacity;
+	obj->queue[obj->front] = value;
+	obj->size += 1;
+	return 1;
+}
+
+bool myCircularDequeInsertLast(MyCircularDeque *obj, int value)
+{
+	if (myCircularDequeIsFull(obj)) {
+		return 0;
+	}
+
+	obj->rear = (obj->rear + 1) % obj->capacity;
+	obj->queue[obj->rear] = value;
+	obj->size += 1;
+	return 1;
+}
+
+bool myCircularDequeDeleteFront(MyCircularDeque *obj)
+{
+	if (myCircularDequeIsEmpty(obj)) {
+		return 0;
+	}
+
+	obj->front = (obj->front + 1) % obj->capacity;
+	obj->size -= 1;
+	return 1;
+}
+
+bool myCircularDequeDeleteLast(MyCircularDeque *obj)
+{
+	if (myCircularDequeIsEmpty(obj)) {
+		return 0;
+	}
+
+	obj->rear = (obj->rear - 1 + obj->capacity) % obj->capacity;
+	obj->size -= 1;
+	return 1;
+}
+
+int myCircularDequeGetFront(MyCircularDeque *obj)
+{
+	return myCircularDequeIsEmpty(obj) ? -1 : obj->queue[obj->front];
+}
+
+int myCircularDequeGetRear(MyCircularDeque *obj)
+{
+	return myCircularDequeIsEmpty(obj) ? -1 : obj->queue[obj->rear];
+}
+
+bool myCircularDequeIsEmpty(MyCircularDeque *obj)
+{
+	return obj->size < 1;
+}
+
+bool myCircularDequeIsFull(MyCircularDeque *obj)
+{
+	return obj->size >= obj->capacity;
 }
