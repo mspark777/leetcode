5cfdd0c010a7ee4debde50abda0ee228a9c01310
mspark777 <topangel777@gmail.com>
2024-10-02 17:16:15 +0900

    657. Robot Return to Origin

--- src/c/main.c
+++ src/c/main.c
@@ -1,36 +1,21 @@
 #include "./main.h"
 
-void helper(struct TreeNode *node, unsigned int *first_min,
-	    unsigned int *second_min)
+bool judgeCircle(char *moves)
 {
-	if (node == NULL) {
-		return;
+	int x = 0;
+	int y = 0;
+	for (int i = 0; moves[i] != 0; i += 1) {
+		const char ch = moves[i];
+		if (ch == 'U') {
+			y += 1;
+		} else if (ch == 'D') {
+			y -= 1;
+		} else if (ch == 'R') {
+			x += 1;
+		} else if (ch == 'L') {
+			x -= 1;
+		}
 	}
 
-	const unsigned int val = node->val;
-
-	if (val < *first_min) {
-		*second_min = *first_min;
-		*first_min = val;
-	} else if ((val > *first_min) && (val < *second_min)) {
-		*second_min = val;
-	}
-
-	helper(node->left, first_min, second_min);
-	helper(node->right, first_min, second_min);
-}
-
-int findSecondMinimumValue(struct TreeNode *root)
-{
-	if (root == NULL) {
-		return -1;
-	}
-
-	const unsigned int MAX = 0xFFFFFFFF;
-
-	unsigned int first_min = root->val;
-	unsigned int second_min = MAX;
-	helper(root, &first_min, &second_min);
-
-	return second_min == MAX ? -1 : second_min;
+	return (x == 0) && (y == 0);
 }
