0b45282ec518122144de82bab60d439a888ed3b1
mspark777 <topangel777@gmail.com>
2024-10-07 14:48:42 +0900

    2696. Minimum String Length After Removing Substrings

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,42 +1,36 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {string} sentence1
- * @param {string} sentence2
- * @return {boolean}
+ * @param {string} s
+ * @return {number}
  */
-function areSentencesSimilar(sentence1, sentence2) {
-  const words1 = sentence1.split(" ");
-  const words2 = sentence2.split(" ");
-  let ends1 = words1.length - 1;
-  let ends2 = words2.length - 1;
+function minLength(s) {
+  /** @type {number[]} */
+  const stack = [];
 
-  if (ends1 > ends2) {
-    return areSentencesSimilar(sentence2, sentence1);
+  for (const ch of s) {
+    if (stack.length < 1) {
+      stack.push(ch);
+      continue;
+    }
+
+    if (ch === "B" && stack.at(-1) === "A") {
+      stack.pop();
+    } else if (ch === "D" && stack.at(-1) === "C") {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
   }
 
-  let start = 0;
-  while (start < words1.length && words1[start] === words2[start]) {
-    start += 1;
-  }
-
-  while (ends1 >= 0 && words1[ends1] === words2[ends2]) {
-    ends1 -= 1;
-    ends2 -= 1;
-  }
-
-  return ends1 < start;
+  return stack.length;
 }
 
 function main() {
-  const inputs = [
-    ["My name is Haley", "My Haley"],
-    ["of", "A lot of words"],
-    ["Eating right now", "Eating"],
-  ];
+  const inputs = ["ABFCACDB", "ACBBD"];
 
-  for (const [s1, s2] of inputs) {
-    const result = areSentencesSimilar(s1, s2);
+  for (const s of inputs) {
+    const result = minLength(s);
     console.log(result);
   }
 }
