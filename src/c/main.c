3c805217baba29cf3005f8883563ec43e76926bd
mspark777 <topangel777@gmail.com>
2024-10-08 16:45:27 +0900

    674. Longest Continuous Increasing Subsequence

--- src/c/main.c
+++ src/c/main.c
@@ -1,16 +1,25 @@
 #include "./main.h"
 
-int minSwaps(char *s)
+int findLengthOfLCIS(int *nums, int nums_size)
 {
-	int top = 0;
-	for (int i = 0; s[i] != 0; i += 1) {
-		const char ch = s[i];
-		if (ch == '[') {
-			top += 1;
-		} else if (top > 0) {
-			top -= 1;
+	int result = 1;
+	int len = 1;
+	for (int i = 1; i < nums_size; i += 1) {
+		const int prev = nums[i - 1];
+		const int curr = nums[i];
+		if (prev < curr) {
+			len += 1;
+		} else {
+			if (len > result) {
+				result = len;
+			}
+			len = 1;
 		}
 	}
 
-	return (top + 1) / 2;
+	if (len > result) {
+		result = len;
+	}
+
+	return result;
 }
