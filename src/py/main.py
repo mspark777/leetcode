bcddb83975c70cbba7888677a4b59062a04354a2
mspark777 <topangel777@gmail.com>
2024-10-14 12:56:54 +0900

    2530. Maximal Score After Applying K Operations

--- src/py/main.py
+++ src/py/main.py
@@ -1,42 +1,30 @@
 from __future__ import annotations
 from typing import List
-from heapq import heappop, heappush
+from heapq import heapify, heappop, heappush
+from math import ceil
 
 
 class Solution:
-    def smallestChair(self, times: List[List[int]], target_friend: int) -> int:
-        sorted_times = sorted(
-            [(arrival, leave, index) for index, (arrival, leave) in enumerate(times)]
-        )
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        result = 0
 
-        next_chair = 0
-        available_chairs: list[int] = []
-        leaving_queue: list[tuple[int, int]] = []
+        max_heap = [-i for i in nums]
+        heapify(max_heap)
 
-        for arrival, leave, index in sorted_times:
-            while leaving_queue and leaving_queue[0][0] <= arrival:
-                _, chair = heappop(leaving_queue)
-                heappush(available_chairs, chair)
+        while k > 0:
+            k -= 1
+            top = -heappop(max_heap)
+            result += top
+            heappush(max_heap, -ceil(top / 3))
 
-            if available_chairs:
-                current_chair = heappop(available_chairs)
-            else:
-                current_chair = next_chair
-                next_chair += 1
-
-            heappush(leaving_queue, (leave, current_chair))
-
-            if index == target_friend:
-                return current_chair
-
-        return 0
+        return result
 
 
 def main():
-    inputs = [([[1, 4], [2, 3], [4, 6]], 1), ([[3, 10], [1, 5], [2, 6]], 0)]
+    inputs = [([10, 10, 10, 10, 10], 5), ([1, 10, 3, 3, 3], 3)]
 
-    for times, target in inputs:
-        result = Solution().smallestChair(times, target)
+    for nums, k in inputs:
+        result = Solution().maxKelements(nums, k)
         print(result)
 
 
