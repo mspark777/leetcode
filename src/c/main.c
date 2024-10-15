48d508975b48eceea6a234b681508163f7232d61
mspark777 <topangel777@gmail.com>
2024-10-15 09:56:15 +0900

    2938. Separate Black and White Balls

--- src/c/main.c
+++ src/c/main.c
@@ -1,57 +1,18 @@
 #include "./main.h"
 
-int to_int(char *nums)
+long long minimumSteps(char *s)
 {
-	int n = 0;
-	int i = 0;
-	int minus = 0;
-	if (nums[i] == '-') {
-		minus = 1;
-		i += 1;
-	}
+	long long result = 0;
+	long long black_ball_count = 0;
 
-	while (nums[i] != 0) {
-		n *= 10;
-		n += nums[i] - '0';
-		i += 1;
-	}
-
-	return minus ? -n : n;
-}
-
-int calPoints(char **operations, int operations_size)
-{
-	int top_idx = -1;
-	int stack[1000] = {
-		0,
-	};
-
-	for (int i = 0; i < operations_size; i += 1) {
-		char *oper = operations[i];
-		char ch = oper[0];
-		if (ch == '+') {
-			int top = stack[top_idx];
-			top_idx -= 1;
-			int new_top = top + stack[top_idx];
-			top_idx += 1;
-			stack[top_idx] = top;
-			top_idx += 1;
-			stack[top_idx] = new_top;
-		} else if (ch == 'D') {
-			int top = stack[top_idx];
-			top_idx += 1;
-			stack[top_idx] = top * 2;
-		} else if (ch == 'C') {
-			top_idx -= 1;
+	for (int i = 0; s[i] != 0; i += 1) {
+		const char ch = s[i];
+		if (ch == '0') {
+			result += black_ball_count;
 		} else {
-			top_idx += 1;
-			stack[top_idx] = to_int(oper);
+			black_ball_count += 1;
 		}
 	}
 
-	int result = 0;
-	for (int i = 0; i <= top_idx; i += 1) {
-		result += stack[i];
-	}
 	return result;
 }
