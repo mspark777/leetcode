d7ccaeb1f59b92dd0a15be07c1f60cca0742f59a
mspark777 <topangel777@gmail.com>
2024-10-17 10:32:41 +0900

    700. Search in a Binary Search Tree

--- src/c/main.c
+++ src/c/main.c
@@ -1,64 +1,15 @@
 #include "./main.h"
 
-char *to_string(int n, int len)
+struct TreeNode *searchBST(struct TreeNode *root, int val)
 {
-	char *s = calloc(sizeof(char), len + 1);
-	for (int i = len - 1; i >= 0; i -= 1) {
-		s[i] = (n % 10) + '0';
-		n /= 10;
+	if (root == NULL) {
+		return NULL;
 	}
 
-	return s;
-}
-
-int get_string_length(int n)
-{
-	int len = 0;
-	while (n > 0) {
-		n /= 10;
-		len += 1;
+	if (root->val == val) {
+		return root;
 	}
 
-	return len;
-}
-
-int to_int(char *s, int len)
-{
-	int n = 0;
-	for (int i = 0; i < len; i += 1) {
-		n *= 10;
-		n += (int)(s[i] - '0');
-	}
-
-	return n;
-}
-
-int maximumSwap(int num)
-{
-	const int n = get_string_length(num);
-	char *s = to_string(num, n);
-
-	int max_digit_idx = -1;
-	int swap_idx0 = -1;
-	int swap_idx1 = -1;
-
-	for (int i = n - 1; i >= 0; i -= 1) {
-		if (max_digit_idx == -1 || s[i] > s[max_digit_idx]) {
-			max_digit_idx = i;
-		} else if (s[i] < s[max_digit_idx]) {
-			swap_idx0 = i;
-			swap_idx1 = max_digit_idx;
-		}
-	}
-
-	if (swap_idx0 != -1 && swap_idx1 != -1) {
-		const char temp = s[swap_idx0];
-		s[swap_idx0] = s[swap_idx1];
-		s[swap_idx1] = temp;
-	}
-
-	const int result = to_int(s, n);
-	free(s);
-
-	return result;
+	struct TreeNode *left = searchBST(root->left, val);
+	return left != NULL ? left : searchBST(root->right, val);
 }
