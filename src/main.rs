e0e05ed0e7dc581eb2c409480889dc1f0528bda9
mspark777 <topangel777@gmail.com>
2024-10-18 09:23:26 +0900

    2044. Count Number of Maximum Bitwise-OR Subsets

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
 struct Solution {}
 
 impl Solution {
-    pub fn pivot_index(nums: Vec<i32>) -> i32 {
-        let mut total_sum = 0;
-        for n in &nums {
-            total_sum += n;
-        }
+    pub fn count_max_or_subsets(nums: Vec<i32>) -> i32 {
+        let mut max_idx = 0;
+        let mut dp = vec![0; 1 << 17];
 
-        let mut left_sum = 0;
-        for (i, &n) in nums.iter().enumerate() {
-            let right_sum = total_sum - (left_sum + n);
-            if right_sum == left_sum {
-                return i as i32;
+        dp[0] = 1;
+
+        for &num in nums.iter() {
+            for i in (0..=max_idx).rev() {
+                let n = num as usize;
+                let idx = i as usize;
+                dp[idx | n] += dp[idx];
             }
 
-            left_sum += n;
+            max_idx |= num;
         }
 
-        return -1;
+        return dp[max_idx as usize];
     }
 }
 
 fn main() {
-    let inputs = vec![vec![1, 7, 3, 6, 5, 6], vec![1, 2, 3], vec![2, 1, -1]];
+    let inputs = vec![vec![3, 1], vec![2, 2, 2], vec![3, 2, 1, 5]];
 
     for input in inputs {
-        let result = Solution::pivot_index(input);
+        let result = Solution::count_max_or_subsets(input);
         println!("{result}");
     }
 }
