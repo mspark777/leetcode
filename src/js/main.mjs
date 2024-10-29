4e44e638cf0dc0724ca931d3c0ee7a6fbe75ea39
mspark777 <topangel777@gmail.com>
2024-10-29 09:59:59 +0900

    2684. Maximum Number of Moves in a Grid

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,49 +1,63 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {number[]} nums
+ * @param {number[][]} grid
  * @return {number}
  */
-function findShortestSubArray(nums) {
-  /** @type {Map<number, number>} */
-  const lefts = new Map();
-  /** @type {Map<number, number>} */
-  const rights = new Map();
-  /** @type {Map<number, number>} */
-  const counts = new Map();
+function maxMoves(grid) {
+  const R = grid.length;
+  const C = grid[0].length;
+  let result = 0;
 
-  for (const [i, num] of nums.entries()) {
-    if (!lefts.has(num)) {
-      lefts.set(num, i);
-    }
-
-    rights.set(num, i);
-
-    const count = counts.get(num) ?? 0;
-    counts.set(num, count + 1);
+  /** @type {number[][]} */
+  const dp = [];
+  for (let i = 0; i < R; i += 1) {
+    dp.push([1, 0]);
   }
 
-  let result = nums.length;
-  const degree = Math.max(...counts.values());
-  for (const [num, count] of counts) {
-    if (count === degree) {
-      const left = lefts.get(num);
-      const right = rights.get(num);
-      result = Math.min(result, right - left + 1);
+  for (let c = 1; c < C; c += 1) {
+    for (let r = 0; r < R; r += 1) {
+      if (grid[r][c] > grid[r][c - 1] && dp[r][0] > 0) {
+        dp[r][1] = Math.max(dp[r][1], dp[r][0] + 1);
+      }
+
+      if (r - 1 >= 0 && grid[r][c] > grid[r - 1][c - 1] && dp[r - 1][0] > 0) {
+        dp[r][1] = Math.max(dp[r][1], dp[r - 1][0] + 1);
+      }
+
+      if (r + 1 < R && grid[r][c] > grid[r + 1][c - 1] && dp[r + 1][0] > 0) {
+        dp[r][1] = Math.max(dp[r][1], dp[r + 1][0] + 1);
+      }
+
+      result = Math.max(result, dp[r][1] - 1);
+    }
+
+    for (let k = 0; k < R; k += 1) {
+      dp[k][0] = dp[k][1];
+      dp[k][1] = 0;
     }
   }
-
   return result;
 }
 
 function main() {
   const inputs = [
-    [1, 2, 2, 3, 1],
-    [1, 2, 2, 3, 1, 4, 2],
+    [
+      [2, 4, 3, 5],
+      [5, 4, 9, 3],
+      [3, 4, 2, 11],
+      [10, 9, 13, 15],
+    ],
+    [
+      [3, 2, 4],
+      [2, 1, 9],
+      [1, 1, 7],
+    ],
   ];
 
-  for (const nums of inputs) {
-    const result = findShortestSubArray(nums);
+  for (const grid of inputs) {
+    const result = maxMoves(grid);
     console.log(result);
   }
 }
