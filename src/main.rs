47cefde55a797f4036f10374328246c44bcbb5af
mspark777 <topangel777@gmail.com>
2024-10-30 15:35:10 +0900

    1671. Minimum Number of Removals to Make Mountain Array

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,71 @@
 struct Solution {}
 
 impl Solution {
-    pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
-        let mut result: Vec<i32> = Vec::new();
-        for n in left..=right {
-            if Self::self_dividing(n) {
-                result.push(n);
+    pub fn minimum_mountain_removals(nums: Vec<i32>) -> i32 {
+        let lis_length = Self::get_longest_increasing_subsequence_length(&nums);
+
+        let mut rev_nums = nums.clone();
+        rev_nums.reverse();
+
+        let lds_length = Self::get_longest_increasing_subsequence_length(&rev_nums);
+
+        let mut result = i64::MAX;
+        for (&lis, &lds) in lis_length.iter().zip(lds_length.iter().rev()) {
+            if lis <= 1 || lds <= 1 {
+                continue;
             }
+
+            let v = Self::as_i64(nums.len()) - Self::as_i64(lis) - Self::as_i64(lds) + 1;
+            result = result.min(v);
         }
 
-        return result;
+        return result as i32;
     }
 
-    fn self_dividing(n: i32) -> bool {
-        let mut i = n;
+    fn as_i64(u: usize) -> i64 {
+        return u as i64;
+    }
 
-        while i > 0 {
-            let d = i % 10;
-            if d == 0 {
-                return false;
-            } else if (n % d) > 0 {
-                return false;
+    fn lower_bound(lis: &Vec<i32>, target: i32) -> usize {
+        let mut left = 0usize;
+        let mut right = lis.len();
+        while left < right {
+            let mid = (left + right) / 2;
+            if lis[mid] < target {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-
-            i /= 10;
         }
 
-        return true;
+        return left;
+    }
+
+    fn get_longest_increasing_subsequence_length(nums: &Vec<i32>) -> Vec<usize> {
+        let mut lis_len = vec![1usize; nums.len()];
+        let mut lis = vec![nums[0]];
+
+        for (i, &num) in nums.iter().enumerate().skip(1) {
+            let idx = Self::lower_bound(&lis, num);
+
+            if idx == lis.len() {
+                lis.push(num);
+            } else {
+                lis[idx] = num;
+            }
+
+            lis_len[i] = lis.len();
+        }
+
+        return lis_len;
     }
 }
 
 fn main() {
-    let inputs = vec![(1, 22), (47, 85)];
+    let inputs = vec![vec![1, 3, 1], vec![2, 1, 1, 5, 6, 2, 3, 1]];
 
     for input in inputs {
-        let result = Solution::self_dividing_numbers(input.0, input.1);
+        let result = Solution::minimum_mountain_removals(input);
         println!("{result:?}");
     }
 }
