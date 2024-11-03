1a63a4e057a8e11122eb578c634750808f11b767
mspark777 <topangel777@gmail.com>
2024-11-03 16:56:22 +0900

    796. Rotate String

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,41 @@
 struct Solution {}
 
 impl Solution {
-    pub fn is_circular_sentence(sentence: String) -> bool {
-        let bytes = sentence.as_bytes();
-        if bytes.first().unwrap() != bytes.last().unwrap() {
+    pub fn rotate_string(s: String, goal: String) -> bool {
+        let slen = s.len();
+        if slen != goal.len() {
             return false;
         }
 
-        for i in 1..(bytes.len() - 1) {
-            if bytes[i] == (' ' as u8) {
-                if bytes[i - 1] != bytes[i + 1] {
-                    return false;
-                }
-            }
+        let doubled_len = slen * 2;
+        let doubled_string = format!("{s}{s}");
+        if let Some(i) = doubled_string.find(&goal) {
+            return i < doubled_len;
         }
 
-        return true;
+        return false;
     }
 }
 
 struct Input {
     s: String,
+    goal: String,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            s: "leetcode exercises sound delightful".to_string(),
+            s: String::from("abcde"),
+            goal: String::from("cdeab"),
         },
         Input {
-            s: "eetcode".to_string(),
-        },
-        Input {
-            s: "Leetcode is cool".to_string(),
+            s: String::from("abcde"),
+            goal: String::from("abced"),
         },
     ];
 
     for input in inputs {
-        let result = Solution::is_circular_sentence(input.s);
+        let result = Solution::rotate_string(input.s, input.goal);
         println!("{result}");
     }
 }
