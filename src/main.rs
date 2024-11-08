81e88f0a96a51bcee57c083ce1fb227856debca0
mspark777 <topangel777@gmail.com>
2024-11-08 23:00:20 +0900

    1829. Maximum XOR for Each Query

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 struct Solution {}
 
 impl Solution {
-    pub fn largest_combination(candidates: Vec<i32>) -> i32 {
-        let mut result = 0;
+    pub fn get_maximum_xor(nums: Vec<i32>, maximum_bit: i32) -> Vec<i32> {
+        let mut xor_product = nums.iter().fold(0, |acc, n| acc ^ n);
+        let mut result = Vec::<i32>::with_capacity(nums.len());
 
-        for i in 0..24 {
-            let mut count = 0;
-            for &num in candidates.iter() {
-                let mask = num & (1 << i);
-                if mask != 0 {
-                    count += 1;
-                }
-            }
-
-            result = result.max(count);
+        let mask = (1 << maximum_bit) - 1;
+        for num in nums.iter().rev() {
+            result.push(mask ^ xor_product);
+            xor_product ^= num;
         }
 
         return result;
@@ -21,21 +16,28 @@ impl Solution {
 }
 
 struct Input {
-    candidates: Vec<i32>,
+    nums: Vec<i32>,
+    maximum_bit: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            candidates: vec![16, 17, 71, 62, 12, 24, 14],
+            nums: vec![0, 1, 1, 3],
+            maximum_bit: 2,
         },
         Input {
-            candidates: vec![8, 8],
+            nums: vec![2, 3, 4, 7],
+            maximum_bit: 2,
+        },
+        Input {
+            nums: vec![0, 1, 2, 2, 5, 7],
+            maximum_bit: 3,
         },
     ];
 
     for input in inputs {
-        let result = Solution::largest_combination(input.candidates);
-        println!("{result}");
+        let result = Solution::get_maximum_xor(input.nums, input.maximum_bit);
+        println!("{result:?}");
     }
 }
