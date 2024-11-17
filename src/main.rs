b8d435ac4c9f11a45b1ab66f820ae534cf531f10
mspark777 <topangel777@gmail.com>
2024-11-17 11:51:15 +0900

    862. Shortest Subarray with Sum at Least K

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
+use std::collections::VecDeque;
+
 struct Solution {}
 
 impl Solution {
-    pub fn results_array(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        return Self::solve(nums, k as usize);
+    pub fn shortest_subarray(nums: Vec<i32>, k: i32) -> i32 {
+        let nums_len = nums.len();
+        let mut prefix_sums = vec![0i64; nums_len + 1];
+
+        for i in 1..=nums_len {
+            prefix_sums[i] = prefix_sums[i - 1] + (nums[i - 1] as i64);
+        }
+
+        let mut result = i32::MAX;
+        let mut candidate_indices = VecDeque::<usize>::with_capacity(nums_len + 1);
+        for i in 0..=nums_len {
+            while let Some(&candidate_index) = candidate_indices.front() {
+                let prefix_sum = prefix_sums[i] - prefix_sums[candidate_index];
+                if prefix_sum >= (k as i64) {
+                    result = result.min(Self::sub_usize(i, candidate_index));
+                    candidate_indices.pop_front();
+                } else {
+                    break;
+                }
+            }
+
+            while let Some(&candidate_index) = candidate_indices.back() {
+                if prefix_sums[i] <= prefix_sums[candidate_index] {
+                    candidate_indices.pop_back();
+                } else {
+                    break;
+                }
+            }
+
+            candidate_indices.push_back(i);
+        }
+
+        return if result == i32::MAX { -1 } else { result };
     }
 
-    fn solve(nums: Vec<i32>, k: usize) -> Vec<i32> {
-        if k == 1 {
-            return nums;
-        }
-
-        let nums_len = nums.len();
-        let mut result = vec![-1; nums_len - k + 1];
-        let mut consecutive_count = 1;
-
-        for i in 0..(nums_len - 1) {
-            let current = nums[i] + 1;
-            let next = nums[i + 1];
-            if current == next {
-                consecutive_count += 1;
-            } else {
-                consecutive_count = 1;
-            }
-
-            if consecutive_count >= k {
-                result[i + 2 - k] = nums[i + 1];
-            }
-        }
-
-        return result;
+    fn sub_usize(left: usize, right: usize) -> i32 {
+        return if left < right {
+            -Self::sub_usize(right, left)
+        } else {
+            (left - right) as i32
+        };
     }
 }
 
@@ -40,21 +55,21 @@ struct Input {
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![1, 2, 3, 4, 3, 2, 5],
-            k: 3,
+            nums: vec![1],
+            k: 1,
         },
         Input {
-            nums: vec![2, 2, 2, 2, 2],
+            nums: vec![1, 2],
             k: 4,
         },
         Input {
-            nums: vec![3, 2, 3, 2, 3, 2],
-            k: 2,
+            nums: vec![2, -1, 2],
+            k: 3,
         },
     ];
 
     for input in inputs {
-        let result = Solution::results_array(input.nums, input.k);
+        let result = Solution::shortest_subarray(input.nums, input.k);
         println!("{result:?}");
     }
 }
