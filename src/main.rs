9009ef5b1de0e043e2ef1de55802339fe422edc7
mspark777 <topangel777@gmail.com>
2024-11-17 15:24:23 +0900

    762. Prime Number of Set Bits in Binary Representation

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,54 @@
-use std::collections::HashMap;
-
 struct Solution {}
 
 impl Solution {
-    pub fn shortest_completing_word(license_plate: String, words: Vec<String>) -> String {
-        let license_map = Self::to_map(&license_plate);
-        let mut result = String::new();
-
-        for word in words.iter() {
-            let w_map = Self::to_map(word);
-            let mut found = true;
-            for (ch, &count) in license_map.iter() {
-                if let Some(&w_count) = w_map.get(ch) {
-                    if w_count < count {
-                        found = false;
-                        break;
-                    }
-                } else {
-                    found = false;
-                    break;
-                }
-            }
-
-            if !found {
-                continue;
-            }
-
-            if result.is_empty() {
-                result = word.clone();
-            } else if word.len() < result.len() {
-                result = word.clone();
-            }
+    pub fn count_prime_set_bits(left: i32, right: i32) -> i32 {
+        let mut result = 0;
+        for i in left..=right {
+            result += if Self::is_prime(i.count_ones()) { 1 } else { 0 };
         }
 
         return result;
     }
 
-    fn is_letter(ch: char) -> bool {
-        return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z');
-    }
-
-    fn to_map(s: &String) -> HashMap<char, i32> {
-        let mut ch_map = HashMap::<char, i32>::with_capacity(s.len());
-        for ch in s.to_lowercase().chars() {
-            if Self::is_letter(ch) {
-                if let Some(count) = ch_map.get_mut(&ch) {
-                    *count += 1;
-                } else {
-                    ch_map.insert(ch, 1);
-                }
-            }
-        }
-
-        return ch_map;
+    fn is_prime(i: u32) -> bool {
+        return if i == 2 {
+            true
+        } else if i == 3 {
+            true
+        } else if i == 5 {
+            true
+        } else if i == 7 {
+            true
+        } else if i == 11 {
+            true
+        } else if i == 13 {
+            true
+        } else if i == 17 {
+            true
+        } else if i == 19 {
+            true
+        } else {
+            false
+        };
     }
 }
 
 struct Input {
-    license_plate: &'static str,
-    words: Vec<&'static str>,
+    left: i32,
+    right: i32,
 }
 
 fn main() {
     let inputs = vec![
+        Input { left: 6, right: 10 },
         Input {
-            license_plate: "1s3 PSt",
-            words: vec!["step", "steps", "stripe", "stepple"],
-        },
-        Input {
-            license_plate: "1s3 456",
-            words: vec!["looks", "pest", "stew", "show"],
+            left: 10,
+            right: 15,
         },
     ];
 
     for input in inputs {
-        let result = Solution::shortest_completing_word(
-            input.license_plate.to_string(),
-            input.words.iter().map(|s| s.to_string()).collect(),
-        );
+        let result = Solution::count_prime_set_bits(input.left, input.right);
         println!("{result}");
     }
 }
