92d5cdf1357ef20d14c21b0b20252b419b5b9cac
mspark777 <topangel777@gmail.com>
2024-11-18 13:56:05 +0900

    1652. Defuse the Bomb

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,65 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_prime_set_bits(left: i32, right: i32) -> i32 {
-        let mut result = 0;
-        for i in left..=right {
-            result += if Self::is_prime(i.count_ones()) { 1 } else { 0 };
+    pub fn decrypt(code: Vec<i32>, k: i32) -> Vec<i32> {
+        let code_len = code.len();
+        if k == 0 {
+            return vec![0; code_len];
+        }
+
+        let key_len = k.abs() as usize;
+        let mut start = 1usize;
+        let mut end = key_len;
+
+        if k < 0 {
+            start = code_len - key_len;
+            end = code_len - 1;
+        }
+
+        let mut result = vec![0; code_len];
+        let mut sum = 0;
+        for i in start..=end {
+            sum += code[i];
+        }
+
+        for i in 0..code_len {
+            result[i] = sum;
+
+            sum -= code[start % code_len];
+
+            start += 1;
+            end += 1;
+
+            sum += code[end % code_len];
         }
 
         return result;
     }
-
-    fn is_prime(i: u32) -> bool {
-        return if i == 2 {
-            true
-        } else if i == 3 {
-            true
-        } else if i == 5 {
-            true
-        } else if i == 7 {
-            true
-        } else if i == 11 {
-            true
-        } else if i == 13 {
-            true
-        } else if i == 17 {
-            true
-        } else if i == 19 {
-            true
-        } else {
-            false
-        };
-    }
 }
 
 struct Input {
-    left: i32,
-    right: i32,
+    code: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = vec![
-        Input { left: 6, right: 10 },
         Input {
-            left: 10,
-            right: 15,
+            code: vec![5, 7, 1, 4],
+            k: 3,
+        },
+        Input {
+            code: vec![1, 2, 3, 4],
+            k: 0,
+        },
+        Input {
+            code: vec![2, 4, 9, 3],
+            k: -2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_prime_set_bits(input.left, input.right);
-        println!("{result}");
+        let result = Solution::decrypt(input.code, input.k);
+        println!("{result:?}");
     }
 }
