0f1ec86bc53135d242c4bde9ebb4da94bd6eaae4
mspark777 <topangel777@gmail.com>
2024-11-19 22:20:02 +0900

    812. Largest Triangle Area

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,42 @@
 struct Solution {}
 
 impl Solution {
-    pub fn number_of_lines(widths: Vec<i32>, s: String) -> Vec<i32> {
-        let mut number_of_lines = 1;
-        let mut width_of_pixels = 0;
+    pub fn largest_triangle_area(points: Vec<Vec<i32>>) -> f64 {
+        let mut max_area = 0.0f64;
 
-        for ch in s.chars() {
-            let width = widths[Self::to_idx(ch)];
-            let next_width = width_of_pixels + width;
-            if next_width > 100 {
-                width_of_pixels = width;
-                number_of_lines += 1;
-            } else {
-                width_of_pixels = next_width;
+        for i in points.iter() {
+            for j in points.iter() {
+                for k in points.iter() {
+                    let area = (i[0] * j[1] + j[0] * k[1] + k[0] * i[1]
+                        - j[0] * i[1]
+                        - k[0] * j[1]
+                        - i[0] * k[1])
+                        .abs();
+                    max_area = max_area.max(area as f64);
+                }
             }
         }
 
-        return vec![number_of_lines, width_of_pixels];
-    }
-
-    fn to_idx(ch: char) -> usize {
-        let code = ch as u8;
-        let a = b'a';
-
-        return (code - a) as usize;
+        return max_area * 0.5;
     }
 }
 
 struct Input {
-    widths: Vec<i32>,
-    s: &'static str,
+    points: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            widths: vec![
-                10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-                10, 10, 10, 10, 10,
-            ],
-            s: "abcdefghijklmnopqrstuvwxyz",
+            points: vec![vec![0, 0], vec![0, 1], vec![1, 0], vec![0, 2], vec![2, 0]],
         },
         Input {
-            widths: vec![
-                4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-                10, 10, 10, 10, 10,
-            ],
-            s: "bbbcccdddaaa",
+            points: vec![vec![1, 0], vec![0, 0], vec![0, 1]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::number_of_lines(input.widths, input.s.to_string());
-        println!("{result:?}");
+        let result = Solution::largest_triangle_area(input.points);
+        println!("{result}");
     }
 }
