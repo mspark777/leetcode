c90500bcebc4f7d72f34b5105c729dcc4c2b89e8
mspark777 <topangel777@gmail.com>
2024-11-20 09:34:58 +0900

    2516. Take K of Each Character From Left and Right

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,67 @@
 struct Solution {}
 
 impl Solution {
-    pub fn largest_triangle_area(points: Vec<Vec<i32>>) -> f64 {
-        let mut max_area = 0.0f64;
+    pub fn take_characters(s: String, k: i32) -> i32 {
+        let mut counts = vec![0; 3];
+        let chars: Vec<char> = s.chars().collect();
 
-        for i in points.iter() {
-            for j in points.iter() {
-                for k in points.iter() {
-                    let area = (i[0] * j[1] + j[0] * k[1] + k[0] * i[1]
-                        - j[0] * i[1]
-                        - k[0] * j[1]
-                        - i[0] * k[1])
-                        .abs();
-                    max_area = max_area.max(area as f64);
-                }
+        for &ch in chars.iter() {
+            let idx = Self::char_to_idx(ch);
+            counts[idx] += 1;
+        }
+
+        for &count in counts.iter() {
+            if count < k {
+                return -1;
             }
         }
 
-        return max_area * 0.5;
+        let mut window = vec![0; 3];
+        let mut left = 0usize;
+        let mut max_window = 0usize;
+
+        for right in 0..chars.len() {
+            window[Self::char_to_idx(chars[right])] += 1;
+
+            while left <= right
+                && (counts[0] - window[0] < k
+                    || counts[1] - window[1] < k
+                    || counts[2] - window[2] < k)
+            {
+                window[Self::char_to_idx(chars[left])] -= 1;
+                left += 1;
+            }
+
+            max_window = max_window.max(right + 1 - left);
+        }
+
+        return (chars.len() - max_window) as i32;
+    }
+
+    fn char_to_idx(ch: char) -> usize {
+        let code = ch as u8;
+        let a = b'a';
+
+        return (code - a) as usize;
     }
 }
 
 struct Input {
-    points: Vec<Vec<i32>>,
+    s: &'static str,
+    k: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            points: vec![vec![0, 0], vec![0, 1], vec![1, 0], vec![0, 2], vec![2, 0]],
+            s: "aabaaaacaabc",
+            k: 2,
         },
-        Input {
-            points: vec![vec![1, 0], vec![0, 0], vec![0, 1]],
-        },
+        Input { s: "a", k: 1 },
     ];
 
     for input in inputs {
-        let result = Solution::largest_triangle_area(input.points);
+        let result = Solution::take_characters(input.s.to_string(), input.k);
         println!("{result}");
     }
 }
