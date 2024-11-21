92ccaf9edcc47ff41ff7a5b1c9b523a67cc32fe1
mspark777 <topangel777@gmail.com>
2024-11-22 00:20:39 +0900

    824. Goat Latin

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,53 @@
 struct Solution {}
 
 impl Solution {
-    pub fn shortest_to_char(s: String, c: char) -> Vec<i32> {
-        let chars: Vec<char> = s.chars().collect();
-        let mut result = vec![0; chars.len()];
+    pub fn to_goat_latin(sentence: String) -> String {
+        let words: Vec<String> = sentence.split(" ").map(|s| s.to_string()).collect();
+        let mut chunks = Vec::<String>::new();
 
-        for (i, &ch) in chars.iter().enumerate() {
-            result[i] = if ch == c { 0 } else { chars.len() as i32 };
+        for (i, word) in words.iter().enumerate() {
+            let done = Self::process_word(word, i + 1);
+            chunks.push(done);
         }
 
-        for i in 1..chars.len() {
-            result[i] = result[i].min(result[i - 1] + 1);
-        }
+        return chunks.join(" ");
+    }
 
-        for i in (0..(chars.len() - 1)).rev() {
-            result[i] = result[i].min(result[i + 1] + 1);
-        }
+    fn is_first_char_vowel(ch: char) -> bool {
+        return match ch {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => true,
+            _ => false,
+        };
+    }
 
-        return result;
+    fn process_word(word: &String, idx: usize) -> String {
+        let first = word.chars().nth(0).unwrap();
+        let append: String = (vec!['a'; idx]).iter().collect();
+        return if Self::is_first_char_vowel(first) {
+            format!("{}ma{}", word, append)
+        } else {
+            let sub: String = word.chars().skip(1).collect();
+            format!("{}{}ma{}", sub, first, append)
+        };
     }
 }
 
 struct Input {
-    s: &'static str,
-    c: char,
+    sentence: &'static str,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            s: "loveleetcode",
-            c: 'e',
+            sentence: "I speak Goat Latin",
         },
-        Input { s: "aaab", c: 'b' },
+        Input {
+            sentence: "The quick brown fox jumped over the lazy dog",
+        },
     ];
 
     for input in inputs {
-        let result = Solution::shortest_to_char(input.s.to_string(), input.c);
-        println!("{result:?}");
+        let result = Solution::to_goat_latin(input.sentence.to_string());
+        println!("{result}");
     }
 }
