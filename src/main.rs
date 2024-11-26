af0103d8a454137ac499a84748859b29c6c2cf7a
mspark777 <topangel777@gmail.com>
2024-11-26 09:49:36 +0900

    2924. Find Champion II

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,50 @@
-use std::collections::{HashSet, VecDeque};
-
 struct Solution {}
 
 impl Solution {
-    pub fn sliding_puzzle(board: Vec<Vec<i32>>) -> i32 {
-        let directions = vec![
-            vec![1, 3],
-            vec![0, 2, 4],
-            vec![1, 5],
-            vec![0, 4],
-            vec![1, 3, 5],
-            vec![2, 4],
-        ];
-        let target = "123450".to_string();
-        let start_state = board
-            .iter()
-            .flatten()
-            .map(|&x| x.to_string())
-            .collect::<String>();
+    pub fn find_champion(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        return Self::solve(n as usize, edges);
+    }
 
-        let mut visited = HashSet::<String>::new();
-        let mut queue = VecDeque::<String>::new();
-        queue.push_back(start_state.clone());
-        visited.insert(start_state);
-
-        let mut result = 0;
-        while !queue.is_empty() {
-            let mut queue_size = queue.len();
-            while queue_size > 0 {
-                queue_size -= 1;
-                let current_state = queue.pop_front().unwrap();
-                if current_state == target {
-                    return result;
-                }
-
-                let zero_pos = current_state.chars().position(|c| c == '0').unwrap();
-
-                for &new_pos in directions[zero_pos].iter() {
-                    let mut next_state = current_state.clone().chars().collect::<Vec<char>>();
-                    let new_char = next_state[new_pos];
-                    let zero_char = next_state[zero_pos];
-                    next_state[zero_pos] = new_char;
-                    next_state[new_pos] = zero_char;
-
-                    let next_str = next_state.iter().collect::<String>();
-                    if visited.contains(&next_str) {
-                        continue;
-                    }
-
-                    visited.insert(next_str.clone());
-                    queue.push_back(next_str);
-                }
-            }
-
-            result += 1;
+    fn solve(n: usize, edges: Vec<Vec<i32>>) -> i32 {
+        let mut indegree = vec![0; n];
+        for edge in edges.iter() {
+            let idx = edge[1] as usize;
+            indegree[idx] += 1;
         }
 
-        return -1;
+        let mut champion = -1;
+        let mut champion_count = 0;
+
+        for (team, &count) in indegree.iter().enumerate() {
+            if count == 0 {
+                champion_count += 1;
+                champion = team as i32;
+            }
+        }
+
+        return if champion_count == 1 { champion } else { -1 };
     }
 }
 
 struct Input {
-    board: Vec<Vec<i32>>,
+    n: i32,
+    edges: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            board: vec![vec![1, 2, 3], vec![4, 0, 5]],
+            n: 3,
+            edges: vec![vec![0, 1], vec![1, 2]],
         },
         Input {
-            board: vec![vec![1, 2, 3], vec![5, 4, 0]],
-        },
-        Input {
-            board: vec![vec![4, 1, 2], vec![5, 0, 3]],
+            n: 4,
+            edges: vec![vec![0, 2], vec![1, 3], vec![1, 2]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::sliding_puzzle(input.board);
+        let result = Solution::find_champion(input.n, input.edges);
         println!("{result}");
     }
 }
