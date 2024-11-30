b0c8fd5173ec9d4858e6f2dddff7f4ef5262a61a
mspark777 <topangel777@gmail.com>
2024-11-30 21:50:27 +0900

    2097. Valid Arrangement of Pairs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,68 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
 struct Solution {}
 
 impl Solution {
-    pub fn flip_and_invert_image(image: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let mut result = Vec::<Vec<i32>>::with_capacity(image.len());
-        for row in image.iter() {
-            let row_len = row.len();
-            let mut new_col = row.clone();
-            let end = (row_len + 1) / 2;
-            for i in 0..end {
-                let j = row_len - (i + 1);
-                new_col[i] = row[j] ^ 1;
-                new_col[j] = row[i] ^ 1;
+    pub fn valid_arrangement(pairs: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut adjacency_matrix = HashMap::<i32, VecDeque<i32>>::new();
+        let mut in_degree = HashMap::<i32, i32>::new();
+        let mut out_degree = HashMap::<i32, i32>::new();
+
+        for pair in pairs.iter() {
+            let start = pair[0];
+            let end = pair[1];
+            adjacency_matrix
+                .entry(start)
+                .or_insert(VecDeque::new())
+                .push_back(end);
+            *out_degree.entry(start).or_insert(0) += 1;
+            *in_degree.entry(end).or_insert(0) += 1;
+        }
+
+        let mut nodes = Vec::<i32>::new();
+        let mut start_node = -1;
+        for (&node, &outd) in out_degree.iter() {
+            let def = 0;
+            let &ind = in_degree.get(&node).unwrap_or(&def);
+            if outd == (ind + 1) {
+                start_node = node;
+                break;
+            }
+        }
+
+        if start_node == -1 {
+            start_node = pairs[0][0];
+        }
+
+        let mut node_stack = Vec::<i32>::new();
+        node_stack.push(start_node);
+
+        while let Some(&node) = node_stack.last() {
+            let may_adjacency = adjacency_matrix.get_mut(&node);
+            if may_adjacency.is_none() {
+                nodes.push(node);
+                node_stack.pop();
+                continue;
             }
 
-            result.push(new_col);
+            let adjacency = may_adjacency.unwrap();
+            if adjacency.is_empty() {
+                nodes.push(node);
+                node_stack.pop();
+            } else {
+                let next_node = adjacency.pop_front().unwrap();
+                node_stack.push(next_node);
+            }
+        }
+
+        nodes.reverse();
+
+        let mut result = Vec::<Vec<i32>>::with_capacity(nodes.len() - 1);
+        for i in 1..nodes.len() {
+            let start = nodes[i - 1];
+            let end = nodes[i];
+            result.push(vec![start, end]);
         }
 
         return result;
@@ -21,26 +70,24 @@ impl Solution {
 }
 
 struct Input {
-    image: Vec<Vec<i32>>,
+    pairs: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            image: vec![vec![1, 1, 0], vec![1, 0, 1], vec![0, 0, 0]],
+            pairs: vec![vec![5, 1], vec![4, 5], vec![11, 9], vec![9, 4]],
         },
         Input {
-            image: vec![
-                vec![1, 1, 0, 0],
-                vec![1, 0, 0, 1],
-                vec![0, 1, 1, 1],
-                vec![1, 0, 1, 0],
-            ],
+            pairs: vec![vec![1, 3], vec![3, 2], vec![2, 1]],
+        },
+        Input {
+            pairs: vec![vec![1, 2], vec![1, 3], vec![2, 1]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::flip_and_invert_image(input.image);
+        let result = Solution::valid_arrangement(input.pairs);
         println!("{result:?}");
     }
 }
