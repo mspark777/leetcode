84a07d60f9218ac42b0daf4bb1ba8c8bc1952e08
mspark777 <topangel777@gmail.com>
2024-12-03 11:05:06 +0900

    2109. Adding Spaces to a String

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,55 @@
 struct Solution {}
 
 impl Solution {
-    pub fn is_prefix_of_word(sentence: String, search_word: String) -> i32 {
-        for (i, word) in sentence.split(" ").enumerate() {
-            let mut chars = word.chars();
-            let mut found = true;
-            for search in search_word.chars() {
-                if let Some(ch) = chars.next() {
-                    if ch != search {
-                        found = false;
-                        break;
-                    }
+    pub fn add_spaces(s: String, spaces: Vec<i32>) -> String {
+        let chars = s.chars().collect::<Vec<char>>();
+        let mut result = Vec::<char>::with_capacity(chars.len() + spaces.len());
+
+        let mut space_index = 0usize;
+        let mut string_index = 0usize;
+
+        while string_index < chars.len() {
+            while space_index < spaces.len() {
+                let space = spaces[space_index] as usize;
+                if string_index == space {
+                    result.push(' ');
+                    space_index += 1;
                 } else {
-                    found = false;
                     break;
                 }
             }
 
-            if found {
-                return (i + 1) as i32;
-            }
+            result.push(chars[string_index]);
+            string_index += 1;
         }
 
-        return -1;
+        return result.iter().collect();
     }
 }
 
 struct Input {
-    sentence: &'static str,
-    search_word: &'static str,
+    s: &'static str,
+    spaces: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            sentence: "i love eating burger",
-            search_word: "burg",
+            s: "LeetcodeHelpsMeLearn",
+            spaces: vec![8, 13, 15],
         },
         Input {
-            sentence: "this problem is an easy problem",
-            search_word: "pro",
+            s: "icodeinpython",
+            spaces: vec![1, 5, 7, 9],
         },
         Input {
-            sentence: "i am tired",
-            search_word: "you",
+            s: "spacing",
+            spaces: vec![0, 1, 2, 3, 4, 5, 6],
         },
     ];
 
     for input in inputs {
-        let result =
-            Solution::is_prefix_of_word(input.sentence.to_string(), input.search_word.to_string());
+        let result = Solution::add_spaces(input.s.to_string(), input.spaces);
         println!("{result}");
     }
 }
