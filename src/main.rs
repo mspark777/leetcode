baf829c0c38c2cdcb69221ce8a0d281eac9cd9b3
mspark777 <topangel777@gmail.com>
2024-12-05 09:54:48 +0900

    2337. Move Pieces to Obtain a String

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,66 @@
 struct Solution {}
 
 impl Solution {
-    pub fn can_make_subsequence(str1: String, str2: String) -> bool {
-        let chars1 = str1.chars().collect::<Vec<char>>();
-        let chars2 = str2.chars().collect::<Vec<char>>();
-        let mut str2_idx = 0usize;
-        let str1_len = chars1.len();
-        let str2_len = chars2.len();
+    pub fn can_change(start: String, target: String) -> bool {
+        return Self::solve(start.chars().collect(), target.chars().collect());
+    }
 
-        for str1_idx in 0..str1_len {
-            if str2_idx >= str2_len {
-                break;
+    fn solve(start: Vec<char>, target: Vec<char>) -> bool {
+        let char_count = start.len();
+        let mut start_index = 0usize;
+        let mut target_index = 0usize;
+
+        while start_index < char_count || target_index < char_count {
+            while start_index < char_count && start[start_index] == '_' {
+                start_index += 1;
             }
 
-            let ch1 = chars1[str1_idx] as u8;
-            let ch2 = chars2[str2_idx] as u8;
-            if ch1 == ch2 {
-                str2_idx += 1;
-            } else if (ch1 + 1) == ch2 {
-                str2_idx += 1;
-            } else if (ch1 - 25) == ch2 {
-                str2_idx += 1;
+            while target_index < char_count && target[target_index] == '_' {
+                target_index += 1;
             }
+
+            if start_index == char_count || target_index == char_count {
+                return start_index == char_count && target_index == char_count;
+            }
+
+            if start[start_index] != target[target_index]
+                || (start[start_index] == 'L' && start_index < target_index)
+                || (start[start_index] == 'R' && start_index > target_index)
+            {
+                return false;
+            }
+
+            start_index += 1;
+            target_index += 1;
         }
 
-        return str2_idx == str2_len;
+        return true;
     }
 }
 
 struct Input {
-    str1: &'static str,
-    str2: &'static str,
+    start: &'static str,
+    target: &'static str,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            str1: "abc",
-            str2: "ad",
+            start: "_L__R__R_",
+            target: "L______RR",
         },
         Input {
-            str1: "zc",
-            str2: "ad",
+            start: "R_L_",
+            target: "__LR",
         },
         Input {
-            str1: "ab",
-            str2: "d",
+            start: "_R",
+            target: "R_",
         },
     ];
 
     for input in inputs {
-        let result = Solution::can_make_subsequence(input.str1.to_string(), input.str2.to_string());
+        let result = Solution::can_change(input.start.to_string(), input.target.to_string());
         println!("{result}");
     }
 }
