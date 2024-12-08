63f529a412ba2825d59eaf7a16997d1838e7827f
mspark777 <topangel777@gmail.com>
2024-12-08 22:28:46 +0900

    2054. Two Best Non-Overlapping Events

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,62 @@
+use std::cmp::Reverse;
+
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_size(nums: Vec<i32>, max_operations: i32) -> i32 {
-        let mut left = 1;
-        let mut right = nums[0];
-
-        for &num in nums.iter() {
-            right = right.max(num);
-        }
-
-        while left < right {
-            let middle = (left + right) / 2;
-            if Self::is_possible(middle, &nums, max_operations) {
-                right = middle;
-            } else {
-                left = middle + 1;
-            }
-        }
-
-        return left;
+    pub fn max_two_events(events: Vec<Vec<i32>>) -> i32 {
+        return Self::solve(events.clone());
     }
 
-    fn is_possible(max_balls_in_bag: i32, nums: &Vec<i32>, max_operaions: i32) -> bool {
-        let mut total_operations = 0;
+    fn solve(mut events: Vec<Vec<i32>>) -> i32 {
+        events.sort_unstable_by_key(|e| (e[0], e[1]));
 
-        for &num in nums.iter() {
-            let n = num as f64;
-            let mbb = max_balls_in_bag as f64;
-            let operations = ((n / mbb).ceil() as i32) - 1;
-            total_operations += operations;
+        let mut queue =
+            std::collections::binary_heap::BinaryHeap::<Reverse<(i32, i32)>>::with_capacity(
+                events.len(),
+            );
 
-            if total_operations > max_operaions {
-                return false;
+        let mut result = 0;
+        let mut max_val = 0;
+        for event in events.iter() {
+            let start = event[0];
+            let end = event[1];
+            let val = event[2];
+            while let Some(&Reverse((s, e))) = queue.peek() {
+                if s < start {
+                    max_val = max_val.max(e);
+                    queue.pop();
+                } else {
+                    break;
+                }
             }
+
+            result = result.max(max_val + val);
+            queue.push(Reverse((end, val)));
         }
 
-        return true;
+        return result;
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    max_operations: i32,
+    events: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![9],
-            max_operations: 2,
+            events: vec![vec![1, 3, 2], vec![4, 5, 2], vec![2, 4, 3]],
         },
         Input {
-            nums: vec![2, 4, 8, 2],
-            max_operations: 4,
+            events: vec![vec![1, 3, 2], vec![4, 5, 2], vec![1, 5, 5]],
+        },
+        Input {
+            events: vec![vec![1, 5, 3], vec![1, 5, 1], vec![6, 6, 5]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_size(input.nums, input.max_operations);
+        let result = Solution::max_two_events(input.events);
         println!("{result}");
     }
 }
