d4ee28465d8b1ee319d31843897bdf03810e882e
mspark777 <topangel777@gmail.com>
2024-12-10 23:35:41 +0900

    2981. Find Longest Special Substring That Occurs Thrice I

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,47 @@
 struct Solution {}
 
 impl Solution {
-    pub fn is_array_special(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<bool> {
-        let mut prefix_list = vec![0; nums.len()];
-
-        for i in 1..nums.len() {
-            let curr = nums[i] & 1;
-            let prev = nums[i - 1] & 1;
-            prefix_list[i] = prefix_list[i - 1];
-            if curr == prev {
-                prefix_list[i] += 1;
+    pub fn maximum_length(s: String) -> i32 {
+        let mut counts = std::collections::HashMap::<(char, usize), usize>::new();
+        let chars = s.chars().collect::<Vec<char>>();
+        for start in 0..chars.len() {
+            let char = chars[start];
+            let mut substring_length = 0usize;
+            for end in start..chars.len() {
+                if char == chars[end] {
+                    substring_length += 1;
+                    *counts.entry((char, substring_length)).or_insert(0) += 1;
+                } else {
+                    break;
+                }
             }
         }
 
-        let mut result = vec![false; queries.len()];
-        for (i, query) in queries.iter().enumerate() {
-            let start = query[0] as usize;
-            let end = query[1] as usize;
-
-            result[i] = prefix_list[start] == prefix_list[end];
+        let mut result = 0usize;
+        for (&key, &value) in counts.iter() {
+            let len = key.1;
+            if value >= 3 && len > result {
+                result = len;
+            }
         }
 
-        return result;
+        return if result == 0 { -1 } else { result as i32 };
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    queries: Vec<Vec<i32>>,
+    s: &'static str,
 }
 
 fn main() {
     let inputs = vec![
-        Input {
-            nums: vec![3, 4, 1, 2, 6],
-            queries: vec![vec![0, 4]],
-        },
-        Input {
-            nums: vec![4, 3, 1, 6],
-            queries: vec![vec![0, 2], vec![2, 3]],
-        },
+        Input { s: "aaaa" },
+        Input { s: "abcdef" },
+        Input { s: "abcaba" },
     ];
 
     for input in inputs {
-        let result = Solution::is_array_special(input.nums, input.queries);
-        println!("{result:?}");
+        let result = Solution::maximum_length(input.s.to_string());
+        println!("{result}");
     }
 }
