555eedb91d6d87ef42b6b6b7d047e7661fa82d67
mspark777 <topangel777@gmail.com>
2024-12-15 22:25:24 +0900

    1792. Maximum Average Pass Ratio

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,109 @@
+struct QueueNode {
+    current_gain: f64,
+    passes: i32,
+    total_students: i32,
+}
+
+impl Ord for QueueNode {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.current_gain < other.current_gain {
+            return std::cmp::Ordering::Less;
+        } else if self.current_gain > other.current_gain {
+            return std::cmp::Ordering::Greater;
+        }
+
+        return (self.passes, self.total_students).cmp(&(other.passes, other.total_students));
+    }
+}
+impl PartialOrd for QueueNode {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for QueueNode {
+    fn eq(&self, other: &Self) -> bool {
+        return match self.cmp(other) {
+            std::cmp::Ordering::Equal => true,
+            _ => false,
+        };
+    }
+}
+
+impl Eq for QueueNode {}
+
 struct Solution {}
 
 impl Solution {
-    pub fn continuous_subarrays(nums: Vec<i32>) -> i64 {
-        let mut left = 0i64;
-        let mut right = 0i64;
-        let mut result = 0i64;
-        let mut window_len = 0i64;
-        let mut cur_min = nums[0];
-        let mut cur_max = nums[0];
-
-        while right < (nums.len() as i64) {
-            // to avoid unread wran
-            let mut win_len = window_len;
-            cur_min = cur_min.min(nums[right as usize]);
-            cur_max = cur_max.max(nums[right as usize]);
-            if (cur_max - cur_min) > 2 {
-                win_len = right - left;
-                result += win_len * (win_len + 1) / 2;
-                left = right;
-                cur_min = nums[right as usize];
-                cur_max = nums[right as usize];
-
-                while left > 0 && (nums[right as usize] - nums[(left - 1) as usize]).abs() <= 2 {
-                    left -= 1;
-                    cur_min = cur_min.min(nums[left as usize]);
-                    cur_max = cur_max.max(nums[left as usize]);
-                }
-
-                if left < right {
-                    win_len = right - left;
-                    result -= win_len * (win_len + 1) / 2;
-                }
-            }
-
-            window_len = win_len;
-            right += 1
+    pub fn max_average_ratio(classes: Vec<Vec<i32>>, extra_students: i32) -> f64 {
+        let mut queue = std::collections::BinaryHeap::<QueueNode>::with_capacity(classes.len());
+        for cls in classes.iter() {
+            let passes = cls[0];
+            let total_students = cls[1];
+            let current_gain = Self::calculate_gain(passes, total_students);
+            queue.push(QueueNode {
+                current_gain,
+                passes,
+                total_students,
+            });
         }
 
-        window_len = right - left;
-        result += window_len * (window_len + 1) / 2;
-        return result;
+        let mut extra = extra_students;
+        while extra > 0 {
+            if queue.is_empty() {
+                break;
+            }
+
+            let top = queue.pop().unwrap();
+            let passes = top.passes + 1;
+            let total_students = top.total_students + 1;
+            let current_gain = Self::calculate_gain(passes, total_students);
+            queue.push(QueueNode {
+                current_gain,
+                passes,
+                total_students,
+            });
+            extra -= 1;
+        }
+
+        let mut total_pass_ratio = 0f64;
+
+        while let Some(top) = queue.pop() {
+            total_pass_ratio += Self::as_f64(top.passes) / Self::as_f64(top.total_students);
+        }
+
+        return total_pass_ratio / (classes.len() as f64);
+    }
+
+    fn calculate_gain(passes: i32, total_students: i32) -> f64 {
+        return Self::as_f64(passes + 1) / Self::as_f64(total_students + 1)
+            - Self::as_f64(passes) / Self::as_f64(total_students);
+    }
+
+    fn as_f64(i: i32) -> f64 {
+        return i as f64;
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    classes: Vec<Vec<i32>>,
+    extra_students: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![5, 4, 2, 4],
+            classes: vec![vec![1, 2], vec![3, 5], vec![2, 2]],
+            extra_students: 2,
         },
         Input {
-            nums: vec![1, 2, 3],
+            classes: vec![vec![2, 4], vec![3, 9], vec![4, 5], vec![2, 10]],
+            extra_students: 4,
         },
     ];
 
     for input in inputs {
-        let result = Solution::continuous_subarrays(input.nums);
+        let result = Solution::max_average_ratio(input.classes, input.extra_students);
         println!("{result}");
     }
 }
