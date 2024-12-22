69c449999b1a91f5cf2975f950d5715d298e33d0
mspark777 <topangel777@gmail.com>
2024-12-22 23:30:38 +0900

    2940. Find Building Where Alice and Bob Can Meet

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn max_k_divisible_components(
-        n: i32,
-        edges: Vec<Vec<i32>>,
-        values: Vec<i32>,
-        k: i32,
-    ) -> i32 {
-        if n < 2 {
-            return 1;
-        }
+    pub fn leftmost_building_queries(heights: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut store_queries = vec![Vec::<(i32, i32)>::new(); heights.len()];
+        let mut result = vec![-1; queries.len()];
 
-        let mut result = 0;
-        let mut graph = vec![vec![]; n as usize];
-        let mut in_degree = vec![0; n as usize];
-
-        for edge in edges.iter() {
-            let node1 = edge[0] as usize;
-            let node2 = edge[1] as usize;
-            graph[node1].push(node2);
-            graph[node2].push(node1);
-            in_degree[node1] += 1;
-            in_degree[node2] += 1
-        }
-
-        let mut long_values = values.iter().map(|&n| n as usize).collect::<Vec<_>>();
-        let mut queue = std::collections::VecDeque::<usize>::with_capacity(in_degree.len());
-        for (node, &degree) in in_degree.iter().enumerate() {
-            if degree == 1 {
-                queue.push_back(node);
-            }
-        }
-
-        while let Some(current_node) = queue.pop_front() {
-            in_degree[current_node] -= 1;
-            let mut add_value = 0;
-            if long_values[current_node] % (k as usize) == 0 {
-                result += 1;
+        for (i, query) in queries.iter().enumerate() {
+            let a = query[0] as usize;
+            let b = query[1] as usize;
+            if (a < b) && (heights[a] < heights[b]) {
+                result[i] = query[1];
+            } else if (a > b) && (heights[a] > heights[b]) {
+                result[i] = query[0];
+            } else if a == b {
+                result[i] = query[0];
             } else {
-                add_value = long_values[current_node];
+                store_queries[a.max(b)].push((heights[a].max(heights[b]), i as i32));
+            }
+        }
+
+        let mut queue =
+            std::collections::BinaryHeap::<std::cmp::Reverse<(i32, i32)>>::with_capacity(
+                queries.len(),
+            );
+        for (i, &height) in heights.iter().enumerate() {
+            while let Some(std::cmp::Reverse(top)) = queue.peek() {
+                if top.0 < height {
+                    result[top.1 as usize] = i as i32;
+                    queue.pop();
+                } else {
+                    break;
+                }
             }
 
-            for &neighbor_node in graph[current_node].iter() {
-                if in_degree[neighbor_node] == 0 {
-                    continue;
-                }
-
-                in_degree[neighbor_node] -= 1;
-                long_values[neighbor_node] += add_value;
-                if in_degree[neighbor_node] == 1 {
-                    queue.push_back(neighbor_node);
-                }
+            for element in store_queries[i].iter() {
+                queue.push(std::cmp::Reverse(*element));
             }
         }
 
@@ -59,38 +43,24 @@ impl Solution {
 }
 
 struct Input {
-    n: i32,
-    edges: Vec<Vec<i32>>,
-    values: Vec<i32>,
-    k: i32,
+    heights: Vec<i32>,
+    queries: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            n: 5,
-            edges: vec![vec![0, 2], vec![1, 2], vec![1, 3], vec![2, 4]],
-            values: vec![1, 8, 1, 4, 4],
-            k: 6,
+            heights: vec![6, 4, 8, 5, 2, 7],
+            queries: vec![vec![0, 1], vec![0, 3], vec![2, 4], vec![3, 4], vec![2, 2]],
         },
         Input {
-            n: 7,
-            edges: vec![
-                vec![0, 1],
-                vec![0, 2],
-                vec![1, 3],
-                vec![1, 4],
-                vec![2, 5],
-                vec![2, 6],
-            ],
-            values: vec![3, 0, 6, 1, 5, 2, 1],
-            k: 3,
+            heights: vec![5, 3, 8, 2, 6, 1, 4, 6],
+            queries: vec![vec![0, 7], vec![3, 5], vec![5, 2], vec![3, 0], vec![1, 6]],
         },
     ];
 
     for input in inputs {
-        let result =
-            Solution::max_k_divisible_components(input.n, input.edges, input.values, input.k);
-        println!("{result}");
+        let result = Solution::leftmost_building_queries(input.heights, input.queries);
+        println!("{result:?}");
     }
 }
