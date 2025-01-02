3a8de7bdc6c11d7b45c45bd8b904c9ba781e125c
mspark777 <topangel777@gmail.com>
2025-01-02 15:46:27 +0900

    2559. Count Vowel Strings in Ranges

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,60 @@
 struct Solution {}
 
 impl Solution {
-    pub fn mincost_tickets(days: Vec<i32>, costs: Vec<i32>) -> i32 {
-        let last_day = days.last().unwrap().clone() as usize;
-        let mut dp = vec![0; last_day + 1];
+    pub fn vowel_strings(words: Vec<String>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut result = vec![0; queries.len()];
+        let vowels = std::collections::HashSet::<char>::from(['a', 'e', 'i', 'o', 'u']);
+        let mut prefix_sum = vec![0; words.len()];
+        let mut sum = 0;
 
-        let mut i = 0usize;
-        for day in 1..=last_day {
-            if (day as i32) < days[i] {
-                dp[day] = dp[day - 1];
+        for (i, word) in words.iter().enumerate() {
+            let chars = word.chars().collect::<Vec<char>>();
+            let first = chars.first().unwrap();
+            let last = chars.last().unwrap();
+
+            if vowels.contains(&first) && vowels.contains(&last) {
+                sum += 1;
+            }
+
+            prefix_sum[i] = sum;
+        }
+
+        for (i, query) in queries.iter().enumerate() {
+            let l = query[0] as usize;
+            let r = query[1] as usize;
+            if l == 0 {
+                result[i] = prefix_sum[r];
             } else {
-                i += 1;
-                dp[day] = (dp[day - 1] + costs[0])
-                    .min(dp[Self::safe(day, 7)] + costs[1])
-                    .min(dp[Self::safe(day, 30)] + costs[2]);
+                result[i] = prefix_sum[r] - prefix_sum[l - 1];
             }
         }
 
-        return dp[last_day];
-    }
-
-    fn safe(left: usize, right: usize) -> usize {
-        return if left <= right { 0 } else { left - right };
+        return result;
     }
 }
 
 struct Input {
-    days: Vec<i32>,
-    costs: Vec<i32>,
+    words: Vec<&'static str>,
+    queries: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            days: vec![1, 4, 6, 7, 8, 20],
-            costs: vec![2, 7, 15],
+            words: vec!["aba", "bcb", "ece", "aa", "e"],
+            queries: vec![vec![0, 2], vec![1, 4], vec![1, 1]],
         },
         Input {
-            days: vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31],
-            costs: vec![2, 7, 15],
+            words: vec!["a", "e", "i"],
+            queries: vec![vec![0, 2], vec![0, 1], vec![2, 2]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::mincost_tickets(input.days, input.costs);
+        let result = Solution::vowel_strings(
+            input.words.iter().map(|s| s.to_string()).collect(),
+            input.queries,
+        );
         println!("{result:?}");
     }
 }
