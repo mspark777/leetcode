6a8139584d69f8d8877d1e0279a8afae6a252b3e
mspark777 <topangel777@gmail.com>
2025-01-03 09:40:44 +0900

    2270. Number of Ways to Split Array

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,21 @@
 struct Solution {}
 
 impl Solution {
-    pub fn vowel_strings(words: Vec<String>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut result = vec![0; queries.len()];
-        let vowels = std::collections::HashSet::<char>::from(['a', 'e', 'i', 'o', 'u']);
-        let mut prefix_sum = vec![0; words.len()];
-        let mut sum = 0;
+    pub fn ways_to_split_array(nums: Vec<i32>) -> i32 {
+        let mut left_sum = 0i64;
+        let mut right_sum = 0i64;
 
-        for (i, word) in words.iter().enumerate() {
-            let chars = word.chars().collect::<Vec<char>>();
-            let first = chars.first().unwrap();
-            let last = chars.last().unwrap();
-
-            if vowels.contains(&first) && vowels.contains(&last) {
-                sum += 1;
-            }
-
-            prefix_sum[i] = sum;
+        for num in nums.iter().cloned() {
+            right_sum += num as i64;
         }
 
-        for (i, query) in queries.iter().enumerate() {
-            let l = query[0] as usize;
-            let r = query[1] as usize;
-            if l == 0 {
-                result[i] = prefix_sum[r];
-            } else {
-                result[i] = prefix_sum[r] - prefix_sum[l - 1];
+        let mut result = 0;
+        for num in nums.iter().take(nums.len() - 1).cloned() {
+            left_sum += num as i64;
+            right_sum -= num as i64;
+
+            if left_sum >= right_sum {
+                result += 1;
             }
         }
 
@@ -34,27 +24,21 @@ impl Solution {
 }
 
 struct Input {
-    words: Vec<&'static str>,
-    queries: Vec<Vec<i32>>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            words: vec!["aba", "bcb", "ece", "aa", "e"],
-            queries: vec![vec![0, 2], vec![1, 4], vec![1, 1]],
+            nums: vec![10, 4, -8, 7],
         },
         Input {
-            words: vec!["a", "e", "i"],
-            queries: vec![vec![0, 2], vec![0, 1], vec![2, 2]],
+            nums: vec![2, 3, 1, 0],
         },
     ];
 
     for input in inputs {
-        let result = Solution::vowel_strings(
-            input.words.iter().map(|s| s.to_string()).collect(),
-            input.queries,
-        );
+        let result = Solution::ways_to_split_array(input.nums);
         println!("{result:?}");
     }
 }
