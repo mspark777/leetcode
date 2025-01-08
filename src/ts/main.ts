5cebbb6fba6aa8292b34aa0c975a34f94493a2fc
mspark777 <topangel777@gmail.com>
2025-01-09 00:05:35 +0900

    3042. Count Prefix and Suffix Pairs I

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,52 +1,35 @@
 import "@total-typescript/ts-reset";
 
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
+function isPrefixAndSubfix(str1: string, str2: string): boolean {
+  return (
+    str2.slice(0, str1.length) === str1 &&
+    str2.slice(str2.length - str1.length) === str1
+  );
 }
 
-function dfs(
-  left: TreeNode | null,
-  right: TreeNode | null,
-  level: number,
-): void {
-  if (left == null || right == null) {
-    return;
+function countPrefixSuffixPairs(words: string[]): number {
+  let result = 0;
+
+  for (let i = 0; i < words.length; i += 1) {
+    for (let j = i + 1; j < words.length; j += 1) {
+      if (isPrefixAndSubfix(words[i] as string, words[j] as string)) {
+        result += 1;
+      }
+    }
   }
 
-  if (level % 2 == 0) {
-    const temp = left.val;
-    left.val = right.val;
-    right.val = temp;
-  }
-
-  dfs(left.left, right.right, level + 1);
-  dfs(left.right, right.left, level + 1);
-}
-
-function reverseOddLevels(root: TreeNode | null): TreeNode | null {
-  if (root == null) {
-    return null;
-  }
-
-  dfs(root.left, root.right, 0);
-  return root;
+  return result;
 }
 
 function main(): void {
-  const inputs: Array<number[]> = [
-    [4, 3, 6, 16, 8, 2],
-    [2, 3, 5, 6, 7],
+  const inputs: Array<string[]> = [
+    ["a", "aba", "ababa", "aa"],
+    ["pa", "papa", "ma", "mama"],
+    ["abab", "ab"],
   ];
 
-  for (const nums of inputs) {
-    const result = longestSquareStreak(nums);
+  for (const words of inputs) {
+    const result = countPrefixSuffixPairs(words);
     console.log(result);
   }
 }
