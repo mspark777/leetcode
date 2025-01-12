a3c84c8ccb5c0d197a9bbaee4d7a70804d550944
mspark777 <topangel777@gmail.com>
2025-01-12 14:30:49 +0900

    2116. Check if a Parentheses String Can Be Valid

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,82 @@
 struct Solution {}
 
 impl Solution {
-    pub fn can_construct(s: String, k: i32) -> bool {
-        let mut odd_count = 0u32;
-        let mut s_len = 0;
+    pub fn can_be_valid(s: String, locked: String) -> bool {
+        let s = s.chars().collect::<Vec<char>>();
+        let locked = locked.chars().collect::<Vec<char>>();
 
-        for ch in s.chars() {
-            let code = ch as u32;
-            let a = 'a' as u32;
-            odd_count ^= 1 << (code - a);
-            s_len += 1;
-        }
-
-        if s_len < k {
+        if s.len() & 1 == 1 {
             return false;
-        } else if s_len == k {
-            return true;
         }
 
-        return odd_count.count_ones() <= (k as u32);
+        let mut open_brackets = 0;
+        let mut unlocked = 0;
+
+        for (&bracket, &lock) in s.iter().zip(locked.iter()) {
+            if lock == '0' {
+                unlocked += 1;
+            } else if bracket == '(' {
+                open_brackets += 1;
+            } else if bracket == ')' {
+                if open_brackets > 0 {
+                    open_brackets -= 1;
+                } else if unlocked > 0 {
+                    unlocked -= 1;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        let mut balance = 0;
+
+        for (&bracket, &lock) in s.iter().rev().zip(locked.iter().rev()) {
+            if lock == '0' {
+                balance -= 1;
+                unlocked -= 1;
+            } else if bracket == '(' {
+                balance += 1;
+                open_brackets -= 1;
+            } else if bracket == ')' {
+                balance -= 1;
+            }
+
+            if balance > 0 {
+                return false;
+            }
+
+            if unlocked == 0 && open_brackets == 0 {
+                break;
+            }
+        }
+
+        return if open_brackets <= 0 { true } else { false };
     }
 }
 
 struct Input {
     s: &'static str,
-    k: i32,
+    locked: &'static str,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            s: "annabelle",
-            k: 2,
+            s: "))()))",
+            locked: "010100",
         },
         Input {
-            s: "leetcode",
-            k: 3,
+            s: "()()",
+            locked: "0000",
         },
-        Input { s: "true", k: 4 },
+        Input {
+            s: ")",
+            locked: "0",
+        },
     ];
 
     for input in inputs {
-        let result = Solution::can_construct(input.s.to_string(), input.k);
+        let result = Solution::can_be_valid(input.s.to_string(), input.locked.to_string());
         println!("{result:?}");
     }
 }
