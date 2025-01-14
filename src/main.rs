9c6c22902909b432cbc52f8ad9cc654d55c6de6b
mspark777 <topangel777@gmail.com>
2025-01-15 00:13:42 +0900

    888. Fair Candy Swap

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,46 @@
 struct Solution {}
 
 impl Solution {
-    pub fn projection_area(grid: Vec<Vec<i32>>) -> i32 {
-        let n = grid.len();
-        let mut result = 0;
+    pub fn fair_candy_swap(alice_sizes: Vec<i32>, bob_sizes: Vec<i32>) -> Vec<i32> {
+        let sum_a = alice_sizes.iter().sum::<i32>();
+        let sum_b = bob_sizes.iter().sum::<i32>();
+        let diff = (sum_a - sum_b) / 2;
+        let alice_set = std::collections::HashSet::<i32>::from_iter(alice_sizes.iter().cloned());
 
-        for r in 0..n {
-            let mut best_row = 0;
-            let mut best_col = 0;
-            for c in 0..n {
-                if grid[r][c] > 0 {
-                    result += 1;
-                }
-
-                best_row = best_row.max(grid[r][c]);
-                best_col = best_col.max(grid[c][r]);
+        for bob in bob_sizes.iter().cloned() {
+            let target = bob + diff;
+            if alice_set.contains(&target) {
+                return vec![bob + diff, bob];
             }
-
-            result += best_row + best_col;
         }
 
-        return result;
+        return bob_sizes;
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    alice_sizes: Vec<i32>,
+    bob_sizes: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            grid: vec![vec![1, 2], vec![3, 4]],
+            alice_sizes: vec![1, 1],
+            bob_sizes: vec![2, 2],
         },
         Input {
-            grid: vec![vec![2]],
+            alice_sizes: vec![1, 2],
+            bob_sizes: vec![2, 3],
         },
         Input {
-            grid: vec![vec![1, 0], vec![0, 2]],
+            alice_sizes: vec![2],
+            bob_sizes: vec![1, 3],
         },
     ];
 
     for input in inputs {
-        let result = Solution::projection_area(input.grid);
+        let result = Solution::fair_candy_swap(input.alice_sizes, input.bob_sizes);
         println!("{result:?}");
     }
 }
