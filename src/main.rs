3432922fa5d1f015806121e58036dc08a9c5dd67
mspark777 <topangel777@gmail.com>
2025-01-14 12:09:29 +0900

    2657. Find the Prefix Common Array of Two Arrays

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_length(s: String) -> i32 {
-        let mut frequencies = vec![0; 26];
-        let mut result = 0;
+    pub fn find_the_prefix_common_array(a_list: Vec<i32>, b_list: Vec<i32>) -> Vec<i32> {
+        let n = a_list.len();
+        let mut result = vec![0; n];
+        let mut frequencies = vec![0; n];
+        let mut common_count = 0;
 
-        for ch in s.chars() {
-            let code = ch as usize;
-            let a = 'a' as usize;
-            let idx = code - a;
-            frequencies[idx] += 1;
-        }
+        for (i, (&a, &b)) in a_list.iter().zip(b_list.iter()).enumerate() {
+            let a_idx = (a as usize) - 1;
+            let b_idx = (b as usize) - 1;
 
-        for frequency in frequencies.iter().cloned() {
-            if frequency == 0 {
-                continue;
-            } else if frequency & 1 == 1 {
-                result += 1;
-            } else {
-                result += 2;
+            frequencies[a_idx] += 1;
+            if frequencies[a_idx] == 2 {
+                common_count += 1;
             }
+
+            frequencies[b_idx] += 1;
+            if frequencies[b_idx] == 2 {
+                common_count += 1;
+            }
+
+            result[i] = common_count;
         }
 
         return result;
@@ -27,14 +29,24 @@ impl Solution {
 }
 
 struct Input {
-    s: &'static str,
+    a: Vec<i32>,
+    b: Vec<i32>,
 }
 
 fn main() {
-    let inputs = vec![Input { s: "abaacbcbb" }, Input { s: "aa" }];
+    let inputs = vec![
+        Input {
+            a: vec![1, 3, 2, 4],
+            b: vec![3, 1, 2, 4],
+        },
+        Input {
+            a: vec![2, 3, 1],
+            b: vec![3, 1, 2],
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::minimum_length(input.s.to_string());
+        let result = Solution::find_the_prefix_common_array(input.a, input.b);
         println!("{result:?}");
     }
 }
