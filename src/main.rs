1a824888d99572742904002b491e0af2a1565a01
mspark777 <topangel777@gmail.com>
2025-01-16 22:05:43 +0900

    2425. Bitwise XOR of All Pairings

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,45 @@
 struct Solution {}
 
 impl Solution {
-    pub fn surface_area(grid: Vec<Vec<i32>>) -> i32 {
-        let dr_list = [0, 1, 0, -1];
-        let dc_list = [1, 0, -1, 0];
-        let n = grid.len();
-        let mut result = 0;
+    pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        let mut xor1 = 0u32;
+        let mut xor2 = 0u32;
 
-        for r in 0..n {
-            for c in 0..n {
-                let cell = grid[r][c];
-                if cell <= 0 {
-                    continue;
-                }
-
-                result += 2;
-                for (&dr, &dc) in dr_list.iter().zip(dc_list.iter()) {
-                    let maybe_nr = Self::next(r, dr, n);
-                    let maybe_nc = Self::next(c, dc, n);
-                    let mut nv = 0;
-                    if let (Some(nr), Some(nc)) = (maybe_nr, maybe_nc) {
-                        nv = grid[nr][nc];
-                    }
-
-                    result += 0.max(grid[r][c] - nv);
-                }
+        if nums2.len() & 1 == 1 {
+            for num in nums1.iter().cloned() {
+                xor1 ^= num as u32;
             }
         }
 
-        return result;
-    }
-
-    fn next(u: usize, d: i32, n: usize) -> Option<usize> {
-        if d == 0 {
-            return Some(u);
-        } else if d > 0 {
-            let nu = u + d as usize;
-            if nu < n {
-                return Some(nu);
+        if nums1.len() & 1 == 1 {
+            for num in nums2.iter().cloned() {
+                xor2 ^= num as u32;
             }
-
-            return None;
         }
 
-        let d = (d * -1) as usize;
-        if u < d {
-            return None;
-        }
-
-        return Some(u - d);
+        return (xor1 ^ xor2) as i32;
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    nums1: Vec<i32>,
+    nums2: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            grid: vec![vec![1, 2], vec![3, 4]],
+            nums1: vec![2, 1, 3],
+            nums2: vec![10, 2, 5, 0],
         },
         Input {
-            grid: vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]],
-        },
-        Input {
-            grid: vec![vec![2, 2, 2], vec![2, 1, 2], vec![2, 2, 2]],
+            nums1: vec![1, 2],
+            nums2: vec![3, 4],
         },
     ];
 
     for input in inputs {
-        let result = Solution::surface_area(input.grid);
+        let result = Solution::xor_all_nums(input.nums1, input.nums2);
         println!("{result:?}");
     }
 }
