debd07cb935c82d7412835304694f25836c2f78a
mspark777 <topangel777@gmail.com>
2025-01-18 00:49:34 +0900

    2683. Neighboring Bitwise XOR

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,35 @@
 struct Solution {}
 
 impl Solution {
-    pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-        let mut xor1 = 0u32;
-        let mut xor2 = 0u32;
-
-        if nums2.len() & 1 == 1 {
-            for num in nums1.iter().cloned() {
-                xor1 ^= num as u32;
-            }
+    pub fn does_valid_array_exist(derived: Vec<i32>) -> bool {
+        let mut xor = 0u32;
+        for n in derived.iter().cloned() {
+            xor ^= n as u32;
         }
 
-        if nums1.len() & 1 == 1 {
-            for num in nums2.iter().cloned() {
-                xor2 ^= num as u32;
-            }
-        }
-
-        return (xor1 ^ xor2) as i32;
+        return xor == 0;
     }
 }
 
 struct Input {
-    nums1: Vec<i32>,
-    nums2: Vec<i32>,
+    derived: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            nums1: vec![2, 1, 3],
-            nums2: vec![10, 2, 5, 0],
+            derived: vec![1, 1, 0],
         },
         Input {
-            nums1: vec![1, 2],
-            nums2: vec![3, 4],
+            derived: vec![1, 1],
+        },
+        Input {
+            derived: vec![1, 0],
         },
     ];
 
     for input in inputs {
-        let result = Solution::xor_all_nums(input.nums1, input.nums2);
+        let result = Solution::does_valid_array_exist(input.derived);
         println!("{result:?}");
     }
 }
