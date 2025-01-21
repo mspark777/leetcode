f815d501b09fc7c27b35bd8ab111fb547a2818f2
mspark777 <topangel777@gmail.com>
2025-01-21 18:12:03 +0900

    2017. Grid Game

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
 struct Solution {}
 
 impl Solution {
-    pub fn first_complete_index(arr: Vec<i32>, mat: Vec<Vec<i32>>) -> i32 {
-        let mut num_to_index = std::collections::HashMap::<i32, usize>::with_capacity(arr.len());
-        for (i, n) in arr.iter().cloned().enumerate() {
-            num_to_index.insert(n, i);
+    pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
+        let mut first_row_sum = 0i64;
+        let mut second_row_sum = 0i64;
+        let mut result = i64::MAX;
+
+        for point in grid[0].iter().cloned() {
+            first_row_sum += point as i64;
         }
 
-        let mut result = i32::MAX;
-        let num_rows = mat.len();
-        let num_cols = mat[0].len();
-
-        for row in 0..num_rows {
-            let mut last_element_idx = i32::MIN;
-            for col in 0..num_cols {
-                let cell = mat[row][col];
-                let &idx = num_to_index.get(&cell).unwrap();
-                last_element_idx = last_element_idx.max(idx as i32);
-            }
-
-            result = result.min(last_element_idx);
-        }
-
-        for col in 0..num_cols {
-            let mut last_element_idx = i32::MIN;
-            for row in 0..num_rows {
-                let cell = mat[row][col];
-                let &idx = num_to_index.get(&cell).unwrap();
-                last_element_idx = last_element_idx.max(idx as i32);
-            }
-
-            result = result.min(last_element_idx);
+        for (&first, &second) in grid[0].iter().zip(grid[1].iter()) {
+            first_row_sum -= first as i64;
+            result = result.min(first_row_sum.max(second_row_sum));
+            second_row_sum += second as i64;
         }
 
         return result;
@@ -38,24 +21,24 @@ impl Solution {
 }
 
 struct Input {
-    arr: Vec<i32>,
-    mat: Vec<Vec<i32>>,
+    grid: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            arr: vec![1, 3, 4, 2],
-            mat: vec![vec![1, 4], vec![2, 3]],
+            grid: vec![vec![2, 5, 4], vec![1, 5, 1]],
         },
         Input {
-            arr: vec![2, 8, 7, 4, 1, 3, 5, 6, 9],
-            mat: vec![vec![3, 2, 5], vec![1, 4, 6], vec![8, 7, 9]],
+            grid: vec![vec![3, 3, 1], vec![8, 5, 2]],
+        },
+        Input {
+            grid: vec![vec![1, 3, 1, 15], vec![1, 3, 3, 1]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::first_complete_index(input.arr, input.mat);
+        let result = Solution::grid_game(input.grid);
         println!("{result:?}");
     }
 }
