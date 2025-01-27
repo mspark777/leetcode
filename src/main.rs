c54b93350094b1eb84541066e04d1938f8cfcabc
mspark777 <topangel777@gmail.com>
2025-01-27 16:06:32 +0900

    1462. Course Schedule IV

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,63 @@
 struct Solution {}
 
 impl Solution {
-    pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
-        let mut counts = std::collections::HashMap::<i32, i32>::new();
-        for c in deck.iter().cloned() {
-            let count = counts.entry(c).or_insert(0);
-            *count += 1;
+    pub fn check_if_prerequisite(
+        num_courses: i32,
+        prerequisites: Vec<Vec<i32>>,
+        queries: Vec<Vec<i32>>,
+    ) -> Vec<bool> {
+        let num_courses = num_courses as usize;
+        let mut is_prerequisite = vec![vec![false; num_courses]; num_courses];
+        for edge in prerequisites.iter() {
+            is_prerequisite[edge[0] as usize][edge[1] as usize] = true;
         }
 
-        let mut gcd = *counts.get(&deck[0]).unwrap();
-        counts.remove(&deck[0]);
-
-        for count in counts.values().cloned() {
-            gcd = Self::gcd(gcd, count);
+        for intermediate in 0..num_courses {
+            for src in 0..num_courses {
+                for target in 0..num_courses {
+                    is_prerequisite[src][target] = is_prerequisite[src][target]
+                        || (is_prerequisite[src][intermediate]
+                            && is_prerequisite[intermediate][target]);
+                }
+            }
         }
 
-        return gcd >= 2;
-    }
-
-    fn gcd(a: i32, b: i32) -> i32 {
-        return if a == 0 { b } else { Self::gcd(b % a, a) };
+        let mut result = vec![false; queries.len()];
+        for (i, query) in queries.iter().enumerate() {
+            result[i] = is_prerequisite[query[0] as usize][query[1] as usize];
+        }
+        return result;
     }
 }
 
 struct Input {
-    deck: Vec<i32>,
+    num_courses: i32,
+    prerequisites: Vec<Vec<i32>>,
+    queries: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            deck: vec![1, 2, 3, 4, 4, 3, 2, 1],
+            num_courses: 2,
+            prerequisites: vec![vec![1, 0]],
+            queries: vec![vec![0, 1], vec![1, 0]],
         },
         Input {
-            deck: vec![1, 1, 1, 2, 2, 2, 3, 3],
+            num_courses: 2,
+            prerequisites: vec![],
+            queries: vec![vec![1, 0], vec![0, 1]],
+        },
+        Input {
+            num_courses: 3,
+            prerequisites: vec![vec![1, 2], vec![1, 0], vec![2, 0]],
+            queries: vec![vec![1, 0], vec![1, 2]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::has_groups_size_x(input.deck);
+        let result =
+            Solution::check_if_prerequisite(input.num_courses, input.prerequisites, input.queries);
         println!("{result:?}");
     }
 }
