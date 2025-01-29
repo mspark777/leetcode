cd685235e6cb9fa1b4a67a583c5be01cb33e8c48
mspark777 <topangel777@gmail.com>
2025-01-29 16:33:47 +0900

    684. Redundant Connection

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,79 @@
 struct Solution {}
 
+struct DSU {
+    size: Vec<usize>,
+    representative: Vec<usize>,
+}
+
+impl DSU {
+    fn new(n: usize) -> Self {
+        return Self {
+            size: vec![1; n],
+            representative: (0..n).collect(),
+        };
+    }
+
+    fn find(&mut self, node: usize) -> usize {
+        if self.representative[node] == node {
+            return node;
+        }
+
+        self.representative[node] = self.find(self.representative[node]);
+        return self.representative[node];
+    }
+
+    fn do_union(&mut self, node_one: usize, node_two: usize) -> bool {
+        let node_one = self.find(node_one);
+        let node_two = self.find(node_two);
+
+        if node_one == node_two {
+            return false;
+        }
+
+        if self.size[node_one] > self.size[node_two] {
+            self.representative[node_two] = node_one;
+            self.size[node_one] += self.size[node_two];
+        } else {
+            self.representative[node_one] = node_two;
+            self.size[node_two] += self.size[node_one];
+        }
+
+        return true;
+    }
+}
+
 impl Solution {
-    pub fn reverse_only_letters(s: String) -> String {
-        let s = s.chars().collect::<Vec<char>>();
-        let mut chars = Vec::<char>::with_capacity(s.len());
+    pub fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut dsu = DSU::new(edges.len());
 
-        let mut j = (s.len() as i32) - 1;
-        for i in 0..s.len() {
-            let left = s[i];
-            if Self::is_letter(left) {
-                while j >= 0 && !Self::is_letter(s[j as usize]) {
-                    j -= 1;
-                }
-
-                if j >= 0 {
-                    chars.push(s[j as usize]);
-                    j -= 1;
-                }
-            } else {
-                chars.push(left);
+        for edge in edges.iter() {
+            let a = edge[0] - 1;
+            let b = edge[1] - 1;
+            if !dsu.do_union(a as usize, b as usize) {
+                return edge.clone();
             }
         }
 
-        return chars.iter().collect();
-    }
-
-    fn is_letter(ch: char) -> bool {
-        let ch = ch as u8;
-
-        return ((b'a' <= ch) && (ch <= b'z')) || ((b'A' <= ch) && (ch <= b'Z'));
+        return Vec::new();
     }
 }
 
 struct Input {
-    s: &'static str,
+    edges: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
-        Input { s: "ab-cd" },
-        Input { s: "a-bC-dEf-ghIj" },
         Input {
-            s: "Test1ng-Leet=code-Q!",
+            edges: vec![vec![1, 2], vec![1, 3], vec![2, 3]],
+        },
+        Input {
+            edges: vec![vec![1, 2], vec![2, 3], vec![3, 4], vec![1, 4], vec![1, 5]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::reverse_only_letters(input.s.to_string());
+        let result = Solution::find_redundant_connection(input.edges);
         println!("{result:?}");
     }
 }
