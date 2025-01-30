5295e447f3aaf0ab155fc165b49a8f12f4543e7d
mspark777 <topangel777@gmail.com>
2025-01-30 18:34:32 +0900

    2493. Divide Nodes Into the Maximum Number of Groups

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,135 @@
 struct Solution {}
 
-struct DSU {
-    size: Vec<usize>,
-    representative: Vec<usize>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        return Self {
-            size: vec![1; n],
-            representative: (0..n).collect(),
-        };
-    }
-
-    fn find(&mut self, node: usize) -> usize {
-        if self.representative[node] == node {
-            return node;
-        }
-
-        self.representative[node] = self.find(self.representative[node]);
-        return self.representative[node];
-    }
-
-    fn do_union(&mut self, node_one: usize, node_two: usize) -> bool {
-        let node_one = self.find(node_one);
-        let node_two = self.find(node_two);
-
-        if node_one == node_two {
-            return false;
-        }
-
-        if self.size[node_one] > self.size[node_two] {
-            self.representative[node_two] = node_one;
-            self.size[node_one] += self.size[node_two];
-        } else {
-            self.representative[node_one] = node_two;
-            self.size[node_two] += self.size[node_one];
-        }
-
-        return true;
-    }
-}
-
 impl Solution {
-    pub fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut dsu = DSU::new(edges.len());
+    pub fn magnificent_sets(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        let n = n as usize;
+        let mut adjacent_list = vec![Vec::<usize>::new(); n];
+        let mut parent = vec![n; n];
+        let mut depth = vec![0usize; n];
 
         for edge in edges.iter() {
-            let a = edge[0] - 1;
-            let b = edge[1] - 1;
-            if !dsu.do_union(a as usize, b as usize) {
-                return edge.clone();
+            let a = (edge[0] - 1) as usize;
+            let b = (edge[1] - 1) as usize;
+
+            adjacent_list[a].push(b);
+            adjacent_list[b].push(a);
+            Self::union(a, b, &mut parent, &mut depth);
+        }
+
+        let mut num_of_groups_for_component = std::collections::HashMap::<usize, usize>::new();
+        for node in 0..n {
+            if let Some(number_of_group) = Self::get_number_of_groups(&adjacent_list, node, n) {
+                let root_node = Self::find(node, &mut parent);
+                let component = num_of_groups_for_component
+                    .entry(root_node)
+                    .or_insert(number_of_group);
+
+                *component = number_of_group.max(*component);
+            } else {
+                return -1;
             }
         }
 
-        return Vec::new();
+        let mut result = 0;
+        for &group in num_of_groups_for_component.values() {
+            result += group as i32;
+        }
+        return result;
+    }
+
+    fn find(mut node: usize, parent: &mut Vec<usize>) -> usize {
+        while parent[node] != parent.len() {
+            node = parent[node];
+        }
+
+        return node;
+    }
+
+    fn union(mut node1: usize, mut node2: usize, parent: &mut Vec<usize>, depth: &mut Vec<usize>) {
+        node1 = Self::find(node1, parent);
+        node2 = Self::find(node2, parent);
+        if node1 == node2 {
+            return;
+        }
+
+        if depth[node1] < depth[node2] {
+            let t1 = node1;
+            let t2 = node2;
+            node1 = t2;
+            node2 = t1;
+        }
+
+        parent[node2] = node1;
+        if depth[node1] == depth[node2] {
+            depth[node1] += 1;
+        }
+    }
+
+    fn get_number_of_groups(
+        adjacent_list: &Vec<Vec<usize>>,
+        src_node: usize,
+        n: usize,
+    ) -> Option<usize> {
+        let mut queue = vec![src_node];
+        let mut layer_seen: Vec<Option<usize>> = vec![None; n];
+
+        layer_seen[src_node] = Some(0);
+        let mut deepest_layer = 0;
+
+        while !queue.is_empty() {
+            let num_of_nodes_in_layer = queue.len();
+            for i in 0..num_of_nodes_in_layer {
+                let current_node = queue[i];
+                for neighbor in adjacent_list[current_node].iter().cloned() {
+                    if let Some(seen) = layer_seen[neighbor] {
+                        if seen == deepest_layer {
+                            return None;
+                        }
+                    } else {
+                        layer_seen[neighbor] = Some(deepest_layer + 1);
+                        queue.push(neighbor);
+                    }
+                }
+            }
+
+            queue = Vec::from_iter(queue.iter().skip(num_of_nodes_in_layer).cloned());
+            deepest_layer += 1;
+        }
+
+        return Some(deepest_layer);
     }
 }
 
 struct Input {
+    n: i32,
     edges: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            edges: vec![vec![1, 2], vec![1, 3], vec![2, 3]],
+            n: 6,
+            edges: vec![
+                vec![1, 2],
+                vec![1, 4],
+                vec![1, 5],
+                vec![2, 6],
+                vec![2, 3],
+                vec![4, 6],
+            ],
         },
         Input {
-            edges: vec![vec![1, 2], vec![2, 3], vec![3, 4], vec![1, 4], vec![1, 5]],
+            n: 3,
+            edges: vec![vec![1, 2], vec![2, 3], vec![3, 1]],
+        },
+        Input {
+            n: 2,
+            edges: vec![vec![1, 2]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_redundant_connection(input.edges);
+        let result = Solution::magnificent_sets(input.n, input.edges);
         println!("{result:?}");
     }
 }
