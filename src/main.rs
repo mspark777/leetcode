01976375182357413d1031756f0106ac2cc63e42
mspark777 <topangel777@gmail.com>
2025-01-31 18:19:22 +0900

    827. Making A Large Island

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,164 @@
 struct Solution {}
 
-impl Solution {
-    pub fn magnificent_sets(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-        let n = n as usize;
-        let mut adjacent_list = vec![Vec::<usize>::new(); n];
-        let mut parent = vec![n; n];
-        let mut depth = vec![0usize; n];
+struct DisjointSet {
+    parent_list: Vec<usize>,
+    island_size_list: Vec<usize>,
+}
 
-        for edge in edges.iter() {
-            let a = (edge[0] - 1) as usize;
-            let b = (edge[1] - 1) as usize;
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        let parent_list = (0..n).collect::<Vec<usize>>();
+        let island_size_list = vec![1; n];
 
-            adjacent_list[a].push(b);
-            adjacent_list[b].push(a);
-            Self::union(a, b, &mut parent, &mut depth);
-        }
-
-        let mut num_of_groups_for_component = std::collections::HashMap::<usize, usize>::new();
-        for node in 0..n {
-            if let Some(number_of_group) = Self::get_number_of_groups(&adjacent_list, node, n) {
-                let root_node = Self::find(node, &mut parent);
-                let component = num_of_groups_for_component
-                    .entry(root_node)
-                    .or_insert(number_of_group);
-
-                *component = number_of_group.max(*component);
-            } else {
-                return -1;
-            }
-        }
-
-        let mut result = 0;
-        for &group in num_of_groups_for_component.values() {
-            result += group as i32;
-        }
-        return result;
+        return Self {
+            parent_list,
+            island_size_list,
+        };
     }
 
-    fn find(mut node: usize, parent: &mut Vec<usize>) -> usize {
-        while parent[node] != parent.len() {
-            node = parent[node];
+    fn find(&mut self, node: usize) -> usize {
+        if self.parent_list[node] == node {
+            return node;
         }
 
-        return node;
+        self.parent_list[node] = self.find(self.parent_list[node]);
+        return self.parent_list[node];
     }
 
-    fn union(mut node1: usize, mut node2: usize, parent: &mut Vec<usize>, depth: &mut Vec<usize>) {
-        node1 = Self::find(node1, parent);
-        node2 = Self::find(node2, parent);
-        if node1 == node2 {
+    fn union(&mut self, node_a: usize, node_b: usize) {
+        let root_a = self.find(node_a);
+        let root_b = self.find(node_b);
+
+        if root_a == root_b {
             return;
         }
 
-        if depth[node1] < depth[node2] {
-            let t1 = node1;
-            let t2 = node2;
-            node1 = t2;
-            node2 = t1;
-        }
-
-        parent[node2] = node1;
-        if depth[node1] == depth[node2] {
-            depth[node1] += 1;
+        if self.island_size_list[root_a] < self.island_size_list[root_b] {
+            self.parent_list[root_a] = root_b;
+            self.island_size_list[root_b] += self.island_size_list[root_a];
+        } else {
+            self.parent_list[root_b] = root_a;
+            self.island_size_list[root_a] += self.island_size_list[root_b];
         }
     }
+}
 
-    fn get_number_of_groups(
-        adjacent_list: &Vec<Vec<usize>>,
-        src_node: usize,
-        n: usize,
-    ) -> Option<usize> {
-        let mut queue = vec![src_node];
-        let mut layer_seen: Vec<Option<usize>> = vec![None; n];
+impl Solution {
+    pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
+        let rows = grid.len();
+        let columns = grid[0].len();
+        let mut ds = DisjointSet::new(rows * columns);
 
-        layer_seen[src_node] = Some(0);
-        let mut deepest_layer = 0;
+        let row_directions = [-1, 1, 0, 0];
+        let column_directions = [0, 0, 1, -1];
 
-        while !queue.is_empty() {
-            let num_of_nodes_in_layer = queue.len();
-            for i in 0..num_of_nodes_in_layer {
-                let current_node = queue[i];
-                for neighbor in adjacent_list[current_node].iter().cloned() {
-                    if let Some(seen) = layer_seen[neighbor] {
-                        if seen == deepest_layer {
-                            return None;
-                        }
-                    } else {
-                        layer_seen[neighbor] = Some(deepest_layer + 1);
-                        queue.push(neighbor);
+        for current_row in 0..rows {
+            for current_column in 0..columns {
+                if grid[current_row][current_column] == 0 {
+                    continue;
+                }
+
+                let current_node = (columns * current_row) + current_column;
+                for direction in 0..4usize {
+                    let neighbor_row = (current_row as i32) + row_directions[direction];
+                    let neighbor_column = (current_column as i32) + column_directions[direction];
+                    if (neighbor_row < 0) || (neighbor_column < 0) {
+                        continue;
+                    }
+
+                    let neighbor_row = neighbor_row as usize;
+                    if neighbor_row >= rows {
+                        continue;
+                    }
+
+                    let neighbor_column = neighbor_column as usize;
+                    if neighbor_column >= columns {
+                        continue;
+                    }
+
+                    if grid[neighbor_row][neighbor_column] == 1 {
+                        let neighbor_node = (columns * neighbor_row) + neighbor_column;
+                        ds.union(current_node, neighbor_node);
                     }
                 }
             }
-
-            queue = Vec::from_iter(queue.iter().skip(num_of_nodes_in_layer).cloned());
-            deepest_layer += 1;
         }
 
-        return Some(deepest_layer);
+        let mut max_island_size = 0usize;
+        let mut has_zero = false;
+        let mut unique_roots = std::collections::HashSet::<usize>::new();
+
+        for current_row in 0..rows {
+            for current_column in 0..columns {
+                if grid[current_row][current_column] == 1 {
+                    continue;
+                }
+
+                has_zero = true;
+                let mut current_island_size = 1;
+
+                for direction in 0..4usize {
+                    let neighbor_row = (current_row as i32) + row_directions[direction];
+                    let neighbor_column = (current_column as i32) + column_directions[direction];
+                    if (neighbor_row < 0) || (neighbor_column < 0) {
+                        continue;
+                    }
+
+                    let neighbor_row = neighbor_row as usize;
+                    if neighbor_row >= rows {
+                        continue;
+                    }
+
+                    let neighbor_column = neighbor_column as usize;
+                    if neighbor_column >= columns {
+                        continue;
+                    }
+
+                    if grid[neighbor_row][neighbor_column] == 1 {
+                        let neighbor_node = (columns * neighbor_row) + neighbor_column;
+                        let root = ds.find(neighbor_node);
+                        unique_roots.insert(root);
+                    }
+                }
+
+                for root in unique_roots.iter().cloned() {
+                    current_island_size += ds.island_size_list[root];
+                }
+
+                max_island_size = max_island_size.max(current_island_size);
+                unique_roots.clear();
+            }
+        }
+
+        let result = if has_zero {
+            max_island_size
+        } else {
+            rows * columns
+        };
+
+        return result as i32;
     }
 }
 
 struct Input {
-    n: i32,
-    edges: Vec<Vec<i32>>,
+    grid: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            n: 6,
-            edges: vec![
-                vec![1, 2],
-                vec![1, 4],
-                vec![1, 5],
-                vec![2, 6],
-                vec![2, 3],
-                vec![4, 6],
-            ],
+            grid: vec![vec![1, 0], vec![0, 1]],
         },
         Input {
-            n: 3,
-            edges: vec![vec![1, 2], vec![2, 3], vec![3, 1]],
+            grid: vec![vec![1, 1], vec![1, 0]],
         },
         Input {
-            n: 2,
-            edges: vec![vec![1, 2]],
+            grid: vec![vec![1, 1], vec![1, 1]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::magnificent_sets(input.n, input.edges);
+        let result = Solution::largest_island(input.grid);
         println!("{result:?}");
     }
 }
