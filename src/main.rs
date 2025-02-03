cad8921636b272ac84939294ba7c11741041784f
mspark777 <topangel777@gmail.com>
2025-02-03 10:20:21 +0900

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 struct Solution {}
 
 impl Solution {
-    pub fn check(nums: Vec<i32>) -> bool {
-        let n = nums.len();
-        if n < 2 {
-            return true;
-        }
+    pub fn longest_monotonic_subarray(nums: Vec<i32>) -> i32 {
+        let mut inc_length = 1;
+        let mut dec_length = 1;
+        let mut result = 1;
 
-        let mut inversion_count = 0;
-        for i in 0..nums.len() {
-            let curr = nums[i];
-            let next_idx = (i + 1) % nums.len();
-            let next = nums[next_idx];
-            if curr > next {
-                inversion_count += 1;
+        for (curr, next) in nums.windows(2).map(|w| (w[0], w[1])) {
+            if curr < next {
+                inc_length += 1;
+                dec_length = 1;
+            } else if curr > next {
+                dec_length += 1;
+                inc_length = 1;
+            } else {
+                inc_length = 1;
+                dec_length = 1;
             }
 
-            if inversion_count > 1 {
-                return false;
-            }
+            result = result.max(inc_length.max(dec_length));
         }
 
-        return inversion_count <= 1;
+        return result;
     }
 }
 
@@ -32,18 +32,18 @@ struct Input {
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![3, 4, 5, 1, 2],
+            nums: vec![1, 4, 3, 3, 2],
         },
         Input {
-            nums: vec![2, 1, 3, 4],
+            nums: vec![3, 3, 3, 3],
         },
         Input {
-            nums: vec![1, 2, 3],
+            nums: vec![3, 2, 1],
         },
     ];
 
     for input in inputs {
-        let result = Solution::check(input.nums);
+        let result = Solution::longest_monotonic_subarray(input.nums);
         println!("{result:?}");
     }
 }
