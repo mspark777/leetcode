09a0162ce520381215d27c7f6b5227ef5b684db8
mspark777 <topangel777@gmail.com>
2025-02-05 00:18:42 +0900

    1800. Maximum Ascending Subarray Sum

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 struct Solution {}
 
 impl Solution {
-    pub fn longest_monotonic_subarray(nums: Vec<i32>) -> i32 {
-        let mut inc_length = 1;
-        let mut dec_length = 1;
-        let mut result = 1;
-
-        for (curr, next) in nums.windows(2).map(|w| (w[0], w[1])) {
-            if curr < next {
-                inc_length += 1;
-                dec_length = 1;
-            } else if curr > next {
-                dec_length += 1;
-                inc_length = 1;
+    pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
+        let mut result = nums[0];
+        let mut sum = nums[0];
+        for sub in nums.windows(2) {
+            let left = sub[0];
+            let right = sub[1];
+            if left < right {
+                sum += right;
             } else {
-                inc_length = 1;
-                dec_length = 1;
+                sum = right;
             }
 
-            result = result.max(inc_length.max(dec_length));
+            result = result.max(sum);
         }
 
         return result;
@@ -32,18 +27,18 @@ struct Input {
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![1, 4, 3, 3, 2],
+            nums: vec![10, 20, 30, 5, 10, 50],
         },
         Input {
-            nums: vec![3, 3, 3, 3],
+            nums: vec![10, 20, 30, 40, 50],
         },
         Input {
-            nums: vec![3, 2, 1],
+            nums: vec![12, 17, 15, 13, 10, 11, 12],
         },
     ];
 
     for input in inputs {
-        let result = Solution::longest_monotonic_subarray(input.nums);
+        let result = Solution::max_ascending_sum(input.nums);
         println!("{result:?}");
     }
 }
