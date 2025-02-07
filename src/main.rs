933f0db5728e476ad011bc6e0ab85d88579ee72f
mspark777 <topangel777@gmail.com>
2025-02-07 09:36:55 +0900

    3160. Find the Number of Distinct Colors Among the Balls

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,53 @@
 struct Solution {}
 
 impl Solution {
-    pub fn are_almost_equal(s1: String, s2: String) -> bool {
-        let mut first_diff = (' ', ' ');
-        let mut second_diff = (' ', ' ');
-        let mut num_diffs = 0usize;
+    pub fn query_results(_limit: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let n = queries.len();
+        let mut result = vec![0; n];
+        let mut color_map = std::collections::HashMap::<i32, i32>::with_capacity(n);
+        let mut ball_map = std::collections::HashMap::<i32, i32>::with_capacity(n);
 
-        for (ch1, ch2) in s1.chars().zip(s2.chars()) {
-            if ch1 == ch2 {
-                continue;
+        for (i, query) in queries.iter().enumerate() {
+            let ball = query[0];
+            if let Some(prev_color) = ball_map.get(&ball) {
+                let prev_count = color_map.get_mut(prev_color).unwrap();
+                if (*prev_count) > 1 {
+                    *prev_count -= 1;
+                } else {
+                    color_map.remove(prev_color);
+                }
             }
 
-            num_diffs += 1;
-            if num_diffs > 2 {
-                return false;
-            } else if num_diffs == 1 {
-                first_diff.0 = ch1;
-                first_diff.1 = ch2;
-            } else {
-                second_diff.0 = ch1;
-                second_diff.1 = ch2;
-            }
+            let color = query[1];
+            ball_map.insert(ball, color);
+            let count = color_map.entry(color).or_insert(0);
+            *count += 1;
+            result[i] = color_map.len() as i32;
         }
 
-        return (first_diff.0 == second_diff.1) && (first_diff.1 == second_diff.0);
+        return result;
     }
 }
 
 struct Input {
-    s1: &'static str,
-    s2: &'static str,
+    limit: i32,
+    queries: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            s1: "bank",
-            s2: "kanb",
+            limit: 4,
+            queries: vec![vec![1, 4], vec![2, 5], vec![1, 3], vec![3, 4]],
         },
         Input {
-            s1: "attack",
-            s2: "defend",
-        },
-        Input {
-            s1: "kelb",
-            s2: "kelb",
+            limit: 4,
+            queries: vec![vec![0, 1], vec![1, 2], vec![2, 2], vec![3, 4], vec![4, 5]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::are_almost_equal(input.s1.to_string(), input.s2.to_string());
+        let result = Solution::query_results(input.limit, input.queries);
         println!("{result:?}");
     }
 }
