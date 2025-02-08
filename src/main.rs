590d5cd70f08996a61b43b559936c6cf8014a8ce
mspark777 <topangel777@gmail.com>
2025-02-08 17:10:31 +0900

    2349. Design a Number Container System

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,60 @@
-struct RecentCounter {
-    queue: std::collections::VecDeque<i32>,
+struct NumberContainers {
+    number_to_indices:
+        std::collections::HashMap<i32, std::collections::BinaryHeap<std::cmp::Reverse<usize>>>,
+    index_to_numbers: std::collections::HashMap<usize, i32>,
 }
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
-impl RecentCounter {
+impl NumberContainers {
     fn new() -> Self {
         return Self {
-            queue: std::collections::VecDeque::new(),
+            number_to_indices: std::collections::HashMap::new(),
+            index_to_numbers: std::collections::HashMap::new(),
         };
     }
 
-    fn ping(&mut self, t: i32) -> i32 {
-        self.queue.push_back(t);
-        let threshold = t - 3000;
-        while let Some(&num) = self.queue.front() {
-            if num < threshold {
-                self.queue.pop_front();
-            } else {
-                break;
+    fn change(&mut self, index: i32, number: i32) {
+        let index = index as usize;
+        *self.index_to_numbers.entry(index).or_insert(number) = number;
+        let queue = self
+            .number_to_indices
+            .entry(number)
+            .or_insert(std::collections::BinaryHeap::new());
+        queue.push(std::cmp::Reverse(index));
+    }
+
+    fn find(&mut self, number: i32) -> i32 {
+        if let None = self.number_to_indices.get(&number) {
+            return -1;
+        }
+
+        if let Some(queue) = self.number_to_indices.get_mut(&number) {
+            while let Some(std::cmp::Reverse(top)) = queue.peek() {
+                if let Some(&n) = self.index_to_numbers.get(top) {
+                    if n == number {
+                        return (*top) as i32;
+                    }
+
+                    queue.pop();
+                } else {
+                    queue.pop();
+                }
             }
         }
 
-        return self.queue.len() as i32;
+        return -1;
     }
 }
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * let obj = NumberContainers::new();
+ * obj.change(index, number);
+ * let ret_2: i32 = obj.find(number);
+ */
 
 struct Input {
     limit: i32,
