bd42d246730d704b80daacddea4d160663518b9c
mspark777 <topangel777@gmail.com>
2025-02-12 15:20:35 +0900

    2342. Max Sum of a Pair With Equal Sum of Digits

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,46 @@
 struct Solution {}
 
 impl Solution {
-    pub fn remove_occurrences(s: String, part: String) -> String {
-        let s = s.chars().collect::<Vec<char>>();
-        let part = part.chars().collect::<Vec<char>>();
-        let kmp_lps = Self::compute_longest_prefix_suffix(&part);
-        let mut char_stack = Vec::<char>::new();
-        let mut pattern_indexes = vec![0usize; s.len() + 1];
+    pub fn maximum_sum(nums: Vec<i32>) -> i32 {
+        let mut result = -1;
+        let mut digit_mapping = [0; 82];
 
-        let mut str_index = 0usize;
-        let mut pattern_index = 0usize;
-        while str_index < s.len() {
-            let current_char = s[str_index];
-            char_stack.push(current_char);
-
-            if current_char == part[pattern_index] {
-                pattern_index += 1;
-                pattern_indexes[char_stack.len()] = pattern_index;
-                if pattern_index == part.len() {
-                    let mut remaining_length = part.len();
-                    while remaining_length > 0 {
-                        char_stack.pop();
-                        remaining_length -= 1;
-                    }
-
-                    pattern_index = if char_stack.is_empty() {
-                        0
-                    } else {
-                        pattern_indexes[char_stack.len()]
-                    };
-                }
-            } else {
-                if pattern_index > 0 {
-                    str_index -= 1;
-                    pattern_index = kmp_lps[pattern_index - 1];
-                    char_stack.pop();
-                } else {
-                    pattern_indexes[char_stack.len()] = 0;
-                }
+        for num in nums.iter().cloned() {
+            let mut digit_sum = 0usize;
+            let mut curr_value = num;
+            while curr_value > 0 {
+                let curr_digit = curr_value % 10;
+                digit_sum += curr_digit as usize;
+                curr_value /= 10;
             }
 
-            str_index += 1;
-        }
-
-        let mut result = Vec::<char>::with_capacity(char_stack.len());
-        while let Some(ch) = char_stack.pop() {
-            result.push(ch);
-        }
-
-        return result.iter().rev().collect();
-    }
-
-    fn compute_longest_prefix_suffix(pattern: &Vec<char>) -> Vec<usize> {
-        let mut lps = vec![0usize; pattern.len()];
-
-        let mut current = 1usize;
-        let mut prefix_length = 0usize;
-        while current < pattern.len() {
-            if pattern[current] == pattern[prefix_length] {
-                prefix_length += 1;
-                lps[current] = prefix_length;
-                current += 1;
-            } else if prefix_length != 0 {
-                prefix_length = lps[prefix_length - 1];
-            } else {
-                lps[current] = 0;
-                current += 1;
+            if digit_mapping[digit_sum] > 0 {
+                result = result.max(digit_mapping[digit_sum] + num);
             }
+
+            digit_mapping[digit_sum] = num.max(digit_mapping[digit_sum]);
         }
 
-        return lps;
+        return result;
     }
 }
 
 struct Input {
-    s: &'static str,
-    part: &'static str,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            s: "daabcbaabcbc",
-            part: "abc",
+            nums: vec![18, 43, 36, 13, 7],
         },
         Input {
-            s: "axxxxyyyyb",
-            part: "xy",
+            nums: vec![10, 12, 19, 14],
         },
     ];
 
     for input in inputs {
-        let result = Solution::remove_occurrences(input.s.to_string(), input.part.to_string());
+        let result = Solution::maximum_sum(input.nums);
         println!("{result:?}");
     }
 }
