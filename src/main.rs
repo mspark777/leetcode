540b1184a945d9e1b12213c579f28f99366248ba
mspark777 <topangel777@gmail.com>
2025-02-19 00:53:19 +0900

    2375. Construct Smallest Number From DI String

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn construct_distanced_sequence(n: i32) -> Vec<i32> {
-        let n = n as usize;
-        let mut result = vec![0; 2 * n - 1];
-        let mut is_number_used = vec![false; n + 1];
+    pub fn smallest_number(pattern: String) -> String {
+        let pattern = pattern.chars().collect::<Vec<_>>();
+        let mut nums = Vec::<char>::with_capacity(pattern.len());
+        let mut stack = Vec::<usize>::with_capacity(pattern.len());
 
-        Self::find_lexicographically_largest_sequence(0, &mut result, &mut is_number_used, n);
+        for i in 0..=pattern.len() {
+            stack.push(i + 1);
 
-        return result.iter().map(|&i| i as i32).collect();
-    }
-
-    fn find_lexicographically_largest_sequence(
-        current_index: usize,
-        result_sequence: &mut Vec<usize>,
-        is_number_used: &mut Vec<bool>,
-        target_number: usize,
-    ) -> bool {
-        if current_index == result_sequence.len() {
-            return true;
-        }
-
-        if result_sequence[current_index] != 0 {
-            return Self::find_lexicographically_largest_sequence(
-                current_index + 1,
-                result_sequence,
-                is_number_used,
-                target_number,
-            );
-        }
-
-        for number_to_place in (1..=target_number).rev() {
-            if is_number_used[number_to_place] {
-                continue;
-            }
-
-            is_number_used[number_to_place] = true;
-            result_sequence[current_index] = number_to_place;
-            if number_to_place == 1 {
-                if Self::find_lexicographically_largest_sequence(
-                    current_index + 1,
-                    result_sequence,
-                    is_number_used,
-                    target_number,
-                ) {
-                    return true;
+            if i == pattern.len() || pattern[i] == 'I' {
+                while let Some(top) = stack.pop() {
+                    let zero = '0' as usize;
+                    nums.push((top + zero) as u8 as char);
                 }
-            } else if ((current_index + number_to_place) < result_sequence.len())
-                && (result_sequence[current_index + number_to_place] == 0)
-            {
-                result_sequence[current_index + number_to_place] = number_to_place;
-                if Self::find_lexicographically_largest_sequence(
-                    current_index + 1,
-                    result_sequence,
-                    is_number_used,
-                    target_number,
-                ) {
-                    return true;
-                }
-
-                result_sequence[current_index + number_to_place] = 0;
             }
-
-            result_sequence[current_index] = 0;
-            is_number_used[number_to_place] = false;
         }
 
-        return false;
+        return nums.iter().collect();
     }
 }
 
 struct Input {
-    n: i32,
+    pattern: &'static str,
 }
 
 fn main() {
-    let inputs = vec![Input { n: 3 }, Input { n: 5 }];
+    let inputs = vec![
+        Input {
+            pattern: "IIIDIDDD",
+        },
+        Input { pattern: "DDD" },
+    ];
 
     for input in inputs {
-        let result = Solution::construct_distanced_sequence(input.n);
+        let result = Solution::smallest_number(input.pattern.to_string());
         println!("{result:?}");
     }
 }
