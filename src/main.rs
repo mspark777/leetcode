a43be4ae1e7f5501539ac08e8739caa20dbdaeee
mspark777 <topangel777@gmail.com>
2025-02-20 23:07:35 +0900

    1980. Find Unique Binary String

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,15 @@
 struct Solution {}
 
 impl Solution {
-    pub fn get_happy_string(n: i32, k: i32) -> String {
-        let total = 3 * (1 << (n - 1));
-        if k > total {
-            return "".to_string();
-        }
+    pub fn find_different_binary_string(nums: Vec<String>) -> String {
+        let mut result = Vec::<char>::with_capacity(nums.len());
 
-        let mut next_smallest = std::collections::HashMap::<char, char>::with_capacity(3);
-        next_smallest.insert('a', 'b');
-        next_smallest.insert('b', 'a');
-        next_smallest.insert('c', 'a');
-
-        let mut next_greatest = std::collections::HashMap::<char, char>::with_capacity(3);
-        next_greatest.insert('a', 'c');
-        next_greatest.insert('b', 'c');
-        next_greatest.insert('c', 'b');
-
-        let mut k = k;
-        let mut result = vec!['a'; n as usize];
-        let start_a = 1;
-        let start_b = start_a + (1 << (n - 1));
-        let start_c = start_b + (1 << (n - 1));
-
-        if k < start_b {
-            k -= start_a;
-        } else if k < start_c {
-            result[0] = 'b';
-            k -= start_b;
-        } else {
-            result[0] = 'c';
-            k -= start_c;
-        }
-
-        for ch_idx in 1..n {
-            let mid_point = 1 << (n - ch_idx - 1);
-            if k < mid_point {
-                result[ch_idx as usize] = next_smallest[&result[ch_idx as usize - 1]];
+        for i in 0..nums.len() {
+            let ch = nums[i].chars().nth(i).unwrap();
+            if ch == '1' {
+                result.push('0');
             } else {
-                result[ch_idx as usize] = next_greatest[&result[ch_idx as usize - 1]];
-                k -= mid_point;
+                result.push('1');
             }
         }
 
@@ -48,19 +18,26 @@ impl Solution {
 }
 
 struct Input {
-    n: i32,
-    k: i32,
+    nums: Vec<&'static str>,
 }
 
 fn main() {
     let inputs = vec![
-        Input { n: 1, k: 3 },
-        Input { n: 1, k: 4 },
-        Input { n: 3, k: 9 },
+        Input {
+            nums: vec!["01", "10"],
+        },
+        Input {
+            nums: vec!["00", "01"],
+        },
+        Input {
+            nums: vec!["111", "011", "001"],
+        },
     ];
 
     for input in inputs {
-        let result = Solution::get_happy_string(input.n, input.k);
+        let result = Solution::find_different_binary_string(
+            input.nums.iter().map(|s| s.to_string()).collect(),
+        );
         println!("{result:?}");
     }
 }
