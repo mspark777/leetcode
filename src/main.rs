553ab4e13e32d3ed4a19a4174f979f26fac55bea
mspark777 <topangel777@gmail.com>
2025-02-24 22:45:55 +0900

    1009. Complement of Base 10 Integer

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,26 @@
 struct Solution {}
 
 impl Solution {
-    pub fn largest_sum_after_k_negations(nums: Vec<i32>, k: i32) -> i32 {
-        let mut nums = nums.clone();
-        nums.sort_unstable();
+    pub fn bitwise_complement(n: i32) -> i32 {
+        let mut mask = 1;
 
-        let mut k = k;
-
-        for num in nums.iter_mut() {
-            if k < 1 {
-                break;
-            }
-
-            if *num < 0 {
-                *num = -*num;
-                k -= 1;
-            } else {
-                break;
-            }
+        while mask < n {
+            mask = (mask << 1) | 1;
         }
 
-        let mut result = 0;
-        let mut min_num = i32::MAX;
-        for num in nums.iter().cloned() {
-            result += num;
-            min_num = min_num.min(num);
-        }
-
-        return result - (k & 1) * min_num * 2;
+        return mask ^ n;
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    n: i32,
 }
 
 fn main() {
-    let inputs = vec![
-        Input {
-            nums: vec![4, 2, 3],
-            k: 1,
-        },
-        Input {
-            nums: vec![3, -1, 0, 2],
-            k: 3,
-        },
-        Input {
-            nums: vec![2, -3, -1, 5, -4],
-            k: 2,
-        },
-    ];
+    let inputs = vec![Input { n: 5 }, Input { n: 7 }, Input { n: 10 }];
 
     for input in inputs {
-        let result = Solution::largest_sum_after_k_negations(input.nums, input.k);
+        let result = Solution::bitwise_complement(input.n);
         println!("{result:?}");
     }
 }
