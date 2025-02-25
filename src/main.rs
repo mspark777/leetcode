9dd06f61ed9228d77e895104cad62423e921f294
mspark777 <topangel777@gmail.com>
2025-02-25 16:07:29 +0900

    1524. Number of Sub-arrays With Odd Sum

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
 struct Solution {}
 
 impl Solution {
-    pub fn bitwise_complement(n: i32) -> i32 {
-        let mut mask = 1;
+    pub fn num_of_subarrays(arr: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let mut prefix_sum = 0;
+        let mut odd_count = 0;
+        let mut even_count = 1;
 
-        while mask < n {
-            mask = (mask << 1) | 1;
+        for num in arr.iter().cloned() {
+            prefix_sum += num;
+
+            if prefix_sum & 1 == 0 {
+                result += odd_count;
+                even_count += 1;
+            } else {
+                result += even_count;
+                odd_count += 1;
+            }
+
+            result %= 1_000_000_007;
         }
 
-        return mask ^ n;
+        return result;
     }
 }
 
 struct Input {
-    n: i32,
+    arr: Vec<i32>,
 }
 
 fn main() {
-    let inputs = vec![Input { n: 5 }, Input { n: 7 }, Input { n: 10 }];
+    let inputs = vec![
+        Input { arr: vec![1, 3, 5] },
+        Input { arr: vec![2, 4, 6] },
+        Input {
+            arr: vec![1, 2, 3, 4, 5, 6, 7],
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::bitwise_complement(input.n);
+        let result = Solution::num_of_subarrays(input.arr);
         println!("{result:?}");
     }
 }
