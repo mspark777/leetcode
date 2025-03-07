30d3abfe8e04f6ea8a510db4ac04333776be6d0f
mspark777 <topangel777@gmail.com>
2025-03-07 22:21:38 +0900

    2523. Closest Prime Numbers in Range

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,80 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_missing_and_repeated_values(grid: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut sum = 0i64;
-        let mut sqr_sum = 0i64;
-        let n = grid.len();
+    pub fn closest_primes(left: i32, right: i32) -> Vec<i32> {
+        let mut prev_prime = -1;
+        let mut closet_a = -1;
+        let mut closet_b = -1;
+        let mut min_difference = 1_000_000;
 
-        for row in 0..n {
-            for col in 0..n {
-                let num = grid[row][col] as i64;
-                sum += num;
-                sqr_sum += num * num;
+        for candidate in left..=right {
+            if !Self::is_prime(candidate) {
+                continue;
             }
+
+            if prev_prime == -1 {
+                prev_prime = candidate;
+                continue;
+            }
+
+            let difference = candidate - prev_prime;
+            if difference < min_difference {
+                min_difference = difference;
+                closet_a = prev_prime;
+                closet_b = candidate;
+            }
+
+            if difference == 2 || difference == 1 {
+                return vec![prev_prime, candidate];
+            }
+
+            prev_prime = candidate;
         }
 
-        let total = (n * n) as i64;
-        let sum_diff = sum - total * (total + 1) / 2;
-        let sqr_diff = sqr_sum - total * (total + 1) * (2 * total + 1) / 6;
-        let repeat = (sqr_diff / sum_diff + sum_diff) / 2;
-        let missing = (sqr_diff / sum_diff - sum_diff) / 2;
+        return if closet_a != -1 {
+            vec![closet_a, closet_b]
+        } else {
+            vec![-1, -1]
+        };
+    }
 
-        return vec![repeat as i32, missing as i32];
+    fn is_prime(n: i32) -> bool {
+        if n < 2 {
+            return false;
+        } else if (n == 2) || (n == 3) {
+            return true;
+        } else if n & 1 == 0 {
+            return false;
+        }
+
+        let mut divisor = 3;
+        while (divisor * divisor) <= n {
+            if n % divisor == 0 {
+                return false;
+            }
+            divisor += 2;
+        }
+
+        return true;
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    left: i32,
+    right: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            grid: vec![vec![1, 3], vec![2, 2]],
+            left: 10,
+            right: 19,
         },
-        Input {
-            grid: vec![vec![9, 1, 7], vec![8, 9, 2], vec![3, 4, 6]],
-        },
+        Input { left: 4, right: 6 },
     ];
 
     for input in inputs {
-        let result = Solution::find_missing_and_repeated_values(input.grid);
+        let result = Solution::closest_primes(input.left, input.right);
         println!("{result:?}");
     }
 }
