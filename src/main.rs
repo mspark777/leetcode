7b2aa17d31a6efab06d8a72934129b446ed0affe
mspark777 <topangel777@gmail.com>
2025-03-10 00:38:04 +0900

    3208. Alternating Groups II

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,58 @@
 struct Solution {}
 
 impl Solution {
-    pub fn closest_primes(left: i32, right: i32) -> Vec<i32> {
-        let mut prev_prime = -1;
-        let mut closet_a = -1;
-        let mut closet_b = -1;
-        let mut min_difference = 1_000_000;
+    pub fn number_of_alternating_groups(colors: Vec<i32>, k: i32) -> i32 {
+        let n = colors.len();
+        let k = k as usize;
+        let mut result = 0;
+        let mut alternating_elements_count = 1;
+        let mut last_color = colors[0];
 
-        for candidate in left..=right {
-            if !Self::is_prime(candidate) {
+        for i in 1..(n + k - 1) {
+            let idx = i % n;
+
+            if colors[idx] == last_color {
+                alternating_elements_count = 1;
+                last_color = colors[idx];
                 continue;
             }
 
-            if prev_prime == -1 {
-                prev_prime = candidate;
-                continue;
+            alternating_elements_count += 1;
+
+            if alternating_elements_count >= k {
+                result += 1;
             }
 
-            let difference = candidate - prev_prime;
-            if difference < min_difference {
-                min_difference = difference;
-                closet_a = prev_prime;
-                closet_b = candidate;
-            }
-
-            if difference == 2 || difference == 1 {
-                return vec![prev_prime, candidate];
-            }
-
-            prev_prime = candidate;
+            last_color = colors[idx];
         }
 
-        return if closet_a != -1 {
-            vec![closet_a, closet_b]
-        } else {
-            vec![-1, -1]
-        };
-    }
-
-    fn is_prime(n: i32) -> bool {
-        if n < 2 {
-            return false;
-        } else if (n == 2) || (n == 3) {
-            return true;
-        } else if n & 1 == 0 {
-            return false;
-        }
-
-        let mut divisor = 3;
-        while (divisor * divisor) <= n {
-            if n % divisor == 0 {
-                return false;
-            }
-            divisor += 2;
-        }
-
-        return true;
+        return result;
     }
 }
 
 struct Input {
-    left: i32,
-    right: i32,
+    colors: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            left: 10,
-            right: 19,
+            colors: vec![0, 1, 0, 1, 0],
+            k: 3,
         },
-        Input { left: 4, right: 6 },
+        Input {
+            colors: vec![0, 1, 0, 0, 1, 0, 1],
+            k: 6,
+        },
+        Input {
+            colors: vec![1, 1, 0, 1],
+            k: 4,
+        },
     ];
 
     for input in inputs {
-        let result = Solution::closest_primes(input.left, input.right);
+        let result = Solution::number_of_alternating_groups(input.colors, input.k);
         println!("{result:?}");
     }
 }
