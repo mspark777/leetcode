a63b55e681a3834de1648d75055af8a62825d14a
mspark777 <topangel777@gmail.com>
2025-03-11 22:41:15 +0900

    1021. Remove Outermost Parentheses

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 struct Solution {}
 
 impl Solution {
-    pub fn number_of_substrings(s: String) -> i32 {
-        let s = s.chars().collect::<Vec<char>>();
-        let len = s.len();
-        let mut last_pos = [-1, -1, -1];
-        let mut result = 0;
+    pub fn remove_outer_parentheses(s: String) -> String {
+        let mut result = Vec::<char>::new();
+        let mut balance = 0;
 
-        for pos in 0..len {
-            let code = Self::code(s[pos]);
-            last_pos[code] = pos as i32;
+        for ch in s.chars() {
+            if ch == '(' {
+                if balance > 0 {
+                    result.push(ch);
+                }
 
-            result += 1 + last_pos.iter().min().unwrap();
+                balance += 1;
+            } else {
+                balance -= 1;
+
+                if balance > 0 {
+                    result.push(ch);
+                }
+            }
         }
 
-        return result;
-    }
-
-    fn code(ch: char) -> usize {
-        return ((ch as u8) - b'a') as usize;
+        return result.iter().collect();
     }
 }
 
@@ -28,13 +31,15 @@ struct Input {
 
 fn main() {
     let inputs = vec![
-        Input { s: "abcabc" },
-        Input { s: "aaacb" },
-        Input { s: "abc" },
+        Input { s: "(()())(())" },
+        Input {
+            s: "(()())(())(()(()))",
+        },
+        Input { s: "()()" },
     ];
 
     for input in inputs {
-        let result = Solution::number_of_substrings(input.s.to_string());
+        let result = Solution::remove_outer_parentheses(input.s.to_string());
         println!("{result:?}");
     }
 }
