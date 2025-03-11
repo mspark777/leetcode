2b8a226fe73bd43078429a41ac4fd1270f04efef
mspark777 <topangel777@gmail.com>
2025-03-11 15:24:08 +0900

    1358. Number of Substrings Containing All Three Characters

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_of_substrings(word: String, k: i32) -> i64 {
-        let word = word.chars().collect::<Vec<char>>();
-        let k = k as usize;
-        return Self::at_least_k(&word, k) - Self::at_least_k(&word, k + 1);
-    }
+    pub fn number_of_substrings(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        let len = s.len();
+        let mut last_pos = [-1, -1, -1];
+        let mut result = 0;
 
-    fn is_vowel(c: char) -> bool {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
+        for pos in 0..len {
+            let code = Self::code(s[pos]);
+            last_pos[code] = pos as i32;
 
-    fn at_least_k(word: &Vec<char>, k: usize) -> i64 {
-        let mut result = 0usize;
-        let mut start = 0usize;
-        let mut end = 0usize;
-        let mut vowel_count = std::collections::HashMap::<char, usize>::new();
-        let mut consonant_count = 0usize;
-
-        while end < word.len() {
-            let new_letter = word[end];
-            if Self::is_vowel(new_letter) {
-                vowel_count
-                    .entry(new_letter)
-                    .and_modify(|x| *x += 1)
-                    .or_insert(1);
-            } else {
-                consonant_count += 1;
-            }
-
-            while vowel_count.len() == 5 && consonant_count >= k {
-                result += word.len() - end;
-                let start_letter = word[start];
-                if Self::is_vowel(start_letter) {
-                    let count = vowel_count.get_mut(&start_letter).unwrap();
-                    if *count <= 1 {
-                        vowel_count.remove(&start_letter);
-                    } else {
-                        *count -= 1;
-                    }
-                } else {
-                    consonant_count -= 1;
-                }
-                start += 1;
-            }
-
-            end += 1;
+            result += 1 + last_pos.iter().min().unwrap();
         }
 
-        return result as i64;
+        return result;
+    }
+
+    fn code(ch: char) -> usize {
+        return ((ch as u8) - b'a') as usize;
     }
 }
 
 struct Input {
-    word: &'static str,
-    k: i32,
+    s: &'static str,
 }
 
 fn main() {
     let inputs = vec![
-        Input {
-            word: "aeioqq",
-            k: 1,
-        },
-        Input {
-            word: "aeiou",
-            k: 0,
-        },
-        Input {
-            word: "ieaouqqieaouqq",
-            k: 1,
-        },
+        Input { s: "abcabc" },
+        Input { s: "aaacb" },
+        Input { s: "abc" },
     ];
 
     for input in inputs {
-        let result = Solution::count_of_substrings(input.word.to_string(), input.k);
+        let result = Solution::number_of_substrings(input.s.to_string());
         println!("{result:?}");
     }
 }
