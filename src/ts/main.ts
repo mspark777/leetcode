8ccf939ff8bce71ffe9b49982b79cac4187b0fe8
mspark777 <topangel777@gmail.com>
2025-03-12 22:55:54 +0900

    1022. Sum of Root To Leaf Binary Numbers

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,78 +1,43 @@
 import "@total-typescript/ts-reset";
 
-class Solve {
-  private tree: number[][];
-  private distanceFromBob: number[];
-  private readonly n: number;
-
-  public constructor(edges: number[][], amount: number[]) {
-    this.n = amount.length;
-    this.tree = [];
-    this.distanceFromBob = [];
-    for (let i = 0; i < this.n; i += 1) {
-      this.tree.push([]);
-      this.distanceFromBob.push(0);
-    }
-
-    for (const edge of edges) {
-      const [a, b] = edge as [number, number];
-      this.tree[a]?.push(b);
-      this.tree[b]?.push(a);
-    }
-  }
-
-  public solve(bob: number, amount: number[]): number {
-    return this.findPaths(0, 0, 0, bob, amount);
-  }
-
-  private findPaths(
-    sourceNode: number,
-    parentNode: number,
-    time: number,
-    bob: number,
-    amount: number[],
-  ): number {
-    let maxIncome = 0;
-    let maxChild = Number.MIN_SAFE_INTEGER;
-
-    if (sourceNode === bob) {
-      this.distanceFromBob[sourceNode] = 0;
-    } else {
-      this.distanceFromBob[sourceNode] = this.n;
-    }
-
-    for (const adjacentNode of this.tree[sourceNode] as number[]) {
-      if (adjacentNode !== parentNode) {
-        maxChild = Math.max(
-          maxChild,
-          this.findPaths(adjacentNode, sourceNode, time + 1, bob, amount),
-        );
-        this.distanceFromBob[sourceNode] = Math.min(
-          this.distanceFromBob[sourceNode] as number,
-          (this.distanceFromBob[adjacentNode] as number) + 1,
-        );
-      }
-    }
-
-    if ((this.distanceFromBob[sourceNode] as number) > time) {
-      maxIncome += amount[sourceNode] as number;
-    } else if (this.distanceFromBob[sourceNode] === time) {
-      maxIncome += Number(BigInt(amount[sourceNode] as number) / 2n);
-    }
-
-    return maxChild === Number.MIN_SAFE_INTEGER
-      ? maxIncome
-      : maxIncome + maxChild;
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
 }
 
-function mostProfitablePath(
-  edges: number[][],
-  bob: number,
-  amount: number[],
-): number {
-  const solve = new Solve(edges, amount);
-  return solve.solve(bob, amount);
+interface Result {
+  n: number;
+}
+
+function solve(node: TreeNode, sum: number, result: Result): void {
+  const n = (sum << 1) | node.val;
+  if (node.left == null && node.right == null) {
+    result.n += n;
+  } else {
+    if (node.left != null) {
+      solve(node.left, n, result);
+    }
+
+    if (node.right != null) {
+      solve(node.right, n, result);
+    }
+  }
+}
+
+function sumRootToLeaf(root: TreeNode | null): number {
+  if (root == null) {
+    return 0;
+  }
+
+  const result: Result = { n: 0 };
+  solve(root, 0, result);
+  return result.n;
 }
 
 interface Input {
