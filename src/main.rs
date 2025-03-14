5d224895e66679d65c94b89b7acf0e3f704fba98
mspark777 <topangel777@gmail.com>
2025-03-14 22:17:19 +0900

    1025. Divisor Game

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,20 @@
 struct Solution {}
 
 impl Solution {
-    pub fn maximum_candies(candies: Vec<i32>, k: i64) -> i32 {
-        let mut left = 0;
-        let mut right = *candies.iter().max().unwrap();
-
-        while left < right {
-            let middle = (left + right + 1) / 2;
-            if Self::can_allocate_candies(&candies, k, middle) {
-                left = middle;
-            } else {
-                right = middle - 1;
-            }
-        }
-
-        return left;
-    }
-
-    fn can_allocate_candies(candies: &Vec<i32>, k: i64, num_of_candies: i32) -> bool {
-        let mut max_num_of_children = 0i64;
-
-        for pile in candies.iter().cloned() {
-            max_num_of_children += (pile / num_of_candies) as i64;
-        }
-
-        return max_num_of_children >= k;
+    pub fn divisor_game(n: i32) -> bool {
+        return n & 1 == 0;
     }
 }
 
 struct Input {
-    candies: Vec<i32>,
-    k: i64,
+    n: i32,
 }
 
 fn main() {
-    let inputs = vec![
-        Input {
-            candies: vec![5, 8, 6],
-            k: 3,
-        },
-        Input {
-            candies: vec![2, 5],
-            k: 11,
-        },
-    ];
+    let inputs = vec![Input { n: 2 }, Input { n: 3 }];
 
     for input in inputs {
-        let result = Solution::maximum_candies(input.candies, input.k);
+        let result = Solution::divisor_game(input.n);
         println!("{result:?}");
     }
 }
