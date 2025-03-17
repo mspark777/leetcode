5386834e513ef362a118bf1a0c0281bd5d88b9cd
mspark777 <topangel777@gmail.com>
2025-03-17 13:24:23 +0900

    2206. Divide Array Into Equal Pairs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,37 @@
 struct Solution {}
 
 impl Solution {
-    pub fn repair_cars(ranks: Vec<i32>, cars: i32) -> i64 {
-        let cars = cars as usize;
-        let mut low = 1;
-        let mut high = cars * cars * (ranks[0] as usize);
+    pub fn divide_array(nums: Vec<i32>) -> bool {
+        let mut unpaired = std::collections::HashSet::<i32>::with_capacity(nums.len());
 
-        while low < high {
-            let mid = (low + high) / 2;
-            let mut cars_repaired = 0usize;
-
-            for rank in ranks.iter().cloned() {
-                let fmid = mid as f64;
-                let rank = rank as f64;
-                let repaired = (fmid / rank).sqrt();
-                cars_repaired += repaired as usize;
-            }
-
-            if cars_repaired < cars {
-                low = mid + 1;
+        for num in nums.iter() {
+            if unpaired.contains(num) {
+                unpaired.remove(num);
             } else {
-                high = mid;
+                unpaired.insert(*num);
             }
         }
 
-        return low as i64;
+        return unpaired.is_empty();
     }
 }
 
 struct Input {
-    ranks: Vec<i32>,
-    cars: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            ranks: vec![4, 2, 3, 1],
-            cars: 10,
+            nums: vec![3, 2, 3, 2, 2, 2],
         },
         Input {
-            ranks: vec![5, 1, 8],
-            cars: 6,
+            nums: vec![1, 2, 3, 4],
         },
     ];
 
     for input in inputs {
-        let result = Solution::repair_cars(input.ranks, input.cars);
+        let result = Solution::divide_array(input.nums);
         println!("{result:?}");
     }
 }
