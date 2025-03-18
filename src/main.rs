a66a32e1495d40f837e3c59452f2d9aab9276059
mspark777 <topangel777@gmail.com>
2025-03-19 00:54:32 +0900

    2401. Longest Nice Subarray

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 struct Solution {}
 
 impl Solution {
-    pub fn divide_array(nums: Vec<i32>) -> bool {
-        let mut unpaired = std::collections::HashSet::<i32>::with_capacity(nums.len());
+    pub fn longest_nice_subarray(nums: Vec<i32>) -> i32 {
+        let mut used_bits = 0;
+        let mut window_start = 0usize;
+        let mut max_length = 0usize;
 
-        for num in nums.iter() {
-            if unpaired.contains(num) {
-                unpaired.remove(num);
-            } else {
-                unpaired.insert(*num);
+        for window_end in 0..nums.len() {
+            while (used_bits & nums[window_end]) != 0 {
+                used_bits ^= nums[window_start];
+                window_start += 1;
             }
+
+            used_bits |= nums[window_end];
+            max_length = max_length.max(window_end - window_start + 1);
         }
 
-        return unpaired.is_empty();
+        return max_length as i32;
     }
 }
 
@@ -23,15 +27,15 @@ struct Input {
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![3, 2, 3, 2, 2, 2],
+            nums: vec![1, 3, 8, 48, 10],
         },
         Input {
-            nums: vec![1, 2, 3, 4],
+            nums: vec![3, 1, 5, 11, 13],
         },
     ];
 
     for input in inputs {
-        let result = Solution::divide_array(input.nums);
+        let result = Solution::longest_nice_subarray(input.nums);
         println!("{result:?}");
     }
 }
