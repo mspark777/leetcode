11afc7d4c4baa7917fe7c2e202eab56590b58833
mspark777 <topangel777@gmail.com>
2025-03-20 22:15:27 +0900

    3108. Minimum Cost Walk in Weighted Graph

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,104 @@
 struct Solution {}
 
+struct Helper {
+    parent: Vec<usize>,
+    depth: Vec<usize>,
+    n: usize,
+}
+
+impl Helper {
+    fn new(n: usize) -> Self {
+        return Self {
+            n,
+            parent: vec![n; n],
+            depth: vec![0; n],
+        };
+    }
+
+    pub fn find(&mut self, node: usize) -> usize {
+        if self.parent[node] == self.n {
+            return node;
+        }
+
+        self.parent[node] = self.find(self.parent[node]);
+        return self.parent[node];
+    }
+
+    pub fn union(&mut self, node1: usize, node2: usize) {
+        let mut root1 = self.find(node1);
+        let mut root2 = self.find(node2);
+
+        if root1 == root2 {
+            return;
+        }
+
+        if self.depth[root1] < self.depth[root2] {
+            let t1 = root1;
+            let t2 = root2;
+            root1 = t2;
+            root2 = t1;
+        }
+
+        self.parent[root2] = root1;
+        if self.depth[root1] == self.depth[root2] {
+            self.depth[root1] += 1;
+        }
+    }
+}
+
 impl Solution {
-    pub fn min_operations(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let mut result = 0;
-        let mut nums = nums.clone();
+    pub fn minimum_cost(n: i32, edges: Vec<Vec<i32>>, query: Vec<Vec<i32>>) -> Vec<i32> {
+        let n = n as usize;
+        let mut helper = Helper::new(n);
 
-        for i in 0..=(n - 3) {
-            if nums[i] != 0 {
-                continue;
+        for edge in edges.iter() {
+            helper.union(edge[0] as usize, edge[1] as usize);
+        }
+
+        let mut component_cost = vec![u32::MAX; n];
+        for edge in edges.iter() {
+            let root = helper.find(edge[0] as usize);
+            component_cost[root] &= edge[2] as u32;
+        }
+
+        let mut result = Vec::<i32>::new();
+        for q in query.iter() {
+            let start = q[0] as usize;
+            let end = q[1] as usize;
+
+            if helper.find(start) != helper.find(end) {
+                result.push(-1);
+            } else {
+                let root = helper.find(start);
+                result.push(component_cost[root] as i32);
             }
-
-            nums[i] = 1;
-            nums[i + 1] = if nums[i + 1] == 0 { 1 } else { 0 };
-            nums[i + 2] = if nums[i + 2] == 0 { 1 } else { 0 };
-            result += 1;
         }
-
-        if (nums[n - 1] == 0) || (nums[n - 2] == 0) {
-            return -1;
-        }
-
         return result;
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
+    edges: Vec<Vec<i32>>,
+    query: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![0, 1, 1, 1, 0, 0],
+            n: 5,
+            edges: vec![vec![0, 1, 7], vec![1, 3, 7], vec![1, 2, 1]],
+            query: vec![vec![0, 3], vec![3, 4]],
         },
         Input {
-            nums: vec![0, 1, 1, 1],
+            n: 3,
+            edges: vec![vec![0, 2, 7], vec![0, 1, 15], vec![1, 2, 1]],
+            query: vec![vec![1, 2]],
         },
     ];
 
     for input in inputs {
-        let result = Solution::min_operations(input.nums);
+        let result = Solution::minimum_cost(input.n, input.edges, input.query);
         println!("{result:?}");
     }
 }
