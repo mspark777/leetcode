641674dfd270fe6170625fa78fe1d2ad823c64b2
mspark777 <topangel777@gmail.com>
2025-03-29 15:08:13 +0900

    2818. Apply Operations to Maximize Score

--- src/main.rs
+++ src/main.rs
@@ -1,172 +1,143 @@
 struct Solution {}
 
-#[derive(Clone, Copy)]
-struct Cell {
-    row: usize,
-    col: usize,
-    value: i32,
-}
-
-impl Cell {
-    fn new(row: usize, col: usize, value: i32) -> Self {
-        return Self { row, col, value };
-    }
-}
-
-struct Query {
-    idx: usize,
-    value: i32,
-}
-
-impl Query {
-    fn new(idx: usize, value: i32) -> Self {
-        return Self { idx, value };
-    }
-}
-
-struct UnionFind {
-    parent: Vec<usize>,
-    size: Vec<usize>,
-    n: usize,
-}
-
-impl UnionFind {
-    fn new(n: usize) -> Self {
-        return Self {
-            parent: vec![n; n],
-            size: vec![1; n],
-            n,
-        };
-    }
-
-    fn find(&mut self, node: usize) -> usize {
-        if self.parent[node] >= self.n {
-            return node;
-        }
-
-        self.parent[node] = self.find(self.parent[node]);
-        return self.parent[node];
-    }
-
-    fn union(&mut self, node_a: usize, node_b: usize) -> bool {
-        let root_a = self.find(node_a);
-        let root_b = self.find(node_b);
-        if root_a == root_b {
-            return false;
-        }
-
-        if self.size[root_a] > self.size[root_b] {
-            self.parent[root_b] = root_a;
-            self.size[root_a] += self.size[root_b];
-        } else {
-            self.parent[root_a] = root_b;
-            self.size[root_b] += self.size[root_a];
-        }
-
-        return true;
-    }
-
-    fn get_size(&mut self, node: usize) -> i32 {
-        let parent = self.find(node);
-        return self.size[parent] as i32;
-    }
-}
+const MOD: i64 = 1000000007;
 
 impl Solution {
-    pub fn max_points(grid: Vec<Vec<i32>>, queries: Vec<i32>) -> Vec<i32> {
-        let row_count = grid.len();
-        let col_count = grid[0].len();
-        let total_cells = row_count * col_count;
+    pub fn maximum_score(nums: Vec<i32>, k: i32) -> i32 {
+        let n = nums.len();
+        let mut prime_scores = vec![0usize; n];
 
-        let mut sorted_queries = queries
+        let max_element = nums.iter().cloned().max().unwrap();
+        let primes = Self::get_primes(max_element as usize);
+
+        for (i, num) in nums.iter().cloned().enumerate() {
+            let mut num = num as usize;
+            for prime in primes.iter().cloned() {
+                if (prime * prime) > num {
+                    break;
+                } else if (num % prime) != 0 {
+                    continue;
+                }
+
+                prime_scores[i] += 1;
+                while (num % prime) == 0 {
+                    num /= prime;
+                }
+            }
+
+            if num > 1 {
+                prime_scores[i] += 1;
+            }
+        }
+
+        let mut next_dominant = vec![n as i64; n];
+        let mut prev_dominant = vec![-1i64; n];
+        let mut decreasing_prime_score_stack = Vec::<i32>::new();
+
+        for i in 0..n {
+            while let Some(&top) = decreasing_prime_score_stack.last() {
+                if prime_scores[top as usize] >= prime_scores[i] {
+                    break;
+                }
+
+                let top_idx = decreasing_prime_score_stack.pop().unwrap();
+                next_dominant[top_idx as usize] = i as i64;
+            }
+
+            if let Some(&top) = decreasing_prime_score_stack.last() {
+                prev_dominant[i] = top as i64;
+            }
+
+            decreasing_prime_score_stack.push(i as i32);
+        }
+
+        let mut num_of_subarrays = vec![0i64; n];
+        for i in 0..n {
+            let j = i as i64;
+            num_of_subarrays[i] = (next_dominant[i] - j) * (j - prev_dominant[i]);
+        }
+
+        let mut sorted_array = nums
             .iter()
             .cloned()
             .enumerate()
-            .map(|(idx, q)| Query::new(idx, q))
+            .map(|(i, n)| (n, i))
             .collect::<Vec<_>>();
 
-        sorted_queries.sort_unstable_by_key(|q| q.value);
+        sorted_array.sort_unstable_by_key(|k| -k.0);
 
-        let mut sorted_cells = Vec::<Cell>::with_capacity(total_cells);
-        for (i, row) in grid.iter().enumerate() {
-            for (j, cell) in row.iter().cloned().enumerate() {
-                sorted_cells.push(Cell::new(i, j, cell));
-            }
+        let mut result = 1i64;
+        let mut processing_index = 0usize;
+
+        let mut k = k as i64;
+        while k > 0 {
+            let (num, i) = sorted_array[processing_index];
+            processing_index += 1;
+            let operations = k.min(num_of_subarrays[i]);
+
+            result = (result * Self::power(num as i64, operations)) % MOD;
+            k -= operations;
         }
 
-        sorted_cells.sort_unstable_by_key(|c| c.value);
-
-        let mut uf = UnionFind::new(total_cells);
-        let mut result = vec![0; queries.len()];
-        let mut cell_idx = 0usize;
-
-        let row_directions = [0, 0, 1, -1];
-        let col_directions = [1, -1, 0, 0];
-
-        for query in sorted_queries.iter() {
-            while (cell_idx < total_cells) && (sorted_cells[cell_idx].value < query.value) {
-                let cell = sorted_cells[cell_idx];
-                let row = cell.row;
-                let col = cell.col;
-                let cell_id = row * col_count + col;
-
-                for dir in 0..4usize {
-                    let may_new_row = Self::next(row, row_directions[dir], row_count);
-                    let may_new_col = Self::next(col, col_directions[dir], col_count);
-                    if let (Some(new_row), Some(new_col)) = (may_new_row, may_new_col) {
-                        if grid[new_row][new_col] < query.value {
-                            uf.union(cell_id, new_row * col_count + new_col);
-                        }
-                    }
-                }
-
-                cell_idx += 1;
-            }
-
-            result[query.idx] = if query.value > grid[0][0] {
-                uf.get_size(0)
-            } else {
-                0
-            };
-        }
-
-        return result;
+        return result as i32;
     }
 
-    fn next(cur: usize, dir: i32, max: usize) -> Option<usize> {
-        if dir >= 0 {
-            let idx = cur + (dir as usize);
-            return if idx < max { Some(idx) } else { None };
+    fn power(base: i64, exponent: i64) -> i64 {
+        let mut base = base;
+        let mut exponent = exponent;
+        let mut res = 1i64;
+
+        while exponent > 0 {
+            if (exponent & 1) == 1 {
+                res = (res * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            exponent /= 2;
         }
 
-        let dir = (-dir) as usize;
-        if cur < dir {
-            return None;
+        return res;
+    }
+
+    fn get_primes(limit: usize) -> Vec<usize> {
+        let mut is_prime = vec![true; limit + 1];
+        let mut primes = Vec::<usize>::with_capacity(limit);
+
+        for num in 2..limit {
+            if !is_prime[num] {
+                continue;
+            }
+
+            primes.push(num);
+
+            for multiple in ((num * num)..=limit).step_by(num) {
+                is_prime[multiple] = false;
+            }
         }
 
-        return Some(cur - dir);
+        return primes;
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
-    queries: Vec<i32>,
+    nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            grid: vec![vec![1, 2, 3], vec![2, 5, 7], vec![3, 5, 1]],
-            queries: vec![5, 6, 2],
+            nums: vec![8, 3, 9, 3, 8],
+            k: 2,
         },
         Input {
-            grid: vec![vec![5, 2, 1], vec![1, 1, 2]],
-            queries: vec![3],
+            nums: vec![19, 12, 14, 6, 10, 18],
+            k: 3,
         },
     ];
 
     for input in inputs {
-        let result = Solution::max_points(input.grid, input.queries);
+        let result = Solution::maximum_score(input.nums, input.k);
         println!("{result:?}");
     }
 }
