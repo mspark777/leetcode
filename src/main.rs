6f9983ab5e6c4e760806ed9cc4a0c37f3e2ca79d
mspark777 <topangel777@gmail.com>
2025-04-02 10:09:05 +0900

    2873. Maximum Value of an Ordered Triplet I

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,41 @@
 struct Solution {}
 
 impl Solution {
-    pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
-        let n = questions.len();
-        let mut dp = vec![0i64; n];
-        dp[n - 1] = questions[n - 1][0] as i64;
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let mut result = 0i64;
+        let mut imax = 0i64;
+        let mut dmax = 0i64;
 
-        for i in (0..(n - 1)).rev() {
-            let question = &questions[i];
-            let points = question[0] as i64;
-            let brainpower = question[1] as usize;
-            let next_idx = i + brainpower + 1;
-            let mut solve_points = points;
-            if next_idx < n {
-                solve_points += dp[next_idx];
-            }
-
-            let skip_points = dp[i + 1];
-            dp[i] = solve_points.max(skip_points);
+        for num in nums.iter().cloned() {
+            let num = num as i64;
+            result = result.max(dmax * num);
+            dmax = dmax.max(imax - num);
+            imax = imax.max(num);
         }
 
-        return dp[0];
+        return result;
     }
 }
 
 struct Input {
-    questions: Vec<Vec<i32>>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            questions: vec![vec![3, 2], vec![4, 3], vec![4, 4], vec![2, 5]],
+            nums: vec![12, 6, 1, 2, 7],
         },
         Input {
-            questions: vec![vec![1, 1], vec![2, 2], vec![3, 3], vec![4, 4], vec![5, 5]],
+            nums: vec![1, 10, 3, 4, 19],
+        },
+        Input {
+            nums: vec![1, 2, 3],
         },
     ];
 
     for input in inputs {
-        let result = Solution::most_points(input.questions);
+        let result = Solution::maximum_triplet_value(input.nums);
         println!("{result:?}");
     }
 }
