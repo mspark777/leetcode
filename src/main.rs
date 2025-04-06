288373d8a41a787ab7efccce8aac266f5b4ff264
mspark777 <topangel777@gmail.com>
2025-04-06 15:17:19 +0900

    368. Largest Divisible Subset

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
-        if nums.is_empty() {
-            return 0;
+    pub fn largest_divisible_subset(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums = nums.clone();
+        nums.sort_unstable();
+
+        let n = nums.len();
+        let mut prev = vec![Vec::<i32>::new(); n + 1];
+
+        for i in 1..=n {
+            let mut curr = vec![Vec::<i32>::new(); n + 1];
+            for j in 1..=n {
+                let mut x = Vec::<i32>::new();
+                if j == 1 {
+                    x = prev[i].clone();
+                    x.push(nums[i - 1]);
+                } else if (nums[j - 1] % nums[i - 1]) == 0 {
+                    x = prev[i].clone();
+                    x.push(nums[i - 1]);
+                } else if (nums[i - 1] % nums[j - 1]) == 0 {
+                    x = prev[i].clone();
+                    x.push(nums[i - 1]);
+                }
+
+                let y = &prev[j];
+                if y.len() > x.len() {
+                    curr[j] = y.clone();
+                } else {
+                    curr[j] = x.clone();
+                }
+            }
+
+            prev = curr;
         }
 
-        let mut or = 0usize;
-        for num in nums.iter().cloned() {
-            or |= num as usize;
-        }
-
-        let shift = nums.len() - 1;
-        return (or << shift) as i32;
+        return prev[1].clone();
     }
 }
 
@@ -22,17 +44,16 @@ struct Input {
 
 fn main() {
     let inputs = vec![
-        Input { nums: vec![1, 3] },
         Input {
-            nums: vec![5, 1, 6],
+            nums: vec![1, 2, 3],
         },
         Input {
-            nums: vec![1, 10, 3, 4, 19],
+            nums: vec![1, 2, 4, 8],
         },
     ];
 
     for input in inputs {
-        let result = Solution::subset_xor_sum(input.nums);
+        let result = Solution::largest_divisible_subset(input.nums);
         println!("{result:?}");
     }
 }
