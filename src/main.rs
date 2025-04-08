2b4f510d8f6fcde35099569638143fd2fb668c8d
mspark777 <topangel777@gmail.com>
2025-04-08 22:50:55 +0900

    3396. Minimum Number of Operations to Make Elements in Array Distinct

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
 struct Solution {}
 
 impl Solution {
-    pub fn can_partition(nums: Vec<i32>) -> bool {
-        let sum = nums.iter().sum::<i32>();
-        let sum = sum as usize;
-        if (sum & 1) == 1 {
-            return false;
-        }
+    pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+        let mut seen = std::collections::HashSet::<i32>::with_capacity(nums.len());
 
-        let target = sum / 2;
-        let mut dp = vec![false; target + 1];
-        dp[0] = true;
-
-        for num in nums.iter().cloned() {
-            let num = num as usize;
-            for curr_sum in (num..=target).rev() {
-                dp[curr_sum] = dp[curr_sum] || dp[curr_sum - num];
-                if dp[target] {
-                    return true;
-                }
+        for (i, num) in nums.iter().enumerate().rev() {
+            if seen.contains(num) {
+                let i = i as i32;
+                return i / 3 + 1;
+            } else {
+                seen.insert(*num);
             }
         }
 
-        return dp[target];
+        return 0;
     }
 }
 
@@ -33,15 +24,18 @@ struct Input {
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![1, 5, 11, 5],
+            nums: vec![1, 2, 3, 4, 2, 3, 3, 5, 7],
         },
         Input {
-            nums: vec![1, 2, 3, 5],
+            nums: vec![4, 5, 6, 4, 4],
+        },
+        Input {
+            nums: vec![6, 7, 8, 9],
         },
     ];
 
     for input in inputs {
-        let result = Solution::can_partition(input.nums);
+        let result = Solution::minimum_operations(input.nums);
         println!("{result:?}");
     }
 }
