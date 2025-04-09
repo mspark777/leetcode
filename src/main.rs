7e32a567e152d0c55b95ed3465f37aa315d5ecbf
mspark777 <topangel777@gmail.com>
2025-04-09 09:34:19 +0900

    3375. Minimum Operations to Make Array Values Equal to K

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,43 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-        let mut seen = std::collections::HashSet::<i32>::with_capacity(nums.len());
-
-        for (i, num) in nums.iter().enumerate().rev() {
-            if seen.contains(num) {
-                let i = i as i32;
-                return i / 3 + 1;
-            } else {
-                seen.insert(*num);
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut num_set = std::collections::HashSet::<i32>::with_capacity(nums.len());
+        for num in nums.iter().cloned() {
+            if num < k {
+                return -1;
+            } else if num > k {
+                num_set.insert(num);
             }
         }
 
-        return 0;
+        return num_set.len() as i32;
     }
 }
 
 struct Input {
     nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            nums: vec![1, 2, 3, 4, 2, 3, 3, 5, 7],
+            nums: vec![5, 2, 5, 4, 5],
+            k: 2,
         },
         Input {
-            nums: vec![4, 5, 6, 4, 4],
+            nums: vec![2, 1, 2],
+            k: 2,
         },
         Input {
-            nums: vec![6, 7, 8, 9],
+            nums: vec![9, 7, 5, 3],
+            k: 1,
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_operations(input.nums);
+        let result = Solution::min_operations(input.nums, input.k);
         println!("{result:?}");
     }
 }
