18ab093be22161b637210a212cb66459d2de9777
mspark777 <topangel777@gmail.com>
2025-04-12 00:06:24 +0900

    2843. Count Symmetric Integers

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,47 @@
 struct Solution {}
 
 impl Solution {
-    pub fn number_of_powerful_int(start: i64, finish: i64, limit: i32, s: String) -> i64 {
-        let start = (start - 1).to_string();
-        let finish = finish.to_string();
-        return Self::calculate(&finish, &s, limit) - Self::calculate(&start, &s, limit);
-    }
+    pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
+        let mut result = 0;
 
-    fn calculate(x: &str, s: &str, limit: i32) -> i64 {
-        if x.len() < s.len() {
-            return 0;
-        } else if x.len() == s.len() {
-            return if x >= s { 1 } else { 0 };
-        }
-
-        let suffix = &x[x.len() - s.len()..];
-        let mut count = 0i64;
-        let pre_len = x.len() - s.len();
-        for i in 0..pre_len {
-            let digit = x.chars().nth(i).unwrap().to_digit(10).unwrap() as i32;
-            if limit < digit {
-                count += ((limit + 1) as i64).pow((pre_len - i) as u32);
-                return count;
+        for n in low..=high {
+            if n < 100 {
+                if (n % 11) == 0 {
+                    result += 1;
+                }
+            } else if n < 1000 {
+                continue;
+            } else if n >= 10000 {
+                continue;
             }
-            count += (digit as i64) * ((limit + 1) as i64).pow((pre_len - 1 - i) as u32);
-        }
-        if suffix >= s {
-            count += 1;
+
+            let left = (n / 1000) + ((n % 1000) / 100);
+            let right = ((n % 100) / 10) + (n % 10);
+            if left == right {
+                result += 1;
+            }
         }
 
-        return count;
+        return result;
     }
 }
 
 struct Input {
-    start: i64,
-    finish: i64,
-    limit: i32,
-    s: &'static str,
+    low: i32,
+    high: i32,
 }
 
 fn main() {
     let inputs = vec![
+        Input { low: 1, high: 100 },
         Input {
-            start: 1,
-            finish: 6000,
-            limit: 4,
-            s: "124",
-        },
-        Input {
-            start: 15,
-            finish: 215,
-            limit: 6,
-            s: "10",
-        },
-        Input {
-            start: 1000,
-            finish: 2000,
-            limit: 4,
-            s: "3000",
+            low: 1200,
+            high: 1230,
         },
     ];
 
     for input in inputs {
-        let result = Solution::number_of_powerful_int(
-            input.start,
-            input.finish,
-            input.limit,
-            input.s.to_string(),
-        );
+        let result = Solution::count_symmetric_integers(input.low, input.high);
         println!("{result:?}");
     }
 }
