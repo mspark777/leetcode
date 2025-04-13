e427c90181273091c183ec1764d8c2dfd3a645bb
mspark777 <topangel777@gmail.com>
2025-04-14 00:47:47 +0900

    1922. Count Good Numbers

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_good_integers(n: i32, k: i32) -> i64 {
-        let mut dict = std::collections::HashSet::<String>::new();
-        let base = 10i32.pow(((n - 1) / 2) as u32);
-        let skip = (n & 1) as usize;
+    const MOD: i64 = 1000000007;
+    pub fn count_good_numbers(n: i64) -> i32 {
+        let left = Self::quickmul(5, (n + 1) / 2);
+        let right = Self::quickmul(4, n / 2);
+        return ((left * right) % Self::MOD) as i32;
+    }
 
-        for i in base..(base * 10) {
-            let s = i.to_string();
-            let rev = s.chars().rev().skip(skip).collect::<String>();
-            let combined = format!("{}{}", s, rev);
-            let palindromic_integer = combined.parse::<i64>().unwrap();
-            if palindromic_integer % (k as i64) == 0 {
-                let mut sorted_chars = combined.chars().collect::<Vec<char>>();
-                sorted_chars.sort();
-                dict.insert(sorted_chars.into_iter().collect());
-            }
-        }
+    fn quickmul(x: i32, y: i64) -> i64 {
+        let mut result = 1i64;
+        let mut mul = x as i64;
+        let mut y = y;
 
-        let mut factorial = vec![1i64; (n + 1) as usize];
-        for i in 1..=(n as usize) {
-            factorial[i] = factorial[i - 1] * (i as i64);
-        }
-
-        let mut result = 0i64;
-        for s in dict {
-            let mut cnt = vec![0; 10];
-            for c in s.chars() {
-                cnt[c.to_digit(10).unwrap() as usize] += 1;
-            }
-            let mut tot = (n as i64 - cnt[0] as i64) * factorial[(n - 1) as usize];
-            for &x in cnt.iter() {
-                tot /= factorial[x];
+        while y > 0 {
+            if (y & 1) == 1 {
+                result = (result * mul) % Self::MOD;
             }
 
-            result += tot;
+            mul = (mul * mul) % Self::MOD;
+            y >>= 1;
         }
+
         return result;
     }
 }
 
 struct Input {
-    n: i32,
-    k: i32,
+    n: i64,
 }
 
 fn main() {
-    let inputs = vec![
-        Input { n: 3, k: 5 },
-        Input { n: 1, k: 4 },
-        Input { n: 5, k: 6 },
-    ];
+    let inputs = vec![Input { n: 1 }, Input { n: 4 }, Input { n: 50 }];
 
     for input in inputs {
-        let result = Solution::count_good_integers(input.n, input.k);
+        let result = Solution::count_good_numbers(input.n);
         println!("{result:?}");
     }
 }
