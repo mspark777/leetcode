b7bb8ba3a23f3c05643768d62b53b4f2e4a0e7fb
mspark777 <topangel777@gmail.com>
2025-05-02 11:52:58 +0900

    838. Push Dominoes

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,73 @@
 struct Solution {}
 
 impl Solution {
-    pub fn max_task_assign(tasks: Vec<i32>, workers: Vec<i32>, pills: i32, strength: i32) -> i32 {
-        let n = tasks.len();
-        let m = workers.len();
-        let mut tasks = tasks;
-        let mut workers = workers;
+    pub fn push_dominoes(dominoes: String) -> String {
+        const LEFT: char = 'L';
+        const RIGHT: char = 'R';
+        const STAND: char = '.';
 
-        tasks.sort();
-        workers.sort();
+        let dominoes = dominoes.chars().collect::<Vec<char>>();
+        let dominoes_len = dominoes.len();
+        let max_force = dominoes_len as i32;
 
-        let mut left = 1;
-        let mut right = m.min(n);
-        let mut result = 0usize;
-        while left <= right {
-            let mid = (left + right) / 2;
-            if Self::check(mid, pills, m, &workers, strength, &tasks) {
-                result = mid;
-                left = mid + 1;
+        let mut force = 0;
+        let mut forces = vec![0; dominoes_len];
+        for (i, domino) in dominoes.iter().cloned().enumerate() {
+            if domino == LEFT {
+                force = 0;
+            } else if domino == RIGHT {
+                force = max_force;
             } else {
-                right = mid - 1;
+                force = force.max(1) - 1;
+            }
+
+            forces[i] += force;
+        }
+
+        force = 0;
+        for (i, domino) in dominoes.iter().cloned().enumerate().rev() {
+            if domino == LEFT {
+                force = max_force;
+            } else if domino == RIGHT {
+                force = 0;
+            } else {
+                force = force.max(1) - 1;
+            }
+
+            forces[i] -= force;
+        }
+
+        let mut result = vec![' '; dominoes_len];
+        for (i, force) in forces.into_iter().enumerate() {
+            if force < 0 {
+                result[i] = LEFT;
+            } else if force > 0 {
+                result[i] = RIGHT;
+            } else {
+                result[i] = STAND;
             }
         }
 
-        return result as i32;
-    }
-
-    fn check(
-        mid: usize,
-        p: i32,
-        m: usize,
-        workers: &Vec<i32>,
-        strength: i32,
-        tasks: &Vec<i32>,
-    ) -> bool {
-        let mut p = p;
-        let mut ws = std::collections::VecDeque::<i32>::new();
-        let mut ptr = m - 1;
-        for i in (0..mid).rev() {
-            while ptr as i32 >= (mid - mid) as i32 && workers[ptr] + strength >= tasks[i] {
-                ws.push_front(workers[ptr]);
-                ptr -= 1;
-            }
-
-            if ws.is_empty() {
-                return false;
-            }
-
-            if *ws.back().unwrap() >= tasks[i] {
-                ws.pop_back();
-            } else {
-                if p == 0 {
-                    return false;
-                }
-
-                p -= 1;
-                ws.pop_front();
-            }
-        }
-
-        return true;
+        return result.iter().collect();
     }
 }
 
 struct Input {
-    tasks: Vec<i32>,
-    workers: Vec<i32>,
-    pills: i32,
-    strength: i32,
+    dominoes: String,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            tasks: [3, 2, 1].to_vec(),
-            workers: [3, 2, 1].to_vec(),
-            pills: 1,
-            strength: 1,
+            dominoes: "RR.L".to_string(),
         },
         Input {
-            tasks: [5, 4].to_vec(),
-            workers: [0, 0, 0].to_vec(),
-            pills: 1,
-            strength: 5,
-        },
-        Input {
-            tasks: [10, 15, 30].to_vec(),
-            workers: [0, 10, 10, 10, 10].to_vec(),
-            pills: 3,
-            strength: 10,
+            dominoes: ".L.R...LR..L..".to_string(),
         },
     ];
 
     for input in inputs {
-        let result =
-            Solution::max_task_assign(input.tasks, input.workers, input.pills, input.strength);
+        let result = Solution::push_dominoes(input.dominoes);
         println!("{result:?}");
     }
 }
