e5e25716e64ed6b24b9fb046059af41024504077
mspark777 <topangel777@gmail.com>
2025-05-05 18:01:41 +0900

    790. Domino and Tromino Tiling

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,37 @@
 struct Solution {}
 
 impl Solution {
-    pub fn num_equiv_domino_pairs(dominoes: Vec<Vec<i32>>) -> i32 {
-        let mut nums = vec![0; 100];
-        let mut result = 0;
-
-        for domino in dominoes.iter() {
-            let a = domino[0];
-            let b = domino[1];
-            let num = (if a < b { a * 10 + b } else { b * 10 + a }) as usize;
-
-            result += nums[num];
-            nums[num] += 1;
+    pub fn num_tilings(n: i32) -> i32 {
+        if n < 3 {
+            return n;
         }
 
-        return result;
+        let n = n as i64;
+        const MOD: i64 = 1_000_000_007;
+        let mut ways_prev1 = 2i64;
+        let mut ways_prev2 = 1i64;
+        let mut tromino_sum = 1i64;
+
+        for _ in 3..=n {
+            let current = (ways_prev1 + ways_prev2 + 2 * tromino_sum) % MOD;
+            tromino_sum = (tromino_sum + ways_prev2) % MOD;
+            ways_prev2 = ways_prev1;
+            ways_prev1 = current;
+        }
+
+        return ways_prev1 as i32;
     }
 }
 
 struct Input {
-    dominoes: Vec<Vec<i32>>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = vec![
-        Input {
-            dominoes: [[1, 2], [2, 1], [3, 4], [5, 6]]
-                .to_vec()
-                .iter()
-                .map(|s| s.to_vec())
-                .collect(),
-        },
-        Input {
-            dominoes: [[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]]
-                .to_vec()
-                .iter()
-                .map(|s| s.to_vec())
-                .collect(),
-        },
-    ];
+    let inputs = vec![Input { n: 3 }, Input { n: 1 }];
 
     for input in inputs {
-        let result = Solution::num_equiv_domino_pairs(input.dominoes);
+        let result = Solution::num_tilings(input.n);
         println!("{result:?}");
     }
 }
