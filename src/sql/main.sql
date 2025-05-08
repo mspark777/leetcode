bfff44f2a97653272b74419f6dccba21f4b2641b
mspark777 <topangel777@gmail.com>
2025-05-08 21:35:57 +0900

    1050. Actors and Directors Who Cooperated At Least Three Times

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,13 +1,6 @@
-SELECT
-  name
-FROM
-  SalesPerson
-WHERE
-  sales_id NOT IN (
-    SELECT
-    sales_id
-    FROM
-    Orders o
-    JOIN Company c ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-  );
+select actor_id, director_id
+from actordirector
+group by actor_id, director_id
+having count(*) >= 3
+;
+
