ef9a5abcd98e269191266f7dcb1353cc7ecd61f1
mspark777 <topangel777@gmail.com>
2025-05-08 09:44:48 +0900

    3342. Find Minimum Time to Reach Last Room II

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,38 @@ impl Solution {
         let n = move_time.len();
         let m = move_time[0].len();
         let inf = i32::MAX / 2;
-        let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
         let mut d = vec![vec![inf; m]; n];
-        let mut visit = vec![vec![false; m]; n];
-        let n = n as i32;
-        let m = m as i32;
+        let mut v = vec![vec![false; m]; n];
+        let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+        let last_n = n - 1;
+        let last_m = m - 1;
+        let n = n as isize;
+        let m = m as isize;
 
         d[0][0] = 0;
         let mut queue =
             std::collections::BinaryHeap::<std::cmp::Reverse<(i32, usize, usize)>>::new();
         queue.push(std::cmp::Reverse((0, 0, 0)));
 
-        while let Some(std::cmp::Reverse((_dir, x, y))) = queue.pop() {
-            if visit[x][y] {
+        while let Some(std::cmp::Reverse((_dist, x, y))) = queue.pop() {
+            if v[x][y] {
                 continue;
+            } else if (x == last_n) && (y == last_m) {
+                break;
             }
 
-            visit[x][y] = true;
+            v[x][y] = true;
             for (dx, dy) in dirs.iter().cloned() {
-                let nx = (x as i32) + dx;
-                let ny = (y as i32) + dy;
+                let nx = x as isize + dx;
+                let ny = y as isize + dy;
                 if (nx < 0) || (nx >= n) || (ny < 0) || (ny >= m) {
                     continue;
                 }
 
                 let nx = nx as usize;
                 let ny = ny as usize;
-                let dist = d[x][y].max(move_time[nx][ny]) + 1;
+                let dist = d[x][y].max(move_time[nx][ny]) + ((x + y) % 2) as i32 + 1;
                 if d[nx][ny] > dist {
                     d[nx][ny] = dist;
                     queue.push(std::cmp::Reverse((dist, nx, ny)));
@@ -53,7 +58,7 @@ fn main() {
             move_time: vec![vec![0, 4], vec![4, 4]],
         },
         Input {
-            move_time: vec![vec![0, 0, 0], vec![0, 0, 0]],
+            move_time: vec![vec![0, 0, 0, 0], vec![0, 0, 0, 0]],
         },
         Input {
             move_time: vec![vec![0, 1], vec![1, 2]],
