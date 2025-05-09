92272aafd0505fbc399a1c6e496f00a84d9b8ba3
mspark777 <topangel777@gmail.com>
2025-05-09 21:23:36 +0900

    3343. Count Number of Balanced Permutations

--- src/go/main.go
+++ src/go/main.go
@@ -2,47 +2,76 @@ package main
 
 import "fmt"
 
-func max(a, b int64) int64 {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func maximumTripletValue(nums []int) int64 {
-	result := int64(0)
-	imax := int64(0)
-	dmax := int64(0)
-
-	for _, num := range nums {
-		num := int64(num)
-		result = max(result, dmax*num)
-		dmax = max(dmax, imax-num)
-		imax = max(imax, num)
+func countBalancedPermutations(num string) int {
+	const MOD = 1_000_000_007
+	tot := 0
+	n := len(num)
+	cnt := make([]int, 10)
+	for _, ch := range num {
+		d := int(ch - '0')
+		cnt[d] += 1
+		tot += d
 	}
 
-	return result
+	if (tot & 1) == 1 {
+		return 0
+	}
+
+	target := tot / 2
+	maxOdd := (n + 1) / 2
+	comb := make([][]int, maxOdd+1)
+
+	for i := range comb {
+		comb[i] = make([]int, maxOdd+1)
+		comb[i][i] = 1
+		comb[i][0] = 1
+		for j := 1; j < i; j += 1 {
+			comb[i][j] = (comb[i-1][j] + comb[i-1][j-1]) % MOD
+		}
+	}
+	f := make([][]int, target+1)
+	for i := range f {
+		f[i] = make([]int, maxOdd+1)
+	}
+	f[0][0] = 1
+	psum, totSum := 0, 0
+	for i := 0; i <= 9; i += 1 {
+		psum += cnt[i]
+		totSum += i * cnt[i]
+		for oddCnt := min(psum, maxOdd); oddCnt >= max(0, psum-(n-maxOdd)); oddCnt -= 1 {
+			evenCnt := psum - oddCnt
+			for curr := min(totSum, target); curr >= max(0, totSum-target); curr -= 1 {
+				res := 0
+				for j := max(0, cnt[i]-evenCnt); j <= min(cnt[i], oddCnt) && i*j <= curr; j += 1 {
+					ways := comb[oddCnt][j] * comb[evenCnt][cnt[i]-j] % MOD
+					res = (res + ways*f[curr-i*j][oddCnt-j]%MOD) % MOD
+				}
+				f[curr][oddCnt] = res % MOD
+			}
+		}
+	}
+	return f[target][maxOdd]
 }
 
 type input struct {
-	nums []int
+	num string
 }
 
 func main() {
 	inputs := []input{
 		{
-			nums: []int{12, 6, 1, 2, 7},
+			num: "123",
 		},
 		{
-			nums: []int{1, 10, 3, 4, 19},
+			num: "112",
 		},
 		{
-			nums: []int{1, 2, 3},
+			num: "12345",
 		},
 	}
 
 	for _, input := range inputs {
-		result := maximumTripletValue(input.nums)
+		result := countBalancedPermutations(input.num)
 		fmt.Println(result)
 	}
 }
