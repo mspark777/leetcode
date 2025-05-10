e6ac1a98c385786a7f4ff2aaa6811766bd0c82c2
mspark777 <topangel777@gmail.com>
2025-05-10 10:36:11 +0900

    2918. Minimum Equal Sum of Two Arrays After Replacing Zeros

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,55 @@
 struct Solution {}
 
 impl Solution {
-    pub fn min_time_to_reach(move_time: Vec<Vec<i32>>) -> i32 {
-        let n = move_time.len();
-        let m = move_time[0].len();
-        let inf = i32::MAX / 2;
-        let mut d = vec![vec![inf; m]; n];
-        let mut v = vec![vec![false; m]; n];
-        let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    pub fn min_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i64 {
+        let mut sum1 = 0i64;
+        let mut sum2 = 0i64;
+        let mut zero1 = 0;
+        let mut zero2 = 0;
 
-        let last_n = n - 1;
-        let last_m = m - 1;
-        let n = n as isize;
-        let m = m as isize;
-
-        d[0][0] = 0;
-        let mut queue =
-            std::collections::BinaryHeap::<std::cmp::Reverse<(i32, usize, usize)>>::new();
-        queue.push(std::cmp::Reverse((0, 0, 0)));
-
-        while let Some(std::cmp::Reverse((_dist, x, y))) = queue.pop() {
-            if v[x][y] {
-                continue;
-            } else if (x == last_n) && (y == last_m) {
-                break;
-            }
-
-            v[x][y] = true;
-            for (dx, dy) in dirs.iter().cloned() {
-                let nx = x as isize + dx;
-                let ny = y as isize + dy;
-                if (nx < 0) || (nx >= n) || (ny < 0) || (ny >= m) {
-                    continue;
-                }
-
-                let nx = nx as usize;
-                let ny = ny as usize;
-                let dist = d[x][y].max(move_time[nx][ny]) + ((x + y) % 2) as i32 + 1;
-                if d[nx][ny] > dist {
-                    d[nx][ny] = dist;
-                    queue.push(std::cmp::Reverse((dist, nx, ny)));
-                }
+        for n in nums1.iter().cloned() {
+            sum1 += n as i64;
+            if n == 0 {
+                sum1 += 1;
+                zero1 += 1;
             }
         }
 
-        return *d.last().unwrap().last().unwrap();
+        for n in nums2.iter().cloned() {
+            sum2 += n as i64;
+            if n == 0 {
+                sum2 += 1;
+                zero2 += 1;
+            }
+        }
+
+        if ((zero1 == 0) && (sum2 > sum1)) || ((zero2 == 0) && (sum1 > sum2)) {
+            return -1;
+        }
+
+        return sum1.max(sum2);
     }
 }
 
 struct Input {
-    move_time: Vec<Vec<i32>>,
+    nums1: Vec<i32>,
+    nums2: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            move_time: vec![vec![0, 4], vec![4, 4]],
+            nums1: vec![3, 2, 0, 1, 0],
+            nums2: vec![6, 5, 0],
         },
         Input {
-            move_time: vec![vec![0, 0, 0, 0], vec![0, 0, 0, 0]],
-        },
-        Input {
-            move_time: vec![vec![0, 1], vec![1, 2]],
+            nums1: vec![2, 0, 2, 0],
+            nums2: vec![1, 4],
         },
     ];
 
     for input in inputs {
-        let result = Solution::min_time_to_reach(input.move_time);
+        let result = Solution::min_sum(input.nums1, input.nums2);
         println!("{result:?}");
     }
 }
