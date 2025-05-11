45896f30e3c8b7441bb59e9c6e675f8ee8f2d2dc
mspark777 <topangel777@gmail.com>
2025-05-11 10:42:12 +0900

    1550. Three Consecutive Odds

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,39 @@
 struct Solution {}
 
 impl Solution {
-    pub fn min_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i64 {
-        let mut sum1 = 0i64;
-        let mut sum2 = 0i64;
-        let mut zero1 = 0;
-        let mut zero2 = 0;
-
-        for n in nums1.iter().cloned() {
-            sum1 += n as i64;
-            if n == 0 {
-                sum1 += 1;
-                zero1 += 1;
+    pub fn three_consecutive_odds(arr: Vec<i32>) -> bool {
+        let mut count = 0;
+        for num in arr.iter().cloned() {
+            if (num & 1) == 1 {
+                count += 1;
+                if count == 3 {
+                    return true;
+                }
+            } else {
+                count = 0;
             }
         }
 
-        for n in nums2.iter().cloned() {
-            sum2 += n as i64;
-            if n == 0 {
-                sum2 += 1;
-                zero2 += 1;
-            }
-        }
-
-        if ((zero1 == 0) && (sum2 > sum1)) || ((zero2 == 0) && (sum1 > sum2)) {
-            return -1;
-        }
-
-        return sum1.max(sum2);
+        return false;
     }
 }
 
 struct Input {
-    nums1: Vec<i32>,
-    nums2: Vec<i32>,
+    arr: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            nums1: vec![3, 2, 0, 1, 0],
-            nums2: vec![6, 5, 0],
+            arr: vec![2, 6, 4, 1],
         },
         Input {
-            nums1: vec![2, 0, 2, 0],
-            nums2: vec![1, 4],
+            arr: vec![1, 2, 34, 3, 4, 5, 7, 23, 12],
         },
     ];
 
     for input in inputs {
-        let result = Solution::min_sum(input.nums1, input.nums2);
+        let result = Solution::three_consecutive_odds(input.arr);
         println!("{result:?}");
     }
 }
