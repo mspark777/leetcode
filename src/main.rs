188c1a5c47f9c1c9587e418d8db1a7cc5b23cd86
mspark777 <topangel777@gmail.com>
2025-05-14 14:48:36 +0900

    3337. Total Characters in String After Transformations II

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,105 @@
 struct Solution {}
 
+const MOD: i64 = 1_000_000_007;
+const L: usize = 26;
+
+#[derive(Clone)]
+struct Mat {
+    a: [[i64; L]; L],
+}
+
+impl Mat {
+    fn new() -> Self {
+        return Self { a: [[0; L]; L] };
+    }
+
+    fn identity() -> Self {
+        let mut m = Self::new();
+        for i in 0..L {
+            m.a[i][i] = 1;
+        }
+        return m;
+    }
+}
+
+fn mul(left: &Mat, right: &Mat) -> Mat {
+    let mut result = Mat::new();
+    for i in 0..L {
+        for j in 0..L {
+            for k in 0..L {
+                result.a[i][j] = (result.a[i][j] + left.a[i][k] * right.a[k][j]) % MOD;
+            }
+        }
+    }
+    return result;
+}
+
+fn quickmul(x: &Mat, mut y: i32) -> Mat {
+    let mut ans = Mat::identity();
+    let mut cur = x.clone();
+    while y > 0 {
+        if y & 1 == 1 {
+            ans = mul(&ans, &cur);
+        }
+        cur = mul(&cur, &cur);
+        y >>= 1;
+    }
+    return ans;
+}
+
 impl Solution {
-    pub fn three_consecutive_odds(arr: Vec<i32>) -> bool {
-        let mut count = 0;
-        for num in arr.iter().cloned() {
-            if (num & 1) == 1 {
-                count += 1;
-                if count == 3 {
-                    return true;
-                }
-            } else {
-                count = 0;
+    pub fn length_after_transformations(s: String, t: i32, nums: Vec<i32>) -> i32 {
+        let mut mat = Mat::new();
+        for i in 0..L {
+            for j in 1..=nums[i] {
+                let j = j as usize;
+                mat.a[(i + j) % L][i] = 1;
             }
         }
 
-        return false;
+        let res = quickmul(&mat, t);
+        let mut f = [0; L];
+        for ch in s.chars() {
+            f[(ch as u8 - b'a') as usize] += 1;
+        }
+        let mut ans: i64 = 0;
+        for i in 0..L {
+            for j in 0..L {
+                ans = (ans + res.a[i][j] * f[j]) % MOD;
+            }
+        }
+
+        return ans as i32;
     }
 }
 
 struct Input {
-    arr: Vec<i32>,
+    s: &'static str,
+    t: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            arr: vec![2, 6, 4, 1],
+            s: "abcyy",
+            t: 2,
+            nums: vec![
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+            ],
         },
         Input {
-            arr: vec![1, 2, 34, 3, 4, 5, 7, 23, 12],
+            s: "azbk",
+            t: 1,
+            nums: vec![
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            ],
         },
     ];
 
     for input in inputs {
-        let result = Solution::three_consecutive_odds(input.arr);
+        let result =
+            Solution::length_after_transformations(input.s.to_string(), input.t, input.nums);
         println!("{result:?}");
     }
 }
