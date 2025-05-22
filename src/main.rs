eb754babdd42d195eac6d4c137fdd35c2a8be06c
mspark777 <topangel777@gmail.com>
2025-05-22 22:28:21 +0900

    3362. Zero Array Transformation III

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,59 @@
 struct Solution {}
 
 impl Solution {
-    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
-        let n = matrix.len();
-        let m = matrix[0].len();
-        let mut fr = false;
-        let mut fc = false;
+    pub fn max_removal(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
+        let mut queries = queries;
+        queries.sort_unstable_by(|a, b| a[0].cmp(&b[0]));
 
-        for r in 0..n {
-            for c in 0..m {
-                if matrix[r][c] != 0 {
-                    continue;
-                }
+        let mut heap = std::collections::BinaryHeap::<i32>::new();
+        let mut delta_array = vec![0; nums.len() + 1];
+        let mut operations = 0;
+        let mut j = 0;
+        for i in 0..nums.len() {
+            operations += delta_array[i];
+            while j < queries.len() && queries[j][0] == i as i32 {
+                heap.push(queries[j][1]);
+                j += 1;
+            }
 
-                fr = if fr { true } else { r == 0 };
-                fc = if fc { true } else { c == 0 };
-                matrix[0][c] = 0;
-                matrix[r][0] = 0;
+            while operations < nums[i] && !heap.is_empty() && *heap.peek().unwrap() >= i as i32 {
+                operations += 1;
+                let end = heap.pop().unwrap() as usize;
+                delta_array[end + 1] -= 1;
+            }
+
+            if operations < nums[i] {
+                return -1;
             }
         }
 
-        for r in 1..n {
-            for c in 1..m {
-                if matrix[r][0] == 0 {
-                    matrix[r][c] = 0;
-                } else if matrix[0][c] == 0 {
-                    matrix[r][c] = 0;
-                }
-            }
-        }
-
-        if fr {
-            for c in 0..m {
-                matrix[0][c] = 0;
-            }
-        }
-
-        if fc {
-            for r in 0..n {
-                matrix[r][0] = 0;
-            }
-        }
+        return heap.len() as i32;
     }
 }
 
 struct Input {
-    matrix: Vec<Vec<i32>>,
+    nums: Vec<i32>,
+    queries: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            matrix: vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]],
+            nums: vec![2, 0, 2],
+            queries: vec![vec![0, 2], vec![0, 2], vec![1, 1]],
         },
         Input {
-            matrix: vec![vec![0, 1, 2, 0], vec![3, 4, 5, 2], vec![1, 3, 1, 5]],
+            nums: vec![1, 1, 1, 1],
+            queries: vec![vec![1, 3], vec![0, 2], vec![1, 3], vec![1, 2]],
+        },
+        Input {
+            nums: vec![1, 2, 3, 4],
+            queries: vec![vec![0, 3]],
         },
     ];
 
     for mut input in inputs {
-        Solution::set_zeroes(&mut input.matrix);
-        println!("{:?}", input.matrix);
+        let result = Solution::max_removal(input.nums, input.queries);
+        println!("{:?}", result);
     }
 }
