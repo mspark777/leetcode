d38f78a9b68b14c6235f0ef4332c231a248eeb67
mspark777 <topangel777@gmail.com>
2025-05-25 22:57:14 +0900

    2131. Longest Palindrome by Concatenating Two Letter Words

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,63 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
-        let mut result = Vec::<i32>::with_capacity(words.len());
-        for (i, word) in words.iter().enumerate() {
-            if word.contains(x) {
-                result.push(i as i32);
+    pub fn longest_palindrome(words: Vec<String>) -> i32 {
+        let mut counts = std::collections::HashMap::<&str, i32>::with_capacity(words.len());
+        for word in words.iter() {
+            let w = word.as_str();
+            counts.entry(w).and_modify(|c| *c += 1).or_insert(1);
+        }
+
+        let mut result = 0;
+        let mut central = false;
+
+        for (&word, count) in counts.iter() {
+            let bytes = word.as_bytes();
+            let first = bytes[0];
+            let second = bytes[1];
+            if first == second {
+                if (count & 1) == 0 {
+                    result += count;
+                } else {
+                    result += count - 1;
+                    central = true;
+                }
+            } else if first < second {
+                let rword = String::from_utf8(vec![second, first]).unwrap();
+                if let Some(rcount) = counts.get(rword.as_str()) {
+                    result += 2 * count.min(rcount);
+                }
             }
         }
 
-        return result;
+        if central {
+            result += 1;
+        }
+
+        return result * 2;
     }
 }
 
 struct Input {
     words: Vec<&'static str>,
-    x: char,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            words: ["leet", "code"].to_vec(),
-            x: 'e',
+            words: ["lc", "cl", "gg"].to_vec(),
         },
         Input {
-            words: ["abc", "bcd", "aaaa", "cbc"].to_vec(),
-            x: 'a',
+            words: ["ab", "ty", "yt", "lc", "cl", "ab"].to_vec(),
         },
         Input {
-            words: ["abc", "bcd", "aaaa", "cbc"].to_vec(),
-            x: 'z',
+            words: ["cc", "ll", "xx"].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_words_containing(
-            input.words.iter().map(|s| s.to_string()).collect(),
-            input.x,
-        );
+        let result =
+            Solution::longest_palindrome(input.words.iter().map(|s| s.to_string()).collect());
         println!("{:?}", result);
     }
 }
