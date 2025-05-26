6b504551b36a610e51a2930c933ce4d4d1455068
mspark777 <topangel777@gmail.com>
2025-05-27 01:04:39 +0900

    1857. Largest Color Value in a Directed Graph

--- src/py/main.py
+++ src/py/main.py
@@ -1,36 +1,58 @@
+from collections import deque
+
+
 class Solution:
-    def lengthAfterTransformations(self, s: str, t: int) -> int:
-        mod = 1000000007
-        counts = [0] * 26
-        for ch in s:
-            counts[ord(ch) - ord("a")] += 1
+    def largestPathValue(self, colors: str, edges: list[list[int]]) -> int:
+        n = len(colors)
+        graph: list[list[int]] = [[] for _ in range(n)]
+        indegree = [0] * n
+        for u, v in edges:
+            graph[u].append(v)
+            indegree[v] += 1
 
-        for _ in range(t):
-            next = [0] * 26
-            next[0] = counts[25]
-            next[1] = (counts[25] + counts[0]) % mod
-            for i in range(2, 26):
-                next[i] = counts[i - 1]
-            counts = next
+        dp = [[0] * 26 for _ in range(n)]
+        queue = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+                dp[i][ord(colors[i]) - ord("a")] = 1
 
-        result = sum(counts) % mod
-        return result
+        visited = 0
+        max_color_value = 0
+
+        while queue:
+            u = queue.popleft()
+            visited += 1
+
+            for v in graph[u]:
+                for c in range(26):
+                    dp[v][c] = max(
+                        dp[v][c],
+                        dp[u][c] + (1 if c == ord(colors[v]) - ord("a") else 0),
+                    )
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    queue.append(v)
+
+            max_color_value = max(max_color_value, max(dp[u]))
+
+        return max_color_value if visited == n else -1
 
 
 class Input:
-    s: str
-    t: int
+    colors: str
+    edges: list[list[int]]
 
-    def __init__(self, s: str, t: int):
-        self.s = s
-        self.t = t
+    def __init__(self, colors: str, edges: list[list[int]]):
+        self.colors = colors
+        self.edges = edges
 
 
 def main():
-    inputs = [Input("abcyy", 2), Input("azbk", 1)]
+    inputs = [Input("abaca", [[0, 1], [0, 2], [2, 3], [3, 4]]), Input("a", [[0, 0]])]
 
     for input in inputs:
-        result = Solution().lengthAfterTransformations(input.s, input.t)
+        result = Solution().largestPathValue(input.colors, input.edges)
         print(result)
 
 
