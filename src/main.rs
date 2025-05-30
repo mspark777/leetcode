466f757f03c6ecf4070f74a59690bfec0f684558
mspark777 <topangel777@gmail.com>
2025-05-30 16:25:53 +0900

    2359. Find Closest Node to Given Two Nodes

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,64 @@
 struct Solution {}
 
 impl Solution {
-    fn build(edges: &Vec<Vec<i32>>, color: &mut Vec<usize>) -> Vec<i32> {
-        let n = edges.len() + 1;
-        let mut children = vec![vec![]; n];
-        for edge in edges {
-            let u = edge[0] as usize;
-            let v = edge[1] as usize;
-            children[u].push(v as i32);
-            children[v].push(u as i32);
+    fn dfs(node: i32, edges: &Vec<i32>, dist: &mut Vec<i32>, visit: &mut Vec<bool>) {
+        let node = node as usize;
+        visit[node] = true;
+        let neighbor = edges[node];
+        if (neighbor != -1) && !visit[neighbor as usize] {
+            dist[neighbor as usize] = 1 + dist[node];
+            Self::dfs(neighbor, edges, dist, visit);
         }
-
-        let res = Self::dfs(0, -1, 0, &children, color);
-        return vec![res, (n as i32) - res];
     }
 
-    fn dfs(
-        node: usize,
-        parent: i32,
-        depth: usize,
-        children: &Vec<Vec<i32>>,
-        color: &mut Vec<usize>,
-    ) -> i32 {
-        let mut res = 1 - (depth % 2) as i32;
-        color[node] = depth % 2;
+    pub fn closest_meeting_node(edges: Vec<i32>, node1: i32, node2: i32) -> i32 {
+        let n = edges.len();
+        let mut dist1 = vec![i32::MAX; n];
+        let mut dist2 = vec![i32::MAX; n];
+        let mut visit1 = vec![false; n];
+        let mut visit2 = vec![false; n];
 
-        for &child in &children[node] {
-            if child == parent {
-                continue;
+        dist1[node1 as usize] = 0;
+        dist2[node2 as usize] = 0;
+        Self::dfs(node1, &edges, &mut dist1, &mut visit1);
+        Self::dfs(node2, &edges, &mut dist2, &mut visit2);
+
+        let mut min_dist_node = -1;
+        let mut min_dist_till_now = i32::MAX;
+
+        for curr_node in 0..n {
+            if min_dist_till_now > dist1[curr_node].max(dist2[curr_node]) {
+                min_dist_node = curr_node as i32;
+                min_dist_till_now = dist1[curr_node].max(dist2[curr_node]);
             }
-
-            res += Self::dfs(child as usize, node as i32, depth + 1, children, color);
         }
 
-        return res;
-    }
-
-    pub fn max_target_nodes(edges1: Vec<Vec<i32>>, edges2: Vec<Vec<i32>>) -> Vec<i32> {
-        let n = edges1.len() + 1;
-        let m = edges2.len() + 1;
-        let mut color1 = vec![0; n];
-        let mut color2 = vec![0; m];
-        let count1 = Self::build(&edges1, &mut color1);
-        let count2 = Self::build(&edges2, &mut color2);
-        let mut res = vec![0; n];
-        for i in 0..n {
-            res[i] = count1[color1[i]] + count2[0].max(count2[1]);
-        }
-
-        return res;
+        return min_dist_node;
     }
 }
 
 struct Input {
-    edges1: Vec<[i32; 2]>,
-    edges2: Vec<[i32; 2]>,
+    edges: Vec<i32>,
+    node1: i32,
+    node2: i32,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            edges1: vec![[0, 1], [0, 2], [2, 3], [2, 4]],
-            edges2: vec![[0, 1], [0, 2], [0, 3], [2, 7], [1, 4], [4, 5], [4, 6]],
+            edges: vec![2, 2, 3, -1],
+            node1: 0,
+            node2: 1,
         },
         Input {
-            edges1: vec![[0, 1], [0, 2], [0, 3], [0, 4]],
-            edges2: vec![[0, 1], [1, 2], [2, 3]],
+            edges: vec![1, 2, -1],
+            node1: 0,
+            node2: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::max_target_nodes(
-            input.edges1.iter().map(|e| e.to_vec()).collect(),
-            input.edges2.iter().map(|e| e.to_vec()).collect(),
-        );
+        let result = Solution::closest_meeting_node(input.edges, input.node1, input.node2);
         println!("{:?}", result);
     }
 }
