eefad80545e34c811bf98fdb75b7a39e0583081e
mspark777 <topangel777@gmail.com>
2025-06-01 19:25:56 +0900

    2929. Distribute Candies Among Children II

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,32 @@
 struct Solution {}
 
 impl Solution {
-    pub fn snakes_and_ladders(board: Vec<Vec<i32>>) -> i32 {
-        let n = board.len();
-        let mut min_rolls = vec![-1; n * n + 1];
-        let mut queue = std::collections::VecDeque::<i32>::new();
+    pub fn distribute_candies(n: i32, limit: i32) -> i64 {
+        return Self::cal(n + 2) - 3 * Self::cal(n - limit + 1)
+            + 3 * Self::cal(n - (limit + 1) * 2 + 2)
+            - Self::cal(n - 3 * (limit + 1) + 2);
+    }
 
-        min_rolls[1] = 0;
-        queue.push_back(1);
-
-        while let Some(x) = queue.pop_front() {
-            for i in 1..=6 {
-                let n = n as i32;
-                if (x + i) > (n * n) {
-                    break;
-                }
-
-                let t = x + i;
-                let row = (t - 1) / n;
-                let col = (t - 1) % n;
-                let v = if row & 1 == 1 {
-                    board[(n - 1 - row) as usize][(n - 1 - col) as usize]
-                } else {
-                    board[(n - 1 - row) as usize][col as usize]
-                };
-
-                let y = if v > 0 { v } else { t };
-
-                if y == n * n {
-                    return min_rolls[x as usize] + 1;
-                }
-
-                if min_rolls[y as usize] == -1 {
-                    min_rolls[y as usize] = min_rolls[x as usize] + 1;
-                    queue.push_back(y);
-                }
-            }
+    fn cal(x: i32) -> i64 {
+        if x < 0 {
+            return 0;
         }
 
-        return -1;
+        let x = x as i64;
+        return x * (x - 1) / 2;
     }
 }
 
 struct Input {
-    board: Vec<Vec<i32>>,
+    n: i32,
+    limit: i32,
 }
 
 fn main() {
-    let inputs = vec![
-        Input {
-            board: vec![
-                vec![-1, -1, -1, -1, -1, -1],
-                vec![-1, -1, -1, -1, -1, -1],
-                vec![-1, -1, -1, -1, -1, -1],
-                vec![-1, 35, -1, -1, 13, -1],
-                vec![-1, -1, -1, -1, -1, -1],
-                vec![-1, 15, -1, -1, -1, -1],
-            ],
-        },
-        Input {
-            board: vec![vec![-1, -1], vec![-1, 3]],
-        },
-        Input {
-            board: vec![
-                vec![-1, 1, 2, -1],
-                vec![2, 13, 15, -1],
-                vec![-1, 10, -1, -1],
-                vec![-1, 6, 2, 8],
-            ],
-        },
-        Input {
-            board: vec![
-                vec![-1, -1, 27, 13, -1, 25, -1],
-                vec![-1, -1, -1, -1, -1, -1, -1],
-                vec![44, -1, 8, -1, -1, 2, -1],
-                vec![-1, 30, -1, -1, -1, -1, -1],
-                vec![3, -1, 20, -1, 46, 6, -1],
-                vec![-1, -1, -1, -1, -1, -1, 29],
-                vec![-1, 29, 21, 33, -1, -1, -1],
-            ],
-        },
-    ];
+    let inputs = vec![Input { n: 5, limit: 2 }, Input { n: 3, limit: 3 }];
 
     for input in inputs {
-        let result = Solution::snakes_and_ladders(input.board);
+        let result = Solution::distribute_candies(input.n, input.limit);
         println!("{:?}", result);
     }
 }
