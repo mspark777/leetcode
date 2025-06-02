9f192faa4865577a29e2e02d05f7623c624058d2
mspark777 <topangel777@gmail.com>
2025-06-02 11:46:52 +0900

    135. Candy

--- src/go/main.go
+++ src/go/main.go
@@ -2,76 +2,68 @@ package main
 
 import "fmt"
 
-func countBalancedPermutations(num string) int {
-	const MOD = 1_000_000_007
-	tot := 0
-	n := len(num)
-	cnt := make([]int, 10)
-	for _, ch := range num {
-		d := int(ch - '0')
-		cnt[d] += 1
-		tot += d
+func count(n int) int {
+	return (n * (n + 1)) / 2
+}
+
+func candy(ratings []int) int {
+	ratings_len := len(ratings)
+	if ratings_len < 2 {
+		return ratings_len
 	}
 
-	if (tot & 1) == 1 {
-		return 0
-	}
+	candies := 0
+	up := 0
+	down := 0
+	old_slope := 0
 
-	target := tot / 2
-	maxOdd := (n + 1) / 2
-	comb := make([][]int, maxOdd+1)
-
-	for i := range comb {
-		comb[i] = make([]int, maxOdd+1)
-		comb[i][i] = 1
-		comb[i][0] = 1
-		for j := 1; j < i; j += 1 {
-			comb[i][j] = (comb[i-1][j] + comb[i-1][j-1]) % MOD
+	for i := 1; i < ratings_len; i += 1 {
+		cur := ratings[i]
+		prev := ratings[i-1]
+		slope := 0
+		if cur > prev {
+			slope = 1
+		} else if cur < prev {
+			slope = -1
 		}
-	}
-	f := make([][]int, target+1)
-	for i := range f {
-		f[i] = make([]int, maxOdd+1)
-	}
-	f[0][0] = 1
-	psum, totSum := 0, 0
-	for i := 0; i <= 9; i += 1 {
-		psum += cnt[i]
-		totSum += i * cnt[i]
-		for oddCnt := min(psum, maxOdd); oddCnt >= max(0, psum-(n-maxOdd)); oddCnt -= 1 {
-			evenCnt := psum - oddCnt
-			for curr := min(totSum, target); curr >= max(0, totSum-target); curr -= 1 {
-				res := 0
-				for j := max(0, cnt[i]-evenCnt); j <= min(cnt[i], oddCnt) && i*j <= curr; j += 1 {
-					ways := comb[oddCnt][j] * comb[evenCnt][cnt[i]-j] % MOD
-					res = (res + ways*f[curr-i*j][oddCnt-j]%MOD) % MOD
-				}
-				f[curr][oddCnt] = res % MOD
-			}
+
+		if ((old_slope > 0) && (slope == 0)) || ((old_slope < 0) && (slope >= 0)) {
+			candies += count(up) + count(down) + max(up, down)
+			up = 0
+			down = 0
 		}
+
+		if slope > 0 {
+			up += 1
+		} else if slope < 0 {
+			down += 1
+		} else {
+			candies += 1
+		}
+
+		old_slope = slope
 	}
-	return f[target][maxOdd]
+
+	candies += count(up) + count(down) + max(up, down) + 1
+	return candies
 }
 
 type input struct {
-	num string
+	ratings []int
 }
 
 func main() {
 	inputs := []input{
 		{
-			num: "123",
+			ratings: []int{1, 0, 2},
 		},
 		{
-			num: "112",
-		},
-		{
-			num: "12345",
+			ratings: []int{1, 2, 2},
 		},
 	}
 
 	for _, input := range inputs {
-		result := countBalancedPermutations(input.num)
+		result := candy(input.ratings)
 		fmt.Println(result)
 	}
 }
