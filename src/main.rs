f468dfe92b2f89f4da60f7317c602fc1a27cce08
mspark777 <topangel777@gmail.com>
2025-06-09 22:05:12 +0900

    440. K-th Smallest in Lexicographical Order

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,57 @@
 struct Solution {}
 
 impl Solution {
-    pub fn lexical_order(n: i32) -> Vec<i32> {
-        let mut result = vec![0i32; n as usize];
-        let mut cur = 1i32;
-        let mut i = 0usize;
+    pub fn find_kth_number(n: i32, k: i32) -> i32 {
+        let n = n as i64;
+        let mut curr = 1i64;
+        let mut k = k as i64;
+        k -= 1;
 
-        while i < (n as usize) {
-            result[i] = cur;
-
-            if (cur * 10) <= n {
-                cur *= 10;
-                i += 1;
-                continue;
+        while k > 0 {
+            let steps = Self::count_steps(n, curr, curr + 1);
+            if steps <= k {
+                curr += 1;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k -= 1;
             }
-
-            while ((cur % 10) == 9) || (cur >= n) {
-                cur /= 10;
-            }
-
-            cur += 1;
-            i += 1;
         }
 
-        return result;
+        return curr as i32;
+    }
+
+    fn count_steps(n: i64, prefix1: i64, prefix2: i64) -> i64 {
+        let mut prefix1 = prefix1;
+        let mut prefix2 = prefix2;
+        let mut steps = 0i64;
+        while prefix1 <= n {
+            steps += (n + 1).min(prefix2) - prefix1;
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+
+        return steps;
     }
 }
 
 struct Input {
     n: i32,
+    k: i32,
 }
 
 fn main() {
-    let inputs = vec![Input { n: 13 }, Input { n: 2 }];
+    let inputs = vec![
+        Input { n: 13, k: 2 },
+        Input { n: 1, k: 1 },
+        Input {
+            n: 681692778,
+            k: 351251360,
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::lexical_order(input.n);
+        let result = Solution::find_kth_number(input.n, input.k);
         println!("{:?}", result);
     }
 }
