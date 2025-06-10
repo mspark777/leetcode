a605dbdc4534a1fcb0ef704afe15a1f3cbd98aae
mspark777 <topangel777@gmail.com>
2025-06-10 10:01:23 +0900

    3442. Maximum Difference Between Even and Odd Frequency I

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,36 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_kth_number(n: i32, k: i32) -> i32 {
-        let n = n as i64;
-        let mut curr = 1i64;
-        let mut k = k as i64;
-        k -= 1;
+    pub fn max_difference(s: String) -> i32 {
+        let mut counts = std::collections::HashMap::<char, i32>::with_capacity(26);
+        let mut min_even = 0;
+        for ch in s.chars() {
+            counts.entry(ch).and_modify(|c| *c += 1).or_insert(1);
+            min_even += 1;
+        }
 
-        while k > 0 {
-            let steps = Self::count_steps(n, curr, curr + 1);
-            if steps <= k {
-                curr += 1;
-                k -= steps;
+        let mut max_odd = 1;
+        for count in counts.values().cloned() {
+            if count & 1 == 1 {
+                max_odd = max_odd.max(count);
             } else {
-                curr *= 10;
-                k -= 1;
+                min_even = min_even.min(count);
             }
         }
 
-        return curr as i32;
-    }
-
-    fn count_steps(n: i64, prefix1: i64, prefix2: i64) -> i64 {
-        let mut prefix1 = prefix1;
-        let mut prefix2 = prefix2;
-        let mut steps = 0i64;
-        while prefix1 <= n {
-            steps += (n + 1).min(prefix2) - prefix1;
-            prefix1 *= 10;
-            prefix2 *= 10;
-        }
-
-        return steps;
+        return max_odd - min_even;
     }
 }
 
 struct Input {
-    n: i32,
-    k: i32,
+    s: &'static str,
 }
 
 fn main() {
-    let inputs = vec![
-        Input { n: 13, k: 2 },
-        Input { n: 1, k: 1 },
-        Input {
-            n: 681692778,
-            k: 351251360,
-        },
-    ];
+    let inputs = vec![Input { s: "aaaaabbc" }, Input { s: "abcabcab" }];
 
     for input in inputs {
-        let result = Solution::find_kth_number(input.n, input.k);
+        let result = Solution::max_difference(input.s.to_string());
         println!("{:?}", result);
     }
 }
