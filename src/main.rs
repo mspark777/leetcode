e688767633ebae019ff94f87db3e9d6c877d7af4
mspark777 <topangel777@gmail.com>
2025-06-14 11:59:38 +0900

    2566. Maximum Difference by Remapping a Digit

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,33 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimize_max(nums: Vec<i32>, p: i32) -> i32 {
-        let mut nums = nums;
-        nums.sort();
-        let n = nums.len();
-        let mut left = 0;
-        let mut right = nums[n - 1] - nums[0];
-        while left < right {
-            let mid = (left + right) / 2;
-            if Self::count_valid_pairs(&nums, mid) >= p {
-                right = mid;
+    pub fn min_max_difference(num: i32) -> i32 {
+        let s = num.to_string();
+        let mut max_num = num;
+        for ch in s.chars() {
+            if ch == '9' {
+                continue;
             } else {
-                left = mid + 1;
+                max_num = s.replace(ch, "9").parse().unwrap();
+                break;
             }
         }
 
-        return left;
-    }
-
-    fn count_valid_pairs(nums: &Vec<i32>, threshold: i32) -> i32 {
-        let mut idx = 0usize;
-        let mut count = 0;
-        let n = nums.len();
-
-        while idx < (n - 1) {
-            let l = nums[idx];
-            let r = nums[idx + 1];
-            if (r - l) <= threshold {
-                count += 1;
-                idx += 1;
-            }
-            idx += 1;
-        }
-
-        return count;
+        let first = s.chars().next().unwrap();
+        let min_num = s.replace(first, "0").parse::<i32>().unwrap();
+        max_num - min_num
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    p: i32,
+    num: i32,
 }
 
 fn main() {
-    let inputs = vec![
-        Input {
-            nums: vec![10, 1, 2, 7, 1, 3],
-            p: 2,
-        },
-        Input {
-            nums: vec![4, 2, 1, 2],
-            p: 1,
-        },
-    ];
+    let inputs = vec![Input { num: 11891 }, Input { num: 90 }];
 
     for input in inputs {
-        let result = Solution::minimize_max(input.nums, input.p);
+        let result = Solution::min_max_difference(input.num);
         println!("{:?}", result);
     }
 }
