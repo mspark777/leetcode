4b2a88caed352a2862639403b61d5f6c7ff473d1
mspark777 <topangel777@gmail.com>
2025-06-22 14:16:04 +0900

    2138. Divide a String Into Groups of Size k

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,47 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_deletions(word: String, k: i32) -> i32 {
-        let mut counts = vec![0; 26];
-        let mut result = 0;
-        for ch in word.chars() {
-            result += 1;
-            let idx = ((ch as u8) - b'a') as usize;
-            counts[idx] += 1;
-        }
-
-        for a in counts.iter().cloned() {
-            if a < 1 {
-                continue;
-            }
-
-            let mut deleted = 0;
-            for b in counts.iter().cloned() {
-                if b < 1 {
-                    continue;
+    pub fn divide_string(s: String, k: i32, fill: char) -> Vec<String> {
+        let k = k as usize;
+        s.chars()
+            .collect::<Vec<char>>()
+            .chunks(k)
+            .map(|chunk| {
+                if chunk.len() >= k {
+                    chunk.iter().collect()
+                } else {
+                    [chunk.to_vec(), vec![fill; k - chunk.len()]]
+                        .concat()
+                        .iter()
+                        .collect()
                 }
-
-                if a > b {
-                    deleted += b;
-                } else if b > (a + k) {
-                    deleted += b - (a + k);
-                }
-            }
-
-            result = result.min(deleted);
-        }
-
-        result
+            })
+            .collect()
     }
 }
 
 struct Input {
-    word: &'static str,
+    s: &'static str,
     k: i32,
+    fill: char,
 }
 
 fn main() {
     let inputs = vec![
         Input {
-            word: "aabcaba",
-            k: 0,
+            s: "abcdefghi",
+            k: 3,
+            fill: 'x',
         },
         Input {
-            word: "dabdcbdcdcd",
-            k: 2,
-        },
-        Input {
-            word: "aaabaaa",
-            k: 2,
+            s: "abcdefghij",
+            k: 3,
+            fill: 'x',
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_deletions(input.word.to_string(), input.k);
+        let result = Solution::divide_string(input.s.to_string(), input.k, input.fill);
         println!("{:?}", result);
     }
 }
