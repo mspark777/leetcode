967412a63ef6e5dc2dfea360b61ad29affe6024b
mspark777 <topangel777@gmail.com>
2025-07-10 09:39:26 +0900

    3440. Reschedule Meetings for Maximum Free Time II

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 struct Solution {}
 
 impl Solution {
-    pub fn max_free_time(event_time: i32, k: i32, start_time: Vec<i32>, end_time: Vec<i32>) -> i32 {
+    pub fn max_free_time(event_time: i32, start_time: Vec<i32>, end_time: Vec<i32>) -> i32 {
         let n = start_time.len();
-        let k = k as usize;
-        let mut result = 0;
-        let mut t = 0;
+        let mut q = vec![false; n];
+        let mut t1 = 0;
+        let mut t2 = 0;
 
         for i in 0..n {
-            t += end_time[i] - start_time[i];
-            let left = if i <= k - 1 { 0 } else { end_time[i - k] };
+            if end_time[i] - start_time[i] <= t1 {
+                q[i] = true;
+            }
+
+            if i == 0 {
+                t1 = t1.max(start_time[i]);
+            } else {
+                t1 = t1.max(start_time[i] - end_time[i - 1]);
+            }
+
+            let j = n - (i + 1);
+            if end_time[j] - start_time[j] <= t2 {
+                q[j] = true;
+            }
+
+            if i == 0 {
+                t2 = t2.max(event_time - end_time[j]);
+            } else {
+                t2 = t2.max(start_time[n - i] - end_time[j]);
+            }
+        }
+
+        let mut result = 0;
+        for i in 0..n {
+            let left = if i == 0 { 0 } else { end_time[i - 1] };
             let right = if i == n - 1 {
                 event_time
             } else {
                 start_time[i + 1]
             };
 
-            result = result.max(right - left - t);
-            if i >= k - 1 {
-                let j = (i + 1) - k;
-                t -= end_time[j] - start_time[j];
+            result = if q[i] {
+                result.max(right - left)
+            } else {
+                result.max(right - left - (end_time[i] - start_time[i]))
             }
         }
 
@@ -29,7 +52,6 @@ impl Solution {
 
 struct Input {
     event_time: i32,
-    k: i32,
     start_time: Vec<i32>,
     end_time: Vec<i32>,
 }
@@ -38,27 +60,33 @@ fn main() {
     let inputs = vec![
         Input {
             event_time: 5,
-            k: 1,
             start_time: [1, 3].to_vec(),
-            end_time: [2, 4].to_vec(),
+            end_time: [2, 5].to_vec(),
         },
         Input {
             event_time: 10,
-            k: 1,
-            start_time: [0, 2, 9].to_vec(),
-            end_time: [1, 4, 10].to_vec(),
+            start_time: [0, 7, 9].to_vec(),
+            end_time: [1, 8, 10].to_vec(),
+        },
+        Input {
+            event_time: 10,
+            start_time: [0, 3, 7, 9].to_vec(),
+            end_time: [1, 4, 8, 10].to_vec(),
         },
         Input {
             event_time: 5,
-            k: 2,
             start_time: [0, 1, 2, 3, 4].to_vec(),
             end_time: [1, 2, 3, 4, 5].to_vec(),
+        },
+        Input {
+            event_time: 17,
+            start_time: [6, 9, 16].to_vec(),
+            end_time: [8, 16, 17].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result =
-            Solution::max_free_time(input.event_time, input.k, input.start_time, input.end_time);
+        let result = Solution::max_free_time(input.event_time, input.start_time, input.end_time);
         println!("{:?}", result);
     }
 }
