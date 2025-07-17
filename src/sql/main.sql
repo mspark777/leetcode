08a5f1f5f1cc6425daab75e8ebe7740c4af1209e
mspark777 <topangel777@gmail.com>
2025-07-17 11:43:45 +0900

    1068. Product Sales Analysis I

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,6 +1,5 @@
-select actor_id, director_id
-from actordirector
-group by actor_id, director_id
-having count(*) >= 3
+select p.product_name as product_name, s.year as year, s.price as price
+from product as p
+join sales as s on p.product_id = s.product_id
 ;
 
