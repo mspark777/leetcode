cadffb85e52f8f8dbe87d3ba38b383e9b81b7d5c
mspark777 <topangel777@gmail.com>
2025-07-18 16:32:09 +0900

    1084. Sales Analysis III

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,6 +1,7 @@
-select p.project_id as project_id, round(avg(e.experience_years), 2) as average_years
-from project as p
-join employee as e on p.employee_id = e.employee_id
-group by p.project_id
+select s.product_id as product_id, p.product_name as product_name
+from sales as s
+join product as p on s.product_id = p.product_id
+group by s.product_id, p.product_name
+having min(s.sale_date) >= '2019-01-01' and max(s.sale_date) <= '2019-03-31'
 ;
 
