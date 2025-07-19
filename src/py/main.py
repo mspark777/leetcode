51e46849c9ead1d3f84454d1028bb943d2990e3d
mspark777 <topangel777@gmail.com>
2025-07-19 16:19:08 +0900

    1114. Print in Order

--- src/py/main.py
+++ src/py/main.py
@@ -1,5 +1,35 @@
-from typing import List
+from typing import List, Callable
+from threading import Lock
 import math
+
+
+class Foo:
+    first_lock: Lock
+    second_lock: Lock
+
+    def __init__(self):
+        self.first_lock = Lock()
+        self.second_lock = Lock()
+        self.first_lock.acquire()
+        self.second_lock.acquire()
+
+    def first(self, printFirst: "Callable[[], None]") -> None:
+
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.first_lock.release()
+
+    def second(self, printSecond: "Callable[[], None]") -> None:
+        # printSecond() outputs "second". Do not change or remove this line.
+        with self.first_lock:
+            printSecond()
+            self.second_lock.release()
+
+    def third(self, printThird: "Callable[[], None]") -> None:
+
+        # printThird() outputs "third". Do not change or remove this line.
+        with self.second_lock:
+            printThird()
 
 
 class Solution:
