de840619db0090e8c2028865dbf302f7d8387fee
mspark777 <topangel777@gmail.com>
2025-07-21 09:50:13 +0900

    1179. Reformat Department Table

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,5 +1,18 @@
-select distinct author_id as id
-from views
-where author_id = viewer_id
-order by author_id
+select
+    id,
+    sum(case when month = 'Jan' then revenue else null end) as jan_revenue,
+    sum(case when month = 'Feb' then revenue else null end) as feb_revenue,
+    sum(case when month = 'Mar' then revenue else null end) as mar_revenue,
+    sum(case when month = 'Apr' then revenue else null end) as apr_revenue,
+    sum(case when month = 'May' then revenue else null end) as may_revenue,
+    sum(case when month = 'Jun' then revenue else null end) as jun_revenue,
+    sum(case when month = 'Jul' then revenue else null end) as jul_revenue,
+    sum(case when month = 'Aug' then revenue else null end) as aug_revenue,
+    sum(case when month = 'Sep' then revenue else null end) as sep_revenue,
+    sum(case when month = 'Oct' then revenue else null end) as oct_revenue,
+    sum(case when month = 'Nov' then revenue else null end) as nov_revenue,
+    sum(case when month = 'Dec' then revenue else null end) as dec_revenue
+from department
+group by id
+;
 
