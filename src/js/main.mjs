a8070ed5b60977f1ba8f71631ab864df2ee11b5a
mspark777 <topangel777@gmail.com>
2025-07-21 10:13:56 +0900

    1184. Distance Between Bus Stops

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,18 +1,34 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 /**
- * @param {string} address
- * @return {string}
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
  */
-var defangIPaddr = function (address) {
-  return address.split(".").join("[.]");
+var distanceBetweenBusStops = function (distance, start, destination) {
+  if (start > destination) {
+    const t = start;
+    start = destination;
+    destination = t;
+  }
+
+  const clockwise = distance
+    .slice(start, destination)
+    .reduce((acc, i) => acc + i);
+  const total = distance.reduce((acc, i) => acc + i);
+  return Math.min(clockwise, total - clockwise);
 };
 
 function main() {
-  const inputs = ["1.1.1.1", "255.100.50.0"];
+  const inputs = [
+    [[1, 2, 3, 4], 0, 1],
+    [[1, 2, 3, 4], 0, 2],
+    [[1, 2, 3, 4], 0, 3],
+  ];
 
-  for (const input of inputs) {
-    const result = defangIPaddr(input);
+  for (const [distance, start, destination] of inputs) {
+    const result = distanceBetweenBusStops(distance, start, destination);
     console.log(result);
   }
 }
