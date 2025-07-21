1369d6da50687fa1b4581b4e5a3b0c2650604f96
mspark777 <topangel777@gmail.com>
2025-07-21 09:37:16 +0900

    1175. Prime Arrangements

--- src/go/main.go
+++ src/go/main.go
@@ -1,47 +1,45 @@
 package main
 
 import "fmt"
-import "strconv"
 
-func offset(year int) []int {
-	if ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0) {
-		return []int{0, 31, 60, 91, 121, 152,
-			182, 213, 244, 274, 305, 335}
+func makeFancyString(s string) string {
+	chars := []rune(s)
+	n := len(chars)
+	if n < 3 {
+		return s
 	}
 
-	return []int{0, 31, 59, 90, 120, 151,
-		181, 212, 243, 273, 304, 334}
-}
+	j := 2
 
-func dayOfYear(date string) int {
-	year, _ := strconv.Atoi(date[0:4])
-	month, _ := strconv.Atoi(date[5:7])
-	day, _ := strconv.Atoi(date[8:])
-	return offset(year)[month-1] + day
+	for i := 2; i < n; i += 1 {
+		if chars[i] != chars[j-1] || chars[i] != chars[j-2] {
+			chars[j] = chars[i]
+			j += 1
+		}
+	}
+
+	return string(chars[0:j])
 }
 
 type input struct {
-	date string
+	s string
 }
 
 func main() {
 	inputs := []input{
 		{
-			date: "2019-01-09",
+			s: "leeetcode",
 		},
 		{
-			date: "2019-02-10",
+			s: "aaabaaaa",
 		},
 		{
-			date: "2008-10-10",
-		},
-		{
-			date: "2016-02-09",
+			s: "aab",
 		},
 	}
 
 	for _, input := range inputs {
-		result := dayOfYear(input.date)
+		result := makeFancyString(input.s)
 		fmt.Println(result)
 	}
 }
--- src/py/main.py
+++ src/py/main.py
@@ -1,65 +1,51 @@
-from typing import List, Callable
-from threading import Lock
-import math
-
-
-class Foo:
-    first_lock: Lock
-    second_lock: Lock
-
-    def __init__(self):
-        self.first_lock = Lock()
-        self.second_lock = Lock()
-        self.first_lock.acquire()
-        self.second_lock.acquire()
-
-    def first(self, printFirst: "Callable[[], None]") -> None:
-
-        # printFirst() outputs "first". Do not change or remove this line.
-        printFirst()
-        self.first_lock.release()
-
-    def second(self, printSecond: "Callable[[], None]") -> None:
-        # printSecond() outputs "second". Do not change or remove this line.
-        with self.first_lock:
-            printSecond()
-            self.second_lock.release()
-
-    def third(self, printThird: "Callable[[], None]") -> None:
-
-        # printThird() outputs "third". Do not change or remove this line.
-        with self.second_lock:
-            printThird()
+from math import factorial
 
 
 class Solution:
-    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        x = int(math.sqrt(candies * 2 + 0.25) - 0.5)
-        result = [0] * num_people
-        for i in range(num_people):
-            m = x // num_people
-            if (x % num_people) > i:
-                m += 1
-
-            result[i] = m * (i + 1) + m * (m - 1) // 2 * num_people
-        result[x % num_people] += candies - x * (x + 1) // 2
-        return result
+    def numPrimeArrangements(self, n: int) -> int:
+        p = {
+            2,
+            3,
+            5,
+            7,
+            11,
+            13,
+            17,
+            19,
+            23,
+            29,
+            31,
+            37,
+            41,
+            43,
+            47,
+            53,
+            59,
+            61,
+            67,
+            71,
+            73,
+            79,
+            83,
+            89,
+            97,
+        }
+        primes = [x for x in range(2, n + 1) if x in p]
+        return (factorial(len(primes)) * factorial(n - len(primes))) % int(1e9 + 7)
 
 
 class Input:
-    candies: int
-    num_people: int
+    n: int
 
-    def __init__(self, candies: int, num_people: int):
-        self.candies = candies
-        self.num_people = num_people
+    def __init__(self, n: int):
+        self.n = n
 
 
 def main():
-    inputs = [Input(7, 4), Input(10, 3)]
+    inputs = [Input(5), Input(100)]
 
     for input in inputs:
-        result = Solution().distributeCandies(input.candies, input.num_people)
+        result = Solution().numPrimeArrangements(input.n)
         print(result)
 
 
