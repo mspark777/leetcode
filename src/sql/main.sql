6c88cb8f866671b5357cd611a057ec718be28d7c
mspark777 <topangel777@gmail.com>
2025-07-22 15:09:59 +0900

    1211. Queries Quality and Percentage

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,18 +1,10 @@
 select
-    id,
-    sum(case when month = 'Jan' then revenue else null end) as jan_revenue,
-    sum(case when month = 'Feb' then revenue else null end) as feb_revenue,
-    sum(case when month = 'Mar' then revenue else null end) as mar_revenue,
-    sum(case when month = 'Apr' then revenue else null end) as apr_revenue,
-    sum(case when month = 'May' then revenue else null end) as may_revenue,
-    sum(case when month = 'Jun' then revenue else null end) as jun_revenue,
-    sum(case when month = 'Jul' then revenue else null end) as jul_revenue,
-    sum(case when month = 'Aug' then revenue else null end) as aug_revenue,
-    sum(case when month = 'Sep' then revenue else null end) as sep_revenue,
-    sum(case when month = 'Oct' then revenue else null end) as oct_revenue,
-    sum(case when month = 'Nov' then revenue else null end) as nov_revenue,
-    sum(case when month = 'Dec' then revenue else null end) as dec_revenue
-from department
-group by id
+    query_name,
+    round(avg(rating * 1.0 / position), 2) as quality,
+    round(
+        avg(case when rating < 3 then 1 else 0 end) * 100.0, 2
+    ) as poor_query_percentage
+from queries
+group by query_name
 ;
 
