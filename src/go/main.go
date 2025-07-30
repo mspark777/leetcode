306e2344064254f45527579fd12b26d377c3dfd3
mspark777 <topangel777@gmail.com>
2025-07-30 11:02:21 +0900

    1260. Shift 2D Grid

--- src/go/main.go
+++ src/go/main.go
@@ -1,48 +1,51 @@
 package main
 
 import "fmt"
-import "math"
 
-func maximumUniqueSubarray(nums []int) int {
-	result := math.MinInt
-	sum := 0
-	visits := make(map[int]int)
-	left := 0
-	right := 0
-	for right < len(nums) {
-		rn := nums[right]
-		if v, ok := visits[rn]; ok {
-			for left <= v {
-				sum -= nums[left]
-				left += 1
-			}
+func shiftGrid(grid [][]int, k int) [][]int {
+	n := len(grid)
+	m := len(grid[0])
+
+	result := make([][]int, n)
+	for i := range result {
+		result[i] = make([]int, m)
+	}
+
+	for r := 0; r < n; r += 1 {
+		for c := 0; c < m; c += 1 {
+			new_r := (r + (c+k)/m) % n
+			new_c := (c + k) % m
+
+			result[new_r][new_c] = grid[r][c]
 		}
-
-		sum += rn
-		result = max(result, sum)
-		visits[rn] = right
-		right += 1
 	}
 
 	return result
 }
 
 type input struct {
-	nums []int
+	grid [][]int
+	k    int
 }
 
 func main() {
 	inputs := []input{
 		{
-			nums: []int{4, 2, 4, 5, 6},
+			grid: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			k:    1,
 		},
 		{
-			nums: []int{5, 2, 1, 2, 5, 2, 1, 2, 5},
+			grid: [][]int{{3, 8, 1, 9}, {19, 7, 2, 5}, {4, 6, 11, 10}, {12, 0, 21, 13}},
+			k:    4,
+		},
+		{
+			grid: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			k:    9,
 		},
 	}
 
 	for _, input := range inputs {
-		result := maximumUniqueSubarray(input.nums)
+		result := shiftGrid(input.grid, input.k)
 		fmt.Println(result)
 	}
 }
