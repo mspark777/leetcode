0434255c2d305110fbb0d7ec50cd596f6fa98349
mspark777 <topangel777@gmail.com>
2025-07-30 10:50:55 +0900

    1252. Cells with Odd Values in a Matrix

--- src/py/main.py
+++ src/py/main.py
@@ -1,51 +1,49 @@
-from math import factorial
+from typing import List
 
 
 class Solution:
-    def numPrimeArrangements(self, n: int) -> int:
-        p = {
-            2,
-            3,
-            5,
-            7,
-            11,
-            13,
-            17,
-            19,
-            23,
-            29,
-            31,
-            37,
-            41,
-            43,
-            47,
-            53,
-            59,
-            61,
-            67,
-            71,
-            73,
-            79,
-            83,
-            89,
-            97,
-        }
-        primes = [x for x in range(2, n + 1) if x in p]
-        return (factorial(len(primes)) * factorial(n - len(primes))) % int(1e9 + 7)
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        rows = [0] * m
+        cols = [0] * n
+
+        for index in indices:
+            rows[index[0]] += 1
+            cols[index[1]] += 1
+
+        even_rows = m
+        even_cols = n
+        odd_rows = 0
+        odd_cols = 0
+
+        for cell in rows:
+            if (cell & 1) == 1:
+                odd_rows += 1
+                even_rows -= 1
+
+        for cell in cols:
+            if (cell & 1) == 1:
+                odd_cols += 1
+                even_cols -= 1
+
+        return odd_rows * even_cols + even_rows * odd_cols
 
 
 class Input:
+    m: int
     n: int
+    indices: List[List[int]]
 
-    def __init__(self, n: int):
+    def __init__(self, m: int, n: int, indices: list[list[int]]):
+        self.m = m
         self.n = n
+        self.indices = indices
 
 
 def main():
-    inputs = [Input(5), Input(100)]
+    inputs = [Input(2, 3, [[0, 1], [1, 1]]), Input(2, 2, [[1, 1], [0, 0]])]
 
     for input in inputs:
-        result = Solution().numPrimeArrangements(input.n)
+        result = Solution().oddCells(input.m, input.n, input.indices)
         print(result)
 
 
