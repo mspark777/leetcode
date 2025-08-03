d10607a6a2ded2211d3b90b9e762be26d3e159b7
mspark777 <topangel777@gmail.com>
2025-08-03 15:33:28 +0900

    2106. Maximum Fruits Harvested After at Most K Steps

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,63 @@
 struct Solution {}
 
 impl Solution {
-    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
-        let num_rows = num_rows as usize;
-        let mut rows = Vec::<Vec<i32>>::with_capacity(num_rows);
-        for i in 0..num_rows {
-            let mut row = vec![1; i + 1];
-            let i = i as i32;
-            let prev = i - 1;
-            for j in 1..i {
-                let j = j as usize;
-                let prev = prev as usize;
-                row[j] = rows[prev][j - 1] + rows[prev][j];
+    pub fn max_total_fruits(fruits: Vec<Vec<i32>>, start_pos: i32, k: i32) -> i32 {
+        let n = fruits.len();
+        let mut left = 0usize;
+        let mut right = 0usize;
+        let mut sum = 0;
+        let mut result = 0;
+
+        while right < n {
+            sum += fruits[right][1];
+            while (left <= right) && (Self::step(&fruits, start_pos, left, right) > k) {
+                sum -= fruits[left][1];
+                left += 1;
             }
 
-            rows.push(row);
+            result = result.max(sum);
+            right += 1;
         }
 
-        rows
+        result
+    }
+
+    fn step(fruits: &Vec<Vec<i32>>, start_pos: i32, left: usize, right: usize) -> i32 {
+        let l = fruits[left][0];
+        let r = fruits[right][0];
+        (start_pos - r).abs().min((start_pos - l).abs()) + r - l
     }
 }
 
 struct Input {
-    num_rows: i32,
+    fruits: Vec<Vec<i32>>,
+    start_pos: i32,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [Input { num_rows: 5 }, Input { num_rows: 1 }];
+    let inputs = [
+        Input {
+            fruits: [[2, 8], [6, 3], [8, 6]].map(|f| f.to_vec()).to_vec(),
+            start_pos: 5,
+            k: 4,
+        },
+        Input {
+            fruits: [[0, 9], [4, 1], [5, 7], [6, 2], [7, 4], [10, 9]]
+                .map(|f| f.to_vec())
+                .to_vec(),
+            start_pos: 5,
+            k: 4,
+        },
+        Input {
+            fruits: [[0, 3], [6, 4], [8, 5]].map(|f| f.to_vec()).to_vec(),
+            start_pos: 3,
+            k: 2,
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::generate(input.num_rows);
+        let result = Solution::max_total_fruits(input.fruits, input.start_pos, input.k);
         println!("{:?}", result);
     }
 }
