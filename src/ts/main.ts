a9e4a2a2e395cf37fe812af18ae703ca28c95a3e
mspark777 <topangel777@gmail.com>
2025-08-04 22:42:05 +0900

    1290. Convert Binary Number in a Linked List to Integer

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,38 +1,55 @@
 import "@total-typescript/ts-reset";
 
-function minCostToMoveChips(position: number[]): number {
-  let odd = 0;
-  let even = 0;
-  for (const p of position) {
-    if (p % 2 === 0) {
-      even += 1;
-    } else {
-      odd += 1;
-    }
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function getDecimalValue(head: ListNode | null): number {
+  let result = 0;
+
+  for (let node = head; node != null; node = node.next) {
+    result = (result << 1) | node.val;
   }
 
-  return Math.min(even, odd);
+  return result;
+}
+
+function tolist(nums: number[] | null): ListNode | null {
+  if (nums == null) {
+    return null;
+  }
+
+  const dummy = new ListNode();
+  let tail = dummy;
+  for (const n of nums) {
+    tail.next = new ListNode(n);
+    tail = tail.next;
+  }
+
+  return dummy.next;
 }
 
 interface Input {
-  position: number[];
+  nums: number[];
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      position: [1, 2, 3],
+      nums: [1, 0, 1],
     },
     {
-      position: [2, 2, 2, 3, 3],
-    },
-    {
-      position: [1, 1000000000],
+      nums: [0],
     },
   ];
 
   for (const input of inputs) {
-    const result = minCostToMoveChips(input.position);
+    const result = getDecimalValue(tolist(input.nums));
     console.log(result);
   }
 }
