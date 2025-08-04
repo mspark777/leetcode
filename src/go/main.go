dc6aea1a230fc3a8926f4b7d677fe6c47cd575bd
mspark777 <topangel777@gmail.com>
2025-08-04 14:09:24 +0900

    1281. Subtract the Product and Sum of Digits of an Integer

--- src/go/main.go
+++ src/go/main.go
@@ -2,50 +2,36 @@ package main
 
 import "fmt"
 
-func shiftGrid(grid [][]int, k int) [][]int {
-	n := len(grid)
-	m := len(grid[0])
+func subtractProductAndSum(n int) int {
+	mul := 1
+	sum := 0
+	for n > 0 {
+		d := n % 10
+		n /= 10
 
-	result := make([][]int, n)
-	for i := range result {
-		result[i] = make([]int, m)
+		mul *= d
+		sum += d
 	}
 
-	for r := 0; r < n; r += 1 {
-		for c := 0; c < m; c += 1 {
-			new_r := (r + (c+k)/m) % n
-			new_c := (c + k) % m
-
-			result[new_r][new_c] = grid[r][c]
-		}
-	}
-
-	return result
+	return mul - sum
 }
 
 type input struct {
-	grid [][]int
-	k    int
+	n int
 }
 
 func main() {
 	inputs := []input{
 		{
-			grid: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
-			k:    1,
+			n: 234,
 		},
 		{
-			grid: [][]int{{3, 8, 1, 9}, {19, 7, 2, 5}, {4, 6, 11, 10}, {12, 0, 21, 13}},
-			k:    4,
-		},
-		{
-			grid: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
-			k:    9,
+			n: 4421,
 		},
 	}
 
 	for _, input := range inputs {
-		result := shiftGrid(input.grid, input.k)
+		result := subtractProductAndSum(input.n)
 		fmt.Println(result)
 	}
 }
