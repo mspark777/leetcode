e78afe4fe8075b68c11bdcd660b31b2a8ad7c81c
mspark777 <topangel777@gmail.com>
2025-08-05 17:54:01 +0900

    1299. Replace Elements with Greatest Element on Right Side

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,55 +1,35 @@
 import "@total-typescript/ts-reset";
 
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+function replaceElements(arr: number[]): number[] {
+  const result = [...arr];
+  const n = arr.length;
+  let max = -1;
 
-function getDecimalValue(head: ListNode | null): number {
-  let result = 0;
-
-  for (let node = head; node != null; node = node.next) {
-    result = (result << 1) | node.val;
+  for (const [i, num] of [...arr].reverse().entries()) {
+    const j = n - (i + 1);
+    result[j] = max;
+    max = Math.max(max, num);
   }
 
   return result;
 }
 
-function tolist(nums: number[] | null): ListNode | null {
-  if (nums == null) {
-    return null;
-  }
-
-  const dummy = new ListNode();
-  let tail = dummy;
-  for (const n of nums) {
-    tail.next = new ListNode(n);
-    tail = tail.next;
-  }
-
-  return dummy.next;
-}
-
 interface Input {
-  nums: number[];
+  arr: number[];
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      nums: [1, 0, 1],
+      arr: [17, 18, 5, 4, 6, 1],
     },
     {
-      nums: [0],
+      arr: [400],
     },
   ];
 
   for (const input of inputs) {
-    const result = getDecimalValue(tolist(input.nums));
+    const result = replaceElements(input.arr);
     console.log(result);
   }
 }
