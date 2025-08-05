67615714a548e50878cacecdbb1f35740bc55ea2
mspark777 <topangel777@gmail.com>
2025-08-05 22:21:42 +0900

    1309. Decrypt String from Alphabet to Integer Mapping

--- src/py/main.py
+++ src/py/main.py
@@ -1,49 +1,31 @@
-from typing import List
-
-
 class Solution:
-    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        rows = [0] * m
-        cols = [0] * n
+    def freqAlphabets(self, s: str) -> str:
+        result: list[str] = []
+        i = 0
+        while i < len(s):
+            if ((i + 2) < len(s)) and (s[i + 2] == "#"):
+                val = int(s[i : i + 2])
+                result.append(chr(val + 96))
+                i += 3
+            else:
+                result.append(chr(int(s[i]) + 96))
+                i += 1
 
-        for index in indices:
-            rows[index[0]] += 1
-            cols[index[1]] += 1
-
-        even_rows = m
-        even_cols = n
-        odd_rows = 0
-        odd_cols = 0
-
-        for cell in rows:
-            if (cell & 1) == 1:
-                odd_rows += 1
-                even_rows -= 1
-
-        for cell in cols:
-            if (cell & 1) == 1:
-                odd_cols += 1
-                even_cols -= 1
-
-        return odd_rows * even_cols + even_rows * odd_cols
+        return "".join(result)
 
 
 class Input:
-    m: int
-    n: int
-    indices: List[List[int]]
+    s: str
 
-    def __init__(self, m: int, n: int, indices: list[list[int]]):
-        self.m = m
-        self.n = n
-        self.indices = indices
+    def __init__(self, s: str):
+        self.s = s
 
 
 def main():
-    inputs = [Input(2, 3, [[0, 1], [1, 1]]), Input(2, 2, [[1, 1], [0, 0]])]
+    inputs = [Input("10#11#12"), Input("1326#")]
 
     for input in inputs:
-        result = Solution().oddCells(input.m, input.n, input.indices)
+        result = Solution().freqAlphabets(input.s)
         print(result)
 
 
