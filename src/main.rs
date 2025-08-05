a8203c4eaaf213a13e30ab86f774ada6248dd689
mspark777 <topangel777@gmail.com>
2025-08-05 17:45:21 +0900

    3477. Fruits Into Baskets II

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,44 @@
 struct Solution {}
 
 impl Solution {
-    pub fn total_fruit(fruits: Vec<i32>) -> i32 {
-        let mut baskets = std::collections::HashMap::<i32, i32>::with_capacity(3);
-        let mut left = 0usize;
-        let mut result = 0usize;
-
-        for (right, rfruit) in fruits.iter().cloned().enumerate() {
-            baskets.entry(rfruit).and_modify(|f| *f += 1).or_insert(1);
-
-            while baskets.len() > 2 {
-                let lfruit = fruits[left];
-                let lcount = baskets.get_mut(&lfruit).unwrap();
-                if *lcount > 1 {
-                    *lcount -= 1;
-                } else {
-                    baskets.remove(&lfruit);
+    pub fn num_of_unplaced_fruits(fruits: Vec<i32>, baskets: Vec<i32>) -> i32 {
+        let mut baskets = baskets;
+        let mut result = 0;
+        for fruit in fruits.iter().cloned() {
+            let mut unset = 1;
+            for capacity in baskets.iter_mut() {
+                if fruit <= *capacity {
+                    *capacity = 0;
+                    unset = 0;
+                    break;
                 }
-
-                left += 1;
             }
-
-            result = result.max(right + 1 - left);
+            result += unset;
         }
 
-        result as i32
+        result
     }
 }
 
 struct Input {
     fruits: Vec<i32>,
+    baskets: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            fruits: [1, 2, 3].to_vec(),
+            fruits: [4, 2, 5].to_vec(),
+            baskets: [3, 5, 4].to_vec(),
         },
         Input {
-            fruits: [0, 1, 2, 2].to_vec(),
-        },
-        Input {
-            fruits: [1, 2, 3, 2, 2].to_vec(),
+            fruits: [3, 6, 1].to_vec(),
+            baskets: [6, 4, 7].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::total_fruit(input.fruits);
+        let result = Solution::num_of_unplaced_fruits(input.fruits, input.baskets);
         println!("{:?}", result);
     }
 }
