ba23a03936cc4981863c2510c2fff764b2091b6f
mspark777 <topangel777@gmail.com>
2025-08-11 00:35:48 +0900

    1374. Generate a String With Characters That Have Odd Counts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,31 +1,28 @@
 import "@total-typescript/ts-reset";
 
-function daysBetweenDates(date1: string, date2: string): number {
-  const d1 = new Date(date1);
-  const d2 = new Date(date2);
-  const diff = Math.abs(d1.getTime() - d2.getTime());
-  return Number(BigInt(diff) / 86400000n);
+function generateTheString(n: number): string {
+  return (n & 1) === 1 ? "a".repeat(n) : "a".repeat(n - 1) + "b";
 }
 
 interface Input {
-  date1: string;
-  date2: string;
+  n: number;
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      date1: "2019-06-29",
-      date2: "2019-06-30",
+      n: 4,
     },
     {
-      date1: "2020-01-15",
-      date2: "2019-12-31",
+      n: 2,
+    },
+    {
+      n: 7,
     },
   ];
 
   for (const input of inputs) {
-    const result = daysBetweenDates(input.date1, input.date2);
+    const result = generateTheString(input.n);
     console.log(result);
   }
 }
