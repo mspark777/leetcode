f92bec33f2a10a333e03f02c4af7ff726b5bdbee
mspark777 <topangel777@gmail.com>
2025-08-12 10:14:52 +0900

    1378. Replace Employee ID With The Unique Identifier

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,7 +1,5 @@
-select product_name, sum(unit) as unit
-from products as p
-join orders as o on o.product_id = p.product_id
-where order_date between '2020-02-01' and '2020-02-29'
-group by p.product_name
-having sum(unit) >= 100
+select u.unique_id as unique_id, e.name as name
+from employees as e
+left outer join employeeuni as u on e.id = u.id
+;
 
