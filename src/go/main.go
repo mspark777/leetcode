c6245c1520e7776e2b2d6a8bde9db3a79f151c15
mspark777 <topangel777@gmail.com>
2025-08-16 11:30:10 +0900

    1480. Running Sum of 1d Array

--- src/go/main.go
+++ src/go/main.go
@@ -4,80 +4,36 @@ import (
 	"fmt"
 )
 
-type TrieNode struct {
-	frequency int
-	children  map[rune]*TrieNode
-}
+func runningSum(nums []int) []int {
+	result := make([]int, len(nums))
+	result[0] = nums[0]
 
-func newTrieNode() *TrieNode {
-	node := new(TrieNode)
-	node.frequency = 0
-	node.children = make(map[rune]*TrieNode)
-	return node
-}
-
-func insertWord(root *TrieNode, word string) {
-	current := root
-	for _, c := range word {
-		if child, ok := current.children[c]; ok {
-			child.frequency += 1
-			current = child
-		} else {
-			child := newTrieNode()
-			child.frequency = 1
-			current.children[c] = child
-			current = child
-		}
-	}
-}
-
-func isSubstring(root *TrieNode, word string) bool {
-	current := root
-	for _, c := range word {
-		current = current.children[c]
-	}
-
-	return current.frequency > 1
-}
-
-func stringMatching(words []string) []string {
-	result := []string{}
-	root := newTrieNode()
-
-	for _, word := range words {
-		for i := 0; i < len(word); i += 1 {
-			insertWord(root, word[i:])
-		}
-	}
-
-	for _, word := range words {
-		if isSubstring(root, word) {
-			result = append(result, word)
-		}
+	for i, n := range nums[1:] {
+		result[i+1] = result[i] + n
 	}
 
 	return result
 }
 
 type input struct {
-	words []string
+	nums []int
 }
 
 func main() {
 	inputs := []input{
 		{
-			words: []string{"mass", "as", "hero", "superhero"},
+			nums: []int{1, 2, 3, 4},
 		},
 		{
-			words: []string{"leetcode", "et", "code"},
+			nums: []int{1, 1, 1, 1, 1},
 		},
 		{
-			words: []string{"blue", "green", "bu"},
+			nums: []int{3, 1, 2, 10, 1},
 		},
 	}
 
 	for _, input := range inputs {
-		result := stringMatching(input.words)
+		result := runningSum(input.nums)
 		fmt.Println(result)
 	}
 }
