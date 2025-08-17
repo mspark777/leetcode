ec01cc3b0c43773c7805345016dfcfec140930ab
mspark777 <topangel777@gmail.com>
2025-08-17 12:31:57 +0900

    1491. Average Salary Excluding the Minimum and Maximum Salary

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,46 +1,30 @@
 import "@total-typescript/ts-reset";
 
-function busyStudent(
-  startTime: number[],
-  endTime: number[],
-  queryTime: number,
-): number {
-  let result = 0;
+function average(salary: number[]): number {
+  const max = salary.reduce((a, b) => Math.max(a, b));
+  const min = salary.reduce((a, b) => Math.min(a, b));
+  const sum = salary.reduce((a, b) => a + b) - (max + min);
+  const count = salary.length - 2;
 
-  for (const [i, s] of startTime.entries()) {
-    const e = endTime[i];
-    if (e == null) {
-      continue;
-    } else if (s <= queryTime && queryTime <= e) {
-      result += 1;
-    }
-  }
-
-  return result;
+  return Number((sum / count).toFixed(5));
 }
 
 interface Input {
-  startTime: number[];
-  endTime: number[];
-  queryTime: number;
+  salary: number[];
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      startTime: [1, 2, 3],
-      endTime: [3, 2, 7],
-      queryTime: 4,
+      salary: [4000, 3000, 1000, 2000],
     },
     {
-      startTime: [4],
-      endTime: [4],
-      queryTime: 4,
+      salary: [1000, 2000, 3000],
     },
   ];
 
   for (const input of inputs) {
-    const result = busyStudent(input.startTime, input.endTime, input.queryTime);
+    const result = average(input.salary);
     console.log(result);
   }
 }
