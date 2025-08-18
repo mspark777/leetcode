76ebe193f6b4a261e8eda239306a5dffc5ebab2b
mspark777 <topangel777@gmail.com>
2025-08-18 14:26:53 +0900

    1507. Reformat Date

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,30 +1,45 @@
 import "@total-typescript/ts-reset";
 
-function average(salary: number[]): number {
-  const max = salary.reduce((a, b) => Math.max(a, b));
-  const min = salary.reduce((a, b) => Math.min(a, b));
-  const sum = salary.reduce((a, b) => a + b) - (max + min);
-  const count = salary.length - 2;
-
-  return Number((sum / count).toFixed(5));
+function reformatDate(date: string): string {
+  const [day, month, year] = date.split(" ") as [string, string, string];
+  const monthMap = new Map(
+    Object.entries({
+      Jan: "01",
+      Feb: "02",
+      Mar: "03",
+      Apr: "04",
+      May: "05",
+      Jun: "06",
+      Jul: "07",
+      Aug: "08",
+      Sep: "09",
+      Oct: "10",
+      Nov: "11",
+      Dec: "12",
+    }),
+  );
+  return `${year}-${monthMap.get(month) as string}-${day.slice(0, -2).padStart(2, "0")}`;
 }
 
 interface Input {
-  salary: number[];
+  date: string;
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      salary: [4000, 3000, 1000, 2000],
+      date: "20th Oct 2052",
     },
     {
-      salary: [1000, 2000, 3000],
+      date: "6th Jun 1933",
+    },
+    {
+      date: "26th May 1960",
     },
   ];
 
   for (const input of inputs) {
-    const result = average(input.salary);
+    const result = reformatDate(input.date);
     console.log(result);
   }
 }
