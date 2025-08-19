9a1b113d698ef3924f2ea365f7665a5c4bf9363d
mspark777 <topangel777@gmail.com>
2025-08-19 10:24:43 +0900

    1517. Find Users With Valid E-Mails

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,9 +1,5 @@
-select
-    sell_date,
-    count(distinct(product, sell_date)) as num_sold,
-    string_agg(distinct product, ',' order by product) as products
-from activities
-group by sell_date
-order by sell_date
+select *
+from users
+where mail ~ '^[a-zA-Z][a-zA-Z0-9\.\-_]*@leetcode\.com$'
 ;
 
