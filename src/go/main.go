343ec3f864310b82085f171627c5931bcaf8a8e1
mspark777 <topangel777@gmail.com>
2025-08-20 22:55:19 +0900

    1560. Most Visited Sector in a Circular Track

--- src/go/main.go
+++ src/go/main.go
@@ -4,50 +4,51 @@ import (
 	"fmt"
 )
 
-func xorB(n int, start int) int {
-	if (n % 2) == 0 {
-		return (n / 2) & 1
-	} else {
-		return ((n / 2) & 1) ^ (start + n - 1)
-	}
-}
+func mostVisited(n int, rounds []int) []int {
+	start := rounds[0]
+	end := rounds[len(rounds)-1]
+	result := []int{}
 
-func xorA(n int, start int) int {
-	if (start & 1) == 1 {
-		return (start - 1) ^ xorB(n+1, start-1)
+	if start <= end {
+		for i := start; i <= end; i += 1 {
+			result = append(result, i)
+		}
 	} else {
-		return xorB(n, start)
-	}
-}
+		for i := 1; i <= end; i += 1 {
+			result = append(result, i)
+		}
 
-func xorOperation(n int, start int) int {
-	result := 2 * xorA(n, start/2)
-	if (n & start & 1) == 1 {
-		result += 1
+		for i := start; i <= n; i += 1 {
+			result = append(result, i)
+		}
 	}
 
 	return result
 }
 
 type input struct {
-	n     int
-	start int
+	n      int
+	rounds []int
 }
 
 func main() {
 	inputs := []input{
 		{
-			n:     5,
-			start: 0,
+			n:      4,
+			rounds: []int{1, 3, 1, 2},
 		},
 		{
-			n:     4,
-			start: 3,
+			n:      2,
+			rounds: []int{2, 1, 2, 1, 2, 1, 2, 1, 2},
+		},
+		{
+			n:      7,
+			rounds: []int{1, 3, 5, 7},
 		},
 	}
 
 	for _, input := range inputs {
-		result := xorOperation(input.n, input.start)
+		result := mostVisited(input.n, input.rounds)
 		fmt.Println(result)
 	}
 }
