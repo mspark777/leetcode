b3d146eee1d04ebf31852e8f70e0cb0754c70414
mspark777 <topangel777@gmail.com>
2025-08-25 00:01:48 +0900

    1603. Design Parking System

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,23 @@
 import "@total-typescript/ts-reset";
 
+class ParkingSystem {
+  private spaces: number[];
+  constructor(big: number, medium: number, small: number) {
+    this.spaces = [big, medium, small];
+  }
+
+  addCar(carType: number): boolean {
+    const idx = carType - 1;
+    const space = this.spaces[idx] ?? 0;
+    if (space <= 0) {
+      return false;
+    }
+
+    this.spaces[idx] = space - 1;
+    return true;
+  }
+}
+
 function reorderSpaces(text: string): string {
   const splitted = text.split(" ");
   const words = splitted.filter((word) => word !== "");
