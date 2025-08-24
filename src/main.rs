b8d1130c5885d85eaec15d875b72b275821634ef
mspark777 <topangel777@gmail.com>
2025-08-25 00:16:55 +0900

    1619. Mean of Array After Removing Some Elements

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,41 @@
 struct Solution {}
 
 impl Solution {
-    pub fn modify_string(s: String) -> String {
-        let mut result = s.chars().collect::<Vec<char>>();
+    pub fn trim_mean(arr: Vec<i32>) -> f64 {
+        let mut arr = arr;
+        arr.sort();
+        let len = arr.len();
+        let skip = len / 20;
+        let count = len - 2 * skip;
 
-        for i in 0..result.len() {
-            let ch = result[i];
-            if ch != '?' {
-                continue;
-            }
-
-            let mut candidate = std::collections::HashSet::with_capacity(2);
-            if i < (result.len() - 1) {
-                candidate.insert(result[i + 1]);
-            }
-
-            if i > 0 {
-                candidate.insert(result[i - 1]);
-            }
-
-            result[i] = if !candidate.contains(&'a') {
-                'a'
-            } else if !candidate.contains(&'b') {
-                'b'
-            } else {
-                'c'
-            };
-        }
-
-        result.iter().collect()
+        let sum = arr.iter().skip(skip).take(count).sum::<i32>();
+        sum as f64 / count as f64
     }
 }
 
 struct Input {
-    s: String,
+    arr: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "?zs".to_string(),
+            arr: [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3].to_vec(),
         },
         Input {
-            s: "ubv?w".to_string(),
+            arr: [6, 2, 7, 5, 1, 2, 0, 3, 10, 2, 5, 0, 5, 5, 0, 8, 7, 6, 8, 0].to_vec(),
+        },
+        Input {
+            arr: [
+                6, 0, 7, 0, 7, 5, 7, 8, 3, 4, 0, 7, 8, 1, 6, 8, 1, 1, 2, 4, 8, 1, 9, 5, 4, 3, 8, 5,
+                10, 8, 6, 6, 1, 0, 6, 10, 8, 2, 3, 4,
+            ]
+            .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::modify_string(input.s);
+        let result = Solution::trim_mean(input.arr);
         println!("{:?}", result);
     }
 }
