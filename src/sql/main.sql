b3c8011e38938b343972d0946ad843e7e246e8c9
mspark777 <topangel777@gmail.com>
2025-08-25 11:26:31 +0900

    1633. Percentage of Users Attended a Contest

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,7 +1,11 @@
-select name, sum(amount) as balance
-from users u
-inner join transactions t on u.account = t.account
-group by name
-having sum(amount) > 10000
-;
+with user_count as (select count(user_id) as user_count from users)
+select
+    r.contest_id,
+    round(
+        ((count(r.contest_id)::decimal / uc.user_count::decimal) * 100)::decimal, 2
+    ) as percentage
+from register r
+cross join user_count uc
+group by r.contest_id, uc.user_count
+order by percentage desc, r.contest_id asc
 
