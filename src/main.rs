dcfd4f748795297aa0b994cd384624a1ca196eca
mspark777 <topangel777@gmail.com>
2025-08-28 10:31:22 +0900

    1678. Goal Parser Interpretation

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,30 @@
 struct Solution {}
 
 impl Solution {
-    pub fn get_maximum_generated(n: i32) -> i32 {
-        if n == 0 {
-            return 0;
-        }
-
-        let n = n as usize;
-        let mut nums = vec![0; n + 1];
-        let mut result = 1;
-
-        nums[1] = 1;
-        for i in 2..nums.len() {
-            if (i & 1) == 1 {
-                let idx = i / 2;
-                nums[i] = nums[idx] + nums[idx + 1];
-            } else {
-                nums[i] = nums[i / 2];
-            }
-            result = result.max(nums[i]);
-        }
-
-        result
+    pub fn interpret(command: String) -> String {
+        command.clone().replace("()", "o").replace("(al)", "al")
     }
 }
 
 struct Input {
-    n: i32,
+    command: String,
 }
 
 fn main() {
-    let inputs = [Input { n: 7 }, Input { n: 2 }, Input { n: 3 }];
+    let inputs = [
+        Input {
+            command: "G()(al)".to_string(),
+        },
+        Input {
+            command: "G()()()()(al)".to_string(),
+        },
+        Input {
+            command: "(al)G(al)()()G".to_string(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::get_maximum_generated(input.n);
+        let result = Solution::interpret(input.command);
         println!("{:?}", result);
     }
 }
