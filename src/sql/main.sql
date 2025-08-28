d68cf92d2856e722ed91a350e9883baf9db7e26c
mspark777 <topangel777@gmail.com>
2025-08-28 10:32:34 +0900

    1683. Invalid Tweets]

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,6 +1,2 @@
-select
-    user_id, concat(upper(left(name, 1)), lower(right(name, length(name) - 1))) as name
-from users
-order by user_id
-;
+select tweet_id from tweets where length(content) > 15
 
