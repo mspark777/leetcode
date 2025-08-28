510450490112e395f23cd81766a27b6d31804113
mspark777 <topangel777@gmail.com>
2025-08-28 10:11:25 +0900

    1661. Average Time of Process per Machine

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,11 +1,12 @@
-with user_count as (select count(user_id) as user_count from users)
 select
-    r.contest_id,
-    round(
-        ((count(r.contest_id)::decimal / uc.user_count::decimal) * 100)::decimal, 2
-    ) as percentage
-from register r
-cross join user_count uc
-group by r.contest_id, uc.user_count
-order by percentage desc, r.contest_id asc
+    a1.machine_id,
+    round(avg(a1.timestamp - a2.timestamp)::numeric, 3) as processing_time
+from activity a1
+join
+    activity a2
+    on a1.machine_id = a2.machine_id
+    and a1.process_id = a2.process_id
+    and a1.activity_type = 'end'
+    and a2.activity_type = 'start'
+group by a1.machine_id
 
