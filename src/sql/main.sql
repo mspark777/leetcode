c31eb3922335dbedc5bd5715bd9fd5c0ba6843cc
mspark777 <topangel777@gmail.com>
2025-08-30 10:34:12 +0900

    1731. The Number of Employees Which Report to Each Employee

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,6 +1,8 @@
-select user_id, count(user_id) as followers_count
-from followers
-group by user_id
-order by user_id asc
+select
+    m.employee_id, m.name, count(*) as reports_count, round(avg(e.age)) as average_age
+from employees e
+join employees m on e.reports_to = m.employee_id
+group by m.employee_id, m.name
+order by m.employee_id
 ;
 
