78f3e6bd708b5f24278cc09c0cd93243ce897ea8
mspark777 <topangel777@gmail.com>
2025-08-30 11:05:25 +0900

    1742. Maximum Number of Balls in a Box

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,77 +1,44 @@
 import "@total-typescript/ts-reset";
 
-class OrderedStream {
-  pos: number;
-  buffer: Array<string | undefined>;
+function countBalls(lowLimit: number, highLimit: number): number {
+  const counts = new Map<number, number>();
 
-  constructor(n: number) {
-    this.pos = 0;
-    this.buffer = new Array<string | undefined>(n).fill(undefined);
+  for (let i = lowLimit; i <= highLimit; i += 1) {
+    let sum = 0;
+    let n = i;
+    while (n > 0) {
+      sum += n % 10;
+      n = Number(BigInt(n) / 10n);
+    }
+    counts.set(sum, (counts.get(sum) ?? 0) + 1);
   }
 
-  insert(idKey: number, value: string): string[] {
-    this.buffer[idKey - 1] = value;
-    const chunks: string[] = [];
-
-    while (this.pos < this.buffer.length) {
-      const chunk = this.buffer[this.pos];
-      if (chunk == null) {
-        break;
-      }
-
-      this.pos += 1;
-      chunks.push(chunk);
-    }
-
-    return chunks;
-  }
-}
-
-function canFormArray(arr: number[], pieces: number[][]): boolean {
-  const pieceMap = new Map(pieces.map((piece) => [piece[0], piece]));
-  let i = 0;
-
-  while (i < arr.length) {
-    const piece = pieceMap.get(arr[i]);
-    if (piece == null) {
-      return false;
-    }
-
-    for (const [j, num] of piece.entries()) {
-      if (num !== arr[i + j]) {
-        return false;
-      }
-    }
-
-    i += piece.length;
-  }
-
-  return true;
+  return Math.max(...counts.values());
 }
 
 interface Input {
-  arr: number[];
-  pieces: number[][];
+  lowLimit: number;
+  highLimit: number;
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      arr: [15, 88],
-      pieces: [[88], [15]],
+      lowLimit: 1,
+      highLimit: 10,
     },
     {
-      arr: [49, 18, 16],
-      pieces: [[16, 18, 49]],
+      lowLimit: 5,
+      highLimit: 15,
     },
     {
-      arr: [91, 4, 64, 78],
-      pieces: [[78], [4, 64], [91]],
+      lowLimit: 19,
+      highLimit: 28,
     },
   ];
 
   for (const input of inputs) {
-    const result = canFormArray(input.arr, input.pieces);
+    const result = countBalls(input.lowLimit, input.highLimit);
     console.log(result);
   }
 }
