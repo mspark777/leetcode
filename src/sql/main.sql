7044fda3960ef9dcc9ceb4356fc982d0b3fb06a4
mspark777 <topangel777@gmail.com>
2025-08-31 14:33:07 +0900

    1757. Recyclable and Low Fat Products

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,5 +1,2 @@
-select event_day as day, emp_id, sum(out_time - in_time) as total_time
-from employees
-group by emp_id, event_day
-;
+select product_id from products where low_fats = 'Y' and recyclable = 'Y'
 
