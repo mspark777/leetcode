b71e4c3f09fd481ac48633eeaf07ee92208bb467
mspark777 <topangel777@gmail.com>
2025-09-01 10:25:19 +0900

    1796. Second Largest Digit in a String

--- src/py/main.py
+++ src/py/main.py
@@ -1,27 +1,35 @@
-from typing import List
-
-
 class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return max(map(sum, accounts))
+    def secondHighest(self, s: str) -> int:
+        first = -1
+        result = -1
+        for c in s:
+            if not c.isdigit():
+                continue
+            n = int(c)
+            if n > first:
+                result = first
+                first = n
+            elif n > result and n < first:
+                result = n
+
+        return result
 
 
 class Input:
-    accounts: list[list[int]]
+    s: str
 
-    def __init__(self, accounts: list[list[int]]):
-        self.accounts = accounts
+    def __init__(self, s: str):
+        self.s = s
 
 
 def main():
     inputs = [
-        Input([[1, 2, 3], [3, 2, 1]]),
-        Input([[1, 5], [7, 3], [3, 5]]),
-        Input([[2, 8, 7], [7, 1, 3], [1, 9, 5]]),
+        Input("dfa12321afd"),
+        Input("abc1111"),
     ]
 
     for input in inputs:
-        result = Solution().maximumWealth(input.accounts)
+        result = Solution().secondHighest(input.s)
         print(result)
 
 
