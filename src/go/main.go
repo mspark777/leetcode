001dc35e39b8fe82cce04eaab13af5bddae63b90
mspark777 <topangel777@gmail.com>
2025-09-01 10:18:14 +0900

    1791. Find Center of Star Graph

--- src/go/main.go
+++ src/go/main.go
@@ -4,57 +4,36 @@ import (
 	"fmt"
 )
 
-func canChoose(groups [][]int, nums []int) bool {
-	i := 0
-	n := len(nums)
-	g := len(groups)
-	for j := 0; (i < g) && (j < n); j += 1 {
-		group := groups[i]
-		l := len(group)
-		match := true
-		if (j + l) > n {
-			return false
-		}
+func findCenter(edges [][]int) int {
+	firstU := edges[0][0]
+	firstV := edges[0][1]
+	secondU := edges[1][0]
+	secondV := edges[1][1]
 
-		for k := 0; k < l; k += 1 {
-			if group[k] != nums[j+k] {
-				match = false
-				break
-			}
-		}
-
-		if match {
-			j += l - 1
-			i += 1
-		}
+	if firstU == secondU || firstU == secondV {
+		return firstU
 	}
 
-	return i == g
+	return firstV
 }
 
 type input struct {
-	groups [][]int
-	nums   []int
+	edges [][]int
 }
 
 func main() {
 	inputs := []input{
 		{
-			groups: [][]int{{1, -1, -1}, {3, -2, 0}},
-			nums:   []int{1, -1, 0, 1, -1, -1, 3, -2, 0},
+			edges: [][]int{{1, 2}, {2, 3}, {4, 2}},
 		},
 		{
-			groups: [][]int{{10, -2}, {1, 2, 3, 4}},
-			nums:   []int{1, 2, 3, 4, 10, -2},
-		},
-		{
-			groups: [][]int{{1, 2, 3}, {3, 4}},
-			nums:   []int{7, 7, 1, 2, 3, 4, 7, 7},
+
+			edges: [][]int{{1, 2}, {5, 1}, {1, 3}, {1, 4}},
 		},
 	}
 
 	for _, input := range inputs {
-		result := canChoose(input.groups, input.nums)
+		result := findCenter(input.edges)
 		fmt.Println(result)
 	}
 }
