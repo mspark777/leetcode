522a0b795ff1a78ac955759b6bc52a7097c2f32e
mspark777 <topangel777@gmail.com>
2025-09-02 11:30:27 +0900

    1812. Determine Color of a Chessboard Square

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,52 +1,30 @@
 import "@total-typescript/ts-reset";
 
-function countMatches(
-  items: string[][],
-  ruleKey: string,
-  ruleValue: string,
-): number {
-  return items.reduce((acc, [type, color, name]) => {
-    if (ruleKey === "type" && ruleValue === type) {
-      return acc + 1;
-    } else if (ruleKey === "color" && ruleValue === color) {
-      return acc + 1;
-    } else if (ruleKey === "name" && ruleValue === name) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
+function squareIsWhite(coordinates: string): boolean {
+  const first = coordinates.charCodeAt(0) - "a".charCodeAt(0);
+  const second = Number(coordinates[1]);
+  return (first + second) % 2 === 0;
 }
 
 interface Input {
-  items: string[][];
-  ruleKey: string;
-  ruleValue: string;
+  coordinates: string;
 }
 
 function main(): void {
   const inputs: Input[] = [
     {
-      items: [
-        ["phone", "blue", "pixel"],
-        ["computer", "silver", "lenovo"],
-        ["phone", "gold", "iphone"],
-      ],
-      ruleKey: "color",
-      ruleValue: "silver",
+      coordinates: "a1",
     },
     {
-      items: [
-        ["phone", "blue", "pixel"],
-        ["computer", "silver", "phone"],
-        ["phone", "gold", "iphone"],
-      ],
-      ruleKey: "type",
-      ruleValue: "phone",
+      coordinates: "h3",
+    },
+    {
+      coordinates: "c7",
     },
   ];
 
   for (const input of inputs) {
-    const result = countMatches(input.items, input.ruleKey, input.ruleValue);
+    const result = squareIsWhite(input.coordinates);
     console.log(result);
   }
 }
