3b6a1b9d3db9f7808e9652f3035c016b8404fe61
mspark777 <topangel777@gmail.com>
2025-09-07 00:21:02 +0900

    1869. Longer Contiguous Segments of Ones than Zeros

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 struct Solution {}
 
 impl Solution {
-    pub fn sort_sentence(s: String) -> String {
-        let mut result = s.split_whitespace().collect::<Vec<_>>();
-        result.sort_unstable_by_key(|s| s.chars().last().unwrap() as u8);
-        result
-            .iter()
-            .map(|s| s[..s.len() - 1].to_string())
-            .collect::<Vec<_>>()
-            .join(" ")
+    pub fn check_zero_ones(s: String) -> bool {
+        let mut zero_count = 0;
+        let mut one_count = 0;
+        let mut count = 0;
+        let mut last_ch = s.chars().next().unwrap();
+        for ch in s.chars() {
+            if last_ch == ch {
+                count += 1;
+                continue;
+            }
+
+            last_ch = ch;
+            if ch == '1' {
+                zero_count = zero_count.max(count);
+            } else {
+                one_count = one_count.max(count);
+            }
+            count = 1;
+        }
+
+        if last_ch == '1' {
+            one_count.max(count) > zero_count
+        } else {
+            one_count > zero_count.max(count)
+        }
     }
 }
 
@@ -19,15 +36,18 @@ struct Input {
 fn main() {
     let inputs = [
         Input {
-            s: "is2 sentence4 This1 a3".to_string(),
+            s: "1101".to_string(),
         },
         Input {
-            s: "Myself2 Me1 I4 and3".to_string(),
+            s: "111000".to_string(),
+        },
+        Input {
+            s: "110100010".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::sort_sentence(input.s);
+        let result = Solution::check_zero_ones(input.s);
         println!("{:?}", result);
     }
 }
