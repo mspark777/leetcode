a67a998d9fed8b0277acf9d65ca5c15da85c9fd1
mspark777 <topangel777@gmail.com>
2025-09-15 16:06:00 +0900

    2006. Count Number of Pairs With Absolute Difference K

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,49 @@
 struct Solution {}
 
 impl Solution {
-    pub fn num_of_strings(patterns: Vec<String>, word: String) -> i32 {
-        patterns.iter().fold(
-            0,
-            |acc, pattern| {
-                if word.contains(pattern) { acc + 1 } else { acc }
-            },
-        )
+    pub fn count_k_difference(nums: Vec<i32>, k: i32) -> i32 {
+        let mut counts = std::collections::HashMap::<i32, i32>::with_capacity(nums.len());
+        for num in nums.iter().cloned() {
+            counts
+                .entry(num)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        }
+
+        let mut result = 0;
+        for num in counts.keys().cloned() {
+            if let Some(target) = counts.get(&(num + k)) {
+                result += counts.get(&num).unwrap() * target;
+            }
+        }
+
+        result
     }
 }
 
 struct Input {
-    patterns: Vec<String>,
-    word: String,
+    nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            patterns: ["a", "abc", "bc", "d"].map(|s| s.to_string()).to_vec(),
-            word: "abc".to_string(),
+            nums: [1, 2, 2, 1].to_vec(),
+            k: 1,
         },
         Input {
-            patterns: ["a", "b", "c"].map(|s| s.to_string()).to_vec(),
-            word: "aaaaabbbbb".to_string(),
+            nums: [1, 3].to_vec(),
+            k: 3,
         },
         Input {
-            patterns: ["a", "a", "a"].map(|s| s.to_string()).to_vec(),
-            word: "ab".to_string(),
+            nums: [3, 2, 1, 5, 4].to_vec(),
+            k: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::num_of_strings(input.patterns, input.word);
+        let result = Solution::count_k_difference(input.nums, input.k);
         println!("{:?}", result);
     }
 }
