6f44c07c335e8dc04951d7750b1a06b4775eb08a
mspark777 <topangel777@gmail.com>
2025-09-15 15:56:20 +0900

    1995. Count Special Quadruplets

--- src/py/main.py
+++ src/py/main.py
@@ -1,32 +1,37 @@
-class Solution:
-    def minTimeToType(self, word: str) -> int:
-        result = len(word)
-        prev = "a"
+from typing import List
+from collections import defaultdict
 
-        for ch in word:
-            diff = abs(ord(ch) - ord(prev))
-            result += min(diff, 26 - diff)
-            prev = ch
+
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        result = 0
+        n = len(nums)
+        seen = defaultdict(int)
+        for a in range(n - 1, 0, -1):
+            for b in range(a - 1, -1, -1):
+                result += seen[nums[a] + nums[b]]
+            for d in range(n - 1, a, -1):
+                seen[nums[d] - nums[a]] += 1
 
         return result
 
 
 class Input:
-    word: str
+    nums: list[int]
 
-    def __init__(self, word: str):
-        self.word = word
+    def __init__(self, nums: list[int]):
+        self.nums = nums
 
 
 def main():
     inputs = [
-        Input("abc"),
-        Input("bza"),
-        Input("zjpc"),
+        Input([1, 2, 3, 6]),
+        Input([3, 3, 6, 4, 5]),
+        Input([1, 1, 1, 3, 5]),
     ]
 
     for input in inputs:
-        result = Solution().minTimeToType(input.word)
+        result = Solution().countQuadruplets(input.nums)
         print(result)
 
 
