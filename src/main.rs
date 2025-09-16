210eec2612f71a016fc0711b04c693ed755fdb2d
mspark777 <topangel777@gmail.com>
2025-09-16 16:40:43 +0900

    2022. Convert 1D Array Into 2D Array

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,43 @@
 struct Solution {}
 
 impl Solution {
-    pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
-        operations
-            .iter()
-            .fold(0, |result, oper| match oper.chars().nth(1) {
-                Some('+') => result + 1,
-                _ => result - 1,
-            })
+    pub fn construct2_d_array(original: Vec<i32>, m: i32, n: i32) -> Vec<Vec<i32>> {
+        let m = m as usize;
+        let n = n as usize;
+        match original.len() == (m * n) {
+            true => original.chunks(n).map(|chunk| chunk.to_vec()).collect(),
+            _ => vec![],
+        }
     }
 }
 
 struct Input {
-    operations: Vec<String>,
+    original: Vec<i32>,
+    m: i32,
+    n: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            operations: ["--X", "X++", "X++"].map(|s| s.to_string()).to_vec(),
+            original: [1, 2, 3, 4].to_vec(),
+            m: 2,
+            n: 2,
         },
         Input {
-            operations: ["++X", "++X", "X++"].map(|s| s.to_string()).to_vec(),
+            original: [1, 2, 3].to_vec(),
+            m: 1,
+            n: 3,
         },
         Input {
-            operations: ["X++", "++X", "--X", "X--"].map(|s| s.to_string()).to_vec(),
+            original: [1, 2].to_vec(),
+            m: 1,
+            n: 1,
         },
     ];
 
     for input in inputs {
-        let result = Solution::final_value_after_operations(input.operations);
+        let result = Solution::construct2_d_array(input.original, input.m, input.n);
         println!("{:?}", result);
     }
 }
