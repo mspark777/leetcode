705b3c71bf0a77293315598764ca4726272e2528
mspark777 <topangel777@gmail.com>
2025-09-17 22:35:18 +0900

    2032. Two Out of Three

--- src/py/main.py
+++ src/py/main.py
@@ -1,37 +1,37 @@
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
-    def countQuadruplets(self, nums: List[int]) -> int:
-        result = 0
-        n = len(nums)
-        seen = defaultdict(int)
-        for a in range(n - 1, 0, -1):
-            for b in range(a - 1, -1, -1):
-                result += seen[nums[a] + nums[b]]
-            for d in range(n - 1, a, -1):
-                seen[nums[d] - nums[a]] += 1
-
-        return result
+    def twoOutOfThree(
+        self, nums1: List[int], nums2: List[int], nums3: List[int]
+    ) -> List[int]:
+        return list(
+            (set(nums1) & set(nums2))
+            | (set(nums2) & set(nums3))
+            | (set(nums1) & set(nums3))
+        )
 
 
 class Input:
-    nums: list[int]
+    nums1: list[int]
+    nums2: list[int]
+    nums3: list[int]
 
-    def __init__(self, nums: list[int]):
-        self.nums = nums
+    def __init__(self, nums1: list[int], nums2: list[int], nums3: list[int]):
+        self.nums1 = nums1
+        self.nums2 = nums2
+        self.nums3 = nums3
 
 
 def main():
     inputs = [
-        Input([1, 2, 3, 6]),
-        Input([3, 3, 6, 4, 5]),
-        Input([1, 1, 1, 3, 5]),
+        Input([1, 1, 3, 2], [2, 3], [3]),
+        Input([3, 1], [2, 3], [1, 2]),
+        Input([1, 2, 2], [4, 3, 3], [5]),
     ]
 
     for input in inputs:
-        result = Solution().countQuadruplets(input.nums)
+        result = Solution().twoOutOfThree(input.nums1, input.nums2, input.nums3)
         print(result)
 
 
