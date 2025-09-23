63bad53e3dcf7c42ed0083b1a408715de27cfb1d
mspark777 <topangel777@gmail.com>
2025-09-23 11:05:55 +0900

    2068. Check Whether Two Strings are Almost Equivalent

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,46 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_vowel_substrings(word: String) -> i32 {
-        let s = word.chars().collect::<Vec<char>>();
-        return Self::at_most_k(&s, 5) - Self::at_most_k(&s, 4);
-    }
-
-    fn at_most_k(s: &Vec<char>, k: i32) -> i32 {
-        let k = k as usize;
-        let mut result = 0;
-        let mut i = 0usize;
-        let mut counts = std::collections::HashMap::<char, i32>::new();
-
-        for (j, ch) in s.iter().cloned().enumerate() {
-            if !Self::is_vowel(ch) {
-                i = j + 1;
-                counts.clear();
-                continue;
-            }
-
-            counts.entry(ch).and_modify(|c| *c += 1).or_insert(1);
-            while counts.len() > k {
-                let left = s[i];
-                counts.entry(left).and_modify(|c| *c -= 1);
-                if let Some(&c) = counts.get(&left) {
-                    if c == 0 {
-                        counts.remove(&left);
-                    }
-                }
-                i += 1;
-            }
-
-            result += j + 1 - i;
-        }
-
-        result as i32
-    }
-
-    fn is_vowel(c: char) -> bool {
-        c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+    pub fn check_almost_equivalent(word1: String, word2: String) -> bool {
+        word1
+            .chars()
+            .zip(word2.chars())
+            .fold(vec![0; 26], |mut counts, (left, right)| {
+                let a = 'a' as usize;
+                let lcode = left as usize - a;
+                let rcode = right as usize - a;
+                counts[lcode] += 1;
+                counts[rcode] -= 1;
+                counts
+            })
+            .iter()
+            .all(|&c| c >= -3 && c <= 3)
     }
 }
 
 struct Input {
-    word: String,
+    word1: String,
+    word2: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            word: "aeiouu".to_string(),
+            word1: "aaaa".to_string(),
+            word2: "bccb".to_string(),
         },
         Input {
-            word: "unicornarihan".to_string(),
+            word1: "abcdeef".to_string(),
+            word2: "abaaacc".to_string(),
         },
         Input {
-            word: "cuaieuouac".to_string(),
+            word1: "cccddabba".to_string(),
+            word2: "babababab".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_vowel_substrings(input.word);
+        let result = Solution::check_almost_equivalent(input.word1, input.word2);
         println!("{:?}", result);
     }
 }
