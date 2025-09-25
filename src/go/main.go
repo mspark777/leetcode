ee760f5f4477aaffdc4e6225f7625726c9b63b04
mspark777 <topangel777@gmail.com>
2025-09-25 09:59:15 +0900

    2085. Count Common Words With One Occurrence

--- src/go/main.go
+++ src/go/main.go
@@ -4,43 +4,52 @@ import (
 	"fmt"
 )
 
-func minimumMoves(s string) int {
-	result := 0
-	i := 0
+func countWords(words1 []string, words2 []string) int {
+	counts := make(map[string]int)
+	for _, word := range words1 {
+		counts[word] += 1
+	}
 
-	ch := []rune(s)
-
-	for i < len(ch) {
-		if ch[i] == 'X' {
-			result += 1
-			i += 3
-		} else {
-			i += 1
+	for _, word := range words2 {
+		if count, ok := counts[word]; ok {
+			if count < 2 {
+				counts[word] -= 1
+			}
 		}
 	}
 
+	result := 0
+	for _, count := range counts {
+		if count == 0 {
+			result += 1
+		}
+	}
 	return result
 }
 
 type input struct {
-	s string
+	words1 []string
+	words2 []string
 }
 
 func main() {
 	inputs := []input{
 		{
-			s: "XXX",
+			words1: []string{"leetcode", "is", "amazing", "as", "is"},
+			words2: []string{"amazing", "leetcode", "is"},
 		},
 		{
-			s: "XXOX",
+			words1: []string{"b", "bb", "bbb"},
+			words2: []string{"a", "aa", "aaa"},
 		},
 		{
-			s: "OOOO",
+			words1: []string{"a", "ab"},
+			words2: []string{"a", "a", "a", "ab"},
 		},
 	}
 
 	for _, input := range inputs {
-		result := minimumMoves(input.s)
+		result := countWords(input.words1, input.words2)
 		fmt.Println(result)
 	}
 }
