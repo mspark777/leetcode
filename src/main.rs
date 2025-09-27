12e897f5ed8e1dc7e97ce5f4264b0016cce1976e
mspark777 <topangel777@gmail.com>
2025-09-27 17:51:23 +0900

    2099. Find Subsequence of Length K With the Largest Sum

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,39 @@
 struct Solution {}
 
 impl Solution {
-    pub fn target_indices(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut count = 0usize;
-        let mut less_than = 0;
+    pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut sort_by_num = nums.iter().cloned().enumerate().collect::<Vec<_>>();
+        sort_by_num.sort_by_key(|v| -v.1);
 
-        for n in nums.iter().cloned() {
-            if n == target {
-                count += 1;
-            } else if n < target {
-                less_than += 1;
-            }
-        }
-
-        let mut result = vec![0; count];
-        for i in 0..count {
-            result[i] = less_than;
-            less_than += 1;
-        }
-
-        result
+        let mut sort_by_idx = sort_by_num.iter().take(k as usize).collect::<Vec<_>>();
+        sort_by_idx.sort_by_key(|v| v.0);
+        sort_by_idx.iter().map(|v| v.1).collect()
     }
 }
 
 struct Input {
     nums: Vec<i32>,
-    target: i32,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 5, 2, 3].to_vec(),
-            target: 2,
+            nums: [2, 1, 3, 3].to_vec(),
+            k: 2,
         },
         Input {
-            nums: [1, 2, 5, 2, 3].to_vec(),
-            target: 3,
+            nums: [-1, -2, 3, 4].to_vec(),
+            k: 3,
         },
         Input {
-            nums: [1, 2, 5, 2, 3].to_vec(),
-            target: 5,
+            nums: [3, 4, 3, 3].to_vec(),
+            k: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::target_indices(input.nums, input.target);
+        let result = Solution::max_subsequence(input.nums, input.k);
         println!("{:?}", result);
     }
 }
