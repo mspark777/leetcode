a2da5afcb17d4766a101f9fb08d27ce49190dbf7
mspark777 <topangel777@gmail.com>
2025-09-28 16:00:36 +0900

    2114. Maximum Number of Words Found in Sentences

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,39 @@
 struct Solution {}
 
+fn to_count(s: &String) -> i32 {
+    s.split(' ').filter(|s| !s.is_empty()).count() as i32
+}
+
 impl Solution {
-    pub fn count_points(rings: String) -> i32 {
-        let mut masks = vec![0u32; 10];
-        let colors = rings.chars().step_by(2);
-        let roads = rings.chars().skip(1).step_by(2);
-        for (color, road) in colors.zip(roads) {
-            let road_num = road.to_digit(10).unwrap() as usize;
-            let color_mask = match color {
-                'R' => 1u32,
-                'G' => 2u32,
-                'B' => 4u32,
-                _ => unreachable!(),
-            };
-            masks[road_num] |= color_mask;
-        }
-
-        let mut result = 0;
-        for mask in masks.iter().cloned() {
-            if mask == 7 {
-                result += 1;
-            }
-        }
-
-        result
+    pub fn most_words_found(sentences: Vec<String>) -> i32 {
+        sentences.iter().map(to_count).max().unwrap()
     }
 }
 
 struct Input {
-    rings: String,
+    sentences: Vec<String>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            rings: "B0B6G0R6R0R6G9".to_string(),
+            sentences: [
+                "alice and bob love  leetcode",
+                "i think so too",
+                "this is great thanks   very much",
+            ]
+            .map(|s| s.to_string())
+            .to_vec(),
         },
         Input {
-            rings: "B0R0G0R9R0B0G0".to_string(),
-        },
-        Input {
-            rings: "G4".to_string(),
+            sentences: ["please wait", "continue to fight", "continue to win"]
+                .map(|s| s.to_string())
+                .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_points(input.rings);
+        let result = Solution::most_words_found(input.sentences);
         println!("{:?}", result);
     }
 }
