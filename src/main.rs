3f36c1569d5e4fc293c12bd22b1dd401fcab44bd
mspark777 <topangel777@gmail.com>
2025-09-28 16:08:40 +0900

    2119. A Number After a Double Reversal

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,20 @@
 struct Solution {}
 
-fn to_count(s: &String) -> i32 {
-    s.split(' ').filter(|s| !s.is_empty()).count() as i32
-}
-
 impl Solution {
-    pub fn most_words_found(sentences: Vec<String>) -> i32 {
-        sentences.iter().map(to_count).max().unwrap()
+    pub fn is_same_after_reversals(num: i32) -> bool {
+        (num == 0) || ((num % 10) != 0)
     }
 }
 
 struct Input {
-    sentences: Vec<String>,
+    num: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            sentences: [
-                "alice and bob love  leetcode",
-                "i think so too",
-                "this is great thanks   very much",
-            ]
-            .map(|s| s.to_string())
-            .to_vec(),
-        },
-        Input {
-            sentences: ["please wait", "continue to fight", "continue to win"]
-                .map(|s| s.to_string())
-                .to_vec(),
-        },
-    ];
+    let inputs = [Input { num: 526 }, Input { num: 1800 }, Input { num: 0 }];
 
     for input in inputs {
-        let result = Solution::most_words_found(input.sentences);
+        let result = Solution::is_same_after_reversals(input.num);
         println!("{:?}", result);
     }
 }
