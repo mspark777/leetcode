4cfbace04c3f1f4c2d0fc2600e56bd13084a61fb
mspark777 <topangel777@gmail.com>
2025-10-01 09:28:27 +0900

    2154. Keep Multiplying Found Values by Two

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_elements(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        if n < 1 {
-            return 0;
+    pub fn find_final_value(nums: Vec<i32>, original: i32) -> i32 {
+        let set = nums
+            .iter()
+            .cloned()
+            .collect::<std::collections::HashSet<_>>();
+
+        let mut result = original;
+        while set.contains(&result) {
+            result *= 2;
         }
 
-        let mut min = nums[0];
-        let mut max = nums[0];
-        let mut min_appear = 1usize;
-        let mut max_appear = 1usize;
-
-        for num in nums.iter().cloned().skip(1) {
-            if num < min {
-                min = num;
-                min_appear = 1;
-            } else if num == min {
-                min_appear += 1;
-            }
-
-            if max < num {
-                max = num;
-                max_appear = 1;
-            } else if max == num {
-                max_appear += 1;
-            }
-        }
-
-        let result = match min == max {
-            true => 0,
-            _ => n - min_appear - max_appear,
-        };
-
-        result as i32
+        result
     }
 }
 
 struct Input {
     nums: Vec<i32>,
+    original: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [11, 7, 2, 15].to_vec(),
+            nums: [5, 3, 6, 1, 12].to_vec(),
+            original: 3,
         },
         Input {
-            nums: [-3, 3, 3, 90].to_vec(),
+            nums: [2, 7, 9].to_vec(),
+            original: 4,
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_elements(input.nums);
+        let result = Solution::find_final_value(input.nums, input.original);
         println!("{:?}", result);
     }
 }
