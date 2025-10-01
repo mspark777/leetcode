029b12c16fb0f409ee27f217f8d9afd44eacb963
mspark777 <topangel777@gmail.com>
2025-10-01 09:14:14 +0900

    2148. Count Elements With Strictly Smaller and Greater Elements

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,58 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_cost(cost: Vec<i32>) -> i32 {
-        let mut cost = cost;
-        cost.sort_unstable();
-        let n = cost.len() % 3;
+    pub fn count_elements(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        if n < 1 {
+            return 0;
+        }
 
-        cost.iter()
-            .cloned()
-            .enumerate()
-            .fold(0, |acc, (i, c)| match (i % 3) != n {
-                true => acc + c,
-                _ => acc,
-            })
+        let mut min = nums[0];
+        let mut max = nums[0];
+        let mut min_appear = 1usize;
+        let mut max_appear = 1usize;
+
+        for num in nums.iter().cloned().skip(1) {
+            if num < min {
+                min = num;
+                min_appear = 1;
+            } else if num == min {
+                min_appear += 1;
+            }
+
+            if max < num {
+                max = num;
+                max_appear = 1;
+            } else if max == num {
+                max_appear += 1;
+            }
+        }
+
+        let result = match min == max {
+            true => 0,
+            _ => n - min_appear - max_appear,
+        };
+
+        result as i32
     }
 }
 
 struct Input {
-    cost: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            cost: [1, 2, 3].to_vec(),
+            nums: [11, 7, 2, 15].to_vec(),
         },
         Input {
-            cost: [6, 5, 7, 9, 2, 2].to_vec(),
-        },
-        Input {
-            cost: [5, 5].to_vec(),
+            nums: [-3, 3, 3, 90].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_cost(input.cost);
+        let result = Solution::count_elements(input.nums);
         println!("{:?}", result);
     }
 }
