621affeb970caffdceae14192fcb5fbec54d1ece
mspark777 <topangel777@gmail.com>
2025-10-02 10:38:42 +0900

    2164. Sort Even and Odd Indices Independently

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_sum(num: i32) -> i32 {
-        let mut digits: Vec<i32> = num
-            .to_string()
-            .chars()
-            .map(|c| c.to_digit(10).unwrap() as i32)
-            .collect();
-        digits.sort();
+    pub fn sort_even_odd(nums: Vec<i32>) -> Vec<i32> {
+        let mut odd = Vec::<i32>::with_capacity(nums.len() / 2);
+        let mut even = Vec::<i32>::with_capacity(nums.len() / 2 + (nums.len() & 1));
+        for (i, n) in nums.iter().cloned().enumerate() {
+            if i & 1 == 1 {
+                odd.push(n);
+            } else {
+                even.push(n);
+            }
+        }
 
-        (digits[0] + digits[1]) * 10 + digits[2] + digits[3]
+        even.sort_unstable();
+        odd.sort_unstable_by_key(|n| -n);
+        let mut result = Vec::<i32>::with_capacity(nums.len());
+        for (e, o) in even.iter().cloned().zip(odd.iter().cloned()) {
+            result.push(e);
+            result.push(o);
+        }
+
+        if nums.len() & 1 == 1 {
+            result.push(*even.last().unwrap());
+        }
+
+        result
     }
 }
 
 struct Input {
-    num: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { num: 2932 }, Input { num: 4009 }];
+    let inputs = [
+        Input {
+            nums: [4, 1, 2, 3].to_vec(),
+        },
+        Input {
+            nums: [2, 1].to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::minimum_sum(input.num);
+        let result = Solution::sort_even_odd(input.nums);
         println!("{:?}", result);
     }
 }
