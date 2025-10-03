bbece84fdce7b5c8eeaa0e1d7a47226fd851ebe1
mspark777 <topangel777@gmail.com>
2025-10-03 17:08:51 +0900

    2180. Count Integers With Even Digit Sum

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,31 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
-        let k = k as usize;
-        let n = nums.len();
-        let mut result = 0;
-        for i in 0..n - 1 {
-            for j in i + 1..n {
-                if (((i * j) % k) == 0) && (nums[i] == nums[j]) {
-                    result += 1;
-                }
-            }
+    pub fn count_even(num: i32) -> i32 {
+        let temp = num;
+        let mut sum = 0;
+        let mut num = num;
+        while num > 0 {
+            sum += num % 10;
+            num /= 10;
         }
-        result
+
+        match sum & 1 {
+            0 => temp / 2,
+            _ => (temp - 1) / 2,
+        }
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    num: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [3, 1, 2, 2, 2, 1, 3].to_vec(),
-            k: 2,
-        },
-        Input {
-            nums: [1, 2, 3, 4].to_vec(),
-            k: 1,
-        },
-    ];
+    let inputs = [Input { num: 4 }, Input { num: 30 }];
 
     for input in inputs {
-        let result = Solution::count_pairs(input.nums, input.k);
+        let result = Solution::count_even(input.num);
         println!("{:?}", result);
     }
 }
