94afdf5c9d5e17c13d5136e4ced5c415acc69470
mspark777 <topangel777@gmail.com>
2025-10-03 16:12:24 +0900

    2176. Count Equal and Divisible Pairs in an Array

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_operations(num1: i32, num2: i32) -> i32 {
+    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let k = k as usize;
+        let n = nums.len();
         let mut result = 0;
-        let mut num1 = num1;
-        let mut num2 = num2;
-
-        while (num1 != 0) && (num2 != 0) {
-            if num1 >= num2 {
-                num1 -= num2;
-            } else {
-                num2 -= num1;
+        for i in 0..n - 1 {
+            for j in i + 1..n {
+                if (((i * j) % k) == 0) && (nums[i] == nums[j]) {
+                    result += 1;
+                }
             }
-            result += 1;
         }
-
         result
     }
 }
 
 struct Input {
-    num1: i32,
-    num2: i32,
+    nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [Input { num1: 2, num2: 3 }, Input { num1: 10, num2: 10 }];
+    let inputs = [
+        Input {
+            nums: [3, 1, 2, 2, 2, 1, 3].to_vec(),
+            k: 2,
+        },
+        Input {
+            nums: [1, 2, 3, 4].to_vec(),
+            k: 1,
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::count_operations(input.num1, input.num2);
+        let result = Solution::count_pairs(input.nums, input.k);
         println!("{:?}", result);
     }
 }
