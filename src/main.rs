dcd63e8c213bc5ac72f0199a733882ed8bddf139
mspark777 <topangel777@gmail.com>
2025-10-04 15:11:49 +0900

    2190. Most Frequent Number Following Key In an Array

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_even(num: i32) -> i32 {
-        let temp = num;
-        let mut sum = 0;
-        let mut num = num;
-        while num > 0 {
-            sum += num % 10;
-            num /= 10;
-        }
+    pub fn most_frequent(nums: Vec<i32>, key: i32) -> i32 {
+        let mut map = std::collections::HashMap::<i32, i32>::new();
+        let mut max = 0;
+        let mut result = 0;
+        for i in 1..nums.len() {
+            let prev = nums[i - 1];
+            let num = nums[i];
+            if prev != key {
+                continue;
+            }
 
-        match sum & 1 {
-            0 => temp / 2,
-            _ => (temp - 1) / 2,
+            let count = *map.entry(num).and_modify(|count| *count += 1).or_insert(1);
+            if count > max {
+                max = count;
+                result = num;
+            }
         }
+        result
     }
 }
 
 struct Input {
-    num: i32,
+    nums: Vec<i32>,
+    key: i32,
 }
 
 fn main() {
-    let inputs = [Input { num: 4 }, Input { num: 30 }];
+    let inputs = [
+        Input {
+            nums: [1, 100, 200, 1, 100].to_vec(),
+            key: 1,
+        },
+        Input {
+            nums: [2, 2, 2, 2, 3].to_vec(),
+            key: 2,
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::count_even(input.num);
+        let result = Solution::most_frequent(input.nums, input.key);
         println!("{:?}", result);
     }
 }
