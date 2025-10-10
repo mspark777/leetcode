cf0e76d0a367fd91c967449738ab6fb4ae6e6649
mspark777 <topangel777@gmail.com>
2025-10-10 10:04:22 +0900

    2224. Minimum Number of Operations to Convert Time

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,56 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_hill_valley(nums: Vec<i32>) -> i32 {
-        let mut result = 0;
-        let mut left = 0usize;
+    pub fn convert_time(current: String, correct: String) -> i32 {
+        let curr = Self::to_int(current.as_str());
+        let correct = Self::to_int(correct.as_str());
+        let mut count = 0;
 
-        for i in 1..(nums.len() - 1) {
-            if nums[i] == nums[i + 1] {
-                continue;
-            }
-
-            if (nums[i] > nums[left] && nums[i] > nums[i + 1])
-                || (nums[i] < nums[left] && nums[i] < nums[i + 1])
-            {
-                result += 1;
-            }
-            left = i;
+        let mut diff = correct - curr;
+        for by in [60, 15, 5] {
+            let temp = diff / by;
+            count += temp;
+            diff -= temp * by;
         }
 
-        result
+        count + diff
+    }
+
+    fn to_int(s: &str) -> i32 {
+        let mut chunks = s.split(':');
+        let left = match chunks.next() {
+            Some(v) => v.parse::<i32>().unwrap(),
+            _ => 0,
+        };
+
+        let right = match chunks.next() {
+            Some(v) => v.parse::<i32>().unwrap(),
+            _ => 0,
+        };
+
+        left * 60 + right
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    current: String,
+    correct: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [2, 4, 1, 1, 6, 5].to_vec(),
+            current: "02:30".to_string(),
+            correct: "04:35".to_string(),
         },
         Input {
-            nums: [6, 6, 5, 5, 4, 1].to_vec(),
+            current: "11:00".to_string(),
+            correct: "11:01".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_hill_valley(input.nums);
+        let result = Solution::convert_time(input.current, input.correct);
         println!("{:?}", result);
     }
 }
