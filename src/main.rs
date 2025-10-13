ef59eb52ebba932aba37634db3ddca44776ed275
mspark777 <topangel777@gmail.com>
2025-10-13 22:29:18 +0900

    2243. Calculate Digit Sum of a String

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_closest_number(nums: Vec<i32>) -> i32 {
-        nums.iter()
-            .cloned()
-            .fold((-100000, 100000), |(result, distance), cur| {
-                let d = cur.abs();
-                if d < distance {
-                    (cur, d)
-                } else if d == distance {
-                    (result.max(cur), d)
-                } else {
-                    (result, distance)
+    pub fn digit_sum(s: String, k: i32) -> String {
+        let k = k as usize;
+        let mut nums = s.chars().collect::<Vec<char>>();
+
+        while nums.len() > k {
+            let mut sums = Vec::<char>::with_capacity((nums.len() / k) + 1);
+            for chunk in nums.chunks(k) {
+                let sum = chunk
+                    .iter()
+                    .fold(0u32, |acc, x| acc + x.to_digit(10).unwrap());
+
+                for ch in sum.to_string().chars() {
+                    sums.push(ch);
                 }
-            })
-            .0
+            }
+
+            nums = sums;
+        }
+
+        nums.iter().collect()
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [-4, -2, 1, 4, 8].to_vec(),
+            s: "11111222223".to_string(),
+            k: 3,
         },
         Input {
-            nums: [2, -1, 1].to_vec(),
+            s: "00000000".to_string(),
+            k: 3,
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_closest_number(input.nums);
+        let result = Solution::digit_sum(input.s, input.k);
         println!("{:?}", result);
     }
 }
