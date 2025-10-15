cbf32f050f0bc4a1185d272a8e469421ead99047
mspark777 <topangel777@gmail.com>
2025-10-15 11:22:47 +0900

    2273. Find Resultant Array After Removing Anagrams

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,57 @@
 struct Solution {}
 
 impl Solution {
-    pub fn divisor_substrings(num: i32, k: i32) -> i32 {
-        let mut result = 0;
-        let mut cur = 0;
-        let mut pow = 1;
-        let mut n = num;
-        let mut k = k;
-        while n > 0 {
-            cur += (n % 10) * pow;
-            k -= 1;
-            if k > 0 {
-                pow *= 10;
-            } else {
-                if cur > 0 && num % cur == 0 {
-                    result += 1
-                }
-                cur /= 10;
-            }
+    pub fn remove_anagrams(words: Vec<String>) -> Vec<String> {
+        let mut result = Vec::<String>::new();
+        result.push(words[0].clone());
+        let mut prev = Self::prime_hash(words[0].as_str());
 
-            n /= 10;
+        for word in words.iter().skip(1) {
+            let hash = Self::prime_hash(word.as_str());
+            if hash != prev {
+                result.push(word.clone());
+                prev = hash;
+            }
         }
 
         result
     }
+
+    fn prime_hash(s: &str) -> i32 {
+        let primes = [
+            1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
+            83, 89, 97,
+        ];
+
+        const MOD: i64 = 1000000007;
+        let mut hash = 1i64;
+        for c in s.chars() {
+            let code = c as usize;
+            let idx = code - 'a' as usize;
+            hash = (hash * primes[idx]) % MOD;
+        }
+        hash as i32
+    }
 }
 
 struct Input {
-    num: i32,
-    k: i32,
+    words: Vec<String>,
 }
 
 fn main() {
-    let inputs = [Input { num: 240, k: 2 }, Input { num: 430043, k: 2 }];
+    let inputs = [
+        Input {
+            words: ["abba", "baba", "bbaa", "cd", "cd"]
+                .map(|s| s.to_string())
+                .to_vec(),
+        },
+        Input {
+            words: ["a", "b", "c", "d", "e"].map(|s| s.to_string()).to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::divisor_substrings(input.num, input.k);
+        let result = Solution::remove_anagrams(input.words);
         println!("{:?}", result);
     }
 }
