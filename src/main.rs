89bd8cb682d0a8e195726f498219afe856d883ae
mspark777 <topangel777@gmail.com>
2025-10-21 09:48:22 +0900

    2335. Minimum Amount of Time to Fill Cups

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,37 @@
 struct Solution {}
 
 impl Solution {
-    pub fn decode_message(key: String, message: String) -> String {
-        let mut ch_map = std::collections::HashMap::<char, char>::with_capacity(26);
-        let mut ch_code = 'a' as u8;
-
-        for key in key.chars() {
-            if key == ' ' {
-                continue;
-            } else if ch_map.contains_key(&key) {
-                continue;
-            } else {
-                let ch = ch_code as char;
-                ch_map.insert(key, ch);
-                ch_code += 1;
-            }
+    pub fn fill_cups(amount: Vec<i32>) -> i32 {
+        let mut max_count = 0;
+        let mut sum = 0;
+        for a in amount.iter().cloned() {
+            max_count = max_count.max(a);
+            sum += a;
         }
 
-        let mut result = Vec::<char>::with_capacity(message.len());
-        for msg in message.chars() {
-            if msg == ' ' {
-                result.push(msg);
-            } else if let Some(&ch) = ch_map.get(&msg) {
-                result.push(ch);
-            }
-        }
-
-        result.iter().collect()
+        max_count.max((sum + 1) / 2)
     }
 }
 
 struct Input {
-    key: String,
-    message: String,
+    amount: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            key: "the quick brown fox jumps over the lazy dog".to_string(),
-            message: "vkbs bs t suepuv".to_string(),
+            amount: [1, 4, 2].to_vec(),
         },
         Input {
-            key: "eljuxhpwnyrdgtqkviszcfmabo".to_string(),
-            message: "zwx hnfx lqantp mnoeius ycgk vcnjrdb".to_string(),
+            amount: [5, 4, 4].to_vec(),
+        },
+        Input {
+            amount: [5, 0, 0].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::decode_message(input.key, input.message);
+        let result = Solution::fill_cups(input.amount);
         println!("{:?}", result);
     }
 }
