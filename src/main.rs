ea05b216f9471a24965bed5af7e6e13d82525551
mspark777 <topangel777@gmail.com>
2025-10-24 16:51:02 +0900

    2351. First Letter to Appear Twice

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,40 @@
 struct Solution {}
 
 impl Solution {
-    pub fn best_hand(ranks: Vec<i32>, suits: Vec<char>) -> String {
-        let flush_set = std::collections::HashSet::<char>::from_iter(suits.iter().cloned());
-        if flush_set.len() == 1 {
-            return "Flush".to_string();
+    pub fn repeated_character(s: String) -> char {
+        let mut checks = vec![false; 26];
+
+        for c in s.chars() {
+            let code = c as usize;
+            let a = 'a' as usize;
+            let idx = code - a;
+            if checks[idx] {
+                return c;
+            }
+
+            checks[idx] = true
         }
 
-        let mut same_ranks = std::collections::HashMap::<i32, i32>::with_capacity(ranks.len());
-        let mut same_count = 0;
-        for rank in ranks.iter().cloned() {
-            let count = same_ranks.entry(rank).and_modify(|e| *e += 1).or_insert(1);
-            same_count = same_count.max(*count);
-        }
-
-        return match same_count {
-            2 => "Pair",
-            1 | 0 => "High Card",
-            _ => "Three of a Kind",
-        }
-        .to_string();
+        return ' ';
     }
 }
 
 struct Input {
-    ranks: Vec<i32>,
-    suits: Vec<char>,
+    s: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            ranks: [13, 2, 3, 1, 9].to_vec(),
-            suits: ['a', 'a', 'a', 'a', 'a'].to_vec(),
+            s: "abccbaacz".to_string(),
         },
         Input {
-            ranks: [4, 4, 2, 4, 4].to_vec(),
-            suits: ['d', 'a', 'a', 'b', 'c'].to_vec(),
-        },
-        Input {
-            ranks: [10, 10, 2, 12, 9].to_vec(),
-            suits: ['a', 'b', 'c', 'a', 'd'].to_vec(),
+            s: "abcdd".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::best_hand(input.ranks, input.suits);
+        let result = Solution::repeated_character(input.s);
         println!("{:?}", result);
     }
 }
