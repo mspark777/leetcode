3ecca049b78daed5b170de68261423c77a0a2d73
mspark777 <topangel777@gmail.com>
2025-10-29 11:34:32 +0900

    2395. Find Subarrays With Equal Sum

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,44 @@
 struct Solution {}
 
 impl Solution {
-    pub fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
-        let mut accumulates = nums.clone();
-        accumulates.sort_unstable();
-
-        for i in 1..accumulates.len() {
-            accumulates[i] += accumulates[i - 1];
+    pub fn find_subarrays(nums: Vec<i32>) -> bool {
+        if nums.len() < 3 {
+            return false;
         }
 
-        let mut result = vec![0; queries.len()];
-        for (i, query) in queries.iter().cloned().enumerate() {
-            let mut left = 0usize;
-            let mut right = nums.len();
-
-            while left < right {
-                let midx = (left + right) / 2;
-                if accumulates[midx] <= query {
-                    left = midx + 1;
-                    result[i] = left as i32;
-                } else {
-                    right = midx;
-                }
+        let mut sums = std::collections::HashSet::<i32>::with_capacity(nums.len() - 1);
+        for (left, right) in nums.iter().cloned().zip(nums.iter().skip(1).cloned()) {
+            let sum = left + right;
+            if sums.contains(&sum) {
+                return true;
+            } else {
+                sums.insert(sum);
             }
         }
 
-        result
+        return false;
     }
 }
 
 struct Input {
     nums: Vec<i32>,
-    queries: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [4, 5, 2, 1].to_vec(),
-            queries: [3, 10, 21].to_vec(),
+            nums: [4, 2, 4].to_vec(),
         },
         Input {
-            nums: [2, 3, 4, 5].to_vec(),
-            queries: [1].to_vec(),
+            nums: [1, 2, 3, 4, 5].to_vec(),
+        },
+        Input {
+            nums: [0, 0, 0].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::answer_queries(input.nums, input.queries);
+        let result = Solution::find_subarrays(input.nums);
         println!("{:?}", result);
     }
 }
