4b4b8498f6678d76508e4cc03ed3cd170166d7a8
mspark777 <topangel777@gmail.com>
2025-10-29 11:49:04 +0900

    2399. Check Distances Between Same Letters

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,56 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_subarrays(nums: Vec<i32>) -> bool {
-        if nums.len() < 3 {
-            return false;
-        }
+    pub fn check_distances(s: String, distance: Vec<i32>) -> bool {
+        let mut indexes = vec![0; 26];
 
-        let mut sums = std::collections::HashSet::<i32>::with_capacity(nums.len() - 1);
-        for (left, right) in nums.iter().cloned().zip(nums.iter().skip(1).cloned()) {
-            let sum = left + right;
-            if sums.contains(&sum) {
-                return true;
-            } else {
-                sums.insert(sum);
+        for (i, ch) in s.chars().enumerate() {
+            let i = i as i32;
+            let a = 'a' as usize;
+            let code = ch as usize;
+            let idx = code - a;
+
+            let pos = indexes[idx];
+            if pos == 0 {
+                indexes[idx] -= i + 1;
+                continue;
+            }
+
+            let diff = i + pos;
+            if distance[idx] != diff {
+                return false;
             }
         }
 
-        return false;
+        return true;
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
+    distance: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [4, 2, 4].to_vec(),
+            s: "abaccb".to_string(),
+            distance: [
+                1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ]
+            .to_vec(),
         },
         Input {
-            nums: [1, 2, 3, 4, 5].to_vec(),
-        },
-        Input {
-            nums: [0, 0, 0].to_vec(),
+            s: "aa".to_string(),
+            distance: [
+                1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ]
+            .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_subarrays(input.nums);
+        let result = Solution::check_distances(input.s, input.distance);
         println!("{:?}", result);
     }
 }
