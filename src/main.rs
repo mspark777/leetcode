a69ef514e9c25f24b2ee897566c93980fbb3284b
mspark777 <topangel777@gmail.com>
2025-11-04 11:17:06 +0900

    2469. Convert the Temperature

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,20 @@
 struct Solution {}
 
 impl Solution {
-    pub fn distinct_averages(nums: Vec<i32>) -> i32 {
-        let mut nums = nums;
-        nums.sort_unstable();
-
-        let mut sum_set = std::collections::HashSet::<String>::new();
-        let last = nums.len() - 1;
-        for l in 0..last {
-            let r = last - l;
-            if l >= r {
-                break;
-            }
-
-            let min = nums[l];
-            let max = nums[r];
-            let sum = (max + min) as f64;
-            sum_set.insert((sum / 2.0).to_string());
-        }
-
-        sum_set.len() as i32
+    pub fn convert_temperature(celsius: f64) -> Vec<f64> {
+        vec![celsius + 273.15, celsius * 1.8 + 32.0]
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    celsius: f64,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [4, 1, 4, 0, 3, 5].to_vec(),
-        },
-        Input {
-            nums: [1, 100].to_vec(),
-        },
-    ];
+    let inputs = [Input { celsius: 36.50 }, Input { celsius: 122.11 }];
 
     for input in inputs {
-        let result = Solution::distinct_averages(input.nums);
+        let result = Solution::convert_temperature(input.celsius);
         println!("{:?}", result);
     }
 }
