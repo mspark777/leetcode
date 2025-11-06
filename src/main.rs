114c6f353708340cba03c0f271d5cd8f62524159
mspark777 <topangel777@gmail.com>
2025-11-07 00:37:25 +0900

    2500. Delete Greatest Value in Each Row

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
 struct Solution {}
 
 impl Solution {
-    pub fn maximum_value(strs: Vec<String>) -> i32 {
-        strs.iter()
-            .map(|s| match s.parse::<i32>() {
-                Ok(i) => i,
-                _ => s.len() as i32,
-            })
-            .max()
-            .unwrap_or(0)
+    pub fn delete_greatest_value(grid: Vec<Vec<i32>>) -> i32 {
+        let mut grid = grid;
+        for row in grid.iter_mut() {
+            row.sort_unstable();
+        }
+
+        let mut result = 0;
+        for i in 0..grid[0].len() {
+            let mut m = 0;
+            for row in grid.iter() {
+                m = m.max(row[i]);
+            }
+
+            result += m;
+        }
+        result
     }
 }
 
 struct Input {
-    strs: Vec<String>,
+    grid: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            strs: ["alic3", "bob", "3", "4", "00000"]
-                .map(|s| s.to_string())
-                .to_vec(),
+            grid: [[1, 2, 4], [3, 3, 1]].map(|r| r.to_vec()).to_vec(),
         },
         Input {
-            strs: ["1", "01", "001", "0001"].map(|s| s.to_string()).to_vec(),
+            grid: [[10]].map(|r| r.to_vec()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::maximum_value(input.strs);
+        let result = Solution::delete_greatest_value(input.grid);
         println!("{:?}", result);
     }
 }
