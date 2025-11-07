a68dca94bb66da247e4bd40b0c39f2af4d4d0c56
mspark777 <topangel777@gmail.com>
2025-11-07 12:27:59 +0900

    2506. Count Pairs Of Similar Strings

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
 struct Solution {}
 
 impl Solution {
-    pub fn delete_greatest_value(grid: Vec<Vec<i32>>) -> i32 {
-        let mut grid = grid;
-        for row in grid.iter_mut() {
-            row.sort_unstable();
-        }
-
+    pub fn similar_pairs(words: Vec<String>) -> i32 {
         let mut result = 0;
-        for i in 0..grid[0].len() {
-            let mut m = 0;
-            for row in grid.iter() {
-                m = m.max(row[i]);
+        let mut masks = std::collections::HashMap::<u32, i32>::new();
+
+        for word in words.iter() {
+            let mut mask = 0u32;
+            for ch in word.chars() {
+                let a = 'a' as u32;
+                let code = ch as u32;
+                let shift = code - a;
+                mask |= 1 << shift;
             }
 
-            result += m;
+            let count = masks.entry(mask).or_insert(0);
+            result += *count;
+            *count += 1;
         }
+
         result
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    words: Vec<String>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            grid: [[1, 2, 4], [3, 3, 1]].map(|r| r.to_vec()).to_vec(),
+            words: ["aba", "aabb", "abcd", "bac", "aabc"]
+                .map(|r| r.to_string())
+                .to_vec(),
         },
         Input {
-            grid: [[10]].map(|r| r.to_vec()).to_vec(),
+            words: ["aabb", "ab", "ba"].map(|r| r.to_string()).to_vec(),
+        },
+        Input {
+            words: ["nba", "cba", "dba"].map(|r| r.to_string()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::delete_greatest_value(input.grid);
+        let result = Solution::similar_pairs(input.words);
         println!("{:?}", result);
     }
 }
