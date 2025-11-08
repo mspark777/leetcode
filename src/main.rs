10d479ed2638801092152fe31b6787c11b421a0f
mspark777 <topangel777@gmail.com>
2025-11-08 15:43:13 +0900

    2515. Shortest Distance to Target String in a Circular Array

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,58 @@
 struct Solution {}
 
 impl Solution {
-    pub fn capture_forts(forts: Vec<i32>) -> i32 {
-        const ENEMY: i32 = 0;
+    pub fn closest_target(words: Vec<String>, target: String, start_index: i32) -> i32 {
+        let n = words.len() as i32;
+        let mut shortest = i32::MAX;
 
-        let mut result = 0i32;
-        let mut j = 0usize;
-        let n = forts.len();
-        for i in 0..n {
-            if forts[i] != ENEMY {
-                if forts[j] == -forts[i] {
-                    result = result.max(((i - j) as i32) - 1)
-                }
-
-                j = i;
+        for (i, word) in words.iter().enumerate() {
+            if word.as_str() != target.as_str() {
+                continue;
             }
+
+            let i = i as i32;
+            let to_target = (start_index - i).abs();
+            let to_starting = (n - to_target).abs();
+
+            shortest = shortest.min(to_target).min(to_starting);
         }
 
-        result
+        match shortest {
+            i32::MAX => -1,
+            _ => shortest,
+        }
     }
 }
 
 struct Input {
-    forts: Vec<i32>,
+    words: Vec<String>,
+    target: String,
+    start_index: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            forts: [1, 0, 0, -1, 0, 0, 0, 0, 1].to_vec(),
+            words: ["hello", "i", "am", "leetcode", "hello"]
+                .map(|s| s.to_string())
+                .to_vec(),
+            target: "hello".to_string(),
+            start_index: 1,
         },
         Input {
-            forts: [0, 0, 1, -1].to_vec(),
+            words: ["a", "b", "leetcode"].map(|s| s.to_string()).to_vec(),
+            target: "leetcode".to_string(),
+            start_index: 0,
+        },
+        Input {
+            words: ["i", "eat", "leetcode"].map(|s| s.to_string()).to_vec(),
+            target: "ate".to_string(),
+            start_index: 0,
         },
     ];
 
     for input in inputs {
-        let result = Solution::capture_forts(input.forts);
+        let result = Solution::closest_target(input.words, input.target, input.start_index);
         println!("{:?}", result);
     }
 }
