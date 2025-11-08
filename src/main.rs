41f46de460e2d04bd0fdb2dc33058fa142cf9f3f
mspark777 <topangel777@gmail.com>
2025-11-08 16:01:45 +0900

    2525. Categorize Box According to Criteria

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,58 @@
 struct Solution {}
 
 impl Solution {
-    pub fn count_digits(num: i32) -> i32 {
-        let mut result = 0;
-        let mut digits = num;
+    pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
+        let bulkey = Self::is_bulkey(length, width, height);
+        let heavy = mass >= 100;
 
-        while digits > 0 {
-            let digit = digits % 10;
-            digits /= 10;
-            if digit == 0 {
-                continue;
-            } else if (num % digit) == 0 {
-                result += 1;
-            }
+        match (bulkey, heavy) {
+            (true, true) => "Both".to_string(),
+            (true, false) => "Bulky".to_string(),
+            (false, true) => "Heavy".to_string(),
+            _ => "Neither".to_string(),
+        }
+    }
+
+    fn is_bulkey(length: i32, width: i32, height: i32) -> bool {
+        const MAX_DIMESTION: i32 = 10_000;
+        if length >= MAX_DIMESTION {
+            return true;
+        } else if width >= MAX_DIMESTION {
+            return true;
+        } else if height >= MAX_DIMESTION {
+            return true;
         }
 
-        result
+        let volume = (length as i64) * (width as i64) * (height as i64);
+        volume >= 1_000_000_000
     }
 }
 
 struct Input {
-    num: i32,
+    length: i32,
+    width: i32,
+    height: i32,
+    mass: i32,
 }
 
 fn main() {
-    let inputs = [Input { num: 7 }, Input { num: 121 }, Input { num: 1248 }];
+    let inputs = [
+        Input {
+            length: 1000,
+            width: 35,
+            height: 700,
+            mass: 300,
+        },
+        Input {
+            length: 200,
+            width: 50,
+            height: 800,
+            mass: 50,
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::count_digits(input.num);
+        let result = Solution::categorize_box(input.length, input.width, input.height, input.mass);
         println!("{:?}", result);
     }
 }
