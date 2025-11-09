869eb7c881c1574c8158011ec93ab30e3571218a
mspark777 <topangel777@gmail.com>
2025-11-09 15:52:44 +0900

    2535. Difference Between Element Sum and Digit Sum of an Array

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,43 @@
 struct Solution {}
 
 impl Solution {
-    pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
-        let bulkey = Self::is_bulkey(length, width, height);
-        let heavy = mass >= 100;
-
-        match (bulkey, heavy) {
-            (true, true) => "Both".to_string(),
-            (true, false) => "Bulky".to_string(),
-            (false, true) => "Heavy".to_string(),
-            _ => "Neither".to_string(),
+    pub fn difference_of_sum(nums: Vec<i32>) -> i32 {
+        let mut element_sum = 0;
+        let mut digit_sum = 0;
+        for num in nums.iter().cloned() {
+            element_sum += num;
+            digit_sum += Self::digit_sum(num);
         }
+
+        (element_sum - digit_sum).abs()
     }
 
-    fn is_bulkey(length: i32, width: i32, height: i32) -> bool {
-        const MAX_DIMESTION: i32 = 10_000;
-        if length >= MAX_DIMESTION {
-            return true;
-        } else if width >= MAX_DIMESTION {
-            return true;
-        } else if height >= MAX_DIMESTION {
-            return true;
+    fn digit_sum(mut num: i32) -> i32 {
+        let mut sum = 0;
+        while num > 0 {
+            sum += num % 10;
+            num /= 10;
         }
-
-        let volume = (length as i64) * (width as i64) * (height as i64);
-        volume >= 1_000_000_000
+        sum
     }
 }
 
 struct Input {
-    length: i32,
-    width: i32,
-    height: i32,
-    mass: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            length: 1000,
-            width: 35,
-            height: 700,
-            mass: 300,
+            nums: [1, 15, 6, 3].to_vec(),
         },
         Input {
-            length: 200,
-            width: 50,
-            height: 800,
-            mass: 50,
+            nums: [1, 2, 3, 4].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::categorize_box(input.length, input.width, input.height, input.mass);
+        let result = Solution::difference_of_sum(input.nums);
         println!("{:?}", result);
     }
 }
