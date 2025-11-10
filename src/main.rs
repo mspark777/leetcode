103f275d8c50f2875df47aad6485b6a0783ea17d
mspark777 <topangel777@gmail.com>
2025-11-10 10:56:10 +0900

    2562. Find the Array Concatenation Value

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 struct Solution {}
 
 impl Solution {
-    pub fn separate_digits(nums: Vec<i32>) -> Vec<i32> {
-        let mut digits = Vec::<i32>::new();
-        for num in nums.iter().rev().cloned() {
-            let mut n = num;
-            while n > 0 {
-                let digit = n % 10;
-                n /= 10;
-                digits.push(digit);
-            }
+    pub fn find_the_array_conc_val(nums: Vec<i32>) -> i64 {
+        let n = nums.len();
+        let mut left = 0usize;
+        let mut right = n - 1;
+        let mut result = 0i64;
+
+        while left < right {
+            let num = Self::concat(nums[left], nums[right]);
+            result += num;
+            left += 1;
+            right -= 1;
         }
 
-        digits.iter().rev().cloned().collect()
+        if (n & 1) == 1 {
+            result += nums[left] as i64;
+        }
+
+        result
+    }
+
+    fn concat(left: i32, right: i32) -> i64 {
+        let mut pow = 10;
+        while pow <= right {
+            pow *= 10
+        }
+
+        let left = (left * pow) as i64;
+        left + (right as i64)
     }
 }
 
@@ -23,15 +39,15 @@ struct Input {
 fn main() {
     let inputs = [
         Input {
-            nums: [13, 25, 83, 77].to_vec(),
+            nums: [7, 52, 2, 4].to_vec(),
         },
         Input {
-            nums: [7, 1, 3, 9].to_vec(),
+            nums: [5, 14, 13, 8, 12].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::separate_digits(input.nums);
+        let result = Solution::find_the_array_conc_val(input.nums);
         println!("{:?}", result);
     }
 }
