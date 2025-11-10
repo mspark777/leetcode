454682cbfccaf41a0f07f2075d4180d2db4df720
mspark777 <topangel777@gmail.com>
2025-11-10 10:39:49 +0900

    2553. Separate the Digits in an Array

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module mspark777/lettcode
 
-go 1.25.3
+go 1.25.4
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 struct Solution {}
 
 impl Solution {
-    pub fn distinct_integers(n: i32) -> i32 {
-        1.max(n - 1)
+    pub fn separate_digits(nums: Vec<i32>) -> Vec<i32> {
+        let mut digits = Vec::<i32>::new();
+        for num in nums.iter().rev().cloned() {
+            let mut n = num;
+            while n > 0 {
+                let digit = n % 10;
+                n /= 10;
+                digits.push(digit);
+            }
+        }
+
+        digits.iter().rev().cloned().collect()
     }
 }
 
 struct Input {
-    n: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { n: 5 }, Input { n: 3 }, Input { n: 886996 }];
+    let inputs = [
+        Input {
+            nums: [13, 25, 83, 77].to_vec(),
+        },
+        Input {
+            nums: [7, 1, 3, 9].to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::distinct_integers(input.n);
+        let result = Solution::separate_digits(input.nums);
         println!("{:?}", result);
     }
 }
