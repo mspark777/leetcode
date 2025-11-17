0ebe4b9c471014b4d8a53d0e0a3db83fe8ea2880
mspark777 <topangel777@gmail.com>
2025-11-18 00:45:33 +0900

    2591. Distribute Money to Maximum Children

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,39 @@
 struct Solution {}
 
 impl Solution {
-    pub fn split_num(num: i32) -> i32 {
-        let mut nums: Vec<i32> = num
-            .to_string()
-            .chars()
-            .map(|c| ((c as u8) - b'0') as i32)
-            .collect();
-        nums.sort_unstable();
-
-        let mut num1 = 0;
-        let mut num2 = 0;
-
-        for i in (1..nums.len()).step_by(2) {
-            num1 = num1 * 10 + nums[i - 1];
-            num2 = num2 * 10 + nums[i];
+    pub fn dist_money(money: i32, children: i32) -> i32 {
+        let money = money - children;
+        if money < 0 {
+            return -1;
+        } else if ((money / 7) == children) && ((money % 7) == 0) {
+            return children;
+        } else if ((money / 7) == (children - 1)) && ((money % 7) == 3) {
+            return children - 2;
         }
 
-        if (nums.len() & 1) == 1 {
-            num1 = num1 * 10 + *nums.last().unwrap();
-        }
-
-        num1 + num2
+        (children - 1).min(money / 7)
     }
 }
 
 struct Input {
-    num: i32,
+    money: i32,
+    children: i32,
 }
 
 fn main() {
-    let inputs = [Input { num: 4325 }, Input { num: 687 }];
+    let inputs = [
+        Input {
+            money: 20,
+            children: 3,
+        },
+        Input {
+            money: 16,
+            children: 2,
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::split_num(input.num);
+        let result = Solution::dist_money(input.money, input.children);
         println!("{:?}", result);
     }
 }
