8ab81221b67b8b5c65572770ab5118db87939067
mspark777 <topangel777@gmail.com>
2025-11-25 10:31:17 +0900

    2643. Row With Maximum Ones

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,41 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_column_width(grid: Vec<Vec<i32>>) -> Vec<i32> {
-        let n = grid[0].len();
+    pub fn row_and_maximum_ones(mat: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut row_idx = 0usize;
+        let mut count_ones = 0usize;
 
-        let mut result = vec![0; n];
-        for i in 0..n {
-            let mut max_width = 0;
-            for row in grid.iter() {
-                max_width = max_width.max(Self::width(row[i]));
+        for (i, row) in mat.iter().enumerate() {
+            let count = row.iter().copied().filter(|&n| n == 1).count();
+            if count > count_ones {
+                row_idx = i;
+                count_ones = count
             }
-
-            result[i] = max_width;
         }
 
-        result
-    }
-
-    fn width(mut n: i32) -> i32 {
-        if n == 0 {
-            return 1;
-        }
-
-        let mut l = 0;
-        if n < 0 {
-            l += 1;
-            n *= -1;
-        }
-
-        while n > 0 {
-            n /= 10;
-            l += 1;
-        }
-
-        l
+        vec![row_idx as i32, count_ones as i32]
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    mat: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            grid: [[1], [22], [333]].map(|a| a.to_vec()).to_vec(),
+            mat: [[0, 1], [1, 0]].map(|a| a.to_vec()).to_vec(),
         },
         Input {
-            grid: [[-15, 1, 3], [15, 7, 12], [5, 6, -2]]
-                .map(|a| a.to_vec())
-                .to_vec(),
+            mat: [[0, 0, 0], [0, 1, 1]].map(|a| a.to_vec()).to_vec(),
+        },
+        Input {
+            mat: [[0, 0], [1, 1], [0, 0]].map(|a| a.to_vec()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_column_width(input.grid);
+        let result = Solution::row_and_maximum_ones(input.mat);
         println!("{:?}", result);
     }
 }
