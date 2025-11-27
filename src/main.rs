cac02d23a765641b25b8ed34e6cb3ab97b9eadf9
mspark777 <topangel777@gmail.com>
2025-11-27 09:29:33 +0900

    2660. Determine the Winner of a Bowling Game

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,62 @@
 struct Solution {}
 
 impl Solution {
-    pub fn maximize_sum(nums: Vec<i32>, k: i32) -> i32 {
-        let max = nums.iter().copied().max().unwrap_or(0);
+    pub fn is_winner(player1: Vec<i32>, player2: Vec<i32>) -> i32 {
+        let mut prev1 = [0, 0];
+        let mut prev2 = [0, 0];
+        let mut score1 = 0;
+        let mut score2 = 0;
+        for (p1, p2) in player1.iter().copied().zip(player2.iter().copied()) {
+            let double1 = (prev1[0] >= 10) || (prev1[1] >= 10);
+            if double1 {
+                score1 += p1 * 2;
+            } else {
+                score1 += p1;
+            }
 
-        (max * k) + ((k * (k - 1)) / 2)
+            let double2 = (prev2[0] >= 10) || (prev2[1] >= 10);
+            if double2 {
+                score2 += p2 * 2;
+            } else {
+                score2 += p2;
+            }
+
+            prev1[0] = prev1[1];
+            prev1[1] = p1;
+
+            prev2[0] = prev2[1];
+            prev2[1] = p2;
+        }
+
+        if score1 < score2 {
+            2
+        } else if score1 > score2 {
+            1
+        } else {
+            0
+        }
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    player1: Vec<i32>,
+    player2: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 3, 4, 5].to_vec(),
-            k: 3,
+            player1: [5, 10, 3, 2].to_vec(),
+            player2: [6, 5, 7, 3].to_vec(),
         },
         Input {
-            nums: [5, 5, 5].to_vec(),
-            k: 2,
+            player1: [3, 5, 7, 6].to_vec(),
+            player2: [8, 10, 10, 2].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::maximize_sum(input.nums, input.k);
+        let result = Solution::is_winner(input.player1, input.player2);
         println!("{:?}", result);
     }
 }
