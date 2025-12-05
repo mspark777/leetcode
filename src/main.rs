63339c4c6dc9474da75b61bf63d83e90e69c5b4b
mspark777 <topangel777@gmail.com>
2025-12-05 22:40:06 +0900

    2717. Semi-Ordered Permutation

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,46 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimized_string_length(s: String) -> i32 {
-        std::collections::HashSet::<char>::from_iter(s.chars()).len() as i32
+    pub fn semi_ordered_permutation(nums: Vec<i32>) -> i32 {
+        let mut one = 0usize;
+        let mut last = nums.len();
+
+        for (i, num) in nums.iter().copied().enumerate() {
+            let num = num as usize;
+            if num == 1 {
+                one = i;
+            } else if num == nums.len() {
+                last = i;
+            }
+        }
+
+        if one > last {
+            last += 1;
+        }
+
+        (nums.len() + one - (last + 1)) as i32
     }
 }
 
 struct Input {
-    s: String,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "aaabc".to_string(),
+            nums: [2, 1, 4, 3].to_vec(),
         },
         Input {
-            s: "cbbd".to_string(),
+            nums: [2, 4, 1, 3].to_vec(),
         },
         Input {
-            s: "baadccad".to_string(),
+            nums: [1, 3, 4, 2, 5].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimized_string_length(input.s);
+        let result = Solution::semi_ordered_permutation(input.nums);
         println!("{:?}", result);
     }
 }
