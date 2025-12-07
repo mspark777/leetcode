b27ad37bf0a8df6c45d00b818a986963ed3db4a8
mspark777 <topangel777@gmail.com>
2025-12-07 11:47:25 +0900

    2726. Calculator with Method Chaining

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,67 @@
-/**
- * @param {Function} fn
- * @param {Array} args
- * @param {number} t
- * @return {Function}
- */
-var cancellable = function (fn, args, t) {
-  const f = () => fn.apply(null, args);
-  f();
-  const id = setInterval(f, t);
-  return () => {
-    clearInterval(id);
-  };
-};
+class Calculator {
+  /**
+   * @param {number} value
+   */
+  constructor(value) {
+    this.value = value;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value) {
+    this.value += value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value) {
+    this.value -= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  multiply(value) {
+    this.value *= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+
+    this.value /= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {
+    this.value = Math.pow(this.value, value);
+    return this;
+  }
+
+  /**
+   * @return {number}
+   */
+  getResult() {
+    return this.value;
+  }
+}
 
 /**
  * @param {number[]} nums
