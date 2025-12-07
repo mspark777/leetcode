aafcc0f98fea34fc6051a5e8c9937c43df5159f2
mspark777 <topangel777@gmail.com>
2025-12-07 12:01:47 +0900

    2729. Check if The Number is Fascinating

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,37 @@
 struct Solution {}
 
 impl Solution {
-    pub fn semi_ordered_permutation(nums: Vec<i32>) -> i32 {
-        let mut one = 0usize;
-        let mut last = nums.len();
+    pub fn is_fascinating(n: i32) -> bool {
+        let nums = [n, n * 2, n * 3];
+        let mut visits = [false; 10];
+        visits[0] = true;
 
-        for (i, num) in nums.iter().copied().enumerate() {
-            let num = num as usize;
-            if num == 1 {
-                one = i;
-            } else if num == nums.len() {
-                last = i;
+        for mut digits in nums {
+            while digits > 0 {
+                let digit = (digits % 10) as usize;
+                digits /= 10;
+
+                if visits[digit] {
+                    return false;
+                } else {
+                    visits[digit] = true;
+                }
             }
         }
 
-        if one > last {
-            last += 1;
-        }
-
-        (nums.len() + one - (last + 1)) as i32
+        true
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [2, 1, 4, 3].to_vec(),
-        },
-        Input {
-            nums: [2, 4, 1, 3].to_vec(),
-        },
-        Input {
-            nums: [1, 3, 4, 2, 5].to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 192 }, Input { n: 100 }];
 
     for input in inputs {
-        let result = Solution::semi_ordered_permutation(input.nums);
+        let result = Solution::is_fascinating(input.n);
         println!("{:?}", result);
     }
 }
