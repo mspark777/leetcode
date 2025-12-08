24450a291ec34ced1a85b70c1c70dcf000610db1
mspark777 <topangel777@gmail.com>
2025-12-08 11:32:03 +0900

    2739. Total Distance Traveled

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_non_min_or_max(nums: Vec<i32>) -> i32 {
-        if nums.len() < 3 {
-            return -1;
-        }
-
-        let max = nums.iter().copied().take(3).max().unwrap();
-        let min = nums.iter().copied().take(3).min().unwrap();
-
-        nums.iter().copied().take(3).sum::<i32>() - (max + min)
+    pub fn distance_traveled(main_tank: i32, additional_tank: i32) -> i32 {
+        (main_tank + additional_tank.min((main_tank - 1) / 4)) * 10
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    main_tank: i32,
+    adadditional_tank: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [3, 2, 1, 4].to_vec(),
+            main_tank: 5,
+            adadditional_tank: 10,
         },
         Input {
-            nums: [1, 2].to_vec(),
-        },
-        Input {
-            nums: [2, 1, 3].to_vec(),
+            main_tank: 1,
+            adadditional_tank: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_non_min_or_max(input.nums);
+        let result = Solution::distance_traveled(input.main_tank, input.adadditional_tank);
         println!("{:?}", result);
     }
 }
