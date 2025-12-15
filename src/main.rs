e055c72a59ddd28d70bb693644f0db952d5cf782
mspark777 <topangel777@gmail.com>
2025-12-15 11:41:30 +0900

    2769. Find the Maximum Achievable Number

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
 struct Solution {}
 
 impl Solution {
-    pub fn alternating_subarray(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let mut dp = -1;
-        let mut result = -1;
-        for i in 1..n {
-            if (dp > 0) && (nums[i] == nums[i - 2]) {
-                dp += 1;
-            } else if nums[i] == (nums[i - 1] + 1) {
-                dp = 2;
-            } else {
-                dp = -1;
-            }
-
-            result = result.max(dp);
-        }
-
-        result
+    pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+        num + t * 2
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    num: i32,
+    t: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [2, 3, 4, 3, 4].to_vec(),
-        },
-        Input {
-            nums: [4, 5, 6].to_vec(),
-        },
-    ];
+    let inputs = [Input { num: 5, t: 1 }, Input { num: 3, t: 2 }];
 
     for input in inputs {
-        let result = Solution::alternating_subarray(input.nums);
+        let result = Solution::the_maximum_achievable_x(input.num, input.t);
         println!("{:?}", result);
     }
 }
