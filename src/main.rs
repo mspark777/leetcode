c8b7da6e0d4254f02b7f136d637223d53e01a087
mspark777 <topangel777@gmail.com>
2025-12-18 11:02:44 +0900

    2788. Split Strings by Separator

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,41 @@
-struct Solution {}
+struct Solution;
 
 impl Solution {
-    pub fn is_good(nums: Vec<i32>) -> bool {
-        let max = nums.iter().copied().max().unwrap() as usize;
-        if nums.len() != (max + 1) {
-            return false;
-        }
-
-        let mut counts = vec![0; nums.len()];
-        for num in nums.iter().copied() {
-            let num = num as usize;
-            counts[num] += 1;
-            if (num != max) && (counts[num] > 1) {
-                return false;
-            }
-        }
-
-        counts[max] == 2
+    pub fn split_words_by_separator(words: Vec<String>, separator: char) -> Vec<String> {
+        words
+            .iter()
+            .flat_map(|word| word.split(separator))
+            .filter(|&s| !s.is_empty())
+            .map(|s| s.to_string())
+            .collect()
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    words: Vec<String>,
+    separator: char,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [2, 1, 3].to_vec(),
+            words: ["one.two.three", "four.five", "six"]
+                .map(|s| s.to_string())
+                .to_vec(),
+            separator: '.',
         },
         Input {
-            nums: [1, 3, 3, 2].to_vec(),
+            words: ["$easy$", "$problem$"].map(|s| s.to_string()).to_vec(),
+            separator: '$',
         },
         Input {
-            nums: [1, 1].to_vec(),
-        },
-        Input {
-            nums: [3, 4, 4, 1, 2, 1].to_vec(),
+            words: ["|||"].map(|s| s.to_string()).to_vec(),
+            separator: '|',
         },
     ];
 
     for input in inputs {
-        let result = Solution::is_good(input.nums);
+        let result = Solution::split_words_by_separator(input.words, input.separator);
         println!("{:?}", result);
     }
 }
