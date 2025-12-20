72f0db60c0e031bde9fb3a6cced021e666228382
mspark777 <topangel777@gmail.com>
2025-12-20 23:02:51 +0900

    2815. Max Pair Sum in an Array

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,53 @@
 struct Solution;
 
 impl Solution {
-    pub fn final_string(s: String) -> String {
-        let mut chars = Vec::<char>::with_capacity(s.len());
-        for ch in s.chars() {
-            if ch == 'i' {
-                chars.reverse();
-            } else {
-                chars.push(ch);
+    pub fn max_sum(nums: Vec<i32>) -> i32 {
+        let mut result = -1;
+        let mut max_num = [0; 10];
+        for num in nums {
+            let max_digit = Self::max_digit(num);
+            if max_num[max_digit] != 0 {
+                result = result.max(max_num[max_digit] + num);
             }
+
+            max_num[max_digit] = max_num[max_digit].max(num);
         }
 
-        chars.iter().collect()
+        result
+    }
+
+    fn max_digit(n: i32) -> usize {
+        let mut n = n as usize;
+        let mut m = 0usize;
+
+        while n > 0 {
+            m = m.max(n % 10);
+            n /= 10;
+        }
+
+        m
     }
 }
 
 struct Input {
-    s: String,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "string".to_string(),
+            nums: [112, 131, 411].to_vec(),
         },
         Input {
-            s: "poiinter".to_string(),
+            nums: [2536, 1613, 3366, 162].to_vec(),
+        },
+        Input {
+            nums: [51, 71, 17, 24, 42].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::final_string(input.s);
+        let result = Solution::max_sum(input.nums);
         println!("{:?}", result);
     }
 }
