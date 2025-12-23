29ef805b860b5472677160fa96381da7072226c9
mspark777 <topangel777@gmail.com>
2025-12-23 10:20:47 +0900

    2833. Furthest Point From Origin

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,42 @@
 struct Solution;
 
 impl Solution {
-    pub fn is_acronym(words: Vec<String>, s: String) -> bool {
-        let mut word = words.iter();
-        let mut ch = s.chars();
-        loop {
-            match (word.next(), ch.next()) {
-                (Some(w), Some(c)) if w.chars().nth(0).unwrap_or(' ') == c => continue,
-                (None, None) => break,
-                _ => return false,
-            };
+    pub fn furthest_distance_from_origin(moves: String) -> i32 {
+        let mut l = 0;
+        let mut r = 0;
+        let mut u = 0;
+
+        for m in moves.chars() {
+            match m {
+                'L' => l += 1,
+                'R' => r += 1,
+                _ => u += 1,
+            }
         }
 
-        true
+        l.max(r) + u - l.min(r)
     }
 }
 
 struct Input {
-    words: Vec<String>,
-    s: String,
+    moves: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            words: ["alice", "bob", "charlie"].map(|s| s.to_string()).to_vec(),
-            s: "abc".to_string(),
+            moves: "L_RL__R".to_string(),
         },
         Input {
-            words: ["an", "apple"].map(|s| s.to_string()).to_vec(),
-            s: "a".to_string(),
+            moves: "_R__LL_".to_string(),
         },
         Input {
-            words: ["never", "gonna", "give", "up", "on", "you"]
-                .map(|s| s.to_string())
-                .to_vec(),
-            s: "ngguoy".to_string(),
+            moves: "_______".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::is_acronym(input.words, input.s);
+        let result = Solution::furthest_distance_from_origin(input.moves);
         println!("{:?}", result);
     }
 }
