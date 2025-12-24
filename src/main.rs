4f9315130933b3cf835c630181c394e697672fe6
mspark777 <topangel777@gmail.com>
2025-12-24 10:13:05 +0900

    2839. Check if Strings Can be Made Equal With Operations I

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,46 @@
 struct Solution;
 
 impl Solution {
-    pub fn furthest_distance_from_origin(moves: String) -> i32 {
-        let mut l = 0;
-        let mut r = 0;
-        let mut u = 0;
+    pub fn can_be_equal(s1: String, s2: String) -> bool {
+        let mut chars = s1.chars();
+        let s10 = chars.next().unwrap();
+        let s11 = chars.next().unwrap();
+        let s12 = chars.next().unwrap();
+        let s13 = chars.next().unwrap();
 
-        for m in moves.chars() {
-            match m {
-                'L' => l += 1,
-                'R' => r += 1,
-                _ => u += 1,
-            }
-        }
+        let mut chars = s2.chars();
+        let s20 = chars.next().unwrap();
+        let s21 = chars.next().unwrap();
+        let s22 = chars.next().unwrap();
+        let s23 = chars.next().unwrap();
 
-        l.max(r) + u - l.min(r)
+        Self::check(s10, s12, s20, s22) && Self::check(s11, s13, s21, s23)
+    }
+
+    fn check(a: char, b: char, c: char, d: char) -> bool {
+        (a == c && b == d) || (a == d && b == c)
     }
 }
 
 struct Input {
-    moves: String,
+    s1: String,
+    s2: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            moves: "L_RL__R".to_string(),
+            s1: "abcd".to_string(),
+            s2: "cdab".to_string(),
         },
         Input {
-            moves: "_R__LL_".to_string(),
-        },
-        Input {
-            moves: "_______".to_string(),
+            s1: "abcd".to_string(),
+            s2: "dacb".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::furthest_distance_from_origin(input.moves);
+        let result = Solution::can_be_equal(input.s1, input.s2);
         println!("{:?}", result);
     }
 }
