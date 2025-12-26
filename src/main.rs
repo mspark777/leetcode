f9d7a8670723b2a93004ac3ff5cd5bbb9d481e60
mspark777 <topangel777@gmail.com>
2025-12-26 15:58:41 +0900

    2859. Sum of Values at Indices With K Set Bits

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,38 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_right_shifts(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let mut count = 0;
-        let mut point = 0usize;
-        let mut idx = 0usize;
-
-        for i in 1..n {
-            if nums[i] < nums[i - 1] {
-                idx = i;
-                point = n - i;
-                if count == 1 {
-                    return -1;
-                } else {
-                    count += 1;
-                }
+    pub fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
+        let k = k as u32;
+        let mut result = 0;
+        for (i, num) in nums.into_iter().enumerate() {
+            if i.count_ones() == k {
+                result += num;
             }
         }
 
-        if count == 0 {
-            return 0;
-        }
-
-        if (nums[0] < nums[n - 1]) || (nums[idx - 1] < nums[n - 1]) {
-            return -1;
-        }
-
-        point as i32
+        result
     }
 }
 
 struct Input {
     nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [3, 4, 5, 1, 2].to_vec(),
+            nums: [5, 10, 1, 5, 2].to_vec(),
+            k: 1,
         },
         Input {
-            nums: [1, 3, 5].to_vec(),
-        },
-        Input {
-            nums: [2, 1, 4].to_vec(),
+            nums: [4, 3, 2, 1].to_vec(),
+            k: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_right_shifts(input.nums);
+        let result = Solution::sum_indices_with_k_set_bits(input.nums, input.k);
         println!("{:?}", result);
     }
 }
