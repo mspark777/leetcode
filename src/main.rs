1e706a56c38e145042ea9ab3584fd4294d2dddf2
mspark777 <topangel777@gmail.com>
2025-12-27 15:48:11 +0900

    2899. Last Visited Integers

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,45 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
-        let mut collections = std::collections::HashSet::<i32>::with_capacity(k as usize);
-        let mut result = 0;
+    pub fn last_visited_integers(nums: Vec<i32>) -> Vec<i32> {
+        let mut seen_idx = -1i32;
+        let mut seen = Vec::<i32>::new();
+        let mut result = Vec::<i32>::new();
 
-        for num in nums.into_iter().rev() {
-            result += 1;
-            if num <= k {
-                collections.insert(num);
-            }
-
-            if collections.len() == k as usize {
-                break;
+        for num in nums {
+            if num == -1 {
+                if seen_idx >= 0 {
+                    result.push(seen.get(seen_idx as usize).copied().unwrap_or(-1));
+                } else {
+                    result.push(-1);
+                }
+                seen_idx -= 1;
+            } else {
+                seen.push(num);
+                seen_idx = (seen.len() as i32) - 1;
             }
         }
+
         result
     }
 }
 
 struct Input {
     nums: Vec<i32>,
-    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [3, 1, 5, 4, 2].to_vec(),
-            k: 2,
+            nums: [1, 2, -1, -1, -1].to_vec(),
         },
         Input {
-            nums: [3, 1, 5, 4, 2].to_vec(),
-            k: 5,
-        },
-        Input {
-            nums: [3, 2, 5, 3, 1].to_vec(),
-            k: 3,
+            nums: [1, -1, 2, -1, -1].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::min_operations(input.nums, input.k);
+        let result = Solution::last_visited_integers(input.nums);
         println!("{:?}", result);
     }
 }
