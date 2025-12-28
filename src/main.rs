d61edd5e34bcf21f84732cb7e485727cd5f48f5b
mspark777 <topangel777@gmail.com>
2025-12-28 11:37:44 +0900

    2903. Find Indices With Index and Value Difference I

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,63 @@
 struct Solution;
 
 impl Solution {
-    pub fn last_visited_integers(nums: Vec<i32>) -> Vec<i32> {
-        let mut seen_idx = -1i32;
-        let mut seen = Vec::<i32>::new();
-        let mut result = Vec::<i32>::new();
+    pub fn find_indices(nums: Vec<i32>, index_difference: i32, value_difference: i32) -> Vec<i32> {
+        let mut min_idx = 0usize;
+        let mut max_idx = 0usize;
+        let n = nums.len();
+        let index_difference = index_difference as usize;
 
-        for num in nums {
-            if num == -1 {
-                if seen_idx >= 0 {
-                    result.push(seen.get(seen_idx as usize).copied().unwrap_or(-1));
-                } else {
-                    result.push(-1);
-                }
-                seen_idx -= 1;
-            } else {
-                seen.push(num);
-                seen_idx = (seen.len() as i32) - 1;
+        for i in index_difference..n {
+            let j = i - index_difference;
+            if nums[j] < nums[min_idx] {
+                min_idx = j
+            }
+
+            if nums[j] > nums[max_idx] {
+                max_idx = j
+            }
+
+            if (nums[i] - nums[min_idx]) >= value_difference {
+                return vec![min_idx as i32, i as i32];
+            }
+
+            if (nums[max_idx] - nums[i]) >= value_difference {
+                return vec![max_idx as i32, i as i32];
             }
         }
 
-        result
+        vec![-1, -1]
     }
 }
 
 struct Input {
     nums: Vec<i32>,
+    index_difference: i32,
+    value_difference: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, -1, -1, -1].to_vec(),
+            nums: [5, 1, 4, 1].to_vec(),
+            index_difference: 2,
+            value_difference: 4,
         },
         Input {
-            nums: [1, -1, 2, -1, -1].to_vec(),
+            nums: [2, 1].to_vec(),
+            index_difference: 0,
+            value_difference: 0,
+        },
+        Input {
+            nums: [1, 2, 3].to_vec(),
+            index_difference: 2,
+            value_difference: 4,
         },
     ];
 
     for input in inputs {
-        let result = Solution::last_visited_integers(input.nums);
+        let result =
+            Solution::find_indices(input.nums, input.index_difference, input.value_difference);
         println!("{:?}", result);
     }
 }
