a89c793d8ad63497edef32df1ad6dbf5359cbba2
mspark777 <topangel777@gmail.com>
2025-12-29 13:28:52 +0900

    2917. Find the K-or of an Array

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 struct Solution;
 
 impl Solution {
-    pub fn sum_counts(nums: Vec<i32>) -> i32 {
-        let mut result = 0;
-        let n = nums.len();
+    pub fn find_k_or(nums: Vec<i32>, k: i32) -> i32 {
+        let mut bits = vec![0; 32];
+        for mut num in nums.iter().copied() {
+            let mut i = 0usize;
+            while num > 0 {
+                bits[i] += num & 1;
+                num >>= 1;
+                i += 1;
+            }
+        }
 
-        for i in 0..n {
-            let mut set = 0u128;
-            for num in nums.iter().skip(i) {
-                set |= 1 << num;
-                let distinct = set.count_ones() as i32;
-                result += distinct * distinct;
+        let mut result = 0;
+        for (i, b) in bits.into_iter().enumerate() {
+            if b >= k {
+                result |= 1 << i;
             }
         }
 
@@ -20,20 +25,27 @@ impl Solution {
 
 struct Input {
     nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 1].to_vec(),
+            nums: [7, 12, 9, 8, 9, 15].to_vec(),
+            k: 4,
         },
         Input {
-            nums: [1, 1].to_vec(),
+            nums: [2, 12, 1, 11, 4, 5].to_vec(),
+            k: 6,
+        },
+        Input {
+            nums: [10, 8, 5, 9, 11, 6, 8].to_vec(),
+            k: 1,
         },
     ];
 
     for input in inputs {
-        let result = Solution::sum_counts(input.nums);
+        let result = Solution::find_k_or(input.nums, input.k);
         println!("{:?}", result);
     }
 }
