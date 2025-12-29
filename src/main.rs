aa5cc8c1e505db3f92004996258a26965abacc9b
mspark777 <topangel777@gmail.com>
2025-12-29 12:25:36 +0900

    2913. Subarrays Distinct Element Sum of Squares I

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_sum(nums: Vec<i32>) -> i32 {
+    pub fn sum_counts(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
         let n = nums.len();
-        let mut left = vec![0; n];
-        let mut right = vec![0; n];
 
-        left[0] = nums[0];
-        right[n - 1] = nums[n - 1];
-
-        for i in 1..n {
-            left[i] = left[i - 1].min(nums[i])
-        }
-
-        for i in (0..(n - 1)).rev() {
-            right[i] = right[i + 1].min(nums[i])
-        }
-
-        const MAX_SUM: i32 = 2_000_000_000;
-        let mut min_sum = MAX_SUM;
-
-        for i in 1..(n - 1) {
-            if (left[i] < nums[i]) && (right[i] < nums[i]) {
-                min_sum = min_sum.min(left[i] + nums[i] + right[i]);
+        for i in 0..n {
+            let mut set = 0u128;
+            for num in nums.iter().skip(i) {
+                set |= 1 << num;
+                let distinct = set.count_ones() as i32;
+                result += distinct * distinct;
             }
         }
 
-        match min_sum == MAX_SUM {
-            true => -1,
-            _ => min_sum,
-        }
+        result
     }
 }
 
@@ -40,18 +25,15 @@ struct Input {
 fn main() {
     let inputs = [
         Input {
-            nums: [8, 6, 1, 5, 3].to_vec(),
+            nums: [1, 2, 1].to_vec(),
         },
         Input {
-            nums: [5, 4, 8, 7, 10, 2].to_vec(),
-        },
-        Input {
-            nums: [6, 5, 4, 3, 4, 5].to_vec(),
+            nums: [1, 1].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_sum(input.nums);
+        let result = Solution::sum_counts(input.nums);
         println!("{:?}", result);
     }
 }
