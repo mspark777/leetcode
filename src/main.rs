43a5b8cbf2da8999a0bc6479db8082011b64eab1
mspark777 <topangel777@gmail.com>
2025-12-30 10:26:25 +0900

    2923. Find Champion I

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,35 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_k_or(nums: Vec<i32>, k: i32) -> i32 {
-        let mut bits = vec![0; 32];
-        for mut num in nums.iter().copied() {
-            let mut i = 0usize;
-            while num > 0 {
-                bits[i] += num & 1;
-                num >>= 1;
-                i += 1;
-            }
-        }
+    pub fn find_champion(grid: Vec<Vec<i32>>) -> i32 {
+        let n = grid.len();
+        let winner = (1..n).fold(0usize, |i, j| match grid[i][j] {
+            1 => i,
+            _ => j,
+        });
 
-        let mut result = 0;
-        for (i, b) in bits.into_iter().enumerate() {
-            if b >= k {
-                result |= 1 << i;
-            }
-        }
-
-        result
+        winner as i32
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    grid: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [7, 12, 9, 8, 9, 15].to_vec(),
-            k: 4,
+            grid: [[0, 1], [0, 0]].map(|r| r.to_vec()).to_vec(),
         },
         Input {
-            nums: [2, 12, 1, 11, 4, 5].to_vec(),
-            k: 6,
-        },
-        Input {
-            nums: [10, 8, 5, 9, 11, 6, 8].to_vec(),
-            k: 1,
+            grid: [[0, 0, 1], [1, 0, 1], [0, 0, 0]]
+                .map(|r| r.to_vec())
+                .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_k_or(input.nums, input.k);
+        let result = Solution::find_champion(input.grid);
         println!("{:?}", result);
     }
 }
