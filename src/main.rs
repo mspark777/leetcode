73b35998475c325ecb23816c6ac275ee606cb0a8
mspark777 <topangel777@gmail.com>
2026-01-01 21:53:40 +0900

    2928. Distribute Candies Among Children I

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,49 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_champion(grid: Vec<Vec<i32>>) -> i32 {
-        let n = grid.len();
-        let winner = (1..n).fold(0usize, |i, j| match grid[i][j] {
-            1 => i,
-            _ => j,
-        });
+    pub fn distribute_candies(n: i32, limit: i32) -> i32 {
+        let all_cases = ((n + 2) * (n + 1)) / 2;
 
-        winner as i32
+        if (3 * limit) < n {
+            return 0;
+        }
+
+        if (3 * limit) == n {
+            return 1;
+        }
+
+        let exclude_one = n - (limit + 1) + 2;
+        let one_limit = match exclude_one > 1 {
+            true => 3 * ((exclude_one * (exclude_one - 1)) / 2),
+            _ => 0,
+        };
+
+        let exclude_two = n - 2 * (limit + 1) + 2;
+        let two_limit = match exclude_two > 1 {
+            true => 3 * ((exclude_two * (exclude_two - 1)) / 2),
+            _ => 0,
+        };
+
+        let exclude_three = n - 3 * (limit + 1) + 2;
+        let three_limit = match exclude_three > 1 {
+            true => (exclude_three * (exclude_three - 1)) / 2,
+            _ => 0,
+        };
+
+        all_cases - one_limit + two_limit - three_limit
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    n: i32,
+    limit: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            grid: [[0, 1], [0, 0]].map(|r| r.to_vec()).to_vec(),
-        },
-        Input {
-            grid: [[0, 0, 1], [1, 0, 1], [0, 0, 0]]
-                .map(|r| r.to_vec())
-                .to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 5, limit: 2 }, Input { n: 3, limit: 3 }];
 
     for input in inputs {
-        let result = Solution::find_champion(input.grid);
+        let result = Solution::distribute_candies(input.n, input.limit);
         println!("{:?}", result);
     }
 }
