739d5801c3fc6cdd608aaa59fe04881065525d74
mspark777 <topangel777@gmail.com>
2026-01-10 12:22:04 +0900

    2960. Count Tested Devices After Test Operations

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,37 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_intersection_values(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        vec![Self::count(&nums1, &nums2), Self::count(&nums2, &nums1)]
-    }
+    pub fn count_tested_devices(battery_percentages: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let mut step = 0;
 
-    fn count(left: &[i32], right: &[i32]) -> i32 {
-        use std::collections::HashSet;
-
-        let set = HashSet::<i32>::from_iter(right.iter().copied());
-        let mut count = 0;
-        for num in left {
-            if set.contains(num) {
-                count += 1;
+        for p in battery_percentages {
+            if p > step {
+                step += 1;
+                result += 1;
             }
         }
 
-        count
+        result
     }
 }
 
 struct Input {
-    nums1: Vec<i32>,
-    nums2: Vec<i32>,
+    battery_percentages: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums1: [2, 3, 2].to_vec(),
-            nums2: [1, 2].to_vec(),
+            battery_percentages: [1, 1, 2, 1, 3].to_vec(),
         },
         Input {
-            nums1: [4, 3, 2, 3, 1].to_vec(),
-            nums2: [2, 2, 5, 2, 3, 6].to_vec(),
-        },
-        Input {
-            nums1: [3, 4, 2, 3].to_vec(),
-            nums2: [1, 5].to_vec(),
+            battery_percentages: [0, 1, 2].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_intersection_values(input.nums1, input.nums2);
+        let result = Solution::count_tested_devices(input.battery_percentages);
         println!("{:?}", result);
     }
 }
