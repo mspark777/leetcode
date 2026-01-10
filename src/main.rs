0b041427535a2b2048e097ad66bb13a03def82d2
mspark777 <topangel777@gmail.com>
2026-01-10 14:14:52 +0900

    2970. Count the Number of Incremovable Subarrays I

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,67 @@
 struct Solution;
 
 impl Solution {
-    pub fn count_tested_devices(battery_percentages: Vec<i32>) -> i32 {
-        let mut result = 0;
-        let mut step = 0;
+    pub fn incremovable_subarray_count(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut start = Vec::<i32>::new();
+        for num in nums.iter().copied() {
+            match start.last() {
+                Some(&s) if s < num => start.push(num),
+                None => start.push(num),
+                _ => break,
+            };
+        }
 
-        for p in battery_percentages {
-            if p > step {
-                step += 1;
-                result += 1;
+        let mut end = Vec::<i32>::new();
+        for num in nums.iter().copied().rev() {
+            match end.last() {
+                Some(&e) if e > num => end.push(num),
+                None => end.push(num),
+                _ => break,
+            };
+        }
+
+        if (start.len() + end.len()) > n {
+            return (n * (n + 1) / 2) as i32;
+        }
+
+        let mut result = start.len() + end.len() + 1;
+        let mut i = 0usize;
+        let mut j = 0usize;
+
+        end.reverse();
+        while (i < start.len()) && (j < end.len()) {
+            if start[i] < end[j] {
+                result += end.len() - j;
+                i += 1;
+            } else {
+                j += 1;
             }
         }
 
-        result
+        result as i32
     }
 }
 
 struct Input {
-    battery_percentages: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            battery_percentages: [1, 1, 2, 1, 3].to_vec(),
+            nums: [1, 2, 3, 4].to_vec(),
         },
         Input {
-            battery_percentages: [0, 1, 2].to_vec(),
+            nums: [6, 5, 7, 8].to_vec(),
+        },
+        Input {
+            nums: [8, 7, 6, 6].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_tested_devices(input.battery_percentages);
+        let result = Solution::incremovable_subarray_count(input.nums);
         println!("{:?}", result);
     }
 }
