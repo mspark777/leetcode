f0eaacb4c1bbad51e714f793a2361b6a10ff8fd4
mspark777 <topangel777@gmail.com>
2026-01-14 10:34:39 +0900

    3010. Divide an Array Into Subarrays With Minimum Cost I

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,42 @@
 struct Solution;
 
 impl Solution {
-    pub fn area_of_max_diagonal(dimensions: Vec<Vec<i32>>) -> i32 {
-        let mut max_diagonal = 0;
-        let mut result = 0;
-
-        for rect in dimensions {
-            let length = rect[0];
-            let width = rect[1];
-            let diagonal = length * length + width * width;
-            let area = length * width;
-
-            if diagonal > max_diagonal {
-                max_diagonal = diagonal;
-                result = area;
-            } else if diagonal == max_diagonal {
-                result = result.max(area);
+    pub fn minimum_cost(nums: Vec<i32>) -> i32 {
+        let first = nums[0];
+        let mut second = 51;
+        let mut third = 51;
+        for num in nums.iter().skip(1).copied() {
+            if num < second {
+                third = second;
+                second = num;
+            } else if (num < third) && (num >= second) {
+                third = num;
             }
         }
 
-        result
+        first + second + third
     }
 }
 
 struct Input {
-    dimensions: Vec<Vec<i32>>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            dimensions: [[9, 3], [8, 6]].map(|v| v.to_vec()).to_vec(),
+            nums: [1, 2, 3, 12].to_vec(),
         },
         Input {
-            dimensions: [[3, 4], [4, 3]].map(|v| v.to_vec()).to_vec(),
+            nums: [5, 4, 3].to_vec(),
+        },
+        Input {
+            nums: [10, 3, 1, 1].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::area_of_max_diagonal(input.dimensions);
+        let result = Solution::minimum_cost(input.nums);
         println!("{:?}", result);
     }
 }
