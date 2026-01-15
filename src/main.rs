d11fac15cb208290682a7216da6555a878498631
mspark777 <topangel777@gmail.com>
2026-01-15 10:38:44 +0900

    3014. Minimum Number of Pushes to Type Word I

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_cost(nums: Vec<i32>) -> i32 {
-        let first = nums[0];
-        let mut second = 51;
-        let mut third = 51;
-        for num in nums.iter().skip(1).copied() {
-            if num < second {
-                third = second;
-                second = num;
-            } else if (num < third) && (num >= second) {
-                third = num;
-            }
+    pub fn minimum_pushes(word: String) -> i32 {
+        let mut n = word.len() as i32;
+        let mut result = 0;
+        let mut count = 1;
+
+        while n >= 8 {
+            n -= 8;
+            result += count * 8;
+            count += 1;
         }
 
-        first + second + third
+        result += count * n;
+        result
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    word: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 3, 12].to_vec(),
+            word: "abcde".to_string(),
         },
         Input {
-            nums: [5, 4, 3].to_vec(),
-        },
-        Input {
-            nums: [10, 3, 1, 1].to_vec(),
+            word: "xycdefghij".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_cost(input.nums);
+        let result = Solution::minimum_pushes(input.word);
         println!("{:?}", result);
     }
 }
