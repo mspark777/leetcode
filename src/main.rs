be5ec18a208d5c7e849de04be7cdb2c66a84f1ba
mspark777 <topangel777@gmail.com>
2026-01-18 11:43:16 +0900

    3069. Distribute Elements Into Two Arrays I

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
-        nums.iter().copied().filter(|&n| n < k).count() as i32
+    pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut last1 = nums[0];
+        let mut last2 = nums[1];
+        let mut arr1 = vec![last1];
+        let mut arr2 = vec![last2];
+
+        for num in nums.iter().skip(2).copied() {
+            if last1 > last2 {
+                arr1.push(num);
+                last1 = num;
+            } else {
+                arr2.push(num);
+                last2 = num;
+            }
+        }
+
+        arr1.append(&mut arr2);
+        arr1
     }
 }
 
 struct Input {
     nums: Vec<i32>,
-    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [2, 11, 10, 1, 3].to_vec(),
-            k: 10,
+            nums: [2, 1, 3].to_vec(),
         },
         Input {
-            nums: [1, 1, 2, 4, 9].to_vec(),
-            k: 1,
-        },
-        Input {
-            nums: [1, 1, 2, 4, 9].to_vec(),
-            k: 9,
+            nums: [5, 4, 3, 8].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::min_operations(input.nums, input.k);
+        let result = Solution::result_array(input.nums);
         println!("{:?}", result);
     }
 }
