8c77d0456ecf3f2ba1342d32a41b110304c3be07
mspark777 <topangel777@gmail.com>
2026-01-18 11:31:39 +0900

    3065. Minimum Operations to Exceed Threshold Value I

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,34 @@
 struct Solution;
 
 impl Solution {
-    pub fn is_possible_to_split(nums: Vec<i32>) -> bool {
-        let mut counts = vec![0; 100];
-        for num in nums {
-            let idx = (num - 1) as usize;
-            counts[idx] += 1;
-            if counts[idx] > 2 {
-                return false;
-            }
-        }
-
-        true
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        nums.iter().copied().filter(|&n| n < k).count() as i32
     }
 }
 
 struct Input {
     nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 1, 2, 2, 3, 4].to_vec(),
+            nums: [2, 11, 10, 1, 3].to_vec(),
+            k: 10,
         },
         Input {
-            nums: [1, 1, 1, 1].to_vec(),
+            nums: [1, 1, 2, 4, 9].to_vec(),
+            k: 1,
+        },
+        Input {
+            nums: [1, 1, 2, 4, 9].to_vec(),
+            k: 9,
         },
     ];
 
     for input in inputs {
-        let result = Solution::is_possible_to_split(input.nums);
+        let result = Solution::min_operations(input.nums, input.k);
         println!("{:?}", result);
     }
 }
