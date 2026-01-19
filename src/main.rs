8b280109a081ce29d1f2aff08ebc77f2016a2d31
mspark777 <topangel777@gmail.com>
2026-01-19 15:25:02 +0900

    3074. Apple Redistribution into Boxes

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,44 @@
 struct Solution;
 
 impl Solution {
-    pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
-        let mut last1 = nums[0];
-        let mut last2 = nums[1];
-        let mut arr1 = vec![last1];
-        let mut arr2 = vec![last2];
+    pub fn minimum_boxes(apple: Vec<i32>, capacity: Vec<i32>) -> i32 {
+        let mut boxes = capacity;
+        boxes.sort_unstable();
 
-        for num in nums.iter().skip(2).copied() {
-            if last1 > last2 {
-                arr1.push(num);
-                last1 = num;
-            } else {
-                arr2.push(num);
-                last2 = num;
+        let mut apple_count = apple.iter().sum::<i32>();
+        let mut result = 0;
+
+        for b in boxes.iter().rev().copied() {
+            apple_count -= b;
+            result += 1;
+            if apple_count < 1 {
+                break;
             }
         }
 
-        arr1.append(&mut arr2);
-        arr1
+        result
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    apple: Vec<i32>,
+    capacity: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [2, 1, 3].to_vec(),
+            apple: [1, 3, 2].to_vec(),
+            capacity: [4, 3, 1, 5, 2].to_vec(),
         },
         Input {
-            nums: [5, 4, 3, 8].to_vec(),
+            apple: [5, 5, 5].to_vec(),
+            capacity: [2, 4, 2, 7].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::result_array(input.nums);
+        let result = Solution::minimum_boxes(input.apple, input.capacity);
         println!("{:?}", result);
     }
 }
