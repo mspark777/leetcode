9825fa49d2bd3a35047cb7f7020ebf2b8f085acb
mspark777 <topangel777@gmail.com>
2026-01-26 13:28:15 +0900

    3099. Harshad Number

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,30 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
-        let mut len = usize::MAX;
-
-        for i in 0..nums.len() {
-            let mut current = 0;
-            for (j, num) in nums.iter().copied().enumerate().skip(i) {
-                current |= num;
-                if current >= k {
-                    len = len.min(j + 1 - i);
-                    break;
-                }
-            }
+    pub fn sum_of_the_digits_of_harshad_number(x: i32) -> i32 {
+        let mut n = x;
+        let mut sum = 0;
+        while n > 0 {
+            sum += n % 10;
+            n /= 10;
         }
 
-        match len {
-            usize::MAX => -1,
-            _ => len as i32,
+        match x % sum {
+            0 => sum,
+            _ => -1,
         }
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    x: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [1, 2, 3].to_vec(),
-            k: 2,
-        },
-        Input {
-            nums: [2, 1, 8].to_vec(),
-            k: 10,
-        },
-        Input {
-            nums: [1, 2].to_vec(),
-            k: 0,
-        },
-    ];
+    let inputs = [Input { x: 18 }, Input { x: 23 }];
 
     for input in inputs {
-        let result = Solution::minimum_subarray_length(input.nums, input.k);
+        let result = Solution::sum_of_the_digits_of_harshad_number(input.x);
         println!("{:?}", result);
     }
 }
