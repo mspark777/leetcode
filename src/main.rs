bbd44aebdc8d3d2d31e10257d759145e414786b5
mspark777 <topangel777@gmail.com>
2026-02-02 21:56:08 +0900

    3110. Score of a String

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 struct Solution;
 
 impl Solution {
-    pub fn sum_of_the_digits_of_harshad_number(x: i32) -> i32 {
-        let mut n = x;
-        let mut sum = 0;
-        while n > 0 {
-            sum += n % 10;
-            n /= 10;
+    pub fn score_of_string(s: String) -> i32 {
+        let mut result = 0;
+        for (left, right) in s.chars().zip(s.chars().skip(1)) {
+            let l = left as i32;
+            let r = right as i32;
+            result += (l - r).abs();
         }
 
-        match x % sum {
-            0 => sum,
-            _ => -1,
-        }
+        result
     }
 }
 
 struct Input {
-    x: i32,
+    s: String,
 }
 
 fn main() {
-    let inputs = [Input { x: 18 }, Input { x: 23 }];
+    let inputs = [
+        Input {
+            s: "hello".to_string(),
+        },
+        Input {
+            s: "zaz".to_string(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::sum_of_the_digits_of_harshad_number(input.x);
+        let result = Solution::score_of_string(input.s);
         println!("{:?}", result);
     }
 }
