49df6d77228b98911522f1535f3b9210e32ca5fb
mspark777 <topangel777@gmail.com>
2026-02-12 10:12:32 +0900

    3168. Minimum Number of Chairs in a Waiting Room

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 struct Solution;
 
 impl Solution {
-    pub fn number_of_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i32 {
+    pub fn minimum_chairs(s: String) -> i32 {
         let mut result = 0;
-        for n1 in nums1 {
-            for n2 in nums2.iter().copied() {
-                let n = n2 * k;
-                if (n1 % n) == 0 {
-                    result += 1;
-                }
-            }
+        let mut count = 0;
+
+        for ch in s.chars() {
+            match ch {
+                'E' => count += 1,
+                _ => count -= 1,
+            };
+
+            result = result.max(count)
         }
 
         result
@@ -17,27 +19,24 @@ impl Solution {
 }
 
 struct Input {
-    nums1: Vec<i32>,
-    nums2: Vec<i32>,
-    k: i32,
+    s: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums1: [1, 3, 4].to_vec(),
-            nums2: [1, 3, 4].to_vec(),
-            k: 1,
+            s: "EEEEEEE".to_string(),
         },
         Input {
-            nums1: [1, 2, 4, 12].to_vec(),
-            nums2: [2, 4].to_vec(),
-            k: 3,
+            s: "ELELEEL".to_string(),
+        },
+        Input {
+            s: "ELEELEELLL".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::number_of_pairs(input.nums1, input.nums2, input.k);
+        let result = Solution::minimum_chairs(input.s);
         println!("{:?}", result);
     }
 }
