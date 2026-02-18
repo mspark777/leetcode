468d13c6e4e3f8d6404c95968d24fa91125f2906
mspark777 <topangel777@gmail.com>
2026-02-18 09:32:36 +0900

    3216. Lexicographically Smallest String After a Swap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 struct Solution;
 
 impl Solution {
-    pub fn get_encrypted_string(s: String, k: i32) -> String {
-        let chars = s.chars().collect::<Vec<char>>();
-        let mut result = Vec::<char>::with_capacity(chars.len());
-        let n = chars.len();
-        let k = k as usize;
+    pub fn get_smallest_string(s: String) -> String {
+        let mut result = s.chars().collect::<Vec<char>>();
+        let n = result.len();
+        for i in 1..n {
+            let left = result[i - 1] as u8;
+            let right = result[i] as u8;
 
-        for i in 0..n {
-            let j = (i + k) % n;
-            result.push(chars[j]);
+            if ((left & 1) == (right & 1)) && (left > right) {
+                result[i - 1] = right as char;
+                result[i] = left as char;
+                break;
+            }
         }
 
         result.iter().collect()
@@ -18,23 +21,20 @@ impl Solution {
 
 struct Input {
     s: String,
-    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "dart".to_string(),
-            k: 3,
+            s: "45320".to_string(),
         },
         Input {
-            s: "aaa".to_string(),
-            k: 1,
+            s: "001".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::get_encrypted_string(input.s, input.k);
+        let result = Solution::get_smallest_string(input.s);
         println!("{:?}", result);
     }
 }
