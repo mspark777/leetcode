ae180d2f8db0c7930d5989ef0c452cc3b8822b12
mspark777 <topangel777@gmail.com>
2026-02-19 09:51:29 +0900

    3226. Number of Bit Changes to Make Two Integers Equal

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 struct Solution;
 
 impl Solution {
-    pub fn winning_player(x: i32, y: i32) -> String {
-        match x.min(y >> 2) & 1 {
-            1 => "Alice".to_string(),
-            _ => "Bob".to_string(),
+    pub fn min_changes(n: i32, k: i32) -> i32 {
+        let k = k ^ n;
+        let cnt = k.count_ones();
+
+        let k = k & n;
+        match cnt == k.count_ones() {
+            true => cnt as i32,
+            _ => -1,
         }
     }
 }
 
 struct Input {
-    x: i32,
-    y: i32,
+    n: i32,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [Input { x: 2, y: 7 }, Input { x: 4, y: 11 }];
+    let inputs = [
+        Input { n: 13, k: 4 },
+        Input { n: 21, k: 21 },
+        Input { n: 14, k: 13 },
+    ];
 
     for input in inputs {
-        let result = Solution::winning_player(input.x, input.y);
+        let result = Solution::min_changes(input.n, input.k);
         println!("{:?}", result);
     }
 }
