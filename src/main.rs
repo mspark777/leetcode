119dce9b043072adbba76efbfa33f51023480899
mspark777 <topangel777@gmail.com>
2026-02-21 14:52:37 +0900

    3248. Snake in Matrix

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,44 @@
 struct Solution;
 
 impl Solution {
-    pub fn winning_player_count(n: i32, pick: Vec<Vec<i32>>) -> i32 {
-        let n = n as usize;
-        let mut counts = vec![[0usize; 11]; n + 1];
-
-        for p in pick {
-            let x = p[0] as usize;
-            let y = p[1] as usize;
-
-            counts[x][y] += 1;
-        }
-
-        let mut result = 0;
-        for (i, count) in counts.into_iter().enumerate() {
-            for cnt in count {
-                if cnt > i {
-                    result += 1;
-                    break;
-                }
+    pub fn final_position_of_snake(n: i32, commands: Vec<String>) -> i32 {
+        let mut r = 0;
+        let mut c = 0;
+        for command in commands {
+            if command == "UP" {
+                r -= 1;
+            } else if command == "DOWN" {
+                r += 1;
+            } else if command == "RIGHT" {
+                c += 1;
+            } else if command == "LEFT" {
+                c -= 1;
             }
         }
 
-        result
+        r * n + c
     }
 }
 
 struct Input {
     n: i32,
-    pick: Vec<Vec<i32>>,
+    commands: Vec<String>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            n: 4,
-            pick: [[0, 0], [1, 0], [1, 0], [2, 1], [2, 1], [2, 0]]
-                .map(|v| v.to_vec())
-                .to_vec(),
+            n: 2,
+            commands: ["RIGHT", "DOWN"].map(|s| s.to_string()).to_vec(),
         },
         Input {
-            n: 5,
-            pick: [[1, 1], [1, 2], [1, 3], [1, 4]]
-                .map(|v| v.to_vec())
-                .to_vec(),
-        },
-        Input {
-            n: 5,
-            pick: [[1, 1], [2, 4], [2, 4], [2, 4]]
-                .map(|v| v.to_vec())
-                .to_vec(),
+            n: 3,
+            commands: ["DOWN", "RIGHT", "UP"].map(|s| s.to_string()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::winning_player_count(input.n, input.pick);
+        let result = Solution::final_position_of_snake(input.n, input.commands);
         println!("{:?}", result);
     }
 }
