fca29e63a785363b6597415b5efd45247e9538a0
mspark777 <topangel777@gmail.com>
2026-02-23 18:19:46 +0900

    3274. Check if Two Chessboard Squares Have the Same Color

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,42 @@
 struct Solution;
 
 impl Solution {
-    pub fn generate_key(num1: i32, num2: i32, num3: i32) -> i32 {
-        let mut div = 1;
-        let mut result = 0;
+    pub fn check_two_chessboards(coordinate1: String, coordinate2: String) -> bool {
+        let origins = ['a', '0'].map(|o| o as u32);
+        let p1 = coordinate1
+            .chars()
+            .zip(origins)
+            .map(|(c, o)| (c as u32) - o)
+            .sum::<u32>();
+        let p2 = coordinate2
+            .chars()
+            .zip(origins)
+            .map(|(c, o)| (c as u32) - o)
+            .sum::<u32>();
 
-        while (num1 >= div) || (num2 >= div) || (num3 >= div) {
-            let n1 = (num1 / div) % 10;
-            let n2 = (num2 / div) % 10;
-            let n3 = (num3 / div) % 10;
-            result += div * n1.min(n2).min(n3);
-            div *= 10;
-        }
-
-        result
+        (p1 & 1) == (p2 & 1)
     }
 }
 
 struct Input {
-    num1: i32,
-    num2: i32,
-    num3: i32,
+    coordinate1: String,
+    coordinate2: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            num1: 1,
-            num2: 10,
-            num3: 1000,
+            coordinate1: "a1".to_string(),
+            coordinate2: "c3".to_string(),
         },
         Input {
-            num1: 987,
-            num2: 879,
-            num3: 798,
-        },
-        Input {
-            num1: 1,
-            num2: 2,
-            num3: 3,
+            coordinate1: "a1".to_string(),
+            coordinate2: "h3".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::generate_key(input.num1, input.num2, input.num3);
+        let result = Solution::check_two_chessboards(input.coordinate1, input.coordinate2);
         println!("{:?}", result);
     }
 }
