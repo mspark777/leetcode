3ad5e0e0fd042fe8d5dc842f70fcdfaf9be25eb6
mspark777 <topangel777@gmail.com>
2026-02-23 18:37:35 +0900

    3280. Convert Date to Binary

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,31 @@
 struct Solution;
 
 impl Solution {
-    pub fn check_two_chessboards(coordinate1: String, coordinate2: String) -> bool {
-        let origins = ['a', '0'].map(|o| o as u32);
-        let p1 = coordinate1
-            .chars()
-            .zip(origins)
-            .map(|(c, o)| (c as u32) - o)
-            .sum::<u32>();
-        let p2 = coordinate2
-            .chars()
-            .zip(origins)
-            .map(|(c, o)| (c as u32) - o)
-            .sum::<u32>();
-
-        (p1 & 1) == (p2 & 1)
+    pub fn convert_date_to_binary(date: String) -> String {
+        date.split('-')
+            .map(|s| s.parse::<u32>().unwrap())
+            .map(|n| format!("{:b}", n))
+            .collect::<Vec<String>>()
+            .join("-")
     }
 }
 
 struct Input {
-    coordinate1: String,
-    coordinate2: String,
+    date: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            coordinate1: "a1".to_string(),
-            coordinate2: "c3".to_string(),
+            date: "2080-02-29".to_string(),
         },
         Input {
-            coordinate1: "a1".to_string(),
-            coordinate2: "h3".to_string(),
+            date: "1900-01-01".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::check_two_chessboards(input.coordinate1, input.coordinate2);
+        let result = Solution::convert_date_to_binary(input.date);
         println!("{:?}", result);
     }
 }
