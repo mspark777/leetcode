e6434bbd6c09e0e2a9fdf8e24404437e89546a21
mspark777 <topangel777@gmail.com>
2026-02-24 10:05:39 +0900

    3289. The Two Sneaky Numbers of Digitville

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
 struct Solution;
 
 impl Solution {
-    pub fn stable_mountains(height: Vec<i32>, threshold: i32) -> Vec<i32> {
-        let mut result = Vec::<i32>::new();
-        for i in 1..height.len() {
-            if height[i - 1] > threshold {
-                result.push(i as i32);
-            }
+    pub fn get_sneaky_numbers(nums: Vec<i32>) -> Vec<i32> {
+        let mut sum = 0.0;
+        let mut squared_sum = 0.0;
+        for n in nums.iter().copied() {
+            let f = n as f64;
+            sum += f;
+            squared_sum += f * f;
         }
 
-        result
+        let n = nums.len() as i32 - 2;
+        let sum2 = sum - (n * (n - 1) / 2) as f64;
+        let squared_sum2 = squared_sum - (n * (n - 1) * (2 * n - 1) / 6) as f64;
+        let t = (2.0 * squared_sum2 - sum2 * sum2).sqrt();
+        let x1 = (sum2 - t) / 2.0;
+        let x2 = (sum2 + t) / 2.0;
+        vec![x1 as i32, x2 as i32]
     }
 }
 
 struct Input {
-    height: Vec<i32>,
-    threshold: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            height: [1, 2, 3, 4, 5].to_vec(),
-            threshold: 2,
+            nums: [0, 1, 1, 0].to_vec(),
         },
         Input {
-            height: [10, 1, 10, 1, 10].to_vec(),
-            threshold: 3,
+            nums: [0, 3, 2, 1, 3, 2].to_vec(),
         },
         Input {
-            height: [10, 1, 10, 1, 10].to_vec(),
-            threshold: 10,
+            nums: [7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::stable_mountains(input.height, input.threshold);
+        let result = Solution::get_sneaky_numbers(input.nums);
         println!("{:?}", result);
     }
 }
