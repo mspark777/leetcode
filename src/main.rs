93c5cf2393bf2a2a2cbf490ebbc1807350022336
mspark777 <topangel777@gmail.com>
2026-02-26 09:58:22 +0900

    3340. Check Balanced String

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,36 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_x_sum(nums: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
-        use std::collections::HashMap;
+    pub fn is_balanced(num: String) -> bool {
+        let sum = num
+            .chars()
+            .map(|ch| (ch as i32) - ('0' as i32))
+            .enumerate()
+            .fold(0, |acc, (i, ch)| match i & 1 {
+                1 => acc + ch,
+                _ => acc - ch,
+            });
 
-        let n = nums.len();
-        let k = k as usize;
-        let x = x as usize;
-        let mut result = Vec::<i32>::with_capacity(n + 1 - k);
-
-        for i in 0..=(n - k) {
-            let mut counts = HashMap::<i32, i32>::new();
-            for n in nums.iter().copied().skip(i).take(k) {
-                counts.entry(n).and_modify(|c| *c += 1).or_insert(1);
-            }
-
-            let mut frequencies = counts
-                .into_iter()
-                .map(|(num, count)| (count, num))
-                .collect::<Vec<_>>();
-            frequencies.sort_unstable_by(|a, b| b.0.cmp(&a.0).then_with(|| b.1.cmp(&a.1)));
-            let xsum: i32 = frequencies
-                .into_iter()
-                .take(x)
-                .map(|(count, num)| count * num)
-                .sum();
-
-            result.push(xsum);
-        }
-
-        result
+        sum == 0
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
-    x: i32,
+    num: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 1, 2, 2, 3, 4, 2, 3].to_vec(),
-            k: 6,
-            x: 2,
+            num: "1234".to_string(),
         },
         Input {
-            nums: [3, 8, 7, 8, 7, 5].to_vec(),
-            k: 2,
-            x: 2,
+            num: "24123".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_x_sum(input.nums, input.k, input.x);
+        let result = Solution::is_balanced(input.num);
         println!("{:?}", result);
     }
 }
