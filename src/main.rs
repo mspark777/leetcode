fc47ad4ef0bc4cf473f42ccb72b2a071a29cc624
mspark777 <topangel777@gmail.com>
2026-02-27 09:40:21 +0900

    3354. Make Array Elements Equal to Zero

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,45 @@
 struct Solution;
 
 impl Solution {
-    pub fn has_increasing_subarrays(nums: Vec<i32>, k: i32) -> bool {
-        let n = nums.len();
-        let mut count = 1;
-        let mut pre_count = 0;
+    pub fn count_valid_selections(nums: Vec<i32>) -> i32 {
         let mut result = 0;
+        let mut left = 0;
+        let mut right = nums.iter().sum::<i32>();
 
-        for i in 1..n {
-            if nums[i] > nums[i - 1] {
-                count += 1;
+        for num in nums {
+            if num == 0 {
+                if (0..2).contains(&(left - right)) {
+                    result += 1;
+                }
+
+                if (0..2).contains(&(right - left)) {
+                    result += 1;
+                }
             } else {
-                pre_count = count;
-                count = 1;
+                left += num;
+                right -= num;
             }
-            result = result.max(pre_count.min(count)).max(count / 2);
         }
-
-        result >= k
+        result
     }
 }
 
 struct Input {
     nums: Vec<i32>,
-    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [2, 5, 7, 8, 9, 2, 3, 4, 3, 1].to_vec(),
-            k: 3,
+            nums: [1, 0, 2, 0, 3].to_vec(),
         },
         Input {
-            nums: [1, 2, 3, 4, 4, 4, 4, 5, 6, 7].to_vec(),
-            k: 5,
+            nums: [2, 3, 4, 0, 4, 1, 0].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::has_increasing_subarrays(input.nums, input.k);
+        let result = Solution::count_valid_selections(input.nums);
         println!("{:?}", result);
     }
 }
