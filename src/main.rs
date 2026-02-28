2be4b5a8c070a07b98075bfa973937e53b289204
mspark777 <topangel777@gmail.com>
2026-02-28 14:27:34 +0900

    3360. Stone Removal Game

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,28 @@
 struct Solution;
 
 impl Solution {
-    pub fn count_valid_selections(nums: Vec<i32>) -> i32 {
-        let mut result = 0;
-        let mut left = 0;
-        let mut right = nums.iter().sum::<i32>();
+    pub fn can_alice_win(n: i32) -> bool {
+        let mut stones = n;
+        let mut removing = 10;
 
-        for num in nums {
-            if num == 0 {
-                if (0..2).contains(&(left - right)) {
-                    result += 1;
-                }
-
-                if (0..2).contains(&(right - left)) {
-                    result += 1;
-                }
-            } else {
-                left += num;
-                right -= num;
-            }
+        while stones >= 0 {
+            stones -= removing;
+            removing -= 1;
         }
-        result
+
+        (removing & 1) == 0
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [1, 0, 2, 0, 3].to_vec(),
-        },
-        Input {
-            nums: [2, 3, 4, 0, 4, 1, 0].to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 12 }, Input { n: 1 }];
 
     for input in inputs {
-        let result = Solution::count_valid_selections(input.nums);
+        let result = Solution::can_alice_win(input.n);
         println!("{:?}", result);
     }
 }
