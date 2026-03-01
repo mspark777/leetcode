fbeae0bb3d09fa0401017702c98113794c0f012d
mspark777 <topangel777@gmail.com>
2026-03-01 11:09:29 +0900

    3370. Smallest Number With All Set Bits

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,24 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_sum_subarray(nums: Vec<i32>, l: i32, r: i32) -> i32 {
-        let l = l as usize;
-        let r = r as usize;
-        let n = nums.len();
-        let mut min_sum = i32::MAX;
-        let mut found = false;
-
-        for k in l..=r {
-            let mut sum = nums.iter().copied().take(k).sum::<i32>();
-            if sum > 0 {
-                min_sum = min_sum.min(sum);
-                found = true;
-            }
-
-            for i in k..n {
-                sum += nums[i];
-                sum -= nums[i - k];
-
-                if sum > 0 {
-                    min_sum = min_sum.min(sum);
-                    found = true;
-                }
-            }
+    pub fn smallest_number(n: i32) -> i32 {
+        let mut result = 1;
+        while result < n {
+            result = (result << 1) | 1;
         }
-
-        match found {
-            true => min_sum,
-            _ => -1,
-        }
+        result
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    l: i32,
-    r: i32,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [3, -2, 1, 4].to_vec(),
-            l: 2,
-            r: 3,
-        },
-        Input {
-            nums: [-2, 2, -3, 1].to_vec(),
-            l: 2,
-            r: 3,
-        },
-        Input {
-            nums: [1, 2, 3, 4].to_vec(),
-            l: 2,
-            r: 4,
-        },
-    ];
+    let inputs = [Input { n: 5 }, Input { n: 10 }, Input { n: 3 }];
 
     for input in inputs {
-        let result = Solution::minimum_sum_subarray(input.nums, input.l, input.r);
+        let result = Solution::smallest_number(input.n);
         println!("{:?}", result);
     }
 }
