6029e75d510d987eaeffd05eb1dcf4f95f35b4b4
mspark777 <topangel777@gmail.com>
2026-03-01 11:45:44 +0900

    3379. Transformed Array

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 struct Solution;
 
 impl Solution {
-    pub fn smallest_number(n: i32) -> i32 {
-        let mut result = 1;
-        while result < n {
-            result = (result << 1) | 1;
+    pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len() as i32;
+        let mut result = vec![0; n as usize];
+
+        for (i, num) in nums.iter().copied().enumerate() {
+            let i = i as i32;
+            let j = (((i + num) % n) + n) % n;
+            result[i as usize] = nums[j as usize];
         }
+
         result
     }
 }
 
 struct Input {
-    n: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { n: 5 }, Input { n: 10 }, Input { n: 3 }];
+    let inputs = [
+        Input {
+            nums: [3, -2, 1, 1].to_vec(),
+        },
+        Input {
+            nums: [-1, 4, -1].to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::smallest_number(input.n);
+        let result = Solution::construct_transformed_array(input.nums);
         println!("{:?}", result);
     }
 }
