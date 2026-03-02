24dc6b53dc0b052bdb687912b936b7c3257c750e
mspark777 <topangel777@gmail.com>
2026-03-02 17:12:21 +0900

    3386. Button with Longest Push Time

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 struct Solution;
 
 impl Solution {
-    pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len() as i32;
-        let mut result = vec![0; n as usize];
+    pub fn button_with_longest_time(events: Vec<Vec<i32>>) -> i32 {
+        let mut result = events[0][0];
+        let mut prev_time = events[0][1];
+        let mut max_time = prev_time;
 
-        for (i, num) in nums.iter().copied().enumerate() {
-            let i = i as i32;
-            let j = (((i + num) % n) + n) % n;
-            result[i as usize] = nums[j as usize];
+        for event in events.iter().skip(1) {
+            let index = event[0];
+            let time = event[1];
+            let taken = time - prev_time;
+
+            if max_time < taken {
+                result = index;
+                max_time = taken;
+            } else if max_time == taken {
+                result = result.min(index);
+            }
+
+            prev_time = time;
         }
 
         result
@@ -16,21 +26,23 @@ impl Solution {
 }
 
 struct Input {
-    nums: Vec<i32>,
+    events: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [3, -2, 1, 1].to_vec(),
+            events: [[1, 2], [2, 5], [3, 9], [1, 15]]
+                .map(|v| v.to_vec())
+                .to_vec(),
         },
         Input {
-            nums: [-1, 4, -1].to_vec(),
+            events: [[10, 5], [1, 7]].map(|v| v.to_vec()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::construct_transformed_array(input.nums);
+        let result = Solution::button_with_longest_time(input.events);
         println!("{:?}", result);
     }
 }
