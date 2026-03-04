f4fcc7de096713c4b13f9ecc90284c3ac98a8eee
mspark777 <topangel777@gmail.com>
2026-03-04 09:37:06 +0900

    3411. Maximum Subarray With Equal Products

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,64 @@
 struct Solution;
 
 impl Solution {
-    pub fn has_match(s: String, p: String) -> bool {
-        let (left, right) = p.split_once("*").unwrap();
+    pub fn max_length(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
 
-        match (s.find(left), s.rfind(right)) {
-            (Some(i), Some(j)) => i + left.len() <= j,
-            _ => false,
+        for (i, p) in nums.iter().copied().enumerate() {
+            let mut p = p as i64;
+            let mut gcd = p;
+            let mut lcm = p;
+            let mut len = 1;
+
+            for n in nums.iter().skip(i + 1).copied() {
+                let n = n as i64;
+                gcd = Self::gcd(gcd, n);
+                p *= n;
+                lcm = Self::lcm(lcm, n);
+
+                if p != (gcd * lcm) {
+                    break;
+                }
+
+                len += 1;
+                result = result.max(len);
+            }
         }
+
+        result
+    }
+
+    fn gcd(a: i64, b: i64) -> i64 {
+        match b {
+            0 => a,
+            _ => Self::gcd(b, a % b),
+        }
+    }
+
+    fn lcm(a: i64, b: i64) -> i64 {
+        (a / Self::gcd(a, b)) * b
     }
 }
 
 struct Input {
-    s: String,
-    p: String,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "leetcode".to_string(),
-            p: "ee*e".to_string(),
+            nums: [1, 2, 1, 2, 1, 1, 1].to_vec(),
         },
         Input {
-            s: "car".to_string(),
-            p: "s*v".to_string(),
+            nums: [2, 3, 4, 5, 6].to_vec(),
         },
         Input {
-            s: "luck".to_string(),
-            p: "u*".to_string(),
+            nums: [1, 2, 3, 1, 4, 5, 1].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::has_match(input.s, input.p);
+        let result = Solution::max_length(input.nums);
         println!("{:?}", result);
     }
 }
