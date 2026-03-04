cd067de5e73fa704034bbe1cbd4cbab7104d8c4d
mspark777 <topangel777@gmail.com>
2026-03-05 08:49:09 +0900

    3417. Zigzag Grid Traversal With Skip

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,50 @@
 struct Solution;
 
 impl Solution {
-    pub fn max_length(nums: Vec<i32>) -> i32 {
-        let mut result = 0;
+    pub fn zigzag_traversal(grid: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut result = Vec::<i32>::new();
 
-        for (i, p) in nums.iter().copied().enumerate() {
-            let mut p = p as i64;
-            let mut gcd = p;
-            let mut lcm = p;
-            let mut len = 1;
-
-            for n in nums.iter().skip(i + 1).copied() {
-                let n = n as i64;
-                gcd = Self::gcd(gcd, n);
-                p *= n;
-                lcm = Self::lcm(lcm, n);
-
-                if p != (gcd * lcm) {
-                    break;
+        for (i, row) in grid.iter().enumerate() {
+            if (i & 1) == 0 {
+                for (j, n) in row.iter().copied().enumerate() {
+                    if (j & 1) == 0 {
+                        result.push(n);
+                    }
                 }
-
-                len += 1;
-                result = result.max(len);
+            } else {
+                for (j, n) in row.iter().copied().enumerate().rev() {
+                    if (j & 1) == 1 {
+                        result.push(n);
+                    }
+                }
             }
         }
 
         result
     }
-
-    fn gcd(a: i64, b: i64) -> i64 {
-        match b {
-            0 => a,
-            _ => Self::gcd(b, a % b),
-        }
-    }
-
-    fn lcm(a: i64, b: i64) -> i64 {
-        (a / Self::gcd(a, b)) * b
-    }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    grid: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 1, 2, 1, 1, 1].to_vec(),
+            grid: [[1, 2], [3, 4]].map(|v| v.to_vec()).to_vec(),
         },
         Input {
-            nums: [2, 3, 4, 5, 6].to_vec(),
+            grid: [[2, 1], [2, 1], [2, 1]].map(|v| v.to_vec()).to_vec(),
         },
         Input {
-            nums: [1, 2, 3, 1, 4, 5, 1].to_vec(),
+            grid: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+                .map(|v| v.to_vec())
+                .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::max_length(input.nums);
+        let result = Solution::zigzag_traversal(input.grid);
         println!("{:?}", result);
     }
 }
