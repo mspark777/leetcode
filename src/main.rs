e8f771a018892e4ea693f61f476e8995ca601b1a
mspark777 <topangel777@gmail.com>
2026-03-07 12:34:41 +0900

    3438. Find Valid Pair of Adjacent Digits in String

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
 struct Solution;
 
 impl Solution {
-    pub fn count_partitions(nums: Vec<i32>) -> i32 {
-        let result = match nums.iter().sum::<i32>() & 1 {
-            0 => nums.len() - 1,
-            _ => 0,
-        };
+    pub fn find_valid_pair(s: String) -> String {
+        use std::collections::HashMap;
 
-        result as i32
+        let mut counts = HashMap::<char, i32>::new();
+        for ch in s.chars() {
+            counts.entry(ch).and_modify(|c| *c += 1).or_insert(1);
+        }
+
+        for (left, right) in s.chars().zip(s.chars().skip(1)) {
+            if left == right {
+                continue;
+            }
+
+            let lcount = counts.get(&left).copied().unwrap_or_default();
+            let rcount = counts.get(&right).copied().unwrap_or_default();
+            let lnum = (left as i32) - ('0' as i32);
+            let rnum = (right as i32) - ('0' as i32);
+
+            if (lnum == lcount) && (rnum == rcount) {
+                return format!("{}{}", left, right);
+            }
+        }
+
+        String::new()
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [10, 10, 3, 7, 6].to_vec(),
+            s: "2523533".to_string(),
         },
         Input {
-            nums: [1, 2, 2].to_vec(),
+            s: "221".to_string(),
         },
         Input {
-            nums: [2, 4, 6, 8].to_vec(),
+            s: "22".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::count_partitions(input.nums);
+        let result = Solution::find_valid_pair(input.s);
         println!("{:?}", result);
     }
 }
