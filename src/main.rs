c4c48c2ba95f3c6d61f28adc7e9cb1700cdb8a50
mspark777 <topangel777@gmail.com>
2026-03-08 09:07:25 +0900

    3456. Find Special Substring of Length K

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,47 @@
 struct Solution;
 
 impl Solution {
-    pub fn sum_of_good_numbers(nums: Vec<i32>, k: i32) -> i32 {
+    pub fn has_special_substring(s: String, k: i32) -> bool {
         let k = k as usize;
-        let n = nums.len();
-        let mut result = 0;
-        for (i, num) in nums.iter().copied().enumerate() {
-            let left = match i < k {
-                true => true,
-                _ => num > nums[i - k],
-            };
+        let mut n = 0usize;
+        let mut i = 0usize;
+        let mut left = s.chars().next().unwrap();
 
-            let right = match (i + k) < n {
-                true => num > nums[i + k],
-                _ => true,
-            };
-
-            if left && right {
-                result += num;
+        for (j, right) in s.chars().enumerate() {
+            n += 1;
+            if left == right {
+                continue;
+            } else if (j - i) == k {
+                return true;
             }
+
+            left = right;
+            i = j;
         }
 
-        result
+        (n - i) == k
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
     k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 3, 2, 1, 5, 4].to_vec(),
-            k: 2,
+            s: "aaabaaa".to_string(),
+            k: 3,
         },
         Input {
-            nums: [2, 1].to_vec(),
-            k: 1,
+            s: "abc".to_string(),
+            k: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::sum_of_good_numbers(input.nums, input.k);
+        let result = Solution::has_special_substring(input.s, input.k);
         println!("{:?}", result);
     }
 }
