f9ab7c60d9221e7d40daeb03850c365fccab3622
mspark777 <topangel777@gmail.com>
2026-03-11 08:40:48 +0900

    3492. Maximum Containers on a Ship

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,33 @@
 struct Solution;
 
 impl Solution {
-    pub fn max_sum(nums: Vec<i32>) -> i32 {
-        use std::collections::HashSet;
-        let set = nums
-            .iter()
-            .copied()
-            .filter(|&n| n > 0)
-            .collect::<HashSet<i32>>();
-
-        match set.is_empty() {
-            true => nums.iter().copied().max().unwrap(),
-            _ => set.iter().sum(),
-        }
+    pub fn max_containers(n: i32, w: i32, max_weight: i32) -> i32 {
+        (n * n).min(max_weight / w)
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
+    w: i32,
+    max_weight: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 3, 4, 5].to_vec(),
+            n: 2,
+            w: 3,
+            max_weight: 15,
         },
         Input {
-            nums: [1, 1, 0, 1, 1].to_vec(),
-        },
-        Input {
-            nums: [1, 2, -1, -2, 1, 0, -1].to_vec(),
+            n: 3,
+            w: 5,
+            max_weight: 20,
         },
     ];
 
     for input in inputs {
-        let result = Solution::max_sum(input.nums);
+        let result = Solution::max_containers(input.n, input.w, input.max_weight);
         println!("{:?}", result);
     }
 }
