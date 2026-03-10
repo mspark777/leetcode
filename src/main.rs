51283b02d7c7b9d4684250b0c7067befe08a9616
mspark777 <topangel777@gmail.com>
2026-03-10 09:33:17 +0900

    3487. Maximum Unique Subarray Sum After Deletion

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,40 @@
 struct Solution;
 
 impl Solution {
-    pub fn total_numbers(digits: Vec<i32>) -> i32 {
-        let mut frequencies = [0; 10];
+    pub fn max_sum(nums: Vec<i32>) -> i32 {
+        use std::collections::HashSet;
+        let set = nums
+            .iter()
+            .copied()
+            .filter(|&n| n > 0)
+            .collect::<HashSet<i32>>();
 
-        for digit in digits.iter().copied() {
-            let d = digit as usize;
-            frequencies[d] += 1;
+        match set.is_empty() {
+            true => nums.iter().copied().max().unwrap(),
+            _ => set.iter().sum(),
         }
-
-        let mut zero = 0;
-        let mut even = 0;
-        let mut all = 0;
-
-        for (i, frequency) in frequencies.iter().copied().enumerate() {
-            if frequency < 1 {
-                continue;
-            }
-
-            if i == 0 {
-                zero += 1;
-            }
-
-            if (i & 1) == 0 {
-                even += 1;
-            }
-
-            all += 1;
-        }
-
-        let mut result = even * (all - 1) * (all - 2);
-        if zero == 1 {
-            result -= (even - 1) * (all - 2);
-        }
-
-        for (i, frequency) in frequencies.iter().copied().enumerate() {
-            if frequency < 2 {
-                continue;
-            }
-
-            if i == 0 {
-                result += all - 1;
-            } else if (i & 1) == 1 {
-                result += even;
-            } else {
-                result += 3 * (even - 1) - zero;
-                result += 2 * (all - even);
-            }
-        }
-
-        for frequency in frequencies.iter().skip(2).step_by(2).copied() {
-            if frequency >= 3 {
-                result += 1;
-            }
-        }
-
-        result
     }
 }
 
 struct Input {
-    digits: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            digits: [1, 2, 3, 4].to_vec(),
+            nums: [1, 2, 3, 4, 5].to_vec(),
         },
         Input {
-            digits: [0, 2, 2].to_vec(),
+            nums: [1, 1, 0, 1, 1].to_vec(),
         },
         Input {
-            digits: [6, 6, 6].to_vec(),
-        },
-        Input {
-            digits: [1, 3, 5].to_vec(),
+            nums: [1, 2, -1, -2, 1, 0, -1].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::total_numbers(input.digits);
+        let result = Solution::max_sum(input.nums);
         println!("{:?}", result);
     }
 }
