e01cc5ce656f4bd911cd1f77c5b3dfcb71d34081
mspark777 <topangel777@gmail.com>
2026-03-15 16:22:54 +0900

    3541. Find Most Frequent Vowel and Consonant

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,43 @@
 struct Solution;
 
 impl Solution {
-    pub fn max_product(n: i32) -> i32 {
-        let mut first = 0;
-        let mut second = 0;
-        let mut result = 0;
-        let mut n = n;
+    pub fn max_freq_sum(s: String) -> i32 {
+        const A: usize = 'a' as usize;
+        let mut frequencies = [0i32; 26];
 
-        while n > 0 {
-            let digit = n % 10;
-            n /= 10;
-
-            if digit > first {
-                second = first;
-                first = digit;
-            } else if digit > second {
-                second = digit;
-            }
-
-            result = result.max(first * second);
+        for ch in s.chars() {
+            let code = ch as usize;
+            let idx = code - A;
+            frequencies[idx] += 1;
         }
 
-        result
+        let mut max_vowel = 0;
+        for vowel in [b'a', b'e', b'i', b'o', b'u'] {
+            let idx = (vowel as usize) - A;
+            max_vowel = max_vowel.max(frequencies[idx]);
+            frequencies[idx] = 0;
+        }
+
+        max_vowel + frequencies.iter().copied().max().unwrap_or_default()
     }
 }
 
 struct Input {
-    n: i32,
+    s: String,
 }
 
 fn main() {
-    let inputs = [Input { n: 31 }, Input { n: 22 }, Input { n: 124 }];
+    let inputs = [
+        Input {
+            s: "successes".to_string(),
+        },
+        Input {
+            s: "aeiaeia".to_string(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::max_product(input.n);
+        let result = Solution::max_freq_sum(input.s);
         println!("{:?}", result);
     }
 }
