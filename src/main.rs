7563392afd11a226e41d36a1c990ae39b3a3bb21
mspark777 <topangel777@gmail.com>
2026-03-16 10:29:02 +0900

    3560. Find Minimum Log Transportation Cost

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,35 @@
 struct Solution;
 
 impl Solution {
-    pub fn smallest_index(nums: Vec<i32>) -> i32 {
-        nums.into_iter()
-            .enumerate()
-            .find(|&(i, n)| Self::digit_sum(n) == i)
-            .map(|(i, _)| i as i32)
-            .unwrap_or(-1)
-    }
+    pub fn min_cutting_cost(n: i32, m: i32, k: i32) -> i64 {
+        let n = n as i64;
+        let m = m as i64;
+        let k = k as i64;
+        let mut result = 0i64;
 
-    fn digit_sum(mut n: i32) -> usize {
-        let mut sum = 0;
-        while n > 0 {
-            sum += n % 10;
-            n /= 10;
+        if n > k {
+            result += (n - k) * k;
         }
 
-        sum as usize
+        if m > k {
+            result += (m - k) * k;
+        }
+
+        result
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
+    m: i32,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [1, 3, 2].to_vec(),
-        },
-        Input {
-            nums: [1, 10, 11].to_vec(),
-        },
-        Input {
-            nums: [1, 2, 3].to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 6, m: 5, k: 5 }, Input { n: 4, m: 4, k: 6 }];
 
     for input in inputs {
-        let result = Solution::smallest_index(input.nums);
+        let result = Solution::min_cutting_cost(input.n, input.m, input.k);
         println!("{:?}", result);
     }
 }
