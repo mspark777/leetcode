8c130de8fef545f8b0b1aaad9008015039afc1e9
mspark777 <topangel777@gmail.com>
2026-03-18 08:40:21 +0900

    3602. Hexadecimal and Hexatrigesimal Conversion

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,42 @@
 struct Solution;
 
 impl Solution {
-    pub fn check_prime_frequency(nums: Vec<i32>) -> bool {
-        let mut counts = [0; 101];
-        for num in nums {
-            let idx = num as usize;
-            counts[idx] += 1;
+    pub fn concat_hex36(n: i32) -> String {
+        let base16 = Self::to_base(n * n, 16);
+        let base36 = Self::to_base(n * n * n, 36);
+
+        format!("{}{}", base16, base36)
+    }
+
+    fn to_base(mut num: i32, base: i32) -> String {
+        let symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            .chars()
+            .collect::<Vec<char>>();
+        let mut indexes = Vec::<usize>::with_capacity(((num / base) + 1) as usize);
+        while num > 0 {
+            let idx = (num % base) as usize;
+            indexes.push(idx);
+            num /= base;
         }
 
-        counts
+        indexes.reverse();
+        indexes
             .into_iter()
-            .find(|&count| Self::check(count))
-            .is_some()
-    }
-
-    fn check(count: i32) -> bool {
-        let d = Self::divable;
-        match count {
-            2 | 3 | 5 | 7 => true,
-            _ if (count == 1) || d(count, 2) || d(count, 3) || d(count, 5) || d(count, 7) => false,
-            _ => true,
-        }
-    }
-
-    fn divable(left: i32, right: i32) -> bool {
-        (left % right) == 0
+            .map(|idx| symbols[idx].to_string())
+            .collect::<Vec<_>>()
+            .join("")
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [1, 2, 3, 4, 5, 4].to_vec(),
-        },
-        Input {
-            nums: [1, 2, 3, 4, 5].to_vec(),
-        },
-        Input {
-            nums: [2, 2, 2, 4, 4].to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 13 }, Input { n: 36 }];
 
     for input in inputs {
-        let result = Solution::check_prime_frequency(input.nums);
+        let result = Solution::concat_hex36(input.n);
         println!("{:?}", result);
     }
 }
