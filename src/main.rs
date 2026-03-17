98f0d5f814f7d97406f81cc91973079197729583
mspark777 <topangel777@gmail.com>
2026-03-17 16:59:05 +0900

    3591. Check if Any Element Has Prime Frequency

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,52 @@
 struct Solution;
 
 impl Solution {
-    pub fn generate_tag(caption: String) -> String {
-        const LOWER: i32 = 0;
-        const UPPER: i32 = 1;
-        const MAX_LEN: usize = 100;
-
-        let mut result = String::with_capacity(caption.len().min(MAX_LEN));
-        result.push('#');
-
-        let mut current = LOWER;
-        for ch in caption.trim().chars() {
-            if ch == ' ' {
-                current = UPPER;
-            } else if current == LOWER {
-                result.push(ch.to_ascii_lowercase());
-            } else {
-                current = LOWER;
-                result.push(ch.to_ascii_uppercase());
-            }
-
-            if result.len() >= MAX_LEN {
-                break;
-            }
+    pub fn check_prime_frequency(nums: Vec<i32>) -> bool {
+        let mut counts = [0; 101];
+        for num in nums {
+            let idx = num as usize;
+            counts[idx] += 1;
         }
 
-        result
+        counts
+            .into_iter()
+            .find(|&count| Self::check(count))
+            .is_some()
+    }
+
+    fn check(count: i32) -> bool {
+        let d = Self::divable;
+        match count {
+            2 | 3 | 5 | 7 => true,
+            _ if (count == 1) || d(count, 2) || d(count, 3) || d(count, 5) || d(count, 7) => false,
+            _ => true,
+        }
+    }
+
+    fn divable(left: i32, right: i32) -> bool {
+        (left % right) == 0
     }
 }
 
 struct Input {
-    caption: String,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input {
-        caption: "Leetcode daily streak achieved".to_string(),
-    }, Input {
-        caption: "can I Go There".to_string(),
-    }, Input {
-        caption: "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh".to_string()
-    }];
+    let inputs = [
+        Input {
+            nums: [1, 2, 3, 4, 5, 4].to_vec(),
+        },
+        Input {
+            nums: [1, 2, 3, 4, 5].to_vec(),
+        },
+        Input {
+            nums: [2, 2, 2, 4, 4].to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::generate_tag(input.caption);
+        let result = Solution::check_prime_frequency(input.nums);
         println!("{:?}", result);
     }
 }
