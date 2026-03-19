1991ec8f89ff08eb6f0e6f2cbca6c004fc3ffce5
mspark777 <topangel777@gmail.com>
2026-03-19 09:26:02 +0900

    3622. Check Divisibility by Digit Sum and Product

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,32 @@
-use std::cmp::Ordering;
-
 struct Solution;
 
 impl Solution {
-    pub fn validate_coupons(
-        code: Vec<String>,
-        business_line: Vec<String>,
-        is_active: Vec<bool>,
-    ) -> Vec<String> {
-        let mut list = code
-            .into_iter()
-            .zip(business_line)
-            .zip(is_active)
-            .filter(|((c, b), a)| Self::valid_code(c) && Self::valid_business(b) && *a)
-            .map(|((c, b), _)| (c, b))
-            .collect::<Vec<(String, String)>>();
-        list.sort_by(Self::sort_by);
-        list.into_iter().map(|(code, _)| code).collect()
-    }
+    pub fn check_divisibility(n: i32) -> bool {
+        let mut sum = 0;
+        let mut product = 1;
+        let mut num = n;
 
-    fn valid_code(code: &str) -> bool {
-        !code.is_empty() && code.chars().find(Self::check_invalid_code).is_none()
-    }
+        while num > 0 {
+            let digit = num % 10;
+            num /= 10;
 
-    fn check_invalid_code(ch: &char) -> bool {
-        let lower = 'a'..='z';
-        let upper = 'A'..='Z';
-        let digit = '0'..='9';
-        let under = '_';
-
-        let ok = lower.contains(ch) || upper.contains(ch) || digit.contains(ch) || (*ch == under);
-        !ok
-    }
-
-    fn valid_business(line: &str) -> bool {
-        ["electronics", "grocery", "pharmacy", "restaurant"].contains(&line)
-    }
-
-    fn sort_by(left: &(String, String), right: &(String, String)) -> Ordering {
-        match left.1.cmp(&right.1) {
-            Ordering::Equal => left.0.cmp(&right.0),
-            order => order,
+            sum += digit;
+            product *= digit;
         }
+
+        (n % (sum + product)) == 0
     }
 }
 
 struct Input {
-    code: Vec<String>,
-    business_line: Vec<String>,
-    is_active: Vec<bool>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            code: ["SAVE20", "", "PHARMA5", "SAVE@20"]
-                .map(|s| s.to_string())
-                .to_vec(),
-            business_line: ["restaurant", "grocery", "pharmacy", "restaurant"]
-                .map(|s| s.to_string())
-                .to_vec(),
-            is_active: [true, true, true, true].to_vec(),
-        },
-        Input {
-            code: ["GROCERY15", "ELECTRONICS_50", "DISCOUNT10"]
-                .map(|s| s.to_string())
-                .to_vec(),
-            business_line: ["grocery", "electronics", "invalid"]
-                .map(|s| s.to_string())
-                .to_vec(),
-            is_active: [false, true, true].to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 99 }, Input { n: 23 }];
 
     for input in inputs {
-        let result = Solution::validate_coupons(input.code, input.business_line, input.is_active);
+        let result = Solution::check_divisibility(input.n);
         println!("{:?}", result);
     }
 }
