ec46efb6b1a9f0af8a075388a5a863fbadfb1e6d
mspark777 <topangel777@gmail.com>
2026-03-19 09:37:49 +0900

    3633. Earliest Finish Time for Land and Water Rides I

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,70 @@
 struct Solution;
 
 impl Solution {
-    pub fn check_divisibility(n: i32) -> bool {
-        let mut sum = 0;
-        let mut product = 1;
-        let mut num = n;
+    pub fn earliest_finish_time(
+        land_start_time: Vec<i32>,
+        land_duration: Vec<i32>,
+        water_start_time: Vec<i32>,
+        water_duration: Vec<i32>,
+    ) -> i32 {
+        let land = land_start_time
+            .iter()
+            .copied()
+            .zip(land_duration.iter().copied())
+            .map(|(s, d)| s + d)
+            .min()
+            .unwrap();
 
-        while num > 0 {
-            let digit = num % 10;
-            num /= 10;
+        let water = water_start_time
+            .iter()
+            .copied()
+            .zip(water_duration.iter().copied())
+            .map(|(s, d)| s + d)
+            .min()
+            .unwrap();
 
-            sum += digit;
-            product *= digit;
-        }
+        let water_land = land_start_time
+            .iter()
+            .copied()
+            .zip(land_duration.iter().copied())
+            .map(|(s, d)| s.max(water) + d)
+            .min()
+            .unwrap();
 
-        (n % (sum + product)) == 0
+        let land_water = water_start_time
+            .iter()
+            .copied()
+            .zip(water_duration.iter().copied())
+            .map(|(s, d)| s.max(land) + d)
+            .min()
+            .unwrap();
+
+        water_land.min(land_water)
     }
 }
 
 struct Input {
-    n: i32,
+    land_start_time: Vec<i32>,
+    land_duration: Vec<i32>,
+    water_start_time: Vec<i32>,
+    water_duration: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { n: 99 }, Input { n: 23 }];
+    let inputs = [Input {
+        land_start_time: [2, 8].to_vec(),
+        land_duration: [4, 1].to_vec(),
+        water_start_time: [6].to_vec(),
+        water_duration: [3].to_vec(),
+    }];
 
     for input in inputs {
-        let result = Solution::check_divisibility(input.n);
+        let result = Solution::earliest_finish_time(
+            input.land_start_time,
+            input.land_duration,
+            input.water_start_time,
+            input.water_duration,
+        );
         println!("{:?}", result);
     }
 }
