54c1b0c6bf57c78cc6b602ec27a162748f63a6e1
mspark777 <topangel777@gmail.com>
2026-03-20 11:35:12 +0900

    3643. Flip Square Submatrix Vertically

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,64 @@
 struct Solution;
 
 impl Solution {
-    pub fn is_trionic(nums: Vec<i32>) -> bool {
-        let n = nums.len();
-        if nums[0] >= nums[1] {
-            return false;
-        }
+    pub fn reverse_submatrix(mut grid: Vec<Vec<i32>>, x: i32, y: i32, k: i32) -> Vec<Vec<i32>> {
+        let x = x as usize;
+        let y = y as usize;
+        let k = k as usize;
 
-        let mut count = 1;
-        for i in 2..n {
-            if nums[i - 1] == nums[i] {
-                return false;
+        let mut top = x;
+        let mut bottom = x + k - 1;
+        let left = y;
+        let right = y + k - 1;
+
+        while top < bottom {
+            #[allow(clippy::needless_range_loop)]
+            for c in left..=right {
+                let t = grid[top][c];
+                grid[top][c] = grid[bottom][c];
+                grid[bottom][c] = t;
             }
 
-            if ((nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i])) < 0 {
-                count += 1;
-            }
+            top += 1;
+            bottom -= 1;
         }
 
-        count == 3
+        grid
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    grid: Vec<Vec<i32>>,
+    x: i32,
+    y: i32,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 3, 5, 4, 2, 6].to_vec(),
+            grid: [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12],
+                [13, 14, 15, 16],
+            ]
+            .map(|v| v.to_vec())
+            .to_vec(),
+            x: 1,
+            y: 0,
+            k: 3,
         },
         Input {
-            nums: [2, 1, 3].to_vec(),
+            grid: [[3, 4, 2, 3], [2, 3, 4, 2]].map(|v| v.to_vec()).to_vec(),
+            x: 0,
+            y: 2,
+            k: 2,
         },
     ];
 
     for input in inputs {
-        let result = Solution::is_trionic(input.nums);
+        let result = Solution::reverse_submatrix(input.grid, input.x, input.y, input.k);
         println!("{:?}", result);
     }
 }
