b1744d8d56f53f6dd72998dbad57560319b540b4
mspark777 <topangel777@gmail.com>
2026-03-20 11:21:23 +0900

    3637. Trionic Array I

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,43 @@
 struct Solution;
 
 impl Solution {
-    pub fn earliest_finish_time(
-        land_start_time: Vec<i32>,
-        land_duration: Vec<i32>,
-        water_start_time: Vec<i32>,
-        water_duration: Vec<i32>,
-    ) -> i32 {
-        let land = land_start_time
-            .iter()
-            .copied()
-            .zip(land_duration.iter().copied())
-            .map(|(s, d)| s + d)
-            .min()
-            .unwrap();
+    pub fn is_trionic(nums: Vec<i32>) -> bool {
+        let n = nums.len();
+        if nums[0] >= nums[1] {
+            return false;
+        }
 
-        let water = water_start_time
-            .iter()
-            .copied()
-            .zip(water_duration.iter().copied())
-            .map(|(s, d)| s + d)
-            .min()
-            .unwrap();
+        let mut count = 1;
+        for i in 2..n {
+            if nums[i - 1] == nums[i] {
+                return false;
+            }
 
-        let water_land = land_start_time
-            .iter()
-            .copied()
-            .zip(land_duration.iter().copied())
-            .map(|(s, d)| s.max(water) + d)
-            .min()
-            .unwrap();
+            if ((nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i])) < 0 {
+                count += 1;
+            }
+        }
 
-        let land_water = water_start_time
-            .iter()
-            .copied()
-            .zip(water_duration.iter().copied())
-            .map(|(s, d)| s.max(land) + d)
-            .min()
-            .unwrap();
-
-        water_land.min(land_water)
+        count == 3
     }
 }
 
 struct Input {
-    land_start_time: Vec<i32>,
-    land_duration: Vec<i32>,
-    water_start_time: Vec<i32>,
-    water_duration: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input {
-        land_start_time: [2, 8].to_vec(),
-        land_duration: [4, 1].to_vec(),
-        water_start_time: [6].to_vec(),
-        water_duration: [3].to_vec(),
-    }];
+    let inputs = [
+        Input {
+            nums: [1, 3, 5, 4, 2, 6].to_vec(),
+        },
+        Input {
+            nums: [2, 1, 3].to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::earliest_finish_time(
-            input.land_start_time,
-            input.land_duration,
-            input.water_start_time,
-            input.water_duration,
-        );
+        let result = Solution::is_trionic(input.nums);
         println!("{:?}", result);
     }
 }
