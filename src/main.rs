1fa529af79cc13c266b48af3bbbfcc628f347dfa
mspark777 <topangel777@gmail.com>
2026-03-21 10:22:09 +0900

    3658. GCD of Odd and Even Sums

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,20 @@
 struct Solution;
 
 impl Solution {
-    pub fn reverse_submatrix(mut grid: Vec<Vec<i32>>, x: i32, y: i32, k: i32) -> Vec<Vec<i32>> {
-        let x = x as usize;
-        let y = y as usize;
-        let k = k as usize;
-
-        let mut top = x;
-        let mut bottom = x + k - 1;
-        let left = y;
-        let right = y + k - 1;
-
-        while top < bottom {
-            #[allow(clippy::needless_range_loop)]
-            for c in left..=right {
-                let t = grid[top][c];
-                grid[top][c] = grid[bottom][c];
-                grid[bottom][c] = t;
-            }
-
-            top += 1;
-            bottom -= 1;
-        }
-
-        grid
+    pub fn gcd_of_odd_even_sums(n: i32) -> i32 {
+        n
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
-    x: i32,
-    y: i32,
-    k: i32,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            grid: [
-                [1, 2, 3, 4],
-                [5, 6, 7, 8],
-                [9, 10, 11, 12],
-                [13, 14, 15, 16],
-            ]
-            .map(|v| v.to_vec())
-            .to_vec(),
-            x: 1,
-            y: 0,
-            k: 3,
-        },
-        Input {
-            grid: [[3, 4, 2, 3], [2, 3, 4, 2]].map(|v| v.to_vec()).to_vec(),
-            x: 0,
-            y: 2,
-            k: 2,
-        },
-    ];
+    let inputs = [Input { n: 4 }, Input { n: 5 }];
 
     for input in inputs {
-        let result = Solution::reverse_submatrix(input.grid, input.x, input.y, input.k);
+        let result = Solution::gcd_of_odd_even_sums(input.n);
         println!("{:?}", result);
     }
 }
