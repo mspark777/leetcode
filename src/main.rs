3bf4eec257effd7270ee475e54488dff800fe8ee
mspark777 <topangel777@gmail.com>
2026-03-22 09:22:11 +0900

    3674. Minimum Operations to Equalize Array

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,32 @@
 struct Solution;
 
 impl Solution {
-    pub fn recover_order(order: Vec<i32>, friends: Vec<i32>) -> Vec<i32> {
-        use std::collections::HashSet;
-
-        let friend_set = HashSet::<i32>::from_iter(friends);
-        let mut result = Vec::<i32>::with_capacity(friend_set.len());
-        for id in order {
-            if friend_set.contains(&id) {
-                result.push(id);
-            }
-        }
-
-        result
+    pub fn min_operations(nums: Vec<i32>) -> i32 {
+        let first = nums[0];
+        nums.into_iter()
+            .skip(1)
+            .find(|&num| num != first)
+            .map(|_| 1)
+            .unwrap_or_default()
     }
 }
 
 struct Input {
-    order: Vec<i32>,
-    friends: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            order: [3, 1, 2, 5, 4].to_vec(),
-            friends: [1, 3, 4].to_vec(),
+            nums: [1, 2].to_vec(),
         },
         Input {
-            order: [1, 4, 5, 3, 2].to_vec(),
-            friends: [2, 5].to_vec(),
+            nums: [5, 5, 5].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::recover_order(input.order, input.friends);
+        let result = Solution::min_operations(input.nums);
         println!("{:?}", result);
     }
 }
