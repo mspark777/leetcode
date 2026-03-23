e75d7a876a8d94c73de6f834152a8c69fff258cf
mspark777 <topangel777@gmail.com>
2026-03-24 08:27:33 +0900

    3683. Earliest Time to Finish One Task

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,31 @@
 struct Solution;
 
 impl Solution {
-    pub fn smallest_absent(nums: Vec<i32>) -> i32 {
-        use std::collections::HashSet;
-
-        let mut sum = 0;
-        let n = nums.len() as i32;
-        for num in nums.iter().copied() {
-            sum += num;
-        }
-
-        let n_set = HashSet::<i32>::from_iter(nums);
-        let mut result = 0.max(sum / n) + 1;
-
-        while n_set.contains(&result) {
-            result += 1;
-        }
-
-        result
+    pub fn earliest_time(tasks: Vec<Vec<i32>>) -> i32 {
+        tasks
+            .into_iter()
+            .map(|v| v.into_iter().sum())
+            .min()
+            .unwrap_or_default()
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    tasks: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [3, 5].to_vec(),
+            tasks: [[1, 6], [2, 3]].map(|v| v.to_vec()).to_vec(),
         },
         Input {
-            nums: [-1, 1, 2].to_vec(),
-        },
-        Input {
-            nums: [4, -1].to_vec(),
+            tasks: [[100, 100], [100, 100]].map(|v| v.to_vec()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::smallest_absent(input.nums);
+        let result = Solution::earliest_time(input.tasks);
         println!("{:?}", result);
     }
 }
