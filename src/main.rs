060975a4b385bd856af986e6db258deb775b5358
mspark777 <topangel777@gmail.com>
2026-03-24 08:36:12 +0900

    3684. Maximize Sum of At Most K Distinct Elements

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
 struct Solution;
 
 impl Solution {
-    pub fn earliest_time(tasks: Vec<Vec<i32>>) -> i32 {
-        tasks
-            .into_iter()
-            .map(|v| v.into_iter().sum())
-            .min()
-            .unwrap_or_default()
+    pub fn max_k_distinct(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        use std::collections::HashSet;
+
+        let k = k as usize;
+        let mut nums = nums;
+
+        nums.sort_by_key(|n| -n);
+        let mut checked = HashSet::<i32>::from_iter(nums.iter().copied());
+        let mut result = Vec::<i32>::with_capacity(checked.len());
+        for num in nums {
+            if checked.contains(&num) {
+                result.push(num);
+                checked.remove(&num);
+
+                if result.len() >= k {
+                    break;
+                }
+            }
+        }
+
+        result
     }
 }
 
 struct Input {
-    tasks: Vec<Vec<i32>>,
+    nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            tasks: [[1, 6], [2, 3]].map(|v| v.to_vec()).to_vec(),
+            nums: [84, 93, 100, 77, 90].to_vec(),
+            k: 3,
         },
         Input {
-            tasks: [[100, 100], [100, 100]].map(|v| v.to_vec()).to_vec(),
+            nums: [84, 93, 100, 77, 93].to_vec(),
+            k: 3,
+        },
+        Input {
+            nums: [1, 1, 1, 2, 2, 2].to_vec(),
+            k: 6,
         },
     ];
 
     for input in inputs {
-        let result = Solution::earliest_time(input.tasks);
+        let result = Solution::max_k_distinct(input.nums, input.k);
         println!("{:?}", result);
     }
 }
