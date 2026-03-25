04b069e2c4f46b37782a2a925a3f2fc970df44f1
mspark777 <topangel777@gmail.com>
2026-03-25 11:03:00 +0900

    3692. Majority Frequency Characters

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
 struct Solution;
 
 impl Solution {
-    pub fn even_number_bitwise_o_rs(nums: Vec<i32>) -> i32 {
-        nums.into_iter().fold(0, |result, num| match num & 1 {
-            0 => result | num,
-            _ => result,
-        })
+    pub fn majority_frequency_group(s: String) -> String {
+        let mut freqs = [0; 26];
+        for c in s.bytes() {
+            freqs[(c - b'a') as usize] += 1;
+        }
+        let mut fprim = vec![0; 101];
+        for f in freqs.iter() {
+            fprim[*f as usize] += 1;
+        }
+        let max_prim = fprim[1..].iter().max().unwrap();
+        let freq_grp = fprim.iter().rposition(|x| x == max_prim).unwrap() as i32;
+        let mut res = String::new();
+        for (idx, f) in freqs.iter().enumerate() {
+            if *f == freq_grp {
+                res.push((idx as u8 + b'a') as char)
+            }
+        }
+
+        res
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 3, 4, 5, 6].to_vec(),
+            s: "aaabbbccdddde".to_string(),
         },
         Input {
-            nums: [7, 9, 11].to_vec(),
+            s: "abcd".to_string(),
         },
         Input {
-            nums: [1, 8, 16].to_vec(),
+            s: "pfpfgi".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::even_number_bitwise_o_rs(input.nums);
+        let result = Solution::majority_frequency_group(input.s);
         println!("{:?}", result);
     }
 }
