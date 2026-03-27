16403898311141869d50bda39fb1f22dc86325f0
mspark777 <topangel777@gmail.com>
2026-03-27 09:26:40 +0900

    3718

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 struct Solution;
 
 impl Solution {
-    pub fn sum_divisible_by_k(nums: Vec<i32>, k: i32) -> i32 {
-        let mut counts = [0; 101];
-        for num in nums {
-            let idx = num as usize;
-            counts[idx] += 1;
-        }
+    pub fn missing_multiple(nums: Vec<i32>, k: i32) -> i32 {
+        use std::collections::HashSet;
+        let num_set = HashSet::<i32>::from_iter(nums);
 
-        let mut result = 0;
-        for (i, count) in counts.into_iter().enumerate().skip(1) {
-            if (count > 0) && ((count % k) == 0) {
-                result += (i as i32) * count;
+        for i in (k..201).step_by(k as usize) {
+            if !num_set.contains(&i) {
+                return i;
             }
         }
 
-        result
+        0
     }
 }
 
@@ -27,21 +23,17 @@ struct Input {
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 2, 3, 3, 3, 3, 4].to_vec(),
+            nums: [8, 2, 3, 4, 6].to_vec(),
             k: 2,
         },
         Input {
-            nums: [1, 2, 3, 4, 5].to_vec(),
-            k: 2,
-        },
-        Input {
-            nums: [4, 4, 4, 1, 2, 3].to_vec(),
-            k: 3,
+            nums: [1, 4, 7, 10, 15].to_vec(),
+            k: 5,
         },
     ];
 
     for input in inputs {
-        let result = Solution::sum_divisible_by_k(input.nums, input.k);
+        let result = Solution::missing_multiple(input.nums, input.k);
         println!("{:?}", result);
     }
 }
