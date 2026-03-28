18212e374be9a86009de299b7bec74c3491cf6a8
mspark777 <topangel777@gmail.com>
2026-03-28 12:05:22 +0900

    3740

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_moves(nums: Vec<i32>) -> i32 {
-        let mut max_num = 0;
-        let mut sum = 0;
-        let n = nums.len() as i32;
+    pub fn minimum_distance(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
 
-        for num in nums {
-            sum += num;
-            max_num = max_num.max(num);
+        let mut min_distance = i32::MAX;
+        let mut index_map = HashMap::<i32, Vec<i32>>::new();
+
+        for (i, num) in nums.into_iter().enumerate() {
+            let i = i as i32;
+
+            let index_list = index_map
+                .entry(num)
+                .and_modify(|l| l.push(i))
+                .or_insert(vec![i]);
+
+            let list_len = index_list.len();
+            if list_len < 3 {
+                continue;
+            }
+
+            let i = index_list[list_len - 3];
+            let k = index_list[list_len - 1];
+            min_distance = min_distance.min(k - i);
         }
 
-        (max_num * n) - sum
+        match min_distance {
+            i32::MAX => -1,
+            _ => min_distance * 2,
+        }
     }
 }
 
@@ -22,15 +39,16 @@ struct Input {
 fn main() {
     let inputs = [
         Input {
-            nums: [2, 1, 3].to_vec(),
+            nums: [1, 2, 1, 1, 3].to_vec(),
         },
         Input {
-            nums: [4, 4, 5].to_vec(),
+            nums: [1, 1, 2, 3, 2, 1, 2].to_vec(),
         },
+        Input { nums: [1].to_vec() },
     ];
 
     for input in inputs {
-        let result = Solution::min_moves(input.nums);
+        let result = Solution::minimum_distance(input.nums);
         println!("{:?}", result);
     }
 }
