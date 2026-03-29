f54ee004d6b588b1f0b2076afe8b5ce38e1fd1b9
mspark777 <topangel777@gmail.com>
2026-03-29 11:06:27 +0900

    3754

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_flips(n: i32) -> i32 {
-        let n = n as u32;
-        let rev = n.reverse_bits() >> n.leading_zeros();
-        (n ^ rev).count_ones() as i32
+    pub fn sum_and_multiply(n: i32) -> i64 {
+        let mut power = 1i64;
+        let mut n = n as i64;
+        let mut x = 0i64;
+        let mut sum = 0i64;
+
+        while n > 0 {
+            let digit = n % 10;
+            n /= 10;
+
+            if digit != 0 {
+                sum += digit;
+                x += digit * power;
+                power *= 10;
+            }
+        }
+
+        x * sum
     }
 }
 
@@ -13,10 +27,10 @@ struct Input {
 }
 
 fn main() {
-    let inputs = [Input { n: 7 }, Input { n: 10 }];
+    let inputs = [Input { n: 10203004 }, Input { n: 1000 }];
 
     for input in inputs {
-        let result = Solution::minimum_flips(input.n);
+        let result = Solution::sum_and_multiply(input.n);
         println!("{:?}", result);
     }
 }
