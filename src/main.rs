bcb1c8100bc94aee9c60f7da99aaeaecda1e957c
mspark777 <topangel777@gmail.com>
2026-03-29 10:44:37 +0900

    3745

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
 struct Solution;
 
 impl Solution {
-    pub fn minimum_distance(nums: Vec<i32>) -> i32 {
-        use std::collections::HashMap;
+    pub fn maximize_expression_of_three(nums: Vec<i32>) -> i32 {
+        let mut a = i32::MIN;
+        let mut b = i32::MIN;
+        let mut c = i32::MAX;
 
-        let mut min_distance = i32::MAX;
-        let mut index_map = HashMap::<i32, Vec<i32>>::new();
-
-        for (i, num) in nums.into_iter().enumerate() {
-            let i = i as i32;
-
-            let index_list = index_map
-                .entry(num)
-                .and_modify(|l| l.push(i))
-                .or_insert(vec![i]);
-
-            let list_len = index_list.len();
-            if list_len < 3 {
-                continue;
+        for num in nums {
+            if a < num {
+                b = a;
+                a = num;
+            } else if b < num {
+                b = num;
             }
 
-            let i = index_list[list_len - 3];
-            let k = index_list[list_len - 1];
-            min_distance = min_distance.min(k - i);
+            if c > num {
+                c = num;
+            }
         }
 
-        match min_distance {
-            i32::MAX => -1,
-            _ => min_distance * 2,
-        }
+        a + b - c
     }
 }
 
@@ -39,16 +30,15 @@ struct Input {
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 2, 1, 1, 3].to_vec(),
+            nums: [1, 4, 2, 5].to_vec(),
         },
         Input {
-            nums: [1, 1, 2, 3, 2, 1, 2].to_vec(),
+            nums: [-2, 0, 5, -2, 4].to_vec(),
         },
-        Input { nums: [1].to_vec() },
     ];
 
     for input in inputs {
-        let result = Solution::minimum_distance(input.nums);
+        let result = Solution::maximize_expression_of_three(input.nums);
         println!("{:?}", result);
     }
 }
