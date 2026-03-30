d04f9a8d0f6dd77488cabdfa9f81756af375e206
mspark777 <topangel777@gmail.com>
2026-03-31 08:36:59 +0900

    3783

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,33 @@
 struct Solution;
 
 impl Solution {
-    pub fn abs_difference(nums: Vec<i32>, k: i32) -> i32 {
-        let mut nums = nums;
-        nums.sort();
+    pub fn mirror_distance(n: i32) -> i32 {
+        (n - Self::reverse(n)).abs()
+    }
 
-        let mut result = 0;
+    fn reverse(mut n: i32) -> i32 {
+        let mut r = 0;
 
-        for (left, right) in nums
-            .iter()
-            .copied()
-            .zip(nums.iter().copied().rev())
-            .take(k as usize)
-        {
-            result += right - left;
+        while n > 0 {
+            let d = n % 10;
+            n /= 10;
+
+            r = r * 10 + d;
         }
 
-        result
+        r
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            nums: [5, 2, 2, 4].to_vec(),
-            k: 2,
-        },
-        Input {
-            nums: [100].to_vec(),
-            k: 1,
-        },
-    ];
+    let inputs = [Input { n: 25 }, Input { n: 10 }, Input { n: 7 }];
 
     for input in inputs {
-        let result = Solution::abs_difference(input.nums, input.k);
+        let result = Solution::mirror_distance(input.n);
         println!("{:?}", result);
     }
 }
