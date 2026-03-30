e97eb133ef957c7d94c12687eb7fd5b9dbe12ae2
mspark777 <topangel777@gmail.com>
2026-03-30 15:27:53 +0900

    3774

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,44 @@
 struct Solution;
 
 impl Solution {
-    pub fn sort_by_reflection(nums: Vec<i32>) -> Vec<i32> {
+    pub fn abs_difference(nums: Vec<i32>, k: i32) -> i32 {
         let mut nums = nums;
+        nums.sort();
 
-        nums.sort_by(|&a, &b| Self::compare(a as u32, b as u32));
-        nums
-    }
+        let mut result = 0;
 
-    fn compare(left: u32, right: u32) -> std::cmp::Ordering {
-        let l = left.reverse_bits() >> left.leading_zeros();
-        let r = right.reverse_bits() >> right.leading_zeros();
-
-        match l.cmp(&r) {
-            std::cmp::Ordering::Equal => left.cmp(&right),
-            ord => ord,
+        for (left, right) in nums
+            .iter()
+            .copied()
+            .zip(nums.iter().copied().rev())
+            .take(k as usize)
+        {
+            result += right - left;
         }
+
+        result
     }
 }
 
 struct Input {
     nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [4, 5, 4].to_vec(),
+            nums: [5, 2, 2, 4].to_vec(),
+            k: 2,
         },
         Input {
-            nums: [3, 6, 5, 8].to_vec(),
+            nums: [100].to_vec(),
+            k: 1,
         },
     ];
 
     for input in inputs {
-        let result = Solution::sort_by_reflection(input.nums);
+        let result = Solution::abs_difference(input.nums, input.k);
         println!("{:?}", result);
     }
 }
