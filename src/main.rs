700d509c2fc085ef0f88323252a0f5f0d6a46f7f
mspark777 <topangel777@gmail.com>
2026-04-08 08:50:28 +0900

    3838. Weighted Word Mapping

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
 struct Solution;
 
 impl Solution {
-    pub fn dominant_indices(nums: Vec<i32>) -> i32 {
-        let mut count = nums.len() as i32;
-        let mut sum = nums.iter().copied().sum::<i32>();
-        let mut result = 0;
+    pub fn map_word_weights(words: Vec<String>, weights: Vec<i32>) -> String {
+        let mut mapped_list = Vec::<char>::with_capacity(words.len());
 
-        for num in nums {
-            if count == 1 {
-                break;
-            }
+        for word in words {
+            let weight = word
+                .chars()
+                .fold(0, |acc, ch| acc + Self::weight(&weights, ch))
+                % 26;
 
-            count -= 1;
-            sum -= num;
-            let average = sum / count;
-            if num > average {
-                result += 1;
-            }
+            let ch = Self::mapped(weight);
+            mapped_list.push(ch);
         }
 
-        result
+        mapped_list.into_iter().collect()
+    }
+
+    fn weight(weights: &[i32], ch: char) -> i32 {
+        let idx = ((ch as u8) - b'a') as usize;
+        weights[idx]
+    }
+
+    fn mapped(weight: i32) -> char {
+        let idx = (25 - weight) as u8;
+        (b'a' + idx) as char
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    words: Vec<String>,
+    weights: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [5, 4, 3].to_vec(),
+            words: ["abcd", "def", "xyz"].map(|s| s.to_string()).to_vec(),
+            weights: [
+                5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2,
+            ]
+            .to_vec(),
         },
         Input {
-            nums: [4, 1, 2].to_vec(),
+            words: ["abcd", "def", "xyz"].map(|s| s.to_string()).to_vec(),
+            weights: [
+                5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2,
+            ]
+            .to_vec(),
         },
         Input {
-            nums: [37, 96, 96].to_vec(),
+            words: ["abcd", "def", "xyz"].map(|s| s.to_string()).to_vec(),
+            weights: [
+                5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2,
+            ]
+            .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::dominant_indices(input.nums);
+        let result = Solution::map_word_weights(input.words, input.weights);
         println!("{:?}", result);
     }
 }
