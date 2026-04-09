07bcae0de5293b0d8e07268aa5dbabc73679d738
mspark777 <topangel777@gmail.com>
2026-04-09 11:14:16 +0900

    3852. Smallest Pair With Different Frequencies

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,63 @@
 struct Solution;
 
 impl Solution {
-    pub fn toggle_light_bulbs(bulbs: Vec<i32>) -> Vec<i32> {
-        let mut lights = vec![false; 100];
-
-        for bulb in bulbs {
-            let idx = (bulb - 1) as usize;
-            lights[idx] = !lights[idx];
+    pub fn min_distinct_freq_pair(nums: Vec<i32>) -> Vec<i32> {
+        if nums.len() < 3 {
+            return vec![-1, -1];
         }
 
-        lights
-            .into_iter()
-            .enumerate()
-            .filter(|&(_, l)| l)
-            .map(|(i, _)| (i + 1) as i32)
-            .collect()
+        let mut frequencies = vec![0; 100];
+        for num in nums {
+            let idx = (num - 1) as usize;
+            frequencies[idx] += 1;
+        }
+
+        let mut x = -1;
+        let mut x_frequency = -1;
+        let mut y = -1;
+
+        for (i, frequency) in frequencies.into_iter().enumerate() {
+            if frequency == 0 {
+                continue;
+            }
+
+            let n = (i + 1) as i32;
+            if x_frequency < 0 {
+                x_frequency = frequency;
+                x = n;
+                continue;
+            }
+
+            if frequency != x_frequency {
+                y = n;
+                break;
+            }
+        }
+
+        match (x, y) {
+            (-1, _) | (_, -1) => vec![-1, -1],
+            _ => vec![x, y],
+        }
     }
 }
 
 struct Input {
-    bulbs: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            bulbs: [10, 30, 20, 10].to_vec(),
+            nums: [1, 1, 2, 2, 3, 4].to_vec(),
         },
         Input {
-            bulbs: [100, 100].to_vec(),
+            nums: [1, 5].to_vec(),
         },
+        Input { nums: [7].to_vec() },
     ];
 
     for input in inputs {
-        let result = Solution::toggle_light_bulbs(input.bulbs);
+        let result = Solution::min_distinct_freq_pair(input.nums);
         println!("{:?}", result);
     }
 }
