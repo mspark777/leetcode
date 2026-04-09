d49cf1f7de3b3a6c4065c73ddd47afd953b49c6d
mspark777 <topangel777@gmail.com>
2026-04-09 15:36:52 +0900

    3856. Trim Trailing Vowels

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,43 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_distinct_freq_pair(nums: Vec<i32>) -> Vec<i32> {
-        if nums.len() < 3 {
-            return vec![-1, -1];
-        }
-
-        let mut frequencies = vec![0; 100];
-        for num in nums {
-            let idx = (num - 1) as usize;
-            frequencies[idx] += 1;
-        }
-
-        let mut x = -1;
-        let mut x_frequency = -1;
-        let mut y = -1;
-
-        for (i, frequency) in frequencies.into_iter().enumerate() {
-            if frequency == 0 {
-                continue;
-            }
-
-            let n = (i + 1) as i32;
-            if x_frequency < 0 {
-                x_frequency = frequency;
-                x = n;
-                continue;
-            }
-
-            if frequency != x_frequency {
-                y = n;
+    pub fn trim_trailing_vowels(s: String) -> String {
+        let chars = s.chars().collect::<Vec<char>>();
+        let mut right = -1;
+        for (i, ch) in chars.into_iter().enumerate().rev() {
+            if !Self::is_vowel(ch) {
+                right = i as i32;
                 break;
             }
         }
 
-        match (x, y) {
-            (-1, _) | (_, -1) => vec![-1, -1],
-            _ => vec![x, y],
+        match right {
+            -1 => String::new(),
+            _ => s.chars().take((right + 1) as usize).collect(),
         }
+    }
+
+    fn is_vowel(ch: char) -> bool {
+        matches!(ch, 'a' | 'e' | 'i' | 'o' | 'u')
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 1, 2, 2, 3, 4].to_vec(),
+            s: "idea".to_string(),
         },
         Input {
-            nums: [1, 5].to_vec(),
+            s: "day".to_string(),
         },
-        Input { nums: [7].to_vec() },
     ];
 
     for input in inputs {
-        let result = Solution::min_distinct_freq_pair(input.nums);
+        let result = Solution::trim_trailing_vowels(input.s);
         println!("{:?}", result);
     }
 }
