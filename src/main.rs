7941ef54d8463fb949372d96a44bcf4237ef127c
mspark777 <topangel777@gmail.com>
2026-04-11 18:21:55 +0900

    3880

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
 struct Solution;
 
 impl Solution {
-    pub fn count_commas(n: i32) -> i32 {
-        0.max(n - 999)
+    pub fn min_absolute_difference(nums: Vec<i32>) -> i32 {
+        const INVALID: i32 = 101;
+        let mut left = INVALID;
+        let mut right = INVALID;
+        let mut difference = INVALID;
+
+        for (i, num) in nums.into_iter().enumerate() {
+            if num == 1 {
+                left = i as i32;
+            } else if num == 2 {
+                right = i as i32;
+            }
+
+            if (left != INVALID) && (right != INVALID) {
+                difference = (left - right).abs().min(difference);
+            }
+        }
+
+        match difference {
+            INVALID => -1,
+            _ => difference,
+        }
     }
 }
 
 struct Input {
-    n: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { n: 1002 }, Input { n: 998 }];
+    let inputs = [
+        Input {
+            nums: [1, 0, 0, 2, 0, 1].to_vec(),
+        },
+        Input {
+            nums: [1, 0, 1, 0].to_vec(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::count_commas(input.n);
+        let result = Solution::min_absolute_difference(input.nums);
         println!("{:?}", result);
     }
 }
