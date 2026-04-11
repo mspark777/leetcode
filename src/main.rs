d7b28f0c976b000ee006302a17faeaf90ba9a080
mspark777 <topangel777@gmail.com>
2026-04-11 16:55:32 +0900

    3870

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,20 @@
 struct Solution;
 
 impl Solution {
-    pub fn first_unique_even(nums: Vec<i32>) -> i32 {
-        let mut counts = [0; 100];
-
-        for num in nums.iter().copied() {
-            if (num & 1) == 1 {
-                continue;
-            }
-
-            let idx = (num as usize) - 1;
-            counts[idx] += 1;
-        }
-
-        for num in nums {
-            if (num & 1) == 1 {
-                continue;
-            }
-
-            let idx = (num as usize) - 1;
-            if counts[idx] == 1 {
-                return num;
-            }
-        }
-
-        -1
+    pub fn count_commas(n: i32) -> i32 {
+        0.max(n - 999)
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [Input {
-        nums: [3, 4, 2, 5, 4, 6].to_vec(),
-    }];
+    let inputs = [Input { n: 1002 }, Input { n: 998 }];
 
     for input in inputs {
-        let result = Solution::first_unique_even(input.nums);
+        let result = Solution::count_commas(input.n);
         println!("{:?}", result);
     }
 }
