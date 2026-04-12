c68afaffcf0fed3d724000eb909faf701aa963f8
mspark777 <topangel777@gmail.com>
2026-04-12 11:29:16 +0900

    3894. Traffic Signal Color

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
 struct Solution;
 
 impl Solution {
-    pub fn first_matching_index(s: String) -> i32 {
-        let s = s.chars().collect::<Vec<char>>();
-        let mut left = 0;
-        let mut right = s.len() - 1;
-
-        while left <= right {
-            if s[left] == s[right] {
-                return left as i32;
-            }
-
-            left += 1;
-            right -= 1;
+    pub fn traffic_signal(timer: i32) -> String {
+        match timer {
+            0 => "Green".to_string(),
+            30 => "Orange".to_string(),
+            (31..91) => "Red".to_string(),
+            _ => "Invalid".to_string(),
         }
-
-        -1
     }
 }
 
 struct Input {
-    s: String,
+    timer: i32,
 }
 
 fn main() {
-    let inputs = [
-        Input {
-            s: "abcacbd".to_string(),
-        },
-        Input {
-            s: "abc".to_string(),
-        },
-    ];
+    let inputs = [Input { timer: 60 }, Input { timer: 5 }];
 
     for input in inputs {
-        let result = Solution::first_matching_index(input.s);
+        let result = Solution::traffic_signal(input.timer);
         println!("{:?}", result);
     }
 }
