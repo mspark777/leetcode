b1735aadd5ff8f7ead9958f9ae7940e4f697e86e
mspark777 <topangel777@gmail.com>
2026-04-12 11:25:20 +0900

    3884

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,40 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_absolute_difference(nums: Vec<i32>) -> i32 {
-        const INVALID: i32 = 101;
-        let mut left = INVALID;
-        let mut right = INVALID;
-        let mut difference = INVALID;
+    pub fn first_matching_index(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        let mut left = 0;
+        let mut right = s.len() - 1;
 
-        for (i, num) in nums.into_iter().enumerate() {
-            if num == 1 {
-                left = i as i32;
-            } else if num == 2 {
-                right = i as i32;
+        while left <= right {
+            if s[left] == s[right] {
+                return left as i32;
             }
 
-            if (left != INVALID) && (right != INVALID) {
-                difference = (left - right).abs().min(difference);
-            }
+            left += 1;
+            right -= 1;
         }
 
-        match difference {
-            INVALID => -1,
-            _ => difference,
-        }
+        -1
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
+    s: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums: [1, 0, 0, 2, 0, 1].to_vec(),
+            s: "abcacbd".to_string(),
         },
         Input {
-            nums: [1, 0, 1, 0].to_vec(),
+            s: "abc".to_string(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::min_absolute_difference(input.nums);
+        let result = Solution::first_matching_index(input.s);
         println!("{:?}", result);
     }
 }
