37dc6a84ad7810260956c7f39bb29cbb09e91f53
mspark777 <topangel777@gmail.com>
2026-04-14 09:56:38 +0900

    187. Repeated DNA Sequences

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,47 @@
 struct Solution;
 
 impl Solution {
-    pub fn traffic_signal(timer: i32) -> String {
-        match timer {
-            0 => "Green".to_string(),
-            30 => "Orange".to_string(),
-            (31..91) => "Red".to_string(),
-            _ => "Invalid".to_string(),
+    pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+        use std::collections::HashSet;
+
+        let s = s.chars().collect::<Vec<char>>();
+        if s.len() < 10 {
+            return Vec::new();
         }
+
+        let mut seen = HashSet::<String>::new();
+        let mut repeated = HashSet::<String>::new();
+        let end = s.len() - 9;
+        for i in 0..end {
+            let e = i + 10;
+            let sub = s[i..e].iter().collect::<String>();
+            if seen.contains(&sub) {
+                repeated.insert(sub);
+            } else {
+                seen.insert(sub);
+            }
+        }
+
+        repeated.into_iter().collect()
     }
 }
 
 struct Input {
-    timer: i32,
+    s: String,
 }
 
 fn main() {
-    let inputs = [Input { timer: 60 }, Input { timer: 5 }];
+    let inputs = [
+        Input {
+            s: "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT".to_string(),
+        },
+        Input {
+            s: "AAAAAAAAAAAAA".to_string(),
+        },
+    ];
 
     for input in inputs {
-        let result = Solution::traffic_signal(input.timer);
+        let result = Solution::find_repeated_dna_sequences(input.s);
         println!("{:?}", result);
     }
 }
