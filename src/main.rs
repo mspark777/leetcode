605a1e0a204786537433e7a1c9e3cc78f8876f27
mspark777 <topangel777@gmail.com>
2026-04-14 10:14:06 +0900

    207

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,59 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
-        use std::collections::HashSet;
+    pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+        use std::collections::VecDeque;
 
-        let s = s.chars().collect::<Vec<char>>();
-        if s.len() < 10 {
-            return Vec::new();
+        let num_courses = num_courses as usize;
+        let mut in_degree = vec![0; num_courses];
+        let mut adjacents = vec![Vec::<usize>::new(); num_courses];
+
+        for prereq in prerequisites {
+            adjacents[prereq[1] as usize].push(prereq[0] as usize);
+            in_degree[prereq[0] as usize] += 1;
         }
 
-        let mut seen = HashSet::<String>::new();
-        let mut repeated = HashSet::<String>::new();
-        let end = s.len() - 9;
-        for i in 0..end {
-            let e = i + 10;
-            let sub = s[i..e].iter().collect::<String>();
-            if seen.contains(&sub) {
-                repeated.insert(sub);
-            } else {
-                seen.insert(sub);
+        let mut queue: VecDeque<usize> = VecDeque::new();
+        for (i, degree) in in_degree.iter().copied().enumerate() {
+            if degree == 0 {
+                queue.push_back(i);
             }
         }
 
-        repeated.into_iter().collect()
+        let mut processed_courses = 0;
+        while let Some(course) = queue.pop_front() {
+            processed_courses += 1;
+            for next_course in adjacents[course].iter().copied() {
+                in_degree[next_course] -= 1;
+                if in_degree[next_course] == 0 {
+                    queue.push_back(next_course);
+                }
+            }
+        }
+
+        processed_courses == num_courses
     }
 }
 
 struct Input {
-    s: String,
+    num_courses: i32,
+    prerequisites: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT".to_string(),
+            num_courses: 2,
+            prerequisites: [[1, 0]].map(|v| v.to_vec()).to_vec(),
         },
         Input {
-            s: "AAAAAAAAAAAAA".to_string(),
+            num_courses: 2,
+            prerequisites: [[1, 0], [0, 1]].map(|v| v.to_vec()).to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_repeated_dna_sequences(input.s);
+        let result = Solution::can_finish(input.num_courses, input.prerequisites);
         println!("{:?}", result);
     }
 }
