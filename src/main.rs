5f13fbf6a2415b14764a12211e0c7bbb046cbeee
mspark777 <topangel777@gmail.com>
2026-04-16 08:27:31 +0900

    213

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,51 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
-        use std::collections::VecDeque;
-
-        let num_courses = num_courses as usize;
-        let mut adjacency_nodes = vec![Vec::<usize>::new(); num_courses];
-        let mut in_degree = vec![0; num_courses];
-
-        for require in prerequisites.iter() {
-            let target = require[0] as usize;
-            let course = require[1] as usize;
-
-            adjacency_nodes[course].push(target);
-            in_degree[target] += 1;
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        if n == 1 {
+            return nums[0];
         }
 
-        let mut queue = VecDeque::<usize>::new();
-        for (i, degree) in in_degree.iter().copied().enumerate() {
-            if degree == 0 {
-                queue.push_back(i);
-            }
+        let exclude_first = Self::start(&nums, 1, n - 1);
+        let exclude_last = Self::start(&nums, 0, n - 2);
+
+        exclude_first.max(exclude_last)
+    }
+
+    fn start(nums: &[i32], left: usize, right: usize) -> i32 {
+        let mut prev1 = 0;
+        let mut prev2 = 0;
+
+        for num in nums.iter().skip(left).take(right + 1).copied() {
+            let curr = prev1.max(prev2 + num);
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        let mut courses = Vec::<i32>::new();
-        while let Some(node) = queue.pop_back() {
-            courses.push(node as i32);
-            for target in adjacency_nodes[node].iter().copied() {
-                in_degree[target] -= 1;
-                if in_degree[target] == 0 {
-                    queue.push_back(target);
-                }
-            }
-        }
-
-        match courses.len() == num_courses {
-            true => courses,
-            _ => Vec::new(),
-        }
+        prev1
     }
 }
 
 struct Input {
-    num_courses: i32,
-    prerequisites: Vec<Vec<i32>>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            num_courses: 2,
-            prerequisites: [[1, 0]].map(|v| v.to_vec()).to_vec(),
+            nums: [2, 3, 2].to_vec(),
         },
         Input {
-            num_courses: 4,
-            prerequisites: [[1, 0], [2, 0], [3, 1], [3, 2]]
-                .map(|v| v.to_vec())
-                .to_vec(),
+            nums: [1, 2, 3, 1].to_vec(),
+        },
+        Input {
+            nums: [1, 2, 3].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::find_order(input.num_courses, input.prerequisites);
+        let result = Solution::rob(input.nums);
         println!("{:?}", result);
     }
 }
