c444e52c876b4c143d648a07a6fe73a633b1015f
mspark777 <topangel777@gmail.com>
2026-04-15 09:32:33 +0900

    210

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,43 @@
 struct Solution;
 
 impl Solution {
-    pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+    pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
         use std::collections::VecDeque;
 
         let num_courses = num_courses as usize;
+        let mut adjacency_nodes = vec![Vec::<usize>::new(); num_courses];
         let mut in_degree = vec![0; num_courses];
-        let mut adjacents = vec![Vec::<usize>::new(); num_courses];
 
-        for prereq in prerequisites {
-            adjacents[prereq[1] as usize].push(prereq[0] as usize);
-            in_degree[prereq[0] as usize] += 1;
+        for require in prerequisites.iter() {
+            let target = require[0] as usize;
+            let course = require[1] as usize;
+
+            adjacency_nodes[course].push(target);
+            in_degree[target] += 1;
         }
 
-        let mut queue: VecDeque<usize> = VecDeque::new();
+        let mut queue = VecDeque::<usize>::new();
         for (i, degree) in in_degree.iter().copied().enumerate() {
             if degree == 0 {
                 queue.push_back(i);
             }
         }
 
-        let mut processed_courses = 0;
-        while let Some(course) = queue.pop_front() {
-            processed_courses += 1;
-            for next_course in adjacents[course].iter().copied() {
-                in_degree[next_course] -= 1;
-                if in_degree[next_course] == 0 {
-                    queue.push_back(next_course);
+        let mut courses = Vec::<i32>::new();
+        while let Some(node) = queue.pop_back() {
+            courses.push(node as i32);
+            for target in adjacency_nodes[node].iter().copied() {
+                in_degree[target] -= 1;
+                if in_degree[target] == 0 {
+                    queue.push_back(target);
                 }
             }
         }
 
-        processed_courses == num_courses
+        match courses.len() == num_courses {
+            true => courses,
+            _ => Vec::new(),
+        }
     }
 }
 
@@ -47,13 +53,15 @@ fn main() {
             prerequisites: [[1, 0]].map(|v| v.to_vec()).to_vec(),
         },
         Input {
-            num_courses: 2,
-            prerequisites: [[1, 0], [0, 1]].map(|v| v.to_vec()).to_vec(),
+            num_courses: 4,
+            prerequisites: [[1, 0], [2, 0], [3, 1], [3, 2]]
+                .map(|v| v.to_vec())
+                .to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::can_finish(input.num_courses, input.prerequisites);
+        let result = Solution::find_order(input.num_courses, input.prerequisites);
         println!("{:?}", result);
     }
 }
