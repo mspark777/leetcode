3318466e9b908a3cba6c9d96869127a4dff70dbb
mspark777 <topangel777@gmail.com>
2026-04-18 13:47:02 +0900

    289

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,63 @@
 struct Solution;
 
 impl Solution {
-    pub fn h_index(citations: Vec<i32>) -> i32 {
-        let n = citations.len() as i32;
-        let mut left = 0;
-        let mut right = n - 1;
-        let mut result = 0;
+    pub fn game_of_life(board: &mut [Vec<i32>]) {
+        let m = board.len() as i32;
+        let n = board[0].len() as i32;
 
-        while left <= right {
-            let mid = (left + right) / 2;
-            if (n - mid) <= citations[mid as usize] {
-                result = n - mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        for i in 0..m {
+            for j in 0..n {
+                let lives = Self::live_neighbors(board, i, j, m, n);
+                let i = i as usize;
+                let j = j as usize;
+                if (board[i][j] == 1) && (2..=3).contains(&lives) {
+                    board[i][j] = 3;
+                }
+
+                if (board[i][j] == 0) && (lives == 3) {
+                    board[i][j] = 2;
+                }
             }
         }
 
-        result
+        for i in 0..m {
+            for j in 0..n {
+                board[i as usize][j as usize] >>= 1;
+            }
+        }
+    }
+
+    fn live_neighbors(board: &[Vec<i32>], i: i32, j: i32, m: i32, n: i32) -> i32 {
+        let mut lives = 0;
+
+        for x in 0.max(i - 1)..=(i + 1).min(m - 1) {
+            for y in 0.max(j - 1)..=(j + 1).min(n - 1) {
+                lives += board[x as usize][y as usize] & 1;
+            }
+        }
+        lives -= board[i as usize][j as usize] & 1;
+        lives
     }
 }
 
 struct Input {
-    citations: Vec<i32>,
+    board: Vec<Vec<i32>>,
 }
 
 fn main() {
-    let inputs = [
+    let mut inputs = [
         Input {
-            citations: [0, 1, 3, 5, 6].to_vec(),
+            board: [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+                .map(|v| v.to_vec())
+                .to_vec(),
         },
         Input {
-            citations: [1, 2, 100].to_vec(),
-        },
-        Input {
-            citations: [1].to_vec(),
+            board: [[1, 1], [1, 0]].map(|v| v.to_vec()).to_vec(),
         },
     ];
 
-    for input in inputs {
-        let result = Solution::h_index(input.citations);
-        println!("{:?}", result);
+    for input in inputs.iter_mut() {
+        Solution::game_of_life(&mut input.board);
+        println!("{:?}", input.board);
     }
 }
