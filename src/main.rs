a372cb6d745a817757917291588882d91ff0bcfb
mspark777 <topangel777@gmail.com>
2026-04-18 11:15:16 +0900

    274

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,39 @@
 struct Solution;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
-
-use std::cell::RefCell;
-use std::rc::Rc;
 impl Solution {
-    pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
-        let mut count = k;
-        let mut result = 0;
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let n = citations.len();
+        let mut citations = citations;
+        citations.sort();
 
-        Self::inorder(root.as_ref(), &mut count, &mut result);
-        result
-    }
-
-    fn inorder(node: Option<&Rc<RefCell<TreeNode>>>, count: &mut i32, result: &mut i32) {
-        let node = match node {
-            Some(n) => n,
-            _ => return,
-        };
-
-        if *count == 0 {
-            return;
-        }
-
-        Self::inorder(node.borrow().left.as_ref(), count, result);
-        *count -= 1;
-        if *count == 0 {
-            *result = node.borrow().val;
-        }
-
-        Self::inorder(node.borrow().right.as_ref(), count, result);
+        citations
+            .into_iter()
+            .enumerate()
+            .find(|&(i, c)| {
+                let count = (n - i) as i32;
+                c >= count
+            })
+            .map(|v| (n - v.0) as i32)
+            .unwrap_or_default()
     }
 }
 
 struct Input {
-    s: String,
+    citations: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "3+2*2".to_string(),
+            citations: [3, 0, 6, 1, 5].to_vec(),
         },
         Input {
-            s: " 3+5 / 2 ".to_string(),
+            citations: [1, 3, 1].to_vec(),
         },
     ];
 
     for input in inputs {
-        let result = Solution::calculate(input.s);
+        let result = Solution::h_index(input.citations);
         println!("{:?}", result);
     }
 }
