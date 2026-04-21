5a3cf0dfa74d509313a3629250b2e5e9c87f2514
mspark777 <topangel777@gmail.com>
2026-04-21 09:49:16 +0900

    313

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,55 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_min_height_trees(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
-        use std::collections::VecDeque;
-
+    pub fn nth_super_ugly_number(n: i32, primes: Vec<i32>) -> i32 {
         if n == 1 {
-            return vec![0];
+            return 1;
         }
 
         let n = n as usize;
-        let mut adjacent_nodes = vec![Vec::<i32>::new(); n];
-        let mut degree = vec![0; n];
+        let prime_count = primes.len();
+        let mut prime_indices = vec![0usize; prime_count];
+        let mut uglys = vec![0i64; n];
+        uglys[0] = 1;
 
-        for edge in edges.iter() {
-            let a = edge[0];
-            let b = edge[1];
+        for i in 1..n {
+            let mut min_val = i64::MAX;
 
-            adjacent_nodes[a as usize].push(b);
-            adjacent_nodes[b as usize].push(a);
-
-            degree[a as usize] += 1;
-            degree[b as usize] += 1;
-        }
-
-        let mut queue = VecDeque::<i32>::new();
-        for (i, d) in degree.iter().copied().enumerate() {
-            if d == 1 {
-                queue.push_back(i as i32);
+            for j in 0..prime_count {
+                let prime = primes[j] as i64;
+                let ugly = uglys[prime_indices[j]];
+                min_val = min_val.min(prime * ugly);
             }
-        }
 
-        let mut nodes = n;
-        while nodes > 2 {
-            let size = queue.len();
-            nodes -= size;
+            uglys[i] = min_val;
 
-            for _ in 0..size {
-                let node = queue.pop_front().unwrap();
-                for adjacent in adjacent_nodes[node as usize].iter().copied() {
-                    degree[adjacent as usize] -= 1;
-
-                    if degree[adjacent as usize] == 1 {
-                        queue.push_back(adjacent);
-                    }
+            for j in 0..prime_count {
+                let prime = primes[j] as i64;
+                let ugly = uglys[prime_indices[j]];
+                let val = prime * ugly;
+                if min_val == val {
+                    prime_indices[j] += 1;
                 }
             }
         }
 
-        queue.into_iter().collect()
+        uglys[n - 1] as i32
     }
 }
 
 struct Input {
     n: i32,
-    edges: Vec<Vec<i32>>,
+    primes: Vec<i32>,
 }
 
 fn main() {
     let inputs = [Input {
-        n: 4,
-        edges: [[1, 0], [1, 2], [1, 3]].map(|v| v.to_vec()).to_vec(),
+        n: 12,
+        primes: [2, 7, 13, 19].to_vec(),
     }];
 
     for input in inputs {
-        let result = Solution::find_min_height_trees(input.n, input.edges);
+        let result = Solution::nth_super_ugly_number(input.n, input.primes);
         println!("{:?}", result);
     }
 }
