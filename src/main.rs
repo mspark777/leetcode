6d4d18eb40d8179e2e24db3c3ee7b3341c78c938
mspark777 <topangel777@gmail.com>
2026-04-21 09:58:55 +0900

    322

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,38 @@
 struct Solution;
 
 impl Solution {
-    pub fn nth_super_ugly_number(n: i32, primes: Vec<i32>) -> i32 {
-        if n == 1 {
-            return 1;
-        }
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let mut dp = vec![amount + 1; (amount + 1) as usize];
+        dp[0] = 0;
 
-        let n = n as usize;
-        let prime_count = primes.len();
-        let mut prime_indices = vec![0usize; prime_count];
-        let mut uglys = vec![0i64; n];
-        uglys[0] = 1;
-
-        for i in 1..n {
-            let mut min_val = i64::MAX;
-
-            for j in 0..prime_count {
-                let prime = primes[j] as i64;
-                let ugly = uglys[prime_indices[j]];
-                min_val = min_val.min(prime * ugly);
-            }
-
-            uglys[i] = min_val;
-
-            for j in 0..prime_count {
-                let prime = primes[j] as i64;
-                let ugly = uglys[prime_indices[j]];
-                let val = prime * ugly;
-                if min_val == val {
-                    prime_indices[j] += 1;
+        for i in 1..=amount {
+            for coin in coins.iter().copied() {
+                if (i - coin) >= 0 {
+                    dp[i as usize] = dp[i as usize].min(1 + dp[(i - coin) as usize]);
                 }
             }
         }
 
-        uglys[n - 1] as i32
+        match dp[amount as usize] > amount {
+            true => -1,
+            _ => dp[amount as usize],
+        }
     }
 }
 
 struct Input {
-    n: i32,
-    primes: Vec<i32>,
+    coins: Vec<i32>,
+    amount: i32,
 }
 
 fn main() {
     let inputs = [Input {
-        n: 12,
-        primes: [2, 7, 13, 19].to_vec(),
+        coins: [1, 2, 5].to_vec(),
+        amount: 11,
     }];
 
     for input in inputs {
-        let result = Solution::nth_super_ugly_number(input.n, input.primes);
+        let result = Solution::coin_change(input.coins, input.amount);
         println!("{:?}", result);
     }
 }
