dd77f7a871d15ea649203adea8c12547d94c2f95
mspark777 <topangel777@gmail.com>
2026-04-28 22:01:32 +0900

    337. House Robber III

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
 struct Solution;
 
+use std::cell::RefCell;
+use std::rc::Rc;
 impl Solution {
-    pub fn is_valid_serialization(preorder: String) -> bool {
-        let mut nodes = 1;
-        for node in preorder.split(',').map(|s| s.as_bytes()) {
-            if node[0] == b',' {
-                continue;
-            }
+    pub fn rob(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let amount = Self::travel(root);
+        amount.0.max(amount.1)
+    }
 
-            nodes -= 1;
-
-            if nodes < 0 {
-                return false;
-            }
-
-            if node[0] != b'#' {
-                nodes += 2
-            }
+    fn travel(node: Option<Rc<RefCell<TreeNode>>>) -> (i32, i32) {
+        match node {
+            Some(n) => Self::travel1(n.clone()),
+            _ => (0, 0),
         }
+    }
 
-        nodes == 0
+    fn travel1(node: Rc<RefCell<TreeNode>>) -> (i32, i32) {
+        let left = Self::travel(node.borrow().left.clone());
+        let right = Self::travel(node.borrow().right.clone());
+
+        (
+            left.0.max(left.1) + right.0.max(right.1),
+            node.borrow().val + left.0 + right.0,
+        )
     }
 }
 
