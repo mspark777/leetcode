8d36b5b6fcab072f5264573f6d41009752cc7e35
mspark777 <topangel777@gmail.com>
2026-05-01 21:59:03 +0900

    365

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
 struct Solution;
 
 impl Solution {
-    pub fn count_numbers_with_unique_digits(n: i32) -> i32 {
-        if n == 0 {
-            return 1;
+    pub fn can_measure_water(x: i32, y: i32, target: i32) -> bool {
+        if (x + y) < target {
+            return false;
         }
 
-        let mut result = 10;
-        let mut unique_digits = 9;
-        let mut available_number = 9;
-
-        for _ in 1..n {
-            if available_number <= 0 {
-                break;
-            }
-            unique_digits *= available_number;
-            result += unique_digits;
-            available_number -= 1;
+        if (x == target) || (y == target) || ((x + y) == target) {
+            return true;
         }
 
-        result
+        (target % Self::gcd(x, y)) == 0
+    }
+
+    fn gcd(mut x: i32, mut y: i32) -> i32 {
+        while y != 0 {
+            let temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        x
     }
 }
 
 struct Input {
-    n: i32,
+    x: i32,
+    y: i32,
+    target: i32,
 }
 
 fn main() {
-    let inputs = [Input { n: 2 }, Input { n: 0 }];
+    let inputs = [
+        Input {
+            x: 3,
+            y: 5,
+            target: 4,
+        },
+        Input {
+            x: 2,
+            y: 6,
+            target: 5,
+        },
+    ];
 
     for input in inputs.into_iter() {
-        let result = Solution::count_numbers_with_unique_digits(input.n);
+        let result = Solution::can_measure_water(input.x, input.y, input.target);
         println!("{:?}", result);
     }
 }
