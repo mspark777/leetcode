f40126c39651db7555e9a50c14773643732088e7
mspark777 <topangel777@gmail.com>
2026-05-05 22:03:08 +0900

    375

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 struct Solution;
 
 impl Solution {
-    pub fn get_sum(mut a: i32, mut b: i32) -> i32 {
-        while b != 0 {
-            let temp = a & b;
-            a ^= b;
-            b = temp << 1;
+    pub fn get_money_amount(n: i32) -> i32 {
+        let n = n as usize;
+        let mut dp = vec![vec![0i32; n + 1]; n + 1];
+
+        for len in 2..=n {
+            for start in 1..=(n + 1 - len) {
+                let mut minres = i32::MAX;
+                for piv in (start + (len - 1) / 2)..(start + len - 1) {
+                    let res = (piv as i32) + dp[start][piv - 1].max(dp[piv + 1][start + len - 1]);
+                    minres = minres.min(res as i32);
+                }
+
+                dp[start][start + len - 1] = minres;
+            }
         }
 
-        a
+        dp[1][n]
     }
 }
 
 struct Input {
-    a: i32,
-    b: i32,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [Input { a: 1, b: 2 }, Input { a: 2, b: 3 }];
+    let inputs = [Input { n: 10 }, Input { n: 1 }, Input { n: 2 }];
 
     for input in inputs.into_iter() {
-        let result = Solution::get_sum(input.a, input.b);
+        let result = Solution::get_money_amount(input.n);
         println!("{:?}", result);
     }
 }
