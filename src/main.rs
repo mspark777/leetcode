c96adba99316fae17ca2ca9c7a75b47746df96e0
mspark777 <topangel777@gmail.com>
2026-05-08 09:44:05 +0900

    388

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,51 @@
 struct Solution;
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum NestedInteger {
-    Int(i32),
-    List(Vec<NestedInteger>),
-}
-
 impl Solution {
-    pub fn deserialize(s: String) -> NestedInteger {
-        if s.starts_with('[') {
-            let mut parts = Vec::<String>::new();
-            let mut acc = String::new();
-            let mut count = 0;
-            let inner = s
-                .strip_prefix('[')
-                .unwrap()
-                .strip_suffix(']')
-                .unwrap()
-                .chars();
-            for ch in inner {
-                if count == 0 && ch == ',' {
-                    parts.push(acc);
-                    acc = String::new();
-                    continue;
-                }
+    pub fn length_longest_path(input: String) -> i32 {
+        use std::collections::HashMap;
 
-                if ch == '[' {
-                    count += 1;
-                } else if ch == ']' {
-                    count -= 1;
-                }
+        let mut depth_map = HashMap::<usize, usize>::new();
+        let mut max_len = 0;
 
-                acc.push(ch);
+        for line in input.split('\n') {
+            let level = line
+                .as_bytes()
+                .iter()
+                .enumerate()
+                .rev()
+                .find(|b| (*b.1) == b'\t')
+                .map(|b| b.0 + 1)
+                .unwrap_or_default();
+            let name = &line[level..];
+            let level_len = depth_map.get(&level).copied().unwrap_or_default();
+
+            if name.chars().find(|&c| c == '.').is_some() {
+                max_len = max_len.max(level_len + name.len());
+            } else {
+                depth_map.insert(level + 1, level_len + name.len() + 1);
             }
-            parts.push(acc);
-
-            NestedInteger::List(
-                parts
-                    .iter()
-                    .filter(|s| !s.is_empty())
-                    .map(|s| Self::deserialize(s.to_string()))
-                    .collect(),
-            )
-        } else {
-            NestedInteger::Int(s.parse().unwrap())
         }
+
+        max_len as i32
     }
 }
 
 struct Input {
-    s: String,
+    input: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "324".to_string(),
+            input: "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext".to_string()
         },
         Input {
-            s: "[123,[456,[789]]]".to_string(),
+            input:"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext".to_string()
         },
     ];
 
     for input in inputs.into_iter() {
-        let result = Solution::deserialize(input.s);
+        let result = Solution::length_longest_path(input.input);
         println!("{:?}", result);
     }
 }
