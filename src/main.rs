3b5901ae6a610a87380e8a1659936441f4dc8b88
mspark777 <topangel777@gmail.com>
2026-05-09 11:25:30 +0900

    395

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,90 @@
 struct Solution;
 
-#[derive(Eq, PartialEq)]
-enum OpType {
-    Left,
-    Right,
-}
-
 impl Solution {
-    pub fn last_remaining(n: i32) -> i32 {
-        let mut n = n;
-        let mut diff = 1;
-        let mut result = 1;
-        let mut op_type = OpType::Right;
-        while n != 1 {
-            result = match op_type {
-                OpType::Right => result + diff,
-                OpType::Left if (n & 1) == 1 => result + diff,
-                _ => result,
-            };
+    pub fn longest_substring(s: String, k: i32) -> i32 {
+        let bytes = s.as_bytes();
+        let n = s.len();
+        let max_unique_count = Self::max_unique_letters(&s);
+        let mut result = 0i32;
 
-            n /= 2;
-            op_type = match op_type {
-                OpType::Left => OpType::Right,
-                OpType::Right => OpType::Left,
-            };
-            diff *= 2;
+        for curr_unique in 1..=max_unique_count {
+            let mut count_map = [0; 26];
+            let mut window_start = 0usize;
+            let mut window_end = 0usize;
+            let mut unique = 0i32;
+            let mut count_at_least_k = 0i32;
+
+            while window_end < n {
+                if unique <= curr_unique {
+                    let idx = (bytes[window_end] - b'a') as usize;
+                    if count_map[idx] == 0 {
+                        unique += 1;
+                    }
+
+                    count_map[idx] += 1;
+                    if count_map[idx] == k {
+                        count_at_least_k += 1;
+                    }
+
+                    window_end += 1;
+                } else {
+                    let idx = (bytes[window_start] - b'a') as usize;
+                    if count_map[idx] == k {
+                        count_at_least_k -= 1;
+                    }
+
+                    count_map[idx] -= 1;
+                    if count_map[idx] == 0 {
+                        unique -= 1;
+                    }
+
+                    window_start += 1;
+                }
+
+                if (unique == curr_unique) && (unique == count_at_least_k) {
+                    let curr_len = window_end - window_start;
+                    result = result.max(curr_len as i32);
+                }
+            }
         }
 
         result
     }
+
+    fn max_unique_letters(s: &str) -> i32 {
+        let mut flags = [false; 26];
+        let mut max_unique = 0;
+        for ch in s.chars() {
+            let flag_idx = (ch as usize) - ('a' as usize);
+            if !flags[flag_idx] {
+                max_unique += 1;
+                flags[flag_idx] = true;
+            }
+        }
+
+        max_unique
+    }
 }
 
 struct Input {
-    n: i32,
+    s: String,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [Input { n: 9 }, Input { n: 1 }];
+    let inputs = [
+        Input {
+            s: "aaabb".to_string(),
+            k: 3,
+        },
+        Input {
+            s: "ababbc".to_string(),
+            k: 2,
+        },
+    ];
 
     for input in inputs.into_iter() {
-        let result = Solution::last_remaining(input.n);
+        let result = Solution::longest_substring(input.s, input.k);
         println!("{:?}", result);
     }
 }
