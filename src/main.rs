997daf767e28ad851cbb6aedcc18d0f3b25b9fb1
mspark777 <topangel777@gmail.com>
2026-05-09 15:48:29 +0900

    396

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,41 @@
 struct Solution;
 
 impl Solution {
-    pub fn longest_substring(s: String, k: i32) -> i32 {
-        let bytes = s.as_bytes();
-        let n = s.len();
-        let max_unique_count = Self::max_unique_letters(&s);
-        let mut result = 0i32;
+    pub fn max_rotate_function(nums: Vec<i32>) -> i32 {
+        let n = nums.len() as i32;
+        let mut f = 0;
+        let mut num_sum = 0;
+        for (i, num) in nums.iter().copied().enumerate() {
+            num_sum += num;
+            f += (i as i32) * num;
+        }
 
-        for curr_unique in 1..=max_unique_count {
-            let mut count_map = [0; 26];
-            let mut window_start = 0usize;
-            let mut window_end = 0usize;
-            let mut unique = 0i32;
-            let mut count_at_least_k = 0i32;
-
-            while window_end < n {
-                if unique <= curr_unique {
-                    let idx = (bytes[window_end] - b'a') as usize;
-                    if count_map[idx] == 0 {
-                        unique += 1;
-                    }
-
-                    count_map[idx] += 1;
-                    if count_map[idx] == k {
-                        count_at_least_k += 1;
-                    }
-
-                    window_end += 1;
-                } else {
-                    let idx = (bytes[window_start] - b'a') as usize;
-                    if count_map[idx] == k {
-                        count_at_least_k -= 1;
-                    }
-
-                    count_map[idx] -= 1;
-                    if count_map[idx] == 0 {
-                        unique -= 1;
-                    }
-
-                    window_start += 1;
-                }
-
-                if (unique == curr_unique) && (unique == count_at_least_k) {
-                    let curr_len = window_end - window_start;
-                    result = result.max(curr_len as i32);
-                }
-            }
+        let mut result = f;
+        for num in nums.iter().copied().rev() {
+            f += num_sum - n * num;
+            result = result.max(f);
         }
 
         result
     }
-
-    fn max_unique_letters(s: &str) -> i32 {
-        let mut flags = [false; 26];
-        let mut max_unique = 0;
-        for ch in s.chars() {
-            let flag_idx = (ch as usize) - ('a' as usize);
-            if !flags[flag_idx] {
-                max_unique += 1;
-                flags[flag_idx] = true;
-            }
-        }
-
-        max_unique
-    }
 }
 
 struct Input {
-    s: String,
-    k: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "aaabb".to_string(),
-            k: 3,
+            nums: [4, 3, 2, 6].to_vec(),
         },
         Input {
-            s: "ababbc".to_string(),
-            k: 2,
+            nums: [100].to_vec(),
         },
     ];
 
     for input in inputs.into_iter() {
-        let result = Solution::longest_substring(input.s, input.k);
+        let result = Solution::max_rotate_function(input.nums);
         println!("{:?}", result);
     }
 }
