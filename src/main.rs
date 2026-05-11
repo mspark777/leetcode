e3c452f2d3cd2577fd476925c5f0282d81561237
mspark777 <topangel777@gmail.com>
2026-05-11 10:16:35 +0900

    399

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,113 @@
-struct Solution {
-    nums: Vec<i32>,
+struct Solution;
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+struct Edge {
+    node: String,
+    val: f64,
 }
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl Solution {
-    fn new(nums: Vec<i32>) -> Self {
-        Self { nums }
-    }
+    fn build_graph(data: Vec<Vec<String>>, vals: Vec<f64>) -> HashMap<String, Vec<Edge>> {
+        let mut g = HashMap::new();
+        for i in 0..data.len() {
+            g.entry(data[i][0].clone())
+                .or_insert(Vec::new())
+                .push(Edge {
+                    node: data[i][1].clone(),
+                    val: vals[i],
+                });
 
-    fn pick(&self, target: i32) -> i32 {
-        let mut idx = 0usize;
-        let mut count = 0;
-        for (i, num) in self.nums.iter().copied().enumerate() {
-            if num != target {
-                continue;
-            }
-
-            count += 1;
-
-            if (rand::random::<i32>() % count) == 0 {
-                idx = i;
-            }
+            g.entry(data[i][1].clone())
+                .or_insert(Vec::new())
+                .push(Edge {
+                    node: data[i][0].clone(),
+                    val: 1.0 / vals[i],
+                });
         }
 
-        idx as i32
+        g
+    }
+
+    fn search(v1: String, v2: String, g: &HashMap<String, Vec<Edge>>) -> f64 {
+        let mut frontier = vec![Edge {
+            node: v1.clone(),
+            val: 1.0,
+        }];
+        let mut seen = HashSet::new();
+        seen.insert(&v1);
+
+        while !frontier.is_empty() {
+            let mut new_frontier = Vec::new();
+            for v in frontier {
+                if !g.contains_key(&v.node) {
+                    continue;
+                }
+
+                for n in &g[&v.node] {
+                    if n.node == v2 {
+                        return v.val * n.val;
+                    }
+
+                    if !seen.contains(&n.node) {
+                        seen.insert(&n.node);
+                        new_frontier.push(Edge {
+                            node: n.node.clone(),
+                            val: v.val * n.val,
+                        });
+                    }
+                }
+            }
+
+            frontier = new_frontier;
+        }
+
+        -1.0
+    }
+
+    fn answer_query(q: Vec<String>, g: &HashMap<String, Vec<Edge>>) -> f64 {
+        match q[1] == q[0] {
+            true if g.contains_key(&q[1]) => 1.0,
+            true => -1.0,
+            _ => Self::search(q[0].clone(), q[1].clone(), g),
+        }
+    }
+
+    pub fn calc_equation(
+        equations: Vec<Vec<String>>,
+        values: Vec<f64>,
+        queries: Vec<Vec<String>>,
+    ) -> Vec<f64> {
+        let g = Self::build_graph(equations, values);
+        let mut res = Vec::with_capacity(queries.len());
+        for q in queries {
+            res.push(Self::answer_query(q, &g));
+        }
+
+        res
     }
 }
 
-/**
- * Your Solution object will be instantiated and called as such:
- * let obj = Solution::new(nums);
- * let ret_1: i32 = obj.pick(target);
- */
-
 struct Input {
-    n: i32,
+    equations: Vec<Vec<String>>,
+    values: Vec<f64>,
+    queries: Vec<Vec<String>>,
 }
 
 fn main() {
-    let inputs = [Input { n: 8 }, Input { n: 7 }];
+    let inputs = [Input {
+        equations: [["a", "b"], ["b", "c"]]
+            .map(|v| v.map(|v| v.to_string()).to_vec())
+            .to_vec(),
+        values: [2.0, 3.0].to_vec(),
+        queries: [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]
+            .map(|v| v.map(|v| v.to_string()).to_vec())
+            .to_vec(),
+    }];
 
     for input in inputs.into_iter() {
-        let result = Solution::integer_replacement(input.n);
+        let result = Solution::calc_equation(input.equations, input.values, input.queries);
         println!("{:?}", result);
     }
 }
