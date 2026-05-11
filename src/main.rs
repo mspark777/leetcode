50dbe7c8b0de557279be82087b84833d6c711904
mspark777 <topangel777@gmail.com>
2026-05-11 10:31:01 +0900

    400

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,37 @@
 struct Solution;
 
-use std::collections::HashMap;
-use std::collections::HashSet;
-
-struct Edge {
-    node: String,
-    val: f64,
-}
-
 impl Solution {
-    fn build_graph(data: Vec<Vec<String>>, vals: Vec<f64>) -> HashMap<String, Vec<Edge>> {
-        let mut g = HashMap::new();
-        for i in 0..data.len() {
-            g.entry(data[i][0].clone())
-                .or_insert(Vec::new())
-                .push(Edge {
-                    node: data[i][1].clone(),
-                    val: vals[i],
-                });
+    pub fn find_nth_digit(n: i32) -> i32 {
+        let mut n = n as i64;
+        let mut i = 1i64;
+        let mut count = 9i64;
+        let mut start = 1i64;
 
-            g.entry(data[i][1].clone())
-                .or_insert(Vec::new())
-                .push(Edge {
-                    node: data[i][0].clone(),
-                    val: 1.0 / vals[i],
-                });
+        while n > (i * count) {
+            n -= i * count;
+            i += 1;
+            count *= 10;
+            start *= 10;
         }
 
-        g
-    }
+        let nums = (start + ((n - 1) / i)).to_string();
+        let nums = nums.as_bytes();
+        let idx = ((n - 1) % i) as usize;
+        let num = nums[idx];
 
-    fn search(v1: String, v2: String, g: &HashMap<String, Vec<Edge>>) -> f64 {
-        let mut frontier = vec![Edge {
-            node: v1.clone(),
-            val: 1.0,
-        }];
-        let mut seen = HashSet::new();
-        seen.insert(&v1);
-
-        while !frontier.is_empty() {
-            let mut new_frontier = Vec::new();
-            for v in frontier {
-                if !g.contains_key(&v.node) {
-                    continue;
-                }
-
-                for n in &g[&v.node] {
-                    if n.node == v2 {
-                        return v.val * n.val;
-                    }
-
-                    if !seen.contains(&n.node) {
-                        seen.insert(&n.node);
-                        new_frontier.push(Edge {
-                            node: n.node.clone(),
-                            val: v.val * n.val,
-                        });
-                    }
-                }
-            }
-
-            frontier = new_frontier;
-        }
-
-        -1.0
-    }
-
-    fn answer_query(q: Vec<String>, g: &HashMap<String, Vec<Edge>>) -> f64 {
-        match q[1] == q[0] {
-            true if g.contains_key(&q[1]) => 1.0,
-            true => -1.0,
-            _ => Self::search(q[0].clone(), q[1].clone(), g),
-        }
-    }
-
-    pub fn calc_equation(
-        equations: Vec<Vec<String>>,
-        values: Vec<f64>,
-        queries: Vec<Vec<String>>,
-    ) -> Vec<f64> {
-        let g = Self::build_graph(equations, values);
-        let mut res = Vec::with_capacity(queries.len());
-        for q in queries {
-            res.push(Self::answer_query(q, &g));
-        }
-
-        res
+        (num - b'0') as i32
     }
 }
 
 struct Input {
-    equations: Vec<Vec<String>>,
-    values: Vec<f64>,
-    queries: Vec<Vec<String>>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [Input {
-        equations: [["a", "b"], ["b", "c"]]
-            .map(|v| v.map(|v| v.to_string()).to_vec())
-            .to_vec(),
-        values: [2.0, 3.0].to_vec(),
-        queries: [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]
-            .map(|v| v.map(|v| v.to_string()).to_vec())
-            .to_vec(),
-    }];
+    let inputs = [Input { n: 3 }, Input { n: 11 }, Input { n: 1000000000 }];
 
     for input in inputs.into_iter() {
-        let result = Solution::calc_equation(input.equations, input.values, input.queries);
+        let result = Solution::find_nth_digit(input.n);
         println!("{:?}", result);
     }
 }
