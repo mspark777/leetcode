cab2721873797d92609108016d6d014724223be7
mspark777 <topangel777@gmail.com>
2026-05-12 11:37:30 +0900

    413

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,42 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_nth_digit(n: i32) -> i32 {
-        let mut n = n as i64;
-        let mut i = 1i64;
-        let mut count = 9i64;
-        let mut start = 1i64;
+    pub fn number_of_arithmetic_slices(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut result = 0;
+        let mut curr = 0;
 
-        while n > (i * count) {
-            n -= i * count;
-            i += 1;
-            count *= 10;
-            start *= 10;
+        for i in 2..n {
+            let n0 = nums[i - 2];
+            let n1 = nums[i - 1];
+            let n2 = nums[i];
+
+            if (n2 - n1) == (n1 - n0) {
+                curr += 1;
+                result += curr;
+            } else {
+                curr = 0;
+            }
         }
 
-        let nums = (start + ((n - 1) / i)).to_string();
-        let nums = nums.as_bytes();
-        let idx = ((n - 1) % i) as usize;
-        let num = nums[idx];
-
-        (num - b'0') as i32
+        result
     }
 }
 
 struct Input {
-    n: i32,
+    nums: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { n: 3 }, Input { n: 11 }, Input { n: 1000000000 }];
+    let inputs = [
+        Input {
+            nums: [1, 2, 3, 4].to_vec(),
+        },
+        Input { nums: [1].to_vec() },
+    ];
 
     for input in inputs.into_iter() {
-        let result = Solution::find_nth_digit(input.n);
+        let result = Solution::number_of_arithmetic_slices(input.nums);
         println!("{:?}", result);
     }
 }
