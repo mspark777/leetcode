8b8699873c0fbaabcfda81e96983804b07e8f5b4
mspark777 <topangel777@gmail.com>
2026-05-13 10:02:59 +0900

    424

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,57 @@
 struct Solution;
 
 impl Solution {
-    pub fn original_digits(s: String) -> String {
-        use std::collections::HashMap;
+    pub fn character_replacement(s: String, k: i32) -> i32 {
+        let codes = s
+            .as_bytes()
+            .iter()
+            .copied()
+            .map(|b| (b - b'A') as usize)
+            .collect::<Vec<usize>>();
+        let n = codes.len();
+        let mut counts = [0; 26];
+        let mut result = 0;
+        let mut i = 0usize;
+        let mut max_count = 0i32;
 
-        let mut counts = HashMap::<char, i32>::new();
-        for ch in s.chars() {
-            counts.entry(ch).and_modify(|c| *c += 1).or_insert(1);
+        for j in 0..n {
+            let jcode = codes[j];
+            counts[jcode] += 1;
+            max_count = max_count.max(counts[jcode]);
+
+            while ((j + 1 - i) as i32) - max_count > k {
+                counts[codes[i]] -= 1;
+                i += 1;
+            }
+
+            if ((j + 1 - i) as i32) > result {
+                result = (j + 1 - i) as i32;
+            }
         }
 
-        let idx_map = [
-            ('z', 0),
-            ('w', 2),
-            ('u', 4),
-            ('x', 6),
-            ('g', 8),
-            ('o', 1),
-            ('h', 3),
-            ('f', 5),
-            ('s', 7),
-            ('i', 9),
-        ];
-        let mut repeats = [0; 10];
-        for (l, i) in idx_map.into_iter() {
-            repeats[i] = counts.get(&l).copied().unwrap_or_default();
-        }
-
-        repeats[1] -= repeats[0] + repeats[2] + repeats[4];
-        repeats[3] -= repeats[8];
-        repeats[5] -= repeats[4];
-        repeats[7] -= repeats[6];
-        repeats[9] -= repeats[5] + repeats[6] + repeats[8];
-
-        repeats
-            .into_iter()
-            .enumerate()
-            .map(|(i, r)| match r > 0 {
-                true => i.to_string().repeat(r as usize),
-                _ => String::new(),
-            })
-            .collect::<Vec<String>>()
-            .join("")
+        result
     }
 }
 
 struct Input {
     s: String,
+    k: i32,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "owoztneoer".to_string(),
+            s: "ABAB".to_string(),
+            k: 2,
         },
         Input {
-            s: "fivefuro".to_string(),
+            s: "AABABBA".to_string(),
+            k: 1,
         },
     ];
 
     for input in inputs.into_iter() {
-        let result = Solution::original_digits(input.s);
+        let result = Solution::character_replacement(input.s, input.k);
         println!("{:?}", result);
     }
 }
