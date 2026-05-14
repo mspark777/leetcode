4645f2baedd083af5fca914125ccb8d6ffb609f5
mspark777 <topangel777@gmail.com>
2026-05-14 15:16:42 +0900

    436

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,48 @@
 struct Solution;
 
 impl Solution {
-    pub fn character_replacement(s: String, k: i32) -> i32 {
-        let codes = s
-            .as_bytes()
-            .iter()
-            .copied()
-            .map(|b| (b - b'A') as usize)
-            .collect::<Vec<usize>>();
-        let n = codes.len();
-        let mut counts = [0; 26];
-        let mut result = 0;
-        let mut i = 0usize;
-        let mut max_count = 0i32;
+    pub fn find_right_interval(intervals: Vec<Vec<i32>>) -> Vec<i32> {
+        use std::collections::BTreeMap;
 
-        for j in 0..n {
-            let jcode = codes[j];
-            counts[jcode] += 1;
-            max_count = max_count.max(counts[jcode]);
-
-            while ((j + 1 - i) as i32) - max_count > k {
-                counts[codes[i]] -= 1;
-                i += 1;
-            }
-
-            if ((j + 1 - i) as i32) > result {
-                result = (j + 1 - i) as i32;
-            }
+        let mut btree = BTreeMap::<i32, usize>::new();
+        for (i, interval) in intervals.iter().enumerate() {
+            let start = interval[0];
+            btree.insert(start, i);
         }
 
+        let mut result = Vec::<i32>::with_capacity(intervals.len());
+        for interval in intervals.iter() {
+            let end = interval[1];
+            if let Some(node) = btree.range(end..).next() {
+                let idx = *node.1 as i32;
+                result.push(idx);
+            } else {
+                result.push(-1);
+            }
+        }
         result
     }
 }
 
 struct Input {
-    s: String,
-    k: i32,
+    intervals: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            s: "ABAB".to_string(),
-            k: 2,
+            intervals: [[1, 2]].map(|v| v.to_vec()).to_vec(),
         },
         Input {
-            s: "AABABBA".to_string(),
-            k: 1,
+            intervals: [[3, 4], [2, 3], [1, 2]].map(|v| v.to_vec()).to_vec(),
+        },
+        Input {
+            intervals: [[1, 4], [2, 3], [3, 4]].map(|v| v.to_vec()).to_vec(),
         },
     ];
 
     for input in inputs.into_iter() {
-        let result = Solution::character_replacement(input.s, input.k);
+        let result = Solution::find_right_interval(input.intervals);
         println!("{:?}", result);
     }
 }
