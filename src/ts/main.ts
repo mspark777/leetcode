f9a00daad12b260e396452f813e652367c3ae39c
mspark777 <topangel777@gmail.com>
2026-05-14 15:00:04 +0900

    430

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,42 @@
 import "@total-typescript/ts-reset";
 
-function averageValue(nums: number[]): number {
-  const filtered = nums
-    .filter((n) => BigInt(n) % 2n === 0n)
-    .filter((n) => BigInt(n) % 3n === 0n);
-  if (filtered.length < 1) {
-    return 0;
+class _Node {
+  val: number;
+  prev: _Node | null;
+  next: _Node | null;
+  child: _Node | null;
+
+  constructor(val?: number, prev?: _Node, next?: _Node, child?: _Node) {
+    this.val = val === undefined ? 0 : val;
+    this.prev = prev === undefined ? null : prev;
+    this.next = next === undefined ? null : next;
+    this.child = child === undefined ? null : child;
+  }
+}
+
+function flatten(head: _Node | null): _Node | null {
+  for (let curr = head; curr != null; curr = curr.next) {
+    if (curr.child == null) {
+      continue;
+    }
+
+    const next = curr.next;
+    curr.next = curr.child;
+    curr.next.prev = curr;
+    curr.child = null;
+
+    let child: _Node | null = curr.next;
+    while (child.next != null) {
+      child = child.next;
+    }
+
+    child.next = next;
+    if (next != null) {
+      next.prev = child;
+    }
   }
 
-  const avg = filtered.reduce((acc, num) => acc + num) / filtered.length;
-
-  return Math.floor(avg);
+  return head;
 }
 
 interface Input {
