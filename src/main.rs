d40289c5d7b09566c45e318d63c9e01458f2d3d8
mspark777 <topangel777@gmail.com>
2026-05-15 10:35:56 +0900

    438

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,53 @@
 struct Solution;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
-
-use std::cell::RefCell;
-use std::rc::Rc;
 impl Solution {
-    pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> i32 {
-        Self::recurse(root.clone(), target_sum as i64, vec![].as_mut())
-    }
+    pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
+        const SIZE: usize = 26;
 
-    pub fn recurse(
-        root: Option<Rc<RefCell<TreeNode>>>,
-        target_sum: i64,
-        vec: &mut Vec<i64>,
-    ) -> i32 {
-        let node = match root {
-            Some(node) => node,
-            _ => return 0,
-        };
+        let idx = |c: char| ((c as u8) - b'a') as usize;
 
-        let node = node.borrow();
-        let mut count = 0;
-        vec.push(0);
-        for x in vec.iter_mut() {
-            *x += node.val as i64;
-            if *x == target_sum {
-                count += 1;
+        let mut anagram = [0u8; SIZE];
+        for c in p.chars() {
+            anagram[idx(c)] += 1;
+        }
+
+        let mut result = Vec::<i32>::new();
+        let mut window = [0u8; SIZE];
+        let s = s.chars().collect::<Vec<char>>();
+        for right in 0..s.len() {
+            window[idx(s[right])] += 1;
+            if right >= p.len() - 1 {
+                let left = right + 1 - p.len();
+                if anagram == window {
+                    result.push(left as i32);
+                }
+                window[idx(s[left])] -= 1;
             }
         }
-        count = count
-            + Self::recurse(node.left.clone(), target_sum, vec)
-            + Self::recurse(node.right.clone(), target_sum, vec);
-        vec.pop();
-        for x in vec.iter_mut() {
-            *x -= node.val as i64
-        }
-        count
+
+        result
     }
 }
 
 struct Input {
-    intervals: Vec<Vec<i32>>,
+    s: String,
+    p: String,
 }
 
 fn main() {
     let inputs = [
         Input {
-            intervals: [[1, 2]].map(|v| v.to_vec()).to_vec(),
+            s: "cbaebabacd".to_string(),
+            p: "abc".to_string(),
         },
         Input {
-            intervals: [[3, 4], [2, 3], [1, 2]].map(|v| v.to_vec()).to_vec(),
-        },
-        Input {
-            intervals: [[1, 4], [2, 3], [3, 4]].map(|v| v.to_vec()).to_vec(),
+            s: "abab".to_string(),
+            p: "ab".to_string(),
         },
     ];
 
     for input in inputs.into_iter() {
-        let result = Solution::find_right_interval(input.intervals);
+        let result = Solution::find_anagrams(input.s, input.p);
         println!("{:?}", result);
     }
 }
