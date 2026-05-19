7fcedcd11dedd7f181ee2d7ca8fa295f090cdba0
mspark777 <topangel777@gmail.com>
2026-05-19 12:45:08 +0900

    456

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,61 @@
 struct Solution;
 
 impl Solution {
-    pub fn four_sum_count(
-        nums1: Vec<i32>,
-        nums2: Vec<i32>,
-        nums3: Vec<i32>,
-        nums4: Vec<i32>,
-    ) -> i32 {
-        use std::collections::HashMap;
-
-        let mut sums = HashMap::<i32, i32>::new();
-        for n1 in nums1.iter().copied() {
-            for n2 in nums2.iter().copied() {
-                let sum = n1 + n2;
-                sums.entry(sum).and_modify(|c| *c += 1).or_insert(1);
-            }
+    pub fn find132pattern(nums: Vec<i32>) -> bool {
+        let mut nums = nums;
+        let n = nums.len();
+        if n < 3 {
+            return false;
         }
 
-        let mut result = 0;
-        for n3 in nums3.iter().copied() {
-            for n4 in nums4.iter().copied() {
-                let target = -(n3 + n4);
-                result += sums.get(&target).copied().unwrap_or_default()
-            }
+        let mut min_array = vec![0; n];
+        min_array[0] = nums[0];
+
+        for i in 1..n {
+            min_array[i] = min_array[i - 1].min(nums[i]);
         }
 
-        result
+        let mut k = n;
+        for j in (1..n).rev() {
+            if nums[j] <= min_array[j] {
+                continue;
+            }
+
+            while (k < n) && (nums[k] <= min_array[j]) {
+                k += 1;
+            }
+
+            if (k < n) && (nums[k] < nums[j]) {
+                return true;
+            }
+
+            k -= 1;
+            nums[k] = nums[j];
+        }
+
+        false
     }
 }
 
 struct Input {
-    nums1: Vec<i32>,
-    nums2: Vec<i32>,
-    nums3: Vec<i32>,
-    nums4: Vec<i32>,
+    nums: Vec<i32>,
 }
 
 fn main() {
     let inputs = [
         Input {
-            nums1: [1, 2].to_vec(),
-            nums2: [-2, -1].to_vec(),
-            nums3: [-1, 2].to_vec(),
-            nums4: [0, 2].to_vec(),
+            nums: [1, 2, 3, 4].to_vec(),
         },
         Input {
-            nums1: [0].to_vec(),
-            nums2: [0].to_vec(),
-            nums3: [0].to_vec(),
-            nums4: [0].to_vec(),
+            nums: [3, 1, 4, 2].to_vec(),
+        },
+        Input {
+            nums: [-1, 3, 2, 0].to_vec(),
         },
     ];
 
     for input in inputs.into_iter() {
-        let result = Solution::four_sum_count(input.nums1, input.nums2, input.nums3, input.nums4);
+        let result = Solution::find132pattern(input.nums);
         println!("{:?}", result);
     }
 }
