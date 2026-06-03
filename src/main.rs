61ada75cd408f7a3b6f6d9d7435fcac561693844
mspark777 <topangel777@gmail.com>
2026-06-03 21:52:53 +0900

    526

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,42 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_longest_word(s: String, mut d: Vec<String>) -> String {
-        d.sort_unstable_by(|a, b| b.len().cmp(&a.len()).then_with(|| a.cmp(b)));
-        for s2 in d {
-            let mut s2_iter = s2.chars().peekable();
-            for c in s.chars() {
-                if let Some(&c2) = s2_iter.peek() {
-                    if c == c2 {
-                        s2_iter.next();
-                    }
-                } else {
-                    break;
-                }
-            }
-            if s2_iter.peek().is_none() {
-                return s2;
+    pub fn count_arrangement(n: i32) -> i32 {
+        let n = n as usize;
+        let mut visited = vec![false; n + 1];
+        let mut result = 0;
+        Self::calculate(n, 1, &mut visited, &mut result);
+
+        result
+    }
+
+    fn calculate(n: usize, pos: usize, visited: &mut [bool], result: &mut i32) {
+        if pos > n {
+            *result += 1;
+            return;
+        }
+
+        for i in 1..=n {
+            if visited[i] {
+                continue;
+            } else if pos.is_multiple_of(i) || i.is_multiple_of(pos) {
+                visited[i] = true;
+                Self::calculate(n, pos + 1, visited, result);
+                visited[i] = false;
             }
         }
-        String::new()
     }
 }
 
-struct Input {}
+struct Input {
+    n: i32,
+}
 
 fn main() {
-    let inputs = [
-        Input {
-            strs: ["aba", "cdc", "eae"].map(|s| s.to_string()).to_vec(),
-        },
-        Input {
-            strs: ["aaa", "aaa", "aa"].map(|s| s.to_string()).to_vec(),
-        },
-        Input {
-            strs: ["aabbcc", "aabbcc", "c"].map(|s| s.to_string()).to_vec(),
-        },
-    ];
+    let inputs = [Input { n: 2 }, Input { n: 1 }];
 
     for input in inputs.into_iter() {
-        let result = Solution::find_lu_slength(input.strs);
+        let result = Solution::count_arrangement(input.n);
         println!("{:?}", result);
     }
 }
