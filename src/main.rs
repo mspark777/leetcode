0a0472536faf3a3505fc71f1b7ea297fa2cecf56
mspark777 <topangel777@gmail.com>
2026-06-04 13:53:17 +0900

    532

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,41 @@
 struct Solution;
 
-use std::collections::VecDeque;
+use std::collections::HashSet;
 
 impl Solution {
-    pub fn update_board(mut board: Vec<Vec<char>>, click: Vec<i32>) -> Vec<Vec<char>> {
-        if board[click[0] as usize][click[1] as usize] == 'M' {
-            board[click[0] as usize][click[1] as usize] = 'X';
-            return board;
-        }
-
-        let n = board.len() as i32;
-        let m = board[0].len() as i32;
-        let dirs = [
-            (-1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, -1),
-            (-1, -1),
-        ];
-        let mut q = VecDeque::new();
-        q.push_back((click[0], click[1]));
-
-        while !q.is_empty() {
-            let (i, j) = q.pop_front().unwrap();
-
-            if board[i as usize][j as usize] == 'E' {
-                let mut mines = 0;
-                let mut neighbors = VecDeque::new();
-                for (di, dj) in &dirs {
-                    let ni = i + di;
-                    let nj = j + dj;
-                    if ni >= 0 && ni < n && nj >= 0 && nj < m {
-                        if board[ni as usize][nj as usize] == 'M' {
-                            mines += 1;
-                        } else if board[ni as usize][nj as usize] == 'E' {
-                            neighbors.push_back((ni, nj));
-                        }
-                    }
-                }
-                if mines > 0 {
-                    board[i as usize][j as usize] = (b'0' + mines) as char;
-                } else {
-                    board[i as usize][j as usize] = 'B';
-                    q.append(&mut neighbors);
-                }
+    pub fn find_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let mut seen_set = HashSet::<i32>::new();
+        let mut result_set = HashSet::<i32>::new();
+        for num in nums.into_iter() {
+            let n = num - k;
+            if seen_set.contains(&n) {
+                result_set.insert(n);
             }
-        }
 
-        board
+            let n = num + k;
+            if seen_set.contains(&n) {
+                result_set.insert(num);
+            }
+
+            seen_set.insert(num);
+        }
+        result_set.len() as i32
     }
 }
 
 struct Input {
-    board: Vec<Vec<char>>,
-    click: Vec<i32>,
+    nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
     let inputs = [Input {
-        board: [
-            ['E', 'E', 'E', 'E', 'E'],
-            ['E', 'E', 'M', 'E', 'E'],
-            ['E', 'E', 'E', 'E', 'E'],
-            ['E', 'E', 'E', 'E', 'E'],
-        ]
-        .map(|v| v.to_vec())
-        .to_vec(),
-        click: [3, 0].to_vec(),
+        nums: [3, 1, 4, 1, 5].to_vec(),
+        k: 2,
     }];
 
     for input in inputs.into_iter() {
-        let result = Solution::update_board(input.board, input.click);
+        let result = Solution::find_pairs(input.nums, input.k);
         println!("{:?}", result);
     }
 }
