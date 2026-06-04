04f59a82d6e54ff1c6f2b13c61289dea35ee06c2
mspark777 <topangel777@gmail.com>
2026-06-04 13:48:35 +0900

    529

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,79 @@
-use rand::Rng;
-use std::collections::BTreeMap;
+struct Solution;
 
-struct Solution {
-    btm: BTreeMap<i32, i32>,
-    n: i32,
-}
+use std::collections::VecDeque;
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl Solution {
-    fn new(w: Vec<i32>) -> Self {
-        let mut btm: BTreeMap<i32, i32> = BTreeMap::new();
-        let mut n = 0;
-        for (i, weight) in w.into_iter().enumerate() {
-            n += weight;
-            btm.insert(n, i as i32);
+    pub fn update_board(mut board: Vec<Vec<char>>, click: Vec<i32>) -> Vec<Vec<char>> {
+        if board[click[0] as usize][click[1] as usize] == 'M' {
+            board[click[0] as usize][click[1] as usize] = 'X';
+            return board;
         }
-        Self { btm, n }
-    }
 
-    fn pick_index(&self) -> i32 {
-        let mut rng = rand::thread_rng();
-        let m: i32 = rng.gen_range(0..self.n);
-        self.btm
-            .range(m + 1..)
-            .next()
-            .map(|v| v.1)
-            .copied()
-            .unwrap_or(-1)
+        let n = board.len() as i32;
+        let m = board[0].len() as i32;
+        let dirs = [
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+            (1, 0),
+            (1, -1),
+            (0, -1),
+            (-1, -1),
+        ];
+        let mut q = VecDeque::new();
+        q.push_back((click[0], click[1]));
+
+        while !q.is_empty() {
+            let (i, j) = q.pop_front().unwrap();
+
+            if board[i as usize][j as usize] == 'E' {
+                let mut mines = 0;
+                let mut neighbors = VecDeque::new();
+                for (di, dj) in &dirs {
+                    let ni = i + di;
+                    let nj = j + dj;
+                    if ni >= 0 && ni < n && nj >= 0 && nj < m {
+                        if board[ni as usize][nj as usize] == 'M' {
+                            mines += 1;
+                        } else if board[ni as usize][nj as usize] == 'E' {
+                            neighbors.push_back((ni, nj));
+                        }
+                    }
+                }
+                if mines > 0 {
+                    board[i as usize][j as usize] = (b'0' + mines) as char;
+                } else {
+                    board[i as usize][j as usize] = 'B';
+                    q.append(&mut neighbors);
+                }
+            }
+        }
+
+        board
     }
 }
 
 struct Input {
-    n: i32,
+    board: Vec<Vec<char>>,
+    click: Vec<i32>,
 }
 
 fn main() {
-    let inputs = [Input { n: 2 }, Input { n: 1 }];
+    let inputs = [Input {
+        board: [
+            ['E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'M', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E'],
+        ]
+        .map(|v| v.to_vec())
+        .to_vec(),
+        click: [3, 0].to_vec(),
+    }];
 
     for input in inputs.into_iter() {
-        let result = Solution::count_arrangement(input.n);
+        let result = Solution::update_board(input.board, input.click);
         println!("{:?}", result);
     }
 }
