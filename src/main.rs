38193a120cb2a7a6dcef99d5c558b59b2bf810a1
mspark777 <topangel777@gmail.com>
2026-06-05 11:06:33 +0900

    537

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,38 @@
-use std::collections::HashMap;
-
-struct Codec {
-    encode_map: HashMap<String, String>,
-    docode_map: HashMap<String, String>,
-    base: String,
-}
-
-impl Codec {
-    fn new() -> Self {
-        Self {
-            encode_map: HashMap::new(),
-            docode_map: HashMap::new(),
-            base: "http://tinyurl.com/".to_string(),
-        }
-    }
-
-    fn encode(&mut self, long_url: String) -> String {
-        if let Some(short) = self.encode_map.get(&long_url) {
-            short.clone()
-        } else {
-            let short = format!("{}{}", self.base, self.encode_map.len() + 1);
-            self.encode_map.insert(long_url.clone(), short.clone());
-            self.docode_map.insert(short.clone(), long_url);
-            short
-        }
-    }
-
-    fn decode(&self, short_url: String) -> String {
-        self.docode_map[&short_url].clone()
-    }
-}
-
 struct Solution;
 
-use std::collections::HashSet;
-
 impl Solution {
-    pub fn find_pairs(nums: Vec<i32>, k: i32) -> i32 {
-        let mut seen_set = HashSet::<i32>::new();
-        let mut result_set = HashSet::<i32>::new();
-        for num in nums.into_iter() {
-            let n = num - k;
-            if seen_set.contains(&n) {
-                result_set.insert(n);
-            }
+    pub fn complex_number_multiply(num1: String, num2: String) -> String {
+        let num1 = Self::split(num1);
+        let num2 = Self::split(num2);
 
-            let n = num + k;
-            if seen_set.contains(&n) {
-                result_set.insert(num);
-            }
+        let real = num1.0 * num2.0 - num1.1 * num2.1;
+        let imagine = num1.0 * num2.1 + num2.0 * num1.1;
+        format!("{}+{}i", real, imagine)
+    }
 
-            seen_set.insert(num);
-        }
-        result_set.len() as i32
+    fn split(num: String) -> (i32, i32) {
+        let mut splitted = num.split('+');
+        let real = splitted.next().unwrap();
+        let imagine = splitted.next().unwrap();
+        let imagine = &imagine[0..(imagine.len() - 1)];
+
+        (real.parse().unwrap(), imagine.parse().unwrap())
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    k: i32,
+    num1: String,
+    num2: String,
 }
 
 fn main() {
     let inputs = [Input {
-        nums: [3, 1, 4, 1, 5].to_vec(),
-        k: 2,
+        num1: "1+1i".to_string(),
+        num2: "1+1i".to_string(),
     }];
 
     for input in inputs.into_iter() {
-        let result = Solution::find_pairs(input.nums, input.k);
+        let result = Solution::complex_number_multiply(input.num1, input.num2);
         println!("{:?}", result);
     }
 }
