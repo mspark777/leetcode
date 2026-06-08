ad4bec3988b159c7540d4c010db6725816cca185
mspark777 <topangel777@gmail.com>
2026-06-08 09:16:10 +0900

    547

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,52 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_min_difference(time_points: Vec<String>) -> i32 {
-        if time_points.len() > 1440 {
+    pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+        let n = is_connected.len();
+        if n == 0 {
             return 0;
         }
 
-        let mut minutes = time_points
-            .iter()
-            .map(|s| Self::parse(s.as_str()))
-            .collect::<Vec<i32>>();
+        let mut seen = vec![false; n];
+        let mut result = 0;
 
-        minutes.sort_unstable();
-        minutes.push(minutes[0] + 1440);
-        minutes.windows(2).map(|w| w[1] - w[0]).min().unwrap()
+        for row in 0..n {
+            if !seen[row] {
+                result += 1;
+                Self::dfs(&is_connected, row, &mut seen);
+            }
+        }
+
+        result
     }
 
-    fn parse(s: &str) -> i32 {
-        let h = s[..2].parse::<i32>().unwrap();
-        let m = s[3..].parse::<i32>().unwrap();
+    fn dfs(graph: &[Vec<i32>], v: usize, seen: &mut [bool]) {
+        if seen[v] {
+            return;
+        }
 
-        h * 60 + m
+        seen[v] = true;
+        for (i, connected) in graph[v].iter().copied().enumerate() {
+            if !seen[i] && (connected == 1) {
+                Self::dfs(graph, i, seen);
+            }
+        }
     }
 }
 
 struct Input {
-    time_points: Vec<String>,
+    is_connected: Vec<Vec<i32>>,
 }
 
 fn main() {
     let inputs = [Input {
-        time_points: ["23:59", "00:00"].map(|s| s.to_string()).to_vec(),
+        is_connected: [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+            .map(|v| v.to_vec())
+            .to_vec(),
     }];
 
     for input in inputs.into_iter() {
-        let result = Solution::find_min_difference(input.time_points);
+        let result = Solution::find_circle_num(input.is_connected);
         println!("{:?}", result);
     }
 }
