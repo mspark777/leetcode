a41d572697913dcb173198bbe18ed41b887a5dd5
mspark777 <topangel777@gmail.com>
2026-06-10 21:59:10 +0900

    556

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,48 @@
 struct Solution;
 
 impl Solution {
-    pub fn least_bricks(wall: Vec<Vec<i32>>) -> i32 {
-        use std::collections::HashMap;
+    pub fn next_greater_element(n: i32) -> i32 {
+        let mut digits = n.to_string().bytes().collect::<Vec<u8>>();
 
-        let c = wall
+        let mut i = digits.len() as i32 - 2;
+        while i >= 0 && digits[i as usize] >= digits[(i + 1) as usize] {
+            i -= 1;
+        }
+
+        if i < 0 {
+            return -1;
+        }
+
+        let mut j = digits.len() as i32 - 1;
+        while digits[j as usize] <= digits[i as usize] {
+            j -= 1;
+        }
+
+        digits.swap(i as usize, j as usize);
+
+        digits[(i as usize + 1)..].reverse();
+
+        let res = digits
             .iter()
-            .fold(HashMap::new(), |mut acc, row| {
-                row.iter()
-                    .take(row.len() - 1)
-                    .scan(0, |state, &x| {
-                        *state += x;
-                        Some(*state)
-                    })
-                    .for_each(|x| {
-                        acc.entry(x).and_modify(|c| *c += 1).or_insert(1);
-                    });
-                acc
-            })
-            .values()
-            .max()
-            .copied()
-            .unwrap_or(0);
+            .fold(0i64, |acc, &d| acc * 10 + (d - b'0') as i64);
 
-        (wall.len() as i32) - c
+        if res > i32::MAX as i64 {
+            -1
+        } else {
+            res as i32
+        }
     }
 }
 
 struct Input {
-    wall: Vec<Vec<i32>>,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [Input {
-        wall: vec![
-            vec![1, 2, 2, 1],
-            vec![3, 1, 2],
-            vec![1, 3, 2],
-            vec![2, 4],
-            vec![3, 1, 2],
-            vec![1, 3, 1, 1],
-        ],
-    }];
+    let inputs = [Input { n: 12 }, Input { n: 21 }];
 
     for input in inputs.into_iter() {
-        let result = Solution::least_bricks(input.wall);
+        let result = Solution::next_greater_element(input.n);
         println!("{:?}", result);
     }
 }
