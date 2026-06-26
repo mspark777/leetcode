9f02278cbe1acb4139dffb7f7fe62520f58a43ac
mspark777 <topangel777@gmail.com>
2026-06-26 16:06:58 +0900

    560

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,46 @@
 struct Solution;
 
 impl Solution {
-    pub fn next_greater_element(n: i32) -> i32 {
-        let mut digits = n.to_string().bytes().collect::<Vec<u8>>();
+    pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        use std::collections::HashMap;
 
-        let mut i = digits.len() as i32 - 2;
-        while i >= 0 && digits[i as usize] >= digits[(i + 1) as usize] {
-            i -= 1;
+        let mut result = 0;
+        let mut sum = 0;
+        let mut sums = HashMap::<i32, i32>::new();
+        sums.insert(0, 1);
+
+        for num in nums {
+            sum += num;
+            if let Some(cnt) = sums.get(&(sum - k)) {
+                result += cnt;
+            }
+
+            sums.entry(sum).and_modify(|cnt| *cnt += 1).or_insert(1);
         }
 
-        if i < 0 {
-            return -1;
-        }
-
-        let mut j = digits.len() as i32 - 1;
-        while digits[j as usize] <= digits[i as usize] {
-            j -= 1;
-        }
-
-        digits.swap(i as usize, j as usize);
-
-        digits[(i as usize + 1)..].reverse();
-
-        let res = digits
-            .iter()
-            .fold(0i64, |acc, &d| acc * 10 + (d - b'0') as i64);
-
-        if res > i32::MAX as i64 {
-            -1
-        } else {
-            res as i32
-        }
+        result
     }
 }
 
 struct Input {
-    n: i32,
+    nums: Vec<i32>,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [Input { n: 12 }, Input { n: 21 }];
+    let inputs = [
+        Input {
+            nums: [1, 1, 1].to_vec(),
+            k: 2,
+        },
+        Input {
+            nums: [1, 2, 3].to_vec(),
+            k: 3,
+        },
+    ];
 
     for input in inputs.into_iter() {
-        let result = Solution::next_greater_element(input.n);
+        let result = Solution::subarray_sum(input.nums, input.k);
         println!("{:?}", result);
     }
 }
