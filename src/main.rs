decbbb36da771951b19b057502c49e174e350618
mspark777 <topangel777@gmail.com>
2026-07-15 23:31:11 +0900

    636

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,54 @@
 struct Solution;
 
 impl Solution {
-    pub fn judge_square_sum(c: i32) -> bool {
-        let mut c = c;
-        let mut i = 2;
+    pub fn exclusive_time(n: i32, logs: Vec<String>) -> Vec<i32> {
+        let n = n as usize;
+        let mut stack = Vec::<i32>::new();
+        let mut result = vec![0; n];
+        let mut s = logs[0].split(':').collect::<Vec<_>>();
 
-        while (i * i) <= c {
-            let mut count = 0;
-            while (c % i) == 0 {
-                count += 1;
-                c /= i;
+        stack.push(s[0].parse().unwrap());
+        let mut prev = s[2].parse::<i32>().unwrap();
+
+        let mut i = 1;
+        while i < logs.len() {
+            s = logs[i].split(':').collect();
+            if s[1] == "start" {
+                if !stack.is_empty() {
+                    result[stack.last().copied().unwrap() as usize] +=
+                        s[2].parse::<i32>().unwrap() - prev;
+                }
+
+                stack.push(s[0].parse().unwrap());
+                prev = s[2].parse::<i32>().unwrap();
+            } else {
+                result[stack.last().copied().unwrap() as usize] +=
+                    s[2].parse::<i32>().unwrap() - prev + 1;
+                stack.pop();
+                prev = s[2].parse::<i32>().unwrap() + 1;
             }
-
-            if ((i % 4) == 3) && ((count & 1) == 1) {
-                return false;
-            }
-
             i += 1;
         }
 
-        (c % 4) != 3
+        result
     }
 }
 
 struct Input {
-    c: i32,
+    n: i32,
+    logs: Vec<String>,
 }
 
 fn main() {
-    let inputs = [Input { c: 5 }, Input { c: 3 }];
+    let inputs = [Input {
+        n: 2,
+        logs: ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
+            .map(|v| v.to_string())
+            .to_vec(),
+    }];
 
     for input in inputs.into_iter() {
-        let result = Solution::judge_square_sum(input.c);
+        let result = Solution::exclusive_time(input.n, input.logs);
         println!("{:?}", result);
     }
 }
