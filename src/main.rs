8a1b8519be5553aa5c0267e74d07d8f47f78b631
mspark777 <topangel777@gmail.com>
2026-07-18 16:51:18 +0900

    647

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,37 @@
 struct Solution;
 
 impl Solution {
-    pub fn solve_equation(equation: String) -> String {
-        let equations = equation.as_bytes();
-        let len = equations.len();
-        let mut tmp = 0;
-        let mut lr = 1;
-        let mut a = 0;
-        let mut b = 0;
-        let mut sign = 1;
-        let mut calc = 0;
+    pub fn count_substrings(s: String) -> i32 {
+        let n = s.len();
+        let s = s.as_bytes();
+        (0..n)
+            .map(|i| Self::count(s, i, i, n) + Self::count(s, i, i + 1, n))
+            .sum()
+    }
 
-        for (idx, equation) in equations.iter().copied().enumerate() {
-            if equation.is_ascii_digit() {
-                tmp = tmp * 10 + (equation - b'0') as i32;
-                calc += 1;
-            }
-            if equation == b'x' {
-                if tmp == 0 && calc == 0 {
-                    a += sign * lr;
-                }
-                a += sign * lr * tmp;
-                calc = 0;
-                tmp = 0;
-            }
-            if equation == b'-' || equation == b'+' || equation == b'=' || idx == len - 1 {
-                b += sign * lr * tmp;
-                calc = 0;
-                tmp = 0;
-                if equation == b'-' {
-                    sign = -1;
-                } else if equation == b'+' {
-                    sign = 1;
-                } else if equation == b'=' {
-                    lr = -1;
-                    sign = 1;
-                }
-            }
-        }
-
-        if a != 0 {
-            format!("x={}", -b / a)
-        } else if b == 0 {
-            "Infinite solutions".to_string()
-        } else {
-            "No solution".to_string()
-        }
+    fn count(s: &[u8], i: usize, j: usize, n: usize) -> i32 {
+        (0..=i)
+            .rev()
+            .zip(j..n)
+            .try_fold(0, |a, (i, j)| match s[i] == s[j] {
+                true => Ok(a + 1),
+                _ => Err(a),
+            })
+            .unwrap_or_else(|a| a)
     }
 }
 
 struct Input {
-    equation: String,
+    s: String,
 }
 
 fn main() {
     let inputs = [Input {
-        equation: "x+5-3+x=6+x-2".to_string(),
+        s: "abc".to_string(),
     }];
 
     for input in inputs.into_iter() {
-        let result = Solution::solve_equation(input.equation);
+        let result = Solution::count_substrings(input.s);
         println!("{:?}", result);
     }
 }
