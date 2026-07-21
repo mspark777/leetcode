4cef7f6d21b36102d091c7a35613578c829b2fdc
mspark777 <topangel777@gmail.com>
2026-07-21 22:46:29 +0900

    672

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
 struct Solution;
 
 impl Solution {
-    pub fn construct_array(n: i32, k: i32) -> Vec<i32> {
-        let n = n as usize;
-        let k = k as usize;
-        let mut c = 0usize;
-        let mut result = vec![0; n];
+    pub fn flip_lights(n: i32, presses: i32) -> i32 {
+        let n = n.min(3);
 
-        for v in 1..(n - k) {
-            result[c] = v as i32;
-            c += 1;
+        match presses {
+            0 => 1,
+            1 if n == 1 => 2,
+            1 if n == 2 => 3,
+            1 => 4,
+            2 if n == 1 => 2,
+            2 if n == 2 => 4,
+            2 => 7,
+            _ if n == 1 => 2,
+            _ if n == 2 => 4,
+            _ => 8,
         }
-
-        for i in 0..=k {
-            result[c] = match i & 1 {
-                0 => (n + (i / 2) - k) as i32,
-                _ => (n - (i / 2)) as i32,
-            };
-            c += 1;
-        }
-
-        result
     }
 }
 
 struct Input {
     n: i32,
-    k: i32,
+    presses: i32,
 }
 
 fn main() {
-    let inputs = [Input { n: 3, k: 1 }];
+    let inputs = [Input { n: 1, presses: 1 }];
 
     for input in inputs.into_iter() {
-        let result = Solution::construct_array(input.n, input.k);
+        let result = Solution::flip_lights(input.n, input.presses);
         println!("{:?}", result);
     }
 }
