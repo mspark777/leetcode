52efa155d482e044269c26e0a316bc3cbcf4dfe1
mspark777 <topangel777@gmail.com>
2026-07-23 15:50:16 +0900

    687

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,52 @@ impl Solution {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn longest_univalue_path(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut max = 0;
+        root.map(|root| {
+            let val = root.borrow().val;
+            Self::dfs(&mut max, Some(root), val)
+        });
+        max
+    }
+
+    fn dfs(max: &mut i32, node: Option<Rc<RefCell<TreeNode>>>, x: i32) -> i32 {
+        node.map_or(0, |node| {
+            let node = node.borrow();
+
+            let l = Self::dfs(max, node.left.clone(), node.val);
+            let r = Self::dfs(max, node.right.clone(), node.val);
+            *max = (*max).max(l + r);
+
+            match x == node.val {
+                true => 1 + l.max(r),
+                _ => 0,
+            }
+        })
+    }
+}
+
 struct Input {
     a: String,
     b: String,
