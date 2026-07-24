26c5d471725db92ee0315f10c4779ff966ede349
mspark777 <topangel777@gmail.com>
2026-07-24 12:39:01 +0900

    688

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,58 @@
 struct Solution;
 
 impl Solution {
-    pub fn repeated_string_match(a: String, b: String) -> i32 {
-        let an = a.len();
-        let bn = b.len();
-        let x = bn / an + 2;
+    pub fn knight_probability(n: i32, k: i32, row: i32, column: i32) -> f64 {
+        let n = n as usize;
+        let k = k as usize;
 
-        a.repeat(x)
-            .find(&b)
-            .map(|i| ((i + bn - 1) / an) + 1)
-            .map(|i| i as i32)
-            .unwrap_or(-1)
-    }
-}
+        let mut f = vec![vec![vec![0.0; n]; n]; k + 1];
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
-
-use std::cell::RefCell;
-use std::rc::Rc;
-impl Solution {
-    pub fn longest_univalue_path(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut max = 0;
-        root.map(|root| {
-            let val = root.borrow().val;
-            Self::dfs(&mut max, Some(root), val)
-        });
-        max
-    }
-
-    fn dfs(max: &mut i32, node: Option<Rc<RefCell<TreeNode>>>, x: i32) -> i32 {
-        node.map_or(0, |node| {
-            let node = node.borrow();
-
-            let l = Self::dfs(max, node.left.clone(), node.val);
-            let r = Self::dfs(max, node.right.clone(), node.val);
-            *max = (*max).max(l + r);
-
-            match x == node.val {
-                true => 1 + l.max(r),
-                _ => 0,
+        for row in &mut f[0] {
+            for cell in row.iter_mut() {
+                *cell = 1.0;
             }
-        })
+        }
+
+        let dirs: [i32; 9] = [-2, -1, 2, 1, -2, 1, 2, -1, -2];
+
+        for h in 1..=k {
+            for i in 0..n {
+                for j in 0..n {
+                    for p in 0..8 {
+                        let x = (i as i32) + dirs[p];
+                        let y = (j as i32) + dirs[p + 1];
+
+                        if x >= 0 && x < (n as i32) && y >= 0 && y < (n as i32) {
+                            let x = x as usize;
+                            let y = y as usize;
+                            f[h][i][j] += f[h - 1][x][y] / 8.0;
+                        }
+                    }
+                }
+            }
+        }
+
+        f[k][row as usize][column as usize]
     }
 }
 
 struct Input {
-    a: String,
-    b: String,
+    n: i32,
+    k: i32,
+    row: i32,
+    column: i32,
 }
 
 fn main() {
     let inputs = [Input {
-        a: "abcd".to_string(),
-        b: "cdabcdab".to_string(),
+        n: 3,
+        k: 2,
+        row: 0,
+        column: 0,
     }];
 
     for input in inputs.into_iter() {
-        let result = Solution::repeated_string_match(input.a, input.b);
+        let result = Solution::knight_probability(input.n, input.k, input.row, input.column);
         println!("{:?}", result);
     }
 }
