40ef01484a897a8f6642271c9f4cd6c331f29d7b
mspark777 <topangel777@gmail.com>
2026-07-27 15:12:38 +0900

    707

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,128 @@ impl Solution {
     }
 }
 
+#[derive(Debug)]
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(val: i32) -> Self {
+        Node { val, next: None }
+    }
+}
+
+struct MyLinkedList {
+    head: Option<Box<Node>>,
+    size: usize,
+}
+
+impl MyLinkedList {
+    fn new() -> Self {
+        MyLinkedList {
+            head: None,
+            size: 0,
+        }
+    }
+
+    fn get(&self, index: i32) -> i32 {
+        if index < 0 || index as usize >= self.size {
+            return -1;
+        }
+
+        let mut current = &self.head;
+        for _ in 0..index {
+            if let Some(node) = current {
+                current = &node.next;
+            }
+        }
+
+        if let Some(node) = current {
+            node.val
+        } else {
+            -1
+        }
+    }
+
+    fn add_at_head(&mut self, val: i32) {
+        let mut new_node = Box::new(Node::new(val));
+        new_node.next = self.head.take();
+        self.head = Some(new_node);
+        self.size += 1;
+    }
+
+    fn add_at_tail(&mut self, val: i32) {
+        let new_node = Box::new(Node::new(val));
+
+        if self.head.is_none() {
+            self.head = Some(new_node);
+        } else {
+            let mut current = &mut self.head;
+            while let Some(node) = current {
+                if node.next.is_none() {
+                    node.next = Some(new_node);
+                    break;
+                }
+                current = &mut node.next;
+            }
+        }
+        self.size += 1;
+    }
+
+    fn add_at_index(&mut self, index: i32, val: i32) {
+        if index < 0 || index as usize > self.size {
+            return;
+        }
+
+        if index == 0 {
+            self.add_at_head(val);
+            return;
+        }
+
+        let mut new_node = Box::new(Node::new(val));
+        let mut current = &mut self.head;
+
+        for _ in 0..(index - 1) {
+            if let Some(node) = current {
+                current = &mut node.next;
+            }
+        }
+
+        if let Some(node) = current {
+            new_node.next = node.next.take();
+            node.next = Some(new_node);
+        }
+        self.size += 1;
+    }
+
+    fn delete_at_index(&mut self, index: i32) {
+        if index < 0 || index as usize >= self.size {
+            return;
+        }
+
+        if index == 0 {
+            self.head = self.head.take().and_then(|node| node.next);
+            self.size -= 1;
+            return;
+        }
+
+        let mut current = &mut self.head;
+        for _ in 0..(index - 1) {
+            if let Some(node) = current {
+                current = &mut node.next;
+            }
+        }
+
+        if let Some(node) = current {
+            if let Some(next_node) = &mut node.next {
+                node.next = next_node.next.take();
+            }
+        }
+        self.size -= 1;
+    }
+}
+
 struct Input {
     nums: Vec<i32>,
     k: i32,
