aaea7fd5441d519ff9fe916deaed04508dd46366
mspark777 <topangel777@gmail.com>
2026-07-28 12:09:47 +0900

    721

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,58 @@ struct Solution;
 
 use std::collections::HashMap;
 
-#[derive(Default)]
-struct Node {
-    children: HashMap<char, Node>,
-    flag: bool,
-}
-
-impl Node {
-    fn insert(&mut self, word: String, best: &mut String) {
-        let mut node = self;
-        let mut valid = true;
-
-        for (i, c) in word.chars().enumerate() {
-            node = node.children.entry(c).or_default();
-            if i == word.len() - 1 {
-                node.flag = true;
-            }
-
-            if !node.flag {
-                valid = false;
-            }
-        }
-
-        if !valid {
-            return;
-        }
-
-        if best.len() < word.len() || (best.len() == word.len() && word.as_str() < best.as_str()) {
-            best.clear();
-            best.extend(word.chars());
-        }
-    }
-}
-
 impl Solution {
-    pub fn longest_word(words: Vec<String>) -> String {
-        let mut words = words;
-        let mut trie = Node::default();
-        let mut best = String::new();
+    pub fn find<'a>(fa: &mut HashMap<&'a String, &'a String>, x: &'a String) -> &'a String {
+        if x == fa[x] {
+            return x;
+        }
+        let fd = Solution::find(fa, fa[x]);
+        fa.insert(x, fd);
+        fa[x]
+    }
 
-        words.sort_by(|a, b| a.len().cmp(&b.len()));
-        for word in words {
-            trie.insert(word, &mut best);
+    pub fn union<'a>(fa: &mut HashMap<&'a String, &'a String>, x: &'a String, y: &'a String) {
+        let a = Solution::find(fa, x);
+        let b = Solution::find(fa, y);
+        fa.insert(b, a);
+    }
+
+    pub fn accounts_merge(accounts: Vec<Vec<String>>) -> Vec<Vec<String>> {
+        let mut fa = HashMap::new();
+        let mut email_name = HashMap::new();
+        for acc in accounts.iter() {
+            let name = &acc[0];
+            for i in 1..acc.len() {
+                fa.insert(&acc[i], &acc[i]);
+                email_name.entry(&acc[i]).or_insert(name);
+            }
+        }
+        for acc in &accounts {
+            for i in 2..acc.len() {
+                Solution::union(&mut fa, &acc[1], &acc[i]);
+            }
+        }
+        let mut name_emails = HashMap::new();
+        for em in email_name.keys() {
+            name_emails
+                .entry(Solution::find(&mut fa, *em))
+                .or_insert(Vec::new())
+                .push(*em);
+        }
+        for v in name_emails.values_mut() {
+            v.sort();
         }
 
-        best
+        let mut res = Vec::new();
+        for fa_email in name_emails.keys() {
+            let mut data = Vec::new();
+            data.push(email_name[*fa_email].to_string());
+            for t in name_emails.get(fa_email).unwrap().iter() {
+                data.push(t.to_string());
+            }
+            res.push(data);
+        }
+        res
     }
 }
 
