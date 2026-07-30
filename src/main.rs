6912eab47faa13fcc08c3fefc5853fd11151f385
mspark777 <topangel777@gmail.com>
2026-07-30 13:51:37 +0900

    743

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
 struct Solution;
 
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 impl Solution {
-    pub fn delete_and_earn(nums: Vec<i32>) -> i32 {
-        let mut sum = vec![0; 10001];
-        let mut dp = vec![0; 10001];
-
-        for num in nums {
-            sum[num as usize] += num;
+    pub fn network_delay_time(times: Vec<Vec<i32>>, n: i32, k: i32) -> i32 {
+        let n = n as usize;
+        let k = k as usize;
+        let mut graph = vec![Vec::<(usize, i32)>::new(); n + 1];
+        for time in times {
+            let u = time[0];
+            let v = time[1];
+            let w = time[2];
+            graph[u as usize].push((v as usize, time[2]));
         }
 
-        dp[1] = sum[1];
+        let mut queue = BinaryHeap::<Reverse<(i32, usize)>>::new();
+        let mut delays = vec![None as Option<i32>; n + 1];
+        let mut visited = 0;
 
-        for i in 2..10001 {
-            dp[i] = i32::max(sum[i] + dp[i - 2], dp[i - 1])
+        queue.push(Reverse((0, k)));
+
+        while let Some(Reverse((delay, node))) = queue.pop() {
+            match delays[node] {
+                None => {
+                    delays[node] = Some(delay);
+                    visited += 1;
+                    if visited == n {
+                        break;
+                    }
+                }
+
+                Some(d) if d <= delay => {
+                    continue;
+                }
+                _ => {
+                    delays[node] = Some(delay);
+                }
+            }
+
+            for (edge, weight) in graph[node].iter().copied() {
+                if delays[edge].is_none() || delays[edge] > Some(delay + weight) {
+                    queue.push(Reverse((delay + weight, edge)));
+                }
+            }
         }
 
-        dp[10001 - 1]
+        if visited != n {
+            return -1;
+        }
+
+        delays
+            .into_iter()
+            .skip(1)
+            .map(|x| x.unwrap_or_default())
+            .max()
+            .unwrap_or_default()
     }
 }
 
