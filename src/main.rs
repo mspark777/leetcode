4d5ad566ad2be90fcced9a15c8ee55d98eebb9de
mspark777 <topangel777@gmail.com>
2026-08-02 09:48:09 +0900

    779

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,24 @@
 struct Solution;
 
 impl Solution {
-    pub fn can_transform(start: String, result: String) -> bool {
-        let mut answer = false;
-        let mut l = 0;
-        let mut r = 0;
-        let mut f = true;
-        for (cur, target) in start.chars().zip(result.chars()) {
-            match cur {
-                'R' if l > 0 => f = false,
-                'R' => r += 1,
-                _ => (),
-            };
-
-            match target {
-                'L' if r > 0 => f = false,
-                'L' => l += 1,
-                'R' if r == 0 => f = false,
-                'R' => r -= 1,
-                _ => (),
-            };
-
-            match cur {
-                'L' if l == 0 => f = false,
-                'L' => l -= 1,
-                _ => (),
-            };
-
-            if f {
-                answer = (l == 0) && (r == 0);
-            } else {
-                answer = false;
-            }
+    pub fn kth_grammar(_n: i32, k: i32) -> i32 {
+        match (k - 1).count_ones() & 1 {
+            0 => 0,
+            _ => 1,
         }
-
-        answer
     }
 }
 
 struct Input {
-    start: String,
-    result: String,
+    n: i32,
+    k: i32,
 }
 
 fn main() {
-    let inputs = [Input {
-        start: "RXXLRXRXL".to_string(),
-        result: "XRLXXRRLX".to_string(),
-    }];
+    let inputs = [Input { n: 1, k: 1 }];
 
     for input in inputs.into_iter() {
-        let result = Solution::can_transform(input.start, input.result);
+        let result = Solution::kth_grammar(input.n, input.k);
         println!("{:?}", result);
     }
 }
