e7cc2a7d83acc8819ebb36969952f2bfc3c915a2
mspark777 <topangel777@gmail.com>
2026-08-16 16:28:48 +0900

    842

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,54 @@
 struct Solution;
 
 impl Solution {
-    pub fn find_replace_string(
-        s: String,
-        indices: Vec<i32>,
-        sources: Vec<String>,
-        targets: Vec<String>,
-    ) -> String {
-        let mut result = String::new();
-        let chars = s.chars().collect::<Vec<char>>();
-        let mut marks = vec![-1; chars.len()];
-        for (idx, i) in indices.iter().enumerate() {
-            let source = sources[idx].chars().collect::<Vec<char>>();
-            let e = *i as usize + source.len();
-            if e > chars.len() {
-                continue;
-            }
-            if source == &chars[*i as usize..e] {
-                for idx2 in *i as usize..e {
-                    marks[idx2] = idx as i32;
-                }
-            }
-        }
-
-        let mut idx = 0;
-
-        while idx < marks.len() {
-            if marks[idx] == -1 {
-                result.push(chars[idx]);
-                idx += 1;
-            } else {
-                let prev = marks[idx];
-                while idx < marks.len() - 1 {
-                    if prev != marks[idx + 1] {
-                        break;
-                    } else {
-                        idx += 1;
-                    }
-                }
-
-                result += targets[prev as usize].as_str();
-                idx += 1;
-            }
-        }
+    pub fn split_into_fibonacci(num: String) -> Vec<i32> {
+        let mut result = Vec::<i32>::new();
+        Self::fun(&mut result, &num, 0);
         result
+    }
+
+    fn fun(result: &mut Vec<i32>, num: &str, ind: usize) -> Option<bool> {
+        if ind == num.len() {
+            return Some(result.len() > 2);
+        }
+
+        let mut val = 0i64;
+        for i in ind..num.len() {
+            val = val * 10 + (num.chars().nth(i)? as u8 - b'0') as i64;
+            if val > i32::MAX as i64 {
+                return Some(false);
+            }
+            let y = match result.len() < 2 {
+                true => 0,
+                _ => result[result.len() - 2] + result[result.len() - 1],
+            };
+
+            if result.len() < 2 || y == val as i32 {
+                result.push(val as i32);
+                if Self::fun(result, num, i + 1)? {
+                    return Some(true);
+                }
+                result.pop();
+            }
+            if i == ind && num.chars().nth(i)? == '0' {
+                return Some(false);
+            }
+        }
+        Some(false)
     }
 }
 
 struct Input {
-    fronts: Vec<i32>,
-    backs: Vec<i32>,
+    num: String,
 }
 
 fn main() {
     let inputs = [Input {
-        s: "(123)".to_string(),
+        num: "1101111".to_string(),
     }];
 
     for input in inputs {
-        let result = Solution::ambiguous_coordinates(input.s);
+        let result = Solution::split_into_fibonacci(input.num);
         println!("{:?}", result);
     }
 }
