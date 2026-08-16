c74baa4cb451d3e410635616e5db4c4e24b095d7
mspark777 <topangel777@gmail.com>
2026-08-16 16:03:31 +0900

    833

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
 struct Solution;
 
 impl Solution {
-    pub fn flipgame(fronts: Vec<i32>, backs: Vec<i32>) -> i32 {
-        use std::collections::HashSet;
+    pub fn find_replace_string(
+        s: String,
+        indices: Vec<i32>,
+        sources: Vec<String>,
+        targets: Vec<String>,
+    ) -> String {
+        let mut result = String::new();
+        let chars = s.chars().collect::<Vec<char>>();
+        let mut marks = vec![-1; chars.len()];
+        for (idx, i) in indices.iter().enumerate() {
+            let source = sources[idx].chars().collect::<Vec<char>>();
+            let e = *i as usize + source.len();
+            if e > chars.len() {
+                continue;
+            }
+            if source == &chars[*i as usize..e] {
+                for idx2 in *i as usize..e {
+                    marks[idx2] = idx as i32;
+                }
+            }
+        }
 
-        let blacklist = fronts
-            .iter()
-            .copied()
-            .zip(backs.iter().copied())
-            .filter(|(f, b)| f == b)
-            .map(|v| v.0)
-            .collect::<HashSet<i32>>();
+        let mut idx = 0;
 
-        fronts
-            .into_iter()
-            .chain(backs)
-            .filter(|v| !blacklist.contains(v))
-            .min()
-            .unwrap_or_default()
+        while idx < marks.len() {
+            if marks[idx] == -1 {
+                result.push(chars[idx]);
+                idx += 1;
+            } else {
+                let prev = marks[idx];
+                while idx < marks.len() - 1 {
+                    if prev != marks[idx + 1] {
+                        break;
+                    } else {
+                        idx += 1;
+                    }
+                }
+
+                result += targets[prev as usize].as_str();
+                idx += 1;
+            }
+        }
+        result
     }
 }
 
