1a9ce156ecced321b662988c9fa883ee243b48b0
mspark777 <topangel777@gmail.com>
2026-08-19 08:56:07 +0900

    865

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
 struct Solution;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 impl Solution {
-    pub fn score_of_parentheses(s: String) -> i32 {
-        let mut stack = vec![0; 1];
-        for c in s.chars() {
-            match c {
-                '(' => stack.push(0),
-                ')' => {
-                    let popped = stack.pop();
-                    let last = stack.last_mut();
-                    if let (Some(popped), Some(last)) = (popped, last) {
-                        *last += 1.max(popped * 2);
-                    }
+    pub fn subtree_with_all_deepest(
+        root: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::dfs(&root).1
+    }
+    pub fn dfs(node: &Option<Rc<RefCell<TreeNode>>>) -> (i32, Option<Rc<RefCell<TreeNode>>>) {
+        match node {
+            None => (0, None),
+            Some(n) => {
+                let n_ref = n.borrow();
+                let (r_deepth, r_ref) = Self::dfs(&n_ref.left);
+                let (l_deepth, l_ref) = Self::dfs(&n_ref.right);
+                if r_deepth > l_deepth {
+                    (r_deepth + 1, r_ref)
+                } else if r_deepth < l_deepth {
+                    (l_deepth + 1, l_ref)
+                } else {
+                    (l_deepth + 1, node.clone())
                 }
-                _ => unreachable!(),
             }
         }
-        stack[0]
     }
 }
 
