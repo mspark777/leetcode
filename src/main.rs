cfa10d829f21250c0e2e7c067b2f3b137312e4d5
mspark777 <topangel777@gmail.com>
2026-08-18 09:57:32 +0900

    853

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,47 @@
 struct Solution;
 
-use std::collections::HashSet;
 impl Solution {
-    pub fn loud_and_rich(richer: Vec<Vec<i32>>, quiet: Vec<i32>) -> Vec<i32> {
-        let mut result: Vec<i32> = vec![];
-        let mut graph: Vec<Vec<i32>> = vec![vec![]; quiet.len()];
+    pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {
+        let mut v = Vec::<(i32, i32)>::from_iter(position.into_iter().zip(speed));
+        v.sort_by_key(|a| a.0);
 
-        for edge in richer {
-            graph[edge[1] as usize].push(edge[0])
-        }
-
-        for item in 0..quiet.len() {
-            let mut stack = vec![item];
-            let mut visited = HashSet::<usize>::new();
-            let mut louder = item;
-            let mut min = quiet[item];
-
-            while let Some(item) = stack.pop() {
-                if visited.contains(&item) {
-                    continue;
+        let mut result = 0;
+        v.iter()
+            .map(|&(p, s)| {
+                let target = target as f32;
+                let p = p as f32;
+                let s = s as f32;
+                (target - p) / s
+            })
+            .rev()
+            .fold(f32::MIN, |cur, t| {
+                if t > cur {
+                    result += 1;
+                    t
+                } else {
+                    cur
                 }
-
-                visited.insert(item);
-                if quiet[item] < min {
-                    min = quiet[item];
-                    louder = item;
-                }
-
-                for dest_item in graph[item].iter().copied() {
-                    stack.push(dest_item as usize);
-                }
-            }
-
-            result.push(louder as i32);
-        }
+            });
 
         result
     }
 }
 
 struct Input {
-    richer: Vec<Vec<i32>>,
-    quiet: Vec<i32>,
+    target: i32,
+    position: Vec<i32>,
+    speed: Vec<i32>,
 }
 
 fn main() {
     let inputs = [Input {
-        richer: [[1, 0], [2, 1], [3, 1], [3, 7], [4, 3], [5, 3], [6, 3]]
-            .map(|v| v.to_vec())
-            .to_vec(),
-        quiet: [3, 2, 5, 4, 6, 1, 7, 0].to_vec(),
+        target: 12,
+        position: [10, 8, 0, 5, 3].to_vec(),
+        speed: [2, 4, 1, 1, 3].to_vec(),
     }];
 
     for input in inputs {
-        let result = Solution::loud_and_rich(input.richer, input.quiet);
+        let result = Solution::car_fleet(input.target, input.position, input.speed);
         println!("{:?}", result);
     }
 }
