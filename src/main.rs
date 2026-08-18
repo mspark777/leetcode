d24c084da68a4dfae1a477b93ee7dd91a979138c
mspark777 <topangel777@gmail.com>
2026-08-18 09:39:51 +0900

    849

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
 struct Solution;
 
 impl Solution {
-    pub fn shifting_letters(s: String, shifts: Vec<i32>) -> String {
-        let mut result = String::new();
-        let mut shift = shifts.iter().copied().fold(0, |acc, cur| (acc + cur) % 26);
+    pub fn max_dist_to_closest(seats: Vec<i32>) -> i32 {
+        let n = seats.len();
+        let end = n + 1;
+        let mut rslt = 0usize;
+        let mut start = end;
 
-        for (i, ch) in s.char_indices() {
-            const A: i32 = b'a' as i32;
-            let idx = (ch as i32) - A;
-            let shifted = A + ((idx + shift) % 26);
-
-            result.push(shifted as u8 as char);
-            shift = (shift - shifts[i]).rem_euclid(26);
+        for (i, seat) in seats.into_iter().enumerate() {
+            if seat == 1 {
+                if start == end {
+                    rslt = i;
+                } else {
+                    rslt = rslt.max((i - start) / 2);
+                }
+                start = i;
+            }
         }
 
-        result
+        rslt.max(n - 1 - start) as i32
     }
 }
 
 struct Input {
-    s: String,
-    shifts: Vec<i32>,
+    seats: Vec<i32>,
 }
 
 fn main() {
     let inputs = [Input {
-        s: "bad".to_string(),
-        shifts: [10, 20, 30].to_vec(),
+        seats: [1, 0, 0, 0, 1, 0, 1].to_vec(),
     }];
 
     for input in inputs {
-        let result = Solution::shifting_letters(input.s, input.shifts);
+        let result = Solution::max_dist_to_closest(input.seats);
         println!("{:?}", result);
     }
 }
