22f9e4e16495ec42826e9d382d1fa42fa811b1b0
mspark777 <topangel777@gmail.com>
2026-08-21 09:57:38 +0900

    893

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 struct Solution;
 
 impl Solution {
-    pub fn stone_game(_piles: Vec<i32>) -> bool {
-        true
+    pub fn num_special_equiv_groups(words: Vec<String>) -> i32 {
+        use std::collections::HashSet;
+
+        let mut seen = HashSet::<String>::new();
+        for word in words {
+            let mut counts = [0; 52];
+            for (i, ch) in word.char_indices() {
+                let code = ch as usize;
+                const A: usize = 'a' as usize;
+                let idx = code - A + (26 * (i & 1));
+                counts[idx] += 1;
+            }
+
+            seen.insert(format!("{:?}", counts));
+        }
+
+        seen.len() as i32
     }
 }
 
 struct Input {
-    piles: Vec<i32>,
+    words: Vec<String>,
 }
 
 fn main() {
     let inputs = [Input {
-        piles: [5, 3, 4, 5].to_vec(),
+        words: ["abcd", "cdab", "cbad", "xyzz", "zzxy", "zzyx"]
+            .map(|v| v.to_string())
+            .to_vec(),
     }];
 
     for input in inputs {
-        let result = Solution::stone_game(input.piles);
+        let result = Solution::num_special_equiv_groups(input.words);
         println!("{:?}", result);
     }
 }
