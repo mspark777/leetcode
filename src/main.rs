43153ba4550f3c947eb2da5e6f9b98a3d22b2f9d
mspark777 <topangel777@gmail.com>
2026-08-21 10:09:49 +0900

    894

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,65 @@
 struct Solution;
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+
 impl Solution {
-    pub fn num_special_equiv_groups(words: Vec<String>) -> i32 {
-        use std::collections::HashSet;
-
-        let mut seen = HashSet::<String>::new();
-        for word in words {
-            let mut counts = [0; 52];
-            for (i, ch) in word.char_indices() {
-                let code = ch as usize;
-                const A: usize = 'a' as usize;
-                let idx = code - A + (26 * (i & 1));
-                counts[idx] += 1;
-            }
-
-            seen.insert(format!("{:?}", counts));
+    pub fn generate(
+        node_cnt: i32,
+        memo: &mut HashMap<i32, Vec<Option<Rc<RefCell<TreeNode>>>>>,
+    ) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+        if let Some(node) = memo.get(&node_cnt) {
+            return node.clone();
         }
 
-        seen.len() as i32
+        let mut result = Vec::<Option<Rc<RefCell<TreeNode>>>>::new();
+
+        for left_node_cnt in 0..node_cnt {
+            let right_node_cnt = node_cnt - left_node_cnt - 1;
+
+            let left_tree = Self::generate(left_node_cnt, memo);
+            let right_tree = Self::generate(right_node_cnt, memo);
+
+            for st1 in left_tree.iter() {
+                for st2 in right_tree.iter() {
+                    let curr_node = Some(Rc::new(RefCell::new(TreeNode {
+                        val: 0,
+                        left: st1.clone(),
+                        right: st2.clone(),
+                    })));
+                    result.push(curr_node);
+                }
+            }
+        }
+        memo.insert(node_cnt, result.clone());
+        result
+    }
+
+    pub fn all_possible_fbt(n: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+        let mut memo = HashMap::new();
+        memo.insert(0, vec![]);
+        memo.insert(1, vec![Some(Rc::new(RefCell::new(TreeNode::new(0))))]);
+
+        Self::generate(n, &mut memo)
     }
 }
 
