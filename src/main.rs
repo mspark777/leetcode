153d835ecb138c4dab26ade4e8d31ec9f192f511
mspark777 <topangel777@gmail.com>
2026-08-22 11:40:57 +0900

    911

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,35 @@ impl Solution {
     }
 }
 
+struct TopVotedCandidate {
+    ld: Vec<i32>,
+    ts: Vec<i32>,
+}
+
+impl TopVotedCandidate {
+    fn new(persons: Vec<i32>, times: Vec<i32>) -> Self {
+        use std::collections::HashMap;
+
+        let mut ld = Vec::with_capacity(persons.len());
+        let mut m = HashMap::new();
+        let mut b = (0, 0, 0);
+        for (p, t) in persons.iter().copied().zip(times.iter().copied()) {
+            let k = m.get(&p).map(|x: &(i32, i32)| x.0).unwrap_or(0);
+            m.insert(p, (k + 1, t));
+            b = b.max((k + 1, t, p));
+            ld.push(b.2);
+        }
+        Self {
+            ld,
+            ts: times.clone(),
+        }
+    }
+
+    fn q(&self, tf: i32) -> i32 {
+        self.ld[self.ts.binary_search(&tf).unwrap_or_else(|err| err - 1)]
+    }
+}
+
 struct Input {
     arr: Vec<i32>,
 }
