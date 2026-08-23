92b3c22a1c8c90eb67ac8d1c7ce0a66f28ff88dc
mspark777 <topangel777@gmail.com>
2026-08-23 12:12:43 +0900

    923

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_add_to_make_valid(s: String) -> i32 {
-        let mut open_c = 0;
-        let mut close_c = 0;
+    pub fn three_sum_multi(arr: Vec<i32>, target: i32) -> i32 {
+        use std::collections::HashMap;
 
-        for c in s.chars() {
-            if c == '(' {
-                open_c += 1;
-            } else if c == ')' && open_c > 0 {
-                open_c -= 1;
-            } else {
-                close_c += 1;
+        let mut single = HashMap::<i32, i32>::new();
+        let mut pairs = HashMap::<i32, i32>::new();
+        const MOD: i32 = 1_000_000_007;
+        let mut result = 0;
+
+        for x in arr {
+            result =
+                (result + pairs.get(&(target - x)).copied().unwrap_or_default()).rem_euclid(MOD);
+
+            for (&y, &k) in single.iter() {
+                pairs
+                    .entry(x + y)
+                    .and_modify(|n| *n = (*n + k).rem_euclid(MOD))
+                    .or_insert(k.rem_euclid(MOD));
             }
+
+            single.entry(x).and_modify(|n| *n += 1).or_insert(1);
         }
-        open_c + close_c
+        result
     }
 }
 
