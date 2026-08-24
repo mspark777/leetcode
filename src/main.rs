497312ca2703ff2db02c9e9b904527b26b49e410
mspark777 <topangel777@gmail.com>
2026-08-24 10:13:00 +0900

    932

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
 struct Solution;
 
 impl Solution {
-    pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
-        let n = nums.len();
-        let mut result = 0;
-        let mut sum = 0;
-        let mut cnt = vec![0; n + 1];
-        cnt[0] = 1;
-        for x in nums {
-            if sum + x - goal >= 0 {
-                result += cnt[(sum + x - goal) as usize];
-            }
-            sum += x;
-            cnt[sum as usize] += 1;
+    pub fn beautiful_array(n: i32) -> Vec<i32> {
+        let n = n as usize;
+        let mut result = vec![1];
+        while result.len() < n {
+            result = result
+                .iter()
+                .copied()
+                .map(|m| m * 2 - 1)
+                .chain(result.iter().copied().map(|m| m * 2))
+                .filter(|&m| m <= (n as i32))
+                .collect();
         }
         result
     }
 }
 
 struct Input {
-    nums: Vec<i32>,
-    goal: i32,
+    n: i32,
 }
 
 fn main() {
-    let inputs = [Input {
-        nums: [1, 0, 1, 0, 1].to_vec(),
-        goal: 2,
-    }];
+    let inputs = [Input { n: 4 }];
 
     for input in inputs {
-        let result = Solution::num_subarrays_with_sum(input.nums, input.goal);
+        let result = Solution::beautiful_array(input.n);
         println!("{:?}", result);
     }
 }
