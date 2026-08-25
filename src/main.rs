686c1b949dd0b1b108ec6389faf36a07dfe9b0f8
mspark777 <topangel777@gmail.com>
2026-08-25 10:48:27 +0900

    937

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,62 @@
 struct Solution;
 
 impl Solution {
-    pub fn shortest_bridge(grid: Vec<Vec<i32>>) -> i32 {
-        let n = grid.len();
+    pub fn reorder_log_files(logs: Vec<String>) -> Vec<String> {
+        use std::cmp::Ordering;
 
-        let mut group: Vec<Vec<i32>> = vec![vec![-1; n]; n];
-        let mut group_ind = 0;
+        let mut logs = logs;
 
-        for i in 0..n {
-            for j in 0..n {
-                if group[i][j] == -1 && grid[i][j] == 1 {
-                    Solution::dfs(&grid, (i, j), &mut group, group_ind);
-                    group_ind += 1;
-                }
+        logs.sort_by(|a, b| {
+            let a_split = a.find(' ').unwrap();
+            let b_split = b.find(' ').unwrap();
+
+            let (a_id, a_log) = a.split_at(a_split);
+            let (b_id, b_log) = b.split_at(b_split);
+
+            let a_is_digit = Self::is_digit_log(a);
+            let b_is_digit = Self::is_digit_log(b);
+
+            match (a_is_digit, b_is_digit) {
+                (true, true) => Ordering::Equal,
+                (false, false) => a_log.cmp(b_log).then_with(|| a_id.cmp(b_id)),
+                (true, false) => Ordering::Greater,
+                (false, true) => Ordering::Less,
             }
-        }
+        });
 
-        let groups = [0, 1]
-            .iter()
-            .map(|group_ind| {
-                group
-                    .iter()
-                    .enumerate()
-                    .flat_map(|(i, v)| {
-                        v.iter()
-                            .enumerate()
-                            .filter(|(_, g)| g == &group_ind)
-                            .map(|(j, _)| (i as i32, j as i32))
-                            .collect::<Vec<_>>()
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-
-        let first_group = &groups[0];
-        let second_group = &groups[1];
-
-        let mut result = i32::MAX;
-
-        for (fi, fj) in first_group {
-            for (si, sj) in second_group {
-                result = result.min((fi - si).abs() + (fj - sj).abs() - 1);
-            }
-        }
-
-        result
+        logs
     }
 
-    fn dfs(grid: &Vec<Vec<i32>>, cur: (usize, usize), group: &mut Vec<Vec<i32>>, group_ind: i32) {
-        let n = grid.len() as i32;
-        group[cur.0][cur.1] = group_ind;
-
-        for (di, dj) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-            let to_i = cur.0 as i32 + di;
-            let to_j = cur.1 as i32 + dj;
-
-            if to_i >= 0 && to_i < n && to_j >= 0 && to_j < n {
-                let to_i = to_i as usize;
-                let to_j = to_j as usize;
-
-                if group[to_i][to_j] == -1 && grid[to_i][to_j] == 1 {
-                    Solution::dfs(grid, (to_i, to_j), group, group_ind);
-                }
-            }
-        }
+    fn is_digit_log(s: &str) -> bool {
+        s.split_whitespace()
+            .nth(1)
+            .unwrap()
+            .chars()
+            .next()
+            .unwrap()
+            .is_ascii_digit()
     }
 }
 
 struct Input {
-    grid: Vec<Vec<i32>>,
+    logs: Vec<String>,
 }
 
 fn main() {
     let inputs = [Input {
-        grid: [
-            [1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1],
+        logs: [
+            "dig1 8 1 5 1",
+            "let1 art can",
+            "dig2 3 6",
+            "let2 own kit dig",
+            "let3 art zero",
         ]
-        .map(|v| v.to_vec())
+        .map(|v| v.to_string())
         .to_vec(),
     }];
 
     for input in inputs {
-        let result = Solution::shortest_bridge(input.grid);
+        let result = Solution::reorder_log_files(input.logs);
         println!("{:?}", result);
     }
 }
