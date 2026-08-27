4c14f2f4a87f884ecf61cf70399f835aa183729e
mspark777 <topangel777@gmail.com>
2026-08-27 13:47:37 +0900

    966

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,44 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_area_free_rect(points: Vec<Vec<i32>>) -> f64 {
-        let points = points
-            .iter()
-            .map(|v| v.iter().map(|&x| x as i64).collect::<Vec<_>>())
-            .collect::<Vec<_>>();
-        let mut res = 0;
-        let mut m = std::collections::HashMap::new();
-        for i in 0..points.len() {
-            for j in i + 1..points.len() {
-                let pt_i_0 = points[i][0];
-                let pt_i_1 = points[i][1];
-                let pt_j_0 = points[j][0];
-                let pt_j_1 = points[j][1];
-                let center = (((pt_i_0 + pt_j_0) as u64) << 16) + (pt_i_1 + pt_j_1) as u64;
-                let v = vec![pt_i_0, pt_i_1, pt_j_0, pt_j_1];
-                m.entry(center).or_insert_with(Vec::new).push(v);
-            }
-        }
-        for (_center, points) in m {
-            for i in 0..points.len() {
-                for j in i + 1..points.len() {
-                    let p1 = &points[i];
-                    let p2 = &points[j];
-                    if (p1[0] - p2[0]) * (p1[0] - p2[2]) + (p1[1] - p2[1]) * (p1[1] - p2[3]) == 0 {
-                        let area = Self::d2(p1[0], p1[1], p2[0], p2[1])
-                            * Self::d2(p1[0], p1[1], p2[2], p2[3]);
-                        if res == 0 || res > area {
-                            res = area;
-                        }
-                    }
+    pub fn spellchecker(wordlist: Vec<String>, queries: Vec<String>) -> Vec<String> {
+        use std::collections::{HashMap, HashSet};
+
+        let (em, lc, vs) = wordlist.into_iter().rev().fold(
+            (HashSet::new(), HashMap::new(), HashMap::new()),
+            |(mut em, mut lc, mut vs), w| {
+                let l = w.to_lowercase();
+                em.insert(w.clone());
+                vs.insert(Self::rv(&l), w.clone());
+                lc.insert(l, w);
+                (em, lc, vs)
+            },
+        );
+        queries
+            .into_iter()
+            .map(|q| {
+                if em.contains(&q) {
+                    return q;
                 }
-            }
-        }
-        (res as f64).sqrt()
+                let ql = q.to_lowercase();
+                if let Some(w) = lc.get(&ql) {
+                    return w.to_owned();
+                }
+                if let Some(w) = vs.get(&Self::rv(&ql)) {
+                    return w.to_owned();
+                }
+                String::new()
+            })
+            .collect()
     }
 
-    fn d2(x1: i64, y1: i64, x2: i64, y2: i64) -> i64 {
-        (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+    fn rv(s: &String) -> String {
+        s.chars()
+            .map(|c| match "eiou".contains(c) {
+                true => 'a',
+                false => c,
+            })
+            .collect()
     }
 }
 
