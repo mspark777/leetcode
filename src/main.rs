6cfe63badabc89d8f079be2f58526f7b55416e7f
mspark777 <topangel777@gmail.com>
2026-08-28 11:47:07 +0900

    978

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 struct Solution;
 
 impl Solution {
-    pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
-        use std::collections::BinaryHeap;
-
-        let k = k as usize;
-        let mut pq = BinaryHeap::with_capacity(k);
-        for p in points {
-            let d = p[0] * p[0] + p[1] * p[1];
-            pq.push((d, vec![p[0], p[1]]));
-            if pq.len() > k {
-                pq.pop();
-            }
+    pub fn max_turbulence_size(arr: Vec<i32>) -> i32 {
+        if arr.len() < 2 {
+            return arr.len() as i32;
         }
 
-        pq.into_iter().map(|(_, p)| p).collect()
+        let n = arr.len();
+        let last = n - 1;
+        let mut best = 1;
+        let mut i = 0;
+        while i < last {
+            if arr[i + 1] != arr[i] {
+                best = best.max(2)
+            }
+
+            let mut j = i + 1;
+            while j < last {
+                j += 1;
+
+                let [a, b, c] = arr[(j - 2)..=j] else {
+                    panic!()
+                };
+
+                if (a < b && b > c) || (a > b && b < c) {
+                    best = best.max(j - i + 1)
+                } else {
+                    i = j - 2;
+                    break;
+                }
+            }
+            i += 1
+        }
+
+        best as i32
     }
 }
 
