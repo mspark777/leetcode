928bfa218294808cfdf3133e15f5a700f464d297
mspark777 <topangel777@gmail.com>
2026-08-28 11:52:22 +0900

    991

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,26 @@
 struct Solution;
 
 impl Solution {
-    pub fn interval_intersection(
-        first_list: Vec<Vec<i32>>,
-        second_list: Vec<Vec<i32>>,
-    ) -> Vec<Vec<i32>> {
-        let mut ans = Vec::<Vec<i32>>::new();
-        let mut first = 0usize;
-        let mut second = 0usize;
-
-        while (first < first_list.len()) && (second < second_list.len()) {
-            let begin = first_list[first][0].max(second_list[second][0]);
-            let end = first_list[first][1].min(second_list[second][1]);
-
-            if begin <= end {
-                ans.push(vec![begin, end]);
-            }
-
-            if second_list[second][1] > first_list[first][1] {
-                first += 1;
-            } else {
-                second += 1;
-            }
-        }
-        ans
+    pub fn broken_calc(start_value: i32, target: i32) -> i32 {
+        let m = 32 - ((target - 1) / start_value).leading_zeros() as i32;
+        let diff = (start_value << m) - target;
+        (diff >> m) + (diff & ((1 << m) - 1)).count_ones() as i32 + m
     }
 }
 
 struct Input {
-    x: i32,
-    y: i32,
-    bound: i32,
+    start_value: i32,
+    target: i32,
 }
 
 fn main() {
     let inputs = [Input {
-        x: 2,
-        y: 3,
-        bound: 10,
+        start_value: 2,
+        target: 3,
     }];
 
     for input in inputs {
-        let result = Solution::powerful_integers(input.x, input.y, input.bound);
+        let result = Solution::broken_calc(input.start_value, input.target);
         println!("{:?}", result);
     }
 }
