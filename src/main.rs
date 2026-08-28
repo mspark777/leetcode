00ec0ae93d3c3c4c917b6f17d122d7e93b3ce5ae
mspark777 <topangel777@gmail.com>
2026-08-28 11:43:26 +0900

    971

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
 struct Solution;
 
-impl Solution {
-    pub fn powerful_integers(x: i32, y: i32, bound: i32) -> Vec<i32> {
-        use std::collections::HashSet;
-        let mut ans = HashSet::new();
-        let mut row = 1;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
 
-        while row < bound {
-            let mut col = 1;
-
-            while row + col <= bound {
-                ans.insert(row + col);
-                if y == 1 {
-                    break;
-                };
-                col *= y;
-            }
-
-            if x == 1 {
-                break;
-            };
-            row *= x;
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
         }
+    }
+}
 
-        ans.into_iter().collect()
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn flip_match_voyage(root: Option<Rc<RefCell<TreeNode>>>, voyage: Vec<i32>) -> Vec<i32> {
+        let root = match root {
+            Some(a) => a,
+            None => return vec![-1],
+        };
+
+        let mut res = vec![];
+        let mut stack = vec![root];
+
+        for cur in 0..voyage.len() {
+            let node = stack.pop().unwrap();
+            if node.borrow().val != voyage[cur] {
+                return vec![-1];
+            }
+            match (&node.borrow().left, &node.borrow().right) {
+                (Some(l), Some(r)) => {
+                    if l.borrow().val == voyage[cur + 1] {
+                        stack.push(r.clone());
+                        stack.push(l.clone());
+                    } else {
+                        stack.push(l.clone());
+                        stack.push(r.clone());
+                        res.push(node.borrow().val);
+                    }
+                }
+                (Some(a), _) | (_, Some(a)) => {
+                    stack.push(a.clone());
+                }
+                _ => (),
+            };
+        }
+        res
     }
 }
 
