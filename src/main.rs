860889d50d7bafa1f1bf7110c4c0036869e4b087
mspark777 <topangel777@gmail.com>
2026-08-29 13:39:10 +0900

    1010

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,20 @@
 struct Solution;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
-
-use std::cell::RefCell;
-use std::rc::Rc;
-type TypeNode = Option<Rc<RefCell<TreeNode>>>;
 impl Solution {
-    pub fn new_node(val: i32, left: TypeNode, right: TypeNode) -> TypeNode {
-        let mut tnode = TreeNode::new(val);
-        tnode.left = left;
-        tnode.right = right;
-        Some(Rc::new(RefCell::new(tnode)))
-    }
+    pub fn num_pairs_divisible_by60(time: Vec<i32>) -> i32 {
+        let mut freq = [0; 61];
+        let mut res = 0;
 
-    pub fn bst_from_preorder(preorder: Vec<i32>) -> TypeNode {
-        Self::dfs(&preorder[..])
-    }
-
-    fn dfs(p: &[i32]) -> TypeNode {
-        if p.len() == 0 {
-            return None;
+        for tim in time {
+            let mut t = (tim % 60) as usize;
+            res += freq[60 - t];
+            if t == 0 {
+                t = 60;
+            }
+            freq[t] += 1;
         }
 
-        if p.len() == 1 {
-            return Self::new_node(p[0], None, None);
-        }
-        let j = match p[1..].binary_search(&p[0]) {
-            Ok(idx) => idx + 1,
-            Err(idx) => idx + 1,
-        };
-
-        let left = Self::dfs(&p[1..j]);
-        let right = Self::dfs(&p[j..]);
-        Self::new_node(p[0], left, right)
+        res
     }
 }
 
