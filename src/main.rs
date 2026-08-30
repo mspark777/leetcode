f31ba9b7053afa050be2178ead39ae7448177306
mspark777 <topangel777@gmail.com>
2026-08-30 14:26:29 +0900

    1023

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,24 @@
 struct Solution;
 
 impl Solution {
-    pub fn num_enclaves(mut grid: Vec<Vec<i32>>) -> i32 {
-        let m = grid.len();
-        let n = grid[0].len();
-
-        for i in 0..m {
-            if grid[i][0] == 1 {
-                Self::dfs(&mut grid, i, 0);
-            }
-            if grid[i][n - 1] == 1 {
-                Self::dfs(&mut grid, i, n - 1);
-            }
-        }
-        for j in 0..n {
-            if grid[0][j] == 1 {
-                Self::dfs(&mut grid, 0, j);
-            }
-            if grid[m - 1][j] == 1 {
-                Self::dfs(&mut grid, m - 1, j);
-            }
-        }
-
-        grid.iter().map(|row| row.iter().sum::<i32>()).sum()
+    pub fn camel_match(queries: Vec<String>, pattern: String) -> Vec<bool> {
+        queries
+            .into_iter()
+            .map(|query| Self::matches(query.as_str(), pattern.as_str()))
+            .collect()
     }
 
-    fn dfs(grid: &mut Vec<Vec<i32>>, i: usize, j: usize) {
-        if i >= grid.len() || j >= grid[0].len() || grid[i][j] == 0 {
-            return;
+    fn matches(query: &str, pattern: &str) -> bool {
+        let query = query.chars();
+        let mut pattern = pattern.chars().peekable();
+        for q in query {
+            if Some(&q) == pattern.peek() {
+                pattern.next();
+            } else if q.is_uppercase() {
+                return false;
+            }
         }
-        grid[i][j] = 0;
-        if i > 0 {
-            Self::dfs(grid, i - 1, j);
-        }
-        if i + 1 < grid.len() {
-            Self::dfs(grid, i + 1, j);
-        }
-        if j > 0 {
-            Self::dfs(grid, i, j - 1);
-        }
-        if j + 1 < grid[0].len() {
-            Self::dfs(grid, i, j + 1);
-        }
+        pattern.next().is_none()
     }
 }
 
