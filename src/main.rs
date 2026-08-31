37f62db8f62dd0cf00f370c5e9d9a913b782e7e0
mspark777 <topangel777@gmail.com>
2026-08-31 09:52:36 +0900

    1054

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 struct Solution;
 
 impl Solution {
-    pub fn prev_perm_opt1(mut arr: Vec<i32>) -> Vec<i32> {
-        let n = arr.len();
+    pub fn rearrange_barcodes(barcodes: Vec<i32>) -> Vec<i32> {
+        let mut barcodes = barcodes;
+        let mut pos = 0;
+        let mut m = std::collections::HashMap::<i32, i32>::new();
+        let mut s = std::collections::BTreeSet::<(i32, i32)>::new();
 
-        for idx in (0..n - 1).rev() {
-            if arr[idx] > arr[idx + 1] {
-                let mut right = idx + 1;
-                let mut swap_idx = idx + 1;
-
-                while right < n && arr[right] < arr[idx] {
-                    if arr[right] != arr[swap_idx] {
-                        swap_idx = right;
-                    }
-                    right += 1;
-                }
-
-                arr.swap(swap_idx, idx);
-                break;
-            }
+        for n in barcodes.iter() {
+            *m.entry(*n).and_modify(|n| *n += 1).or_insert(1);
         }
 
-        arr
+        for (&k, &v) in m.iter() {
+            s.insert((v, k));
+        }
+
+        for (v, k) in s.into_iter().rev() {
+            for _ in 0..v {
+                if pos >= barcodes.len() {
+                    pos = 1;
+                }
+                barcodes[pos] = k;
+                pos += 2;
+            }
+        }
+        barcodes
     }
 }
 
