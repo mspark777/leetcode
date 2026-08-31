0341af6f8a2d18066467314de8a456a7bf5b1332
mspark777 <topangel777@gmail.com>
2026-08-31 09:45:55 +0900

    1040

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_score_triangulation(values: Vec<i32>) -> i32 {
-        let n = values.len();
-        let mut dp = vec![vec![0; n]; n];
-        for j in 2..n {
-            for i in (0..j - 1).rev() {
-                dp[i][j] = i32::MAX;
-                for k in i + 1..j {
-                    dp[i][j] =
-                        dp[i][j].min(dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
-                }
+    pub fn num_moves_stones_ii(stones: Vec<i32>) -> Vec<i32> {
+        let n = stones.len();
+        let mut stones = stones;
+        stones.sort();
+
+        let mut i = 0;
+        let mut low = n as i32;
+        let high = std::cmp::max(
+            stones[n - 1] - n as i32 + 2 - stones[1],
+            stones[n - 2] - stones[0] - n as i32 + 2,
+        );
+
+        for j in 0..n {
+            while stones[j] - stones[i] >= n as i32 {
+                i += 1;
+            }
+            if ((j + 2 - i) == n) && (stones[j] + 2 - stones[i]) == (n as i32) {
+                low = low.min(2);
+            } else {
+                low = low.min(n as i32 - (j - i + 1) as i32);
             }
         }
-        dp[0][n - 1]
+        vec![low, high]
     }
 }
 
