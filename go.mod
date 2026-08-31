cb5d720a66af941feb14c9beab620a2f6e8ab8d4
mspark777 <topangel777@gmail.com>
2026-08-31 09:42:13 +0900

    1034

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module mspark777/lettcode
 
-go 1.26.5
+go 1.27.0
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,58 @@
 struct Solution;
 
 impl Solution {
-    pub fn num_moves_stones(a: i32, b: i32, c: i32) -> Vec<i32> {
-        let mut v = [a, b, c];
-        v.sort_unstable();
+    fn is_valid(i: i32, j: i32, n: i32, m: i32) -> bool {
+        i >= 0 && j >= 0 && i < n && j < m
+    }
 
-        let a = v[0];
-        let b = v[1];
-        let c = v[2];
+    pub fn color_border(grid: Vec<Vec<i32>>, row: i32, col: i32, color: i32) -> Vec<Vec<i32>> {
+        let row = row as usize;
+        let col = col as usize;
+        let n = grid.len();
+        let m = grid[0].len();
+        let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
-        if (c - a - 2) == 0 {
-            return vec![0, 0];
-        } else if ((b - a) < 3) || ((c - b) < 3) {
-            return vec![1, c - a - 2];
+        let mut grid = grid;
+        let mut queue = std::collections::VecDeque::new();
+        let mut is_visited = vec![vec![false; m]; n];
+        let mut border_cells = vec![];
+
+        let original_color = grid[row][col];
+        queue.push_back((row, col));
+        is_visited[row][col] = true;
+
+        while let Some((i, j)) = queue.pop_front() {
+            let mut is_border = false;
+            for &(di, dj) in &directions {
+                let ni = i as i32 + di;
+                let nj = j as i32 + dj;
+
+                if !Self::is_valid(ni, nj, n as i32, m as i32) {
+                    is_border = true;
+                    continue;
+                }
+
+                let ni = ni as usize;
+                let nj = nj as usize;
+
+                if grid[ni][nj] != original_color {
+                    is_border = true;
+                } else if !is_visited[ni][nj] {
+                    is_visited[ni][nj] = true;
+                    queue.push_back((ni, nj));
+                }
+            }
+
+            if is_border {
+                border_cells.push((i, j));
+            }
         }
 
-        vec![2, c - a - 2]
+        for (i, j) in border_cells {
+            grid[i][j] = color;
+        }
+
+        grid
     }
 }
 
