02076ccf529b8af40b67622b151dadaf439333b6
mspark777 <topangel777@gmail.com>
2026-09-01 15:49:16 +0900

    1073

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
 struct Solution;
 
 impl Solution {
-    pub fn rearrange_barcodes(barcodes: Vec<i32>) -> Vec<i32> {
-        let mut barcodes = barcodes;
-        let mut pos = 0;
-        let mut m = std::collections::HashMap::<i32, i32>::new();
-        let mut s = std::collections::BTreeSet::<(i32, i32)>::new();
+    pub fn add_negabinary(arr1: Vec<i32>, arr2: Vec<i32>) -> Vec<i32> {
+        use std::collections::vec_deque::VecDeque;
 
-        for n in barcodes.iter() {
-            *m.entry(*n).and_modify(|n| *n += 1).or_insert(1);
+        let mut deq1: VecDeque<_> = arr1.into();
+        let mut deq2: VecDeque<_> = arr2.into();
+        let mut result: VecDeque<i32> = VecDeque::new();
+        let mut carry: i32 = 0;
+        while !deq1.is_empty() || !deq2.is_empty() {
+            let i1: i32 = deq1.pop_back().unwrap_or(0);
+            let i2: i32 = deq2.pop_back().unwrap_or(0);
+            let i = i1 + i2 + (carry & 0b1);
+            result.push_front(i & 0b1);
+            carry = Self::new_carry(
+                carry,
+                match i > 1 {
+                    true => 0b11,
+                    _ => 0,
+                },
+            );
+        }
+        result.push_front(carry & 0b1);
+        result.push_front((carry >> 1) & 0b1);
+
+        while result.front() == Some(&0) && result.len() > 1 {
+            result.pop_front();
         }
 
-        for (&k, &v) in m.iter() {
-            s.insert((v, k));
-        }
+        result.into()
+    }
 
-        for (v, k) in s.into_iter().rev() {
-            for _ in 0..v {
-                if pos >= barcodes.len() {
-                    pos = 1;
-                }
-                barcodes[pos] = k;
-                pos += 2;
-            }
-        }
-        barcodes
+    fn new_carry(orig: i32, next: i32) -> i32 {
+        ((orig >> 1) + next) & 0b11
     }
 }
 
